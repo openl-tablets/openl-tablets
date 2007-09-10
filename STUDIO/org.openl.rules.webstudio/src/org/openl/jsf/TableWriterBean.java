3400b8d80e1327722a0bce7577d93472aacbbdfe
akazimirski <akazimirski@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2007-09-10 15:19:11 +0000

    EPBDS 21-22

--- STUDIO/org.openl.rules.webstudio/src/org/openl/jsf/TableWriterBean.java
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/jsf/TableWriterBean.java
@@ -1,12 +1,29 @@
 package org.openl.jsf;
 
+import java.io.IOException;
 import java.io.Writer;
+import java.util.Iterator;
 import java.util.Map;
 
 import javax.faces.component.UIComponent;
+import javax.faces.component.UIViewRoot;
+import javax.faces.component.html.HtmlOutputText;
 import javax.faces.context.FacesContext;
+import javax.faces.context.ResponseWriter;
 
-import org.openl.rules.ui.EditorHelper;
+import org.apache.myfaces.shared_impl.renderkit.html.HtmlResponseWriterImpl;
+import org.openl.rules.lang.xls.syntax.TableSyntaxNode;
+import org.openl.rules.table.IGrid;
+import org.openl.rules.table.IGridTable;
+import org.openl.rules.table.ILogicalTable;
+import org.openl.rules.table.ui.FilteredGrid;
+import org.openl.rules.table.ui.IGridFilter;
+import org.openl.rules.table.ui.SimpleHtmlFilter;
+import org.openl.rules.table.xls.SimpleXlsFormatter;
+import org.openl.rules.ui.CellModel;
+import org.openl.rules.ui.ICellModel;
+import org.openl.rules.ui.TableModel;
+import org.openl.rules.ui.TableViewer;
 import org.openl.rules.ui.WebStudio;
 import org.openl.rules.webtools.WebTool;
 
@@ -22,7 +39,7 @@ public class TableWriterBean {
 			}
 		}
 	}
-
+	protected TableModel tableModel;
 	protected int elementID;
 	protected String view;
 	protected String title;
@@ -49,6 +66,15 @@ public class TableWriterBean {
 		return (WebStudio)(getSessionMap().get("studio"));
 	}
 
+	protected HtmlOutputText createText(String text,String id,boolean escape) {
+		//
+		HtmlOutputText result = new HtmlOutputText();
+		result.setId(id);
+		result.setValue(text);
+		result.setEscape(escape);
+		return result;
+	}
+	
 	protected void initialize() {
 		//
 		WebStudio studio = getWebStudio();
@@ -84,15 +110,70 @@ public class TableWriterBean {
 		
 		System.out.println("elementID="+elementID);
 		System.out.println("view=" + view);
+		
+		initializeTableModel(elementID, view, studio);
+	}
+	
+	protected void modifyView2(UIViewRoot root) {
+		//
+		UIComponent spr = root.findComponent("spreadsheet");
+		spr.getChildren().clear();
+		spr.getChildren().add(createText("<table cellspacing=\"0\" cellpadding=\"0\" border=\"0\">\n", root.createUniqueId(), false));
+		for (int i=0; i < tableModel.getCells().length; i++) {
+			spr.getChildren().add(createText("<tr>\n", root.createUniqueId(), false));
+			for (int j=0; j < tableModel.getCells()[i].length; j++) {
+				ICellModel cell = tableModel.getCells()[i][j];
+				if ((null != cell) && (cell.isReal())) {
+					StringBuffer sb = new StringBuffer();
+					sb.append("<td");
+					if (cell instanceof CellModel) {
+						((CellModel)(cell)).atttributesToHtml(sb, tableModel);
+					}
+					
+					StringBuffer id = new StringBuffer();
+					for(int k=0;k<cell.getRowspan();k++) {
+						for(int l=0; l < cell.getColspan(); l++) {
+							id.append("cell-" + String.valueOf(i+k+1) + "-" + String.valueOf(j+l+1) + "_");
+						}
+					}
+					
+					sb.append(" title=\"").append(id).append("\">\n");
+					
+					spr.getChildren().add(createText(sb.toString(), root.createUniqueId(), false));
+					spr.getChildren().add(createText(cell.getContent(), id.append("text").toString(), false));
+					spr.getChildren().add(createText("</td>\n", root.createUniqueId(), false));
+				}
+			}
+			spr.getChildren().add(createText("</tr>\n", root.createUniqueId(), false));
+		}
+		spr.getChildren().add(createText("</table>", root.createUniqueId(), false));
 	}
 	
 	
 	public void render(Writer writer) {
 		//
-		TableWriter tw = new TableWriter(elementID,view,getWebStudio(),ajaxrequest);
-		setInitialRow(tw.getInitialRow());
-		setInitialColumn(tw.getInitialColumn());
-		tw.render(writer);
+//		TableWriter tw = new TableWriter(elementID,view,getWebStudio(),ajaxrequest);
+//		setInitialRow(tw.getInitialRow());
+//		setInitialColumn(tw.getInitialColumn());
+//		tw.render(writer);
+		//
+		FacesContext fc = FacesContext.getCurrentInstance();
+		ResponseWriter rw = fc.getResponseWriter();
+		UIViewRoot root = fc.getViewRoot();
+		try {
+			fc.setResponseWriter(new HtmlResponseWriterImpl(writer, "text/html", "UTF-8"));
+			//printComponent(root,"-");
+			//modifyView(root);
+			if (!ajaxrequest) {
+				modifyView2(root);
+			}
+			//printComponent(root, "-");
+			renderResponse(fc,root.findComponent("spreadsheet"));
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		} finally {
+			fc.setResponseWriter(rw);
+		}
 	}
 
 	public TableWriterBean() {
@@ -178,8 +259,50 @@ public class TableWriterBean {
 	public String getSid() {
 		return sid;
 	}
+	
+	protected void renderResponse(FacesContext context,UIComponent component) throws IOException {
+		//
+		component.encodeBegin(context);
+		if (component.getRendersChildren()) {
+			component.encodeChildren(context);
+		} else {
+			Iterator kids = component.getChildren().iterator();
+			while(kids.hasNext()) {
+				renderResponse(context, (UIComponent)(kids.next()));
+			}
+		}
+		component.encodeEnd(context);
+	}
 
 	
-	
-	
+	protected void initializeTableModel(int elementID,String view,WebStudio studio) {
+		//
+		//System.out.println("initializeTableModel");
+		//::studio.getModel().showTable(elementID, view)
+		TableSyntaxNode tsn = studio.getModel().getNode(elementID);
+		if (tsn == null) return; // table is not selected yet
+		IGridTable gt = tsn.getTable().getGridTable();
+		if (view == null) {view = studio.getMode().getTableMode();}
+		boolean showGrid = studio.getMode().showTableGrid();
+		if (view != null) {
+			ILogicalTable gtx = (ILogicalTable) tsn.getSubTables().get(view);
+			if (gtx != null) gt = gtx.getGridTable();
+		}
+		//::return showTable(gt, showGrid);
+		//::return showTable(gt, (IGridFilter[]) null, showgrid);
+		IGrid htmlGrid = gt.getGrid();
+		if (!(htmlGrid instanceof FilteredGrid))
+		{
+			int N = 2;
+			IGridFilter[] f1 = new IGridFilter[N];
+			f1[0] = new SimpleXlsFormatter();
+			f1[1] = new SimpleHtmlFilter();
+			htmlGrid = new FilteredGrid(gt.getGrid(), f1);
+		}
+
+		TableViewer tv = new TableViewer(htmlGrid, gt.getRegion());
+		tableModel = tv.buildModel();
+		
+		// return new TableViewer().showTable(gt, new ICellFilter[]{cellFilter});
+	}
 }
