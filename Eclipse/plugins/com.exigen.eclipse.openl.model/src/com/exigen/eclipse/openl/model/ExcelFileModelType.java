206fa6d4a20738997e8f824981e7f59422822025
jplams@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <jplams@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2007-09-19 15:09:26 +0000

    EPBDS Navigation to region whithin Excel file is implemented

---
+++ Eclipse/plugins/com.exigen.eclipse.openl.model/src/com/exigen/eclipse/openl/model/ExcelFileModelType.java
@@ -0,0 +1,17 @@
+package com.exigen.eclipse.openl.model;
+
+import java.util.Map;
+
+import com.exigen.eclipse.common.model.DefaultModelType;
+
+public class ExcelFileModelType extends DefaultModelType {
+	private static final long serialVersionUID = 1L;
+
+	public org.eclipse.emf.ecore.EObject createRootElement(Map arg0) {
+		return null;
+	}
+
+	public org.eclipse.emf.ecore.EClass getRootElementType() {
+		return null;
+	}
+}
