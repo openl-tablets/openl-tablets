93efb32a024a6450c4aa39fca2a1ecc83ed62700
jplams@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <jplams@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2007-09-20 08:12:25 +0000

    EPBDS Navigation to region whithin Excel file is implemented

--- Eclipse/plugins/com.exigen.eclipse.openl.model.editor/src/com/exigen/openl/model/openl/presentation/OpenlEditorPlugin.java
+++ Eclipse/plugins/com.exigen.eclipse.openl.model.editor/src/com/exigen/openl/model/openl/presentation/OpenlEditorPlugin.java
@@ -6,13 +6,14 @@
  */
 package com.exigen.openl.model.openl.presentation;
 
-import com.exigen.eclipse.common.model.edit.CommonEditPlugin;
-
 import org.eclipse.emf.common.EMFPlugin;
-
 import org.eclipse.emf.common.ui.EclipseUIPlugin;
-
 import org.eclipse.emf.common.util.ResourceLocator;
+import org.eclipse.jface.resource.ImageRegistry;
+import org.osgi.framework.BundleContext;
+
+import com.exigen.eclipse.common.model.edit.CommonEditPlugin;
+import com.exigen.eclipse.common.ui.CommonUi;
 
 /**
  * This is the central singleton for the Openl editor plugin.
@@ -21,6 +22,10 @@ import org.eclipse.emf.common.util.ResourceLocator;
  * @generated
  */
 public final class OpenlEditorPlugin extends EMFPlugin {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "com.exigen.eclipse.openl.model.editor";
+
 	/**
 	 * Keep track of the singleton.
 	 * <!-- begin-user-doc -->
@@ -36,6 +41,15 @@ public final class OpenlEditorPlugin extends EMFPlugin {
 	 * @generated
 	 */
 	private static Implementation plugin;
+
+	public static class Images {
+		public static final String LINK_16 = "icons/link_16.gif";
+
+		static void register(ImageRegistry imageRegistry) {
+			imageRegistry.put(LINK_16, CommonUi.imageDescriptorFromPlugin(
+					PLUGIN_ID, LINK_16));
+		}
+	}
 
 	/**
 	 * Create the instance.
@@ -91,6 +105,18 @@ public final class OpenlEditorPlugin extends EMFPlugin {
 			// Remember the static instance.
 			//
 			plugin = this;
+		}
+
+		@Override
+		public void start(BundleContext context) throws Exception {
+			super.start(context);
+			Images.register(getImageRegistry());
+		}
+
+		@Override
+		public void stop(BundleContext context) throws Exception {
+			getImageRegistry().dispose();
+			super.stop(context);
 		}
 	}
 
