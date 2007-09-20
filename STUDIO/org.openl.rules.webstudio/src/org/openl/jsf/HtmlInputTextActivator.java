e221c42a8cf9cc2f2adc14a25c542d35f87648ab
anaumenko@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <anaumenko@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2007-09-20 14:26:23 +0000

    EPBDS-1. Removed incorrect @Override annotations.

--- STUDIO/org.openl.rules.webstudio/src/org/openl/jsf/HtmlInputTextActivator.java
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/jsf/HtmlInputTextActivator.java
@@ -7,13 +7,12 @@ import javax.faces.context.FacesContext;
 import org.apache.myfaces.el.ValueBindingImpl;
 
 public class HtmlInputTextActivator implements ICellEditorActivator {
-	
-	@Override
-	public UIComponent createInstance(Object value, Object metadata) {
-		//
-		HtmlInputText result = new HtmlInputText();
-		result.setOnkeyup("javascript:if(13==event.keyCode){stopEditing2();};");
-		//result.setValue(value);
-		return result;
-	}
+
+    public UIComponent createInstance(Object value, Object metadata) {
+        //
+        HtmlInputText result = new HtmlInputText();
+        result.setOnkeyup("javascript:if(13==event.keyCode){stopEditing2();};");
+        //result.setValue(value);
+        return result;
+    }
 }
--- STUDIO/org.openl.rules.webstudio/src/org/openl/jsf/HtmlSelectActivator.java
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/jsf/HtmlSelectActivator.java
@@ -9,21 +9,20 @@ import javax.faces.component.html.HtmlSelectOneListbox;
 import org.openl.jsf.editor.metadata.IHtmlSelectMetadata;
 
 public class HtmlSelectActivator implements ICellEditorActivator {
-	
-	@Override
-	public UIComponent createInstance(Object value, Object metadata) {
-		//
-		HtmlSelectOneListbox result = new HtmlSelectOneListbox();
-		result.setOnchange("javascript:stopEditing2();");
-		result.setSize(1);
-		if (metadata instanceof IHtmlSelectMetadata) {
-			List l = ((IHtmlSelectMetadata)metadata).getList();
-			for (int i=0; i < l.size(); i++) {
-				UISelectItem item = new UISelectItem();
-				item.setItemValue(l.get(i));
-				result.getChildren().add(item);				
-			}
-		}
-		return result;
-	}
+
+    public UIComponent createInstance(Object value, Object metadata) {
+        //
+        HtmlSelectOneListbox result = new HtmlSelectOneListbox();
+        result.setOnchange("javascript:stopEditing2();");
+        result.setSize(1);
+        if (metadata instanceof IHtmlSelectMetadata) {
+            List l = ((IHtmlSelectMetadata)metadata).getList();
+            for (int i=0; i < l.size(); i++) {
+                UISelectItem item = new UISelectItem();
+                item.setItemValue(l.get(i));
+                result.getChildren().add(item);
+            }
+        }
+        return result;
+    }
 }
