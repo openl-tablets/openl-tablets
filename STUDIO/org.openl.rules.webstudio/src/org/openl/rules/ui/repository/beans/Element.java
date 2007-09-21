86db994ca78f2e2c9599554868e30341b289ecf3
abykhavets <abykhavets@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2007-09-21 07:57:06 +0000

    EPBDS-44 Integrate UI and logic
    1) Faces config files splitted on sub files
    2) JSF
    3) JSP + XHTML
    4) Added basic Repository/JCR UI and Beans

---
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/ui/repository/beans/Element.java
@@ -0,0 +1,32 @@
+package org.openl.rules.ui.repository.beans;
+
+public class Element {
+	private String name;
+	private String version;
+	private String lastModified;
+	private String lastModifiedBy;
+	
+	public Element(String n, String v, String lm, String usr) {
+		name = n;
+		version = v;
+		lastModified = lm;
+		lastModifiedBy = usr;
+	}
+
+	public String getLastModified() {
+		return lastModified;
+	}
+
+	public String getLastModifiedBy() {
+		return lastModifiedBy;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getVersion() {
+		return version;
+	}
+
+}
