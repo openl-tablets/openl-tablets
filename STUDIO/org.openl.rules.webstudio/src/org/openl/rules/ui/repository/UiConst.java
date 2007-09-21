22283f1f2ac95fd4d51d4846c06d21f7d2ae03fd
abykhavets@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <abykhavets@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2007-09-21 13:44:15 +0000

    EPBDS-44 Integrate UI and logic
    1) Changed CSS
    2) Updated markup
    3) Added Search screens

--- STUDIO/org.openl.rules.webstudio/src/org/openl/rules/ui/repository/UiConst.java
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/ui/repository/UiConst.java
@@ -11,12 +11,14 @@ public class UiConst {
 	public static final String TYPE_FILE    = "file";
 	public static final String TYPE_FOLDER  = "folder";
 	public static final String TYPE_PROJECT = "project";
+	public static final String TYPE_REPOSITORY = "repository";
 	
 	// <rich:treeNode icon="..." iconLeaf="..."
-	public static final String ICON_FILE    = "/images/tree/tree_document.gif";
-	public static final String ICON_FOLDER  = "/images/tree/tree_folder.gif";
-	public static final String ICON_PROJECT = "/images/tree/tree_server.gif";
+	public static final String ICON_FILE    = "/images/jcr/tree_document.gif";
+	public static final String ICON_FOLDER  = "/images/jcr/tree_folder.gif";
+	public static final String ICON_PROJECT = "/images/jcr/tree_server.gif";
+	public static final String ICON_REPOSITORY = "/images/jcr/tree_storage.gif";
 
-	public static final String ICON_FOLDER_MOD  = "/images/tree/tree_folder_major.gif";
-	public static final String ICON_PROJECT_MOD = "/images/tree/tree_server_major.gif";
+	public static final String ICON_FOLDER_MOD  = "/images/jcr/tree_folder_major.gif";
+	public static final String ICON_PROJECT_MOD = "/images/jcr/tree_server_major.gif";
 }
