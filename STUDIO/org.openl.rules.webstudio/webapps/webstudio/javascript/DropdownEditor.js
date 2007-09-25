ac142a0dc96d727fef596dc94922a1feb4d87cad
anaumenko <anaumenko@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2007-09-25 16:16:16 +0000

    EPBDS-1. Improved UI.

--- STUDIO/org.openl.rules.webstudio/webapps/webstudio/javascript/DropdownEditor.js
+++ STUDIO/org.openl.rules.webstudio/webapps/webstudio/javascript/DropdownEditor.js
@@ -6,43 +6,43 @@
 var DropdownEditor = Prototype.emptyFunction;
 
 DropdownEditor.prototype = Object.extend(new BaseEditor(), {
-	/** Constructor */
-	initialize : function() {
-		this.node = document.createElement("select");
-		this.node.style.width = "100%";
-		this.node.style.border = "0px none";
-		this.node.style.margin = "0px";
-		this.node.style.padding = "0px";
-		
-		var self = this;
-		$H({
-			"": "-- Select a value --",
-			"Item 1": "Mazda",
-			"Item 2": "Opel",
-			"Item 3": "Citroen",
-			"Item 4": "Subaru"
-		}).each(function (pair) {
-			self.addOption(pair.key, pair.value);
-		});
-	},
+  /** Constructor */
+  initialize : function(parent) {
+    this.node = document.createElement("select");
+    this.node.style.width = "100%";
+    this.node.style.border = "0px none";
+    this.node.style.margin = "0px";
+    this.node.style.padding = "0px";
 
-	/**
-	 *  @desc add an option element to this select
-	 *  @type public
-	 */
-	addOption : function(/* String */ value, /* String */ name) {
-		var optionElement = document.createElement("option");
-		optionElement.value = value;
-		optionElement.innerHTML = name;
-		this.node.appendChild(optionElement);
-	},
+    var self = this;
+    $H({
+      "": "-- Select a value --",
+      "Item 1": "Mazda",
+      "Item 2": "Opel",
+      "Item 3": "Citroen",
+      "Item 4": "Subaru"
+    }).each(function (pair) {
+      self.addOption(pair.key, pair.value);
+    });
+  },
 
-	/**
-	 * @desc overrides base class implementation to support situation when nothing is selected.
-	 */
-	isCancelled: function() {
-		return this.node.value == "";	
-	}
+  /**
+   *  @desc add an option element to this select
+   *  @type public
+   */
+  addOption : function(/* String */ value, /* String */ name) {
+    var optionElement = document.createElement("option");
+    optionElement.value = value;
+    optionElement.innerHTML = name;
+    this.node.appendChild(optionElement);
+  },
+
+  /**
+   * @desc overrides base class implementation to support situation when nothing is selected.
+   */
+  isCancelled: function() {
+    return this.node.value == "";
+  }
 });
 
 TableEditor.Editors["selectbox"] = DropdownEditor.prototype;
--- STUDIO/org.openl.rules.webstudio/webapps/webstudio/javascript/TextEditor.js
+++ STUDIO/org.openl.rules.webstudio/webapps/webstudio/javascript/TextEditor.js
@@ -6,31 +6,37 @@
 var TextEditor = Prototype.emptyFunction;
 
 TextEditor.prototype = Object.extend(new BaseEditor(), {
-	  eventHandler : null,
-	 /** Constructor */
-    initialize : function() {
-		  this.node = $(document.createElement("input"));
+    eventHandler : null,
+   /** Constructor */
+    initialize : function(parent) {
+      this.node = $(document.createElement("input"));
         this.node.setAttribute("type", "text");
         this.node.style.border = "0px none";
-        this.node.style.height = "100%";
+        this.node.style.height = (parent.offsetHeight - (Prototype.Browser.IE ? 6 : 4)) + "px";
+
+        this.node.style.fontFamily = parent.style.fontFamily;
+        this.node.style.fontSize = parent.style.fontSize;
+        this.node.style.fontStyle = parent.style.fontStyle;
+        this.node.style.fontWeight = parent.style.fontWeight;
+
         this.node.style.margin = "0px";
         this.node.style.padding = "0px";
         this.node.style.width = "100%";
 
-		 this.eventHandler = this.handleKeyPress.bindAsEventListener(this);
-		 Event.observe(this.node, "keyup", this.eventHandler);
-	 },
+     this.eventHandler = this.handleKeyPress.bindAsEventListener(this);
+     Event.observe(this.node, "keyup", this.eventHandler);
+   },
 
-	 handleKeyPress: function (event) {
-		 switch (event.keyCode) {
-		 case 27: this.cancelEdit(); break;
-		 case 13: this.doneEdit(); break;
-		 }
-	 },
+   handleKeyPress: function (event) {
+     switch (event.keyCode) {
+     case 27: this.cancelEdit(); break;
+     case 13: this.doneEdit(); break;
+     }
+   },
 
-	 destroy: function() {
-		 Event.stopObserving(this.node,  "keyup", this.eventHandler);
-	 }
+   destroy: function() {
+     Event.stopObserving(this.node,  "keyup", this.eventHandler);
+   }
 });
 
 TableEditor.Editors["inputbox"] = TextEditor.prototype;
