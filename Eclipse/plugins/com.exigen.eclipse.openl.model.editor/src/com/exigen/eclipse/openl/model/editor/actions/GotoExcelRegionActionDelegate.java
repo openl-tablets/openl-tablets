50c18eff30ce1fbd972fcbefbb98ea6bd700e172
ahristoforovs <ahristoforovs@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2007-09-25 16:29:57 +0000

    EPBDS Added composite parameter support

--- Eclipse/plugins/com.exigen.eclipse.openl.model.editor/src/com/exigen/eclipse/openl/model/editor/actions/GotoExcelRegionActionDelegate.java
+++ Eclipse/plugins/com.exigen.eclipse.openl.model.editor/src/com/exigen/eclipse/openl/model/editor/actions/GotoExcelRegionActionDelegate.java
@@ -3,7 +3,7 @@ package com.exigen.eclipse.openl.model.editor.actions;
 import java.lang.reflect.Method;
 
 import org.eclipse.core.resources.IFile;
-import org.eclipse.emf.common.util.URI;
+import org.eclipse.core.resources.IStorage;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
@@ -16,13 +16,10 @@ import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.ide.IDE;
 
+import com.exigen.eclipse.common.core.artefact.project.facet.JavaFacet;
 import com.exigen.eclipse.common.core.exception.CommonException;
-import com.exigen.eclipse.common.facet.emf.CommonFacetEmf;
 import com.exigen.eclipse.common.facet.emf.artefact.DomainModel;
-import com.exigen.eclipse.common.facet.emf.artefact.ModelingFacet;
 import com.exigen.eclipse.common.facet.emf.edit.provider.ProviderUtils;
-import com.exigen.eclipse.common.facet.emf.lookup.LookupScope;
-import com.exigen.eclipse.common.facet.emf.manager.ModelState;
 import com.exigen.eclipse.common.ui.util.exception.UiExceptionHandler;
 import com.exigen.openl.model.openl.RuleSet;
 import com.exigen.openl.model.openl.RuleSetFile;
@@ -53,62 +50,71 @@ public class GotoExcelRegionActionDelegate implements IActionDelegate {
 		}
 
 		try {
-			ModelingFacet modelingFacet = domainModel.getProject().getFacet(
-					ModelingFacet.class);
-			LookupScope scope = (modelingFacet.getCommonResourceSet()
-					.getCommonURIConverter().getLookupScope());
-			ModelState modelState = CommonFacetEmf.getModelManager().getState(
-					scope);
-			DomainModel excelFileModel = modelState.getModelByUri(URI
-					.createURI(ruleSetFile.getExcelResourceReference()));
-			if ((excelFileModel == null) || !excelFileModel.exists()) {
+			JavaFacet facet = domainModel.getProject()
+					.getFacet(JavaFacet.class);
+
+			
+			Object excelResource = facet.findNonJavaResource(ruleSetFile
+					.getExcelResourceReference());
+			if (excelResource == null) {
 				throw new CommonException("File \""
 						+ ruleSetFile.getExcelResourceReference()
 						+ "\" is not found");
 			}
+			if (excelResource instanceof IFile) {
+				IFile file = (IFile) excelResource;
+				if (!file.exists())
+					throw new CommonException("File \""
+							+ ruleSetFile.getExcelResourceReference()
+							+ "\" is not found");
+				IEditorPart editor = IDE.openEditor(workbenchPage, file,
+						"org.eclipse.ui.systemInPlaceEditor");
 
-			IFile file = excelFileModel.getCorrespondingFile();
-			IEditorPart editor = IDE.openEditor(workbenchPage, file,
-					"org.eclipse.ui.systemInPlaceEditor");
-			if (ruleSet != null) {
-				if (editor
-						.getClass()
-						.getName()
-						.equals(
-								"org.eclipse.ui.internal.editorsupport.win32.OleEditor")) {
-					Method method = editor.getClass().getMethod(
-							"getClientSite", new Class[0]);
-					OleClientSite oleClientSite = (OleClientSite) method
-							.invoke(editor, new Object[0]);
-					if (oleClientSite == null) {
-						throw new CommonException("Cannot access Excell editor");
-					}
-
-					OleAutomation oleAutomation = new OleAutomation(
-							oleClientSite);
-					try {
-						int[] dispIdArray = oleAutomation
-								.getIDsOfNames(new String[] { "Application" });
-						Variant application = oleAutomation
-								.getProperty(dispIdArray[0]);
-						try {
-							dispIdArray = application.getAutomation()
-									.getIDsOfNames(
-											new String[] { "Goto", "Reference",
-													"Scroll" });
-							application.getAutomation().invoke(
-									dispIdArray[0],
-									new Variant[] {
-											new Variant(ruleSet.getName()),
-											new Variant(true) });
-						} finally {
-							application.dispose();
+				if (ruleSet != null) {
+					if (editor
+							.getClass()
+							.getName()
+							.equals(
+									"org.eclipse.ui.internal.editorsupport.win32.OleEditor")) {
+						Method method = editor.getClass().getMethod(
+								"getClientSite", new Class[0]);
+						OleClientSite oleClientSite = (OleClientSite) method
+								.invoke(editor, new Object[0]);
+						if (oleClientSite == null) {
+							throw new CommonException(
+									"Cannot access Excell editor");
 						}
-					} finally {
-						oleAutomation.dispose();
+
+						OleAutomation oleAutomation = new OleAutomation(
+								oleClientSite);
+						try {
+							int[] dispIdArray = oleAutomation
+									.getIDsOfNames(new String[] { "Application" });
+							Variant application = oleAutomation
+									.getProperty(dispIdArray[0]);
+							try {
+								dispIdArray = application
+										.getAutomation()
+										.getIDsOfNames(
+												new String[] { "Goto",
+														"Reference", "Scroll" });
+								application.getAutomation().invoke(
+										dispIdArray[0],
+										new Variant[] {
+												new Variant(ruleSet.getName()),
+												new Variant(true) });
+							} finally {
+								application.dispose();
+							}
+						} finally {
+							oleAutomation.dispose();
+						}
 					}
 				}
+			} else if (excelResource instanceof IStorage) {
+				throw new CommonException("Non file input is not supported");
 			}
+
 		} catch (CommonException e) {
 			UiExceptionHandler.handleCommonExceptionOnUserAction(e,
 					"Cannot open Excel editor");
