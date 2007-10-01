c516ea422ffca2851bfb2f972fe44d52be99a8b5
anaumenko@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <anaumenko@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2007-10-01 16:40:57 +0000

    EPBDS-1. Adding initial Price and Numeric editors.

---
+++ STUDIO/org.openl.rules.webstudio/webapps/webstudio/javascript/NumericEditor.js
@@ -0,0 +1,41 @@
+/**
+ * Numeric editor.
+ *
+ * @author Andrey Naumenko
+ */
+var NumericEditor = Class.create();
+
+NumericEditor.prototype = Object.extend(new BaseEditor(), {
+    editor_initialize: function() {
+        this.node = $(document.createElement("input"));
+        this.node.setAttribute("type", "text");
+        this.node.style.border = "0px none";
+        this.node.style.height = (this.td.offsetHeight - (Prototype.Browser.IE ? 6 : 4)) + "px";
+
+        this.node.style.fontFamily = this.td.style.fontFamily;
+        this.node.style.fontSize = this.td.style.fontSize;
+        this.node.style.fontStyle = this.td.style.fontStyle;
+        this.node.style.fontWeight = this.td.style.fontWeight;
+        this.node.style.textAlign = this.td.align;
+
+        this.node.style.margin = "0px";
+        this.node.style.padding = "0px";
+        this.node.style.width = "100%";
+
+        this.node.value = this.td.innerHTML.replace(/&nbsp;/g, " ");
+
+        this.node.observe("click", BaseEditor.stopPropagationHandler, false);
+        this.node.observe("mousedown", BaseEditor.stopPropagationHandler, false);
+        this.node.observe("selectstart", BaseEditor.stopPropagationHandler, false);
+
+        this.td.innerHTML = "";
+        this.td.appendChild(this.node);
+        this.node.focus();
+    },
+
+    isCancelled : function() {
+        return (this.initialValue == this.getValue() || isNaN(this.getValue()));
+    }
+});
+
+TableEditor.Editors["numeric"] = NumericEditor;
