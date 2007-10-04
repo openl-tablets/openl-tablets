58a85486f945b962d1b0c02d94256a8cd2805746
jplams <jplams@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2007-10-04 09:36:59 +0000

    EPBDS Impoved work with classloader and file manager

--- Runtime/source/com.exigen.openl.model/src/main/java/com/exigen/openl/component/OpenInstance.java
+++ Runtime/source/com.exigen.openl.model/src/main/java/com/exigen/openl/component/OpenInstance.java
@@ -4,14 +4,19 @@ import org.openl.types.IOpenClass;
 import org.openl.vm.IRuntimeEnv;
 
 public class OpenInstance {
+	ClassLoader classLoader;
 	IOpenClass openClass;
 	Object instance;
 	IRuntimeEnv env;
-	public OpenInstance(IOpenClass openClass, Object instance, IRuntimeEnv env) {
+	public OpenInstance(ClassLoader classLoader, IOpenClass openClass, Object instance, IRuntimeEnv env) {
 		super();
+		this.classLoader = classLoader;
 		this.openClass = openClass;
 		this.instance = instance;
 		this.env = env;
+	}
+	public ClassLoader getClassLoader() {
+		return classLoader;
 	}
 	public Object getInstance() {
 		return instance;
