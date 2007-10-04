67d4284b62b8f02b5fce2644df7ffa80e2a2287f
abykhavets@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <abykhavets@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2007-10-04 09:44:45 +0000

    EPBDS-25 Implement JCR approve of concept
    Support of JCR-170 repositories:
    - Apache Jackrabbit
    - Exigen JCR

---
+++ STUDIO/org.openl.rules.repository.jcr/src/org/openl/rules/repository/jcr/JcrEntity.java
@@ -0,0 +1,115 @@
+package org.openl.rules.repository.jcr;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.jcr.Node;
+import javax.jcr.RepositoryException;
+import javax.jcr.version.Version;
+import javax.jcr.version.VersionHistory;
+import javax.jcr.version.VersionIterator;
+
+import org.openl.rules.repository.REntity;
+import org.openl.rules.repository.RVersion;
+import org.openl.rules.repository.exceptions.RModifyException;
+import org.openl.rules.repository.exceptions.RRepositoryException;
+import org.openl.rules.repository.exceptions.RDeleteException;
+
+/**
+ * Implementation of JCR Entity.
+ * It is linked with node in JCR implementation, always.
+ * 
+ * @author Aleh Bykhavets
+ *
+ */
+public class JcrEntity implements REntity {
+
+    /** node in JCR that corresponds to this entity */
+    private Node node;
+    private String name;
+
+    public JcrEntity(Node node) throws RepositoryException {
+        this.node = node;
+        name = node.getName();
+    }
+
+    /** {@inheritDoc} */
+    public String getName() {
+        return name;
+    }
+
+    /** {@inheritDoc} */
+    public List<RVersion> getVersionHistory() throws RRepositoryException {
+        try {
+            VersionHistory vh = node().getVersionHistory();
+            VersionIterator vi = vh.getAllVersions();
+            LinkedList<RVersion> result = new LinkedList<RVersion>();
+            while (vi.hasNext()) {
+                Version v = vi.nextVersion();
+
+                if (NodeUtil.isRootVersion(v)) {
+                    //TODO Shall we add first (0) version? (It is marker like, no real values)
+                } else {
+                    JcrVersion jvi = new JcrVersion(v);
+                    result.add(jvi);
+                }
+            }
+            return result;
+        } catch (RepositoryException e) {
+            throw new RRepositoryException("Failed to get Version History", e);
+        }        
+    }
+
+    // ------ protected methods ------
+
+    /**
+     * Returns node in JCR that this entity is mapped on. 
+     * 
+     * @return corresponding JCR node
+     */
+    protected Node node() {
+        return node;
+    }
+
+    /**
+     * Checks whether type of the JCR node is correct.
+     * 
+     * @param nodeType expected node type
+     * @throws RepositoryException if failed
+     */
+    protected void checkNodeType(String nodeType) throws RepositoryException {
+        if (!node.isNodeType(nodeType)) {
+            throw new RepositoryException("Invalid NodeType. Expects " + nodeType);
+        }
+    }
+
+    public RVersion getBaseVersion() {
+        try {
+            Version v = node().getBaseVersion();
+            RVersion result = new JcrVersion(v);
+            return result;
+        } catch (RepositoryException e) {
+            e.printStackTrace();
+            return null;
+        }        
+    }
+
+    public REntity getParent() {
+        // TODO FIX ME!!!
+        return null;
+    }
+
+    public void setName(String name) throws RModifyException {
+        // TODO: FIX ME!!!
+    }
+
+    /** {@inheritDoc} */
+    public void delete() throws RDeleteException {
+        try {
+            Node n = node();
+            n.remove();
+        } catch (RepositoryException e) {
+            throw new RDeleteException("Failed to Delete", e);
+        }
+    }
+}
---
+++ STUDIO/org.openl.rules.repository.jcr/src/org/openl/rules/repository/jcr/JcrVersion.java
@@ -0,0 +1,57 @@
+package org.openl.rules.repository.jcr;
+
+import java.util.Date;
+
+import javax.jcr.Node;
+import javax.jcr.RepositoryException;
+import javax.jcr.version.Version;
+
+import org.openl.rules.repository.RUser;
+import org.openl.rules.repository.RVersion;
+
+/**
+ * Implements JCR Version.
+ * 
+ * @author Aleh Bykhavets
+ *
+ */
+public class JcrVersion implements RVersion {
+    /** JCR Version */
+    private Version version;
+
+    // temporary variables, just to reduce 'throws' for getters
+    private String versionName;
+    private Date lastModified;
+    private String modifiedBy;
+
+    public JcrVersion(Version version) throws RepositoryException {
+        this.version = version;
+
+        // storing node's properties into variables to reduce 'throws' for getters
+        versionName = version.getName();
+        Node frozen = version.getNode("jcr:frozenNode");
+
+        lastModified = version.getProperty("jcr:created").getDate().getTime();
+//      if (frozen.hasProperty(JcrNT.PROP_MODIFIED_TIME)) {
+//      lastModified = frozen.getProperty(JcrNT.PROP_MODIFIED_TIME).getDate().getTime();
+//      }
+        if (frozen.hasProperty(JcrNT.PROP_MODIFIED_BY)) { 
+            modifiedBy = frozen.getProperty(JcrNT.PROP_MODIFIED_BY).getString();
+        }
+    }
+
+    /** {@inheritDoc} */
+    public String getName() {
+        return versionName;
+    }
+
+    /** {@inheritDoc} */
+    public Date getCreated() {
+        return lastModified;
+    }
+
+    /** {@inheritDoc} */
+    public RUser getCreatedBy() {
+        return new JcrUser(modifiedBy);
+    }
+}
