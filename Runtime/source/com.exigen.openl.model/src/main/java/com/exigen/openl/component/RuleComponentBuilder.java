39d034c7c0c9d50159206b5c78193117a37c2fe7
jplams@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <jplams@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2007-10-05 12:25:43 +0000

    EPBDS Performance is improved

--- Runtime/source/com.exigen.openl.model/src/main/java/com/exigen/openl/component/RuleComponentBuilder.java
+++ Runtime/source/com.exigen.openl.model/src/main/java/com/exigen/openl/component/RuleComponentBuilder.java
@@ -1,5 +1,6 @@
 package com.exigen.openl.component;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -26,28 +27,78 @@ public class RuleComponentBuilder extends AbstractJava implements ComponentBuild
 	public static String OPENL_NAME = "org.openl.xls";
 	public static String USER_HOME = ".";
 
-
-	public ComponentReference createComponent(ComponentDefinition componentDefinition,
-			List<ParameterValue> params,
-			Map<String, Map<String, Object>> context) throws ComponentInstantiationException {
+	class Key {
+		MFileManager fileManager;
+		String fileName;
+		public Key(MFileManager fileManager, String fileName) {
+			super();
+			this.fileManager = fileManager;
+			this.fileName = fileName;
+		}
+		@Override
+		public int hashCode() {
+			final int PRIME = 31;
+			int result = 1;
+			result = PRIME * result + ((fileManager == null) ? 0 : fileManager.hashCode());
+			result = PRIME * result + ((fileName == null) ? 0 : fileName.hashCode());
+			return result;
+		}
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			final Key other = (Key) obj;
+			if (fileManager == null) {
+				if (other.fileManager != null)
+					return false;
+			} else if (!fileManager.equals(other.fileManager))
+				return false;
+			if (fileName == null) {
+				if (other.fileName != null)
+					return false;
+			} else if (!fileName.equals(other.fileName))
+				return false;
+			return true;
+		}
 		
+		
+	}
+	
+	private static final Map<Key, OpenClassJavaWrapper> cache = new HashMap<Key, OpenClassJavaWrapper>();
+
+	public ComponentReference createComponent(
+			ComponentDefinition componentDefinition,
+			List<ParameterValue> params,
+			Map<String, Map<String, Object>> context)
+			throws ComponentInstantiationException {
+
 		Assert.isTrue(componentDefinition instanceof RuleSetFile);
 		RuleSetFile ruleSetFile = (RuleSetFile) componentDefinition;
-		String fileName = ExcelUtil.getFileName( ruleSetFile);
-//		ClassLoader classLoader = getClassLoader(context);
+		String fileName = ExcelUtil.getFileName(ruleSetFile);
+		// ClassLoader classLoader = getClassLoader(context);
 
 		MFileManager fileManager = ComponentContextHelper
 				.getFileManager(context);
-		fileManager.loadAll();
 		ClassLoader classLoader = ClassLoaderUtil
 				.getFileManagerClassLoader(fileManager);
-
-		OpenClassJavaWrapper wrapper = loadClass(classLoader,fileName);
+		Key key = new Key(fileManager, fileName);
+		OpenClassJavaWrapper wrapper = cache.get(key);
+		if (wrapper == null) {
+			fileManager.loadAll();
+			wrapper = loadClass(classLoader, fileName);
+			cache.put(key, wrapper);
+		}
 		IOpenClass clazz = wrapper.getOpenClassWithErrors();
 		Object instance = clazz.newInstance(wrapper.getEnv());
-		OpenInstance openInstance = new OpenInstance(classLoader,clazz,instance,wrapper.getEnv());
+		OpenInstance openInstance = new OpenInstance(classLoader, clazz,
+				instance, wrapper.getEnv());
 		return new HolderComponentReference(openInstance);
 	}
+
 	public OpenClassJavaWrapper loadClass(ClassLoader classLoader, String fileName){
 		ClassLoader currentClassLoader = Thread.currentThread().getContextClassLoader();
 		Thread.currentThread().setContextClassLoader(classLoader);
