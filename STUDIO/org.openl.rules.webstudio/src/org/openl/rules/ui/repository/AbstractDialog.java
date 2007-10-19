1f02d34164d5c91b7f0f75452f66bb5f4daa43ff
abykhavets <abykhavets@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2007-10-19 15:16:54 +0000

    EPBDS-44 Integrate UI and logic
    Adding/Updating File

---
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/ui/repository/AbstractDialog.java
@@ -0,0 +1,27 @@
+package org.openl.rules.ui.repository;
+
+public abstract class AbstractDialog {
+    private Context context;
+    
+    public void setContext(Context context) {
+        this.context = context;
+    }
+    
+    protected Context getContext() {
+        return context;
+    }
+
+    /**
+     * Returns string for outcome.
+     * 
+     * @param success whether operation was successful or not.
+     * @return outcome: "success" or "fail"
+     */
+    protected String outcome(boolean success) {
+        return (success ? "success" : "fail");
+    }
+
+    protected void refresh() {
+        context.refresh();
+    }
+}
