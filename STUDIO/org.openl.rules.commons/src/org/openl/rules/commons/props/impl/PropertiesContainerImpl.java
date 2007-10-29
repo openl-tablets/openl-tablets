20675a0c7d9fff48b2fdf3e148709002ca40201e
abykhavets <abykhavets@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2007-10-29 16:44:29 +0000

    EPBDS-95 Common Interfaces for different layers
    Added java-docs
    Changed PropertiesContainer.removeProperty method

--- STUDIO/org.openl.rules.commons/src/org/openl/rules/commons/props/impl/PropertiesContainerImpl.java
+++ STUDIO/org.openl.rules.commons/src/org/openl/rules/commons/props/impl/PropertiesContainerImpl.java
@@ -5,17 +5,22 @@ import org.openl.rules.commons.props.*;
 import java.util.Collection;
 import java.util.HashMap;
 
+/**
+ * Implementation of Properties Container
+ */
 public class PropertiesContainerImpl implements PropertiesContainer {
-    protected HashMap<String, Property> properties;
+    private HashMap<String, Property> properties;
 
     public PropertiesContainerImpl() {
         properties = new HashMap<String, Property>();
     }
 
+    /** {@inheritDoc} */
     public boolean hasProperty(String name) {
         return properties.containsKey(name);
     }
 
+    /** {@inheritDoc} */
     public Property getProperty(String name) throws PropertyException {
         Property property = properties.get(name);
         if (property == null) {
@@ -25,17 +30,21 @@ public class PropertiesContainerImpl implements PropertiesContainer {
         return property;
     }
 
+    /** {@inheritDoc} */
     public Collection<Property> getProperties() {
         return properties.values();
     }
 
+    /** {@inheritDoc} */
     public void addProperty(Property property) throws PropertyTypeException {
         String name = property.getName();
         Property existing = properties.get(name);
 
         if (existing == null) {
+            // add if there is no prop with such name
             properties.put(name, property);
         } else {
+            // smart update
             switch (property.getType()) {
                 case DATE:
                     existing.setValue(property.getDate());
@@ -49,7 +58,12 @@ public class PropertiesContainerImpl implements PropertiesContainer {
         }
     }
 
-    public void removeProperty(String name) {
+    /** {@inheritDoc} */
+    public Property removeProperty(String name) throws PropertyException {
+        // throws exception if no prop with such name
+        Property prop = getProperty(name);
+
         properties.remove(name);
+        return prop;
     }
 }
