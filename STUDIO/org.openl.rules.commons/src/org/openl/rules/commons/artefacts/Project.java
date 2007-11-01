affc803de3cc01b487ea87621d787d0eb4bed5e3
abykhavets <abykhavets@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2007-11-01 14:12:49 +0000

    EPBDS-95 Common Interfaces for different layers
    removing

--- STUDIO/org.openl.rules.commons/src/org/openl/rules/commons/artefacts/Project.java
+++
@@ -1,8 +0,0 @@
-package org.openl.rules.commons.artefacts;
-
-public interface Project extends Artefact {
-    Workspace getWorkspace() throws ArtefactException;
-    
-    ArtefactPath getArtefactPath(Artefact artefact) throws ArtefactException;
-    Artefact findArtefactByPath(ArtefactPath artefactPath) throws ArtefactException;
-}
