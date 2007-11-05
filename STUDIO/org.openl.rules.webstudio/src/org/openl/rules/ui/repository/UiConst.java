9ac02709d425da30ea64dd764f0d7d96af095394
abykhavets <abykhavets@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2007-11-05 17:17:23 +0000

    EPBDS-44 Integrate UI and logic
    Add new Project via UserWorkspace

--- STUDIO/org.openl.rules.webstudio/src/org/openl/rules/ui/repository/UiConst.java
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/ui/repository/UiConst.java
@@ -7,20 +7,21 @@ package org.openl.rules.ui.repository;
  *
  */
 public class UiConst {
-	// <rich:treeNode type="..."
-	public static final String TYPE_FILE    = "file";
-	public static final String TYPE_FOLDER  = "folder";
-	public static final String TYPE_PROJECT = "project";
-	public static final String TYPE_REPOSITORY = "repository";
-	
-	// <rich:treeNode icon="..." iconLeaf="..."
-	public static final String ICON_FILE    = "/images/jcr/tree_document.gif";
-	public static final String ICON_FOLDER  = "/images/jcr/tree_folder.gif";
-	public static final String ICON_PROJECT = "/images/jcr/tree_server.gif";
-	public static final String ICON_REPOSITORY = "/images/jcr/tree_storage.gif";
+    // <rich:treeNode type="..."
+    public static final String TYPE_FILE    = "file";
+    public static final String TYPE_FOLDER  = "folder";
+    public static final String TYPE_PROJECT = "project";
+    public static final String TYPE_REPOSITORY = "repository";
 
-	public static final String ICON_FOLDER_MOD  = "/images/jcr/tree_folder_major.gif";
-	public static final String ICON_PROJECT_MOD = "/images/jcr/tree_server_major.gif";
+    // <rich:treeNode icon="..." iconLeaf="..."
+    public static final String ICON_FILE    = "/images/jcr/tree_document.gif";
+    public static final String ICON_FOLDER  = "/images/jcr/tree_folder.gif";
+    public static final String ICON_PROJECT = "/images/jcr/tree_server.gif";
+    public static final String ICON_REPOSITORY = "/images/jcr/tree_storage.gif";
+
+    public static final String ICON_FOLDER_MOD  = "/images/jcr/tree_folder_major.gif";
+    public static final String ICON_PROJECT_MOD = "/images/jcr/tree_server_major.gif";
 
     public static final String OUTCOME_SUCCESS = "success";
+    public static final String OUTCOME_FAILED = "failed";
 }
--- STUDIO/org.openl.rules.webstudio/src/org/openl/rules/ui/repository/handlers/RepositoryContoller.java
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/ui/repository/handlers/RepositoryContoller.java
@@ -1,9 +1,15 @@
 package org.openl.rules.ui.repository.handlers;
 
 import org.openl.rules.ui.repository.RepositoryTreeController;
+import org.openl.rules.ui.repository.UiConst;
 import org.openl.rules.ui.repository.tree.AbstractTreeNode;
+import org.openl.rules.workspace.abstracts.ProjectException;
+import org.openl.util.Log;
 
 import java.util.List;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 
 /**
  * Repository Handler.
@@ -18,6 +24,9 @@ public class RepositoryContoller {
      */
     private RepositoryTreeController repositoryTree;
 
+    // Add new Project
+    private String newProjectName;
+
     /**
      * Gets all projects from a rule repository.
      * 
@@ -27,12 +36,29 @@ public class RepositoryContoller {
         return repositoryTree.getRepositoryNode().getChildNodes();
     }
 
-    // todo: implement
-    public boolean addProject(String newProjectName) {
-        boolean result = true;
-        
+    public void setNewProjectName(String newProjectName) {
+        this.newProjectName = newProjectName;
+    }
+    
+    public String getNewProjectName() {
+        // expect null here
+        return newProjectName;
+    }
 
-        return result;
+    public String addProject() {
+        boolean result = false;
+        
+        try {
+            repositoryTree.getUserWorkspace().createProject(newProjectName);
+            repositoryTree.reInit();
+            result = true;
+        } catch (ProjectException e) {
+            Log.error("Failed to create new project", e);
+            
+            FacesContext.getCurrentInstance().addMessage(null,
+                new FacesMessage("Failed to create new project", e.getMessage()));
+        }        
+        return (result) ? UiConst.OUTCOME_SUCCESS : UiConst.OUTCOME_FAILED;
     }
 
     // todo: implement
