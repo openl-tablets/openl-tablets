fd106e80bf8faa400d8a84ccc36cf4c5dc031e8b
anaumenko <anaumenko@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2007-11-12 10:44:15 +0000

    EPBDS-1. Minor fixes.

---
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/util/FormatPatterns.java
@@ -0,0 +1,17 @@
+package org.openl.rules.webstudio.util;
+
+/**
+ * Contains default format patterns.
+ *
+ * @author Andrey Naumenko
+ */
+public class FormatPatterns {
+    /**
+     * Return date pattern.
+     *
+     * @return
+     */
+    public String getDatePattern() {
+        return "MM/dd/yyyy";
+    }
+}
