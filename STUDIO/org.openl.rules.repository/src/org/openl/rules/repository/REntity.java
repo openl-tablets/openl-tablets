37e83716eaca0cb8a5bebb8ba41e4fb8454783e5
abykhavets@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <abykhavets@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2007-11-12 11:15:08 +0000

    EPBDS-95 Common Interfaces for different layers
    set/get expiration date, effective date, LOB

--- STUDIO/org.openl.rules.repository/src/org/openl/rules/repository/REntity.java
+++ STUDIO/org.openl.rules.repository/src/org/openl/rules/repository/REntity.java
@@ -5,6 +5,7 @@ import org.openl.rules.repository.exceptions.RModifyException;
 import org.openl.rules.repository.exceptions.RRepositoryException;
 
 import java.util.Collection;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -53,6 +54,57 @@ public interface REntity {
      * @throws RRepositoryException if failed
      */
     public String getPath() throws RRepositoryException;
+    
+    /**
+     * Gets effective date for rules entity.
+     * If effective date isn't set method returns <code>null</code>
+     * 
+     * @return effective date or <code>null</code>
+     */
+    public Date getEffectiveDate();
+
+    /**
+     * Gets expiration date for rules entity.
+     * If expiration date isn't set method returns <code>null</code>
+     * 
+     * @return expiration date or <code>null</code>
+     */
+    public Date getExpirationDate();
+
+    /**
+     * Gets line of business for rules entity.
+     * If line of business isn't set method returns <code>null</code>
+     * 
+     * @return line of business or <code>null</code>
+     */
+    public String getLineOfBusiness();
+    
+    /**
+     * Sets effective date for rules entity.
+     * Effective date can be disabled if <code>null</code> is passed.
+     * 
+     * @param date new effective date or <code>null</code>
+     * @throws RModifyException if failed
+     */
+    public void setEffectiveDate(Date date) throws RModifyException;
+
+    /**
+     * Sets expiration date for rules entity.
+     * expiration date can be disabled if <code>null</code> is passed.
+     * 
+     * @param date new expiration date or <code>null</code>
+     * @throws RModifyException if failed
+     */
+    public void setExpirationDate(Date date) throws RModifyException;
+
+    /**
+     * Sets line of business for rules entity.
+     * Line of business can be disabled if <code>null</code> is passed.
+     * 
+     * @param date new line of business or <code>null</code>
+     * @throws RModifyException if failed
+     */
+    public void setLineOfBusiness(String lineOfBusiness) throws RModifyException;
     
     public Collection<RProperty> getProperties();
     public void addProperty(String name, RPropertyType type, Object value) throws RRepositoryException;
