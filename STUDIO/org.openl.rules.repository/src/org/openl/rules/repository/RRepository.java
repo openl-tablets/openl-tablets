04169daed2e16cfb742b71ce28f79ca3d113bcd7
abykhavets@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <abykhavets@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2007-11-13 17:32:43 +0000

    EPBDS-95 Common Interfaces for different layers
    adding deployment descriptor projects

--- STUDIO/org.openl.rules.repository/src/org/openl/rules/repository/RRepository.java
+++ STUDIO/org.openl.rules.repository/src/org/openl/rules/repository/RRepository.java
@@ -67,4 +67,8 @@ public interface RRepository {
      * @return name of repository
      */
     public String getName();
+    
+    public RDeploymentDescriptorProject getDDProject(String name) throws RRepositoryException;
+    public List<RDeploymentDescriptorProject> getDDProjects() throws RRepositoryException;
+    public RDeploymentDescriptorProject createDDProject(String name) throws RRepositoryException;
 }
