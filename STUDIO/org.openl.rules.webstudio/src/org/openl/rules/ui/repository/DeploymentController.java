3507df9e4884fe521de76463098773ea83a0fe78
anaumenko@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <anaumenko@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2007-11-14 16:10:34 +0000

    EPBDS-1. Adjusting deployment descriptor functionality.

---
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/ui/repository/DeploymentController.java
@@ -0,0 +1,171 @@
+package org.openl.rules.ui.repository;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import static org.openl.rules.ui.repository.UiConst.OUTCOME_SUCCESS;
+import org.openl.rules.webstudio.RulesUserSession;
+import org.openl.rules.webstudio.services.upload.UploadService;
+import org.openl.rules.webstudio.util.FacesUtils;
+import org.openl.rules.workspace.abstracts.DeploymentDescriptorProject;
+import org.openl.rules.workspace.abstracts.ProjectDescriptor;
+import org.openl.rules.workspace.abstracts.ProjectException;
+import org.openl.rules.workspace.abstracts.ProjectVersion;
+import org.openl.rules.workspace.dtr.impl.RepositoryProjectVersionImpl;
+import org.openl.rules.workspace.uw.UserWorkspace;
+import org.openl.rules.workspace.uw.UserWorkspaceProject;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import javax.faces.model.SelectItem;
+
+
+/**
+ * Deployment controller.
+ *
+ * @author Andrey Naumenko
+ */
+public class DeploymentController {
+    private final static Log log = LogFactory.getLog(DeploymentController.class);
+    private List<DeploymentDescriptorItem> items;
+    private String projectName;
+    private String version;
+
+    public DeploymentController() {
+        items = new ArrayList<DeploymentDescriptorItem>();
+        items.add(new DeploymentDescriptorItem("Project 1", "1.2.1"));
+        items.add(new DeploymentDescriptorItem("Project 2", "1.2.2",
+                "Conflicts with project 5 v1.0.4"));
+        items.add(new DeploymentDescriptorItem("Project 5", "1.0.4"));
+    }
+
+    public List<DeploymentDescriptorItem> getItems() {
+        return items;
+    }
+
+    public void setItems(List<DeploymentDescriptorItem> items) {
+        this.items = items;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public String addItem() {
+        DeploymentDescriptorItem newItem = new DeploymentDescriptorItem(projectName,
+                version);
+        if (!items.contains(newItem)) {
+            items.add(newItem);
+        }
+        return OUTCOME_SUCCESS;
+    }
+
+    public String deleteItem() {
+        Integer key = Integer.valueOf(FacesUtils.getRequestParameter("key"));
+        items.remove(key.intValue());
+        return OUTCOME_SUCCESS;
+    }
+
+    public String save() {
+        UserWorkspace workspace = getWorkSpace();
+
+        String name = "123";
+
+        try {
+            workspace.createDDProject(name);
+
+            DeploymentDescriptorProject ddp = workspace.getDDProject(name);
+
+            for (DeploymentDescriptorItem item : items) {
+                ProjectDescriptor pd = ddp.createProjectDescriptor(item.getName());
+                pd.setProjectVersion(new RepositoryProjectVersionImpl(1, 10, 100, null));
+            }
+
+            ddp.update();
+        } catch (Exception e) {
+            log.error("Cannot create new DDP " + name);
+            return null;
+        }
+
+        return null;
+    }
+
+    private UserWorkspace getWorkSpace() {
+        RulesUserSession rulesUserSession = (RulesUserSession) FacesUtils.getSessionMap()
+                .get("rulesUserSession");
+
+        UserWorkspace workspace = null;
+
+        try {
+            workspace = rulesUserSession.getUserWorkspace();
+        } catch (Exception e) {
+            log.error("Error obtaining user workspace", e);
+            return null;
+        }
+        return workspace;
+    }
+
+    public SelectItem[] getProjects() {
+        UserWorkspace workspace = getWorkSpace();
+        Collection<UserWorkspaceProject> projects = workspace.getProjects();
+        List<SelectItem> selectItems = new ArrayList<SelectItem>();
+        for (UserWorkspaceProject project : projects) {
+            selectItems.add(new SelectItem(project.getName()));
+        }
+        return selectItems.toArray(new SelectItem[0]);
+    }
+
+    public SelectItem[] getProjectVersions() {
+        UserWorkspace workspace = getWorkSpace();
+        if (projectName == null) {
+            return new SelectItem[0];
+        }
+
+        try {
+            UserWorkspaceProject project = workspace.getProject(projectName);
+
+            List<SelectItem> selectItems = new ArrayList<SelectItem>();
+            for (ProjectVersion version : project.getVersions()) {
+                selectItems.add(new SelectItem(version.getVersionName()));
+            }
+            return selectItems.toArray(new SelectItem[0]);
+        } catch (ProjectException e) {
+            log.error(e);
+        }
+
+        return null;
+    }
+
+    public String deploy() {
+        return OUTCOME_SUCCESS;
+    }
+
+    public String checkIn() {
+        return OUTCOME_SUCCESS;
+    }
+
+    public String checkOut() {
+        return OUTCOME_SUCCESS;
+    }
+
+    public boolean isCheckinable() {
+        return true;
+    }
+
+    public boolean isCheckoutable() {
+        return true;
+    }
+}
