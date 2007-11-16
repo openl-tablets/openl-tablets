7e1b08df14d93a371791a17536a1ede76359d12f
anaumenko@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <anaumenko@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2007-11-16 10:31:59 +0000

    EPBDS-1. Adjusting skin selection.

---
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/web/SkinBean.java
@@ -0,0 +1,33 @@
+package org.openl.rules.webstudio.web;
+
+import javax.faces.model.SelectItem;
+
+
+/**
+ * JSF session bean, that is responsible for storage of current skin.
+ *
+ * @author "Andrey Naumenko"
+ */
+public class SkinBean {
+    private static final String[] skinsArray = new String[] {
+            "DEFAULT", "blueSky", "classic", "deepMarine", "emeraldTown", "japanCherry",
+            "ruby", "wine", "plain"
+        };
+    private String skin = skinsArray[1];
+
+    public String getSkin() {
+        return skin;
+    }
+
+    public void setSkin(String skin) {
+        this.skin = skin;
+    }
+
+    public SelectItem[] getSkins() {
+        SelectItem[] selectItems = new SelectItem[skinsArray.length];
+        for (int i = 0; i < skinsArray.length; i++) {
+            selectItems[i] = new SelectItem(skinsArray[i]);
+        }
+        return selectItems;
+    }
+}
