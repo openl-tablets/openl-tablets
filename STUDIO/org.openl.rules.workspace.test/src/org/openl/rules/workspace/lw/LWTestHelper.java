f7ef58d2a5397d3e7e8d08183a8a4342516ba27c
abykhavets <abykhavets@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2007-11-20 14:16:19 +0000

    EPBDS-1 Version support

--- STUDIO/org.openl.rules.workspace.test/src/org/openl/rules/workspace/lw/LWTestHelper.java
+++ STUDIO/org.openl.rules.workspace.test/src/org/openl/rules/workspace/lw/LWTestHelper.java
@@ -4,7 +4,7 @@ import org.openl.rules.workspace.WorkspaceUser;
 import org.openl.rules.workspace.WorkspaceUserImpl;
 
 public class LWTestHelper {
-    private static final WorkspaceUser USER_TEST = new WorkspaceUserImpl("test");
+    private static final WorkspaceUser USER_TEST = new WorkspaceUserImpl("test", "test");
 
     public static WorkspaceUser getTestUser() {
         return USER_TEST;
