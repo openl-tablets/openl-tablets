89c4cf969960391e707b49a3d3afc06a63e8328b
abykhavets@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <abykhavets@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2007-11-22 14:44:26 +0000

    EPBDS-95 Common Interfaces for different layers
    versions for deployment projects
    refactoring

--- STUDIO/org.openl.rules.repository.jcr/src/org/openl/rules/repository/jcr/JcrCommonArtefact.java
+++ STUDIO/org.openl.rules.repository.jcr/src/org/openl/rules/repository/jcr/JcrCommonArtefact.java
@@ -1,8 +1,15 @@
 package org.openl.rules.repository.jcr;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import javax.jcr.Node;
 import javax.jcr.RepositoryException;
+import javax.jcr.version.Version;
+import javax.jcr.version.VersionHistory;
+import javax.jcr.version.VersionIterator;
 
+import org.openl.rules.repository.RVersion;
 import org.openl.rules.repository.exceptions.RRepositoryException;
 
 public class JcrCommonArtefact {
@@ -19,6 +26,50 @@ public class JcrCommonArtefact {
         return name;
     }
     
+    public void delete() throws RRepositoryException {
+        try {
+            Node n = node();
+
+            NodeUtil.smartCheckout(n, true);
+
+            n.remove();
+        } catch (RepositoryException e) {
+            throw new RRepositoryException("Failed to Delete", e);
+        }
+    }
+
+    public RVersion getBaseVersion() {
+        try {
+            Version v = node().getBaseVersion();
+            RVersion result = new JcrVersion(v);
+            return result;
+        } catch (RepositoryException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public List<RVersion> getVersionHistory() throws RRepositoryException {
+        try {
+            VersionHistory vh = node().getVersionHistory();
+            VersionIterator vi = vh.getAllVersions();
+            LinkedList<RVersion> result = new LinkedList<RVersion>();
+            while (vi.hasNext()) {
+                Version v = vi.nextVersion();
+
+                if (NodeUtil.isRootVersion(v)) {
+                    //TODO Shall we add first (0) version? (It is marker like, no real values)
+                } else {
+                    JcrVersion jvi = new JcrVersion(v);
+                    result.add(jvi);
+                }
+            }
+            return result;
+        } catch (RepositoryException e) {
+            throw new RRepositoryException("Failed to get Version History", e);
+        }
+    }
+
     // --- protected 
     
     protected Node node() {
@@ -34,18 +85,6 @@ public class JcrCommonArtefact {
     protected void checkNodeType(String nodeType) throws RepositoryException {
         if (!node.isNodeType(nodeType)) {
             throw new RepositoryException("Invalid NodeType. Expects " + nodeType);
-        }
-    }
-
-    protected void delete() throws RRepositoryException {
-        try {
-            Node n = node();
-
-            NodeUtil.smartCheckout(n, true);
-
-            n.remove();
-        } catch (RepositoryException e) {
-            throw new RRepositoryException("Failed to Delete", e);
         }
     }
 }
