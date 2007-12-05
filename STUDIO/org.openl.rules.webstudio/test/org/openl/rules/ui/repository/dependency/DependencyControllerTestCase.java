5bc22f27395e63af7e17de282ad2ac5d7a32ab6d
anaumenko <anaumenko@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2007-12-05 10:24:17 +0000

    EPBDS-1. Renamed addDependency into dependencyController.

--- STUDIO/org.openl.rules.webstudio/test/org/openl/rules/ui/repository/dependency/DependencyControllerTestCase.java
+++ STUDIO/org.openl.rules.webstudio/test/org/openl/rules/ui/repository/dependency/DependencyControllerTestCase.java
@@ -3,7 +3,7 @@ package org.openl.rules.ui.repository.dependency;
 import junit.framework.TestCase;
 import org.openl.rules.workspace.abstracts.ProjectVersion;
 
-public class AddDependencyControllerTestCase extends TestCase {
+public class DependencyControllerTestCase extends TestCase {
     public void testVersionFromString() {
         String[] examples = {
                 "1", "1.", "0.4", "0.4.", "2.1.2", "2.1.4.", "21.22.123"
@@ -19,7 +19,7 @@ public class AddDependencyControllerTestCase extends TestCase {
         };
 
         for (int i = 0; i < examples.length; i++) {
-            ProjectVersion projectVersion = AddDependencyController.versionFromString(examples[i]);
+            ProjectVersion projectVersion = DependencyController.versionFromString(examples[i]);
             assertNotNull("got null version", projectVersion);
             assertEquals("incorrect major number", expected[i][0], projectVersion.getMajor());
             assertEquals("incorrect minor number", expected[i][1], projectVersion.getMinor());
@@ -33,7 +33,7 @@ public class AddDependencyControllerTestCase extends TestCase {
         };
 
         for (String s : examples) {
-            assertNull("null expected", AddDependencyController.versionFromString(s));
+            assertNull("null expected", DependencyController.versionFromString(s));
         }
         
     }
