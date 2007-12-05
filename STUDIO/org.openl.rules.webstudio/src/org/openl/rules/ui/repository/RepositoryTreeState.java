0458f3fe0594abe1f31eb04b8abb0c739f978690
anaumenko <anaumenko@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2007-12-05 17:06:40 +0000

    EPBDS-166. Fixed delete in elements list.

--- STUDIO/org.openl.rules.webstudio/src/org/openl/rules/ui/repository/RepositoryTreeState.java
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/ui/repository/RepositoryTreeState.java
@@ -1,8 +1,13 @@
 package org.openl.rules.ui.repository;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-
 import org.openl.rules.ui.repository.tree.AbstractTreeNode;
 import org.openl.rules.ui.repository.tree.TreeDProject;
 import org.openl.rules.ui.repository.tree.TreeFile;
@@ -17,18 +22,9 @@ import org.openl.rules.workspace.dtr.RepositoryException;
 import org.openl.rules.workspace.uw.UserWorkspace;
 import org.openl.rules.workspace.uw.UserWorkspaceDeploymentProject;
 import org.openl.rules.workspace.uw.UserWorkspaceProject;
-
 import org.richfaces.component.UITree;
-
 import org.richfaces.event.NodeSelectedEvent;
-
 import org.richfaces.model.TreeNode;
-
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.Iterator;
-import java.util.List;
 
 
 /**
@@ -38,16 +34,16 @@ import java.util.List;
  */
 public class RepositoryTreeState {
     private final static Log log = LogFactory.getLog(RepositoryTreeState.class);
-    
-    public static final Comparator<ProjectArtefact> ARTEFACT_COMPARATOR 
+
+    public static final Comparator<ProjectArtefact> ARTEFACT_COMPARATOR
     = new Comparator<ProjectArtefact>() {
-	public int compare(ProjectArtefact o1, ProjectArtefact o2) {
-	    if (o1.isFolder() == o2.isFolder()) {
-		return o1.getName().compareTo(o2.getName());
-	    } else {
-		return (o1.isFolder() ? -1 : 1);
-	    }
-	}
+    public int compare(ProjectArtefact o1, ProjectArtefact o2) {
+        if (o1.isFolder() == o2.isFolder()) {
+        return o1.getName().compareTo(o2.getName());
+        } else {
+        return (o1.isFolder() ? -1 : 1);
+        }
+    }
     };
 
     /** Root node for RichFaces's tree.  It is not displayed. */
@@ -58,12 +54,12 @@ public class RepositoryTreeState {
     private UserWorkspace userWorkspace;
 
     private void traverseFolder(TreeFolder folder, Collection<? extends ProjectArtefact> artefacts) {
-	ProjectArtefact[] sortedArtefacts = new ProjectArtefact[artefacts.size()];
-	sortedArtefacts = artefacts.toArray(sortedArtefacts);
-	
-	Arrays.sort(sortedArtefacts, ARTEFACT_COMPARATOR);
-	
-	for (ProjectArtefact artefact : sortedArtefacts) {
+    ProjectArtefact[] sortedArtefacts = new ProjectArtefact[artefacts.size()];
+    sortedArtefacts = artefacts.toArray(sortedArtefacts);
+
+    Arrays.sort(sortedArtefacts, ARTEFACT_COMPARATOR);
+
+    for (ProjectArtefact artefact : sortedArtefacts) {
             String id = artefact.getName();
             if (artefact.isFolder()) {
                 TreeFolder treeFolder = new TreeFolder(id, artefact.getName());
@@ -157,11 +153,9 @@ public class RepositoryTreeState {
     }
 
     /**
-     * Updates repositoryTreeState.selectedNode after add.
-     *
-     * @param name
+     * Updates repositoryTreeState.selectedNode.
      */
-    public void updateSelectionAfterAdd(String name) {
+    public void updateSelection() {
         Iterator<String> it = getSelectedNode().getDataBean().getArtefactPath()
                 .getSegments().iterator();
         TreeNode currentNode = getRulesRepository();
