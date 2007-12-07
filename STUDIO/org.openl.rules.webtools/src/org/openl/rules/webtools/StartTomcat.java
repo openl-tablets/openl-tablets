5282f62be44b15d81dfd982fc458464fabe2ec74
anaumenko@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <anaumenko@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2007-12-07 14:39:00 +0000

    EPBDS-1. Removed @Override.

--- STUDIO/org.openl.rules.webtools/src/org/openl/rules/webtools/StartTomcat.java
+++ STUDIO/org.openl.rules.webtools/src/org/openl/rules/webtools/StartTomcat.java
@@ -14,7 +14,7 @@ import edu.stanford.ejalbert.BrowserLauncher;
 /*
  * Created on Jul 11, 2004
  *
- * Developed by OpenRules Inc 2003-2004 	
+ * Developed by OpenRules Inc 2003-2004
  */
 
 /**
@@ -23,123 +23,122 @@ import edu.stanford.ejalbert.BrowserLauncher;
 public class StartTomcat
 {
 
-	public static void main(String[] args) throws Exception
-	{
-		
-//		System.out.println("OpenL Tomcat Starter,  Version " + OpenLVersion.getVersion() + 
+    public static void main(String[] args) throws Exception
+    {
+
+//		System.out.println("OpenL Tomcat Starter,  Version " + OpenLVersion.getVersion() +
 //				" Build " + OpenLVersion.getBuild() + " http://openl-tablets.sourceforge.net  (c) 2006,2007\n");
-		
-		System.out.println("OpenL Tomcat Starter,  Version " + OpenLVersion.getVersion() + 
-				" Build " + OpenLVersion.getBuild() + " " + 
-				OpenLVersion.getURL()  + " (c) " + OpenLVersion.getCopyrightYear() +"\n");
-		
-		
-		String chome = System.getProperty("catalina.home");
 
-		if (chome == null)
-		{
-			chome = getProperty(args, "catalina.home");
-		}
-		if (chome == null)
-		{
-			chome ="../org.openl.lib.apache.tomcat/apache-tomcat-5.5.17";
-		}
-		File catalinaHome = new File(chome);
-		
-		if (!catalinaHome.exists())
-		{
-			throw new Exception(String.format("\nYou did not set up correctly catalina.home variable. It was \"{0}\".\n Please refer to OpenL Tablets document 'Web Programming and OpenL Tablets'. Chapter - Web Develoment Setup", chome));
-		}	
-		
-		System.setProperty("catalina.home", catalinaHome.getCanonicalPath());
-		
-		System.out.println(
-			"Using tomcat home: " + System.getProperty("catalina.home"));
+        System.out.println("OpenL Tomcat Starter,  Version " + OpenLVersion.getVersion() +
+                " Build " + OpenLVersion.getBuild() + " " +
+                OpenLVersion.getURL()  + " (c) " + OpenLVersion.getCopyrightYear() +"\n");
 
-		String cbase = System.getProperty("catalina.base");
-		
 
-		if (cbase == null)
-		{
-			cbase = getProperty(args, "catalina.base");
-		}
-		if (cbase == null)
-		{
-		    cbase=".";
-		}
-		
-		File catalinaBase = new File(cbase);
-		System.setProperty("catalina.base", catalinaBase.getCanonicalPath());
-		
-		System.out.println(
-				"Using tomcat base: " + System.getProperty("catalina.base"));
-		
-		Class<?> bootstrap = null;
-		try
-		{
-			bootstrap = Class.forName("org.apache.catalina.startup.Bootstrap");
-		}
-		catch(ClassNotFoundException cnfe)
-		{
-			throw new Exception("\n Apache Tomcat bootstrap.jar must be in classpath.");
-		}
-		
-		Method main = bootstrap.getMethod("main", new Class[]{String[].class});
-		
-		new Thread(new BrowserStarter()).start();
-		
-		main.invoke(null, new Object[]{new String[] { "start" }});
-		
+        String chome = System.getProperty("catalina.home");
+
+        if (chome == null)
+        {
+            chome = getProperty(args, "catalina.home");
+        }
+        if (chome == null)
+        {
+            chome ="../org.openl.lib.apache.tomcat/apache-tomcat-5.5.17";
+        }
+        File catalinaHome = new File(chome);
+
+        if (!catalinaHome.exists())
+        {
+            throw new Exception(String.format("\nYou did not set up correctly catalina.home variable. It was \"{0}\".\n Please refer to OpenL Tablets document 'Web Programming and OpenL Tablets'. Chapter - Web Develoment Setup", chome));
+        }
+
+        System.setProperty("catalina.home", catalinaHome.getCanonicalPath());
+
+        System.out.println(
+            "Using tomcat home: " + System.getProperty("catalina.home"));
+
+        String cbase = System.getProperty("catalina.base");
+
+
+        if (cbase == null)
+        {
+            cbase = getProperty(args, "catalina.base");
+        }
+        if (cbase == null)
+        {
+            cbase=".";
+        }
+
+        File catalinaBase = new File(cbase);
+        System.setProperty("catalina.base", catalinaBase.getCanonicalPath());
+
+        System.out.println(
+                "Using tomcat base: " + System.getProperty("catalina.base"));
+
+        Class<?> bootstrap = null;
+        try
+        {
+            bootstrap = Class.forName("org.apache.catalina.startup.Bootstrap");
+        }
+        catch(ClassNotFoundException cnfe)
+        {
+            throw new Exception("\n Apache Tomcat bootstrap.jar must be in classpath.");
+        }
+
+        Method main = bootstrap.getMethod("main", new Class[]{String[].class});
+
+        new Thread(new BrowserStarter()).start();
+
+        main.invoke(null, new Object[]{new String[] { "start" }});
+
 //		org.apache.catalina.startup.Bootstrap.main(new String[] { "start" });
-		
-	}
-	/*
-		C:\3p\jakarta-tomcat-5.0.25\bin>start "Tomcat" "c:\j2sdk1.4.2_04\bin\java"    
-		-Djava.endorsed.dirs="C:\3p\jakarta-tomcat-5.0.25\common\endorsed" 
-		-classpath "c:\j2sdk1.4.2_04\lib\tools.jar;C:\3p\jakarta-tomcat-5.0.25\bin\bootstrap.jar" 
-		-Dcatalina.base="C:\3p\jakarta-tomcat-5.0.25" 
-		-Dcatalina.home="C:\3p\jakarta-tomcat-5.0.25" 
-		-Djava.io.tmpdir="C:\3p\jakarta-tomcat-5.0.25\temp" 
-		org.apache.catalina.startup.Bootstrap  start
-	
-	*/
-	
-	private static String getProperty(String[] args, String prefix) {
-		if (args != null){
-			for (String parameter : args){
-				if (parameter.startsWith(prefix)){
-					return parameter.substring(parameter.indexOf('=')+1).trim();					
-				}
-			}
-		}
-		return null;
-	}
-	
-	private static class BrowserStarter implements Runnable{
 
-		@Override
-		public void run(){
-			try{
-				Thread.sleep(20000);
-			}
-			catch (InterruptedException ex){
-				// Do nothing
-			}
-			
-			try{
-				//NOTICE: The desktop support is available beginning from JDK 1.6
-				if (java.awt.Desktop.isDesktopSupported()){
-					java.awt.Desktop.getDesktop().browse(new URI("http://localhost:8080/webstudio/"));
-				}
-				else{
-					 BrowserLauncher browserLauncher = new BrowserLauncher();
-					 browserLauncher.openURLinBrowser("http://localhost:8080/webstudio/");
-				}
-			}
-			catch (Exception ex){
-				Log.error("Could not start a browser. Error: {0}", ex, ex.getMessage());
-			}
-		}
-		
-	}
+    }
+    /*
+        C:\3p\jakarta-tomcat-5.0.25\bin>start "Tomcat" "c:\j2sdk1.4.2_04\bin\java"
+        -Djava.endorsed.dirs="C:\3p\jakarta-tomcat-5.0.25\common\endorsed"
+        -classpath "c:\j2sdk1.4.2_04\lib\tools.jar;C:\3p\jakarta-tomcat-5.0.25\bin\bootstrap.jar"
+        -Dcatalina.base="C:\3p\jakarta-tomcat-5.0.25"
+        -Dcatalina.home="C:\3p\jakarta-tomcat-5.0.25"
+        -Djava.io.tmpdir="C:\3p\jakarta-tomcat-5.0.25\temp"
+        org.apache.catalina.startup.Bootstrap  start
+
+    */
+
+    private static String getProperty(String[] args, String prefix) {
+        if (args != null){
+            for (String parameter : args){
+                if (parameter.startsWith(prefix)){
+                    return parameter.substring(parameter.indexOf('=')+1).trim();
+                }
+            }
+        }
+        return null;
+    }
+
+    private static class BrowserStarter implements Runnable{
+
+        public void run(){
+            try{
+                Thread.sleep(20000);
+            }
+            catch (InterruptedException ex){
+                // Do nothing
+            }
+
+            try{
+                //NOTICE: The desktop support is available beginning from JDK 1.6
+                if (java.awt.Desktop.isDesktopSupported()){
+                    java.awt.Desktop.getDesktop().browse(new URI("http://localhost:8080/webstudio/"));
+                }
+                else{
+                     BrowserLauncher browserLauncher = new BrowserLauncher();
+                     browserLauncher.openURLinBrowser("http://localhost:8080/webstudio/");
+                }
+            }
+            catch (Exception ex){
+                Log.error("Could not start a browser. Error: {0}", ex, ex.getMessage());
+            }
+        }
+
+    }
 }
