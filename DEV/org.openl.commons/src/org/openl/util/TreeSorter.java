8fd9c346dfea9415cafe3ee4bdf6021d190fc3ad
sshor@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <sshor@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2007-12-07 14:23:28 +0000

    EPBDS - minor change

--- DEV/org.openl.commons/src/org/openl/util/TreeSorter.java
+++ DEV/org.openl.commons/src/org/openl/util/TreeSorter.java
@@ -1,60 +1,59 @@
 package org.openl.util;
 
-public abstract class TreeSorter
+public abstract class TreeSorter<T>
 {
 
-	public abstract Comparable makeKey(Object obj);
+    public abstract Comparable<?> makeKey(T obj);
 
-	public abstract ITreeElement makeElement(Object obj, int i);
-	
-	static public void addElement(ITreeElement targetElement, Object obj, TreeSorter[] sorters, int level)
+    public abstract ITreeElement<T> makeElement(Object obj, int i);
+
+    static public <T> void addElement(ITreeElement<T> targetElement, T obj,
+	    TreeSorter<T>[] sorters, int level)
+    {
+	if (level >= sorters.length)
+	    return;
+
+	ITreeElement.Node<T> target = (ITreeElement.Node<T>) targetElement;
+	Comparable<?> key = sorters[level].makeKey(obj);
+	ITreeElement<T> element = target.getChild(key);
+	if (element == null)
 	{
-		if (level >= sorters.length)
-			return;
-		 
-		ITreeElement.Node target = (ITreeElement.Node)targetElement;
-		Comparable key = sorters[level].makeKey(obj);
-		ITreeElement element = (ITreeElement) target.getChild(key);
-		if (element == null )
+	    element = sorters[level].makeElement(obj, 0);
+	    target.addChild(key, element);
+	} else if (sorters[level].isUnique())
+	{
+	    for (int i = 2; i < 100; ++i)
+	    {
+		Comparable<?> key2 = sorters[level].makeKey(obj, i);
+		element = target.getChild(key2);
+		if (element == null)
 		{
-			element = sorters[level].makeElement(obj, 0);
-			target.addChild(key, element);
+		    element = sorters[level].makeElement(obj, i);
+		    target.addChild(key2, element);
+		    break;
 		}
-		else if (sorters[level].isUnique())
-		{
-			for(int i = 2; i < 100; ++i)
-			{
-				Comparable key2 = sorters[level].makeKey(obj, i);
-				element = (ITreeElement) target.getChild(key2);
-				if (element == null)
-				{
-					element = sorters[level].makeElement(obj, i);
-					target.addChild(key2, element);
-					break;
-				}	
-			}	
-		}	
-		
-		addElement(element, obj, sorters, level + 1);
+	    }
 	}
 
-	/**
-	 * @param obj
-	 * @param i
-	 * @return
-	 */
-	protected Comparable makeKey(Object obj, int i)
-	{
-		throw new UnsupportedOperationException();
-	}
+	addElement(element, obj, sorters, level + 1);
+    }
 
-	/**
-	 * @return
-	 */
-	protected boolean isUnique()
-	{
-		return false;
-	}
-	
-	
+    /**
+     * @param obj
+     * @param i
+     * @return
+     */
+    protected Comparable<?> makeKey(T obj, int i)
+    {
+	throw new UnsupportedOperationException();
+    }
+
+    /**
+     * @return
+     */
+    protected boolean isUnique()
+    {
+	return false;
+    }
+
 }
