59e49ba813373179dbf0769ed0274ad004c01b10
anaumenko@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <anaumenko@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2007-12-22 12:35:35 +0000

    EPBDS. Moving filters to org.openl.rules.workspace.

---
+++ STUDIO/org.openl.rules.workspace/src/org/openl/rules/workspace/filter/PathFilter.java
@@ -0,0 +1,15 @@
+package org.openl.rules.workspace.filter;
+
+/**
+ * An interface to filter out unwanted files and directories when uploading a project.
+ * As an example an application may choose to ignore CSV files (e.g <i>.svn</i> or <i>.cvs</i>) directories.
+ */
+public interface PathFilter {
+    /**
+     * The filter method. Checks a filename in form of: <i>root_folder/sub_folder/.../[file_name]</i>.
+     *
+     * @param filename file or directory name
+     * @return if filter accepts given filename
+     */
+    boolean accept(String path);
+}
