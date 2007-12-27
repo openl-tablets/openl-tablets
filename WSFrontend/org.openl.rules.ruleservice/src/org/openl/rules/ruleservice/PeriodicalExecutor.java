d91a102640e212a2fba7fba0e533d6b947c968e3
aantonik@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <aantonik@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2007-12-27 14:45:44 +0000

    EPBDS-1 javadoc

--- WSFrontend/org.openl.rules.ruleservice/src/org/openl/rules/ruleservice/PeriodicalExecutor.java
+++ WSFrontend/org.openl.rules.ruleservice/src/org/openl/rules/ruleservice/PeriodicalExecutor.java
@@ -3,6 +3,16 @@ package org.openl.rules.ruleservice;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ArrayBlockingQueue;
 
+/**
+ * Runs a given {@link Runnable} every time an event happens. The main method {@link #execute()} runs infinite loop
+ * and if another thread triggers the event via {@link #signal()} method calls <code>run()</code> on the object passed
+ * in the constructor. If method {@link #signal()} was called several times since the last <code>Runnable</code>
+ * execution (e.g. while the previous call to <code>run()</code> has not completed) that <code>Runnable</code>  will be
+ * executed only once the next time. <br/>
+ *
+ * The object constructor calls {@link #signal()} so the <code>action.run()</code> is guaranteed to be called just after
+ * calling {@link #execute()}.
+ */
 public class PeriodicalExecutor {
     private final Runnable action;
     private BlockingQueue<Object> queue = new ArrayBlockingQueue<Object>(1);
