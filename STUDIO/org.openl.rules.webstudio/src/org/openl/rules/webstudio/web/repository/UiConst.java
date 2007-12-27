1017b957f0ec30921d1c4873e8d2d570dfdc70a1
anaumenko <anaumenko@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2007-12-27 12:27:53 +0000

    EPBDS. Adjusted icons.

--- STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/web/repository/UiConst.java
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/web/repository/UiConst.java
@@ -15,19 +15,18 @@ public class UiConst {
     public static final String TYPE_DEPLOYMENT_REPOSITORY = "drepository";
 
     // <rich:treeNode icon="..." iconLeaf="..."
-    public static final String ICON_FILE = "/images/jcr/tree_document.gif";
-    public static final String ICON_FOLDER = "/images/jcr/tree_folder.gif";
-    public static final String ICON_PROJECT = "/images/jcr/tree_server.gif";
-    public static final String ICON_REPOSITORY = "/images/jcr/tree_storage.gif";
+    public static final String ICON_FILE = "/images/repository/document.gif";
+    public static final String ICON_FOLDER = "/images/repository/folder.gif";
+    public static final String ICON_REPOSITORY = "/images/repository/storage.gif";
 
     //
-    public static final String ICON_PROJECT_CHECKED_OUT = "/images/jcr/tree-prj-checkedout.gif";
-    public static final String ICON_PROJECT_CLOSED = "/images/jcr/tree-prj-closed.gif";
-    public static final String ICON_PROJECT_CLOSED_LOCKED = "/images/jcr/tree-prj-closed-locked.gif";
-    public static final String ICON_PROJECT_DELETED = "/images/jcr/tree-prj-deleted.gif";
-    public static final String ICON_PROJECT_LOCAL = "/images/jcr/tree-prj-local.gif";
-    public static final String ICON_PROJECT_OPENED = "/images/jcr/tree-prj-opened.gif";
-    public static final String ICON_PROJECT_OPENED_LOCKED = "/images/jcr/tree-prj-opened-locked.gif";
+    public static final String ICON_PROJECT_CHECKED_OUT = "/images/repository/checkedout.gif";
+    public static final String ICON_PROJECT_CLOSED = "/images/repository/closed.gif";
+    public static final String ICON_PROJECT_CLOSED_LOCKED = "/images/repository/closed-locked.gif";
+    public static final String ICON_PROJECT_DELETED = "/images/repository/deleted.gif";
+    public static final String ICON_PROJECT_LOCAL = "/images/repository/local.gif";
+    public static final String ICON_PROJECT_OPENED = "/images/repository/opened.gif";
+    public static final String ICON_PROJECT_OPENED_LOCKED = "/images/repository/opened-locked.gif";
 
     //
     public static final String OUTCOME_SUCCESS = "success";
