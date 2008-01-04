c5f03a593b691f755145f4822a5c5fac51f3f219
aantonik@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <aantonik@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2008-01-04 12:55:05 +0000

    EPBDS-1 fixing possible NPE

--- STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/web/servlet/RulesUserSession.java
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/web/servlet/RulesUserSession.java
@@ -56,6 +56,8 @@ public class RulesUserSession {
     }
 
     public void sessionDestroyed() {
-        userWorkspace.release();
+        if (userWorkspace != null) {
+            userWorkspace.release();
+        }
     }
 }
