e34a9ecbb19c071d033874cd0ae3dd5d33dc7c1f
aantonik@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <aantonik@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2008-01-04 10:18:40 +0000

    EPBDS-1 temp dir property

--- WSFrontend/trunk/org.openl.rules.ruleservice/src/org/openl/rules/ruleservice/RuleServiceMain.java
+++ WSFrontend/trunk/org.openl.rules.ruleservice/src/org/openl/rules/ruleservice/RuleServiceMain.java
@@ -2,6 +2,7 @@ package org.openl.rules.ruleservice;
 
 import org.openl.rules.repository.exceptions.RRepositoryException;
 import org.openl.rules.repository.RDeploymentListener;
+import org.openl.rules.repository.SmartProps;
 import org.openl.rules.ruleservice.publish.WebServicesDeployAdmin;
 import org.openl.rules.ruleservice.publish.RulesWebServicesPublisher;
 import org.openl.rules.workspace.production.client.JcrRulesClient;
@@ -9,12 +10,22 @@ import org.openl.rules.workspace.production.client.JcrRulesClient;
 import java.io.File;
 
 public class RuleServiceMain {
+    private static String getTempDirectory() {
+        SmartProps props = new SmartProps("rules-production.properties");
+        String value = props.getStr("ruleservice.tmp.dir");
+        if (value == null || value.trim().length() == 0) {
+            return "/tmp/ws-deploy";
+        }
+        
+        return value;
+    }
+
     public static void main(String[] args) throws RRepositoryException, InterruptedException {
         final JcrRulesClient rulesClient = new JcrRulesClient(); 
 
         RulesWebServicesPublisher app = new RulesWebServicesPublisher();
         app.setRulesClient(rulesClient);
-        app.setTempFolder(new File("/tmp/ws-deploy"));
+        app.setTempFolder(new File(getTempDirectory()));
         app.setDeployAdmin(new WebServicesDeployAdmin());
 
         final PeriodicalExecutor executor = new PeriodicalExecutor(app);
