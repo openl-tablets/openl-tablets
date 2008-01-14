da7d6d688487f9a1724b1e8a8613c0ebc2fdb6c6
anaumenko@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <anaumenko@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2008-01-14 14:18:37 +0000

    EPBDS. Added maven_integration branch.

---
+++ STUDIO/branches/maven_integration/org.openl.rules.workspace/src/org/openl/rules/workspace/abstracts/ArtefactPath.java
@@ -0,0 +1,63 @@
+package org.openl.rules.workspace.abstracts;
+
+import java.util.Collection;
+
+/**
+ * Defines path of an Artefact.
+ * I.e. location of some particular artefact in a hierarchy of artefacts
+ * or in a tree of artefacts.
+ * 
+ * @author Aleh Bykhavets
+ */
+public interface ArtefactPath {
+    /**
+     * Gets a segment in the path.
+     *
+     * @param index position of segment in the path
+     * @return value of segment
+     */
+    public String segment(int index);
+
+    /**
+     * Returns number of segments in the path
+     * @return
+     */
+    public int segmentCount();
+
+    /**
+     * Gets the path as a collection of segments.
+     *
+     * @return collection of segments
+     */
+    public Collection<String> getSegments();
+
+    /**
+     * Gets the path as a single string.
+     * All segments are concatenated by special delimiter '/'.
+     *
+     * @return string with the path
+     */
+    public String getStringValue();
+
+    /**
+     * Create new instance of ArtefactPath from base one,
+     * excluding first segment of base path.
+     * <p/>
+     * It is used to translate path of artefact in the workspace 
+     * to path in a project.
+     * 
+     * @return new instance where first segment of base path is excluded.
+     */
+    public ArtefactPath withoutFirstSegment();
+
+    /**
+     * Creates new instance of ArtefactPath from base one,
+     * adding one more segment.
+     * <p/>
+     * It should be used to build artefact paths recursively.
+     * 
+     * @param segment adding segment
+     * @return new instance where specified segment is appended to path
+     */
+    public ArtefactPath withSegment(String segment);
+}
