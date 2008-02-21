11ba5a24fd85d7a929771eb404d6a32ca253a818
anaumenko@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <anaumenko@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2008-02-21 12:43:43 +0000

    EPBDS Creating a tag for the 5.0.5 release of STUDIO.

---
+++ STUDIO/tags/release-5.0.5/org.openl.rules.webstudio.java/src/org/openl/rules/webstudio/web/tableeditor/ICellEditorServerPart.java
@@ -0,0 +1,24 @@
+package org.openl.rules.webstudio.web.tableeditor;
+
+/**
+ * 
+ * @author snshor
+ *
+ * Some editors like code editor with code assist or lookup 
+ * editors may use server part to do complex processing
+ */
+
+public interface ICellEditorServerPart
+{
+
+    /**
+     * Provides server-side validation for input data
+     * 
+     * 
+     * @param input
+     * @return null if input is valid or error message
+     */
+    
+    public String validate(String input);
+    
+}
