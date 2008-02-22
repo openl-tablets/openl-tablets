cfde0d421ef8123e0f5ffde90dfeb17f403e6eec
anaumenko <anaumenko@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2008-02-22 12:38:16 +0000

    EPBDS. Merging changes to org.openl.rules.webstudio.java

---
+++ STUDIO/branches/iteration2_initial/org.openl.rules.webstudio.java/src/org/openl/rules/webstudio/web/repository/diff/DiffController.java
@@ -0,0 +1,81 @@
+package org.openl.rules.webstudio.web.repository.diff;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.openl.rules.repository.CommonVersionImpl;
+import org.openl.rules.webstudio.web.repository.RepositoryTreeState;
+import org.openl.rules.workspace.abstracts.Project;
+import org.openl.rules.workspace.abstracts.ProjectException;
+import org.openl.rules.workspace.dtr.DesignTimeRepository;
+import org.openl.rules.workspace.dtr.RepositoryException;
+import org.openl.rules.workspace.uw.UserWorkspace;
+
+import java.util.List;
+
+
+/**
+ * Used for retrieving project trees with structured diff data.
+ *
+ * @author Andrey Naumenko
+ */
+public class DiffController {
+    private static Log log = LogFactory.getLog(DiffController.class);
+    private List<ProjectDiff.DiffElement> diffElements;
+    private String path;
+    private UserWorkspace userWorkspace;
+    private DesignTimeRepository designTimeRepository;
+    private RepositoryTreeState repositoryTreeState;
+    private Project project1;
+    private Project project2;
+
+    public List<ProjectDiff.DiffElement> getDiffElements() {
+        return diffElements;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public Project getProject1() {
+        return project1;
+    }
+
+    public Project getProject2() {
+        return project2;
+    }
+
+    public void setInit(boolean init) {
+        project1 = repositoryTreeState.getSelectedProject();
+
+        try {
+            project2 = designTimeRepository.getProject(project1.getName(),
+                    new CommonVersionImpl("0.0.1"));
+        } catch (RepositoryException e) {
+            log.error("Error reading project", e);
+            return;
+        }
+
+        try {
+            diffElements = ProjectDiff.getDiff(project1, project2, path, path);
+        } catch (ProjectException e) {
+            log.error("", e);
+        }
+    }
+
+    public void setDesignTimeRepository(DesignTimeRepository designTimeRepository) {
+        this.designTimeRepository = designTimeRepository;
+    }
+
+    public void setUserWorkspace(UserWorkspace userWorkspace) {
+        this.userWorkspace = userWorkspace;
+    }
+
+    public void setRepositoryTreeState(RepositoryTreeState repositoryTreeState) {
+        this.repositoryTreeState = repositoryTreeState;
+    }
+}
