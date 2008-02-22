b82ab63a09336255199f4bc082a580d5f673ea83
anaumenko <anaumenko@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2008-02-22 15:27:09 +0000

    EPBDS Updated mavenrepo-plugins-install.bat script

--- DEV/trunk/org.openl.eclipse31-site/mavenrepo-plugins-install.bat
+++ DEV/trunk/org.openl.eclipse31-site/mavenrepo-plugins-install.bat
@@ -2,3 +2,5 @@ call mvn install:install-file -DgroupId=org.openl.rules -DartifactId=org.openl.c
 call mvn install:install-file -DgroupId=org.openl.rules -DartifactId=org.openl.commons -Dversion=5.0.5 -Dpackaging=jar -Dfile=plugins/org.openl.commons_5.0.5.jar -DgeneratePom=true
 call mvn install:install-file -DgroupId=org.openl.rules -DartifactId=org.openl.rules -Dversion=5.0.5 -Dpackaging=jar -Dfile=plugins/org.openl.rules_5.0.5.jar -DgeneratePom=true
 call mvn install:install-file -DgroupId=org.openl.rules -DartifactId=org.openl.rules.validator -Dversion=5.0.5 -Dpackaging=jar -Dfile=plugins/org.openl.rules.validator_5.0.5.jar -DgeneratePom=true
+call mvn install:install-file -DgroupId=org.openl.rules -DartifactId=org.openl.rules.j -Dversion=5.0.5 -Dpackaging=jar -Dfile=plugins/org.openl.rules.j_5.0.5.jar -DgeneratePom=true
+call mvn install:install-file -DgroupId=org.openl.rules -DartifactId=org.openl.rules.helpers -Dversion=5.0.5 -Dpackaging=jar -Dfile=plugins/org.openl.rules.helpers_5.0.5.jar -DgeneratePom=true
