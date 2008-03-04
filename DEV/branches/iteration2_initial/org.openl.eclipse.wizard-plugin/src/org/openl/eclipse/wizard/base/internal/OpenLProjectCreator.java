8ec3136eb25d9d3593327c5b615e73d7c153642e
aantonik@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <aantonik@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2008-03-04 12:56:22 +0000

    EPBDS adding openl nature using eclipse API

--- DEV/branches/iteration2_initial/org.openl.eclipse.wizard-plugin/src/org/openl/eclipse/wizard/base/internal/OpenLProjectCreator.java
+++ DEV/branches/iteration2_initial/org.openl.eclipse.wizard-plugin/src/org/openl/eclipse/wizard/base/internal/OpenLProjectCreator.java
@@ -6,46 +6,57 @@ import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.OperationCanceledException;
 import org.eclipse.core.runtime.SubProgressMonitor;
+import org.openl.eclipse.base.OpenlNature;
+import org.openl.eclipse.wizard.base.UtilBase;
 
 /**
  * @author Aliaksandr Antonik.
  */
 public class OpenLProjectCreator {
-    private IProjectDescription description;
     private IProject project;
+    private IPath projectLocation;
 
     public OpenLProjectCreator(IProject newProjectHandle, IPath projectLocation) {
         project = newProjectHandle;
-        description = ResourcesPlugin.getWorkspace().newProjectDescription(project.getName());
-        if (projectLocation != null) {
-            description.setLocation(projectLocation);
-        }
-    }
-
-    public IProjectDescription getDescription() {
-        return description;
+        this.projectLocation = projectLocation;
     }
 
     public IProject getProject() {
         return project;
     }
 
-    public void createAndOpen(IProgressMonitor monitor) throws CoreException, OperationCanceledException {
-        try {
-            monitor.beginTask(null, 2000);
+    public void createAndOpen(IProgressMonitor monitor, int ticks) throws CoreException, OperationCanceledException {
+        IProjectDescription description = ResourcesPlugin.getWorkspace().newProjectDescription(project.getName());
+        if (projectLocation != null) {
+            description.setLocation(projectLocation);
+        }
+        project.create(description, new SubProgressMonitor(monitor, ticks / 2));
 
-            project.create(description, new SubProgressMonitor(monitor, 1000));
+        if (monitor.isCanceled()) {
+            throw new OperationCanceledException();
+        }
 
-            if (monitor.isCanceled()) {
-                throw new OperationCanceledException();
-            }
+        project.open(new SubProgressMonitor(monitor, (ticks + 1) / 2));
+    }
 
-            project.open(new SubProgressMonitor(monitor, 1000));
+    public boolean addProjectNature() throws CoreException {
+        IProjectDescription description = project.getDescription();
+        String[] natures = description.getNatureIds();
+        String[] newNatures = new String[natures.length + 1];
+        System.arraycopy(natures, 0, newNatures, 0, natures.length);
+        newNatures[natures.length] = OpenlNature.NATURE_ID;
+        IStatus status = ResourcesPlugin.getWorkspace().validateNatureSet(natures);
 
-        } finally {
-            monitor.done();
+        if (status.getCode() == IStatus.OK) {
+            description.setNatureIds(newNatures);
+            project.setDescription(description, null);
+            return true;
+        } else {
+            UtilBase.handleException("unable to add openl nature");
+            return false;
         }
     }
 }
