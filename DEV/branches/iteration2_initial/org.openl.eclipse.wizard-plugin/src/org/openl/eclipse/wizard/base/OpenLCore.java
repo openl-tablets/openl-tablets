d5992c1f5e49f9c17680604b99593ce637cecab9
aantonik@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <aantonik@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2008-03-06 09:31:45 +0000

    EPBDS reorganizing classes in projects

---
+++ DEV/branches/iteration2_initial/org.openl.eclipse.wizard-plugin/src/org/openl/eclipse/wizard/base/OpenLCore.java
@@ -0,0 +1,12 @@
+package org.openl.eclipse.wizard.base;
+
+import org.eclipse.core.resources.IProject;
+
+/**
+ * @author Aliaksandr Antonik.
+ */
+public class OpenLCore {
+    public static void addOpenLCapabilities(IProject project, INewProjectFromTemplateWizardCustomizer customizer) {
+        
+    }
+}
