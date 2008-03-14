290cdaad05e0cfa0b9c94219af733d82c2e7accf
abykhavets <abykhavets@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2008-03-14 10:41:26 +0000

    EPBDS-236 Smart deployment for a rule project
    New controller for smart redeploy.
    Extracting dependency checker into separate class.
    Code refactoring and clean-up.

--- STUDIO/trunk/org.openl.rules.webstudio.java/src/org/openl/rules/webstudio/web/repository/DeploymentController.java
+++ STUDIO/trunk/org.openl.rules.webstudio.java/src/org/openl/rules/webstudio/web/repository/DeploymentController.java
@@ -1,16 +1,12 @@
 package org.openl.rules.webstudio.web.repository;
 
-import org.apache.commons.lang.StringEscapeUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-import org.openl.rules.repository.CommonVersion;
 import org.openl.rules.repository.CommonVersionImpl;
 import org.openl.rules.webstudio.web.jsf.util.FacesUtils;
-import org.openl.rules.webstudio.web.servlet.RulesUserSession;
 import org.openl.rules.workspace.abstracts.Project;
 import org.openl.rules.workspace.abstracts.ProjectArtefact;
-import org.openl.rules.workspace.abstracts.ProjectDependency;
 import org.openl.rules.workspace.abstracts.ProjectDescriptor;
 import org.openl.rules.workspace.abstracts.ProjectException;
 import org.openl.rules.workspace.abstracts.ProjectVersion;
@@ -21,17 +17,13 @@ import org.openl.rules.workspace.uw.UserWorkspaceDeploymentProject;
 import org.openl.rules.workspace.uw.UserWorkspaceProject;
 import org.openl.rules.workspace.uw.impl.UserWorkspaceDeploymentProjectImpl;
 import org.openl.rules.workspace.uw.impl.UserWorkspaceProjectDescriptorImpl;
-import org.openl.rules.workspace.uw.impl.UserWorkspaceImpl;
 import org.openl.rules.workspace.dtr.DesignTimeRepository;
-import org.openl.rules.workspace.dtr.RepositoryProject;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 import javax.faces.application.FacesMessage;
@@ -167,21 +159,8 @@ public class DeploymentController {
         return null;
     }
 
-    private UserWorkspace getWorkspace() {
-        try {
-            return getRulesUserSession().getUserWorkspace();
-        } catch (Exception e) {
-            log.error("Error obtaining user workspace", e);
-        }
-        return null;
-    }
-
-    private RulesUserSession getRulesUserSession() {
-        return (RulesUserSession) FacesUtils.getSessionMap().get("rulesUserSession");
-    }
-
     public SelectItem[] getProjects() {
-        UserWorkspace workspace = getWorkspace();
+        UserWorkspace workspace = RepositoryUtils.getWorkspace();
         Collection<UserWorkspaceProject> workspaceProjects = workspace.getProjects();
         List<SelectItem> selectItems = new ArrayList<SelectItem>();
 
@@ -204,7 +183,7 @@ public class DeploymentController {
     }
 
     public SelectItem[] getProjectVersions() {
-        UserWorkspace workspace = getWorkspace();
+        UserWorkspace workspace = RepositoryUtils.getWorkspace();
         if (projectName != null) {
             try {
                 UserWorkspaceProject project = workspace.getProject(projectName);
@@ -225,7 +204,7 @@ public class DeploymentController {
     }
 
     public String openSelectedProjects() {
-        UserWorkspace workspace = getWorkspace();
+        UserWorkspace workspace = RepositoryUtils.getWorkspace();
         for (DeploymentDescriptorItem item : items) {
             if (item.isSelected()) {
                 String projectName = item.getName();
@@ -247,32 +226,7 @@ public class DeploymentController {
     public String deploy() {
         UserWorkspaceDeploymentProject project = getSelectedProject();
         if (project != null) {
-            try {
-                ProductionDeployer deployer = getRulesUserSession().getDeployer();
-                DesignTimeRepository dtr = getWorkspace().getDesignTimeRepository();
-
-                Collection<ProjectDescriptor> projectDescriptors = project
-                        .getProjectDescriptors();
-                Collection<Project> projects = new ArrayList<Project>();
-
-                for (ProjectDescriptor pd : projectDescriptors) {
-                    projects.add(dtr.getProject(pd.getProjectName(), pd.getProjectVersion()));
-                }
-
-                DeployID id = getDeployID(project);
-                deployer.deploy(id, projects);
-
-                FacesContext.getCurrentInstance()
-                    .addMessage(null,
-                        new FacesMessage(FacesMessage.SEVERITY_INFO,
-                            "project successfully deployed with id: " + id.getName(), null));
-            } catch (Exception e) {
-                FacesContext.getCurrentInstance()
-                    .addMessage(null,
-                        new FacesMessage(FacesMessage.SEVERITY_ERROR, "failed to deploy",
-                            e.getMessage()));
-                log.error(e);
-            }
+            deploy(project);
         }
         return null;
     }
@@ -283,61 +237,39 @@ public class DeploymentController {
         if (items == null) {
             return;
         }
-        final UserWorkspace workspace = getWorkspace();
-        final DesignTimeRepository dtr = workspace.getDesignTimeRepository();
+        
+        DependencyChecker checker = new DependencyChecker();
+        UserWorkspaceDeploymentProject project = getSelectedProject();
+        checker.addProjects(project);
+        checker.check(items);
+    }
 
-        int n = items.size();
-        Map<String, VersionRange>[] projects = new Map[n];
-        for (int i = 0; i < items.size(); i++) {
-            DeploymentDescriptorItem ddItem = items.get(i);
-            ddItem.setMessages("");
+    protected static void deploy(UserWorkspaceDeploymentProject project) {
+        try {
+            ProductionDeployer deployer = RepositoryUtils.getRulesUserSession().getDeployer();
+            DesignTimeRepository dtr = RepositoryUtils.getWorkspace().getDesignTimeRepository();
 
-            Map<String, VersionRange> prMap = projects[i] = new HashMap<String, VersionRange>();
+            Collection<ProjectDescriptor> projectDescriptors = project
+                    .getProjectDescriptors();
+            Collection<Project> projects = new ArrayList<Project>();
 
-            String projectName = ddItem.getName();
-
-            if (!workspace.hasProject(projectName)) {
-                // no such project (was deleted?)
-                prMap.put(projectName, null);
-
-                ddItem.setMessages(new StringBuilder().append(
-                    "<span style='color:red;'>Cannot find project <b>")
-                    .append(StringEscapeUtils.escapeHtml(projectName))
-                    .append("</b> in the repository</span>.").toString());
-            } else {
-                RepositoryProject repositoryProject = dtr.getProject(projectName, ddItem.getVersion());
-
-                prMap.put(repositoryProject.getName(), new VersionRange(repositoryProject.getVersion(),
-                        repositoryProject.getVersion()));
-
-                // fill project's dependencies
-                for (ProjectDependency dep : repositoryProject.getDependencies()) {
-                    prMap.put(dep.getProjectName(), new VersionRange(dep.getLowerLimit(), dep.getUpperLimit()));
-                }
+            for (ProjectDescriptor pd : projectDescriptors) {
+                projects.add(dtr.getProject(pd.getProjectName(), pd.getProjectVersion()));
             }
-        }
 
-outer:
-        for (int i = 0; i < n; ++i) {
-            DeploymentDescriptorItem deploymentDescriptorItem = items.get(i);
-            for (Map.Entry<String, VersionRange> entries : projects[i].entrySet()) {
-                VersionRange range = entries.getValue();
-                for (int j = 0; j < n; ++j) {
-                    if (i != j) {
-                        VersionRange otherRange = projects[j].get(entries.getKey());
-                        if ((otherRange != null) && !range.intersects(otherRange)) {
-                            deploymentDescriptorItem.setMessages(new StringBuilder().append(
-                                    "<span style='color:red;'>Conflicting with project <b>")
-                                    .append(StringEscapeUtils.escapeHtml(items.get(j)
-                                            .getName()))
-                                    .append("</b></span>. Dependency causing conflict: <b>")
-                                    .append(StringEscapeUtils.escapeHtml(entries.getKey()))
-                                    .append("</b>").toString());
-                            continue outer; // one message is enough
-                        }
-                    }
-                }
-            }
+            DeployID id = getDeployID(project);
+            deployer.deploy(id, projects);
+
+            FacesContext.getCurrentInstance()
+                .addMessage(null,
+                    new FacesMessage(FacesMessage.SEVERITY_INFO,
+                        "Project '" + project.getName() + "' successfully deployed with id: " + id.getName(), null));
+        } catch (Exception e) {
+            FacesContext.getCurrentInstance()
+                .addMessage(null,
+                    new FacesMessage(FacesMessage.SEVERITY_ERROR, "Failed to deploy '" + project.getName() + "'",
+                        e.getMessage()));
+            log.error(e);
         }
     }
 
@@ -405,47 +337,5 @@ outer:
 
     public void setRepositoryTreeState(RepositoryTreeState repositoryTreeState) {
         this.repositoryTreeState = repositoryTreeState;
-    }
-
-    private static class VersionRange {
-        CommonVersion lower;
-        CommonVersion upper;
-
-        private VersionRange(CommonVersion lower, CommonVersion upper) {
-            this.lower = lower;
-            this.upper = upper;
-        }
-
-        private int compare(int i, int j) {
-            return (i == j) ? 0 : ((i < j) ? (-1) : 1);
-        }
-
-        public int compare(CommonVersion o1, CommonVersion o2) {
-            if ((o1 == null) || (o2 == null)) {
-                // null stands for posistive infinity
-                if (o1 != null) {
-                    return -1;
-                }
-                if (o2 != null) {
-                    return 1;
-                }
-                return 0;
-            }
-
-            int c = compare(o1.getMajor(), o2.getMajor());
-            if (c != 0) {
-                return c;
-            }
-            c = compare(o1.getMinor(), o2.getMinor());
-            if (c != 0) {
-                return c;
-            }
-            return compare(o1.getRevision(), o2.getRevision());
-        }
-
-        boolean intersects(VersionRange other) {
-            return (compare(lower, other.upper) <= 0)
-                && (compare(other.lower, upper) <= 0);
-        }
     }
 }
--- STUDIO/trunk/org.openl.rules.webstudio.java/src/org/openl/rules/webstudio/web/repository/DeploymentDescriptorItem.java
+++ STUDIO/trunk/org.openl.rules.webstudio.java/src/org/openl/rules/webstudio/web/repository/DeploymentDescriptorItem.java
@@ -2,28 +2,16 @@ package org.openl.rules.webstudio.web.repository;
 
 import org.openl.rules.repository.CommonVersion;
 
-import java.io.Serializable;
-
-
 /**
  * Represents a project in a deployment descriptor.
  */
-public class DeploymentDescriptorItem implements Serializable {
+public class DeploymentDescriptorItem extends AbstractItem {
     private static final long serialVersionUID = -3870494832804679843L;
-
-    /** Project name. */
-    private String name;
 
     /** Project version. */
     private CommonVersion version;
 
     private String versionName;
-
-    /** Messages associated with this entry. */
-    private String messages;
-
-    /** Boolean flag. If this entry is currently 'selected'. */
-    private boolean selected;
 
     public DeploymentDescriptorItem() {}
 
@@ -32,10 +20,10 @@ public class DeploymentDescriptorItem implements Serializable {
     }
 
     public DeploymentDescriptorItem(String name, CommonVersion version, String messages) {
-        this.name = name;
+        setName(name);
         this.version = version;
         versionName = version.getVersionName();
-        this.messages = messages;
+        setMessages(messages);
     }
 
     public CommonVersion getVersion() {
@@ -46,36 +34,12 @@ public class DeploymentDescriptorItem implements Serializable {
         this.version = version;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getVersionName() {
         return versionName;
     }
 
     public void setVersionName(String version) {
         versionName = version;
-    }
-
-    public String getMessages() {
-        return messages;
-    }
-
-    public void setMessages(String messages) {
-        this.messages = messages;
-    }
-
-    public boolean isSelected() {
-        return selected;
-    }
-
-    public void setSelected(boolean selected) {
-        this.selected = selected;
     }
 
     public boolean equals(Object o) {
@@ -88,12 +52,12 @@ public class DeploymentDescriptorItem implements Serializable {
 
         DeploymentDescriptorItem that = (DeploymentDescriptorItem) o;
 
-        return name.equals(that.name) && version.equals(that.version);
+        return getName().equals(that.getName()) && version.equals(that.version);
     }
 
     public int hashCode() {
         int result;
-        result = name.hashCode();
+        result = getName().hashCode();
         result = (31 * result) + version.hashCode();
         return result;
     }
