0cb04ec77ddd4993061975d584ef06a2bff9d74b
sshor@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <sshor@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2008-03-16 21:49:33 +0000

    EPBDS - moving to 5.0.6

--- DEV/trunk/org.openl.eclipse31-site/mavenrepo-plugins-install.bat
+++ DEV/trunk/org.openl.eclipse31-site/mavenrepo-plugins-install.bat
@@ -1,6 +1,8 @@
-call mvn install:install-file -DgroupId=org.openl.rules -DartifactId=org.openl.core -Dversion=5.0.5 -Dpackaging=jar -Dfile=plugins/org.openl.core_5.0.5.jar -DgeneratePom=true
-call mvn install:install-file -DgroupId=org.openl.rules -DartifactId=org.openl.commons -Dversion=5.0.5 -Dpackaging=jar -Dfile=plugins/org.openl.commons_5.0.5.jar -DgeneratePom=true
-call mvn install:install-file -DgroupId=org.openl.rules -DartifactId=org.openl.rules -Dversion=5.0.5 -Dpackaging=jar -Dfile=plugins/org.openl.rules_5.0.5.jar -DgeneratePom=true
-call mvn install:install-file -DgroupId=org.openl.rules -DartifactId=org.openl.rules.validator -Dversion=5.0.5 -Dpackaging=jar -Dfile=plugins/org.openl.rules.validator_5.0.5.jar -DgeneratePom=true
-call mvn install:install-file -DgroupId=org.openl.rules -DartifactId=org.openl.j -Dversion=5.0.5 -Dpackaging=jar -Dfile=plugins/org.openl.j_5.0.5.jar -DgeneratePom=true
-call mvn install:install-file -DgroupId=org.openl.rules -DartifactId=org.openl.rules.helpers -Dversion=5.0.5 -Dpackaging=jar -Dfile=plugins/org.openl.rules.helpers_5.0.5.jar -DgeneratePom=true
+SET OPENL_VERSION=5.0.6
+
+call mvn install:install-file -DgroupId=org.openl.rules -DartifactId=org.openl.core -Dversion=%OPENL_VERSION% -Dpackaging=jar -Dfile=plugins/org.openl.core_%OPENL_VERSION%.jar -DgeneratePom=true
+call mvn install:install-file -DgroupId=org.openl.rules -DartifactId=org.openl.commons -Dversion=%OPENL_VERSION% -Dpackaging=jar -Dfile=plugins/org.openl.commons_%OPENL_VERSION%.jar -DgeneratePom=true
+call mvn install:install-file -DgroupId=org.openl.rules -DartifactId=org.openl.rules -Dversion=%OPENL_VERSION% -Dpackaging=jar -Dfile=plugins/org.openl.rules_%OPENL_VERSION%.jar -DgeneratePom=true
+call mvn install:install-file -DgroupId=org.openl.rules -DartifactId=org.openl.rules.validator -Dversion=%OPENL_VERSION% -Dpackaging=jar -Dfile=plugins/org.openl.rules.validator_%OPENL_VERSION%.jar -DgeneratePom=true
+call mvn install:install-file -DgroupId=org.openl.rules -DartifactId=org.openl.j -Dversion=%OPENL_VERSION% -Dpackaging=jar -Dfile=plugins/org.openl.j_%OPENL_VERSION%.jar -DgeneratePom=true
+call mvn install:install-file -DgroupId=org.openl.rules -DartifactId=org.openl.rules.helpers -Dversion=%OPENL_VERSION% -Dpackaging=jar -Dfile=plugins/org.openl.rules.helpers_%OPENL_VERSION%.jar -DgeneratePom=true
