4a82066f8b74d078572775390458f61062c5247f
szyrianov <szyrianov@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2008-03-18 17:19:13 +0000

    EPBDS Removing Studio Rutime deployment. Branch to work on EPB 7.0.1 release was created

--- STUDIO/trunk/org.openl.rules.webstudio.java/src/org/openl/rules/webstudio/web/repository/RepositoryUtils.java
+++ STUDIO/trunk/org.openl.rules.webstudio.java/src/org/openl/rules/webstudio/web/repository/RepositoryUtils.java
@@ -8,20 +8,19 @@ import org.openl.rules.webstudio.web.jsf.JSFConst;
 import org.openl.rules.webstudio.web.jsf.util.FacesUtils;
 import org.openl.rules.webstudio.web.servlet.RulesUserSession;
 import org.openl.rules.workspace.abstracts.ProjectVersion;
-import org.openl.rules.workspace.deploy.DeployID;
 import org.openl.rules.workspace.uw.UserWorkspace;
-import org.openl.rules.workspace.uw.UserWorkspaceDeploymentProject;
 
 /**
  * Repository Utilities
  * 
  * @author Aleh Bykhavets
- * 
+ *
  */
 public class RepositoryUtils {
     private final static Log log = LogFactory.getLog(RepositoryUtils.class);
 
-    public static final Comparator<ProjectVersion> VERSIONS_REVERSE_COMPARATOR = new Comparator<ProjectVersion>() {
+    public static final Comparator<ProjectVersion> VERSIONS_REVERSE_COMPARATOR
+    = new Comparator<ProjectVersion>() {
         public int compare(ProjectVersion o1, ProjectVersion o2) {
             return o2.compareTo(o1);
         }
@@ -41,14 +40,5 @@ public class RepositoryUtils {
             log.error("Error obtaining user workspace", e);
         }
         return null;
-    }
-
-    public static DeployID getDeployID(UserWorkspaceDeploymentProject ddProject) {
-        StringBuilder sb = new StringBuilder(ddProject.getName());
-        ProjectVersion projectVersion = ddProject.getVersion();
-        if (projectVersion != null) {
-            sb.append('#').append(projectVersion.getVersionName());
-        }
-        return new DeployID(sb.toString());
     }
 }
