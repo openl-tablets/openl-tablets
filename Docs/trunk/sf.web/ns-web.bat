ae8976d5ea74f3a9734be8daba4dd700f7618272
sshor <sshor@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2008-03-21 03:15:08 +0000

    EPBDS - Initial import.

---
+++ Docs/trunk/sf.web/ns-web.bat
@@ -0,0 +1,6 @@
+
+cd workspace/sf.web
+
+psftp -b ns-web.ftp -bc naumshor@alpha.total-knowledge.com
+ 
+pause
---
+++ Docs/trunk/sf.web/sf-web.bat
@@ -0,0 +1,6 @@
+
+cd ../workspace/sf.web
+
+psftp -b sf-web.ftp -bc snshor@shell.sourceforge.net
+ 
+pause
