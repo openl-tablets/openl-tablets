56d1184be0afc096c0039c4fe17b32de14a24084
anaumenko <anaumenko@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2008-04-03 11:15:35 +0000

    EPBDS. Removed maven_integration branch.

--- STUDIO/branches/maven_integration/org.openl.rules.workspace/src/org/openl/rules/workspace/dtr/RepositoryDDProject.java
+++
@@ -1,8 +0,0 @@
-package org.openl.rules.workspace.dtr;
-
-import org.openl.rules.workspace.abstracts.DeploymentDescriptorProject;
-import org.openl.rules.workspace.abstracts.ProjectException;
-
-public interface RepositoryDDProject extends DeploymentDescriptorProject, RepositoryProject {
-    void update(DeploymentDescriptorProject srcArtefact) throws ProjectException;
-}
