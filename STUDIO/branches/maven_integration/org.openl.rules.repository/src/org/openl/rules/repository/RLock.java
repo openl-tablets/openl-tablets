77df1c7da7ac0251c0b8493f088a8ec002d40122
anaumenko@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <anaumenko@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2008-04-03 11:15:35 +0000

    EPBDS. Removed maven_integration branch.

--- STUDIO/branches/maven_integration/org.openl.rules.repository/src/org/openl/rules/repository/RLock.java
+++
@@ -1,9 +0,0 @@
-package org.openl.rules.repository;
-
-import java.util.Date;
-
-public interface RLock {
-    boolean isLocked();
-    Date getLockedAt();
-    CommonUser getLockedBy();
-}
--- STUDIO/branches/maven_integration/org.openl.rules.repository/src/org/openl/rules/repository/RPropertyType.java
+++
@@ -1,6 +0,0 @@
-package org.openl.rules.repository;
-
-public enum RPropertyType {
-    STRING,
-    DATE
-}
