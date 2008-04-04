e20842a166daabb0fcdc6ef80477d9e48adf2061
anaumenko@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <anaumenko@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2008-04-04 10:00:16 +0000

    EPBDS. Fixed merging bugs.

--- STUDIO/branches/iteration2_merge/org.openl.rules.repository.jcr/test/org/openl/rules/repository/PopulateDProjects.java
+++ STUDIO/branches/iteration2_merge/org.openl.rules.repository.jcr/test/org/openl/rules/repository/PopulateDProjects.java
@@ -16,20 +16,21 @@ public class PopulateDProjects {
                 RDeploymentDescriptorProject dp1 = repository.createDDProject("ddp1");
                 dp1.createProjectDescriptor("prj1").setProjectVersion(new RV2(0, 0, 10));
                 dp1.createProjectDescriptor("prj2").setProjectVersion(new RV2(0, 0, 20));
-                
+
                 dp1.commit(user);
             } else {
                 System.out.println("> Has some deployments projects");
             }
 
-            
             List<RDeploymentDescriptorProject> projects = repository.getDDProjects();
             System.out.println("> OpenL Rules Projects: " + projects.size());
             for (RDeploymentDescriptorProject prj : projects) {
-                System.out.println("  " + prj.getName() + " marked=" + prj.isMarked4Deletion() + " ver=" + prj.getActiveVersion().getVersionName());
-                
+                System.out.println("  " + prj.getName() + " marked=" + prj.isMarked4Deletion() + " ver="
+                        + prj.getActiveVersion().getVersionName());
+
                 for (RProjectDescriptor descr : prj.getProjectDescriptors()) {
-                    System.out.println("    " + descr.getProjectName() + " " + descr.getProjectVersion().getVersionName());
+                    System.out.println("    " + descr.getProjectName() + " "
+                            + descr.getProjectVersion().getVersionName());
                 }
             }
 
@@ -42,31 +43,41 @@ public class PopulateDProjects {
             }
         }
     }
-    
+
     private static class RV2 implements RVersion {
         private CommonVersion version;
 
         private RV2(int major, int minor, int revision) {
             version = new CommonVersionImpl(major, minor, revision);
         }
+
         public Date getCreated() {
             return null;
         }
+
         public CommonUser getCreatedBy() {
             return null;
         }
+
         public int getMajor() {
             return version.getMajor();
         }
+
         public int getMinor() {
             return version.getMinor();
         }
+
         public int getRevision() {
             return version.getRevision();
         }
+
         public String getVersionName() {
             return version.getVersionName();
         }
-        
+
+        public int compareTo(CommonVersion o) {
+            return new CommonVersionImpl(this).compareTo(o);
+        }
+
     }
 }
