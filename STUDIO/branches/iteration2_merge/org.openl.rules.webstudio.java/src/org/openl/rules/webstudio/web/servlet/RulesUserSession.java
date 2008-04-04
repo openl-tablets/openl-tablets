a9246a13512836dcd66b047fff63def60aae4f34
anaumenko@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <anaumenko@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2008-04-04 11:32:04 +0000

    EPBDS. Fixed merging bugs.

--- STUDIO/branches/iteration2_merge/org.openl.rules.webstudio.java/src/org/openl/rules/webstudio/web/servlet/RulesUserSession.java
+++ STUDIO/branches/iteration2_merge/org.openl.rules.webstudio.java/src/org/openl/rules/webstudio/web/servlet/RulesUserSession.java
@@ -1,18 +1,26 @@
 package org.openl.rules.webstudio.web.servlet;
 
+import org.acegisecurity.userdetails.UserDetails;
 import org.openl.rules.workspace.MultiUserWorkspaceManager;
 import org.openl.rules.workspace.WorkspaceException;
 import org.openl.rules.workspace.WorkspaceUser;
+import org.openl.rules.workspace.WorkspaceUserImpl;
 import org.openl.rules.workspace.abstracts.ProjectException;
+import org.openl.rules.workspace.deploy.DeploymentException;
+import org.openl.rules.workspace.deploy.ProductionDeployer;
+import org.openl.rules.workspace.deploy.ProductionDeployerManager;
+import org.openl.rules.workspace.deploy.impl.ProductionDeployerManagerImpl;
 import org.openl.rules.workspace.uw.UserWorkspace;
 import org.openl.util.Log;
 
 public class RulesUserSession {
-    private WorkspaceUser user;
+    private UserDetails user;
     private UserWorkspace userWorkspace;
     private MultiUserWorkspaceManager workspaceManager;
+    private ProductionDeployer deployer;
+    private ProductionDeployerManager deployerManager = new ProductionDeployerManagerImpl();
 
-    public void setUser(WorkspaceUser user) {
+    public void setUser(UserDetails user) {
         this.user = user;
     }
 
@@ -26,13 +34,26 @@ public class RulesUserSession {
 
     public synchronized UserWorkspace getUserWorkspace() throws WorkspaceException, ProjectException {
         if (userWorkspace == null) {
-            userWorkspace = workspaceManager.getUserWorkspace(user);
+            userWorkspace = workspaceManager.getUserWorkspace(new WorkspaceUserImpl(getUserName()));
             userWorkspace.activate();
         }
 
         return userWorkspace;
     }
 
+    public String getUserName() {
+        if (user == null) {
+            return null;
+        }
+        return user.getUsername();
+    }
+
+    public synchronized ProductionDeployer getDeployer() throws DeploymentException {
+        if (deployer == null) {
+            deployer = deployerManager.getDeployer(new WorkspaceUserImpl(user.getUsername()));
+        }
+        return deployer;
+    }
 
     public void sessionWillPassivate() {
         userWorkspace.passivate();
