3e2da322aa18cc3ff67d00ee30f0683b8ea011d9
szyrianov@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <szyrianov@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2008-04-21 13:11:07 +0000

    EPBDS Removing branch iteration2_initial. It has been already merged into iteration2_merge. It's not required any more

--- STUDIO/branches/iteration2_initial/org.openl.rules.workspace/src/org/openl/rules/workspace/uw/UserWorkspaceListener.java
+++
@@ -1,7 +0,0 @@
-package org.openl.rules.workspace.uw;
-
-import java.util.EventListener;
-
-public interface UserWorkspaceListener extends EventListener {
-    void workspaceReleased(UserWorkspace workspace);
-}
--- WSFrontend/branches/iteration2_initial/org.openl.rules.ruleservice/src/org/openl/rules/ruleservice/publish/DeployAdmin.java
+++
@@ -1,8 +0,0 @@
-package org.openl.rules.ruleservice.publish;
-
-import java.util.List;
-
-public interface DeployAdmin {
-    void deploy(String serviceName, ClassLoader loader, List<WSInfo> infoList);
-    void undeploy(String serviceName);
-}
