7386fbd38c2273da52c92aa3f457cad4c9de03e6
sshor@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <sshor@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2008-04-30 20:57:24 +0000

    EPBDS - from trunk

--- DEV/branches/iteration2_merge/org.openl.test/tst_openl.j/org/openl/binding/RunTest.java
+++ DEV/branches/iteration2_merge/org.openl.test/tst_openl.j/org/openl/binding/RunTest.java
@@ -17,6 +17,16 @@ public class RunTest extends TestCase
 
 	public void testRun()
 	{
+
+		_runNoError("String x=null; x == null || x.length() < 10", true, "org.openl.j");
+		_runNoError("String x=null; x != null && x.length() < 10", false, "org.openl.j");
+
+		_runNoError("String x=null; Boolean b = true; b || x.length() < 10", true, "org.openl.j");
+		_runNoError("String x=null; Boolean b = false; b && x.length() < 10", false, "org.openl.j");
+
+		_runNoError("String x=\"abc\"; x == null || x.length() < 10", true, "org.openl.j");
+		_runNoError("String x=\"abc\"; x != null && x.length() < 10", true, "org.openl.j");
+		
 		_runNoError("int x = 5; x += 4", new Integer(9), "org.openl.j");
 		_runNoError("DoubleValue d1 = new DoubleValue(5); DoubleValue d2 = new DoubleValue(4); d1 += d2; d1.getValue()", new Double(9), "org.openl.rules.java");
 		_runNoError("int i=0; for(int j=0; j < 10; ) {i += j;j++;} i", new Integer(45), "org.openl.j");
@@ -45,6 +55,8 @@ public class RunTest extends TestCase
 		_runNoError("System.out << 35 << \"zzzz\" ", System.out, "org.openl.j");
 		
 		_runNoError("|-10| ", 10, "org.openl.j");
+		
+		
 		
 		
 		
