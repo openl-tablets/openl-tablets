80a1f9bf126836fd7ffabf705119c869a1f55122
aantonik@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <aantonik@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2008-04-30 08:05:34 +0000

    EPBDS-244 fixing natures order when creating new project from plugin, in order to make OpenL icon show

--- DEV/branches/iteration2_merge/org.openl.eclipse.ui.wizard/src/org/openl/eclipse/wizard/base/NewProjectFromTemplateWizard.java
+++ DEV/branches/iteration2_merge/org.openl.eclipse.ui.wizard/src/org/openl/eclipse/wizard/base/NewProjectFromTemplateWizard.java
@@ -103,7 +103,7 @@ public class NewProjectFromTemplateWizard
                     }
 
                     final String[] natures = new String[] {
-                            JavaCore.NATURE_ID, PDE.PLUGIN_NATURE, IOpenlConstants.OPENL_NATURE_ID
+                            IOpenlConstants.OPENL_NATURE_ID, JavaCore.NATURE_ID, PDE.PLUGIN_NATURE
                     };
 
                     for (String nature : natures) {
