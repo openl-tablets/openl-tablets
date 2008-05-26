21a34ad606cded0a2dd0a3bf81a1ff366f2c1ac0
aantonik <aantonik@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2008-05-26 11:30:07 +0000

    EPBDS updating template (fixed Return button in dialogs in IE)

--- DEV/branches/iteration2_merge/org.openl.rules.eclipse.ui.wizard/templates/org.openl.rules.webstudio/webapps/webstudio/javascript/common.js
+++ DEV/branches/iteration2_merge/org.openl.rules.eclipse.ui.wizard/templates/org.openl.rules.webstudio/webapps/webstudio/javascript/common.js
@@ -10,3 +10,16 @@ function focusElement(elementId) {
     field.focus();
   }
 }
+
+function modalInputKeyPress(event, elm) {
+  if (Prototype.Browser.IE) {
+    event = event || window.event;
+    if (event.keyCode == 13) {
+      var submitBtn = document.getElementById(elm.form.name + ":sbt")
+      if (submitBtn)
+      document.getElementById(elm.form.name + ":sbt").click();
+      return false;
+    }
+  }
+  return true;
+}
