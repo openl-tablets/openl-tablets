8a4e1d7a1891c98abd0de12ef5a9a5f5bdaa58ae
anaumenko <anaumenko@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2008-09-26 16:21:17 +0000

    EPBDS. Removed wrong tag release-5.0.6

--- STUDIO/tags/release-5.0.6/org.openl.rules.workspace/src/org/openl/rules/workspace/abstracts/ProjectResource.java
+++
@@ -1,9 +0,0 @@
-package org.openl.rules.workspace.abstracts;
-
-import java.io.InputStream;
-
-public interface ProjectResource extends ProjectArtefact {
-    InputStream getContent() throws ProjectException;
-
-    String getResourceType();
-}
--- STUDIO/tags/release-5.0.6/org.openl.rules.workspace/src/org/openl/rules/workspace/dtr/RepositoryProjectResource.java
+++
@@ -1,6 +0,0 @@
-package org.openl.rules.workspace.dtr;
-
-import org.openl.rules.workspace.abstracts.ProjectResource;
-
-public interface RepositoryProjectResource extends ProjectResource, RepositoryProjectArtefact {
-}
