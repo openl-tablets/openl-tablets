a6514bff24e403c44714c0b9e93ae9c2f26380c7
abykhavets <abykhavets@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2008-10-17 08:27:21 +0000

    EPBDS-316. Apply fixes for EPB 7.0.2 to 5.1.1 version
    Porting from 5.1.0.  Name of template was changed from 'exigen' to 'maven'.

--- DEV/branches/iteration2_merge/org.openl.eclipse.ui.util/src/org/openl/eclipse/ui/util/OpenLUtil.java
+++ DEV/branches/iteration2_merge/org.openl.eclipse.ui.util/src/org/openl/eclipse/ui/util/OpenLUtil.java
@@ -15,7 +15,7 @@ public class OpenLUtil {
 
     private static NewProjectFromTemplateWizardCustomizer getCustomizer() {
         return new NewProjectFromTemplateWizardCustomizer(Activator.getDefault().getBundle(),
-                "NewSimpleOpenLRulesProject") {};
+                "MavenSimpleOpenLRulesProject") {};
     }
 
     public static void removeOpenLCapabilities(IProject project) throws CoreException {
