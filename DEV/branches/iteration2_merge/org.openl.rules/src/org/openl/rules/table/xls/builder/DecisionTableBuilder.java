988bc44a875229a6fd72938e4aeffb130d3863ab
aastrouski <aastrouski@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2008-10-21 10:01:07 +0000

    EPBDS-323: Refactore TableBuilders to extend one base TableBuilder

--- DEV/branches/iteration2_merge/org.openl.rules/src/org/openl/rules/table/xls/builder/DecisionTableBuilder.java
+++ DEV/branches/iteration2_merge/org.openl.rules/src/org/openl/rules/table/xls/builder/DecisionTableBuilder.java
@@ -1,127 +1,49 @@
 package org.openl.rules.table.xls.builder;
 
-import java.io.IOException;
-
 import org.openl.rules.table.xls.XlsSheetGridModel;
-import org.openl.rules.table.IGridRegion;
-import org.openl.rules.table.GridRegion;
-import org.openl.rules.table.ui.ICellStyle;
 import org.openl.rules.lang.xls.IXlsTableNames;
-import org.apache.poi.hssf.usermodel.HSSFCellStyle;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.hssf.usermodel.HSSFFont;
-import org.apache.poi.hssf.usermodel.HSSFCell;
-import org.apache.poi.hssf.util.HSSFColor;
 
 /**
  * Helper class that allows creating new decision tables in specified excel sheet.
  *
- * @author Aliaksandr Antonik.
+ * @author Aliaksandr Antonik
  */
-public class DecisionTableBuilder {
+public class DecisionTableBuilder extends TableBuilder {
+
     /**
-     * Number of rows that usually are used for table header. That is the header cell identifying type of a table,
-     * actions & conditions title, logic, parameter declarations, paramater business names.   
+     * Number of rows that usually are used for table logic element.
+     * That is actions & conditions title, logic, parameter declarations,
+     * paramater business names.   
      */
-    public static final int HEADER_HEIGHT = 5;
-    /**
-     * The sheet to write tables to.
-     */
-    private final XlsSheetGridModel gridModel;
-    /**
-     * Current table region in excel sheet.
-     */
-    private IGridRegion region;
-    private HSSFCellStyle style;
+    public static final int LOGIC_ELEMENT_HEIGHT = 4;
+
     private int elementColumn;
-    private int width, height;
 
     /**
      * Creates new instance.
      *
-     * @param gridModel represents interface for operations with excel sheets.
+     * @param gridModel represents interface for operations with excel sheets
      */
     public DecisionTableBuilder(XlsSheetGridModel gridModel) {
-        if (gridModel == null)
-            throw new IllegalArgumentException("gridModel is null");
-        this.gridModel = gridModel;
+        super(gridModel);
     }
 
-    /**
-     * Begins writing a table.
-     * 
-     * @param width table width in cells
-     * @param height tablel height in cells
-     * 
-     * @throws CreateTableException if unable to create table
-     * @throws IllegalStateException if <code>beginTable()</code> has already been called without subsequent
-     * <code>endTable()</code>.
-     */
+    @Override
     public void beginTable(int width, int height) throws CreateTableException {
-        if (region != null) {
-            throw new IllegalStateException("beginTable has already been called");
-        }
-
-        this.width = width; 
-        this.height = height; 
-        region = gridModel.findEmptyRect(width, height);
-        if (region == null) {
-            throw new CreateTableException("could not find appropriate region for writing");
-        }
-
+        super.beginTable(width, height);
         elementColumn = 0;
-        initStyle();
     }
 
     /**
-     * Finishes writing a table. Saves the changes to excel sheet.
-     *
-     * @throws IOException if an exception occurred when saving.
-     */
-    public void endTable() throws IOException {
-        if (region == null) {
-            throw new IllegalStateException("endTable() call without prior beginTable() call");
-        }
-
-        for (int y = HEADER_HEIGHT; y < height; ++y) {
-            for (int x = 0; x < width; ++x) {
-                writeCell(x, y, 1, "");
-            }
-        }
-
-        region = null;
-        gridModel.getSheetSource().getWorkbookSource().save();
-    }
-
-    /**
-     * Writes decision table header. Requires the header signature, e.g. <br/> <code><i>void hello1(int hour)</i></code>.
+     * Writes decision table header. <br />
+     * Requires the header signature, e.g. <code><i>void hello1(int hour)</i></code><br/>
+     * without Decision table header token <code>Rules</code>
      * 
      * @param signature method signature for the table.
      */
     public void writeHeader(String signature) {
-        if (region == null) {
-            throw new IllegalStateException("beginTable has to be called");
-        }
-
         String headerText = IXlsTableNames.DECISION_TABLE2 + " " + signature;
-        writeCell(0, 0, width, headerText);
-    }
-
-    private void writeCell(int x, int y, int width, String value) {
-        x += region.getLeft();
-        y += region.getTop();
-        if (width == 1) {
-            HSSFCell cell = gridModel.createNewCell(x, y);
-            cell.setCellValue(value);
-            cell.setCellStyle(style);
-        } else {
-            int x2 = x + width - 1; 
-            gridModel.addMergedRegion(new GridRegion(y, x, y, x2));
-            gridModel.setCellValue(x, y, value);
-            for (int col = x; col <= x2; ++col) {
-                gridModel.createNewCell(col, region.getTop()).setCellStyle(style);
-            }
-        }
+        super.writeHeader(headerText, null);
     }
 
     /**
@@ -148,42 +70,43 @@ public class DecisionTableBuilder {
      * @param parameterSignatures array of element parameter signatures
      */
     public void writeElement(String title, String logic, String[] parameterNames, String[] parameterSignatures) {
-        if (parameterNames == null || parameterNames.length == 0)
-            throw new IllegalArgumentException("parameterNames must be not null array of positive length");
-        if (parameterSignatures == null || parameterSignatures.length == 0)
-            throw new IllegalArgumentException("parameterSignatures must be not null array of positive length");
-        if (parameterSignatures.length != parameterNames.length)
-            throw new IllegalArgumentException("numbers of parameter names and parameter signatures must be equal");
-
-        int elementWidth = parameterNames.length;
-        if (elementColumn + elementWidth > height) {
-            throw new IllegalStateException("total elements width is too big, expected height = " + height);
+        if (getTableRegion() == null) {
+            throw new IllegalStateException("beginTable() has to be called");
+        }
+        if (parameterNames == null || parameterNames.length == 0) {
+            throw new IllegalArgumentException(
+                    "parameterNames must be not null array of positive length");
+        }
+        if (parameterSignatures == null || parameterSignatures.length == 0) {
+            throw new IllegalArgumentException(
+                    "parameterSignatures must be not null array of positive length");
+        }
+        if (parameterSignatures.length != parameterNames.length) {
+            throw new IllegalArgumentException(
+                    "numbers of parameter names and parameter signatures must be equal");
         }
 
-        writeCell(elementColumn, 1, elementWidth, title);
-        writeCell(elementColumn, 2, elementWidth, logic);
+        int elementWidth = parameterNames.length;
+        if (elementColumn + elementWidth > getHeight()) {
+            throw new IllegalStateException("total elements width is too big, expected height = " + getHeight());
+        }
+
+        writeCell(elementColumn, getCurrentRow(), elementWidth, 1, title);
+        writeCell(elementColumn, getCurrentRow() + 1, elementWidth, 1, logic);
         for (int i = 0; i < elementWidth; ++i) {
-            writeCell(elementColumn + i, 3, 1, parameterSignatures[i]);
-            writeCell(elementColumn + i, 4, 1, parameterNames[i]);
+            writeCell(elementColumn + i, getCurrentRow() + 2, 1, 1, parameterSignatures[i]);
+            writeCell(elementColumn + i, getCurrentRow() + 3, 1, 1, parameterNames[i]);
         }
 
         elementColumn += elementWidth;
     }
 
-    private HSSFCellStyle initStyle() {
-        if (style == null) {
-            HSSFWorkbook workbook = gridModel.getSheetSource().getWorkbookSource().getWorkbook();
-            HSSFCellStyle cellStyle = workbook.createCellStyle();
-
-            cellStyle.setBorderBottom(ICellStyle.BORDER_THIN);
-            cellStyle.setBorderTop(ICellStyle.BORDER_THIN);
-            cellStyle.setBorderLeft(ICellStyle.BORDER_THIN);
-            cellStyle.setBorderRight(ICellStyle.BORDER_THIN);
-
-            style = cellStyle;
+    @Override
+    public void endTable() throws CreateTableException {
+        if (elementColumn > 0) {
+            incCurrentRow(LOGIC_ELEMENT_HEIGHT);
         }
-
-        return style;
+        super.endTable();
     }
-    
+
 }
