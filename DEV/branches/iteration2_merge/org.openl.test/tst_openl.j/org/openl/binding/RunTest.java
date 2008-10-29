dd9048f3bf3a14e09038afd632c3b4c2783390c0
sshor <sshor@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2008-10-29 19:08:29 +0000

    EPBDS - test parsing Ranges using Grammar

--- DEV/branches/iteration2_merge/org.openl.test/tst_openl.j/org/openl/binding/RunTest.java
+++ DEV/branches/iteration2_merge/org.openl.test/tst_openl.j/org/openl/binding/RunTest.java
@@ -1,7 +1,9 @@
 package org.openl.binding;
 
 import org.openl.OpenL;
+import org.openl.rules.helpers.IntRange;
 import org.openl.syntax.impl.StringSourceCodeModule;
+import org.openl.util.RangeWithBounds;
 
 import junit.framework.Assert;
 import junit.framework.TestCase;
@@ -10,8 +12,13 @@ public class RunTest extends TestCase
 {
 	void _runNoError(String expr, Object expected, String openl)
 	{
+		_runNoError(expr, expected, openl, "method.body");
+	}	
+	
+	void _runNoError(String expr, Object expected, String openl, String parseType)
+	{
 		OpenL op = OpenL.getInstance(openl);
-		Object res = op.evaluate(new StringSourceCodeModule(expr, null));
+		Object res = op.evaluate(new StringSourceCodeModule(expr, null), parseType);
 		Assert.assertEquals(expected, res);
 	}
 
@@ -55,12 +62,28 @@ public class RunTest extends TestCase
 		_runNoError("System.out << 35 << \"zzzz\" ", System.out, "org.openl.j");
 		
 		_runNoError("|-10| ", 10, "org.openl.j");
+		_runNoError("true ? 10 : 20", 10, "org.openl.j");
+		_runNoError("true ? 10 : 20", 10, "org.openl.j");
+		_runNoError("false ? 10 : 20", 20, "org.openl.j");
+		
+		_runNoError("10%", 0.1, "org.openl.j");
+		_runNoError("10% of \n the  50", 5.0, "org.openl.j");
 		
 		
-		
-		
+		_runNoError("long Of =-1;int y=60; Of >>> y ", (long)-1 >>> 60, "org.openl.j");
 		
 	}
+	
+	public void testRange()
+	{
+		_runNoError("< 10K", new RangeWithBounds(Integer.MIN_VALUE,9999), "org.openl.j", "range.literal");
+		_runNoError("<=33.3M", new RangeWithBounds(Integer.MIN_VALUE,33300000), "org.openl.j", "range.literal");
+		_runNoError("5-$10", new RangeWithBounds(5,10), "org.openl.j", "range.literal");
+		_runNoError("2B<", new RangeWithBounds(2000000001,Integer.MAX_VALUE), "org.openl.j", "range.literal");
+		_runNoError("2.1B+", new RangeWithBounds(2100000000,Integer.MAX_VALUE), "org.openl.j", "range.literal");
+		
+	}
+
 
 	
 	public static void main(String[] args)
