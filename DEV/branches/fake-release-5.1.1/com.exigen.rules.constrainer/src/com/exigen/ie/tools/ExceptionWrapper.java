4e9af70d32b7102b725df9cabd3d6e08ba6e8489
szyrianov@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <szyrianov@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2008-11-18 16:10:40 +0000

    EPBDS. Removed unused branch

--- DEV/branches/fake-release-5.1.1/com.exigen.rules.constrainer/src/com/exigen/ie/tools/ExceptionWrapper.java
+++
@@ -1,8 +0,0 @@
-package com.exigen.ie.tools;
-
-public interface ExceptionWrapper
-{
-  //Get the thrown target exception
-
-  public Throwable getTargetException();
-}
--- DEV/branches/fake-release-5.1.1/org.openl.rules.eclipse.ui.wizard/templates/org.openl.examples/RunHelloXmlPeopleOpenl.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-cd %~dp0
-
-@%~dp0/../lang.config/runopenl -file openl/HelloXmlPeople.openl main
-
-pause
-
