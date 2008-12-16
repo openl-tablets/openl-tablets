f6051a156699819363ab20ed4c2c4fe35a27b14e
sshor <sshor@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2008-12-16 02:53:56 +0000

    EPBDS-376 EPBDS continue development

---
+++ DEV/branches/iteration2_merge/org.openl.rules/src/org/openl/rules/calc/ASCellField.java
@@ -0,0 +1,28 @@
+package org.openl.rules.calc;
+
+import org.openl.types.IOpenClass;
+import org.openl.vm.IRuntimeEnv;
+
+public class ASCellField extends ASpreadsheetField {
+
+	public ASCellField(IOpenClass declaringClass, String name, SCell cell) {
+		super(declaringClass, name, cell.getType());
+		this.cell = cell;
+	}
+
+	SCell cell;
+
+	@Override
+	public Object calculate(SpreadsheetResult spreadsheetResult,
+			Object targetModule, Object[] params, IRuntimeEnv env) 
+	{
+		if (cell.isValueCell())
+			return cell.getValue();
+		
+		if (cell.isMethodCell())
+			return cell.getMethod().invoke(spreadsheetResult, params, env);
+		
+		return null;
+	}
+	
+}
