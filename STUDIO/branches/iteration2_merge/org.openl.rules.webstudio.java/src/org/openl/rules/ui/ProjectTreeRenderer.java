f7328014c016760ad04f7d4734fe6a3c62918cec
aastrouski <aastrouski@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2008-12-20 15:33:27 +0000

    EPBDS-394 Fix error links

--- STUDIO/branches/iteration2_merge/org.openl.rules.webstudio.java/src/org/openl/rules/ui/ProjectTreeRenderer.java
+++ STUDIO/branches/iteration2_merge/org.openl.rules.webstudio.java/src/org/openl/rules/ui/ProjectTreeRenderer.java
@@ -3,7 +3,9 @@
  */
 package org.openl.rules.ui;
 
+import org.openl.binding.impl.BoundError;
 import org.openl.rules.lang.xls.ITableNodeTypes;
+import org.openl.rules.lang.xls.syntax.TableSyntaxNode;
 import org.openl.util.ITreeElement;
 
 /**
@@ -47,13 +49,19 @@ public class ProjectTreeRenderer extends DTreeRenderer implements IProjectTypes,
         this.project = project;
     }
 
+    @SuppressWarnings("unchecked")
     protected String makeURL(ITreeElement element) {
-
         if (element.getType().startsWith(PT_TABLE + "."))
             return targetJsp + "?elementID=" + map.getID(element);
 
-        if (element.getType().startsWith(PT_PROBLEM))
-            return "jsp/showError.jsp" + "?elementID=" + map.getID(element);
+        if (element.getType().startsWith(PT_PROBLEM)) {
+            final BoundError problem = (BoundError) ((ProjectTreeElement) element).getProblem();
+            final TableSyntaxNode tsn = (TableSyntaxNode) problem.getTopLevelSyntaxNode();
+            int tableId = project.indexForNode(tsn);
+            return "faces/facelets/tableeditor/showError.xhtml"
+                + "?elementID=" + map.getID(element)
+                + "&tableID=" + tableId;
+        }
 
         return null;
     }
