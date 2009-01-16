86ec23e096485a20d48f7cbf84e71d4df0b95c94
pudalau <pudalau@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2009-01-16 17:20:43 +0000

    EPBDS-428.[maven-release-plugin] copy for tag tech-release-5.2.0.001

---
+++ STUDIO/tags/tech-release-5.2.0.001/org.openl.rules.tableeditor/src/org/openl/rules/tableeditor/renderkit/TableViewerRenderer.java
@@ -0,0 +1,31 @@
+package org.openl.rules.tableeditor.renderkit;
+
+import java.io.IOException;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+
+import org.openl.rules.table.IGridTable;
+import org.openl.rules.tableeditor.util.Constants;
+
+public class TableViewerRenderer extends BaseRenderer {
+
+    @Override
+    public void encodeBegin(FacesContext context, UIComponent component)
+            throws IOException {
+        IGridTable table = (IGridTable) component.getAttributes().get(
+                Constants.ATTRIBUTE_TABLE);
+        new HTMLRenderer().render(table);
+    }
+
+    @Override
+    public boolean getRendersChildren() {
+        return true;
+    }
+
+    @Override
+    public void encodeChildren(FacesContext arg0, UIComponent arg1)
+            throws IOException {
+    }
+
+}
---
+++ STUDIO/tags/tech-release-5.2.0.001/org.openl.rules.webstudio.java/src/org/openl/rules/webstudio/web/ShowTableErrorBean.java
@@ -0,0 +1,34 @@
+package org.openl.rules.webstudio.web;
+
+import org.openl.rules.ui.WebStudio;
+import org.openl.rules.web.jsf.util.FacesUtils;
+import org.openl.rules.webstudio.web.util.WebStudioUtils;
+
+public class ShowTableErrorBean {
+
+    public ShowTableErrorBean() {
+    }
+
+    public int getErrorId() {
+        return getElementId("elementID");
+    }
+
+    public int getTableId() {
+        return getElementId("tableID");
+    }
+
+    private int getElementId(String name) {
+        int elementId = -1;
+        String elementIdStr = FacesUtils.getRequestParameter(name);
+        if (elementIdStr != null) {
+            elementId = Integer.parseInt(elementIdStr);
+        }
+        return elementId;
+    }
+
+    public String getError() {
+        WebStudio webStudio = WebStudioUtils.getWebStudio();
+        return (String) webStudio.getModel().showError(getErrorId());
+    }
+
+}
