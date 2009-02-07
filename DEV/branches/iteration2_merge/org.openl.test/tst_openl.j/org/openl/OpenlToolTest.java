fcea4f197e18389cca2f1c27f06dec1497efda19
sshor <sshor@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2009-02-07 09:48:52 +0000

    EPBDS-432 EPBDS-432 fixing utility methods to compile OpenL code and dynamically define return type

--- DEV/branches/iteration2_merge/org.openl.test/tst_openl.j/org/openl/OpenlToolTest.java
+++ DEV/branches/iteration2_merge/org.openl.test/tst_openl.j/org/openl/OpenlToolTest.java
@@ -1,8 +1,13 @@
 package org.openl;
 import org.openl.OpenL;
 import org.openl.OpenlTool;
+import org.openl.binding.IBindingContext;
+import org.openl.meta.StringValue;
 import org.openl.syntax.impl.StringSourceCodeModule;
+import org.openl.types.IMethodSignature;
 import org.openl.types.IOpenClass;
+import org.openl.types.IOpenMethod;
+import org.openl.types.java.JavaOpenClass;
 
 import junit.framework.Assert;
 import junit.framework.TestCase;
@@ -47,7 +52,28 @@ public class OpenlToolTest extends TestCase
 		
 	}
 	
-
+	public void testMakeMethod()
+	{
+		StringValue srcCode = new StringValue("5");
+		
+		OpenL openl = OpenL.getInstance("org.openl.j");
+		String name = "abc";
+		IMethodSignature signature = IMethodSignature.VOID;
+		IOpenClass declaringClass = null;
+		int depthParameterSearchLevel = 1;
+		IBindingContext cxt = openl.getBinder().makeBindingContext();
+		
+		IOpenMethod m = OpenlTool.makeMethodWithUnknownType(srcCode.asSourceCodeModule(), 
+				openl, name, signature, declaringClass, depthParameterSearchLevel, cxt);
+		assertEquals( JavaOpenClass.INT, m.getType());
+		
+		
+		srcCode = new StringValue("if (true) return 5.0; else return 9.1;");
+		m = OpenlTool.makeMethodWithUnknownType(srcCode.asSourceCodeModule(), 
+				openl, name, signature, declaringClass, depthParameterSearchLevel, cxt);
+		assertEquals( JavaOpenClass.DOUBLE, m.getType());
+		
+	}
 
 
 }
