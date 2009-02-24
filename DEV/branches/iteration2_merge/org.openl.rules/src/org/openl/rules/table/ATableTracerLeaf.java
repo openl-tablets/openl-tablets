d9633d9498b3fabfa5e6a7b7029273a3185ab27a
abykhavets@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <abykhavets@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2009-02-24 16:38:47 +0000

    EPBDS fix tracers

--- DEV/branches/iteration2_merge/org.openl.rules/src/org/openl/rules/table/ATableTracerLeaf.java
+++ DEV/branches/iteration2_merge/org.openl.rules/src/org/openl/rules/table/ATableTracerLeaf.java
@@ -12,6 +12,13 @@ public abstract class ATableTracerLeaf extends ITracerObject.SimpleTracerObject 
     }
     
     public ITableTracerObject[] getTableTracers() {
-        return new ITableTracerObject[]{this};
+        ITracerObject[] tracerObjects = getTracerObjects();
+
+        int size = tracerObjects.length;
+        ITableTracerObject[] temp = new ITableTracerObject[size];
+
+        System.arraycopy(tracerObjects, 0, temp, 0, size);
+
+        return temp;
     }
 }
