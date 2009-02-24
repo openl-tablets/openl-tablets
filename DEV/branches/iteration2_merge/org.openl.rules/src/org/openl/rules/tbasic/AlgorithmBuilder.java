df621027f5269d4989dd0d0214b6b4c1173bf428
szyrianov@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <szyrianov@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2009-02-24 17:08:58 +0000

    EPBDS-432 Tried to add MetaInfo for Algorithm operation

--- DEV/branches/iteration2_merge/org.openl.rules/src/org/openl/rules/tbasic/AlgorithmBuilder.java
+++ DEV/branches/iteration2_merge/org.openl.rules/src/org/openl/rules/tbasic/AlgorithmBuilder.java
@@ -8,11 +8,14 @@ import java.util.Map;
 import org.openl.binding.IBindingContext;
 import org.openl.meta.StringValue;
 import org.openl.rules.lang.xls.syntax.TableSyntaxNode;
+import org.openl.rules.lang.xls.types.CellMetaInfo;
 import org.openl.rules.table.IGridTable;
 import org.openl.rules.table.ILogicalTable;
+import org.openl.rules.table.IWritableGrid;
 import org.openl.rules.table.ui.ICellStyle;
 import org.openl.rules.tbasic.compile.AlgorithmCompiler;
 import org.openl.syntax.impl.SyntaxError;
+import org.openl.types.java.JavaOpenClass;
 
 public class AlgorithmBuilder {
 
@@ -78,20 +81,20 @@ public class AlgorithmBuilder {
         for (int r = 0; r < grid.getLogicalHeight(); r++) {
 
             AlgorithmRow aRow = new AlgorithmRow();
-            
+
             // set sequential number of the row in table
             aRow.setRowNumber(r + 1);
-            
+
             ILogicalTable rowTable = grid.getLogicalRow(r);
             aRow.setGridRegion(rowTable.getGridTable().getRegion());
-            
+
             // parse data row
             for (AlgorithmColumn column : columns.values()) {
                 int c = column.columnIndex;
-                
+
                 ILogicalTable valueTable = rowTable.getLogicalColumn(c);
                 aRow.setValueGridRegion(column.id, valueTable.getGridTable().getRegion());
-                
+
                 String value = grid.getStringValue(c, r);
                 String uri = grid.getUri(c, r);
 
@@ -106,6 +109,7 @@ public class AlgorithmBuilder {
                     ICellStyle cellStyle = grid.getCellStyle(c, r);
                     int i = (cellStyle == null) ? 0 : cellStyle.getIdent();
                     aRow.setOperationLevel(i);
+                    bindMetaInfo(grid, c, r);
                 }
             }
 
@@ -113,6 +117,43 @@ public class AlgorithmBuilder {
         }
 
         return result;
+    }
+
+    /**
+     * Sets CellMetaInfo for operation cell. Thus, editor can use special
+     * controller to validate/limit user input.
+     * 
+     * @param c
+     * @param r
+     * @param grid
+     */
+    private void bindMetaInfo(IGridTable grid, int c, int r) {
+        CellMetaInfo meta = new CellMetaInfo(CellMetaInfo.Type.DT_CA_CODE, null, JavaOpenClass
+                .getOpenClass(AlgorithmOperations.class));
+        IWritableGrid wgrid = IWritableGrid.Tool.getWritableGrid(grid);
+        wgrid.setCellMetaInfo(c, r, meta);
+    }
+
+    // FIXME: eliminate static enum and replace it with rules based data type
+    // (if possible...)
+    public static enum AlgorithmOperations {
+        SET,
+        VAR,
+        IF,
+        ELSE,
+        WHILE,
+        /* FOR_EACH, */
+        SUB,
+        FUNCTION,
+        END_IF,
+        END_WHILE,
+        END_FOR_EACH,
+        END_SUB,
+        END_FUNCTION,
+        GOTO,
+        BREAK,
+        CONTINUE,
+        RETURN;
     }
 
     private void setRowField(AlgorithmRow row, String column, StringValue sv) throws SyntaxError {
