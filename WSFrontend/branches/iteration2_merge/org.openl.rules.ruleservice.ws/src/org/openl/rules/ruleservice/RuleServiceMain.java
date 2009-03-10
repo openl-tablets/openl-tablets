7443323e57325c0dbb1280176bff41c69104ec8f
pudalau <pudalau@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2009-03-10 12:01:07 +0000

    EPBDS-364 Separated RuleService into two projects.

--- WSFrontend/branches/iteration2_merge/org.openl.rules.ruleservice.ws/src/org/openl/rules/ruleservice/RuleServiceMain.java
+++ WSFrontend/branches/iteration2_merge/org.openl.rules.ruleservice.ws/src/org/openl/rules/ruleservice/RuleServiceMain.java
@@ -1,36 +1,26 @@
 package org.openl.rules.ruleservice;
 
 import org.openl.rules.repository.exceptions.RRepositoryException;
-import org.openl.rules.repository.RDeploymentListener;
-import org.openl.rules.ruleservice.loader.DeploymentInfo;
-import org.openl.rules.ruleservice.loader.LoadingListener;
 import org.openl.rules.ruleservice.loader.RulesLoader;
-import org.openl.rules.ruleservice.publish.DeploymentAdmin;
 import org.openl.rules.ruleservice.publish.WebServicesDeployAdmin;
 import org.openl.rules.ruleservice.publish.RulesPublisher;
 import org.openl.rules.ruleservice.resolver.RulesProjectResolver;
-import org.openl.rules.workspace.production.client.JcrRulesClient;
-import org.openl.SmartProps;
-
-import java.io.File;
 
 /**
- * Application main class. Sets up other classes. 
+ * Application main class. Sets up other classes.
  */
-public class RuleServiceMain extends RuleServiceBase{
-    
+public class RuleServiceMain extends RuleServiceBase {
 
-    public static void main(String[] args) throws RRepositoryException, InterruptedException {
+    public static void main(String[] args) throws RRepositoryException, InterruptedException, Exception{
         init();
         runFrontend();
     }
 
-    private static void init() {
+    private static void init() throws Exception{
         loader = new RulesLoader();
         deployAdmin = new WebServicesDeployAdmin();
         resolver = new RulesProjectResolver();
         publisher = new RulesPublisher();
     }
-
 
 }
--- WSFrontend/branches/iteration2_merge/org.openl.rules.ruleservice/src/org/openl/rules/ruleservice/instantiation/WrapperAdjustingInstantiationStrategy.java
+++ WSFrontend/branches/iteration2_merge/org.openl.rules.ruleservice/src/org/openl/rules/ruleservice/instantiation/WrapperAdjustingInstantiationStrategy.java
@@ -1,35 +1,27 @@
 package org.openl.rules.ruleservice.instantiation;
 
-import net.sf.cglib.proxy.MethodInterceptor;
-import net.sf.cglib.proxy.MethodProxy;
-import net.sf.cglib.proxy.Enhancer;
-
-import java.lang.reflect.Method;
 import java.lang.reflect.Field;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-public class CglibInstantiationStrategy implements InstantiationStrategy {
+public class WrapperAdjustingInstantiationStrategy implements InstantiationStrategy {
     private final String userHomeFieldValue;
 
-    public CglibInstantiationStrategy(String userHomeFieldValue) {
+    public WrapperAdjustingInstantiationStrategy(String userHomeFieldValue) {
         this.userHomeFieldValue = userHomeFieldValue;
     }
 
-    private static final Log log = LogFactory.getLog(CglibInstantiationStrategy.class);
-    public Object instantiate(Class<?> clazz) {
-        Enhancer enhancer = new Enhancer();
-		enhancer.setSuperclass(clazz);
-		enhancer.setCallback(new NewInstanceInterceptor(clazz));
+    private static final Log log = LogFactory.getLog(WrapperAdjustingInstantiationStrategy.class);
 
+    public Object instantiate(Class<?> clazz) {
         try {
             Field field = clazz.getField("__userHome");
             field.set(null, userHomeFieldValue);
         } catch (Exception e) {
             log.error("failed to set up __userHome", e);
         }
-        
+
         try {
             Field field = clazz.getField("__src");
             String sourcePath = (String) field.get(null);
@@ -41,26 +33,12 @@ public class CglibInstantiationStrategy implements InstantiationStrategy {
         ClassLoader oldClassLoader = Thread.currentThread().getContextClassLoader();
         Thread.currentThread().setContextClassLoader(clazz.getClassLoader());
         try {
-            return enhancer.create();
+            return clazz.newInstance();
         } catch (Exception e) {
             e.printStackTrace();
             return null;
-        }
-        finally {
+        } finally {
             Thread.currentThread().setContextClassLoader(oldClassLoader);
         }
     }
-
-    private static class NewInstanceInterceptor extends SimpleInstantiationStrategy implements MethodInterceptor {
-		final Class<?> clazz;
-
-		private NewInstanceInterceptor(Class<?> clazz) {
-			this.clazz = clazz;
-		}
-
-		public Object intercept(Object obj, Method method, Object[] args, MethodProxy mp) throws Throwable {
-            Object o = instantiate(clazz);
-            return mp.invoke(o, args);
-        }
-	}
 }
