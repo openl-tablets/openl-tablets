1a9a30f0e32084ac9c2ded6f7d93d7d6ac2d0e03
abykhavets <abykhavets@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2009-03-24 16:50:10 +0000

    EPBDS Spreadsheet tests

--- DEV/branches/iteration2_merge/org.openl.rules/test/org/openl/rules/calc/Test0.java
+++ DEV/branches/iteration2_merge/org.openl.rules/test/org/openl/rules/calc/Test0.java
@@ -8,6 +8,7 @@ import junit.framework.AssertionFailedError;
 import org.junit.Test;
 import org.openl.meta.DoubleValue;
 import org.openl.rules.TestHelper;
+import org.openl.rules.TestUtils;
 
 public class Test0 {
     @Test
@@ -17,7 +18,7 @@ public class Test0 {
         testHelper = new TestHelper<ITestCalc>(xlsFile, ITestCalc.class);
 
         ITestCalc test = testHelper.getInstance();
-        SpreadsheetResult result = test.calc1();
+        SpreadsheetResult result = test.calc();
         assertEquals(2, result.height());
         assertEquals(3, result.width());
 
@@ -33,7 +34,40 @@ public class Test0 {
         }
     }
 
+    @Test
+    public void test2() {
+        TestUtils.assertEx(new Runnable() {
+            public void run() {
+                File xlsFile = new File("test/rules/calc0/calc0-2.xls");
+                TestHelper<ITestCalc> testHelper;
+                testHelper = new TestHelper<ITestCalc>(xlsFile, ITestCalc.class);
+            }
+        }, "Table has no body!");
+    }
+
+    @Test
+    public void test3() {
+        TestUtils.assertEx(new Runnable() {
+            public void run() {
+                File xlsFile = new File("test/rules/calc0/calc0-3.xls");
+                TestHelper<ITestCalc> testHelper;
+                testHelper = new TestHelper<ITestCalc>(xlsFile, ITestCalc.class);
+            }
+        }, "Spreadsheet must have at least 2x2 cells!");
+    }
+
+    @Test
+    public void test4() {
+        TestUtils.assertEx(new Runnable() {
+            public void run() {
+                File xlsFile = new File("test/rules/calc0/calc0-4.xls");
+                TestHelper<ITestCalc> testHelper;
+                testHelper = new TestHelper<ITestCalc>(xlsFile, ITestCalc.class);
+            }
+        }, "Table has no body!", "merge header cell");
+    }
+
     interface ITestCalc {
-        SpreadsheetResult calc1();
+        SpreadsheetResult calc();
     }
 }
