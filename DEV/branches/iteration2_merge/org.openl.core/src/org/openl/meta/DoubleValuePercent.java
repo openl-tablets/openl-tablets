8b4da169cdec0e80a874598932ccaa610ecc6aec
sshor@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <sshor@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2009-04-02 13:18:10 +0000

    EPBDS improve DoubleValue printing/formatting

--- DEV/branches/iteration2_merge/org.openl.core/src/org/openl/meta/DoubleValuePercent.java
+++ DEV/branches/iteration2_merge/org.openl.core/src/org/openl/meta/DoubleValuePercent.java
@@ -1,40 +1,28 @@
 package org.openl.meta;
 
-import java.text.DecimalFormat;
 
-public class DoubleValuePercent extends DoubleValue
-{
-  /**
+public class DoubleValuePercent extends DoubleValue {
+	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 6543033363886217906L;
 
-
-	public DoubleValuePercent()
-  {}
-  public DoubleValuePercent(double d)
-  {super(d);}
-
-	
-	public DoubleValuePercent(String valueStr)
-   {
-  	 super(valueStr);
-   }
-
-
-	public String printValue()
-	{
-		return new DecimalFormat(PERCENT_FORMAT).format(getValue());
+	public DoubleValuePercent() {
+		setFormat(PERCENT_FORMAT);
 	}
 
-	
+	public DoubleValuePercent(double d) {
+		super(d);
+		setFormat(PERCENT_FORMAT);
+	}
+
+	public DoubleValuePercent(String valueStr) {
+		super(valueStr);
+		setFormat(PERCENT_FORMAT);
+	}
+
+
 	static public final String PERCENT_FORMAT = "#.####%";
-	
-//	public DoubleValue add(DoubleValue dv)
-//	{
-//		DoubleValue res = super.add(dv);
-//		res.format = PERCENT_FORMAT;
-//		return res;
-//	}
+
 
 }
