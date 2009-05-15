4375baa3bef7b1d4d2095cc1681cb485e2ebd985
spetrakovsky@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <spetrakovsky@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2009-05-15 11:15:49 +0000

    EPBDS 546 add npv method comments

--- LiveExcel/trunk/org.openl.lib.poi35.dev.modified/src/org/apache/poi/hssf/record/formula/functions/Npv.java
+++ LiveExcel/trunk/org.openl.lib.poi35.dev.modified/src/org/apache/poi/hssf/record/formula/functions/Npv.java
@@ -2,6 +2,15 @@ package org.apache.poi.hssf.record.formula.functions;
 
 import org.apache.poi.hssf.record.formula.eval.EvaluationException;
 
+/**
+ * Calculates the net present value of an investment by using a discount rate
+ * and a series of future payments (negative values) and income (positive
+ * values). Minimum 2 arguments, first arg is the rate of discount over the
+ * length of one period others up to 254 arguments representing the payments and
+ * income.
+ * 
+ * @author SPetrakovsky
+ */
 public class Npv extends NumericFunction.MultiArg {
 
 	public Npv() {
