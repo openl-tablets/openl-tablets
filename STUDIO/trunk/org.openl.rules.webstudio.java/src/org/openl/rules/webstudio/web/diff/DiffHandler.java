d31be8c5f8e8b1d5709744f7ef0d392fedb19530
aastrouski <aastrouski@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2009-05-19 16:26:07 +0000

    EPBDS-536 Add Structured Diff to compare 2 Excel files

---
+++ STUDIO/trunk/org.openl.rules.webstudio.java/src/org/openl/rules/webstudio/web/diff/DiffHandler.java
@@ -0,0 +1,134 @@
+package org.openl.rules.webstudio.web.diff;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import javax.faces.event.ActionEvent;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.myfaces.custom.fileupload.UploadedFile;
+import org.openl.conf.UserContext;
+import org.openl.impl.OpenClassJavaWrapper;
+import org.openl.rules.lang.xls.binding.XlsMetaInfo;
+import org.openl.rules.web.jsf.util.FacesUtils;
+
+import org.openl.rules.diff.test.AbstractProjection;
+import org.openl.rules.diff.test.DiffTreeBuilderImpl;
+import org.openl.rules.diff.test.ProjectionDifferImpl;
+import org.openl.rules.diff.tree.DiffTreeNode;
+import org.openl.rules.diff.xls.XlsProjectionBuilder;
+
+public class DiffHandler {
+    private String id;
+    private String treeId;
+    private UploadedFile file1;
+    private UploadedFile file2;
+    private DiffTreeNode diffTree;
+
+    public void compare(ActionEvent e) {
+        uploadFile(file1);
+        uploadFile(file2);
+
+        UserContext ucxt = new UserContext(Thread.currentThread().getContextClassLoader(), ".");
+
+        OpenClassJavaWrapper wrapper1 = OpenClassJavaWrapper.createWrapper("org.openl.xls", ucxt, file1.getName());
+        XlsMetaInfo xmi1 = (XlsMetaInfo) wrapper1.getOpenClass().getMetaInfo();
+
+        OpenClassJavaWrapper wrapper2 = OpenClassJavaWrapper.createWrapper("org.openl.xls", ucxt, file2.getName());
+        XlsMetaInfo xmi2 = (XlsMetaInfo) wrapper2.getOpenClass().getMetaInfo();
+        
+        AbstractProjection p1 = XlsProjectionBuilder.build(xmi1, "xls1");
+        AbstractProjection p2 = XlsProjectionBuilder.build(xmi2, "xls2");
+
+        DiffTreeBuilderImpl builder = new DiffTreeBuilderImpl();
+        builder.setProjectionDiffer(new ProjectionDifferImpl());
+
+        diffTree = builder.compare(p1, p2);
+    }
+
+    private void uploadFile(UploadedFile file) {
+        InputStream in = null;
+        OutputStream out = null;
+        try {
+            in = file.getInputStream();
+            out = new FileOutputStream(file.getName());
+            IOUtils.copy(in, out);
+        } catch (IOException e) {
+            e.printStackTrace();
+            //log.error("Error while uploading file", e);
+        } finally {
+            IOUtils.closeQuietly(out);
+            IOUtils.closeQuietly(in);
+        }
+    }
+
+    public DiffTreeNode[] getDiffTreeNodes() {
+        return getDiffTreeNodesByParentId(diffTree, getId());
+    }
+
+    private DiffTreeNode[] getDiffTreeNodesByParentId(DiffTreeNode node, String id) {
+        if (node != null) {
+            if (id == null || id.equals(node.getId())) {
+                return node.getChildren();
+            } else {
+                for (DiffTreeNode child : node.getChildren()) {
+                    DiffTreeNode[] found = getDiffTreeNodesByParentId(child, id);
+                    if (found.length != 0) {
+                        return found;
+                    }
+                }
+            }
+        }
+        return new DiffTreeNode[0];
+    }
+
+    public String getId() {
+        //if (id == null) {
+            id = FacesUtils.getRequestParameter("id");
+        //}
+        return id;
+    }
+
+    public String getTreeId() {
+        return treeId;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setTreeId(String treeId) {
+        this.treeId = treeId;
+    }
+
+    /**
+     * @return the file1
+     */
+    public UploadedFile getFile1() {
+        return file1;
+    }
+
+    /**
+     * @param file1 the file1 to set
+     */
+    public void setFile1(UploadedFile file1) {
+        this.file1 = file1;
+    }
+
+    /**
+     * @return the file2
+     */
+    public UploadedFile getFile2() {
+        return file2;
+    }
+
+    /**
+     * @param file2 the file2 to set
+     */
+    public void setFile2(UploadedFile file2) {
+        this.file2 = file2;
+    }
+
+}
