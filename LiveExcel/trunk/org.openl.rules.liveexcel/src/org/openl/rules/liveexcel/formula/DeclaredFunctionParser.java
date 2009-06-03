7a69c140333cdf55322b77621a953b0230d8a154
dliauchuk <dliauchuk@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2009-06-03 13:50:44 +0000

    EPBDS-585

--- LiveExcel/trunk/org.openl.rules.liveexcel/src/org/openl/rules/liveexcel/formula/DeclaredFunctionParser.java
+++ LiveExcel/trunk/org.openl.rules.liveexcel/src/org/openl/rules/liveexcel/formula/DeclaredFunctionParser.java
@@ -1,9 +1,154 @@
 package org.openl.rules.liveexcel.formula;
 
-public class DeclaredFunctionParser {
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+import org.apache.poi.hssf.record.formula.Ptg;
+import org.apache.poi.hssf.record.formula.RefPtg;
+import org.apache.poi.hssf.usermodel.HSSFEvaluationWorkbook;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.formula.FormulaParser;
+import org.apache.poi.hssf.record.formula.StringPtg;
+
+/**
+ * Parses defined ol_declare_function functions
+ * 
+ * @author DLiauchuk
+ */
+public class DeclaredFunctionParser {    
     
-    public void parseFunction(String functiondeclaration) {
-        
+    final static String OL_DECLARATION_FUNCTION = "ol_declare_function";
+    final Logger LOGGER = Logger.getLogger(DeclaredFunctionParser.class);    
+    
+    HSSFWorkbook workbook;
+    String formulaString;
+    int pointer = 0;
+    Ptg[] allPtgs;
+    ParsedDeclaredFunction parsDeclFunc = new ParsedDeclaredFunction();
+    List<FunctionParam> listParams = new ArrayList<FunctionParam>();   
+    
+    private DeclaredFunctionParser(String formulaString, HSSFWorkbook workbook) {   
+        this.formulaString = formulaString;
+        this.workbook = workbook;
+    }
+    
+    public static ParsedDeclaredFunction parseFunction(String functionDeclaration, HSSFWorkbook workbook) {       
+        DeclaredFunctionParser parser = new DeclaredFunctionParser(functionDeclaration, workbook);
+        parser.parse();
+        return parser.getParsedDeclFunc();
+    }
+    
+    private ParsedDeclaredFunction getParsedDeclFunc() {        
+        return parsDeclFunc;
     }
 
+    private void parse() {
+        UDFAdder adder = new UDFAdder(OL_DECLARATION_FUNCTION, workbook);
+        adder.makeUDF();
+        HSSFEvaluationWorkbook evalWorkBook = HSSFEvaluationWorkbook.create(workbook);
+        allPtgs = FormulaParser.parse(formulaString, evalWorkBook);
+        
+        try{
+            if(allPtgs.length>=3){        
+                checkName();
+            } else {
+                throw expected("function definition must contain min name and output cell");
+            }
+        } catch (MissingRequiredParametersException e) {
+            e.printStackTrace();
+        }
+        
+    }
+    
+    private void checkName() {
+        pointer = 1;
+        try{
+            if (allPtgs[pointer] instanceof StringPtg) {            
+                parsDeclFunc.setDeclFuncName(allPtgs[pointer].toFormulaString().
+                        substring(1, allPtgs[pointer].toFormulaString().length()-1));
+                pointer++;
+                checkDescription();
+            } else {
+                throw expected("first argument must be a declared function name");
+            }
+        } catch (MissingRequiredParametersException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    private void checkDescription() {  
+        try{
+            if(allPtgs[pointer] instanceof StringPtg&&allPtgs[pointer+1] instanceof RefPtg) {
+                parsDeclFunc.setDescription(allPtgs[pointer].toFormulaString().
+                        substring(1, allPtgs[pointer].toFormulaString().length()-1));
+                parsDeclFunc.setReturnCell((RefPtg)allPtgs[pointer+1]);  
+                pointer = pointer+2;
+                checkParameters();
+            } else { 
+                if (allPtgs[pointer] instanceof RefPtg) {
+                    parsDeclFunc.setReturnCell((RefPtg)allPtgs[pointer]);
+                    pointer++;
+                    checkParameters();
+                } else {
+                    /*if(p[2] is an array) {
+                        also we need to check if 2-nd argument may be an array
+                        of 2 references, containing description and implementation cell.
+                        Such arrays are not still implemented in POI.                              
+                    } else {                            
+                    }*/
+                    throw expected("second argument must be a declared function description," +
+                    		"or if its missed it must be output cell");
+                }   
+            }
+        } catch (MissingRequiredParametersException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    private void checkParameters() {   
+        if(pointer+1<allPtgs.length) {
+            try{
+                if(allPtgs[pointer] instanceof StringPtg&&allPtgs[pointer+1] instanceof RefPtg) {
+                    FunctionParam funcParam = new FunctionParam(allPtgs[pointer].toFormulaString().
+                               substring(1, allPtgs[pointer].toFormulaString().length()-1), 
+                               (RefPtg) allPtgs[pointer+1]);
+                    listParams.add(funcParam);
+                    pointer = pointer+2;                    
+                    checkParameters();                    
+                } else {
+                    if(allPtgs[pointer] instanceof RefPtg) {
+                        FunctionParam funcParam = new FunctionParam("", (RefPtg) allPtgs[pointer]);
+                        listParams.add(funcParam);
+                        pointer++;
+                        checkParameters();
+                        } else {
+                            /*if(p[2] is an array) {
+                            also we need to check if parameters may be an array
+                            of 2 references, containing description and parameter cell.
+                            Such arrays are not still implemented in POI.                              
+                            } else {                            
+                            }*/
+                            throw expected("second argument must be a declared function description");
+                        }
+                    }
+                } catch (MissingRequiredParametersException e) {
+                    e.printStackTrace();
+                }
+                
+        } else {
+            parsDeclFunc.setParameters(listParams);
+        }
+    
+    }
+    
+    /** Report What Was Expected */
+    private RuntimeException expected(String s) {
+        String msg;
+        msg = "The specified formula '" + formulaString
+        + "' was expected: "+s;       
+       
+        return new MissingRequiredParametersException(msg);
+    }
 }
--- LiveExcel/trunk/org.openl.rules.liveexcel/test/org/openl/rules/test/liveexcel/formula/DeclareSearchTest.java
+++ LiveExcel/trunk/org.openl.rules.liveexcel/test/org/openl/rules/test/liveexcel/formula/DeclareSearchTest.java
@@ -2,20 +2,32 @@ package org.openl.rules.test.liveexcel.formula;
 
 import static org.junit.Assert.*;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.log4j.Logger;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
 import org.junit.Test;
-import org.openl.rules.liveexcel.formula.DeclareSearcher;
+import org.openl.rules.liveexcel.formula.DeclaredFunctionSearcher;
 import org.openl.rules.liveexcel.formula.DeclaredFunction;
+import org.openl.rules.liveexcel.formula.DeclaredFunctionParser;
+import org.openl.rules.liveexcel.formula.ParsedDeclaredFunction;
 
 
 public class DeclareSearchTest {
+    
+    final Logger LOGGER = Logger.getLogger(DeclaredFunctionParser.class);
+
 
     @Test
     public void testDeclare() {
-        DeclareSearcher searcher = new DeclareSearcher("./test/resources/Functions_2009.05.18.xls");
+        DeclaredFunctionSearcher searcher = new DeclaredFunctionSearcher("./test/resources/Functions_2009.05.18.xls");
         searcher.findFunctions();
         List<DeclaredFunction> declFunctions= searcher.getFunctionsToParse();
         List<String> finalIndexes = new ArrayList<String>();
@@ -26,11 +38,73 @@ public class DeclareSearchTest {
         List<String> expextedIndexes = new ArrayList<String>();
         expextedIndexes.add("B3");
         expextedIndexes.add("B10");
-        expextedIndexes.add("B16");        
+        expextedIndexes.add("B16");  
+        expextedIndexes.add("D15");
         
-        assertTrue("All Declared functions were found",expextedIndexes.containsAll(finalIndexes));        
+        assertTrue("All Declared functions were found",expextedIndexes.containsAll(finalIndexes));  
     }
-
+    
+    @Test
+    public void testParse1() {  
+        HSSFWorkbook workbook = getWorkbook();
+        String functionText = "ol_declare_function("+'"'+"func1"+'"'+","+'"'+"Computes total of its inputs"+'"'+", C8," +
+        		""+'"'+"input one"+'"'+", C8,"+'"'+"input two"+'"'+", C5,"+'"'+"input three"+'"'+", C6)";
+        ParsedDeclaredFunction parsFunc = DeclaredFunctionParser.parseFunction(functionText, workbook);
+        
+        assertTrue("func1".equals(parsFunc.getDeclFuncName()));
+        assertTrue("Computes total of its inputs".equals(parsFunc.getDescription()));
+        assertTrue("C8".equals(parsFunc.getReturnCell().toFormulaString()));
+        assertTrue(parsFunc.getParameters().size()==3);
+        assertTrue(parsFunc.getParameters().get(0).getParamName().equals("input one"));
+        assertTrue(parsFunc.getParameters().get(1).getParamName().equals("input two"));
+        assertTrue(parsFunc.getParameters().get(2).getParamName().equals("input three"));
+    }
+    
+    @Test
+    public void testParse2() {  
+        HSSFWorkbook workbook = getWorkbook();
+        String functionText = "ol_declare_function("+'"'+"func2"+'"'+", C111, C5,"+'"'+"input two"+'"'+",C6,C7)";
+        ParsedDeclaredFunction parsFunc = DeclaredFunctionParser.parseFunction(functionText, workbook);
+        
+        assertTrue("func2".equals(parsFunc.getDeclFuncName()));
+        assertTrue(parsFunc.getDescription()==null);
+        assertTrue("C111".equals(parsFunc.getReturnCell().toFormulaString()));
+        assertTrue(parsFunc.getParameters().get(0).getParamName().equals(""));
+        assertTrue(parsFunc.getParameters().get(1).getParamName().equals("input two"));
+        assertTrue(parsFunc.getParameters().get(2).getParamName().equals(""));     
+    }
+    
+    @Test
+    public void testParse3() {  
+        HSSFWorkbook workbook = getWorkbook();
+        String functionText = "ol_declare_function("+'"'+"func3"+'"'+", C122)";
+        ParsedDeclaredFunction parsFunc = DeclaredFunctionParser.parseFunction(functionText, workbook);
+        
+        assertTrue("func3".equals(parsFunc.getDeclFuncName()));
+        assertTrue(parsFunc.getDescription()==null);
+        assertTrue("C122".equals(parsFunc.getReturnCell().toFormulaString()));
+    }
+    
+    private HSSFWorkbook getWorkbook() {
+        String fileName = "./test/resources/Functions_2009.05.18.xls";
+        HSSFWorkbook workbook = null;
+        InputStream is = null;
+        try {
+            is = new FileInputStream(fileName);
+            POIFSFileSystem fs = new POIFSFileSystem(is);
+            workbook = new HSSFWorkbook(fs);            
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (is != null)
+                    is.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return workbook;
+    }
 }
 
 
