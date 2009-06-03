98e62a6ecf8a8824f0cdec86eae3a60c464e8f7f
pudalau@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <pudalau@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2009-06-03 16:55:32 +0000

    EPBDS-585 Adjusted parsing.

---
+++ LiveExcel/trunk/org.openl.rules.liveexcel/src/org/openl/rules/liveexcel/formula/LiveExcellFunctionDeclaration.java
@@ -0,0 +1,17 @@
+package org.openl.rules.liveexcel.formula;
+
+import org.apache.poi.hssf.record.formula.eval.Eval;
+import org.apache.poi.hssf.record.formula.eval.StringEval;
+import org.apache.poi.hssf.record.formula.eval.ValueEval;
+import org.apache.poi.ss.formula.EvaluationWorkbook;
+
+public class LiveExcellFunctionDeclaration extends LiveExcelFunction {
+
+    public ValueEval evaluate(Eval[] args, EvaluationWorkbook workbook, int srcCellSheet, int srcCellRow, int srcCellCol) {
+        // TODO parsing and adding UDF to workbook
+        ParsedDeclaredFunction function = DeclaredFunctionParser.parseFunction(args);
+        workbook.getWorkbook().registerUserDefinedFunction(function.getDeclFuncName(), function);
+        return new StringEval(function.getDeclFuncName());
+    }
+
+}
