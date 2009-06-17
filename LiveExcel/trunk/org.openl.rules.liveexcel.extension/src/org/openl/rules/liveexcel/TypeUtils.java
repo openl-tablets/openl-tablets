584564b82802b72d69f352372085a13a4edec424
spetrakovsky@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <spetrakovsky@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2009-06-17 09:35:21 +0000

    EPBDS openl extension module

---
+++ LiveExcel/trunk/org.openl.rules.liveexcel.extension/src/org/openl/rules/liveexcel/TypeUtils.java
@@ -0,0 +1,39 @@
+package org.openl.rules.liveexcel;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.poi.hssf.record.formula.eval.BoolEval;
+import org.apache.poi.hssf.record.formula.eval.NumberEval;
+import org.apache.poi.hssf.record.formula.eval.RefEvalBase;
+import org.apache.poi.hssf.record.formula.eval.StringEval;
+import org.apache.poi.hssf.record.formula.eval.ValueEval;
+import org.openl.rules.liveexcel.formula.FunctionParam;
+import org.openl.types.IOpenClass;
+import org.openl.types.java.JavaOpenClass;
+
+public class TypeUtils {
+    
+    public static IOpenClass getParameterClass(FunctionParam functionParam) {
+        ValueEval innerValueEval = ((RefEvalBase)functionParam.getParamCell()).getInnerValueEval();
+        if (innerValueEval instanceof NumberEval) {
+            return JavaOpenClass.DOUBLE;
+        } else if (innerValueEval instanceof BoolEval) {
+            return JavaOpenClass.BOOLEAN;
+        } else if (innerValueEval instanceof StringEval){
+            return JavaOpenClass.STRING;
+        }
+        return JavaOpenClass.OBJECT;
+    }
+    
+    public static String convertName(String name) {
+        if (name != null) {
+            String[] parts = name.split(" ");
+            parts[0] = StringUtils.uncapitalize(parts[0]);
+            for (int i = 1; i < parts.length; i++) {
+                parts[i] = StringUtils.capitalize(parts[i]);
+            }
+            return StringUtils.join(parts);
+        }
+        return null;
+    }
+
+}
