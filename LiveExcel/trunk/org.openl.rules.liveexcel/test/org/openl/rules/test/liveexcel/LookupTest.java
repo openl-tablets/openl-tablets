2fdac7326655be8abcc45c545d51dbc81dac4d71
pudalau <pudalau@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2009-06-18 09:15:00 +0000

    EPBDS Lookup test.

---
+++ LiveExcel/trunk/org.openl.rules.liveexcel/test/org/openl/rules/test/liveexcel/LookupTest.java
@@ -0,0 +1,32 @@
+package org.openl.rules.test.liveexcel;
+
+import org.apache.poi.hssf.usermodel.HSSFFormulaEvaluator;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.junit.Test;
+import org.openl.rules.liveexcel.formula.DeclaredFunctionSearcher;
+import org.openl.rules.test.liveexcel.formula.PerformanceAndThreadSafetyTest;
+
+import static org.junit.Assert.*;
+
+public class LookupTest {
+    @Test
+    public void test() {
+        HSSFWorkbook workbook = PerformanceAndThreadSafetyTest.getHSSFWorkbook("./test/resources/LookupTest.xls");
+        new DeclaredFunctionSearcher(workbook).findFunctions();
+        Sheet sheet = workbook.getSheetAt(0);
+        Cell cell = sheet.getRow(11).getCell(0); 
+        Cell cell2 = sheet.getRow(12).getCell(0); 
+        Cell cell3ExpanionData = sheet.getRow(13).getCell(0); 
+        Cell cell4Horizontal = sheet.getRow(14).getCell(0); 
+        Cell cell5Vertical = sheet.getRow(15).getCell(0); 
+        HSSFFormulaEvaluator.evaluateAllFormulaCells(workbook);
+        assertTrue("1".equals(cell.getStringCellValue()));
+        assertTrue("0.965".equals(cell2.getStringCellValue()));
+        assertTrue("0.985".equals(cell3ExpanionData.getStringCellValue()));
+        assertTrue("0.945".equals(cell4Horizontal.getStringCellValue()));
+        assertTrue("1.001".equals(cell5Vertical.getStringCellValue()));
+    }
+
+}
