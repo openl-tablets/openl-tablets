a7bfcd56b7cef9a2b238a098006391aec6b49c47
sshor <sshor@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2009-06-30 02:28:38 +0000

    EPBDS - updated test

--- DEV/trunk/org.openl.test/tst/org/openl/util/TreeMapTest.java
+++ DEV/trunk/org.openl.test/tst/org/openl/util/TreeMapTest.java
@@ -14,17 +14,50 @@ import junit.framework.TestCase;
 public class TreeMapTest extends TestCase {
 
     public void testInterval() {
-        IntervalMap map = new IntervalMap();
+        IntervalMap<String, String> map = new IntervalMap<String, String>();
 
-        // map.put("1", "9", "18");
+         map.putInterval("1", "9", "18");
         map.putInterval("2", "4", "23");
 
-        // map.put("6", "9", "68");
-        // map.put("3", "7", "36");
+         map.putInterval("6", "9", "68");
+         map.putInterval("3", "7", "36");
 
         map.putInterval("5", "7", "56");
-        // map.put("1", "9", "18X");
+        map.putInterval("1", "9", "18X");
 
+        System.out.println("0=" + map.getInInterval("0"));
+        System.out.println("1=" + map.getInInterval("1"));
+        System.out.println("2=" + map.getInInterval("2"));
+        System.out.println("3=" + map.getInInterval("3"));
+        System.out.println("4=" + map.getInInterval("4"));
+        System.out.println("5=" + map.getInInterval("5"));
+        System.out.println("6=" + map.getInInterval("6"));
+        System.out.println("7=" + map.getInInterval("7"));
+        System.out.println("8=" + map.getInInterval("8"));
+        System.out.println("9=" + map.getInInterval("9"));
+
+        
+        map.removeInterval("1", "9", "18");
+        map.removeInterval("1", "9", "18X");
+        map.removeInterval("6", "9", "68");
+        map.removeInterval("3", "7", "36");
+
+        System.out.println("After remove 18, 18X, 36, 68");
+        System.out.println("0=" + map.getInInterval("0"));
+        System.out.println("1=" + map.getInInterval("1"));
+        System.out.println("2=" + map.getInInterval("2"));
+        System.out.println("3=" + map.getInInterval("3"));
+        System.out.println("4=" + map.getInInterval("4"));
+        System.out.println("5=" + map.getInInterval("5"));
+        System.out.println("6=" + map.getInInterval("6"));
+        System.out.println("7=" + map.getInInterval("7"));
+        System.out.println("8=" + map.getInInterval("8"));
+        System.out.println("9=" + map.getInInterval("9"));
+        
+        map.putInterval("3", "5", "34");
+        map.putInterval("4", "8", "47");
+        map.removeInterval("3", "5", "34");
+        System.out.println("After add 34");
         System.out.println("0=" + map.getInInterval("0"));
         System.out.println("1=" + map.getInInterval("1"));
         System.out.println("2=" + map.getInInterval("2"));
