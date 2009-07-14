ef48612500bd63e6fac0e82a94b7bec58c86f21d
dliauchuk@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <dliauchuk@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2009-07-14 09:14:03 +0000

    EPBDS changes for moving and coping the table

--- DEV/trunk/org.openl.rules/src/org/openl/rules/service/TableService.java
+++ DEV/trunk/org.openl.rules/src/org/openl/rules/service/TableService.java
@@ -1,5 +1,6 @@
 package org.openl.rules.service;
 
+import org.openl.rules.table.IGridRegion;
 import org.openl.rules.table.IGridTable;
 import org.openl.rules.table.xls.XlsSheetGridModel;
 
@@ -7,7 +8,8 @@ public interface TableService {
     
     public void removeTable(IGridTable table) throws TableServiceException;
     
-    public void copyTable(IGridTable table, XlsSheetGridModel destSheetModel) throws TableServiceException;
+    public IGridRegion copyTable(IGridTable table, XlsSheetGridModel destSheetModel) throws TableServiceException;
+
+    public IGridRegion moveTable(IGridTable table, XlsSheetGridModel destSheetModel) throws TableServiceException;
     
-    public void moveTable(IGridTable table, XlsSheetGridModel destSheetModel) throws TableServiceException;
 }
