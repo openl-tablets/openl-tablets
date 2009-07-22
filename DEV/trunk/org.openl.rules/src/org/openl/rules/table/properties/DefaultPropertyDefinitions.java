6b7e6db26e477cd3af4a81cf00cf47f065d320d6
sshor@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <sshor@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2009-07-22 03:23:07 +0000

    EPBDS - initial set of classes for TableProperties

---
+++ DEV/trunk/org.openl.rules/src/org/openl/rules/table/properties/DefaultPropertyDefinitions.java
@@ -0,0 +1,22 @@
+package org.openl.rules.table.properties;
+
+/**
+ * 
+ * @author snshor
+ * Created Jul 21, 2009 
+ *
+ */
+public class DefaultPropertyDefinitions 
+{
+
+	public static TablePropertiesDefinition[] getDefaultDefinitions()
+	{
+		/*---START---*/
+		
+		return null; // will be overridden
+		/*---START---*/
+		
+		
+	}
+	
+}
