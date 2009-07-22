5c13554aad03e044cf3308405ec6edc33ebd6e8d
dliauchuk@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <dliauchuk@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2009-07-22 14:49:41 +0000

    EPBDS code style format

--- DEV/trunk/org.openl.rules.gen/src/org/openl/codegen/ICodeGenContext.java
+++ DEV/trunk/org.openl.rules.gen/src/org/openl/codegen/ICodeGenContext.java
@@ -2,21 +2,20 @@ package org.openl.codegen;
 
 import org.openl.types.IOpenClass;
 
-public interface ICodeGenContext 
-{
+public interface ICodeGenContext {
 
-	/**
-	 * 
-	 * @param rt
-	 * @return the best String representation for the type to use in the code.
-	 */
-	
-	String addReferredType(IOpenClass rt);
-	
-	boolean addNewName(String name, String type);
-	
-	String genNewName(String baseName, String type);
-	
-	ICodeGenContext parentContext();
-	
+    /**
+     * 
+     * @param referredType
+     * @return the best String representation for the type to use in the code.
+     */
+
+    String addReferredType(IOpenClass referredType);
+
+    boolean addNewName(String name, String type);
+
+    String genNewName(String baseName, String type);
+
+    ICodeGenContext parentContext();
+
 }
--- DEV/trunk/org.openl.rules/src/org/openl/rules/table/properties/TablePropertiesDefinition.java
+++ DEV/trunk/org.openl.rules/src/org/openl/rules/table/properties/TablePropertiesDefinition.java
@@ -5,100 +5,123 @@ import org.openl.types.IOpenClass;
 
 public class TablePropertiesDefinition {
 
-	String displayName;
-	String name;
-	
-	
-	boolean	primaryKey;	
-	IOpenClass type;	
-	String group;	
-	boolean businessSearch;	
-	String securityFilter;	
-	String tableType;	
-	String defaultValue;	
-	StringValue constraints;	
-	String format;	
-	String inheritable;	
-	String description;
-	
-	public String getDisplayName() {
-		return displayName;
-	}
-	public void setDisplayName(String displayName) {
-		this.displayName = displayName;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public boolean isPrimaryKey() {
-		return primaryKey;
-	}
-	public void setPrimaryKey(boolean primaryKey) {
-		this.primaryKey = primaryKey;
-	}
-	public IOpenClass getType() {
-		return type;
-	}
-	public void setType(IOpenClass type) {
-		this.type = type;
-	}
-	public String getGroup() {
-		return group;
-	}
-	public void setGroup(String group) {
-		this.group = group;
-	}
-	public boolean isBusinessSearch() {
-		return businessSearch;
-	}
-	public void setBusinessSearch(boolean businessSearch) {
-		this.businessSearch = businessSearch;
-	}
-	public String getSecurityFilter() {
-		return securityFilter;
-	}
-	public void setSecurityFilter(String securityFilter) {
-		this.securityFilter = securityFilter;
-	}
-	public String getTableType() {
-		return tableType;
-	}
-	public void setTableType(String tableType) {
-		this.tableType = tableType;
-	}
-	public String getDefaultValue() {
-		return defaultValue;
-	}
-	public void setDefaultValue(String defaultValue) {
-		this.defaultValue = defaultValue;
-	}
-	public StringValue getConstraints() {
-		return constraints;
-	}
-	public void setConstraints(StringValue constraints) {
-		this.constraints = constraints;
-	}
-	public String getFormat() {
-		return format;
-	}
-	public void setFormat(String format) {
-		this.format = format;
-	}
-	public String getInheritable() {
-		return inheritable;
-	}
-	public void setInheritable(String inheritable) {
-		this.inheritable = inheritable;
-	}
-	public String getDescription() {
-		return description;
-	}
-	public void setDescription(String description) {
-		this.description = description;
-	}
+    private String displayName;
+    private String name;
 
-	
+    private boolean primaryKey;
+    private IOpenClass type;
+    private String group;
+    private boolean businessSearch;
+    private String securityFilter;
+    private String tableType;
+    private String defaultValue;
+    private StringValue constraints;
+    private String format;
+    private String inheritable;
+    private String description;
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isPrimaryKey() {
+        return primaryKey;
+    }
+
+    public void setPrimaryKey(boolean primaryKey) {
+        this.primaryKey = primaryKey;
+    }
+
+    public IOpenClass getType() {
+        return type;
+    }
+
+    public void setType(IOpenClass type) {
+        this.type = type;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public boolean isBusinessSearch() {
+        return businessSearch;
+    }
+
+    public void setBusinessSearch(boolean businessSearch) {
+        this.businessSearch = businessSearch;
+    }
+
+    public String getSecurityFilter() {
+        return securityFilter;
+    }
+
+    public void setSecurityFilter(String securityFilter) {
+        this.securityFilter = securityFilter;
+    }
+
+    public String getTableType() {
+        return tableType;
+    }
+
+    public void setTableType(String tableType) {
+        this.tableType = tableType;
+    }
+
+    public String getDefaultValue() {
+        return defaultValue;
+    }
+
+    public void setDefaultValue(String defaultValue) {
+        this.defaultValue = defaultValue;
+    }
+
+    public StringValue getConstraints() {
+        return constraints;
+    }
+
+    public void setConstraints(StringValue constraints) {
+        this.constraints = constraints;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+
+    public void setFormat(String format) {
+        this.format = format;
+    }
+
+    public String getInheritable() {
+        return inheritable;
+    }
+
+    public void setInheritable(String inheritable) {
+        this.inheritable = inheritable;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
 }
