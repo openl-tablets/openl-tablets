0cde3f17cb2516b70db5e863c2b1ac2190dc6d1d
pudalau@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <pudalau@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2009-07-29 11:56:30 +0000

    EPBDS raw benchmarks for evaluation and parsing.

---
+++ LiveExcel/trunk/org.openl.rules.liveexcel/test/org/openl/rules/test/liveexcel/ParsingTest.java
@@ -0,0 +1,161 @@
+package org.openl.rules.test.liveexcel;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.junit.Test;
+import org.openl.rules.liveexcel.usermodel.LiveExcelWorkbookFactory;
+import org.openl.util.benchmark.Benchmark;
+import org.openl.util.benchmark.BenchmarkInfo;
+import org.openl.util.benchmark.BenchmarkUnit;
+
+//import static org.junit.Assert.*;
+
+public class ParsingTest {
+
+    private static final String BENCHMARK_FILE = "./test/statistics/Benchmarks.xlsx";
+    private static final String FILE_TO_PARSE_STARTING = "./test/resources/ParsingTest/10x";
+
+    private static class ParsingBenchmark extends BenchmarkUnit {
+        private File fileToParse;
+        private long memoryUsed = 0;
+
+        public ParsingBenchmark(File fileToParse) {
+            this.fileToParse = fileToParse;
+        }
+
+        protected String getNameSpecial() {
+            return fileToParse.getName();
+        }
+
+        @Override
+        protected void run() throws Exception {
+            System.gc();
+            long previouslyMemoryUsed = FormulaEvaluationTest.getUsedMemorySize();
+            LiveExcelWorkbookFactory.create(new FileInputStream(fileToParse), null);
+            memoryUsed = FormulaEvaluationTest.getUsedMemorySize() - previouslyMemoryUsed;
+        }
+
+        public long getMemoryUsed() {
+            return memoryUsed;
+        }
+
+        public long getFileSize() {
+            return fileToParse.length();
+        }
+    }
+
+    private static class ParsingStatisticsSaver {
+        private static int PARSING_TIME_OFFSET = 0;
+        private static int USED_MEMORY_OFFSET = 1;
+        private static int FILE_SIZE_OFFSET = 2;
+        private static int XLSX_STARTING_ROW = 11;
+        private static int XLS_STARTING_ROW = 3;
+        private static int COLUMN_10x10 = 1;
+        private static int COLUMN_10x100 = 2;
+        private static int COLUMN_10x1000 = 3;
+
+        private int getColumnToWrite(String benchmarkName) {
+            if (benchmarkName.lastIndexOf("1000") != -1) {
+                return COLUMN_10x1000;
+            } else if (benchmarkName.lastIndexOf("100") != -1) {
+                return COLUMN_10x100;
+            } else {
+                return COLUMN_10x10;
+            }
+        }
+
+        private int getRowToWrite(String benchmarkName, int offset) {
+            if (benchmarkName.endsWith("xlsx")) {
+                return XLSX_STARTING_ROW + offset;
+            } else {
+                return XLS_STARTING_ROW + offset;
+            }
+        }
+
+        private void saveParsingTime(BenchmarkInfo benchmarkInfo, Sheet evaluationBenchmarkSheet) {
+            int rowIndex = getRowToWrite(benchmarkInfo.getUnit().getName(), PARSING_TIME_OFFSET);
+            int columnIndex = getColumnToWrite(benchmarkInfo.getUnit().getName());
+            evaluationBenchmarkSheet.getRow(rowIndex).getCell(columnIndex).setCellValue(benchmarkInfo.avg());
+        }
+
+        private void saveMemoryUsed(ParsingBenchmark benchmark, Sheet evaluationBenchmarkSheet) {
+            int rowIndex = getRowToWrite(benchmark.getName(), USED_MEMORY_OFFSET);
+            int columnIndex = getColumnToWrite(benchmark.getName());
+            evaluationBenchmarkSheet.getRow(rowIndex).getCell(columnIndex).setCellValue(benchmark.getMemoryUsed());
+        }
+
+        private void saveFileSize(ParsingBenchmark benchmark, Sheet evaluationBenchmarkSheet) {
+            int rowIndex = getRowToWrite(benchmark.getName(), FILE_SIZE_OFFSET);
+            int columnIndex = getColumnToWrite(benchmark.getName());
+            evaluationBenchmarkSheet.getRow(rowIndex).getCell(columnIndex).setCellValue(benchmark.getFileSize());
+        }
+
+        private void saveStatistic(BenchmarkInfo benchmarkInfo, Sheet evaluationBenchmarkSheet) {
+            if (benchmarkInfo.getUnit() instanceof ParsingBenchmark) {
+                ParsingBenchmark benchmark = (ParsingBenchmark) benchmarkInfo.getUnit();
+                saveParsingTime(benchmarkInfo, evaluationBenchmarkSheet);
+                saveMemoryUsed(benchmark, evaluationBenchmarkSheet);
+                saveFileSize(benchmark, evaluationBenchmarkSheet);
+            }
+        }
+
+        public void save(Map<String, BenchmarkInfo> statistic) {
+            OutputStream out = null;
+            InputStream in = null;
+            try {
+                Workbook workbook = LiveExcelWorkbookFactory.create((in = new FileInputStream(BENCHMARK_FILE)), null);
+                Sheet parsingBenchmarkSheet = workbook.getSheetAt(1);
+                for (String benchmarkName : statistic.keySet()) {
+                    saveStatistic(statistic.get(benchmarkName), parsingBenchmarkSheet);
+                }
+                workbook.write((out = new FileOutputStream(BENCHMARK_FILE)));
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (in != null)
+                        in.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                try {
+                    if (out != null)
+                        out.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    
+    @Test
+    public void testParsing(){
+        try {
+            List<BenchmarkUnit> buList = new ArrayList<BenchmarkUnit>();
+
+            for (int i = 10; i < 1001; i *= 10) {
+                buList.add(new ParsingBenchmark(new File(FILE_TO_PARSE_STARTING + i + ".xlsx")));
+                buList.add(new ParsingBenchmark(new File(FILE_TO_PARSE_STARTING + i + ".xls")));
+            }
+
+            Map<String, BenchmarkInfo> res = new Benchmark((BenchmarkUnit[]) buList.toArray(new BenchmarkUnit[buList
+                    .size()])).measureAll(1000);
+            new ParsingStatisticsSaver().save(res);
+            assertTrue(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+            assertTrue(false);
+        }
+    }
+}
