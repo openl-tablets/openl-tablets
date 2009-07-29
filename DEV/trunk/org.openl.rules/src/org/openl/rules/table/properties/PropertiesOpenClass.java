f5c2948e9e1644e3d192e27938763ff6de1a23ca
sshor <sshor@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2009-07-29 03:30:06 +0000

    EPBDS - renamed TablePropertiesDefinition to TablePropertyDefinition, generated DefaultTablePropertyDefinitions

--- DEV/trunk/org.openl.rules/src/org/openl/rules/table/properties/PropertiesOpenClass.java
+++ DEV/trunk/org.openl.rules/src/org/openl/rules/table/properties/PropertiesOpenClass.java
@@ -2,25 +2,27 @@ package org.openl.rules.table.properties;
 
 /**
  * 
- * @author snshor Created Jul 21, 2009
- * 
- * This class is used to load TableProperties as data beans
+ * @author snshor
+ * Created Jul 21, 2009 
+ *
+ *	This class is used to load TableProperties as data beans
  */
 
-public class PropertiesOpenClass {
+public class PropertiesOpenClass 
+{
+	
+	Class<?> propertiesBeanClass;
+	TablePropertyDefinition[] definitions;
+	
+	public PropertiesOpenClass(Class<?> propertiesBeanClass, TablePropertyDefinition[] definitions)
+	{
+		this.propertiesBeanClass = propertiesBeanClass;
+		this.definitions = definitions;
+		initialize();
+	}
 
-    private Class<?> propertiesBeanClass;
-    private TablePropertiesDefinition[] definitions;
-
-    public PropertiesOpenClass(Class<?> propertiesBeanClass,
-            TablePropertiesDefinition[] definitions) {
-        this.propertiesBeanClass = propertiesBeanClass;
-        this.definitions = definitions;
-        initialize();
-    }
-
-    private void initialize() {
-        // TODO Auto-generated method stub
-
-    }
+	private void initialize() {
+		// TODO Auto-generated method stub
+		
+	}
 }
