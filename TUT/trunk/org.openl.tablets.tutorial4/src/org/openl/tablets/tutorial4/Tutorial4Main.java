694cc0431f0b7b62da6af6436d7e19a80e87128a
agamanovich <agamanovich@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2009-08-25 14:06:30 +0000

    EPBDS Updated tutorial 4: removed *Calc objects with Spreadsheet components.

--- TUT/trunk/org.openl.tablets.tutorial4/src/org/openl/tablets/tutorial4/Tutorial4Main.java
+++ TUT/trunk/org.openl.tablets.tutorial4/src/org/openl/tablets/tutorial4/Tutorial4Main.java
@@ -4,6 +4,7 @@
  */
 package org.openl.tablets.tutorial4;
 
+import org.openl.rules.calc.SpreadsheetResult;
 import org.openl.types.impl.DynamicObject;
 import org.openl.vm.Tracer;
 
@@ -90,39 +91,42 @@ public class Tutorial4Main {
 
 	private void calculatePolicyPremium(DynamicObject policy) {
 
-		DynamicObject pc = wrapper.calculatePolicy(policy);
+		SpreadsheetResult pc = wrapper.processPolicy(policy);
 
 		System.out.println();
-		System.out.println(String.format("'%s' premium calculation report.", ((DynamicObject)pc.getFieldValue("policy")).getFieldValue("name")));
+		System.out.println(String.format("'%s' premium calculation report.", 
+				((DynamicObject)pc.getFieldValue("$Value$Policy")).getFieldValue("name")));
 		System.out.println();
 
-		DynamicObject[] vehicles = (DynamicObject[])pc.getFieldValue("vehicles");
+		SpreadsheetResult[] vehicles = (SpreadsheetResult[])pc.getFieldValue("$Value$Vehicles");
 		System.out.println(" Vehicles:");
-		for (DynamicObject vc : vehicles) {
+		
+		for (SpreadsheetResult vc : vehicles) {
 			System.out.println();
-			System.out.println(" Vehicle: " + ((DynamicObject)vc.getFieldValue("vehicle")).getFieldValue("name"));
-			System.out.println("	Theft rating:   " + vc.getFieldValue("theftRating"));
-			System.out.println("	Injury rating:  " + vc.getFieldValue("injuryRating"));
-			System.out.println("	Eligibility:    " + vc.getFieldValue("eligibility"));
+			System.out.println(" Vehicle: " + ((DynamicObject)vc.getFieldValue("$Value$Vehicle")).getFieldValue("name"));
+			System.out.println("	Theft rating:   " + vc.getFieldValue("$Value$Theft Rating"));
+			System.out.println("	Injury rating:  " + vc.getFieldValue("$Value$Injury Rating"));
+			System.out.println("	Eligibility:    " + vc.getFieldValue("$Value$Eligibility"));
 		}
 
 		System.out.println();
 
-		DynamicObject[] drivers = (DynamicObject[])pc.getFieldValue("drivers");
+		SpreadsheetResult[] drivers = (SpreadsheetResult[])pc.getFieldValue("$Value$Drivers");
 		System.out.println(" Drivers:");
-		for (DynamicObject dc : drivers) {
+		
+		for (SpreadsheetResult dc : drivers) {
 			System.out.println();
-			System.out.println(" Driver: " + ((DynamicObject)dc.getFieldValue("driver")).getFieldValue("name"));
-			System.out.println("	Age Type:    " + dc.getFieldValue("ageType"));
-			System.out.println("	Risk:        " + dc.getFieldValue("driverRisk"));
-			System.out.println("	Eligibility: " + dc.getFieldValue("eligibility"));
+			System.out.println(" Driver: " + ((DynamicObject)dc.getFieldValue("$Value$Driver")).getFieldValue("name"));
+			System.out.println("	Age Type:    " + dc.getFieldValue("$Value$Age Type"));
+			System.out.println("	Risk:        " + dc.getFieldValue("$Value$Driver Risk"));
+			System.out.println("	Eligibility: " + dc.getFieldValue("$Value$Eligibility"));
 		}
 
 		System.out.println();
 		System.out.println(" Result:");
-		System.out.println("	Score:       " + pc.getFieldValue("score"));
-		System.out.println("	Eligibility: " + pc.getFieldValue("eligibility"));
-		System.out.println("	Premium:     " + pc.getFieldValue("premium"));
+		System.out.println("	Score:       " + pc.getFieldValue("$Value$Score"));
+		System.out.println("	Eligibility: " + pc.getFieldValue("$Value$Eligibility"));
+		System.out.println("	Premium:     " + pc.getFieldValue("$Value$Premium"));
 
 		System.out.println();
 		System.out.println();
