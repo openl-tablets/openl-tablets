1db33f35e62a6ab7a3d0a556ac2e7061b6bf0777
dliauchuk@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <dliauchuk@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2009-08-25 09:57:57 +0000

    EPBDS tests for business search

---
+++ DEV/trunk/org.openl.rules/test/org/openl/rules/search/BusSearchTest.java
@@ -0,0 +1,73 @@
+package org.openl.rules.search;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+import org.openl.conf.UserContext;
+import org.openl.impl.OpenClassJavaWrapper;
+import org.openl.meta.StringValue;
+import org.openl.rules.lang.xls.binding.XlsMetaInfo;
+import org.openl.rules.lang.xls.binding.TableProperties.Property;
+import org.openl.rules.lang.xls.syntax.TableSyntaxNode;
+import org.openl.rules.lang.xls.syntax.XlsModuleSyntaxNode;
+
+public class BusSearchTest {
+    
+    private String __src = "test/rules/Tutorial_4_Test.xls";
+    
+    private OpenLBussinessSearch search = new OpenLBussinessSearch();
+    
+    private XlsModuleSyntaxNode getTables() {        
+        UserContext ucxt = new UserContext(Thread.currentThread().getContextClassLoader(), ".");
+        OpenClassJavaWrapper wrapper = OpenClassJavaWrapper.createWrapper("org.openl.xls", ucxt, __src);
+        XlsMetaInfo xmi = (XlsMetaInfo) wrapper.getOpenClass().getMetaInfo();
+        XlsModuleSyntaxNode xsn = xmi.getXlsModuleNode();
+        return xsn;
+    }
+    
+    private void initSearchCondition(List<Property> propList) {
+        BussinessSearchCondition searchCondition = new BussinessSearchCondition();        
+        searchCondition.setPropToSearch(propList);
+        search.setBusSearchCondit(searchCondition);
+    }
+    
+    @Test 
+    public void testTableByName() {
+        List<Property> propList = new ArrayList<Property>(){{
+            add(new Property(new StringValue("name"), new StringValue("Vehicle Discounts"))); 
+        }};
+        initSearchCondition(propList);
+        Object searchResult = search.search(getTables());
+        if((searchResult != null) && (searchResult instanceof OpenLBussinessSearchResult)) {
+            List<TableSyntaxNode> foundTables = ((OpenLBussinessSearchResult) searchResult).getFoundTables();
+            assertTrue("There is only one table for this cryteria",foundTables.size()==1);
+            assertEquals("Display names are identical", "Rules DoubleValue vehicleDiscount(Vehicle vehicle, String vehicleTheftRating)" ,
+                    foundTables.get(0).getDisplayName());            
+        } else {
+            fail();
+        }        
+    }
+    
+    @Test 
+    public void testTableByNameAndCategory() {
+        List<Property> propList = new ArrayList<Property>(){{
+            add(new Property(new StringValue("name"), new StringValue("Vehicle Score Processing Sequence")));
+            add(new Property(new StringValue("category"), new StringValue("Auto-Scoring")));
+        }};
+        initSearchCondition(propList);
+        Object searchResult = search.search(getTables());
+        if((searchResult != null) && (searchResult instanceof OpenLBussinessSearchResult)) {
+            List<TableSyntaxNode> foundTables = ((OpenLBussinessSearchResult) searchResult).getFoundTables();
+            assertTrue("There is only one table for this cryteria",foundTables.size()==1);
+            assertEquals("Display names are identical", "Rules void vehicleScore1(VehicleCalc vc)" ,
+                    foundTables.get(0).getDisplayName());
+            //System.out.println("name: "+ foundTables.get(0).getDisplayName());
+        } else {
+            fail();
+        }        
+    }
+
+}
