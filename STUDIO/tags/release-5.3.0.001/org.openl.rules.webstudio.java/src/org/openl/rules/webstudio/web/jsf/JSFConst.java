a9b16b103372065515f71e2810e9062ecbfc449d
pudalau@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <pudalau@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2009-10-05 07:38:16 +0000

    EPBDS wrong tag.

--- STUDIO/tags/release-5.3.0.001/org.openl.rules.webstudio.java/src/org/openl/rules/webstudio/web/jsf/JSFConst.java
+++
@@ -1,6 +0,0 @@
-package org.openl.rules.webstudio.web.jsf;
-
-public class JSFConst {
-    public static final String OUTCOME_SUCCESS = "success";
-    public static final String RULES_USER_SESSION_ATTR = "rulesUserSession";
-}
