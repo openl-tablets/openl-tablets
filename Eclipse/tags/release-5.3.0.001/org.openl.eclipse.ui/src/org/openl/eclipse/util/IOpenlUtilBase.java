40635527ab67014358c84200edd7d1e2373c2084
pudalau <pudalau@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2009-10-05 08:35:30 +0000

    EPBDS-345.[maven-release-plugin] copy for tag release-5.3.0.001

---
+++ Eclipse/tags/release-5.3.0.001/org.openl.eclipse.ui/src/org/openl/eclipse/util/IOpenlUtilBase.java
@@ -0,0 +1,14 @@
+/*
+ * Created on Oct 2, 2003
+ *
+ * Developed by OpenRules Inc. 2003
+ */
+
+package org.openl.eclipse.util;
+
+/**
+ *
+ * @author sam
+ */
+public interface IOpenlUtilBase extends IUtilBase, IOpenlConstants {
+}
