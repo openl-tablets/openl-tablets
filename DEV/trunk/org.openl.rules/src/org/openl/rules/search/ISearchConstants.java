b0f4498c72547eb84436272e50b8b60a1a989037
dliauchuk@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <dliauchuk@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2009-10-20 09:18:43 +0000

    EPBDS code style changes

--- DEV/trunk/org.openl.rules/src/org/openl/rules/search/ISearchConstants.java
+++ DEV/trunk/org.openl.rules/src/org/openl/rules/search/ISearchConstants.java
@@ -9,18 +9,22 @@ package org.openl.rules.search;
  */
 public interface ISearchConstants {
 
-    static final public String HEADER = "header", PROPERTY = "property";
-    static final public String COLUMN_PARAMETER = "Column Parameter", COLUMN_TYPE = "Column Type";
+    String HEADER = "header", PROPERTY = "property";
+    
+    String COLUMN_PARAMETER = "Column Parameter", COLUMN_TYPE = "Column Type";
 
-    static final public String ANY = "--ANY--";
+    String ANY = "--ANY--";
 
-    static public final String[] typeValues = { HEADER, PROPERTY };
-    static public final String[] colTypeValues = { COLUMN_PARAMETER, COLUMN_TYPE };
+    String[] typeValues = { HEADER, PROPERTY };
+    
+    String[] colTypeValues = { COLUMN_PARAMETER, COLUMN_TYPE };
 
-    static public String ADD_ACTION = "add";
-    static public String DELETE_ACTION = "delete";
+    String ADD_ACTION = "add";
+    
+    String DELETE_ACTION = "delete";
 
-    static public String COL_ADD_ACTION = "colAdd";
-    static public String COL_DELETE_ACTION = "colDelete";
+    String COL_ADD_ACTION = "colAdd";
+    
+    String COL_DELETE_ACTION = "colDelete";
 
 }
