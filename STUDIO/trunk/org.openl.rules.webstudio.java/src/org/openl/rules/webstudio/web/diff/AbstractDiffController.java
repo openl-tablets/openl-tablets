3ac0d3b8876d98b0904f87b01c5aaf64d69d30f7
aastrouski@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <aastrouski@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2009-10-21 11:16:39 +0000

    EPBDS Refactoring

--- STUDIO/trunk/org.openl.rules.webstudio.java/src/org/openl/rules/webstudio/web/diff/AbstractDiffController.java
+++ STUDIO/trunk/org.openl.rules.webstudio.java/src/org/openl/rules/webstudio/web/diff/AbstractDiffController.java
@@ -1,23 +1,8 @@
 package org.openl.rules.webstudio.web.diff;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.faces.application.FacesMessage;
-import javax.faces.component.UIComponent;
-import javax.faces.component.UIInput;
-import javax.faces.context.FacesContext;
-import javax.faces.event.ActionEvent;
-
-import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang.ArrayUtils;
-import org.apache.myfaces.custom.fileupload.UploadedFile;
-import org.openl.rules.lang.xls.XlsHelper;
-import org.openl.rules.lang.xls.binding.XlsMetaInfo;
 import org.openl.rules.table.ICell;
 import org.openl.rules.table.IGridRegion;
 
@@ -32,130 +17,38 @@ import org.openl.rules.webstudio.web.util.WebStudioUtils;
 import org.openl.rules.diff.tree.DiffElement;
 import org.openl.rules.diff.hierarchy.Projection;
 import org.openl.rules.diff.hierarchy.ProjectionProperty;
-import org.openl.rules.diff.test.AbstractProjection;
-import org.openl.rules.diff.test.DiffTreeBuilderImpl;
-import org.openl.rules.diff.test.ProjectionDifferImpl;
 import org.openl.rules.diff.tree.DiffTreeNode;
-import org.openl.rules.diff.xls.XlsProjectionBuilder;
 import org.openl.rules.diff.xls.XlsProjectionType;
 import org.openl.rules.diff.util.DiffHelper;
 
-public class DiffHandler {
-    private String id;
-    private UploadedFile file1;
-    private UploadedFile file2;
+public abstract class AbstractDiffController {
+    private String currentNodeId;
     private DiffTreeNode diffTree;
     private boolean showEqualElements = false;
 
-    public static final String excelContentTypes[] = {
-        "application/vnd.ms-excel",
-        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-    };
-
-    public void compare(ActionEvent e) {
-        uploadFile(file1);
-        uploadFile(file2);
-
-        XlsMetaInfo xmi1 = XlsHelper.getXlsMetaInfo(file1.getName());
-        XlsMetaInfo xmi2 = XlsHelper.getXlsMetaInfo(file2.getName());
-
-        AbstractProjection p1 = XlsProjectionBuilder.build(xmi1, "xls1");
-        AbstractProjection p2 = XlsProjectionBuilder.build(xmi2, "xls2");
-
-        DiffTreeBuilderImpl builder = new DiffTreeBuilderImpl();
-        builder.setProjectionDiffer(new ProjectionDifferImpl());
-
-        diffTree = builder.compare(p1, p2);
-    }
-
-    public void validateContentType(FacesContext context, UIComponent toValidate, Object value) {
-        String errorMessage = "Only Excel files can be compared";
-        if (value != null && value instanceof UploadedFile) {
-            UploadedFile file = (UploadedFile) value;
-            String fileContentType = file.getContentType();
-            if (ArrayUtils.contains(excelContentTypes, fileContentType)) {
-                ((UIInput)toValidate).setValid(true);
-                return;
-            }
-        }
-        ((UIInput)toValidate).setValid(false);
-        FacesMessage message = new FacesMessage(errorMessage);
-        context.addMessage(toValidate.getClientId(context), message);
-    }
-
-    private void uploadFile(UploadedFile file) {
-        InputStream in = null;
-        OutputStream out = null;
-        try {
-            in = file.getInputStream();
-            out = new FileOutputStream(file.getName());
-            IOUtils.copy(in, out);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            IOUtils.closeQuietly(out);
-            IOUtils.closeQuietly(in);
-        }
-    }
+    public abstract String compare();
 
     public DiffTreeNode[] getDiffTreeNodes() {
-        DiffTreeNode tree = DiffHelper.getDiffNodeById(diffTree, getId());
+        DiffTreeNode tree = DiffHelper.getDiffNodeById(getDiffTree(), getCurrentNodeId());
         if (tree != null) {
             return tree.getChildren();
         }
         return new DiffTreeNode[0];
     }
 
-    
-    public String getId() {
-        //if (id == null) {
-            id = FacesUtils.getRequestParameter("id");
-        //}
-        return id;
+    public String getCurrentNodeId() {
+        currentNodeId = FacesUtils.getRequestParameter("id");
+        return currentNodeId;
     }
 
-    public void setId(String id) {
-        this.id = id;
+    public void setCurrentNodeId(String currentNodeId) {
+        this.currentNodeId = currentNodeId;
     }
 
-    /**
-     * @return the file1
-     */
-    public UploadedFile getFile1() {
-        return file1;
-    }
-
-    /**
-     * @param file1 the file1 to set
-     */
-    public void setFile1(UploadedFile file1) {
-        this.file1 = file1;
-    }
-
-    /**
-     * @return the file2
-     */
-    public UploadedFile getFile2() {
-        return file2;
-    }
-
-    /**
-     * @param file2 the file2 to set
-     */
-    public void setFile2(UploadedFile file2) {
-        this.file2 = file2;
-    }
-
-    /**
-     * @return the showEqualElements
-     */
     public boolean isShowEqualElements() {
         return showEqualElements;
     }
 
-    /**
-     * @param showEqualElements the showEqualElements to set
-     */
     public void setShowEqualElements(boolean showEqualElements) {
         this.showEqualElements = showEqualElements;
     }
@@ -169,7 +62,7 @@ public class DiffHandler {
     }
 
     public ITable getTable(int i) {
-        DiffTreeNode tree = DiffHelper.getDiffNodeById(diffTree, getId());
+        DiffTreeNode tree = DiffHelper.getDiffNodeById(getDiffTree(), getCurrentNodeId());
         if (tree != null) {
             DiffElement[] elems = tree.getElements();
             if (elems.length > i) {
@@ -191,7 +84,7 @@ public class DiffHandler {
     }
 
     public IGridFilter getFilter(int i) {
-        DiffTreeNode tree = DiffHelper.getDiffNodeById(diffTree, getId());
+        DiffTreeNode tree = DiffHelper.getDiffNodeById(getDiffTree(), getCurrentNodeId());
         if (tree != null) {
             DiffElement[] elems = tree.getElements();
             if (elems.length > i) {
@@ -222,7 +115,8 @@ public class DiffHandler {
     private IGridFilter makeFilter(IGridTable table, List<ICell> selectedCells) {
         List<IGridRegion> regions = new ArrayList<IGridRegion>();
         for (ICell cell : selectedCells) {
-            IGridRegion region = table.getLogicalRegion(cell.getColumn(), cell.getRow(), 1, 1).getGridTable().getRegion();
+            IGridRegion region = table.getLogicalRegion(cell.getColumn(), cell.getRow(), 1, 1)
+                .getGridTable().getRegion();
             regions.add(region);
         }
         if (regions.isEmpty()) {
@@ -231,6 +125,14 @@ public class DiffHandler {
         IGridRegion[] aRegions = regions.toArray(new IGridRegion[regions.size()]);
         return new ColorGridFilter(new RegionGridSelector(aRegions, true),
                 WebStudioUtils.getWebStudio().getModel().getFilterHolder().makeFilter());
+    }
+
+    public void setDiffTree(DiffTreeNode diffTree) {
+        this.diffTree = diffTree;
+    }
+
+    public DiffTreeNode getDiffTree() {
+        return diffTree;
     }
 
 }
