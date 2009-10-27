2b6078691820b7057ce987f7af803e64a30a8499
dliauchuk <dliauchuk@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2009-10-27 10:51:50 +0000

    EPBDS-comments, refactoring.

--- DEV/trunk/org.openl.rules/src/org/openl/rules/search/PropertySelector.java
+++ DEV/trunk/org.openl.rules/src/org/openl/rules/search/PropertySelector.java
@@ -8,41 +8,57 @@ import org.openl.rules.lang.xls.syntax.TableSyntaxNode;
 import org.openl.util.AStringBoolOperator;
 
 /**
- * @author snshor
- *
+ * Handles checking of the table, during advanced search if it satisfy to the search condition for property 
+ * selections.
+ * @author <i>snshor</i><br>
+ * commented by <i>DLiauchuk<i>
  */
 
 public class PropertySelector extends ATableSyntaxNodeSelector {
-    AStringBoolOperator propertyNameSelector;
+    
+    private AStringBoolOperator propertyNameSelector;
 
-    AStringBoolOperator propertyValueSelector;
+    private AStringBoolOperator propertyValueSelector;
 
     /**
-     * @param se
+     * Constructs the object, according to the info that was set to the search condition.
+     * 
+     * @param se Search condition. 
      */
     public PropertySelector(SearchConditionElement se) {
-        propertyNameSelector = se.isAny(se.getElementValueName()) ? null : AStringBoolOperator.makeOperator("matches", se
-                .getElementValueName());
-        propertyValueSelector = se.isAny(se.getElementValue()) ? null : AStringBoolOperator.makeOperator(se.getOpType2(), se
-                .getElementValue());
+        propertyNameSelector = se.isAny(se.getElementValueName()) ? null : AStringBoolOperator.makeOperator("matches", 
+                se.getElementValueName());
+        propertyValueSelector = se.isAny(se.getElementValue()) ? null : AStringBoolOperator.makeOperator(se.getOpType2(),
+                se.getElementValue());
     }
-
-    public boolean selectProperty(TableProperties.Property prop, TableProperties tp) {
-        return (propertyNameSelector == null || propertyNameSelector.isMatching(prop.getKey().getValue()))
-                && (propertyValueSelector == null || propertyValueSelector.isMatching(tp
-                        .getPropertyValueAsString(prop.getKey().getValue())));
+    
+    /**
+     * Checks if the property from the table matches to the property defined in search condition. 
+     * 
+     * @param property Property from table.
+     * @param tableProperties All properties from table. This parameter is used, because of the method 
+     * <code>{@link TableProperties#getPropertyValueAsString(String)}</code> to get the value of the property as string.
+     * 
+     * @return <code>True</code> if property name from search condition is <code>null</code> or matches to the property 
+     * name from the table and at the same time if the property value from search condition is <code>null</code> or 
+     * matches to the the property value from the table. In other way <code>false</code>.
+     */
+    private boolean isPropertySelected(TableProperties.Property property, TableProperties tableProperties) {
+        return (propertyNameSelector == null || propertyNameSelector.isMatching(property.getKey().getValue()))
+                && (propertyValueSelector == null || propertyValueSelector.isMatching(tableProperties
+                        .getPropertyValueAsString(property.getKey().getValue())));
     }
 
     @Override
     public boolean isTableSelected(TableSyntaxNode tsn) {
-        TableProperties tp = tsn.getTableProperties();
-        if (tp == null) {
+        TableProperties tableProperties = tsn.getTableProperties();
+        if (tableProperties == null) {
             return propertyNameSelector == null && propertyValueSelector == null;
         }
 
-        TableProperties.Property[] pp = tp.getProperties();
-        for (int i = 0; i < pp.length; i++) {
-            if (selectProperty(pp[i], tp)) {
+        TableProperties.Property[] properties = tableProperties.getProperties();
+        for (int i = 0; i < properties.length; i++) {
+            if (isPropertySelected(properties[i], tableProperties)) {
                 return true;
             }
         }
