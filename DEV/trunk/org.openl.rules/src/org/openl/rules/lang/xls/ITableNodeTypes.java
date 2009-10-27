a4a2e82826dc946753104235a4d48ef1666d4589
dliauchuk@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <dliauchuk@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2009-10-27 09:58:16 +0000

    EPBDS-code style changes.

--- DEV/trunk/org.openl.rules/src/org/openl/rules/lang/xls/ITableNodeTypes.java
+++ DEV/trunk/org.openl.rules/src/org/openl/rules/lang/xls/ITableNodeTypes.java
@@ -8,18 +8,31 @@ package org.openl.rules.lang.xls;
 
 /**
  * @author snshor
- *
+ * 
  */
 public interface ITableNodeTypes {
 
-    public static final String WORKBOOK = "Workbook", WORKSHEET = "Worksheet", TABLE = "Table", CELL = "Cell";
+    public static final String WORKBOOK = "Workbook";
+    public static final String WORKSHEET = "Worksheet";
+    public static final String TABLE = "Table";
+    public static final String CELL = "Cell";
 
-    public static final String XLS_MODULE = "xls.module", XLS_WORKBOOK = "xls.workbook", XLS_WORKSHEET = "xls.worksheet", XLS_DT = "xls.dt", XLS_SPREADSHEET = "xls.spreadsheet",
-            XLS_TBASIC = "xls.tbasic", XLS_COLUMN_MATCH = "xls.columnmatch",
-
-            XLS_METHOD = "xls.method", XLS_DATA = "xls.data", XLS_TEST_METHOD = "xls.test.method",
-            XLS_RUN_METHOD = "xls.run.method", XLS_DATATYPE = "xls.datatype", XLS_OPENL = "xls.openl",
-            XLS_TABLE = "xls.table", XLS_ENVIRONMENT = "xls.environment", XLS_PERSISTENT = "xls.persistent",
-            XLS_OTHER = "xls.other";
+    public static final String XLS_MODULE = "xls.module";
+    public static final String XLS_WORKBOOK = "xls.workbook";
+    public static final String XLS_WORKSHEET = "xls.worksheet";
+    public static final String XLS_DT = "xls.dt";
+    public static final String XLS_SPREADSHEET = "xls.spreadsheet";
+    public static final String XLS_TBASIC = "xls.tbasic";
+    public static final String XLS_COLUMN_MATCH = "xls.columnmatch";
+    public static final String XLS_METHOD = "xls.method";
+    public static final String XLS_DATA = "xls.data";
+    public static final String XLS_TEST_METHOD = "xls.test.method";
+    public static final String XLS_RUN_METHOD = "xls.run.method";
+    public static final String XLS_DATATYPE = "xls.datatype";
+    public static final String XLS_OPENL = "xls.openl";
+    public static final String XLS_TABLE = "xls.table";
+    public static final String XLS_ENVIRONMENT = "xls.environment";
+    public static final String XLS_PERSISTENT = "xls.persistent";
+    public static final String XLS_OTHER = "xls.other";
 
 }
