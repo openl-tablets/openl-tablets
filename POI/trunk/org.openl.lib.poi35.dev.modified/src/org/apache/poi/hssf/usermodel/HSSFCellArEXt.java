0673dcd2978af2c79f6b59da122cb5150e39f97c
vabramovs@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <vabramovs@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2009-11-05 10:28:01 +0000

    EPBDS - Javadoc for changes

--- POI/trunk/org.openl.lib.poi35.dev.modified/src/org/apache/poi/hssf/usermodel/HSSFCellArEXt.java
+++ POI/trunk/org.openl.lib.poi35.dev.modified/src/org/apache/poi/hssf/usermodel/HSSFCellArEXt.java
@@ -20,6 +20,9 @@ import org.openxmlformats.schemas.spreadsheetml.x2006.main.CTCellFormula;
 import org.openxmlformats.schemas.spreadsheetml.x2006.main.STCellFormulaType;
 
 /**
+ * Class to provide array formula support for hssf cell.
+ *   For cell belong to Array Formula range arrayFormulaRef is set, null otherwise.
+ *    May be merged to HSSFCell 
  * @author vabramovs
  *
  */
@@ -58,15 +61,26 @@ public class HSSFCellArEXt extends HSSFCell implements CellArExt {
 	public HSSFCellArEXt(HSSFWorkbook book, HSSFSheet sheet, int row, short col) {
 		super(book, sheet, row, col);
 	}
+	/* (non-Javadoc)
+	 * @see org.apache.poi.ss.usermodel.CellArExt#getArrayFormulaRef()
+	 */
 	public ArrayFormula getArrayFormulaRef() {
 		return arrayFormulaRef;
 	}
 
+	/* (non-Javadoc)
+	 * @see org.apache.poi.ss.usermodel.CellArExt#setArrayFormulaRef(org.apache.poi.ss.formula.ArrayFormula)
+	 */
 	public void setArrayFormulaRef(ArrayFormula ref) {
 		arrayFormulaRef = ref;
 		
 	}
 	
+    /**
+     * Set Array Formula for cell's range
+     * @param formula
+     * @param range like "A1:B2"
+     */
     public void setCellFormula(String formula,String  range) {
     	if(formula != null){
         	super.setCellFormula(formula);
@@ -95,33 +109,70 @@ public class HSSFCellArEXt extends HSSFCell implements CellArExt {
 	  /*********** Internal  methods for set values (even in Array Formula range ) from POI  - *************/
 	  /*********** Set public to allow to use this methods from tests *************/
 	
+	/**
+	 * Set comment for cell
+	 * @param comment
+	 */
 	public void setCellCommentInt(Comment comment) {
     	super.setCellComment(comment);
 	}
 	
-   public void setCellErrorValueInt(byte errorCode) {
+   /**
+    * Set Error Value for cell
+    * @param errorCode
+    */
+	public void setCellErrorValueInt(byte errorCode) {
     	super.setCellErrorValue(errorCode);
 	}
-     public void setCellValueInt(boolean value) {
+     /**
+      * Set boolean value for cell
+     * @param value
+     */
+    public void setCellValueInt(boolean value) {
     	super.setCellValue(value);
 	}
+    /**
+      * Set Calendar value for cell
+     * @param value
+     */
     public void setCellValueInt(Calendar value) {
     	super.setCellValue(value);
 	}
+    /**
+      * Set Calendar value for cell
+     * @param value
+     */
     public void setCellValueInt(Date value) {
     	super.setCellValue(value);
 	}
+    /**
+      * Set numeric value for cell
+     * @param value
+     */
     public void setCellValueInt(double value) {
     	super.setCellValue(value);
 	}
+    /**
+     * Set RichText value for cell
+     * @param value
+     */
     public void setCellValueInt(RichTextString value) {
     	super.setCellValue(value);
 	}
+    /**
+     * Set String value for cell
+     * @param value
+     */
     public void setCellValueInt(String value) {
     	super.setCellValue(value);
 	}
-  /*********** Overridden methods from XSSFCell *************/
+  /*********** Overridden methods from HSSFCell *************/
 	
+	/* (non-Javadoc)
+	 * @see org.apache.poi.hssf.usermodel.HSSFCell#setCellComment(org.apache.poi.ss.usermodel.Comment)
+	 * Throws RuntimeException("You cannot change part of an array") if cell belong Array Formula range.
+	 * Use setCellCommentInt()in case of need
+	 */
 	@Override
 	public void setCellComment(Comment comment) {
 		if(arrayFormulaRef != null){
@@ -132,8 +183,13 @@ public class HSSFCellArEXt extends HSSFCell implements CellArExt {
     	super.setCellComment(comment);
 	}
 	
+	/* (non-Javadoc)
+	 * @see org.apache.poi.hssf.usermodel.HSSFCell#setCellErrorValue(byte)
+	 * Throws RuntimeException("You cannot change part of an array") if cell belong Array Formula range.
+	 * Use setCellErrorValueInt()in case of need
+	 */
 	@Override
-	   public void setCellErrorValue(byte errorCode) {
+	 public void setCellErrorValue(byte errorCode) {
 		if(arrayFormulaRef != null){
 			RuntimeException ex = new RuntimeException("You cannot change part of an array");
 			if(!ex.getStackTrace()[1].getClassName().startsWith("org.apache.poi"))
@@ -141,6 +197,11 @@ public class HSSFCellArEXt extends HSSFCell implements CellArExt {
 		}
     	super.setCellErrorValue(errorCode);
 	}
+	/* (non-Javadoc)
+	 * @see org.apache.poi.hssf.usermodel.HSSFCell#setCellValue(boolean)
+	 * Throws RuntimeException("You cannot change part of an array") if cell belong Array Formula range.
+	 * Use setCellValueInt()in case of need
+	 */
 	@Override
     public void setCellValue(boolean value) {
 		if(arrayFormulaRef != null){
@@ -150,6 +211,11 @@ public class HSSFCellArEXt extends HSSFCell implements CellArExt {
 		}
  	super.setCellValue(value);
 	}
+	/* (non-Javadoc)
+	 * @see org.apache.poi.hssf.usermodel.HSSFCell#setCellValue(java.util.Calendar)
+	 * Throws RuntimeException("You cannot change part of an array") if cell belong Array Formula range.
+	 * Use setCellValueInt()in case of need
+	 */
 	@Override
     public void setCellValue(Calendar value) {
 		if(arrayFormulaRef != null){
@@ -159,6 +225,11 @@ public class HSSFCellArEXt extends HSSFCell implements CellArExt {
 		}
  	super.setCellValue(value);
 	}
+	/* (non-Javadoc)
+	 * @see org.apache.poi.hssf.usermodel.HSSFCell#setCellValue(java.util.Date)
+	 * Throws RuntimeException("You cannot change part of an array") if cell belong Array Formula range.
+	 * Use setCellValueInt()in case of need
+	 */
 	@Override
     public void setCellValue(Date value) {
 		if(arrayFormulaRef != null){
@@ -168,6 +239,16 @@ public class HSSFCellArEXt extends HSSFCell implements CellArExt {
 		}
  	super.setCellValue(value);
 	}
+	/* (non-Javadoc)
+	 * @see org.apache.poi.hssf.usermodel.HSSFCell#setCellValue(double)
+	 * Throws RuntimeException("You cannot change part of an array") if cell belong Array Formula range.
+	 * Use setCellValueInt()in case of need
+	 */
+	/* (non-Javadoc)
+	 * @see org.apache.poi.hssf.usermodel.HSSFCell#setCellValue(double)
+	 * Throws RuntimeException("You cannot change part of an array") if cell belong Array Formula range.
+	 * Use setCellValueInt()in case of need
+	 */
 	@Override
     public void setCellValue(double value) {
 		if(arrayFormulaRef != null){
@@ -178,6 +259,11 @@ public class HSSFCellArEXt extends HSSFCell implements CellArExt {
  	super.setCellValue(value);
 	}
 	
+	/* (non-Javadoc)
+	 * @see org.apache.poi.hssf.usermodel.HSSFCell#setCellValue(org.apache.poi.ss.usermodel.RichTextString)
+	 * Throws RuntimeException("You cannot change part of an array") if cell belong Array Formula range.
+	 * Use setCellValueInt()in case of need
+	 */
 	@Override
     public void setCellValue(RichTextString value) {
 		if(arrayFormulaRef != null){
@@ -187,6 +273,11 @@ public class HSSFCellArEXt extends HSSFCell implements CellArExt {
 		}
  	super.setCellValue(value);
 	}
+	/* (non-Javadoc)
+	 * @see org.apache.poi.hssf.usermodel.HSSFCell#setCellValue(java.lang.String)
+	 * Throws RuntimeException("You cannot change part of an array") if cell belong Array Formula range.
+	 * Use setCellValueInt()in case of need
+	 */
 	@Override
     public void setCellValue(String value) {
 		if(arrayFormulaRef != null){
