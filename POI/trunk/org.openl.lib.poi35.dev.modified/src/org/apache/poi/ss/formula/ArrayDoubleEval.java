0e76a6c380054c77e1a04bc8da8e8d6a625134af
vabramovs <vabramovs@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2009-11-05 10:49:43 +0000

    EPBDS - Javadoc for changes

--- POI/trunk/org.openl.lib.poi35.dev.modified/src/org/apache/poi/ss/formula/ArrayDoubleEval.java
+++ POI/trunk/org.openl.lib.poi35.dev.modified/src/org/apache/poi/ss/formula/ArrayDoubleEval.java
@@ -1,8 +1,9 @@
 package org.apache.poi.ss.formula;
-
-
-
-// array contains only Doubles - type checking and convenience 
+/**
+ * Class to support evaluated array of doubles 
+ * @author zsulkins(ZS)
+ */
+//array contains only Doubles - type checking and convenience 
 public class ArrayDoubleEval extends ArrayEval {
 
 	public ArrayDoubleEval(Double[][] array){
@@ -12,8 +13,10 @@ public class ArrayDoubleEval extends ArrayEval {
 	public ArrayDoubleEval(Double[][] array, boolean isIllegalForAggregation){
 		super(array,isIllegalForAggregation);
 	}
-
 	
+	/* (non-Javadoc)
+	 * @see org.apache.poi.ss.formula.ArrayEval#getArrayElement(int, int)
+	 */
 	@Override 
 	public Double getArrayElement(int row, int col) {
 		return (Double)(super.getArrayElement(row, col));
