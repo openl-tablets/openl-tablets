19831d03b416f8da5549fabe18f6e4051c4d59b3
aastrouski <aastrouski@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2009-11-27 17:45:02 +0000

    EPBDS-849 Implement Constraint model Add validation of effective and expiration dates

---
+++ DEV/trunk/org.openl.rules/src/org/openl/rules/table/constraints/ConstraintFactory.java
@@ -0,0 +1,20 @@
+package org.openl.rules.table.constraints;
+
+/**
+ * @author Andrei Astrouski
+ */
+public class ConstraintFactory {
+
+    public Constraint getConstraint(String value) {
+        Constraint constraint = null;
+        value = value == null ? "" : value;
+        if (value.matches(LessThanConstraint.CONSTRAINT_MATCH)) {
+            constraint = new LessThanConstraint(value);
+        } else if (value.matches(MoreThanConstraint.CONSTRAINT_MATCH)) {
+            constraint = new MoreThanConstraint(value);
+        }
+        //to be continued...
+        return constraint;
+    }
+
+}
