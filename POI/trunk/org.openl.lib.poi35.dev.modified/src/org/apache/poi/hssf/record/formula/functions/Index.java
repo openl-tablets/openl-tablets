159e0ac34d93a9e2998e30520fbfba65e5d91c45
zahars@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <zahars@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2009-11-30 10:19:03 +0000

    EPBDS - POI revision 885061 merged

--- POI/trunk/org.openl.lib.poi35.dev.modified/src/org/apache/poi/hssf/record/formula/functions/Index.java
+++ POI/trunk/org.openl.lib.poi35.dev.modified/src/org/apache/poi/hssf/record/formula/functions/Index.java
@@ -25,9 +25,7 @@ import org.apache.poi.hssf.record.formula.eval.MissingArgEval;
 import org.apache.poi.hssf.record.formula.eval.OperandResolver;
 import org.apache.poi.hssf.record.formula.eval.RefEval;
 import org.apache.poi.hssf.record.formula.eval.ValueEval;
-// ZS
 import org.apache.poi.ss.formula.ArrayEval;
-// end changes ZS
 
 /**
  * Implementation for the Excel function INDEX
@@ -48,74 +46,107 @@ import org.apache.poi.ss.formula.ArrayEval;
  * @author Josh Micich
  * @author zsulkins(ZS) - array support
  */
-// ZS 
-public final class Index implements FunctionWithArraySupport, ArrayMode {
+//ZS
+public final class Index implements Function2Arg, Function3Arg, Function4Arg, FunctionWithArraySupport, ArrayMode {
 
-	public ValueEval evaluate(ValueEval[] args, int srcCellRow, int srcCellCol) {
-		return evaluateX(args, srcCellRow, srcCellCol, false);
+	public ValueEval evaluate(int srcRowIndex, int srcColumnIndex, ValueEval arg0, ValueEval arg1) {
+		return evaluateX(srcRowIndex, srcColumnIndex, arg0, arg1, false);
 	}
-
 	
-	protected  ValueEval evaluateX(ValueEval[] args, int srcCellRow, int srcCellCol, boolean supportRowCol) {
-// end changes ZS	
-		int nArgs = args.length;
-		if(nArgs < 2) {
-			// too few arguments
-			return ErrorEval.VALUE_INVALID;
-		}
-		ValueEval firstArg = args[0];
-		if (firstArg instanceof RefEval) {
-			// convert to area ref for simpler code in getValueFromArea()
-			firstArg = ((RefEval)firstArg).offset(0, 0, 0, 0);
-		}
-		// !! changed ZS
-		if (firstArg instanceof ArrayEval){
-			firstArg = ((ArrayEval)firstArg).arrayAsArea();
-			supportRowCol = true;
-		}
-		
-		// end change
-		
-		if(!(firstArg instanceof AreaEval)) {
+	
+	public ValueEval evaluateX(int srcRowIndex, int srcColumnIndex, ValueEval arg0, ValueEval arg1, boolean supportRowColumn) {
 
-			// else the other variation of this function takes an array as the first argument
-			// it seems like interface 'ArrayEval' does not even exist yet
-			throw new RuntimeException("Incomplete code - cannot handle first arg of type ("
-					+ firstArg.getClass().getName() + ")");
+		if (arg0 instanceof ArrayEval){
+			supportRowColumn = true;
 		}
-		AreaEval reference = (AreaEval) firstArg;
-
-		int rowIx = 0;
-		int columnIx = 0;
+// end changes ZS
+		AreaEval reference = convertFirstArg(arg0);
 		boolean colArgWasPassed = false;
+		int columnIx = 0;
 		try {
-			switch(nArgs) {
-				case 4:
-					throw new RuntimeException("Incomplete code" +
-							" - don't know how to support the 'area_num' parameter yet)");
-					// Excel expression might look like this "INDEX( (A1:B4, C3:D6, D2:E5 ), 1, 2, 3)
-					// In this example, the 3rd area would be used i.e. D2:E5, and the overall result would be E2
-					// Token array might be encoded like this: MemAreaPtg, AreaPtg, AreaPtg, UnionPtg, UnionPtg, ParenthesesPtg
-					// The formula parser doesn't seem to support this yet. Not sure if the evaluator does either
-
-				case 3:
-					columnIx = resolveIndexArg(args[2], srcCellRow, srcCellCol);
-					colArgWasPassed = true;
-				case 2:
-					rowIx = resolveIndexArg(args[1], srcCellRow, srcCellCol);
-					break;
-				default:
-					// too many arguments
-					return ErrorEval.VALUE_INVALID;
-			}
-			// ZS
-			// From POI:			return getValueFromArea(reference, rowIx, columnIx, colArgWasPassed, srcCellRow, srcCellCol);
-			return getValueFromArea(reference, rowIx, columnIx, colArgWasPassed, srcCellRow, srcCellCol, supportRowCol);
-			// end changes ZS
+			int rowIx = resolveIndexArg(arg1, srcRowIndex, srcColumnIndex);
+//ZS
+			return getValueFromArea(reference, rowIx, columnIx, colArgWasPassed, srcRowIndex, srcColumnIndex, supportRowColumn);
+// end changes ZS
 		} catch (EvaluationException e) {
 			return e.getErrorEval();
 		}
 	}
+
+//ZS
+	public ValueEval evaluate(int srcRowIndex, int srcColumnIndex, ValueEval arg0, ValueEval arg1,
+			ValueEval arg2) {
+		return evaluateX(srcRowIndex, srcColumnIndex, arg0, arg1, arg2, false);
+	}
+	public ValueEval evaluateX(int srcRowIndex, int srcColumnIndex, ValueEval arg0, ValueEval arg1,
+			ValueEval arg2, boolean supportRowColumn) {
+
+		if (arg0 instanceof ArrayEval){
+			supportRowColumn = true;
+		}
+//	end changes ZS	
+		AreaEval reference = convertFirstArg(arg0);
+
+		boolean colArgWasPassed = true;
+		try {
+			int columnIx = resolveIndexArg(arg2, srcRowIndex, srcColumnIndex);
+			int rowIx = resolveIndexArg(arg1, srcRowIndex, srcColumnIndex);
+//ZS
+			return getValueFromArea(reference, rowIx, columnIx, colArgWasPassed, srcRowIndex, srcColumnIndex, supportRowColumn);
+//			end changes ZS	
+		} catch (EvaluationException e) {
+			return e.getErrorEval();
+		}
+	}
+	public ValueEval evaluate(int srcRowIndex, int srcColumnIndex, ValueEval arg0, ValueEval arg1,
+			ValueEval arg2, ValueEval arg3) {
+		throw new RuntimeException("Incomplete code"
+				+ " - don't know how to support the 'area_num' parameter yet)");
+		// Excel expression might look like this "INDEX( (A1:B4, C3:D6, D2:E5 ), 1, 2, 3)
+		// In this example, the 3rd area would be used i.e. D2:E5, and the overall result would be E2
+		// Token array might be encoded like this: MemAreaPtg, AreaPtg, AreaPtg, UnionPtg, UnionPtg, ParenthesesPtg
+		// The formula parser doesn't seem to support this yet. Not sure if the evaluator does either
+	}
+
+	private static AreaEval convertFirstArg(ValueEval arg0) {
+		ValueEval firstArg = arg0;
+		if (firstArg instanceof RefEval) {
+			// convert to area ref for simpler code in getValueFromArea()
+			return ((RefEval)firstArg).offset(0, 0, 0, 0);
+		}
+		// !! changed ZS
+		if (firstArg instanceof ArrayEval){
+			firstArg = ((ArrayEval)firstArg).arrayAsArea();
+		}
+		// end change		
+		if((firstArg instanceof AreaEval)) {
+			return (AreaEval) firstArg;
+		}
+		// else the other variation of this function takes an array as the first argument
+		// it seems like interface 'ArrayEval' does not even exist yet
+		throw new RuntimeException("Incomplete code - cannot handle first arg of type ("
+				+ firstArg.getClass().getName() + ")");
+
+	}
+
+//ZS	
+	public ValueEval evaluate(ValueEval[] args, int srcRowIndex, int srcColumnIndex) {
+		return evaluateX(args, srcRowIndex, srcColumnIndex, false);
+	}
+	
+	public ValueEval evaluateX(ValueEval[] args, int srcRowIndex, int srcColumnIndex, boolean supportRowColumn) {
+// end changes ZS
+		switch (args.length) {
+			case 2:
+				return evaluateX(srcRowIndex, srcColumnIndex, args[0], args[1], supportRowColumn);
+			case 3:
+				return evaluateX(srcRowIndex, srcColumnIndex, args[0], args[1], args[2], supportRowColumn);
+			case 4:
+				return evaluate(srcRowIndex, srcColumnIndex, args[0], args[1], args[2], args[3]);
+		}
+		return ErrorEval.VALUE_INVALID;
+	}
+
 
 	/**
 	 * @param colArgWasPassed <code>false</code> if the INDEX argument list had just 2 items
@@ -124,10 +155,11 @@ public final class Index implements FunctionWithArraySupport, ArrayMode {
 	 *            <code>true</code>.  This parameter is needed because error codes are slightly
 	 *            different when only 2 args are passed.
 	 */
-//	ZS 
+//ZS
 	private static ValueEval getValueFromArea(AreaEval ae, int pRowIx, int pColumnIx,
-			boolean colArgWasPassed, int srcRowIx, int srcColIx, boolean supportRowColum) throws EvaluationException {
-//	end changes ZS		
+			boolean colArgWasPassed, int srcRowIx, int srcColIx, boolean supportRowColumn) throws EvaluationException {
+//end changes ZS	
+		
 		boolean rowArgWasEmpty = pRowIx == 0;
 		boolean colArgWasEmpty = pColumnIx == 0;
 		int rowIx;
@@ -136,7 +168,7 @@ public final class Index implements FunctionWithArraySupport, ArrayMode {
 		// !!changed ZS
 		// implementation of this function isn't support all features of the Excel
 		// here I'm adding only support for return of entire row or columm
-		if ( supportRowColum && ( (rowArgWasEmpty && !ae.isRow() && pColumnIx<=ae.getWidth() && !colArgWasEmpty) ||
+		if ( supportRowColumn && ( (rowArgWasEmpty && !ae.isRow() && pColumnIx<=ae.getWidth() && !colArgWasEmpty) ||
 			 (colArgWasEmpty && !ae.isColumn() && pRowIx<=ae.getHeight() && !rowArgWasEmpty))
 			){
 			// return row or column
@@ -155,9 +187,8 @@ public final class Index implements FunctionWithArraySupport, ArrayMode {
 			}
 			return (new ArrayEval(result));
 		}
-		// end change
-		
-		
+// end change
+
 		// when the area ref is a single row or a single column,
 		// there are special rules for conversion of rowIx and columnIx
 		if (ae.isRow()) {
@@ -234,7 +265,7 @@ public final class Index implements FunctionWithArraySupport, ArrayMode {
 		if (ev == MissingArgEval.instance) {
 			return 0;
 		}
-		if (ev == BlankEval.INSTANCE) {
+		if (ev == BlankEval.instance) {
 			return 0;
 		}
 		int result = OperandResolver.coerceValueToInt(ev);
@@ -243,7 +274,8 @@ public final class Index implements FunctionWithArraySupport, ArrayMode {
 		}
 		return result;
 	}
-// ZS
+	
+	// ZS
 	/* (non-Javadoc)
 	 * @see org.apache.poi.hssf.record.formula.functions.ArrayMode#evaluateInArrayFormula(org.apache.poi.hssf.record.formula.eval.ValueEval[], int, short)
 	 */
@@ -260,5 +292,5 @@ public final class Index implements FunctionWithArraySupport, ArrayMode {
 			return true;
 		return false;
 	}
-//   end changes ZS
+//   end changes ZS	
 }
