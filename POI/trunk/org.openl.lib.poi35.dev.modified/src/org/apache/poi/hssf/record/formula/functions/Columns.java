96632ae1921182c0bbc37c974b6d5086ab2af466
zahars <zahars@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2009-11-30 09:03:22 +0000

    EPBDS - supported new interface

--- POI/trunk/org.openl.lib.poi35.dev.modified/src/org/apache/poi/hssf/record/formula/functions/Columns.java
+++ POI/trunk/org.openl.lib.poi35.dev.modified/src/org/apache/poi/hssf/record/formula/functions/Columns.java
@@ -22,50 +22,34 @@ import org.apache.poi.hssf.record.formula.eval.ErrorEval;
 import org.apache.poi.hssf.record.formula.eval.NumberEval;
 import org.apache.poi.hssf.record.formula.eval.RefEval;
 import org.apache.poi.hssf.record.formula.eval.ValueEval;
-// ZS
 import org.apache.poi.ss.formula.ArrayEval;
-// end changes ZS
 
 /**
  * Implementation for Excel COLUMNS function.
  *
  * @author Josh Micich
- * @author zshulkins(ZS) array support
  */
-// ZS 
-public final class Columns implements FunctionWithArraySupport {
-// end changes ZS
+public final class Columns extends Fixed1ArgFunction implements FunctionWithArraySupport {
 
-	public ValueEval evaluate(ValueEval[] args, int srcCellRow, int srcCellCol) {
-		switch(args.length) {
-			case 1:
-				// expected
-				break;
-			case 0:
-				// too few arguments
-				return ErrorEval.VALUE_INVALID;
-			default:
-				// too many arguments
-				return ErrorEval.VALUE_INVALID;
-		}
-		ValueEval firstArg = args[0];
+	public ValueEval evaluate(int srcRowIndex, int srcColumnIndex, ValueEval arg0) {
 
 		int result;
 		// !!changed ZS
-		if (firstArg instanceof ArrayEval){
-			firstArg = ((ArrayEval)firstArg).arrayAsArea();
+		if (arg0 instanceof ArrayEval){
+			arg0 = ((ArrayEval)arg0).arrayAsArea();
 		}
 		// end change
-		if (firstArg instanceof AreaEval) {
-			AreaEval ae = (AreaEval) firstArg;
-			result = ae.getLastColumn() - ae.getFirstColumn() + 1;
-		} else if (firstArg instanceof RefEval) {
+		
+		if (arg0 instanceof AreaEval) {
+			result = ((AreaEval) arg0).getWidth();
+		} else if (arg0 instanceof RefEval) {
 			result = 1;
 		} else { // anything else is not valid argument
 			return ErrorEval.VALUE_INVALID;
 		}
 		return new NumberEval(result);
 	}
+	
 //	ZS
 	/* (non-Javadoc)
 	 * @see org.apache.poi.hssf.record.formula.functions.FunctionWithArraySupport#supportArray(int)
