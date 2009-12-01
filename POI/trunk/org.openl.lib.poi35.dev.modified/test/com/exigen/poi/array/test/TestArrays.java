681353412f748c288e380cefad962b9f2c434a3a
pudalau <pudalau@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2009-12-01 15:06:55 +0000

    EPBDS Enabled tests for maven build process.

--- POI/trunk/org.openl.lib.poi35.dev.modified/test/com/exigen/poi/array/test/TestArrays.java
+++ POI/trunk/org.openl.lib.poi35.dev.modified/test/com/exigen/poi/array/test/TestArrays.java
@@ -3,9 +3,10 @@ package com.exigen.poi.array.test;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
-public class ArrayTests {
+public class TestArrays {
+    @org.junit.Test
     public static Test suite() {
-        TestSuite suite = new TestSuite(ArrayTests.class.getName());
+        TestSuite suite = new TestSuite(TestArrays.class.getName());
         	suite.addTest(ArrayFormulaSetRemoveTests.suite());
         	suite.addTest(ArrayEvalTests.suite());
         return suite;
