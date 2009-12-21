5eb850776530ac1ab1cdc71ba4bb4a6c1d6b05f6
pudalau <pudalau@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2009-12-21 14:35:34 +0000

    EPBDS Update after POI changes.

--- POI/trunk/org.openl.lib.poi35.dev.modified/src/org/apache/poi/ss/formula/WorkbookEvaluator.java
+++ POI/trunk/org.openl.lib.poi35.dev.modified/src/org/apache/poi/ss/formula/WorkbookEvaluator.java
@@ -50,7 +50,6 @@ import org.apache.poi.hssf.record.formula.RefPtg;
 import org.apache.poi.hssf.record.formula.StringPtg;
 import org.apache.poi.hssf.record.formula.UnionPtg;
 import org.apache.poi.hssf.record.formula.UnknownPtg;
-import org.apache.poi.hssf.record.formula.eval.AreaEval;
 import org.apache.poi.hssf.record.formula.eval.BlankEval;
 import org.apache.poi.hssf.record.formula.eval.BoolEval;
 import org.apache.poi.hssf.record.formula.eval.ErrorEval;
@@ -59,7 +58,7 @@ import org.apache.poi.hssf.record.formula.eval.MissingArgEval;
 import org.apache.poi.hssf.record.formula.eval.NameEval;
 import org.apache.poi.hssf.record.formula.eval.NameXEval;
 import org.apache.poi.hssf.record.formula.eval.NumberEval;
-import org.apache.poi.hssf.record.formula.eval.RefEval;
+import org.apache.poi.hssf.record.formula.eval.OperandResolver;
 import org.apache.poi.hssf.record.formula.eval.StringEval;
 import org.apache.poi.hssf.record.formula.eval.ValueEval;
 import org.apache.poi.hssf.record.formula.function.FunctionMetadataRegistry;
@@ -213,7 +212,7 @@ public final class WorkbookEvaluator {
 
 	public ValueEval evaluate(EvaluationCell srcCell) {
 		int sheetIndex = getSheetIndex(srcCell.getSheet());
-        return evaluateAny(srcCell, sheetIndex, srcCell.getRowIndex(), srcCell.getColumnIndex(), new EvaluationTracker(_cache));
+		return evaluateAny(srcCell, sheetIndex, srcCell.getRowIndex(), srcCell.getColumnIndex(), new EvaluationTracker(_cache));
 	}
 
 	/**
@@ -282,7 +281,7 @@ public final class WorkbookEvaluator {
 				tracker.endEvaluate(cce);
 			}
 		} else {
-			if (evalListener != null) {
+			if(evalListener != null) {
 				evalListener.onCacheHit(sheetIndex, rowIndex, columnIndex, cce.getValue());
 			}
 			return cce.getValue();
@@ -548,30 +547,28 @@ public final class WorkbookEvaluator {
 		return ArrayFormulaEvaluatorHelper.dereferenceValue(evaluationResult,cell);
 	}
 	/**
-	 * Dereferences a single value from any AreaEval or RefEval evaluation result.
-	 * If the supplied evaluationResult is just a plain value, it is returned as-is.
-	 * @return a <tt>NumberEval</tt>, <tt>StringEval</tt>, <tt>BoolEval</tt>,
-	 *  <tt>BlankEval</tt> or <tt>ErrorEval</tt>. Never <code>null</code>.
+	 * Dereferences a single value from any AreaEval or RefEval evaluation
+	 * result. If the supplied evaluationResult is just a plain value, it is
+	 * returned as-is.
+	 *
+	 * @return a {@link NumberEval}, {@link StringEval}, {@link BoolEval}, or
+	 *         {@link ErrorEval}. Never <code>null</code>. {@link BlankEval} is
+	 *         converted to {@link NumberEval#ZERO}
 	 */
 	public static ValueEval dereferenceValue(ValueEval evaluationResult, int srcRowNum, int srcColNum) {
-		if (evaluationResult instanceof RefEval) {
-			RefEval rv = (RefEval) evaluationResult;
-			return rv.getInnerValueEval();
+		ValueEval value;
+		try {
+			value = OperandResolver.getSingleValue(evaluationResult, srcRowNum, srcColNum);
+		} catch (EvaluationException e) {
+			return e.getErrorEval();
 		}
-		if (evaluationResult instanceof AreaEval) {
-			AreaEval ae = (AreaEval) evaluationResult;
-			if (ae.isRow()) {
-				if (ae.isColumn()) {
-					return ae.getRelativeValue(0, 0);
-				}
-				return ae.getAbsoluteValue(ae.getFirstRow(), srcColNum);
-			}
-			if (ae.isColumn()) {
-				return ae.getAbsoluteValue(srcRowNum, ae.getFirstColumn());
-			}
-			return ErrorEval.VALUE_INVALID;
+		if (value == BlankEval.instance) {
+			// Note Excel behaviour here. A blank final final value is converted to zero.
+			return NumberEval.ZERO;
+			// Formulas _never_ evaluate to blank.  If a formula appears to have evaluated to
+			// blank, the actual value is empty string. This can be verified with ISBLANK().
 		}
-		return evaluationResult;
+		return value;
 	}
 
 
