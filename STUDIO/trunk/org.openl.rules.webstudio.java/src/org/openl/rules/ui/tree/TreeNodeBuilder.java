d5749546b9ec8914b472e14c13f662302d725772
pudalau@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <pudalau@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2010-02-16 07:51:22 +0000

    EPBDS-998 Fixed problems in tree building of properties tables .

--- STUDIO/trunk/org.openl.rules.webstudio.java/src/org/openl/rules/ui/tree/TreeNodeBuilder.java
+++ STUDIO/trunk/org.openl.rules.webstudio.java/src/org/openl/rules/ui/tree/TreeNodeBuilder.java
@@ -7,9 +7,37 @@ package org.openl.rules.ui.tree;
  */
 public interface TreeNodeBuilder<T extends Object> {
     
+    /**
+     * Generates comparable key that will be used for detecting similar nodes.
+     * 
+     * @param object The object for key generating.
+     * @return Key for object.
+     */
     Comparable<?> makeKey(T object);
+    
+    /**
+     * @param object The object that will be displayed in the tree
+     * @param i Order number of object.
+     * @return TreeNode generated for the object.
+     */
     ITreeNode<Object> makeNode(T object, int i);
 
+    /**
+     * Generates comparable key for similar keys. It only used if
+     * <code>isUnique==true</code>. Then we have to create nodes for each
+     * similar object. The key will be generated with using of order number of
+     * key. (The first node have number <code>0</code>, the second -
+     * <code>2</code>, the third <code>3</code> etc.)
+     * 
+     * @param object The object for key generating.
+     * @param i Order number of object.
+     * @return Key for object.
+     */
     Comparable<?> makeKey(T object, int i);
+
+    /**
+     * @return <code>true</code> if each node have to be displayed in even if
+     *         the tree already has element with similar key
+     */
     boolean isUnique();
 }
