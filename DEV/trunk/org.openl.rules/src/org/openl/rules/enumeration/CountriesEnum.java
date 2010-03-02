f359f39137bef69f71b1be7119655bb645bf4c19
dliauchuk@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <dliauchuk@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2010-03-02 15:50:34 +0000

    EPBDS-781 alphabet sorting

--- DEV/trunk/org.openl.rules/src/org/openl/rules/enumeration/CountriesEnum.java
+++ DEV/trunk/org.openl.rules/src/org/openl/rules/enumeration/CountriesEnum.java
@@ -2,22 +2,22 @@ package org.openl.rules.enumeration;
 
 public enum CountriesEnum {
 
-	US("United States of America"),
+	AR("Argentina"),
 	AU("Australia"),
+	BR("Brazil"),
+	CA("Canada"),
+	CH("China"),
+	DE("Germany"),
+	FR("France"),
+	GB("United Kingdom"),
+	IN("India"),
+	IT("Italy"),
 	MX("Mexico"),
 	NZ("New Zealand"),
-	BR("Brazil"),
-	AR("Argentina"),
-	FR("France"),
-	DE("Germany"),
-	CH("China"),
-	IT("Italy"),
 	PT("Portugal"),
-	IN("India"),
-	GB("United Kingdom"),
-	ZA("South Africa"),
 	RU("Russian Federation"),
-	CA("Canada");
+	US("United States of America"),
+	ZA("South Africa");
 
 	private final String displayName;
 
