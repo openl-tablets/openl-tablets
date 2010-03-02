84016527bb5320ef703ed9fa99d18cae0120c560
dliauchuk@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <dliauchuk@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2010-03-02 16:06:08 +0000

    EPBDS-781 update tests with new enums

--- DEV/trunk/org.openl.rules/test/org/openl/rules/property/PropertyTableTest.java
+++ DEV/trunk/org.openl.rules/test/org/openl/rules/property/PropertyTableTest.java
@@ -5,6 +5,9 @@ import java.util.Map;
 
 import org.junit.Test;
 import org.openl.rules.BaseOpenlBuilder;
+import org.openl.rules.enumeration.LanguagesEnum;
+import org.openl.rules.enumeration.RegionsEnum;
+import org.openl.rules.enumeration.UsregionsEnum;
 import org.openl.rules.lang.xls.syntax.TableSyntaxNode;
 import org.openl.rules.table.properties.ITableProperties;
 import org.openl.rules.table.properties.inherit.InheritanceLevel;
@@ -32,15 +35,15 @@ public class PropertyTableTest extends BaseOpenlBuilder{
             assertTrue(categoryProperties.size() == 4);
             assertEquals(InheritanceLevel.CATEGORY.getDisplayName(),(String) categoryProperties.get("scope"));
             assertEquals("newLob",(String) categoryProperties.get("lob"));
-            assertEquals("alaska",(String) categoryProperties.get("usregion"));                
-            assertEquals("east",(String) categoryProperties.get("region"));
+            assertEquals(UsregionsEnum.SE.name(), ((UsregionsEnum)categoryProperties.get("usregion")).name());                
+            assertEquals(RegionsEnum.NCSA.name(),((RegionsEnum) categoryProperties.get("region")).name());
                 
              Map<String, Object> defaultProperties = tableProperties.getPropertiesAppliedByDefault();
             // assertTrue(defaultProperties.size() == 5);
             // assertEquals("US",(String) defaultProperties.get("country"));
             assertEquals(org.openl.rules.enumeration.CurrenciesEnum.USD,
                     (org.openl.rules.enumeration.CurrenciesEnum) defaultProperties.get("currency"));
-            assertEquals("en",(String) defaultProperties.get("lang"));
+            assertEquals(LanguagesEnum.EN.name(),((LanguagesEnum) defaultProperties.get("lang")).name());
             assertTrue((Boolean) defaultProperties.get("active"));
             assertTrue((Boolean) defaultProperties.get("failOnMiss"));
             } else {
