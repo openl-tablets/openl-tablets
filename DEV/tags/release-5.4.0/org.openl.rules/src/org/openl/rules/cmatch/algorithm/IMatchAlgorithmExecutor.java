74219a80a80345dc3d3dec8d49768e6658e5480e
pudalau <pudalau@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2010-03-04 15:07:29 +0000

    EPBDS removed wrong release.

--- DEV/tags/release-5.4.0/org.openl.rules/src/org/openl/rules/cmatch/algorithm/IMatchAlgorithmExecutor.java
+++
@@ -1,8 +0,0 @@
-package org.openl.rules.cmatch.algorithm;
-
-import org.openl.rules.cmatch.ColumnMatch;
-import org.openl.vm.IRuntimeEnv;
-
-public interface IMatchAlgorithmExecutor {
-    Object invoke(Object target, Object[] params, IRuntimeEnv env, ColumnMatch columnMatch);
-}
