f3ba2f43fc807e3346e2445cfbeef3eb29d3f45b
aastrouski@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <aastrouski@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2010-04-17 09:08:36 +0000

    EPBDS-1181 Fix classpath issues

--- TUT/trunk/org.openl.tablets.deploy/ruleservice/run.bat
+++ TUT/trunk/org.openl.tablets.deploy/ruleservice/run.bat
@@ -23,7 +23,7 @@ lib\commons-digester-1.6.jar;^
 lib\commons-discovery-0.4.jar;^
 lib\commons-fileupload-1.2.jar;^
 lib\commons-io-1.3.2.jar;^
-lib\commons-lang-2.4.jar;^
+lib\commons-lang-2.5.jar;^
 lib\commons-logging-1.1.1.jar;^
 lib\commons-pool-1.3.jar;^
 lib\commons-validator-1.3.1.jar;^
