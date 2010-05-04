a5fa43aaa0f467295cce166365032427a516c830
aastrouski <aastrouski@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2010-05-04 12:00:31 +0000

    EPBDS-1255 Rewrite "Simple search" to .xhtml

---
+++ STUDIO/trunk/org.openl.rules.webstudio.java/src/org/openl/rules/webstudio/web/search/SearchResultsBean.java
@@ -0,0 +1,80 @@
+package org.openl.rules.webstudio.web.search;
+
+import org.ajax4jsf.component.UIRepeat;
+import org.openl.rules.ui.ProjectModel;
+import org.openl.rules.ui.WebStudio;
+import org.openl.rules.webstudio.web.util.WebStudioUtils;
+import org.openl.rules.webtools.WebTool;
+import org.openl.rules.webtools.indexer.FileIndexer;
+import org.openl.util.StringTool;
+
+/**
+ * Request scope managed bean providing logic for Search Results include page of OpenL Studio.
+ */
+public class SearchResultsBean {
+
+    private UIRepeat searchResults;
+
+    public SearchResultsBean() {
+    }
+
+    public UIRepeat getSearchResults() {
+        return searchResults;
+    }
+
+    public void setSearchResults(UIRepeat searchResults) {
+        this.searchResults = searchResults;
+    }
+
+    public String getUri() {
+        String[] searchResult = (String[]) searchResults.getRowData();
+        return searchResult[0];
+    }
+
+    public String getEncodedUri() {
+        String uri = getUri();
+        return StringTool.encodeURL(uri);
+    }
+
+    public boolean isReadOnly() {
+        WebStudio studio = WebStudioUtils.getWebStudio();
+        ProjectModel model = studio.getModel();
+        return model.isReadOnly();
+    }
+
+    public boolean isCanViewTable() {
+        WebStudio studio = WebStudioUtils.getWebStudio();
+        ProjectModel model = studio.getModel();
+
+        String uri = getUri();
+
+        return model.getNode(uri) == null;
+    }
+
+    public String getFileHeader() {
+        String uri = getUri();
+        return FileIndexer.showElementHeader(uri);
+    }
+
+    public boolean isXlsFile() {
+        String uri = getUri();
+        if (uri.indexOf(".xls") >= 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isDocFile() {
+        String uri = getUri();
+        if (uri.indexOf(".doc") >= 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public String getXlsOrDocUrlLink() {
+        String uri = getUri();
+        return WebTool.makeXlsOrDocUrl(uri);
+    }
+
+}
