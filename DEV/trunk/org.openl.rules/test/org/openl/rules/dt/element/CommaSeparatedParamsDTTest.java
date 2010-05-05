bcf3f151875363767d57177ec0704ccb0f5974e0
dliauchuk <dliauchuk@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2010-05-05 10:14:43 +0000

    EPBDS update comma separated functionality for primitives. Update tests.

--- DEV/trunk/org.openl.rules/test/org/openl/rules/dt/element/CommaSeparatedParamsDTTest.java
+++ DEV/trunk/org.openl.rules/test/org/openl/rules/dt/element/CommaSeparatedParamsDTTest.java
@@ -22,14 +22,55 @@ public class CommaSeparatedParamsDTTest {
     }
     
     @Test
-    public void testcommaSeparated() {
-        test("firstValue", "comma2", "Good Morning");                
+    public void testcommaSeparatedString() {
+        testString("firstValue", "comma2", "Good Morning");                
         
-        test("value1", "singleValue", "Good Afternoon");                
+        testString("value1", "singleValue", "Good Afternoon");                
     }
     
-    private void test(String income1, String income2, String expectedResult) {
+    @Test
+    public void testCommaSeparatedInt() {
+        testInt(12, 17, "Rule 10 Fire");
+    }
+    
+    @Test
+    public void testArrayParameterBexCodeSnippetsContains() {
+        testBexSnippetContains(27, 29, "Rule 20 Fire");
+    }
+    
+    @Test
+    public void testArrayParametersContainsAll() {
+        testArrayParametersContainsAll(new int[]{12, 14}, new int[]{13, 17}, "Rule 10 Fire");
+    }
+    
+    @Test
+    public void testArrayParametersContainsAllString() {
+        testArrayParametersContainsAllString(new String[]{"12", "14"}, new String[]{"13", "17"}, "Rule 10 Fire");
+    }
+    
+    private void testArrayParametersContainsAllString(String[] income1, String[] income2, String expectedResult) {
+        String result = test.testArrayParametersContainsAllString(income1, income2);
+        assertEquals(expectedResult, result);
+        
+    }
+
+    private void testArrayParametersContainsAll(int[] income1, int[] income2, String expectedResult) {
+        String result = test.testArrayParametersContainsAll(income1, income2);
+        assertEquals(expectedResult, result);
+    }
+
+    private void testInt(int income1, int income2, String expectedResult) {
+        String result = test.helloInt(income1, income2);
+        assertEquals(expectedResult, result);        
+    }
+
+    private void testString(String income1, String income2, String expectedResult) {
         String result = test.hello2(income1, income2);        
+        assertEquals(expectedResult, result);
+    }
+    
+    private void testBexSnippetContains(int income1, int income2, String expectedResult) {
+        String result = test.testArrayParameterBexCodeSnippetsContains(income1, income2);
         assertEquals(expectedResult, result);
     }
 
