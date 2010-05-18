51ac378e3bf1e425b3ac767a37474040d6d6df03
aastrouski@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <aastrouski@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2010-05-18 15:53:55 +0000

    EPBDS-1279 Create wizard for Datatype component

--- STUDIO/trunk/org.openl.rules.webstudio.java/src/org/openl/rules/ui/tablewizard/TypeNamePair.java
+++ STUDIO/trunk/org.openl.rules.webstudio.java/src/org/openl/rules/ui/tablewizard/TypeNamePair.java
@@ -1,10 +1,19 @@
 package org.openl.rules.ui.tablewizard;
 
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
 /**
  * @author Aliaksandr Antonik.
  */
 public class TypeNamePair {
+
+    @NotEmpty(message="Parameter type can not be empty")
     private String type;
+
+    @NotEmpty(message="Parameter name can not be empty")
+    @Pattern(regexp="([a-zA-Z_][a-zA-Z_0-9]*)?", message="Invalid name for parameter")
     private String name;
 
     public String getName() {
