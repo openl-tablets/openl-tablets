1c38b2f8bdf3cc099af6f8d2fa7f6fd6783cb993
pudalau <pudalau@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2010-05-20 06:18:27 +0000

    EPBDS-1293 Reimlemented WSFrontend for new features.

--- WSFrontend/trunk/org.openl.rules.ruleservice.ws/src/org/openl/rules/ruleservice/RuleServiceMain.java
+++ WSFrontend/trunk/org.openl.rules.ruleservice.ws/src/org/openl/rules/ruleservice/RuleServiceMain.java
@@ -1,10 +1,12 @@
 package org.openl.rules.ruleservice;
 
 import org.openl.rules.repository.exceptions.RRepositoryException;
-import org.openl.rules.ruleservice.loader.RulesLoader;
+import org.openl.rules.ruleservice.loader.RulesLoaderJcr;
+import org.openl.rules.ruleservice.publish.DeploymentAdmin;
 import org.openl.rules.ruleservice.publish.WebServicesDeployAdmin;
 import org.openl.rules.ruleservice.publish.RulesPublisher;
 import org.openl.rules.ruleservice.resolver.RulesProjectResolver;
+import org.openl.rules.workspace.production.client.JcrRulesClient;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -13,16 +15,15 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class RuleServiceMain extends RuleServiceBase {
 
-    public RuleServiceMain() {
-        loader = new RulesLoader();
-        deployAdmin = new WebServicesDeployAdmin();
-        resolver = new RulesProjectResolver();
+    public RuleServiceMain() throws RRepositoryException {
+        loader = new RulesLoaderJcr(new JcrRulesClient());
         publisher = new RulesPublisher();
+        publisher.setRulesProjectResolver(new RulesProjectResolver());
+        publisher.setDeployAdmin(getDeploymentAdmin());
     }
-
-    public static void main(String[] args) throws RRepositoryException, InterruptedException, Exception {
+    
+    private DeploymentAdmin getDeploymentAdmin(){
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("cxf.xml");
-
         WebServicesDeployAdmin admin;
         if (applicationContext.containsBean("deploymentAdmin")) {
             admin = (WebServicesDeployAdmin) applicationContext.getBean("deploymentAdmin");
@@ -30,8 +31,11 @@ public class RuleServiceMain extends RuleServiceBase {
             admin = new WebServicesDeployAdmin();
 
         }
+        return admin;
+    }
+
+    public static void main(String[] args) throws RRepositoryException{
         RuleServiceMain ruleService = new RuleServiceMain();
-        ruleService.setDeployAdmin(admin);
-        ruleService.runFrontend();
+        ruleService.run();
     }
 }
