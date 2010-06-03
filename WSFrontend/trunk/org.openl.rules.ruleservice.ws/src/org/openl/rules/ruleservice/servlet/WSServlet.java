50fd248f90e3671a8d99cd2c823759924cb29edb
pudalau@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <pudalau@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2010-06-03 12:43:45 +0000

    EPBDS-1221 WS refactoring.

--- WSFrontend/trunk/org.openl.rules.ruleservice.ws/src/org/openl/rules/ruleservice/servlet/WSServlet.java
+++ WSFrontend/trunk/org.openl.rules.ruleservice.ws/src/org/openl/rules/ruleservice/servlet/WSServlet.java
@@ -4,10 +4,7 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 
 import org.apache.cxf.transport.servlet.CXFServlet;
-import org.openl.rules.repository.exceptions.RRepositoryException;
 import org.openl.rules.ruleservice.RuleServiceBase;
-import org.openl.rules.ruleservice.RuleServiceMain;
-import org.openl.rules.ruleservice.publish.WebServicesDeployAdmin;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
@@ -24,14 +21,9 @@ public class WSServlet extends CXFServlet {
         if (applicationContext.containsBean("ruleService")) {
             ruleService = (RuleServiceBase) applicationContext.getBean("ruleService");
         } else {
-            try {
-                ruleService = new RuleServiceMain();
-            } catch (RRepositoryException e) {
-                throw new ServletException(e);
-            }
+            throw new ServletException(
+                    "Could not instaniate rule service. Make sure that you have configured bean \"ruleService\"");
         }
-        WebServicesDeployAdmin deployAdmin = (WebServicesDeployAdmin)ruleService.getPublisher().getDeployAdmin();
-        deployAdmin.setContext(applicationContext);
         ruleService.run();
     }
 }
