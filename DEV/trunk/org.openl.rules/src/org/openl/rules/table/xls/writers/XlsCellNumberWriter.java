55d4db2802d450b891db887feb7a7d7ee1691971
aastrouski@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <aastrouski@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2010-06-08 12:38:28 +0000

    EPBDS-1369 Fix numeric editor

--- DEV/trunk/org.openl.rules/src/org/openl/rules/table/xls/writers/XlsCellNumberWriter.java
+++ DEV/trunk/org.openl.rules/src/org/openl/rules/table/xls/writers/XlsCellNumberWriter.java
@@ -5,16 +5,16 @@ import org.openl.rules.table.xls.XlsSheetGridModel;
 public class XlsCellNumberWriter extends AXlsCellWriter{
 
     public XlsCellNumberWriter(XlsSheetGridModel xlsSheetGridModel) {
-        super(xlsSheetGridModel);        
+        super(xlsSheetGridModel);
     }
 
     @Override
-    public void writeCellValue() {        
+    public void writeCellValue() {
         Number numberValue = (Number) getValueToWrite();
         getCellToWrite().setCellValue(numberValue.doubleValue());
-        
-        // we need to set cell meta info for the cell, to open appropriate editor for it on UI.        
-        setMetaInfo(Number.class);
-    }    
+
+        // we need to set cell meta info for the cell, to open appropriate editor for it on UI.
+        setMetaInfo(numberValue.getClass());
+    }
 
 }
