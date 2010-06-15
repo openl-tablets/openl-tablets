faa52deb225f736263a4d09a6e6e26b0e3e67ae4
pudalau@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <pudalau@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2010-06-15 15:02:30 +0000

    EPBDS-1221 Small fix.

--- TUT/trunk/org.openl.rules.tutorials/org.openl.tablets.deploy/webapps/demo/WEB-INF/java/org/openl/tablets/tutorial4/client/WebServiceTemplate.java
+++ TUT/trunk/org.openl.rules.tutorials/org.openl.tablets.deploy/webapps/demo/WEB-INF/java/org/openl/tablets/tutorial4/client/WebServiceTemplate.java
@@ -2,14 +2,14 @@ package org.openl.tablets.tutorial4.client;
 
 import org.apache.cxf.aegis.databinding.AegisDatabinding;
 import org.apache.cxf.frontend.ClientProxyFactoryBean;
-import org.openl.tablets.tutorial4.Tutorial4ClientInterface;
+import org.openl.tablets.tutorial4.Tutorial_4Wrapper;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
 public class WebServiceTemplate {
-	private Tutorial4ClientInterface clientInterface;
+	private Tutorial_4Wrapper clientInterface;
 
 	private static final WebServiceTemplate INSTANCE = new WebServiceTemplate();
 
@@ -19,16 +19,15 @@ public class WebServiceTemplate {
 		return INSTANCE;
 	}
 
-	private Tutorial4ClientInterface createClientInterface() throws IOException {
-		ClientProxyFactoryBean factory = new ClientProxyFactoryBean();
-		factory.setServiceClass(Tutorial4ClientInterface.class);
-		factory.setAddress(getAddress());
-		factory.getServiceFactory().setDataBinding(new AegisDatabinding());
+    private Tutorial_4Wrapper createClientInterface() throws IOException {
+        ClientProxyFactoryBean factory = new ClientProxyFactoryBean();
+        factory.setServiceClass(Tutorial_4Wrapper.class);
+        factory.setWsdlLocation(getAddress() + "?wsdl");
+        factory.setDataBinding(new AegisDatabinding());
+        return (Tutorial_4Wrapper) factory.create();
+    }
 
-		return (Tutorial4ClientInterface) factory.create();
-	}
-
-	private synchronized Tutorial4ClientInterface getClientInterface() throws IOException {
+	private synchronized Tutorial_4Wrapper getClientInterface() throws IOException {
 		if (clientInterface == null) {
 			return clientInterface = createClientInterface();
 		}
