f83f1bdc9484bbd74c0fe1517e24da91f952ee95
pudalau@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <pudalau@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2010-06-16 08:27:09 +0000

    EPBDS-345.[maven-release-plugin] copy for tag release-5.7.0

---
+++ TUT/tags/release-5.7.0/org.openl.rules.tutorials/org.openl.rules.examples.insurance/gen/org/openl/generated/beans/InsurableVehicle.java
@@ -0,0 +1,68 @@
+/*
+ * This class has been generated. Do not change it. 
+*/
+
+package org.openl.generated.beans;
+
+import java.lang.String;
+
+public class InsurableVehicle{
+  private int yearlyMileage;
+
+  private boolean hasAbs;
+
+  private java.lang.String make;
+
+  private boolean hasAlarm;
+
+  private java.lang.String id;
+
+  private java.lang.String model;
+
+  private int year;
+
+
+  public void setId(java.lang.String id) {
+   this.id = id;
+}
+  public int getYearlyMileage() {
+   return yearlyMileage;
+}
+  public void setYearlyMileage(int yearlyMileage) {
+   this.yearlyMileage = yearlyMileage;
+}
+  public boolean getHasAbs() {
+   return hasAbs;
+}
+  public void setHasAbs(boolean hasAbs) {
+   this.hasAbs = hasAbs;
+}
+  public java.lang.String getMake() {
+   return make;
+}
+  public void setMake(java.lang.String make) {
+   this.make = make;
+}
+  public boolean getHasAlarm() {
+   return hasAlarm;
+}
+  public void setHasAlarm(boolean hasAlarm) {
+   this.hasAlarm = hasAlarm;
+}
+  public java.lang.String getId() {
+   return id;
+}
+  public java.lang.String getModel() {
+   return model;
+}
+  public int getYear() {
+   return year;
+}
+  public void setModel(java.lang.String model) {
+   this.model = model;
+}
+  public void setYear(int year) {
+   this.year = year;
+}
+
+}
---
+++ TUT/tags/release-5.7.0/org.openl.rules.tutorials/org.openl.rules.examples.insurance/gen/org/openl/generated/beans/VehicleCoverageType.java
@@ -0,0 +1,45 @@
+/*
+ * This class has been generated. Do not change it. 
+*/
+
+package org.openl.generated.beans;
+
+import org.openl.generated.beans.LimitsAndFactors;
+import org.openl.generated.beans.InsurableVehicle;
+
+public class VehicleCoverageType{
+  private org.openl.generated.beans.InsurableVehicle vehicle;
+
+  private org.openl.generated.beans.LimitsAndFactors limitMP;
+
+  private org.openl.generated.beans.LimitsAndFactors limitBI;
+
+  private org.openl.generated.beans.LimitsAndFactors limitPD;
+
+
+  public org.openl.generated.beans.InsurableVehicle getVehicle() {
+   return vehicle;
+}
+  public void setVehicle(org.openl.generated.beans.InsurableVehicle vehicle) {
+   this.vehicle = vehicle;
+}
+  public org.openl.generated.beans.LimitsAndFactors getLimitMP() {
+   return limitMP;
+}
+  public void setLimitMP(org.openl.generated.beans.LimitsAndFactors limitMP) {
+   this.limitMP = limitMP;
+}
+  public org.openl.generated.beans.LimitsAndFactors getLimitBI() {
+   return limitBI;
+}
+  public void setLimitBI(org.openl.generated.beans.LimitsAndFactors limitBI) {
+   this.limitBI = limitBI;
+}
+  public org.openl.generated.beans.LimitsAndFactors getLimitPD() {
+   return limitPD;
+}
+  public void setLimitPD(org.openl.generated.beans.LimitsAndFactors limitPD) {
+   this.limitPD = limitPD;
+}
+
+}
