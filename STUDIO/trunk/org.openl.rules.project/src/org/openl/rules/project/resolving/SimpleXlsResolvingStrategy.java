c92dc9f23bc655b10ca2a88dcc2761ae6f31628a
pudalau <pudalau@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2010-06-29 09:35:52 +0000

    EPBDS-1390 Resolver for simple project with only XLS file.

---
+++ STUDIO/trunk/org.openl.rules.project/src/org/openl/rules/project/resolving/SimpleXlsResolvingStrategy.java
@@ -0,0 +1,62 @@
+package org.openl.rules.project.resolving;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.io.FilenameUtils;
+import org.openl.rules.project.model.Module;
+import org.openl.rules.project.model.ModuleType;
+import org.openl.rules.project.model.PathEntry;
+import org.openl.rules.project.model.ProjectDescriptor;
+import org.openl.util.tree.FileTreeIterator.FileTreeAdaptor;
+
+/**
+ * Resolver for simple OpenL project with only xls file.
+ * 
+ * ProjectDescriptor will be created with modules for each xls.
+ * 
+ * @author PUdalau
+ */
+public class SimpleXlsResolvingStrategy implements ResolvingStrategy {
+
+    public boolean isRulesProject(File folder, FileTreeAdaptor fileTreeAdaptor) {
+        if (!folder.isDirectory()) {
+            return false;
+        }
+        for (File f : folder.listFiles()) {
+            if (f.getName().endsWith(".xls") || f.getName().endsWith(".xlsx")) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public ProjectDescriptor resolveProject(File folder, FileTreeAdaptor fileTreeAdaptor) {
+        ProjectDescriptor project = createDescriptor(folder);
+        List<Module> modules = new ArrayList<Module>();
+        for (File f : folder.listFiles()) {
+            if (f.getName().endsWith(".xls") || f.getName().endsWith(".xlsx")) {
+                modules.add(createModule(project, f));
+            }
+        }
+        project.setModules(modules);
+        project.setName(folder.getName());
+        return project;
+    }
+
+    private Module createModule(ProjectDescriptor project, File xlsFile) {
+        Module module = new Module();
+        module.setProject(project);
+        module.setRulesRootPath(new PathEntry(xlsFile.getAbsolutePath()));
+        module.setType(ModuleType.API);
+        module.setName(FilenameUtils.removeExtension(xlsFile.getName()));
+        return module;
+    }
+
+    private ProjectDescriptor createDescriptor(File folder) {
+        ProjectDescriptor project = new ProjectDescriptor();
+        project.setProjectFolder(folder);
+        return project;
+    }
+}
