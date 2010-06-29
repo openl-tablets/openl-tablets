cb23e54a40fe34ab82c6b3f4b3ae7c567cbb6bac
agamanovich <agamanovich@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2010-06-29 10:44:47 +0000

    EPBDS: Updated simple OpenL Rules project (maven based).

--- Util/trunk/openl-simple-project/src/main/java/template/Main.java
+++ Util/trunk/openl-simple-project/src/main/java/template/Main.java
@@ -1,15 +1,15 @@
 package template;
 
+import gen.template.TemplateJavaWrapper;
+
 /**
- * 
  * This class shows how to execute OpenL Tablets methods using Java wrapper.
  * Looks really simple...
- *
  */
 public class Main {
-	
-	public static void main(String[] args) {
-		TemplateJavaWrapper jw = new TemplateJavaWrapper();
-		jw.hello1(10);
-	}
+
+    public static void main(String[] args) {
+        TemplateJavaWrapper jw = new TemplateJavaWrapper();
+        jw.hello1(10);
+    }
 }
