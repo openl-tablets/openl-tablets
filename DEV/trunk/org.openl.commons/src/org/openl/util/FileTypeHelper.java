69f337f57344bb2afbe2c15c669ad958396f9f27
aastrouski@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <aastrouski@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2010-07-08 08:54:44 +0000

    EPBDS Update Excel file extensions

--- DEV/trunk/org.openl.commons/src/org/openl/util/FileTypeHelper.java
+++ DEV/trunk/org.openl.commons/src/org/openl/util/FileTypeHelper.java
@@ -6,12 +6,14 @@ public class FileTypeHelper {
 
     public static boolean isExcelFile(String fileName) {
         return StringUtils.endsWithIgnoreCase(fileName, ".xls")
-            || StringUtils.endsWithIgnoreCase(fileName, ".xlsx");
+            || StringUtils.endsWithIgnoreCase(fileName, ".xlsx")
+            || StringUtils.endsWithIgnoreCase(fileName, ".xlsm");
     }
 
     public static boolean isWordFile(String fileName) {
         return StringUtils.endsWithIgnoreCase(fileName, ".doc")
-        || StringUtils.endsWithIgnoreCase(fileName, ".docx");
+            || StringUtils.endsWithIgnoreCase(fileName, ".docx")
+            || StringUtils.endsWithIgnoreCase(fileName, ".docm");
     }
 
     public static boolean isZipFile(String fileName) {
