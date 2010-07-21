c09e956fd07940a4362a93d6b8be6b8924416aa2
pudalau <pudalau@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2010-07-21 14:51:38 +0000

    EPBDS-345.[maven-release-plugin] copy for tag release-5.7.1

---
+++ TUT/tags/release-5.7.1/org.openl.rules.tutorials/org.openl.rules.examples.insurance/gen/org/openl/generated/beans/VehicleSymbol.java
@@ -0,0 +1,29 @@
+/*
+ * This class has been generated. Do not change it. 
+*/
+
+package org.openl.generated.beans;
+
+import java.lang.String;
+import org.openl.generated.beans.InsurableVehicle;
+
+public class VehicleSymbol{
+  private org.openl.generated.beans.InsurableVehicle vehicle;
+
+  private java.lang.String symbol;
+
+
+  public org.openl.generated.beans.InsurableVehicle getVehicle() {
+   return vehicle;
+}
+  public void setVehicle(org.openl.generated.beans.InsurableVehicle vehicle) {
+   this.vehicle = vehicle;
+}
+  public void setSymbol(java.lang.String symbol) {
+   this.symbol = symbol;
+}
+  public java.lang.String getSymbol() {
+   return symbol;
+}
+
+}
