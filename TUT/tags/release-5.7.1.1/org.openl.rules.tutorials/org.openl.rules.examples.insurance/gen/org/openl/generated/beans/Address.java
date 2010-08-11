39249d25b3355831d64a6991bb26c65aa2b86d24
pudalau@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <pudalau@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2010-08-11 12:11:15 +0000

    EPBDS-345.[maven-release-plugin] copy for tag release-5.7.1.1

---
+++ TUT/tags/release-5.7.1.1/org.openl.rules.tutorials/org.openl.rules.examples.insurance/gen/org/openl/generated/beans/Address.java
@@ -0,0 +1,52 @@
+/*
+ * This class has been generated. Do not change it. 
+*/
+
+package org.openl.generated.beans;
+
+import java.lang.String;
+
+public class Address{
+  private java.lang.String street2;
+
+  private java.lang.String street1;
+
+  private java.lang.String zip;
+
+  private java.lang.String city;
+
+  private java.lang.String state;
+
+
+  public java.lang.String getStreet2() {
+   return street2;
+}
+  public void setStreet2(java.lang.String street2) {
+   this.street2 = street2;
+}
+  public java.lang.String getStreet1() {
+   return street1;
+}
+  public void setStreet1(java.lang.String street1) {
+   this.street1 = street1;
+}
+  public java.lang.String getZip() {
+   return zip;
+}
+  public void setZip(java.lang.String zip) {
+   this.zip = zip;
+}
+  public java.lang.String getCity() {
+   return city;
+}
+  public void setCity(java.lang.String city) {
+   this.city = city;
+}
+  public java.lang.String getState() {
+   return state;
+}
+  public void setState(java.lang.String state) {
+   this.state = state;
+}
+
+}
---
+++ TUT/tags/release-5.7.1.1/org.openl.rules.tutorials/org.openl.rules.examples.insurance/gen/org/openl/generated/beans/CoverageBase.java
@@ -0,0 +1,44 @@
+/*
+ * This class has been generated. Do not change it. 
+*/
+
+package org.openl.generated.beans;
+
+import java.lang.String;
+
+public class CoverageBase{
+  private double baseMP;
+
+  private double baseBI;
+
+  private double basePD;
+
+  private java.lang.String symbol;
+
+
+  public void setSymbol(java.lang.String symbol) {
+   this.symbol = symbol;
+}
+  public double getBaseMP() {
+   return baseMP;
+}
+  public void setBaseMP(double baseMP) {
+   this.baseMP = baseMP;
+}
+  public double getBaseBI() {
+   return baseBI;
+}
+  public void setBaseBI(double baseBI) {
+   this.baseBI = baseBI;
+}
+  public double getBasePD() {
+   return basePD;
+}
+  public void setBasePD(double basePD) {
+   this.basePD = basePD;
+}
+  public java.lang.String getSymbol() {
+   return symbol;
+}
+
+}
