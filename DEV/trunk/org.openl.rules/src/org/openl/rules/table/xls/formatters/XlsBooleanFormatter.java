50d4bd51301646b443dc0088ebf6ff4de4750af7
dliauchuk@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <dliauchuk@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2010-08-11 14:38:25 +0000

    EPBDS-1521 changed that all convertions from String to Boolean goes through BooleanUtils. update BooleanUtils to work with additional for Openl boolean values.

--- DEV/trunk/org.openl.rules/src/org/openl/rules/table/xls/formatters/XlsBooleanFormatter.java
+++ DEV/trunk/org.openl.rules/src/org/openl/rules/table/xls/formatters/XlsBooleanFormatter.java
@@ -1,5 +1,6 @@
 package org.openl.rules.table.xls.formatters;
 
+import org.openl.util.BooleanUtils;
 import org.openl.util.Log;
 
 public class XlsBooleanFormatter extends AXlsFormatter{
@@ -14,11 +15,12 @@ public class XlsBooleanFormatter extends AXlsFormatter{
         return fBoolean;        
     }
 
-    public Object parse(String value) {        
-        try{
-            return new Boolean(value);
-        }catch (Exception e) {
-            Log.warn("Could not parse Boolean: " + value, e);
+    public Object parse(String value) {
+        Boolean boolValue = BooleanUtils.toBooleanObject(value);
+        if (boolValue != null) {
+            return boolValue;
+        } else {
+            Log.warn("Could not parse Boolean: " + value);
             return value;
         }
     }
