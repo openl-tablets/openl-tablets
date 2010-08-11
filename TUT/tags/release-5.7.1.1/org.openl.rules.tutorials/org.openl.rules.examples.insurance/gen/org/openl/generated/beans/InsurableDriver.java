8794784133feee05b473d3eceef7fdb2f4fe8924
pudalau <pudalau@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2010-08-11 12:11:15 +0000

    EPBDS-345.[maven-release-plugin] copy for tag release-5.7.1.1

---
+++ TUT/tags/release-5.7.1.1/org.openl.rules.tutorials/org.openl.rules.examples.insurance/gen/org/openl/generated/beans/InsurableDriver.java
@@ -0,0 +1,52 @@
+/*
+ * This class has been generated. Do not change it. 
+*/
+
+package org.openl.generated.beans;
+
+import java.lang.String;
+
+public class InsurableDriver{
+  private int dmvPoints;
+
+  private int age;
+
+  private java.lang.String gender;
+
+  private java.lang.String maritalStatus;
+
+  private java.lang.String name;
+
+
+  public int getDmvPoints() {
+   return dmvPoints;
+}
+  public void setDmvPoints(int dmvPoints) {
+   this.dmvPoints = dmvPoints;
+}
+  public int getAge() {
+   return age;
+}
+  public void setAge(int age) {
+   this.age = age;
+}
+  public java.lang.String getGender() {
+   return gender;
+}
+  public void setGender(java.lang.String gender) {
+   this.gender = gender;
+}
+  public java.lang.String getMaritalStatus() {
+   return maritalStatus;
+}
+  public void setMaritalStatus(java.lang.String maritalStatus) {
+   this.maritalStatus = maritalStatus;
+}
+  public java.lang.String getName() {
+   return name;
+}
+  public void setName(java.lang.String name) {
+   this.name = name;
+}
+
+}
