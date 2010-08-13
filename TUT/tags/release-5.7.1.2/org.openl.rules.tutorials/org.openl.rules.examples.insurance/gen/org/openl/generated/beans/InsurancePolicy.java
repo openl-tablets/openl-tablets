6b62734fc11578774e83244e94e4c173d29a7ce1
pudalau <pudalau@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2010-08-13 12:23:30 +0000

    EPBDS-345.[maven-release-plugin] copy for tag release-5.7.1.2

---
+++ TUT/tags/release-5.7.1.2/org.openl.rules.tutorials/org.openl.rules.examples.insurance/gen/org/openl/generated/beans/InsurancePolicy.java
@@ -0,0 +1,47 @@
+/*
+ * This class has been generated. Do not change it. 
+*/
+
+package org.openl.generated.beans;
+
+import org.openl.generated.beans.Address;
+import org.openl.generated.beans.Usage;
+import org.openl.generated.beans.InsurableDriver;
+import org.openl.generated.beans.InsurableVehicle;
+
+public class InsurancePolicy{
+  private org.openl.generated.beans.InsurableDriver[] drivers;
+
+  private org.openl.generated.beans.Usage[] usages;
+
+  private org.openl.generated.beans.InsurableVehicle[] vehicles;
+
+  private org.openl.generated.beans.Address address;
+
+
+  public org.openl.generated.beans.InsurableDriver[] getDrivers() {
+   return drivers;
+}
+  public void setDrivers(org.openl.generated.beans.InsurableDriver[] drivers) {
+   this.drivers = drivers;
+}
+  public org.openl.generated.beans.Usage[] getUsages() {
+   return usages;
+}
+  public void setUsages(org.openl.generated.beans.Usage[] usages) {
+   this.usages = usages;
+}
+  public org.openl.generated.beans.InsurableVehicle[] getVehicles() {
+   return vehicles;
+}
+  public void setVehicles(org.openl.generated.beans.InsurableVehicle[] vehicles) {
+   this.vehicles = vehicles;
+}
+  public org.openl.generated.beans.Address getAddress() {
+   return address;
+}
+  public void setAddress(org.openl.generated.beans.Address address) {
+   this.address = address;
+}
+
+}
