e95340fdefe637730208a709661459383d5004fd
agamanovich <agamanovich@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2010-08-17 10:19:52 +0000

    EPBDS: Added java comments to source and updated unit tests.

--- DEV/trunk/org.openl.core/src/org/openl/types/impl/MethodKey.java
+++ DEV/trunk/org.openl.core/src/org/openl/types/impl/MethodKey.java
@@ -12,77 +12,101 @@ import org.openl.types.java.JavaOpenClass;
 /**
  * 
  * Key for IOpenMethod.
- *
+ * 
  */
 public final class MethodKey {
-    private String name;
-    private IOpenClass[] pars;
-    private IOpenClass[] internalParameters;
+	private String name;
+	private IOpenClass[] pars;
+	private IOpenClass[] internalParameters;
 
-    public MethodKey(IOpenMethod om) {
-        this.name = om.getName();
-        this.pars = om.getSignature().getParameterTypes();
-        this.internalParameters = getNormalizedParams(pars);
-    }
+	public MethodKey(IOpenMethod om) {
+		this.name = om.getName();
+		this.pars = om.getSignature().getParameterTypes();
+		this.internalParameters = getNormalizedParams(pars);
+	}
 
-    public MethodKey(String name, IOpenClass[] pars) {
-        this.name = name;
-        this.pars = pars;
-        this.internalParameters = getNormalizedParams(pars);
-    }
-    
-    private IOpenClass[] getNormalizedParams(IOpenClass[] originalParams) {
-    	
-    	if (originalParams == null) {
-    		return null;
-    	}
-    	
-    	IOpenClass[] normalizedParams = new IOpenClass[originalParams.length];
-    	
-    	for (int i = 0; i < originalParams.length; i++) {
-    		IOpenClass param = originalParams[i]; 
-    		IOpenClass normParam = param;
-    		
-    		if (param instanceof DomainOpenClass) {
-    			normParam = JavaOpenClass.getOpenClass(param.getInstanceClass());
-    		}
-    		
-    		normalizedParams[i] = normParam;
-    	}
-    	
-    	return normalizedParams;
-    }
+	public MethodKey(String name, IOpenClass[] pars) {
+		this.name = name;
+		this.pars = pars;
+		this.internalParameters = getNormalizedParams(pars);
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        if (!(obj instanceof MethodKey)) {
-            return false;
-        }
-        MethodKey mk = (MethodKey) obj;
+	/**
+	 * Normalizes types of method parameters. OpenL engine uses alias data types
+	 * as internal types and they are used only in OpenL. For java users alias
+	 * data types are represented as appropriate java type. While method key
+	 * usage we should use underlying type of alias data type parameter as real
+	 * type of parameter.
+	 * 
+	 * @param originalParams
+	 *            parameters of method
+	 * @return normalized parameters
+	 */
+	private IOpenClass[] getNormalizedParams(IOpenClass[] originalParams) {
 
-        return new EqualsBuilder().append(name, mk.name).append(internalParameters, mk.internalParameters).isEquals();
-    }
+		if (originalParams == null) {
+			return null;
+		}
 
-    @Override
-    public int hashCode() {
-        int hashCode = new HashCodeBuilder().append(name).append(internalParameters).toHashCode();
-        return hashCode;
-    }
+		IOpenClass[] normalizedParams = new IOpenClass[originalParams.length];
 
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(name).append("(");
-        boolean first = true;
-        for (IOpenClass c : pars) {
-            if (!first) {
-                sb.append(",");
-            }
-            sb.append(c.getName());
-            first = false;
-        }
-        sb.append(")");
-        return sb.toString();
-    }
+		for (int i = 0; i < originalParams.length; i++) {
+			IOpenClass param = originalParams[i];
+			IOpenClass normParam = param;
+
+			if (param instanceof DomainOpenClass) {
+				normParam = JavaOpenClass.getOpenClass(param.getInstanceClass());
+			}
+
+			normalizedParams[i] = normParam;
+		}
+
+		return normalizedParams;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		
+		if (!(obj instanceof MethodKey)) {
+			return false;
+		}
+		
+		MethodKey mk = (MethodKey) obj;
+
+		return new EqualsBuilder()
+			.append(name, mk.name)
+			.append(internalParameters, mk.internalParameters)
+			.isEquals();
+	}
+
+	@Override
+	public int hashCode() {
+		int hashCode = new HashCodeBuilder()
+			.append(name)
+			.append(internalParameters)
+			.toHashCode();
+		
+		return hashCode;
+	}
+
+	@Override
+	public String toString() {
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append(name).append("(");
+	
+		boolean first = true;
+		
+		for (IOpenClass c : pars) {
+			if (!first) {
+				sb.append(",");
+			}
+			sb.append(c.getName());
+			first = false;
+		}
+		sb.append(")");
+	
+		return sb.toString();
+	}
 
 }
--- DEV/trunk/org.openl.rules/src/org/openl/rules/datatype/binding/AliasDatatypeBoundNode.java
+++ DEV/trunk/org.openl.rules/src/org/openl/rules/datatype/binding/AliasDatatypeBoundNode.java
@@ -9,23 +9,25 @@ import org.openl.rules.table.ILogicalTable;
 import org.openl.types.impl.DomainOpenClass;
 import org.openl.types.impl.InternalDatatypeClass;
 
+/**
+ * Class that represents bound node for alias data types of 'Datatype' table
+ * component.
+ */
 public class AliasDatatypeBoundNode implements IMemberBoundNode {
 
 	private TableSyntaxNode tableSyntaxNode;
 	private DomainOpenClass domainOpenClass;
-	private ILogicalTable table;
-	private OpenL openl;
 
-	public AliasDatatypeBoundNode(TableSyntaxNode tableSyntaxNode, DomainOpenClass domain, ILogicalTable table, OpenL openl) {
+	public AliasDatatypeBoundNode(TableSyntaxNode tableSyntaxNode,
+			DomainOpenClass domain, ILogicalTable table, OpenL openl) {
 		this.tableSyntaxNode = tableSyntaxNode;
 		this.domainOpenClass = domain;
-		this.table = table;
-		this.openl = openl;
 	}
 
 	public void addTo(ModuleOpenClass openClass) {
-		InternalDatatypeClass internalClassMember = new InternalDatatypeClass(domainOpenClass, openClass);
-        tableSyntaxNode.setMember(internalClassMember);
+		InternalDatatypeClass internalClassMember = new InternalDatatypeClass(
+				domainOpenClass, openClass);
+		tableSyntaxNode.setMember(internalClassMember);
 	}
 
 	public void finalizeBind(IBindingContext cxt) throws Exception {
