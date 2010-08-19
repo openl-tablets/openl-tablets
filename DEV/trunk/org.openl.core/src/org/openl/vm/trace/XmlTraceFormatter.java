ac6067885ee66664db1ae3c8102315d92e44819b
aastrouski <aastrouski@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2010-08-19 12:29:01 +0000

    EPBDS-1503 Trace output into file

---
+++ DEV/trunk/org.openl.core/src/org/openl/vm/trace/XmlTraceFormatter.java
@@ -0,0 +1,14 @@
+package org.openl.vm.trace;
+
+import org.apache.commons.lang.NotImplementedException;
+
+/**
+ * Xml Trace Formatter Stub
+ */
+public class XmlTraceFormatter implements TraceFormatter {
+
+    public String format(ITracerObject[] tracerObjects) {
+        throw new NotImplementedException();
+    }
+
+}
