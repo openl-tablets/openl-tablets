a2f8136ea5622128c71c91bcdf8d47c3668932d3
dliauchuk@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <dliauchuk@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2010-09-06 13:22:40 +0000

    EPBDS refactoring

--- DEV/trunk/org.openl.rules/src/org/openl/rules/lang/xls/main/IRulesLaunchConstants.java
+++ DEV/trunk/org.openl.rules/src/org/openl/rules/lang/xls/main/IRulesLaunchConstants.java
@@ -2,17 +2,17 @@ package org.openl.rules.lang.xls.main;
 
 public interface IRulesLaunchConstants {
 
-    static public final String START_PROJECT_PROPERTY_NAME = "org.openl.rules.start.project";
-    static public final String WEBSTUDIO_BROWSER_URL = "http://localhost:8080/webstudio/";
+    String START_PROJECT_PROPERTY_NAME = "org.openl.rules.start.project";
+    String WEBSTUDIO_BROWSER_URL = "http://localhost:8080/webstudio/";
     
-    static public final String WRAPPER_SEARCH_START_DIR_DEFAULT = "gen";
+    String WRAPPER_SEARCH_START_DIR_DEFAULT = "gen";
     //comma-separated list of search directories
-    static public final String WRAPPER_SEARCH_START_DIR_PROPERTY = "org.openl.rules.wrapper.dir";
+    String WRAPPER_SEARCH_START_DIR_PROPERTY = "org.openl.rules.wrapper.dir";
     
 
-    static public final String WRAPPER_SOURCE_SUFFIX_DEFAULT = "Wrapper.java";
+    String WRAPPER_SOURCE_SUFFIX_DEFAULT = "Wrapper.java";
     //comma-separated list of suffixes
-    static public final String WRAPPER_SOURCE_SUFFIX_PROPERTY = "org.openl.rules.wrapper.suffixes";
+    String WRAPPER_SOURCE_SUFFIX_PROPERTY = "org.openl.rules.wrapper.suffixes";
     
     
 }
