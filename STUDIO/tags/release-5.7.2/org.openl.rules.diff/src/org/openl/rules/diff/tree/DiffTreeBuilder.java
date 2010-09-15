4bc23b81f6fbfe6af2b02a4fd3e3b2d97bfd8f27
pudalau <pudalau@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2010-09-15 06:40:16 +0000

    EPBDS removed wrong tag

--- STUDIO/tags/release-5.7.2/org.openl.rules.diff/src/org/openl/rules/diff/tree/DiffTreeBuilder.java
+++
@@ -1,8 +0,0 @@
-package org.openl.rules.diff.tree;
-
-import org.openl.rules.diff.hierarchy.Projection;
-
-public interface DiffTreeBuilder {
-    DiffTreeNode compare(Projection p1, Projection p2);
-    DiffTreeNode compare(Projection[] projections);
-}
--- STUDIO/tags/release-5.7.2/org.openl.rules.workspace/src/org/openl/rules/workspace/abstracts/ProjectDependency.java
+++
@@ -1,8 +0,0 @@
-package org.openl.rules.workspace.abstracts;
-
-import java.io.Serializable;
-
-import org.openl.rules.repository.RDependency;
-
-public interface ProjectDependency extends Serializable, RDependency {
-}
