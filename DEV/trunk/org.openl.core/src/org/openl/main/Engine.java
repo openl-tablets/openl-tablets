da6b900f957a56be633042de4302e07f7bd82066
dliauchuk@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <dliauchuk@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2010-10-08 09:39:41 +0000

    EPBDS-1761 fix for org.openl.rules.examples.insurance

--- DEV/trunk/org.openl.core/src/org/openl/main/Engine.java
+++ DEV/trunk/org.openl.core/src/org/openl/main/Engine.java
@@ -6,7 +6,10 @@
 
 package org.openl.main;
 
+import java.lang.reflect.Array;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.openl.OpenL;
 import org.openl.conf.IUserContext;
@@ -16,6 +19,7 @@ import org.openl.source.impl.FileSourceCodeModule;
 import org.openl.source.impl.URLSourceCodeModule;
 import org.openl.types.IOpenClass;
 import org.openl.types.IOpenMethod;
+import org.openl.types.java.OpenClassHelper;
 import org.openl.util.Log;
 import org.openl.util.RuntimeExceptionWrapper;
 import org.openl.vm.IRuntimeEnv;
@@ -24,6 +28,7 @@ import org.openl.vm.IRuntimeEnv;
  * @author snshor
  *
  */
+@Deprecated
 public class Engine {
 
     protected IOpenMethod method;
@@ -40,18 +45,25 @@ public class Engine {
     }
 
     public Engine(String openlName, String fileName, String methodName) {
+        this(openlName, fileName, methodName, (Class<?>[])null);
+    }
+    
+    public Engine(String openlName, String fileName, String methodName, Class<?>[] paramTypes) {
         OpenL openl = OpenL.getInstance(openlName);
 
-        init(openl, fileName, methodName);
+        init(openl, fileName, methodName, paramTypes);
     }
 
-    public Engine(String openlName, String fileName, String methodName, IUserContext ucxt) {
-
+    public Engine(String openlName, String fileName, String methodName, Class<?>[] paramTypes, IUserContext ucxt) {
         OpenL openl = OpenL.getInstance(openlName, ucxt);
-        init(openl, fileName, methodName);
+        init(openl, fileName, methodName, null);
+    }
+    
+    public Engine(String openlName, String fileName, String methodName, IUserContext ucxt) {
+        this(openlName, fileName, methodName, null, ucxt);
     }
 
-    protected void init(OpenL openl, String fileName, String methodName) {
+    private void init(OpenL openl, String fileName, String methodName, Class<?>[] paramTypes) {
 
         try {
 
@@ -59,9 +71,11 @@ public class Engine {
                     fileName, null) : (IOpenSourceCodeModule) new URLSourceCodeModule(new URL(fileName));
 
             openClass = OpenLManager.compileModule(openl, src);
-
-            if (methodName != null) {
-                method = openClass.getMatchingMethod(methodName, null);
+            
+            if (methodName != null) {   
+                IOpenClass[] paramsArray = getOpenLParamsArray(paramTypes);
+                
+                method = openClass.getMatchingMethod(methodName, paramsArray);
                 if (method == null) {
                     throw new RuntimeException("Method " + methodName + " not found");
                 }
@@ -73,6 +87,24 @@ public class Engine {
             Log.error(ex);
             throw RuntimeExceptionWrapper.wrap(ex);
         }
+    }
+
+    private IOpenClass[] getOpenLParamsArray(Class<?>[] paramTypes) {
+        List<IOpenClass> paramOpenClasses = new ArrayList<IOpenClass>();
+        if (paramTypes != null) {
+            for (Class<?> paramType : paramTypes) {
+                paramOpenClasses.add(OpenClassHelper.getOpenClass(openClass, paramType));
+            }
+        }
+        IOpenClass[] resultArray = (IOpenClass[]) Array.newInstance(IOpenClass.class, paramOpenClasses.size());
+        IOpenClass[] paramsArray = null;
+        try {
+            paramsArray = paramOpenClasses.toArray(resultArray);
+        } catch (ArrayStoreException e) {
+            // Ignore exception. An exception occurs when element type is
+            // different for elements of array.
+        }
+        return paramsArray;
     }
 
     public Object run(Object[] objects) {
