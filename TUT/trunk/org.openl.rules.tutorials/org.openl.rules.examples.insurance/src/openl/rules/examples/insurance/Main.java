c4117f41e947ac78a66fc8208360d29095d34194
dliauchuk@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <dliauchuk@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2010-10-08 09:40:15 +0000

    EPBDS-1761 fix org.openl.rules.examples.insurance

--- TUT/trunk/org.openl.rules.tutorials/org.openl.rules.examples.insurance/src/openl/rules/examples/insurance/Main.java
+++ TUT/trunk/org.openl.rules.tutorials/org.openl.rules.examples.insurance/src/openl/rules/examples/insurance/Main.java
@@ -12,7 +12,8 @@ public class Main {
 	public static void main(String[] args) {
 		String fileName = "rules/Insurance.xls";
 		String methodName = "main";
-		Engine engine = new Engine("org.openl.xls", fileName, methodName);
+		Class<?>[] paramTypes = new Class<?>[] {String[].class};
+        Engine engine = new Engine("org.openl.xls", fileName, methodName, paramTypes);
 		System.out.println(
 			"\n============================================\n"
 				+ fileName
