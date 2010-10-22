3887bc201ffcb463d7eb61999612be2fe2c564a5
aastrouski <aastrouski@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2010-10-22 10:42:44 +0000

    EPBDS-1790 Fix reevaluating formulas after editing

--- DEV/trunk/org.openl.rules/src/org/openl/rules/table/xls/writers/XlsCellFormulaWriter.java
+++ DEV/trunk/org.openl.rules/src/org/openl/rules/table/xls/writers/XlsCellFormulaWriter.java
@@ -1,5 +1,6 @@
 package org.openl.rules.table.xls.writers;
 
+import org.apache.poi.ss.usermodel.FormulaEvaluator;
 import org.openl.rules.table.xls.XlsSheetGridModel;
 
 public class XlsCellFormulaWriter extends AXlsCellWriter {
@@ -11,6 +12,14 @@ public class XlsCellFormulaWriter extends AXlsCellWriter {
     @Override
     public void writeCellValue(boolean writeMetaInfo) {
         getCellToWrite().setCellFormula(getStringValue().replaceFirst("=", ""));
+
+        // Evaluate formula to get new cell value.
+        try {
+            FormulaEvaluator formulaEvaluator = getXlsSheetGridModel().getSheetSource().getSheet().getWorkbook()
+                .getCreationHelper().createFormulaEvaluator();
+            formulaEvaluator.evaluateFormulaCell(getCellToWrite());
+        } catch (Exception e) {
+        }
 
         if (writeMetaInfo) {
             setMetaInfo(String.class);
