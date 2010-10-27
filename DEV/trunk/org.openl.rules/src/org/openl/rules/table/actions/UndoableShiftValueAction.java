6d3c0055c44b73bf54dd5358b3cb503234c41cef
pudalau@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <pudalau@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2010-10-27 12:38:28 +0000

    EPBDS-1832 Fixed insert/remove row/column operations.

--- DEV/trunk/org.openl.rules/src/org/openl/rules/table/actions/UndoableShiftValueAction.java
+++ DEV/trunk/org.openl.rules/src/org/openl/rules/table/actions/UndoableShiftValueAction.java
@@ -1,8 +1,5 @@
 package org.openl.rules.table.actions;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.openl.rules.table.GridRegion;
 import org.openl.rules.table.IGridRegion;
 import org.openl.rules.table.IGridTable;
@@ -17,8 +14,8 @@ public class UndoableShiftValueAction extends AUndoableCellAction {
 
     private int colFrom, rowFrom;
 
-    private List<IGridRegion> toRestore = new ArrayList<IGridRegion>();
-    private List<IGridRegion> toRemove = new ArrayList<IGridRegion>();
+    private IGridRegion toRestore;
+    private IGridRegion toRemove;
 
     public UndoableShiftValueAction(int colFrom, int rowFrom, int colTo, int rowTo) {
         super(colTo, rowTo);
@@ -26,50 +23,32 @@ public class UndoableShiftValueAction extends AUndoableCellAction {
         this.rowFrom = rowFrom;
     }
 
+    // Save value from initial cell -> move region -> set value to destination
+    // cell
     public void doAction(IGridTable table) {
         IWritableGrid grid = (IWritableGrid) table.getGrid();
-
-        savePrevCell(grid);
-
-        grid.copyCell(colFrom, rowFrom, getCol(), getRow());
-        moveRegion(grid);
+        IGridRegion rrFrom = grid.getRegionStartingAt(colFrom, rowFrom);
+        Object value = grid.getCell(colFrom, rowFrom).getObjectValue();
+        if (rrFrom != null) {
+            toRestore = rrFrom;
+            grid.removeMergedRegion(rrFrom);
+            GridRegion copyFrom = new GridRegion(rrFrom.getTop() + getRow() - rowFrom, rrFrom.getLeft() + getCol()
+                    - colFrom, rrFrom.getBottom() + getRow() - rowFrom, rrFrom.getRight() + getCol() - colFrom);
+            grid.addMergedRegion(copyFrom);
+            toRemove = copyFrom;
+        }
+        grid.setCellValue(getCol(), getRow(), value);
     }
 
+    // Save value from destination cell -> move region back -> set value to
+    // initial cell
     public void undoAction(IGridTable table) {
         IWritableGrid grid = (IWritableGrid) table.getGrid();
-
-        for (IGridRegion region : toRemove) {
-            grid.removeMergedRegion(region);
+        Object value = grid.getCell(getCol(), getRow()).getObjectValue();
+        if (toRemove != null) {
+            grid.removeMergedRegion(toRemove);
+            grid.addMergedRegion(toRestore);
         }
-        for (IGridRegion region : toRestore) {
-            grid.addMergedRegion(region);
-        }
-
-        restorePrevCell(grid);
+        grid.setCellValue(colFrom, rowFrom, value);
     }
-
-    void moveRegion(IWritableGrid wgrid) {
-        toRemove.clear();
-        toRestore.clear();
-        IGridRegion rrFrom = wgrid.getRegionStartingAt(colFrom, rowFrom);
-        IGridRegion rrTo = wgrid.getRegionStartingAt(getCol(), getRow());
-
-        if (rrTo != null) {
-            GridRegion removedRegion = new GridRegion(rrTo);
-            toRestore.add(removedRegion);
-            wgrid.removeMergedRegion(removedRegion);
-        }
-
-        if (rrFrom != null) {
-            toRestore.add(rrFrom);
-            wgrid.removeMergedRegion(rrFrom);
-            GridRegion copyFrom = new GridRegion(rrFrom.getTop() + getRow() - rowFrom,
-                    rrFrom.getLeft() + getCol() - colFrom, rrFrom.getBottom() + getRow() - rowFrom,
-                    rrFrom.getRight() + getCol() - colFrom);
-            wgrid.addMergedRegion(copyFrom);
-            toRemove.add(copyFrom);
-        }
-
-    }
-
 }
