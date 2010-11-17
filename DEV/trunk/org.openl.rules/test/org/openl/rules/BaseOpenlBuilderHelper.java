a37ef6d341751b33016de17ee0c284db922e85b9
dliauchuk@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <dliauchuk@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2010-11-17 15:45:31 +0000

    EPBDS add util method for tests to invoke some method

--- DEV/trunk/org.openl.rules/test/org/openl/rules/BaseOpenlBuilderHelper.java
+++ DEV/trunk/org.openl.rules/test/org/openl/rules/BaseOpenlBuilderHelper.java
@@ -1,10 +1,14 @@
 package org.openl.rules;
 
+import junit.framework.Assert;
+
 import org.openl.conf.UserContext;
 import org.openl.impl.OpenClassJavaWrapper;
 import org.openl.rules.lang.xls.binding.XlsMetaInfo;
 import org.openl.rules.lang.xls.syntax.TableSyntaxNode;
 import org.openl.rules.lang.xls.syntax.XlsModuleSyntaxNode;
+import org.openl.types.IOpenClass;
+import org.openl.types.IOpenMethod;
 
 /**
  * Helper class for building OpenClassJavaWrapper and getting XlsModuleSyntaxNode from it. To get everything you need 
@@ -71,5 +75,21 @@ public abstract class BaseOpenlBuilderHelper {
     
     private void build(String fileToBuildWrapper) {
         buildXlsModuleSyntaxNode(fileToBuildWrapper);        
+    }
+    
+    protected Object invokeMethod(String methodName) {
+        IOpenClass __class = getJavaWrapper().getOpenClassWithErrors(); 
+        
+        IOpenMethod testMethod = __class.getMatchingMethod(methodName, new IOpenClass[] {});
+        
+        Assert.assertNotNull(String.format("Method with name %s exists", methodName), testMethod);
+        
+        Object[] __params = new Object[0];
+        org.openl.vm.IRuntimeEnv environment = new org.openl.vm.SimpleVM().getRuntimeEnv();
+        Object __myInstance = __class.newInstance(environment);
+        
+        Object result = testMethod.invoke(__myInstance, __params, environment);        
+        
+        return result;
     }
 }
