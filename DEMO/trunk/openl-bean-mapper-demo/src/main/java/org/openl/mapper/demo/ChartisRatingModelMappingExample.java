10dd5c0aca14db4de163527c61851f0da8647f7f
agamanovich <agamanovich@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2010-11-18 12:06:43 +0000

    EPBDS: updated mapper demo

--- DEMO/trunk/openl-bean-mapper-demo/src/main/java/org/openl/mapper/demo/ChartisRatingModelMappingExample.java
+++ DEMO/trunk/openl-bean-mapper-demo/src/main/java/org/openl/mapper/demo/ChartisRatingModelMappingExample.java
@@ -2,16 +2,17 @@ package org.openl.mapper.demo;
 
 import com.exigen.chartis.domain.rating.Policy;
 import com.exigen.ipb.base.datatypes.Term;
-import com.exigen.ipb.policy.domain.PolicyEntity;
+import com.exigen.ipb.policy.domain.*;
 import org.openl.mapper.RulesBeanMapper;
 import org.openl.rules.runtime.ApiBasedRulesEngineFactory;
-import org.apache.commons.lang.builder.ToStringBuilder;
-import org.apache.commons.lang.builder.ToStringStyle;
 
 import java.io.File;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.Date;
+import java.util.Arrays;
+import java.math.BigDecimal;
+import java.lang.reflect.Method;
 
 public class ChartisRatingModelMappingExample {
 
@@ -39,9 +40,32 @@ public class ChartisRatingModelMappingExample {
 
         policy.setContractTerm(term);
 
+        PolicyDetail detail = new PolicyDetail();
+
+        RiskItem riskItem1 = new RiskItem();
+        riskItem1.setSeqNo(1);
+        Coverage dwellCoverage = new Coverage();
+        dwellCoverage.setCoverageCd("DWELL");
+        dwellCoverage.setLimitAmount(new BigDecimal(10.5));
+        riskItem1.setCoverages(Arrays.asList(dwellCoverage));
+
+        Form form1 = new Form();
+        form1.setFormCd("PCHO-ESOS");
+
+        riskItem1.setForms(Arrays.asList(form1));
+
+        detail.setRiskItems(Arrays.asList(riskItem1));
+
+        policy.setPolicyDetail(detail);
+
         RulesBeanMapper mapper = new RulesBeanMapper(instanceClass, instance);
         Policy ratingDomainPolicy = mapper.map(policy, Policy.class);
 
-        System.out.println(ratingDomainPolicy);
+        System.out.println("CorrelationId=" + ratingDomainPolicy.getCorrelationId());
+        System.out.println("OriginalDate=" + ratingDomainPolicy.getOriginalDate());
+        System.out.println("EffectiveDate=" + ratingDomainPolicy.getEffectiveDate());
+        System.out.println("EffectiveYear=" + ratingDomainPolicy.getEffectiveYear());
+        System.out.println("InsuredAmount=" + ratingDomainPolicy.getInsuredAmount());
+        System.out.println("HasExclSpecEndorsement=" + ratingDomainPolicy.getHasExclSpecEndorsement());
     }
 }
