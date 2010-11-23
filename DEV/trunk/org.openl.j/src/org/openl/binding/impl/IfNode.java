14429c4a779d600bcc0402630157b066080bfcee
agamanovich <agamanovich@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2010-11-23 13:20:19 +0000

    EPBDS: updated condition evaluation: removed null value from legal results.

--- DEV/trunk/org.openl.j/src/org/openl/binding/impl/IfNode.java
+++ DEV/trunk/org.openl.j/src/org/openl/binding/impl/IfNode.java
@@ -18,10 +18,6 @@ import org.openl.vm.IRuntimeEnv;
  */
 public class IfNode extends ABoundNode {
 
-    /**
-     * @param syntaxNode
-     * @param children
-     */
     public IfNode(ISyntaxNode syntaxNode, IBoundNode[] children) {
         super(syntaxNode, children);
     }
@@ -32,12 +28,13 @@ public class IfNode extends ABoundNode {
      * @see org.openl.binding.IBoundNode#evaluate(org.openl.vm.IRuntimeEnv)
      */
     public Object evaluateRuntime(IRuntimeEnv env) throws OpenLRuntimeException {
+        
         Boolean res = (Boolean) children[0].evaluate(env);
 
         if (res.booleanValue()) {
-            children[1].evaluate(env);
+           children[1].evaluate(env);
         } else if (children.length > 2) {
-            children[2].evaluate(env);
+           children[2].evaluate(env);
         }
 
         return null;
@@ -46,8 +43,10 @@ public class IfNode extends ABoundNode {
     public IOpenClass getType() {
         // return NullOpenClass.the;
         // TODO use both branches
+
         return children[1].getType();
     }
+
     @Override
     public boolean isLiteralExpressionParent() {
         return true;
