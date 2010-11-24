361881aabd0613909d4b3d4ec549389b051cabed
aastrouski@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <aastrouski@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2010-11-24 15:33:07 +0000

    EPBDS-1968 Fix setting fill color for cell with no color

--- DEV/trunk/org.openl.rules/src/org/openl/rules/table/actions/style/SetFillColorAction.java
+++ DEV/trunk/org.openl.rules/src/org/openl/rules/table/actions/style/SetFillColorAction.java
@@ -3,6 +3,7 @@ package org.openl.rules.table.actions.style;
 import org.openl.rules.table.IGridTable;
 import org.openl.rules.table.IWritableGrid;
 import org.openl.rules.table.actions.AUndoableCellAction;
+import org.openl.rules.table.ui.ICellStyle;
 
 public class SetFillColorAction extends AUndoableCellAction {
 
@@ -17,7 +18,8 @@ public class SetFillColorAction extends AUndoableCellAction {
     public void doAction(IGridTable table) {
         IWritableGrid grid = (IWritableGrid) table.getGrid();
 
-        prevColor = grid.getCell(getCol(), getRow()).getStyle().getFillForegroundColor();
+        ICellStyle style = grid.getCell(getCol(), getRow()).getStyle();
+        prevColor = style != null ? style.getFillForegroundColor() : null;
 
         grid.setCellFillColor(getCol(), getRow(), newColor);
     }
