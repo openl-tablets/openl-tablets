ef433d70f8b45c2bcf9197a1d78c1f76998248ce
agamanovich <agamanovich@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2010-12-07 15:18:59 +0000

    EPBDS: updated tests

--- STUDIO/trunk/org.openl.rules.project/test/java/org/openl/rules/project/dependencies/ExternalDependenciesTest.java
+++ STUDIO/trunk/org.openl.rules.project/test/java/org/openl/rules/project/dependencies/ExternalDependenciesTest.java
@@ -5,13 +5,15 @@ import static junit.framework.Assert.assertEquals;
 import java.io.File;
 import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.Calendar;
 
 import org.junit.Test;
+import org.openl.dependency.loader.IDependencyLoader;
+import org.openl.meta.DoubleValue;
 import org.openl.meta.DoubleValueFormula;
 import org.openl.rules.calc.SpreadsheetResult;
 import org.openl.rules.context.DefaultRulesRuntimeContext;
 import org.openl.rules.context.IRulesRuntimeContext;
-import org.openl.rules.enumeration.CountriesEnum;
 import org.openl.rules.project.instantiation.ApiBasedEngineFactoryInstantiationStrategy;
 import org.openl.rules.project.instantiation.ReloadType;
 import org.openl.rules.project.instantiation.RulesServiceEnhancer;
@@ -49,10 +51,9 @@ public class ExternalDependenciesTest {
 
         RulesProjectDependencyManager dependencyManager = new RulesProjectDependencyManager();
         
-        RulesFileDependencyLoader loader1 = new RulesFileDependencyLoader();
-        RulesProjectDependencyLoader loader2 = new RulesProjectDependencyLoader("test/resources/dependencies/test2/module");
+        IDependencyLoader loader1 = new RulesProjectDependencyLoader("test/resources/dependencies/test2/module");
         
-        dependencyManager.setDependencyLoaders(Arrays.asList(loader1, loader2));
+        dependencyManager.setDependencyLoaders(Arrays.asList(loader1));
         ApiBasedEngineFactoryInstantiationStrategy s = new ApiBasedEngineFactoryInstantiationStrategy(descr.getModules().get(0), false, dependencyManager);
         
         Class<?> interfaceClass = s.getServiceClass();
@@ -122,4 +123,58 @@ public class ExternalDependenciesTest {
         assertEquals(Double.valueOf(4970),  Double.valueOf(((DoubleValueFormula)spreadsheetResult.getFieldValue("$Value$Premium")).getValue()));
     }
 
+    @Test
+    public void testDependencies4() throws Exception {
+        ResolvingStrategy strategy = new SimpleXlsResolvingStrategy();
+        ProjectDescriptor descr = strategy.resolveProject(new File("test/resources/dependencies/test4/module"));
+
+        RulesProjectDependencyManager dependencyManager = new RulesProjectDependencyManager();
+        
+        IDependencyLoader loader1 = new RulesProjectDependencyLoader("test/resources/dependencies/test4/module");
+        
+        dependencyManager.setDependencyLoaders(Arrays.asList(loader1));
+        ApiBasedEngineFactoryInstantiationStrategy s = new ApiBasedEngineFactoryInstantiationStrategy(descr.getModules().get(0), false, dependencyManager);
+        
+        RulesServiceEnhancer enhancer = new RulesServiceEnhancer(s);
+
+        Class<?> interfaceClass = enhancer.getServiceClass();
+        Object instance = enhancer.instantiate(ReloadType.NO);
+
+        IRulesRuntimeContext context = new DefaultRulesRuntimeContext();
+        // Get policy profile.
+        //
+        Method method = interfaceClass.getMethod("getPolicyProfile4", new Class[]{ IRulesRuntimeContext.class });
+        Object res = method.invoke(instance, new Object[] { context });
+        Object policy = ((Object[])res)[0];
+
+        method = interfaceClass.getMethod("processPolicy", new Class[]{ IRulesRuntimeContext.class, policy.getClass()});
+        res = method.invoke(instance, new Object[] { context, policy });
+        
+        SpreadsheetResult spreadsheetResult = (SpreadsheetResult)res;
+        assertEquals("Eligible", spreadsheetResult.getFieldValue("$Value$Eligibility"));
+        assertEquals(Double.valueOf(-20), Double.valueOf(((DoubleValueFormula)spreadsheetResult.getFieldValue("$Value$Score")).getValue()));
+        assertEquals(Double.valueOf(2270),  Double.valueOf(((DoubleValueFormula)spreadsheetResult.getFieldValue("$Value$Premium")).getValue()));
+        
+        // Creating current date value
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2009, 5, 15);
+
+        // Setting current date in context, which will be used in dispatch
+        context.setCurrentDate(calendar.getTime());
+
+        method = interfaceClass.getMethod("getTestCars", new Class[]{ IRulesRuntimeContext.class });
+        res = method.invoke(instance, new Object[] { context });
+
+        Object car = ((Object[])res)[1];
+
+        method = interfaceClass.getMethod("getTestAddresses", new Class[]{ IRulesRuntimeContext.class });
+        res = method.invoke(instance, new Object[] { context });
+
+        Object address = ((Object[])res)[4];
+
+        method = interfaceClass.getMethod("getPriceForOrder", new Class[]{ IRulesRuntimeContext.class, car.getClass(), int.class, address.getClass() });
+        res = method.invoke(instance, new Object[] { context, car, 4, address });
+
+        assertEquals(Double.valueOf(189050), Double.valueOf (((DoubleValue) res).getValue()));
+    }
 }
