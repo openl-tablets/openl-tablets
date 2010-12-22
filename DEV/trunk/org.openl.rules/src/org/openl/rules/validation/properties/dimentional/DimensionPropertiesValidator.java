18125b9a0ccf37ef20bea8d354c01c138c97c6fb
dliauchuk@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <dliauchuk@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2010-12-22 12:42:28 +0000

    EPBDS-2056 validation message severity changed to Warning

--- DEV/trunk/org.openl.rules/src/org/openl/rules/validation/properties/dimentional/DimensionPropertiesValidator.java
+++ DEV/trunk/org.openl.rules/src/org/openl/rules/validation/properties/dimentional/DimensionPropertiesValidator.java
@@ -3,9 +3,9 @@ package org.openl.rules.validation.properties.dimentional;
 import java.util.Map;
 
 import org.openl.OpenL;
-import org.openl.exception.OpenLRuntimeException;
-import org.openl.message.OpenLErrorMessage;
+import org.openl.exception.OpenlNotCheckedException;
 import org.openl.message.OpenLMessage;
+import org.openl.message.OpenLWarnMessage;
 import org.openl.rules.dt.DecisionTable;
 import org.openl.rules.dt.type.domains.DimensionPropertiesDomainsCollector;
 import org.openl.rules.dt.type.domains.IDomainAdaptor;
@@ -13,13 +13,11 @@ import org.openl.rules.dt.validator.DesionTableValidationResult;
 import org.openl.rules.dt.validator.DecisionTableValidator;
 import org.openl.rules.lang.xls.syntax.TableSyntaxNode;
 import org.openl.rules.validation.TablesValidator;
-import org.openl.syntax.exception.SyntaxNodeException;
 import org.openl.types.IOpenClass;
 
 import org.openl.validation.ValidationResult;
 import org.openl.validation.ValidationStatus;
 import org.openl.validation.ValidationUtils;
-
 
 public class DimensionPropertiesValidator extends TablesValidator {
     
@@ -33,12 +31,12 @@ public class DimensionPropertiesValidator extends TablesValidator {
         
         for (TableSyntaxNode tsn : tableSyntaxNodes) {
             if (isDimensionPropertiesDispatcherTable(tsn)) {
-                OpenLMessage tableValidationMessage = validateDecisionTable(tsn, propertiesDomains, openClass);
-                if (tableValidationMessage != null) {
+                OpenLMessage validationMessage = validateDecisionTable(tsn, propertiesDomains, openClass);
+                if (validationMessage != null) {
                     if (validationResult == null) {
                         validationResult = new ValidationResult(ValidationStatus.FAIL); 
                     } 
-                    ValidationUtils.addValidationMessage(validationResult, tableValidationMessage);
+                    ValidationUtils.addValidationMessage(validationResult, validationMessage);
                 }
             }                        
         }
@@ -48,28 +46,56 @@ public class DimensionPropertiesValidator extends TablesValidator {
         return ValidationUtils.validationSuccess();
     }
     
+    /**
+     * Validate table and create message, explaining validation result.
+     * 
+     * @param tsn generated DT with dimension properties.
+     * @param propertiesDomains domains for dimension properties.
+     * @param openClass Open class for whole module.
+     * @return message, explaining validation result
+     */
     private OpenLMessage validateDecisionTable(TableSyntaxNode tsn, Map<String, IDomainAdaptor> propertiesDomains, 
             IOpenClass openClass) {
-        OpenLMessage validationMessage = null;
         
+        DesionTableValidationResult tableValidationResult = validate(tsn, propertiesDomains, openClass);
+        
+        return createMessage(tsn, tableValidationResult);        
+    }
+    
+    /**
+     * Validate decision table. Throws {@link OpenlNotCheckedException} if there were any errors 
+     * during validation.
+     * 
+     * @param tsn generated DT with dimension properties.
+     * @param propertiesDomains domains for dimension properties.
+     * @param openClass Open class for whole module.
+     * @return {@link DesionTableValidationResult}
+     */
+    private DesionTableValidationResult validate(TableSyntaxNode tsn,  Map<String, IDomainAdaptor> propertiesDomains,
+            IOpenClass openClass) {
         DesionTableValidationResult tableValidationResult = null;
         try {
             tableValidationResult = DecisionTableValidator.validateTable((DecisionTable)tsn.getMember(), 
                     propertiesDomains, openClass);     
         } catch (Exception t) {
-            throw new OpenLRuntimeException(VALIDATION_FAILED, t);
+            throw new OpenlNotCheckedException(VALIDATION_FAILED, t);
         }
-        
+        return tableValidationResult;
+    }
+    
+    
+    private OpenLMessage createMessage(TableSyntaxNode tsn, DesionTableValidationResult tableValidationResult) {
+        OpenLMessage validationMessage = null;
         if (tableValidationResult != null && tableValidationResult.hasProblems()) {
             tsn.setValidationResult(tableValidationResult);
-            SyntaxNodeException error = new SyntaxNodeException(tableValidationResult.toString(), null, tsn);
-            tsn.addError(error);
-            
-            validationMessage = new OpenLErrorMessage(error);
+            // changed validation message severity to WARNING
+            //
+//            SyntaxNodeException error = new SyntaxNodeException(tableValidationResult.toString(), null, tsn);
+//            tsn.addError(error);            
+//            validationMessage = new OpenLErrorMessage(error);
+            validationMessage = new OpenLWarnMessage(tableValidationResult.toString(), tsn);
         }
-        
         return validationMessage;
-        
     }
 
     private boolean isDimensionPropertiesDispatcherTable(TableSyntaxNode tsn) {
