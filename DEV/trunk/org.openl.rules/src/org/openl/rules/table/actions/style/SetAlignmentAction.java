f25421858a440c84f09c68d09aac1925227d8220
aastrouski <aastrouski@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2010-12-24 13:33:38 +0000

    EPBDS-1974 Fix setting cell styles

--- DEV/trunk/org.openl.rules/src/org/openl/rules/table/actions/style/SetAlignmentAction.java
+++ DEV/trunk/org.openl.rules/src/org/openl/rules/table/actions/style/SetAlignmentAction.java
@@ -3,6 +3,7 @@ package org.openl.rules.table.actions.style;
 import org.openl.rules.table.IGridTable;
 import org.openl.rules.table.IWritableGrid;
 import org.openl.rules.table.actions.AUndoableCellAction;
+import org.openl.rules.table.ui.ICellStyle;
 
 public class SetAlignmentAction extends AUndoableCellAction {
 
@@ -17,7 +18,8 @@ public class SetAlignmentAction extends AUndoableCellAction {
     public void doAction(IGridTable table) {
         IWritableGrid grid = (IWritableGrid) table.getGrid();
 
-        prevAlignment = grid.getCell(getCol(), getRow()).getStyle().getHorizontalAlignment();
+        ICellStyle style = grid.getCell(getCol(), getRow()).getStyle();
+        prevAlignment = style != null ? style.getHorizontalAlignment() : ICellStyle.ALIGN_GENERAL;
 
         grid.setCellAlignment(getCol(), getRow(), newAlignment);
     }
