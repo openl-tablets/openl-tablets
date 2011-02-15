e5663c7edd56bd6878e4c238d3fa063db9ecee41
agamanovich@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <agamanovich@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2011-02-15 08:59:54 +0000

    EPBDS: Removed project. Reason: current mapping demo project doesn't demonstrate mapping features. For more information about mapper and his features see the MAPPING directory.

--- DEMO/trunk/openl-bean-mapper-demo/src/main/java/org/openl/mapper/demo/XmlBeanFactory.java
+++
@@ -1,7 +0,0 @@
-package org.openl.mapper.demo;
-
-public class XmlBeanFactory {
-    public static Object newInstance(Class<?> clazz) {
-         return new org.dozer.factory.XMLBeanFactory().createBean(null, clazz, clazz.getName());
-    }
-}
