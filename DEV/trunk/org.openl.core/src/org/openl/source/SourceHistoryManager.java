4848fcc41c3bfd5e0d46a83c86a5e47861e2ddb0
aastrouski <aastrouski@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2011-03-17 15:11:57 +0000

    EPBDS-2306 Fix Module changes comparing

--- DEV/trunk/org.openl.core/src/org/openl/source/SourceHistoryManager.java
+++ DEV/trunk/org.openl.core/src/org/openl/source/SourceHistoryManager.java
@@ -1,9 +1,11 @@
 package org.openl.source;
 
-import java.util.Map;
+import java.util.SortedMap;
 
 /**
  * @author Andrei Astrouski
+ * 
+ * TODO Refactor - add Source object to encapsulate source data 
  */
 public interface SourceHistoryManager<T> {
 
@@ -13,8 +15,10 @@ public interface SourceHistoryManager<T> {
 
     T get(long version);
 
-    Map<Long, T> get(String[] names);
+    SortedMap<Long, T> get(long... versions);
 
-    Map<Long, T> getAll();
+    SortedMap<Long, T> get(String... names);
+
+    SortedMap<Long, T> getAll();
 
 }
