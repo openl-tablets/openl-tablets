4868b494f7577426fa026ec1d201ec8d78a82a14
pudalau <pudalau@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2011-04-26 08:41:24 +0000

    EPBDS log of failed setting metainfo.

--- DEV/trunk/org.openl.core/src/org/openl/meta/StringValue.java
+++ DEV/trunk/org.openl.core/src/org/openl/meta/StringValue.java
@@ -2,11 +2,14 @@ package org.openl.meta;
 
 import java.net.URL;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.openl.source.IOpenSourceCodeModule;
 import org.openl.source.impl.StringSourceCodeModule;
 import org.openl.source.impl.URLSourceCodeModule;
 
 public class StringValue implements IMetaHolder, CharSequence, Comparable<StringValue> {
+    private static final Log LOG = LogFactory.getLog(StringValue.class);
     private ValueMetaInfo metaInfo;
     private String value;
 
@@ -83,6 +86,7 @@ public class StringValue implements IMetaHolder, CharSequence, Comparable<String
                         metaInfo.getDisplayName(IMetaInfo.LONG), new URLSourceCodeModule(new URL(metaInfo.getSourceUrl())));
                 setMetaInfo(valueMetaInfo);
             } catch (Exception e) {
+                LOG.debug(String.format("Failed to set meta info for StringValue \"%s\"", value), e);
                 setMetaInfo((ValueMetaInfo)null);
             }
         }
