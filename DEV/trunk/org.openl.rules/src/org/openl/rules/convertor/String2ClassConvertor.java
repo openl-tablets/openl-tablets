68fcd0dfa58eac396d1254136594ed57bfb5d50f
agamanovich <agamanovich@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2011-05-14 15:37:12 +0000

    EPBDS: updated string to Class object conversion process.

--- DEV/trunk/org.openl.rules/src/org/openl/rules/convertor/String2ClassConvertor.java
+++ DEV/trunk/org.openl.rules/src/org/openl/rules/convertor/String2ClassConvertor.java
@@ -1,5 +1,7 @@
 package org.openl.rules.convertor;
 
+import java.lang.reflect.Array;
+
 import org.openl.binding.IBindingContext;
 import org.openl.syntax.impl.ISyntaxConstants;
 import org.openl.types.IOpenClass;
@@ -11,10 +13,23 @@ public class String2ClassConvertor implements IString2DataConvertor {
     }
 
     public Object parse(String data, String format, IBindingContext cxt) {
-        IOpenClass c = cxt.findType(ISyntaxConstants.THIS_NAMESPACE, data);
+
+        String typeName;
+        if (data.endsWith("[]")) {
+            typeName = data.substring(0, data.length() - 2);
+        } else {
+            typeName = data;
+        }
+        IOpenClass c = cxt.findType(ISyntaxConstants.THIS_NAMESPACE, typeName);
 
         if (c == null) {
             throw new RuntimeException("Type " + data + " is not found");
+        }
+        
+        if(data.endsWith("[]")) {
+            Class<?> elementType = c.getInstanceClass();
+            Object array = Array.newInstance(elementType, 0);
+            return array.getClass();
         }
 
         return c.getInstanceClass();
