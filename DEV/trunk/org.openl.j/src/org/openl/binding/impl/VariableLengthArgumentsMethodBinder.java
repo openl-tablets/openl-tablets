74dbf70ba3060c263e07f34ef33724e198d2e5c2
dliauchuk <dliauchuk@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2011-05-31 09:37:07 +0000

    EPBDS-2529 fix sonar violations

--- DEV/trunk/org.openl.j/src/org/openl/binding/impl/VariableLengthArgumentsMethodBinder.java
+++ DEV/trunk/org.openl.j/src/org/openl/binding/impl/VariableLengthArgumentsMethodBinder.java
@@ -29,8 +29,8 @@ public class VariableLengthArgumentsMethodBinder extends ANodeBinder {
     public VariableLengthArgumentsMethodBinder(String methodName, IOpenClass[] argumentsTypes, IBoundNode[] children) {
         this.methodName = methodName;    
         if (argumentsTypes == null || argumentsTypes.length < 1) {
-            String message = String.format("At least one argument should exist in method signature(%s) to bind it as variable arguments method", 
-                methodName);
+            String message = String.format("At least one argument should exist in method signature(%s) " +
+            		"to bind it as variable arguments method", methodName);
             throw new OpenlNotCheckedException(message);
         }
         this.argumentsTypes = argumentsTypes.clone();
@@ -106,8 +106,8 @@ public class VariableLengthArgumentsMethodBinder extends ANodeBinder {
         return firstVarArgIndex;
     }
    
-    private IBoundNode getVarArgsMethodNode(ISyntaxNode node, IBindingContext bindingContext, IOpenClass[] methodArguments,
-            int indexOfLastEqualArgumentType) {
+    private IBoundNode getVarArgsMethodNode(ISyntaxNode node, IBindingContext bindingContext, 
+            IOpenClass[] methodArguments, int indexOfLastEqualArgumentType) {
         IMethodCaller methodWithLastArrayArgument = bindingContext.findMethodCaller(ISyntaxConstants.THIS_NAMESPACE, 
             methodName, methodArguments);
         
@@ -118,7 +118,8 @@ public class VariableLengthArgumentsMethodBinder extends ANodeBinder {
         }
         // get the component class of var arg expression
         //
-        Class<?> componentVarArgClass = methodArguments[methodArguments.length - 1].getInstanceClass().getComponentType();
+        Class<?> componentVarArgClass = 
+            methodArguments[methodArguments.length - 1].getInstanceClass().getComponentType();
         
         
         return new VariableArgumentsMethodBoundNode(node, children, methodWithLastArrayArgument, 
