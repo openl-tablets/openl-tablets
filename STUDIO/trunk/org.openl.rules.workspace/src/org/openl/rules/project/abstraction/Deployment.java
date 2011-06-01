ccdb6345fc9fd67833e2848e48f80121396d6840
mkamalov@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <mkamalov@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2011-06-01 15:28:35 +0000

    EPBDS-2533

--- STUDIO/trunk/org.openl.rules.workspace/src/org/openl/rules/project/abstraction/Deployment.java
+++ STUDIO/trunk/org.openl.rules.workspace/src/org/openl/rules/project/abstraction/Deployment.java
@@ -4,53 +4,73 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.openl.rules.common.CommonVersion;
 import org.openl.rules.common.ProjectException;
 import org.openl.rules.repository.api.ArtefactAPI;
 import org.openl.rules.repository.api.FolderAPI;
 import org.openl.rules.repository.api.ResourceAPI;
 
 /**
- * Class representing deployment from ProductionRepository.
- * Deployment is set of logically grouped rules projects. 
+ * Class representing deployment from ProductionRepository. Deployment is set of
+ * logically grouped rules projects.
  * 
  * @author PUdalau
  */
 public class Deployment extends AProject {
-    private Map<String, AProject> projects;
+	private Map<String, AProject> projects;
 
-    public Deployment(FolderAPI api) {
-        super(api);
-        init();
-    }
+	private String deploymentName;
+	private CommonVersion commonVersion;
 
-    protected void init() {
-        refresh();
-        projects = new HashMap<String, AProject>();
-        Map<String, AProjectArtefact> artefacts = new HashMap<String, AProjectArtefact>();
-        for (ArtefactAPI artefactAPI : getAPI().getArtefacts()) {
-            if (artefactAPI.isFolder()) {
-                AProject project = new AProject((FolderAPI) artefactAPI);
-                projects.put(artefactAPI.getName(), project);
-                artefacts.put(artefactAPI.getName(), project);
-            } else {
-                artefacts.put(artefactAPI.getName(), new AProjectResource((ResourceAPI) artefactAPI, getProject()));
-            }
-        }
-        setArtefactsInternal(artefacts);
-    }
+	public Deployment(FolderAPI api) {
+		super(api);
+		init();
+	}
 
-    public Collection<AProject> getProjects() {
-        return projects.values();
-    }
+	public Deployment(FolderAPI api, String deploymentName, CommonVersion commonVersion) {
+		super(api);
+		init();
+		this.commonVersion = commonVersion;
+		this.deploymentName = deploymentName;
+	}
 
-    public AProject getProject(String name) {
-        return projects.get(name);
-    }
+	public CommonVersion getCommonVersion() {
+		return commonVersion;
+	}
 
-    public void addProject(AProject project) throws ProjectException {
-        String projectName = project.getName();
-        addFolder(projectName);
-        init();
-        getProject(projectName).update(project, null, 0, 0);
-    }
+	public String getDeploymentName() {
+		return deploymentName;
+	}
+
+	protected void init() {
+		refresh();
+		projects = new HashMap<String, AProject>();
+		Map<String, AProjectArtefact> artefacts = new HashMap<String, AProjectArtefact>();
+		for (ArtefactAPI artefactAPI : getAPI().getArtefacts()) {
+			if (artefactAPI.isFolder()) {
+				AProject project = new AProject((FolderAPI) artefactAPI);
+				projects.put(artefactAPI.getName(), project);
+				artefacts.put(artefactAPI.getName(), project);
+			} else {
+				artefacts.put(artefactAPI.getName(), new AProjectResource(
+						(ResourceAPI) artefactAPI, getProject()));
+			}
+		}
+		setArtefactsInternal(artefacts);
+	}
+
+	public Collection<AProject> getProjects() {
+		return projects.values();
+	}
+
+	public AProject getProject(String name) {
+		return projects.get(name);
+	}
+
+	public void addProject(AProject project) throws ProjectException {
+		String projectName = project.getName();
+		addFolder(projectName);
+		init();
+		getProject(projectName).update(project, null, 0, 0);
+	}
 }
