08b78df2277d5239998167d891ba1401f35fe89e
dliauchuk <dliauchuk@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2011-06-01 13:59:08 +0000

    EPBDS-2553 add logging for openl project resolving strategies

--- STUDIO/trunk/org.openl.rules.project/src/org/openl/rules/project/resolving/EclipseBasedResolvingStrategy.java
+++ STUDIO/trunk/org.openl.rules.project/src/org/openl/rules/project/resolving/EclipseBasedResolvingStrategy.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.openl.main.OpenLProjectPropertiesLoader;
 import org.openl.rules.lang.xls.main.IRulesLaunchConstants;
 import org.openl.rules.project.model.Module;
@@ -26,6 +28,11 @@ import org.openl.util.tree.FileTreeIterator.FileTreeAdaptor;
  * @author PUdalau
  */
 public class EclipseBasedResolvingStrategy implements ResolvingStrategy {
+    
+    private static final String PROJECT_FILE = ".project";
+    private static final String TEXT_TO_SEARCH = "openlbuilder";
+
+    private static final Log LOG = LogFactory.getLog(EclipseBasedResolvingStrategy.class);
 
     /**
      * {@link FileTreeAdaptor} that have to be used for file search inside the
@@ -47,22 +54,37 @@ public class EclipseBasedResolvingStrategy implements ResolvingStrategy {
     public boolean isRulesProject(File folder) {
         try {
             if (!folder.exists() || !folder.isDirectory()) {
+                LOG.debug(String.format(
+                    "Eclipse based project strategy failed to resolve project folder: " +
+                    "folder %s doesn`t exists of is not a directory", 
+                    folder.getPath()));
                 return false;
             }
-            if (!FileTool.containsFile(folder, ".project", false)) {
+            if (!FileTool.containsFile(folder, PROJECT_FILE, false)) {
+                LOG.debug(String.format(
+                    "Eclipse based project strategy failed to resolve project folder %s: " +
+                    "there is no file %s in the folder", folder.getPath(), PROJECT_FILE));
                 return false;
             }
-            if (!FileTool.containsFileText(folder, ".project", "openlbuilder")) {
+            if (!FileTool.containsFileText(folder, PROJECT_FILE, TEXT_TO_SEARCH)) {
+                LOG.debug(String.format(
+                    "Eclipse based project strategy failed to resolve project folder %s:" +
+                    " %s file doen`t contains %s", folder.getPath(), PROJECT_FILE, TEXT_TO_SEARCH));
                 return false;
             }
             if(listPotentialOpenLWrappersClassNames(folder).length == 0){
+                LOG.debug(String.format(
+                    "Eclipse based project strategy failed to resolve project folder %s:" +
+                    " there is no potential Openl wrappers", folder.getPath()));
                 return false; //no modules.
             }
+            LOG.debug(String.format(
+                "Project in %s folder was resolved as Eclipse based project", folder.getPath()));
             return true;
         } catch (IOException e) {
             return false;
         }
-    }
+    }    
 
     public String[] listPotentialOpenLWrappersClassNames(File project) throws IOException {
 
