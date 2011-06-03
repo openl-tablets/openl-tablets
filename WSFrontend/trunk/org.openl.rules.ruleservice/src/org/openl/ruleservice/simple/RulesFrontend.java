bf41ec66e1960786a09253756e9817636d7fabca
pudalau@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <pudalau@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2011-06-03 10:07:56 +0000

    EPBDS-2542 Implemented JavaClassDeployment admin and simple demo Frontend

---
+++ WSFrontend/trunk/org.openl.rules.ruleservice/src/org/openl/ruleservice/simple/RulesFrontend.java
@@ -0,0 +1,52 @@
+package org.openl.ruleservice.simple;
+
+import org.openl.ruleservice.OpenLService;
+
+public interface RulesFrontend {
+
+    /**
+     * Executes method with specified parameters.
+     * 
+     * @param serviceNmae Name of deployed service
+     * @param ruleName Technical name of the rule to execute
+     * @param inputParamsTypes Types of method input parameters to discover
+     *            method
+     * @param params Parameters for method execution
+     * @return Result of execution
+     */
+    Object execute(String serviceName, String ruleName, Class<?>[] inputParamsTypes, Object[] params);
+
+    /**
+     * Executes method with specified parameters. Method discovery is done based
+     * on parameters types.
+     * 
+     * @param serviceNmae Name of deployed service
+     * @param ruleName Technical name of the rule to execute
+     * @param params Parameters for method execution
+     * @return Result of execution
+     */
+    Object execute(String serviceName, String ruleName, Object... params);
+
+    /**
+     * Gets values defined in rules.
+     * 
+     * @param serviceNmae Name of deployed service
+     * @param fieldName Technical name of the rule to execute
+     * @return Data stored in field
+     */
+    Object getValues(String serviceName, String fieldName);
+
+    /**
+     * Registers service to use it in calculations.
+     * 
+     * @param service Service to register.
+     */
+    void registerService(OpenLService service);
+
+    /**
+     * Unregisters service.
+     * 
+     * @param serviceName Name of the service to unregister.
+     */
+    void unregisterService(String serviceName);
+}
