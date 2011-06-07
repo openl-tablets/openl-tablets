52f61936b754753164b5e0e03ffae7373b0193fd
mkamalov@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <mkamalov@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2011-06-07 12:54:40 +0000

    EPBDS-2531

--- STUDIO/trunk/org.openl.rules.workspace/src/org/openl/rules/project/abstraction/Deployment.java
+++ STUDIO/trunk/org.openl.rules.workspace/src/org/openl/rules/project/abstraction/Deployment.java
@@ -8,7 +8,6 @@ import org.openl.rules.common.CommonVersion;
 import org.openl.rules.common.ProjectException;
 import org.openl.rules.repository.api.ArtefactAPI;
 import org.openl.rules.repository.api.FolderAPI;
-import org.openl.rules.repository.api.ResourceAPI;
 
 /**
  * Class representing deployment from ProductionRepository. Deployment is set of
@@ -43,22 +42,27 @@ public class Deployment extends AProject {
 		if (deploymentName == null) return this.getName();
 		return deploymentName;
 	}
+	
+	@Override
+	public void refresh() {
+		init();
+	}
 
 	protected void init() {
-		refresh();
+		super.refresh();
 		projects = new HashMap<String, AProject>();
-		Map<String, AProjectArtefact> artefacts = new HashMap<String, AProjectArtefact>();
+		//Map<String, AProjectArtefact> artefacts = new HashMap<String, AProjectArtefact>();
 		for (ArtefactAPI artefactAPI : getAPI().getArtefacts()) {
 			if (artefactAPI.isFolder()) {
 				AProject project = new AProject((FolderAPI) artefactAPI);
 				projects.put(artefactAPI.getName(), project);
-				artefacts.put(artefactAPI.getName(), project);
-			} else {
+				//artefacts.put(artefactAPI.getName(), project);
+			} /*else {
 				artefacts.put(artefactAPI.getName(), new AProjectResource(
 						(ResourceAPI) artefactAPI, getProject()));
-			}
+			}*/
 		}
-		setArtefactsInternal(artefacts);
+		//setArtefactsInternal(artefacts);
 	}
 
 	public Collection<AProject> getProjects() {
