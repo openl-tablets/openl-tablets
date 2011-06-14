979ae80f85cf662b5e68fc5133a88c23ac434a8f
mkamalov@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <mkamalov@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2011-06-14 15:00:27 +0000

    LocalTemporaryDeploymentsStorage and JcrDataSource complete tests were added.

--- WSFrontend/trunk/org.openl.rules.ruleservice/test/org/openl/ruleservice/loader/JcrDataSourceTest.java
+++ WSFrontend/trunk/org.openl.rules.ruleservice/test/org/openl/ruleservice/loader/JcrDataSourceTest.java
@@ -1,17 +1,42 @@
 package org.openl.ruleservice.loader;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;                                                                                                     
+
 
 import java.util.List;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.openl.rules.common.CommonVersion;
+import org.openl.rules.common.impl.CommonVersionImpl;
 import org.openl.rules.project.abstraction.Deployment;
 
 public class JcrDataSourceTest {
+    
+    private static IDataSource dataSource;
+    
+    @BeforeClass
+    public static void setDataSource(){
+        dataSource = new JcrDataSource();
+    }
+    
 	@Test
-	public void testGetDeloyments() {
-		IDataSource jcrDataSource = new JcrDataSource();
-		List<Deployment> deployments = jcrDataSource.getDeployments();
-		assertTrue(deployments.size() > 0);
+	public void testJcrDataSource() {
+		assertNotNull(dataSource);
 	}
+	
+	@Test
+    public void testGetDeployments() {
+        List<Deployment> deployments = dataSource.getDeployments();
+        assertTrue(deployments.size() > 0);
+    }
+
+	@Test
+    public void testGetDeployment() {
+	    CommonVersion commonVersion = new CommonVersionImpl("0.0.1");
+	    Deployment deployment = dataSource.getDeployment("org.openl.tablets.tutorial4", commonVersion);
+        assertNotNull(deployment);
+        assertEquals("org.openl.tablets.tutorial4", deployment.getDeploymentName());
+        assertEquals("0.0.1", deployment.getCommonVersion().getVersionName());
+    }
 }
