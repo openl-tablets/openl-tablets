851a75ea32e643d33d5be6eafd844ea15227e43a
aastrouski@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <aastrouski@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2011-06-15 14:21:23 +0000

    EPBDS-2563 WebStudio configuration UI

---
+++ STUDIO/trunk/org.openl.rules.webstudio.java/src/org/openl/rules/webstudio/web/admin/SystemSettingsBean.java
@@ -0,0 +1,60 @@
+package org.openl.rules.webstudio.web.admin;
+
+import org.openl.rules.webstudio.ConfigManager;
+import org.openl.rules.webstudio.web.util.WebStudioUtils;
+
+/**
+ * TODO Remove property getters/setters when migrating to JSF 2.0
+ * 
+ * @author Andrei Astrouski
+ */
+public class SystemSettingsBean {
+
+    private static final String WORKSPACES_ROOT = "webstudio.workspacesRoot";
+    private static final String DATE_PATTERN = "webstudio.datePattern";
+
+    private static final String AUTO_LOGIN = "security.autoLogin";
+    private static final String HIDE_LOGOUT = "webstudio.hideLogout";
+
+    private ConfigManager configManager = WebStudioUtils.getWebStudio().getSystemConfigManager();
+
+    public String getWorkspacesRoot() {
+        return configManager.getStringProperty(WORKSPACES_ROOT);
+    }
+
+    public void setWorkspacesRoot(String workspacesRoot) {
+        configManager.setProperty(WORKSPACES_ROOT, workspacesRoot);
+    }
+
+    public String getDatePattern() {
+        return configManager.getStringProperty(DATE_PATTERN);
+    }
+
+    public void setDatePattern(String datePattern) {
+        configManager.setProperty(DATE_PATTERN, datePattern);
+    }
+
+    public boolean isAutoLogin() {
+        return configManager.getBooleanProperty(AUTO_LOGIN);
+    }
+
+    public void setAutoLogin(boolean autoLogin) {
+        configManager.setProperty(AUTO_LOGIN, autoLogin);
+    }
+
+    public boolean isHideLogout() {
+        return configManager.getBooleanProperty(HIDE_LOGOUT);
+    }
+
+    public void setHideLogout(boolean hideLogout) {
+        configManager.setProperty(HIDE_LOGOUT, hideLogout);
+    }
+
+    public void applyChanges() {
+        boolean saved = configManager.save();
+        if (saved) {
+            WebStudioUtils.getWebStudio().setNeedRestart(true);
+        }
+    }
+
+}
