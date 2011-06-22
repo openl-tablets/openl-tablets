12bb30cc75723e90a01891af91e4b5fe1703edeb
pudalau@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <pudalau@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2011-06-22 14:05:25 +0000

    EPBDS-2574 Data source listening added.

--- WSFrontend/trunk/org.openl.rules.ruleservice/src/org/openl/rules/ruleservice/core/IRuleService.java
+++ WSFrontend/trunk/org.openl.rules.ruleservice/src/org/openl/rules/ruleservice/core/IRuleService.java
@@ -2,6 +2,9 @@ package org.openl.rules.ruleservice.core;
 
 import java.util.List;
 
+import org.openl.rules.ruleservice.loader.IRulesLoader;
+import org.openl.rules.ruleservice.publish.IRulesPublisher;
+
 public interface IRuleService {
 
     OpenLService deploy(ServiceDescription serviceDescription) throws ServiceDeployException;
@@ -13,4 +16,8 @@ public interface IRuleService {
     List<OpenLService> getRunningServices();
 
     OpenLService findServiceByName(String name);
+
+    IRulesLoader getLoader();
+
+    IRulesPublisher getPublisher();
 }
