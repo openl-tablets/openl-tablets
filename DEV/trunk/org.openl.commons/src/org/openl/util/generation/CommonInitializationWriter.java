6fe0f74ac2a0e9fa233d3c0071e7301c3935cdfd
dliauchuk <dliauchuk@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2011-06-24 11:46:44 +0000

    EPBDS add javadocs

--- DEV/trunk/org.openl.commons/src/org/openl/util/generation/CommonInitializationWriter.java
+++ DEV/trunk/org.openl.commons/src/org/openl/util/generation/CommonInitializationWriter.java
@@ -1,5 +1,12 @@
 package org.openl.util.generation;
 
+/**
+ * The most common implementation. For initializing the income value 
+ * returns its String representation by {@link String#valueOf(Object)} 
+ * 
+ * @author DLiauchuk
+ *
+ */
 public class CommonInitializationWriter implements TypeInitializationWriter {
 
     public String getInitialization(Object value) {        
