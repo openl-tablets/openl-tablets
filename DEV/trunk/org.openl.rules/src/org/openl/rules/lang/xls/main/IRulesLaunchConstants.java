1054475700a72c1aafb1c8735803b1485753348e
aastrouski <aastrouski@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2011-07-19 06:56:01 +0000

    EPBDS-2383 Fix workspace setting

--- DEV/trunk/org.openl.rules/src/org/openl/rules/lang/xls/main/IRulesLaunchConstants.java
+++ DEV/trunk/org.openl.rules/src/org/openl/rules/lang/xls/main/IRulesLaunchConstants.java
@@ -3,6 +3,7 @@ package org.openl.rules.lang.xls.main;
 public interface IRulesLaunchConstants {
 
     String START_PROJECT_PROPERTY_NAME = "org.openl.rules.start.project";
+    String LOCAL_WORKSPACE_PROPERTY_NAME = "workspace.local.home";
     String WEBSTUDIO_BROWSER_URL = "http://localhost:8080/webstudio/";
     
     String WRAPPER_SEARCH_START_DIR_DEFAULT = "gen";
