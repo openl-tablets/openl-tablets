cb382f94491341348cfbe3a319cb908fd218009c
pudalau <pudalau@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2011-07-21 13:53:06 +0000

    EPBDS fixed ID field getter & setter

--- DEV/trunk/org.openl.rules/src/org/openl/rules/calc/result/convertor/SimpleStep.java
+++ DEV/trunk/org.openl.rules/src/org/openl/rules/calc/result/convertor/SimpleStep.java
@@ -29,11 +29,11 @@ public class SimpleStep extends CodeStep {
         this.value = value;
     }
     
-    public String getId() {
+    public String getID() {
         return id;
     }
     
-    public void setId(String id) {
+    public void setID(String id) {
         this.id = id;
     }
     
