c7017c7863e740dd263bc7909ea6c37adb3b6f11
aastrouski@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <aastrouski@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2011-08-29 13:19:07 +0000

    EPBDS-2689 Move org.openl.rules.webstudio.java project to org.openl.rules.webstudio

---
+++ STUDIO/trunk/org.openl.rules.webstudio/src/org/openl/rules/ui/tree/TableInstanceTreeNodeBuilder.java
@@ -0,0 +1,131 @@
+package org.openl.rules.ui.tree;
+
+import org.openl.rules.lang.xls.XlsNodeTypes;
+import org.openl.rules.lang.xls.syntax.TableSyntaxNode;
+import org.openl.rules.lang.xls.syntax.TableSyntaxNodeKey;
+import org.openl.rules.ui.IProjectTypes;
+import org.openl.rules.ui.TableSyntaxNodeUtils;
+import org.openl.types.IOpenMethod;
+
+/**
+ * Builds tree node for table instance.
+ * 
+ */
+public class TableInstanceTreeNodeBuilder extends OpenMethodsGroupTreeNodeBuilder {
+
+    private static final String TABLE_INSTANCE_NAME = "Table Instance";
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String[] getDisplayValue(Object sorterObject, int i) {
+
+        TableSyntaxNode tsn = (TableSyntaxNode) sorterObject;
+
+        return TableSyntaxNodeUtils.getTableDisplayValue(tsn, i, getOpenMethodGroupsDictionary());
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getName() {
+        return TABLE_INSTANCE_NAME;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Object getProblems(Object nodeObject) {
+        TableSyntaxNode tsn = (TableSyntaxNode) nodeObject;
+        return tsn.getErrors() != null ? tsn.getErrors() : tsn.getValidationResult();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getType(Object sorterObject) {
+
+        TableSyntaxNode tsn = (TableSyntaxNode) sorterObject;
+
+        return IProjectTypes.PT_TABLE + "." + tsn.getType();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getUrl(Object sorterObject) {
+
+        TableSyntaxNode tsn = (TableSyntaxNode) sorterObject;
+
+        return tsn.getUri();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int getWeight(Object sorterObject) {
+
+        return 0;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean isUnique(TableSyntaxNode tsn) {
+        if (XlsNodeTypes.XLS_PROPERTIES.toString().equals(tsn.getType()) 
+                || XlsNodeTypes.XLS_DATATYPE.toString().equals(tsn.getType())
+                || XlsNodeTypes.XLS_ENVIRONMENT.toString().equals(tsn.getType())) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Object makeObject(TableSyntaxNode tsn) {
+
+        return tsn;
+    }
+
+    @Override
+    public Comparable<?> makeKey(TableSyntaxNode tableSyntaxNode) {
+        if (tableSyntaxNode.getMember() instanceof IOpenMethod) {
+
+            TableSyntaxNodeKey key = new TableSyntaxNodeKey(tableSyntaxNode);
+
+            String keyString = key.toString();
+
+            Object nodeObject = makeObject(tableSyntaxNode);
+
+            String[] displayNames = getDisplayValue(tableSyntaxNode, 0);
+            for(int i = 0; i < displayNames.length; i++){
+                displayNames[i] += keyString;
+            }
+            return new NodeKey(getWeight(nodeObject), displayNames);
+        } else {
+            return super.makeKey(tableSyntaxNode);
+        }
+    }
+
+    @Override
+    public ITreeNode<Object> makeNode(TableSyntaxNode tableSyntaxNode, int i) {
+        Object nodeObject = makeObject(tableSyntaxNode);
+        String[] displayNames = getDisplayValue(nodeObject, 0);
+        // it seems we need to process only those tables that have properties that are using for version sorting.
+        // in other case return original tableSyntaxNode.
+        // ???
+        // author: DLiauchuk
+        ProjectTreeNode projectTreeNode = new VersionedTreeNode(displayNames, tableSyntaxNode);
+        projectTreeNode.setObject(nodeObject);
+        return projectTreeNode;
+    }
+}
