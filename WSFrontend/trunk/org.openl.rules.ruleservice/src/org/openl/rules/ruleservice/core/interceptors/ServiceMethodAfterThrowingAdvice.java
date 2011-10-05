b1874e0ef1223a13239e8f384ee6bb4152b2c3ab
mkamalov@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <mkamalov@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2011-10-05 14:31:22 +0000

    Refactoring...

--- WSFrontend/trunk/org.openl.rules.ruleservice/src/org/openl/rules/ruleservice/core/interceptors/ServiceMethodAfterThrowingAdvice.java
+++ WSFrontend/trunk/org.openl.rules.ruleservice/src/org/openl/rules/ruleservice/core/interceptors/ServiceMethodAfterThrowingAdvice.java
@@ -4,6 +4,6 @@ import java.lang.reflect.Method;
 
 public interface ServiceMethodAfterThrowingAdvice {
 	
-	public void afterThrowing(Method method, Throwable t, Object... args) throws Throwable;
+	void afterThrowing(Method method, Throwable t, Object... args) throws Throwable;
 	
 }
