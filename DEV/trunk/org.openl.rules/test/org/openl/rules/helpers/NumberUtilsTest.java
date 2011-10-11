bbad46ed49e815422f07a9d825ccdfa906eca678
dliauchuk@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <dliauchuk@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2011-10-11 12:08:40 +0000

    EPBDS-2809 Optimize getScale method. Added support for all types.

--- DEV/trunk/org.openl.rules/test/org/openl/rules/helpers/NumberUtilsTest.java
+++ DEV/trunk/org.openl.rules/test/org/openl/rules/helpers/NumberUtilsTest.java
@@ -2,7 +2,11 @@ package org.openl.rules.helpers;
 
 import static org.junit.Assert.*;
 
+import java.math.BigDecimal;
+
 import org.junit.Test;
+import org.openl.meta.BigDecimalValue;
+import org.openl.meta.DoubleValue;
 
 public class NumberUtilsTest {
 	
@@ -38,4 +42,27 @@ public class NumberUtilsTest {
 		assertEquals(0, NumberUtils.getScale((Number)Double.NaN));
 		assertEquals(0, NumberUtils.getScale((Number)Double.NEGATIVE_INFINITY));
 	}
+	
+	@Test
+	public void testFloat() {
+		assertEquals(15, NumberUtils.getScale(Float.valueOf(((float)12.45678))));
+		assertEquals(0, NumberUtils.getScale(Float.NaN));
+		assertEquals(0, NumberUtils.getScale(Float.NEGATIVE_INFINITY));
+		assertEquals(0, NumberUtils.getScale(Float.POSITIVE_INFINITY));
+	}
+	
+	@Test
+	public void testDoubleValue() {
+		assertEquals(4, NumberUtils.getScale(new DoubleValue(1234.5553)));
+		assertEquals(0, NumberUtils.getScale(new DoubleValue(Double.NaN)));
+		assertEquals(0, NumberUtils.getScale(new DoubleValue(Double.NEGATIVE_INFINITY)));
+		assertEquals(0, NumberUtils.getScale(new DoubleValue(Double.POSITIVE_INFINITY)));
+	}
+	
+	@Test
+	public void testBigDecimal() {
+		assertEquals(36, NumberUtils.getScale(new BigDecimal("12.123456789123456789123456789123456789")));
+		assertEquals(35, NumberUtils.getScale(new BigDecimalValue(new BigDecimal("12.12345678912345678912345678912345678"))));
+	}
+	
 }
