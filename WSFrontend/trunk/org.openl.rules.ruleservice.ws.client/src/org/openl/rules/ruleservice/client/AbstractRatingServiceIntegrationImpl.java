4a12f07ab1854852b9b623899b036f1a4f620c03
mkamalov@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <mkamalov@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2011-10-18 13:59:05 +0000

    WS Client API was fixed.

--- WSFrontend/trunk/org.openl.rules.ruleservice.ws.client/src/org/openl/rules/ruleservice/client/AbstractRatingServiceIntegrationImpl.java
+++ WSFrontend/trunk/org.openl.rules.ruleservice.ws.client/src/org/openl/rules/ruleservice/client/AbstractRatingServiceIntegrationImpl.java
@@ -4,16 +4,18 @@ import org.openl.rules.ruleservice.client.mapping.RatingModelMappingProvider;
 import org.openl.rules.ruleservice.client.mapping.ResultMappingProvider;
 
 public abstract class AbstractRatingServiceIntegrationImpl<R, T, K extends RatingServiceInvoker<T>> implements
-        RatingServiceIntegration<R, T, K> {
+        RatingServiceIntegration<R> {
 
     protected RatingModelMappingProvider ratingModelMappingProvider;
 
     protected ResultMappingProvider<T, R> resultMappingProvider;
 
-    public R invoke(K ratingServiceInvoker, Object... args) throws Exception {
-        Object[] ratingModelArgs = ratingModelMappingProvider.mapArgsToRatingModel(args);
-        T result = ratingServiceInvoker.invoke(ratingModelArgs);
-        return resultMappingProvider.mapToResult(result);
+    protected K ratingServiceInvoker;
+
+    public R invoke(Object... args) throws Exception {
+        Object[] ratingModelArgs = getRatingModelMappingProvider().mapArgsToRatingModel(args);
+        T result = getRatingServiceInvoker().invoke(ratingModelArgs);
+        return getResultMappingProvider().mapToResult(result);
     }
 
     public void setRatingModelMappingProvider(RatingModelMappingProvider ratingModelMappingProvider) {
@@ -30,5 +32,13 @@ public abstract class AbstractRatingServiceIntegrationImpl<R, T, K extends Ratin
 
     public ResultMappingProvider<T, R> getResultMappingProvider() {
         return resultMappingProvider;
+    }
+
+    public K getRatingServiceInvoker() {
+        return ratingServiceInvoker;
+    }
+
+    public void setRatingServiceInvoker(K ratingServiceInvoker) {
+        this.ratingServiceInvoker = ratingServiceInvoker;
     }
 }
