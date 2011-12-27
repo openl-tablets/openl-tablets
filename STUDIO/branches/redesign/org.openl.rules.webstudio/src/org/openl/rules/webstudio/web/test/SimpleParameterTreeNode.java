295222f2a8215203616b8b003138bf2d533a38f6
pudalau@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <pudalau@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2011-12-27 09:30:13 +0000

    EPBDS-3036 Fixed 'null' as string values for empty fields.

--- STUDIO/branches/redesign/org.openl.rules.webstudio/src/org/openl/rules/webstudio/web/test/SimpleParameterTreeNode.java
+++ STUDIO/branches/redesign/org.openl.rules.webstudio/src/org/openl/rules/webstudio/web/test/SimpleParameterTreeNode.java
@@ -2,6 +2,9 @@ package org.openl.rules.webstudio.web.test;
 
 import java.util.LinkedHashMap;
 
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.openl.rules.convertor.IString2DataConvertor;
 import org.openl.rules.convertor.String2DataConvertorFactory;
 import org.openl.rules.table.formatters.FormattersManager;
@@ -9,9 +12,14 @@ import org.openl.rules.testmethod.ParameterWithValueDeclaration;
 import org.openl.types.IOpenClass;
 
 public class SimpleParameterTreeNode extends ParameterDeclarationTreeNode {
+    private static final Log LOG = LogFactory.getLog(SimpleParameterTreeNode.class);
+
     public static final String SIMPLE_TYPE = "simple";
 
-    public SimpleParameterTreeNode(String fieldName, Object value, IOpenClass fieldType, ParameterDeclarationTreeNode parent) {
+    public SimpleParameterTreeNode(String fieldName,
+            Object value,
+            IOpenClass fieldType,
+            ParameterDeclarationTreeNode parent) {
         super(fieldName, value, fieldType, parent);
     }
 
@@ -24,18 +32,31 @@ public class SimpleParameterTreeNode extends ParameterDeclarationTreeNode {
         Object value = getValue();
         return FormattersManager.getFormatter(value).format(value);
     }
-
+    
+    public String getValueForEdit() {
+        Object value = getValue();
+        if (value != null) {
+            return FormattersManager.getFormatter(value).format(value);
+        } else {
+            return "";
+        }
+    }
     @Override
     public String getNodeType() {
         return SIMPLE_TYPE;
     }
 
-    public void setDisplayedValue(String value) {
-        IString2DataConvertor convertor = String2DataConvertorFactory.getConvertor(getType().getInstanceClass());
-        try {
-            setValueForced(convertor.parse(value, null, null));
-        } catch (Exception e) {
-            //TODO message on UI
+    public void setValueForEdit(String value) {
+        if (StringUtils.isBlank(value)) {
+            setValueForced(null);
+        } else {
+            try {
+                IString2DataConvertor convertor = String2DataConvertorFactory.getConvertor(getType().getInstanceClass());
+                setValueForced(convertor.parse(value, null, null));
+            } catch (Exception e) {
+                // TODO message on UI
+                LOG.warn(String.format("Failed to set \"%s\" value to field [%s]", value, getName()), e);
+            }
         }
     }
 
