73ac858b79dfdcbd1cb4fe0bd831244c44c8845e
pudalau <pudalau@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2012-01-13 07:00:06 +0000

    EPBDS-2815 Fixed table version comparison.

--- DEV/trunk/org.openl.rules/src/org/openl/rules/lang/xls/binding/TableVersionComparator.java
+++ DEV/trunk/org.openl.rules/src/org/openl/rules/lang/xls/binding/TableVersionComparator.java
@@ -4,7 +4,9 @@ import java.util.Comparator;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.openl.rules.lang.xls.syntax.TableSyntaxNode;
 import org.openl.rules.table.properties.DimensionPropertiesMethodKey;
+import org.openl.rules.table.properties.ITableProperties;
 import org.openl.rules.table.properties.PropertiesHelper;
 import org.openl.types.IOpenMethod;
 import org.openl.util.conf.Version;
@@ -15,35 +17,45 @@ import org.openl.util.conf.Version;
  * @return -1 if the first later or "active", 1 if second later "active" and 0
  *         if tables are "inactive" and have similar versions
  */
-public class TableVersionComparator implements Comparator<IOpenMethod> {
+public class TableVersionComparator implements Comparator<ITableProperties> {
 
     private static Log LOG = LogFactory.getLog(TableVersionComparator.class);
 
-    @Override
     public int compare(IOpenMethod first, IOpenMethod second) {
         if (!new DimensionPropertiesMethodKey(first).equals(new DimensionPropertiesMethodKey(second))) {
             throw new IllegalArgumentException("Uncomparable tables. Tasbles should have similar name,signature and dimension properties.");
         }
-        if (PropertiesHelper.getTableProperties(first).getActive() != null) {
-            if (PropertiesHelper.getTableProperties(first).getActive()) {
+        return compare(PropertiesHelper.getTableProperties(first), PropertiesHelper.getTableProperties(second));
+    }
+
+    public int compare(TableSyntaxNode first, TableSyntaxNode second) {
+        return compare(first.getTableProperties(), second.getTableProperties());
+    }
+
+    @Override
+    public int compare(ITableProperties first, ITableProperties second) {
+        if (first.getActive() != second.getActive()) {
+            if (first.getActive()) {
                 return -1;
-            } else if (PropertiesHelper.getTableProperties(second).getActive()) {
+            } else if (second.getActive()) {
                 return 1;
             }
-        } else {
-            return 0;
         }
+        Version firstNodeVersion = parseVersionForComparison(first.getVersion());
+        Version secondNodeVersion =parseVersionForComparison(second.getVersion());
+        return secondNodeVersion.compareTo(firstNodeVersion);
+    }
+
+    private static Version DEFAULT_VERSION = Version.parseVersion("0.0.0",0, "..");
+    private static Version parseVersionForComparison(String version){
         try {
-            Version firstNodeVersion = Version.parseVersion(PropertiesHelper.getTableProperties(first).getVersion(), 0, "..");
-            Version secondNodeVersion = Version.parseVersion(PropertiesHelper.getTableProperties(second).getVersion(), 0, "..");
-            return secondNodeVersion.compareTo(firstNodeVersion);
+            return Version.parseVersion(version, 0, "..");
         } catch (RuntimeException e) {
             // it is just fix to avoid tree crashing.
             // we need to validate format of the versions, during compilation of
             // Openl and also on UI.
-            LOG.error(e);
+            LOG.warn(e);
+            return DEFAULT_VERSION;
         }
-        return 0;
     }
-
 }
