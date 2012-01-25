74125fa986c02dea667603a49dea78dbf3cd7486
pudalau@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <pudalau@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2012-01-25 12:27:51 +0000

    EPBDS reduced memory to prevent failures during of VM start on some PCs

--- STUDIO/branches/redesign/org.openl.rules.tomcat.lib/apache-tomcat-7.0.8/bin/setenv.bat
+++ STUDIO/branches/redesign/org.openl.rules.tomcat.lib/apache-tomcat-7.0.8/bin/setenv.bat
@@ -1,1 +1,1 @@
-set JAVA_OPTS=%JAVA_OPTS% -Xms256m -Xmx1024m -XX:PermSize=128m -XX:MaxPermSize=512m
+set JAVA_OPTS=%JAVA_OPTS% -Xms256m -Xmx1024m -XX:PermSize=128m -XX:MaxPermSize=256m
--- STUDIO/branches/redesign/org.openl.rules.tomcat.lib/apache-tomcat-7.0.8/bin/setenv.sh
+++ STUDIO/branches/redesign/org.openl.rules.tomcat.lib/apache-tomcat-7.0.8/bin/setenv.sh
@@ -1,1 +1,1 @@
-export JAVA_OPTS="$JAVA_OPTS -Xms256m -Xmx1024m -XX:PermSize=128m -XX:MaxPermSize=512m"
+export JAVA_OPTS="$JAVA_OPTS -Xms256m -Xmx1024m -XX:PermSize=128m -XX:MaxPermSize=256m"
