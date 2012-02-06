6fa31a20ee6eac099287c0979b1711189138344c
pudalau@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <pudalau@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2012-02-06 13:56:38 +0000

    EPBDS-3179 parallel GC instead of default

--- STUDIO/branches/redesign/org.openl.rules.tomcat.lib/apache-tomcat-7.0.8/bin/setenv.bat
+++ STUDIO/branches/redesign/org.openl.rules.tomcat.lib/apache-tomcat-7.0.8/bin/setenv.bat
@@ -1,1 +1,1 @@
-set JAVA_OPTS=%JAVA_OPTS% -Xms256m -Xmx1024m -XX:PermSize=128m -XX:MaxPermSize=256m
+set JAVA_OPTS=%JAVA_OPTS% -Xms256m -Xmx1024m -XX:+UseParallelOldGC -XX:PermSize=128m -XX:MaxPermSize=256m
--- STUDIO/branches/redesign/org.openl.rules.tomcat.lib/apache-tomcat-7.0.8/bin/setenv.sh
+++ STUDIO/branches/redesign/org.openl.rules.tomcat.lib/apache-tomcat-7.0.8/bin/setenv.sh
@@ -1,1 +1,1 @@
-export JAVA_OPTS="$JAVA_OPTS -Xms256m -Xmx1024m -XX:PermSize=128m -XX:MaxPermSize=256m"
+export JAVA_OPTS="$JAVA_OPTS -Xms256m -Xmx1024m -XX:+UseParallelOldGC -XX:PermSize=128m -XX:MaxPermSize=256m"
