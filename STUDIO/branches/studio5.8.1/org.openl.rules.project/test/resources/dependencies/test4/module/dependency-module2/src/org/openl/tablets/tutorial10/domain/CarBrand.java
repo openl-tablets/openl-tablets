ba463fb681ba30d560ddff29d0434dc85de246fe
dliauchuk <dliauchuk@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2012-02-14 11:39:28 +0000

    Copy Studio 5.8.1

---
+++ STUDIO/branches/studio5.8.1/org.openl.rules.project/test/resources/dependencies/test4/module/dependency-module2/src/org/openl/tablets/tutorial10/domain/CarBrand.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package org.openl.tablets.tutorial10.domain;
+
+/**
+ * @author User
+ *
+ */
+public enum CarBrand {
+	BMW,
+	Porche,
+	Audi,
+}
---
+++ STUDIO/branches/studio5.8.1/org.openl.rules.tomcat.lib/apache-tomcat-7.0.8/bin/setenv.bat
@@ -0,0 +1,1 @@
+set JAVA_OPTS=%JAVA_OPTS% -Xms256m -Xmx1024m -XX:PermSize=128m -XX:MaxPermSize=512m
