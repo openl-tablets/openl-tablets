179a6d40ffa89d7d31b6f16f170365010b06c1b0
dliauchuk <dliauchuk@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2012-02-14 11:58:27 +0000

    Delete old tag

--- STUDIO/tags/release-5.8.2/trunk/org.openl.rules.project/test/resources/dependencies/test4/module/dependency-module2/src/org/openl/tablets/tutorial10/Tutorial10Rules.java
+++
@@ -1,9 +0,0 @@
-package org.openl.tablets.tutorial10;
-
-import org.openl.meta.DoubleValue;
-import org.openl.tablets.tutorial10.domain.Address;
-import org.openl.tablets.tutorial10.domain.Car;
-
-public interface Tutorial10Rules {
-    DoubleValue getPriceForOrder(Car car, int numberOfCars, Address address);
-}
