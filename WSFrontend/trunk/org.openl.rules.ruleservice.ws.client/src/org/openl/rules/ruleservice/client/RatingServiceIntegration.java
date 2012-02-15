2a1feec0a9e2e3b7b285bfcdbf4ddc115020dc94
mkamalov <mkamalov@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2012-02-15 14:45:09 +0000

    org.openl.rules.ruleservice.client and org.openl.rules.ruleservice.ws.client were removed.

--- WSFrontend/trunk/org.openl.rules.ruleservice.ws.client/src/org/openl/rules/ruleservice/client/RatingServiceIntegration.java
+++
@@ -1,5 +0,0 @@
-package org.openl.rules.ruleservice.client;
-
-public interface RatingServiceIntegration<R> {
-	public R invoke(Object... args) throws Exception;
-}
--- WSFrontend/trunk/org.openl.rules.ruleservice.ws.client/src/org/openl/rules/ruleservice/client/mapping/RatingModelMappingProvider.java
+++
@@ -1,5 +0,0 @@
-package org.openl.rules.ruleservice.client.mapping;
-
-public interface RatingModelMappingProvider {
-    public Object[] mapArgsToRatingModel(Object... args) throws OpenLClientMappingException;
-}
