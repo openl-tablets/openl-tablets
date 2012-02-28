d4793b7f9a92047a3902b71bce5aa5362f7469cf
dliauchuk@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <dliauchuk@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2012-02-28 14:45:32 +0000

    Delete release tag

--- DEV/tags/release-5.9.0/org.openl.rules.constrainer/src/org/openl/ie/tools/ExceptionWrapper.java
+++
@@ -1,7 +0,0 @@
-package org.openl.ie.tools;
-
-public interface ExceptionWrapper {
-    // Get the thrown target exception
-
-    public Throwable getTargetException();
-}
--- DEV/tags/release-5.9.0/org.openl.rules/src/org/openl/rules/testmethod/result/TestResultComparator.java
+++
@@ -1,6 +0,0 @@
-package org.openl.rules.testmethod.result;
-
-public interface TestResultComparator {
-    
-    boolean compareResult(Object actualResult, Object expectedResult);
-}
