64a8c1d7bb8c32d8d3bf06d480f1212548fa388b
dliauchuk <dliauchuk@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2012-02-28 15:27:58 +0000

    EPBDS-345.[maven-release-plugin] copy for tag release-5.9.0

---
+++ Eclipse/tags/release-5.9.0/org.openl.rules.eclipse.ui.wizard/templates/org.openl.examples/src/org/openl/examples/Result.java
@@ -0,0 +1,34 @@
+/*
+ * Created on Sep 16, 2003
+ *
+ * Developed by Intelligent ChoicePoint Inc. 2003
+ */
+package org.openl.examples;
+
+/**
+ * @author Jacob
+ *
+ * To change the template for this generated type comment go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+public class Result
+{
+	String greeting;
+	
+	/**
+	 * @return
+	 */
+	public String getGreeting()
+	{
+		return greeting;
+	}
+
+	/**
+	 * @param string
+	 */
+	public void setGreeting(String string)
+	{
+		greeting = string;
+	}
+
+}
