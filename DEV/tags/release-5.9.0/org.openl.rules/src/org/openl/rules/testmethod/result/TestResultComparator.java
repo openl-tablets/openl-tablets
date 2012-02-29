cf86bdc8e4050a8d9b9606e282fb614b3165c69f
dliauchuk <dliauchuk@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2012-02-29 10:54:32 +0000

    Delete release tag

--- DEV/tags/release-5.9.0/org.openl.rules/src/org/openl/rules/testmethod/result/TestResultComparator.java
+++
@@ -1,7 +0,0 @@
-package org.openl.rules.testmethod.result;
-
-// TODO: refactor, use java.util.Comparator instead of own interface
-public interface TestResultComparator {
-    
-    boolean compareResult(Object actualResult, Object expectedResult);
-}
