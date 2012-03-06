4e9129e71cb84f4e91d0a7051be2258f836b3423
nsamatov <nsamatov@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2012-03-06 13:52:10 +0000

    fixed bug: false positive when deleting locked folder

--- STUDIO/trunk/org.openl.rules.workspace/src/org/openl/rules/workspace/lw/impl/FolderHelper.java
+++ STUDIO/trunk/org.openl.rules.workspace/src/org/openl/rules/workspace/lw/impl/FolderHelper.java
@@ -27,6 +27,14 @@ public class FolderHelper {
         }
     }
 
+    /**
+     * Clears the folder's content.
+     * The folder itself will not be deleted.
+     * If you want to delete a folder itself, use {@link #deleteFolder(File)} instead
+     * 
+     * @param folder folder, which content will be deleted
+     * @return true if all content is deleted and false if at least one file or sub-folder cannot be deleted.
+     */
     public static boolean clearFolder(File folder) {
         if (log.isDebugEnabled()) {
             log.debug(MsgHelper.format("Clearing folder ''{0}''", folder));
@@ -45,7 +53,9 @@ public class FolderHelper {
             if (file.isFile()) {
                 if (!file.delete()) {
                     failures = true;
-                    log.debug(MsgHelper.format("Failed to delete file ''{0}''!", file.getAbsolutePath()));
+                    if (log.isDebugEnabled()) {
+                        log.debug(MsgHelper.format("Failed to delete file ''{0}''!", file.getAbsolutePath()));
+                    }
                 }
             } else {
                 if (!deleteFolder(file)) {
@@ -57,6 +67,13 @@ public class FolderHelper {
         return !failures;
     }
 
+    /**
+     * Deletes the folder and it's content recursively.
+     * If you do not want to delete a folder itself, use {@link #clearFolder(File)} instead
+     * 
+     * @param folder the folder, that will be deleted
+     * @return true if the folder is deleted and false if the folder cannot be deleted.
+     */
     public static boolean deleteFolder(File folder) {
         if (log.isDebugEnabled()) {
             log.debug(MsgHelper.format("Deleting folder ''{0}''", folder));
@@ -78,13 +95,18 @@ public class FolderHelper {
                     // delete file
                     if (!f.delete()) {
                         failures = true;
-                        log.debug(MsgHelper.format("Failed to delete file ''{0}''!", f.getAbsolutePath()));
+                        if (log.isDebugEnabled()) {
+                            log.debug(MsgHelper.format("Failed to delete file ''{0}''!", f.getAbsolutePath()));
+                        }
                     }
                 }
             }
 
             if (!folder.delete()) {
-                log.debug(MsgHelper.format("Failed to delete folder ''{0}''!", folder.getAbsolutePath()));
+                 failures = true;
+                 if (log.isDebugEnabled()) {
+                    log.debug(MsgHelper.format("Failed to delete folder ''{0}''!", folder.getAbsolutePath()));
+                }
             }
         }
 
