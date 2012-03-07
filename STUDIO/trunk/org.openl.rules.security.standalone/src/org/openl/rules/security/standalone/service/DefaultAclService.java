ef692a9ed411d3b97d1f6acec6ab4a915a09ff39
aastrouski <aastrouski@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2012-03-07 13:41:10 +0000

    EPBDS-3246,3249,3250 Upgrade Hibernate ORM (new name) to v4.1 Upgrade javassist to v3.15.0 Upgrade hsqldb to v2.2.8

--- STUDIO/trunk/org.openl.rules.security.standalone/src/org/openl/rules/security/standalone/service/DefaultAclService.java
+++ STUDIO/trunk/org.openl.rules.security.standalone/src/org/openl/rules/security/standalone/service/DefaultAclService.java
@@ -1,23 +1,23 @@
 package org.openl.rules.security.standalone.service;
 
-import org.hibernate.HibernateException;
-import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 
 import org.hibernate.criterion.Restrictions;
 
 import org.openl.rules.security.standalone.persistence.AccessControlEntry;
 
-import org.springframework.orm.hibernate3.HibernateCallback;
-import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+public class DefaultAclService {
 
-public class DefaultAclService extends HibernateDaoSupport {
+    private SessionFactory sessionFactory;
+
+    public void setSessionFactory(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
 
     public AccessControlEntry getAccessControlEntry(final String userName, final String resource) {
-        return (AccessControlEntry) getHibernateTemplate().execute(new HibernateCallback() {
-            public Object doInHibernate(Session session) throws HibernateException {
-                return session.createCriteria(AccessControlEntry.class).add(Restrictions.eq("loginName", userName))
-                        .add(Restrictions.eq("resource", resource)).uniqueResult();
-            }
-        });
+        return (AccessControlEntry) sessionFactory.getCurrentSession().createCriteria(AccessControlEntry.class)
+                .add(Restrictions.eq("loginName", userName))
+                .add(Restrictions.eq("resource", resource))
+                .uniqueResult();
     }
 }
