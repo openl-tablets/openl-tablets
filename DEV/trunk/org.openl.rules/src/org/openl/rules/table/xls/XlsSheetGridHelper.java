6ec225baad8c8a1b8af4ef1aa3cb12bca87c5dc8
DLiauchuk@01317ed0-3458-11dc-be9f-5b81e4b48cb4 <DLiauchuk@01317ed0-3458-11dc-be9f-5b81e4b48cb4>
2012-03-30 08:50:55 +0000

    EPBDS optimize string concats

--- DEV/trunk/org.openl.rules/src/org/openl/rules/table/xls/XlsSheetGridHelper.java
+++ DEV/trunk/org.openl.rules/src/org/openl/rules/table/xls/XlsSheetGridHelper.java
@@ -61,7 +61,7 @@ public class XlsSheetGridHelper {
     }
     
     private static String getDefaultFileName(Sheet sheet) {
-        return String.format("%s%s", sheet.getSheetName(), VIRTUAL_EXCEL_FILE).intern();
+        return (sheet.getSheetName() + VIRTUAL_EXCEL_FILE).intern();
     }
 
 }
