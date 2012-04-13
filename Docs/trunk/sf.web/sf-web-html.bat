3dd57ff6350ce8ec303822ed5f25e661576c0ee7
mkamalov@mkamalov-PC.internal.corp <mkamalov@mkamalov-PC.internal.corp>
2012-04-13 12:45:59 +0400

    Docs was removed.

--- Docs/trunk/sf.web/sf-web-html.bat
+++
@@ -1,6 +0,0 @@
-
-cd workspace/sf.web
-
-psftp -b sf-web-html.ftp -bc snshor@shell.sourceforge.net
- 
-pause
--- Docs/trunk/sf.web/sf-web.bat
+++
@@ -1,7 +0,0 @@
-cd workspace\sf.web
-
-
-rem psftp -b sf-web.ftp -bc snshor@shell.sourceforge.net
-psftp -b sf-web.ftp snshor,openl-tablets@web.sourceforge.net
- 
-pause
