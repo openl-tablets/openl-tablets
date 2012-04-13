17d2edb396744933373bd656713ec3a811724854
mkamalov@mkamalov-PC.internal.corp <mkamalov@mkamalov-PC.internal.corp>
2012-04-13 12:42:11 +0400

    EPB-Integration was removed.

--- EPB-integration/Runtime/source/com.exigen.openl.model/src/main/java/com/exigen/openl/component/ErrorListener.java
+++
@@ -1,9 +0,0 @@
-package com.exigen.openl.component;
-
-public interface ErrorListener {
-
-	void parsingError(String message, Throwable cause, String location);
-
-	void bindingError(String message, Throwable cause, String location);
-
-}
