91a0766eae4ca99fd50c6b8816b1843c67044e9b
DLiauchuk <none@none>
2012-05-08 14:41:36 +0300

    Overriden cloneInternal to avoid cloning custom SpreadsheetResult

--- DEV/trunk/org.openl.rules/src/org/openl/rules/table/InputArgumentsCloner.java
+++ DEV/trunk/org.openl.rules/src/org/openl/rules/table/InputArgumentsCloner.java
@@ -1,5 +1,8 @@
 package org.openl.rules.table;
 
+import java.util.Map;
+
+import org.objenesis.Objenesis;
 import org.openl.meta.BigDecimalValue;
 import org.openl.meta.BigIntegerValue;
 import org.openl.meta.ByteValue;
@@ -10,41 +13,56 @@ import org.openl.meta.LongValue;
 import org.openl.meta.ShortValue;
 import org.openl.meta.StringValue;
 import org.openl.rules.calc.SpreadsheetResult;
+import org.openl.rules.calc.result.SpreadsheetResultHelper;
 
 import com.rits.cloning.Cloner;
 
 /**
- * Extension for {@link Cloner}. To add OpenL conditionally immutable classes
+ * Extension for {@link Cloner}. To add OpenL classes
  * to prevent cloning instances of them.
  * 
  * TODO: should be analyzed variations of tracing different rules. Check if we have issues with mutatation
- * of listed below OpenL conditionally immutable classes.
+ * of listed below OpenL not cloned classes.
  * 
  * @author DLiauchuk
  *
  */
 public class InputArgumentsCloner extends Cloner {
+    
+    
+    public InputArgumentsCloner() {
+        super();
+        dontCloneClasses();
+    }
+    
+    private void dontCloneClasses() {
+        // Register them as dont clone as in 90% of cases instances of them are not modified in rules.
+        // But always cloning them degrades the performance very much. It becomes impossible to open the trace.
+        //
+        dontClone(SpreadsheetResult.class);
+        dontClone(ByteValue.class);
+        dontClone(ShortValue.class);
+        dontClone(IntValue.class);
+        dontClone(LongValue.class);
+        dontClone(FloatValue.class);
+        dontClone(DoubleValue.class);
+        dontClone(BigIntegerValue.class);
+        dontClone(BigDecimalValue.class);
+        dontClone(StringValue.class);
+    }
+
+    public InputArgumentsCloner(final Objenesis objenesis) {
+        super(objenesis);
+        dontCloneClasses();
+    }
+    
+    //Overriden to avoid cloning generated at runtime custom SpreadsheetResult children classes
 	@Override
-	protected void registerKnownJdkImmutableClasses() {		
-		super.registerKnownJdkImmutableClasses();
-		// register conditionally immutable OpenL classes
-		registerConditionallyOpenLImmutableClasess();
-	}
-	
-	private void registerConditionallyOpenLImmutableClasess() {
-		// though by implementation this classes are mutable,
-		// register them as immutable as in 90% of cases instances of them are not modified in rules.
-		// But always cloning them degrades the performance very much. It becomes impossible to open the trace.
-		//
-		registerImmutable(SpreadsheetResult.class);
-		registerImmutable(ByteValue.class);
-		registerImmutable(ShortValue.class);
-		registerImmutable(IntValue.class);
-		registerImmutable(LongValue.class);
-		registerImmutable(FloatValue.class);
-		registerImmutable(DoubleValue.class);
-		registerImmutable(BigIntegerValue.class);
-		registerImmutable(BigDecimalValue.class);
-		registerImmutable(StringValue.class);
+	public <T> T cloneInternal(final T o, final Map<Object, Object> clones) throws IllegalAccessException {
+	    if (o == null) return null;
+	    if (SpreadsheetResultHelper.isSpreadsheetResult(o.getClass())) {
+	        dontClone(o.getClass());
+	    }
+	    return super.cloneInternal(o, clones);
 	}
 }
