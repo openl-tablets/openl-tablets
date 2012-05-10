7f842afe8d885c87a3913c1bfe72d14bd34df2af
PUdalau@noteimage.internal.corp <PUdalau@noteimage.internal.corp>
2012-05-10 22:26:48 +0400

    Variations from rules.

--- STUDIO/trunk/org.openl.rules.project/src/org/openl/rules/project/instantiation/variation/VariationsFactory.java
+++ STUDIO/trunk/org.openl.rules.project/src/org/openl/rules/project/instantiation/variation/VariationsFactory.java
@@ -11,64 +11,65 @@ package org.openl.rules.project.instantiation.variation;
  * 
  * @author PUdalau
  */
+//TODO complex variations with separator in the path + test
 public abstract class VariationsFactory {
-	/**
-	 * Path that represents modification of root object(argument).
-	 */
-	public static final String THIS_POINTER = ".";
+    /**
+     * Path that represents modification of root object(argument).
+     */
+    public static final String THIS_POINTER = ".";
 
-	/**
-	 * Creates variation using all needed parameters.
-	 * 
-	 * @param vairationId
-	 *            Id for variation.
-	 * @param argumentIndex
-	 *            Index of argument to be modified.
-	 * @param path
-	 *            Path to field that will be modified. Or
-	 *            {@link VariationsFactory.THIS_POINTER} for argument replacemnt
-	 *            variation.
-	 * @param valueToSet
-	 *            Value to set for modified field.
-	 * @return Variation that corresponds the specified path.
-	 */
-	public static Variation getVariation(String vairationId, int argumentIndex,
-			String path, Object valueToSet) {
-		return getVariation(vairationId, argumentIndex, path, valueToSet, false);
-	}
+    /**
+     * Creates variation using all needed parameters.
+     * 
+     * @param vairationId Id for variation.
+     * @param argumentIndex Index of argument to be modified.
+     * @param path Path to field that will be modified. Or
+     *            {@link VariationsFactory.THIS_POINTER} for argument replacemnt
+     *            variation.
+     * @param valueToSet Value to set for modified field.
+     * @return Variation that corresponds the specified path.
+     */
+    public static Variation getVariation(String vairationId, int argumentIndex, String path, Object valueToSet) {
+        return getVariation(vairationId, argumentIndex, path, valueToSet, false);
+    }
 
-	/**
-	 * Creates variation using all needed parameters with possibility to use
-	 * cloning for variations.
-	 * 
-	 * @param vairationId
-	 *            Id for variation.
-	 * @param argumentIndex
-	 *            Index of argument to be modified.
-	 * @param path
-	 *            Path to field that will be modified. Or
-	 *            {@link VariationsFactory.THIS_POINTER} for argument replacemnt
-	 *            variation.
-	 * @param valueToSet
-	 *            Value to set for modified field.
-	 * @param cloneArguments
-	 *            Flag that determines whether the created variation should be
-	 *            wrapped by {@link DeepCloninigVariaion}.
-	 * @return Variation that corresponds the specified path.
-	 */
-	public static Variation getVariation(String vairationId, int argumentIndex,
-			String path, Object valueToSet, boolean cloneArguments) {
-		Variation variation = null;
-		if (path.equals(THIS_POINTER)) {
-			variation = new ArgumentReplacementVariation(vairationId,
-					argumentIndex, valueToSet);
-		} else {
-			variation = new JXPathVariation(vairationId, argumentIndex, path,
-					valueToSet);
-		}
-		if (cloneArguments) {
-			variation = new DeepCloninigVariaion(variation);
-		}
-		return variation;
-	}
+    /**
+     * Creates variation using all needed parameters with possibility to use
+     * cloning for variations.
+     * 
+     * @param vairationId Id for variation.
+     * @param argumentIndex Index of argument to be modified.
+     * @param path Path to field that will be modified. Or
+     *            {@link VariationsFactory.THIS_POINTER} for argument replacemnt
+     *            variation.
+     * @param valueToSet Value to set for modified field.
+     * @param cloneArguments Flag that determines whether the created variation
+     *            should be wrapped by {@link DeepCloninigVariaion}.
+     * @return Variation that corresponds the specified path.
+     */
+    public static Variation getVariation(String vairationId,
+            int argumentIndex,
+            String path,
+            Object valueToSet,
+            boolean cloneArguments) {
+        //TODO check null ID
+        Variation variation = null;
+        if (path.equals(THIS_POINTER)) {
+            variation = new ArgumentReplacementVariation(vairationId, argumentIndex, valueToSet);
+        } else {
+            variation = new JXPathVariation(vairationId, argumentIndex, path, valueToSet);
+        }
+        if (cloneArguments) {
+            variation = new DeepCloninigVariaion(variation);
+        }
+        return variation;
+    }
+
+    public static Variation getVariation(VariationDescription description) {
+        return getVariation(description.getVariationID(),
+            description.getUpdatedArgumentIndex(),
+            description.getPath(),
+            description.getValueToSet(),
+            description.isUseCloning());
+    }
 }
