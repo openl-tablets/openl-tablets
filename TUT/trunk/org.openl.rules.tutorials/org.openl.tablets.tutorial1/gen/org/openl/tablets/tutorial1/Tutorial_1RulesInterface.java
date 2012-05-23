f7510b582b4f944d04a71b80917c58dd3b4ae0fb
DLiauchuk@dlevchuk.internal.corp <DLiauchuk@dlevchuk.internal.corp>
2012-05-23 17:59:42 +0300

    EPBDS-3494 migrate tutorial1 to interface

---
+++ TUT/trunk/org.openl.rules.tutorials/org.openl.tablets.tutorial1/gen/org/openl/tablets/tutorial1/Tutorial_1RulesInterface.java
@@ -0,0 +1,13 @@
+/*
+ * This class has been generated. 
+*/
+
+package org.openl.tablets.tutorial1;
+
+
+public interface Tutorial_1RulesInterface {
+  public static java.lang.String __src = "rules/Tutorial_1.xls";
+
+  void hello1(int hour);
+
+}
