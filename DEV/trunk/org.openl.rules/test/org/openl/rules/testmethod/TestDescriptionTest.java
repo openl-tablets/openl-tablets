b241b2ae173bbdf42c38ef96b46f9ecf364770af
NSamatov@nsamatov.internal.corp <NSamatov@nsamatov.internal.corp>
2012-05-28 15:23:50 +0400

    EPBDS-3440

---
+++ DEV/trunk/org.openl.rules/test/org/openl/rules/testmethod/TestDescriptionTest.java
@@ -0,0 +1,86 @@
+package org.openl.rules.testmethod;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
+import org.openl.types.IMethodSignature;
+import org.openl.types.IOpenMethod;
+import org.openl.vm.IRuntimeEnv;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TestDescriptionTest {
+    @Mock
+    private IRuntimeEnv env;
+    @Mock
+    private Object target;
+
+    private SomeArgument[] arguments;
+
+    @Before
+    public void setUp() {
+        arguments = new SomeArgument[] { new SomeArgument("test") };
+    }
+
+    @Test
+    public void testNotModifyInputParameters() {
+
+        TestDescription description = new TestDescription(createTestMethodMock(), arguments);
+
+        description.runTest(target, env, 1);
+        assertEquals("test", arguments[0].value);
+
+        description.runTest(target, env, 5);
+        assertEquals("test", arguments[0].value);
+    }
+
+    private IOpenMethod createTestMethodMock() {
+        IMethodSignature signature = createMethodSignatureMock();
+        IOpenMethod method = mock(IOpenMethod.class);
+
+        when(method.getSignature()).thenReturn(signature);
+
+        when(method.invoke(any(), any(Object[].class), any(IRuntimeEnv.class))).thenAnswer(new Answer<Void>() {
+            public Void answer(InvocationOnMock invocation) throws Throwable {
+                Object[] params = (Object[]) invocation.getArguments()[1];
+
+                // Modify method input parameters
+                SomeArgument someArgument = (SomeArgument) params[0];
+                someArgument.value = "modified" + someArgument.value;
+                return null;
+            }
+        });
+
+        return method;
+    }
+
+    private IMethodSignature createMethodSignatureMock() {
+        IMethodSignature signature = mock(IMethodSignature.class);
+        when(signature.getNumberOfParameters()).thenReturn(arguments.length);
+
+        when(signature.getParameterName(anyInt())).thenAnswer(new Answer<String>() {
+            public String answer(InvocationOnMock invocation) throws Throwable {
+                return "param" + invocation.getArguments()[0];
+            }
+        });
+
+        return signature;
+    }
+
+    private static class SomeArgument {
+        public String value;
+
+        public SomeArgument(String value) {
+            this.value = value;
+        }
+    }
+}
