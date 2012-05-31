c3cb253748832326d1aa2af3a5d76bf9f8f697c4
NSamatov <NSamatov@nsamatov.internal.corp>
2012-05-31 15:37:45 +0400

    EPBDS-2793 reformat ArrayToolTest

--- DEV/trunk/org.openl.commons/test/org/openl/util/ArrayToolTest.java
+++ DEV/trunk/org.openl.commons/test/org/openl/util/ArrayToolTest.java
@@ -7,28 +7,27 @@ import static org.junit.Assert.assertNull;
 import org.junit.Test;
 
 public class ArrayToolTest {
-	
-	@Test
-	public void testRemoveNulls() {
-		assertNull(ArrayTool.removeNulls(null));
-		Object[] emptyArray = new Object[0];
-		
-		assertArrayEquals(emptyArray, ArrayTool.removeNulls(emptyArray));
+
+    @Test
+    public void testRemoveNulls() {
+        assertNull(ArrayTool.removeNulls(null));
+        Object[] emptyArray = new Object[0];
+
+        assertArrayEquals(emptyArray, ArrayTool.removeNulls(emptyArray));
         assertArrayEquals(emptyArray, ArrayTool.removeNulls(new Object[] { null }));
-		
-		Integer value1 = Integer.valueOf(12);
-		Double value2 = Double.valueOf(11);
-		Object[] array = new Object[]{value1, null, value2};
-		
-		Object[] expected = new Object[]{value1, value2};
-		
-		Object[] actual = ArrayTool.removeNulls(array);
-		assertEquals(2, actual.length);
-		assertArrayEquals(expected, actual);
-		
-		
-		Object[] allNulls = new Object[]{null, null, null, null};
-		Object[] actual1 = ArrayTool.removeNulls(allNulls);
-		assertEquals(0, actual1.length);
-	}
+
+        Integer value1 = Integer.valueOf(12);
+        Double value2 = Double.valueOf(11);
+        Object[] array = new Object[] { value1, null, value2 };
+
+        Object[] expected = new Object[] { value1, value2 };
+
+        Object[] actual = ArrayTool.removeNulls(array);
+        assertEquals(2, actual.length);
+        assertArrayEquals(expected, actual);
+
+        Object[] allNulls = new Object[] { null, null, null, null };
+        Object[] actual1 = ArrayTool.removeNulls(allNulls);
+        assertEquals(0, actual1.length);
+    }
 }
