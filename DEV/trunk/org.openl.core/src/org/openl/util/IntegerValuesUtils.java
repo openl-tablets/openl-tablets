91a11fe69bf794fa394ce8ea39cb28245b6540a9
Pavel Tarasevich <Pavel.Tarasevich@exigenservices.com>
2012-06-15 10:44:37 +0300

    EPBDS-2464: User cannot call datatype arrays of wrapper datatypes (Integer, Double and so on) and <Num>Values (IntValue and so on)

---
+++ DEV/trunk/org.openl.core/src/org/openl/util/IntegerValuesUtils.java
@@ -0,0 +1,52 @@
+package org.openl.util;
+
+import org.openl.meta.IntValue;
+import org.openl.meta.LongValue;
+import org.openl.meta.ShortValue;
+
+public class IntegerValuesUtils {
+    public static boolean isIntegerValue(Class<?> clazz) {
+        if (byte.class.equals(clazz)
+                || short.class.equals(clazz)
+                || int.class.equals(clazz)
+                || long.class.equals(clazz)
+                || Byte.class.equals(clazz)
+                || Short.class.equals(clazz)
+                || Integer.class.equals(clazz)
+                || Long.class.equals(clazz)
+                || ShortValue.class.equals(clazz)
+                || IntValue.class.equals(clazz)
+                || LongValue.class.equals(clazz)) {
+            return true;
+        }
+        return false;
+    }
+    
+    public static Object createNewObjectByType(Class<?> clazz, String value) {
+        if (byte.class.equals(clazz)) {
+            return Byte.valueOf(value).byteValue();
+        } else if (short.class.equals(clazz)) {
+            return Short.valueOf(value).shortValue();
+        } else if (int.class.equals(clazz)) {
+            return Integer.valueOf(value);
+        } else if (long.class.equals(clazz)) {
+            return Long.valueOf(value).longValue();
+        } else if (Byte.class.equals(clazz)) {
+            return Byte.valueOf(value);
+        } else if (Short.class.equals(clazz)) {
+            return Short.valueOf(value);
+        } else if (Integer.class.equals(clazz)) {
+            return Integer.valueOf(value);
+        } else if (Long.class.equals(clazz)) {
+            return Long.valueOf(value);
+        } else if (ShortValue.class.equals(clazz)) {
+            return new org.openl.meta.ShortValue((Short.valueOf(value).shortValue()));
+        } else if (IntValue.class.equals(clazz)) {
+            return new org.openl.meta.IntValue(Integer.valueOf(value).intValue());
+        } else if (LongValue.class.equals(clazz)) {
+            return new org.openl.meta.LongValue((Long.valueOf(value).longValue()));
+        }
+        
+        return null;
+    }
+}
