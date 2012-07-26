7eed49aa8b4cebfa7ea51afaeb30b939379e7e57
NSamatov@nsamatov.internal.corp <NSamatov@nsamatov.internal.corp>
2012-07-26 12:28:50 +0400

    EPBDS-3680 1) fix: In case of installig tomcat as service 'Edit in excel' function isn't work 2) fix: If OS is not Windows, 'Edit in excel' function will not work

--- STUDIO/trunk/org.openl.rules.webstudio/src/org/openl/rules/webstudio/util/ExcelLauncher.java
+++ STUDIO/trunk/org.openl.rules.webstudio/src/org/openl/rules/webstudio/util/ExcelLauncher.java
@@ -6,12 +6,21 @@
 
 package org.openl.rules.webstudio.util;
 
+import java.awt.Desktop;
+import java.io.File;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 /**
  * MS Excel launcher.
  * 
  * @author sam
  */
 public class ExcelLauncher {
+    private static final int LOCK_DETECT_TIMEOUT = 10000;
+
+    private final Log log = LogFactory.getLog(ExcelLauncher.class);
 
     private String scriptPath;
 
@@ -32,13 +41,77 @@ public class ExcelLauncher {
         if (range == null || range.equals("null")) {
             range = "A1";
         }
-        String[] cmdarray = { "wscript", scriptPath, workbookPath, workbookName, worksheetName == null ? "1" : worksheetName, range, };
 
-        Runtime.getRuntime().exec(cmdarray).waitFor();
+        String os = System.getProperty("os.name").toLowerCase();
+        if (os.contains("win")) {
+            String[] cmdarray = { "wscript", scriptPath, workbookPath, workbookName, worksheetName == null ? "1" : worksheetName, range, };
+
+            fixEnvironmentForExcel();
+            Process process = Runtime.getRuntime().exec(cmdarray);
+            Thread lockDetector = createLockDetector(process);
+
+            lockDetector.start();
+            process.waitFor();
+            lockDetector.interrupt();
+        } else {
+            if (Desktop.isDesktopSupported()) {
+                Desktop desktop = Desktop.getDesktop();
+                desktop.open(new File(workbookPath, workbookName));
+            } else {
+                throw new UnsupportedOperationException("Unsupported OS");
+            }
+        }
     }
 
     public static void launch(String scriptPath, String wbPath, String wbName, String wsName, String range) throws Exception {
         new ExcelLauncher(scriptPath, wbPath, wbName, wsName, range).launch();
+    }
+
+    /**
+     * Unfortunately this is a hack.
+     * See this link for details:
+     * http://www.codeproject.com/Questions/161160/Using-windows-service-to-open-an-Excel-file
+     */
+    private void fixEnvironmentForExcel() {
+        String winDir = System.getenv("windir");
+        String x64Path = winDir + "\\SysWow64\\Config\\SystemProfile";
+        String x32Path = winDir + "\\System32\\Config\\SystemProfile";
+        if (absentDesktop(x64Path)) {
+            new File(x64Path, "Desktop").mkdir();
+        }
+        if (absentDesktop(x32Path)) {
+            new File(x32Path, "Desktop").mkdir();
+        }
+    }
+
+    private boolean absentDesktop(String directoryPath) {
+        return exists(directoryPath) && !exists(directoryPath + "\\Desktop");
+    }
+
+    private boolean exists(String path) {
+        return new File(path).exists();
+    }
+
+    private Thread createLockDetector(final Process excelLaunchProcess) {
+        return new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(LOCK_DETECT_TIMEOUT);
+                    try {
+                        excelLaunchProcess.exitValue();
+                    } catch (IllegalThreadStateException e) {
+                        if (log.isErrorEnabled()) {
+                            log.error("ExcelLauncher is locked. Allow GUI interaction for service.");
+                        }
+                        excelLaunchProcess.destroy();
+                    }
+                } catch (InterruptedException e) {
+                    // do nothing
+                }
+
+            }
+        });
     }
 
 }
