9e8281ef23617b49673224f669398e4d810fbadd
PUdalau <PUdalau@noteimage.internal.corp>
2012-09-03 14:37:20 +0400

    EPBDS updated test repositories

--- WSFrontend/trunk/org.openl.rules.ruleservice/test/org/openl/rules/ruleservice/Constants.java
+++ WSFrontend/trunk/org.openl.rules.ruleservice/test/org/openl/rules/ruleservice/Constants.java
@@ -5,7 +5,7 @@ public final class Constants {
     private Constants() {
     }
 
-    public static final String VERSION = "0.0.1";
+    public static final String VERSION = "1";
 
     public static final String DEPLOYMENT_NAME = "org.openl.tablets.tutorial4";
 
