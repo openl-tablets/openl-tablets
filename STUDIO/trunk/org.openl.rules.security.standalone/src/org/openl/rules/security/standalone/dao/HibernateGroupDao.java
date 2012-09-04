987c9cc5578b1fb3208c06a61bcaf6d776a8398c
aastrouski <none@none>
2012-09-04 12:40:18 +0300

    EPBDS-3689, 3690 - User Management

--- STUDIO/trunk/org.openl.rules.security.standalone/src/org/openl/rules/security/standalone/dao/HibernateGroupDao.java
+++ STUDIO/trunk/org.openl.rules.security.standalone/src/org/openl/rules/security/standalone/dao/HibernateGroupDao.java
@@ -1,6 +1,8 @@
 package org.openl.rules.security.standalone.dao;
 
+import org.hibernate.criterion.Restrictions;
 import org.openl.rules.security.standalone.persistence.Group;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Hibernate implementation of {@link GroupDao}.
@@ -11,6 +13,12 @@ public class HibernateGroupDao extends BaseHibernateDao<Group> implements GroupD
 
     public HibernateGroupDao() {
         super(Group.class);
+    }
+
+    @Override
+    @Transactional
+    public Group getGroupByName(final String name) {
+        return (Group) getSession().createCriteria(Group.class).add(Restrictions.eq("name", name)).uniqueResult();
     }
 
 }
