38158f752f641a474a7d6040e895dd0f9188d639
NSamatov@nsamatov.internal.corp <NSamatov@nsamatov.internal.corp>
2012-10-19 19:06:10 +0400

    EPBDS-3990 Test displays as FAILED by actually it's Passed.

---
+++ DEV/trunk/org.openl.core/test/org/openl/binding/impl/OperatorsTest.java
@@ -0,0 +1,32 @@
+package org.openl.binding.impl;
+
+import static org.junit.Assert.*;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.junit.Test;
+
+public class OperatorsTest {
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+    @Test
+    public void testAddToDate() throws Exception {
+        Date date = Operators.add(dateFormat.parse("2012-10-01"), 120);
+        assertEquals("2013-01-29", dateFormat.format(date)); // date without time
+        assertEquals(dateFormat.parse("2013-01-29"), date); // date with time
+    }
+
+    @Test
+    public void testSubstractDays() throws Exception {
+        Date date = Operators.subtract(dateFormat.parse("2012-04-30"), 120);
+        assertEquals("2012-01-01", dateFormat.format(date)); // date without time
+        assertEquals(dateFormat.parse("2012-01-01"), date); // date with time
+    }
+
+    @Test
+    public void testSubtractDates() throws Exception {
+        int diff = Operators.subtract(dateFormat.parse("2013-01-29"), dateFormat.parse("2012-10-01"));
+        assertEquals(120, diff);
+    }
+}
