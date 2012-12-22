edabcfeb9204e9ebc49ccf578bed46f0436cd74c
AAstrouski <none@none>
2012-12-22 15:07:12 +0300

    EPBDS-4096 - Create project from Templates and Tutirials

---
+++ STUDIO/trunk/org.openl.rules.webstudio/src/org/openl/rules/webstudio/web/repository/project/ExcelFilesProjectCreator.java
@@ -0,0 +1,46 @@
+package org.openl.rules.webstudio.web.repository.project;
+
+import org.apache.commons.io.FilenameUtils;
+import org.openl.commons.web.jsf.FacesUtils;
+import org.openl.rules.common.ProjectException;
+import org.openl.rules.webstudio.web.repository.upload.AProjectCreator;
+import org.openl.rules.webstudio.web.repository.upload.RulesProjectBuilder;
+import org.openl.rules.workspace.uw.UserWorkspace;
+
+public class ExcelFilesProjectCreator extends AProjectCreator {
+
+    private ProjectFile[] files;
+
+    public ExcelFilesProjectCreator(String projectName, UserWorkspace userWorkspace, ProjectFile... files) {
+        super(projectName, userWorkspace);
+        this.files = files;
+    }
+
+    @Override
+    protected RulesProjectBuilder getProjectBuilder() throws ProjectException {
+        RulesProjectBuilder projectBuilder = new RulesProjectBuilder(getUserWorkspace(), getProjectName());
+
+        if (files != null) {
+            for (ProjectFile file : files) {
+                try {
+                    if (checkFileSize(file)) {
+                        projectBuilder.addFile(FilenameUtils.getName(file.getName()), file.getInput());
+                    }
+                } catch (Exception e) {
+                    FacesUtils.addWarnMessage("Problem with file " + file.getName() + ". " + e.getMessage());
+                }
+            }
+        }
+
+        return projectBuilder;
+    }
+
+    private boolean checkFileSize(ProjectFile file) {
+        if (file.getSize() > 100 * 1024 * 1024) {
+            FacesUtils.addErrorMessage("Size of the file " + file.getName() + " is more then 100MB.");
+            return false;
+        }
+        return true;
+    }
+
+}
