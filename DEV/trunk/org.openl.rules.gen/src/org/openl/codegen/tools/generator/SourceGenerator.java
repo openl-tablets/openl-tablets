da4c4b1c3ac867df34aa1c193cfe14418eb4303d
AAstrouski <AAstrouski@aastrouski.internal.corp>
2013-02-12 17:15:31 +0300

    IntelliJ IDEA 12+ support

--- DEV/trunk/org.openl.rules.gen/src/org/openl/codegen/tools/generator/SourceGenerator.java
+++ DEV/trunk/org.openl.rules.gen/src/org/openl/codegen/tools/generator/SourceGenerator.java
@@ -1,5 +1,7 @@
 package org.openl.codegen.tools.generator;
 
+import org.apache.commons.io.IOUtils;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -7,9 +9,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Map;
 import java.util.Properties;
-
-import org.apache.poi.util.IOUtils;
-
 
 public class SourceGenerator {
 
