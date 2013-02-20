2dcd239bea3886a4288eea7161675bc775fc973e
AAstrouski <AAstrouski@localhost>
2013-02-20 15:23:38 +0300

    Page loading optimization - Compress CSS files, Update CSS, Minor fixes

--- STUDIO/trunk/org.openl.rules.webstudio/webapps/webstudio/javascript/common.js
+++ STUDIO/trunk/org.openl.rules.webstudio/webapps/webstudio/javascript/common.js
@@ -10,3 +10,21 @@ function focusElement(elementId) {
     field.focus();
   }
 }
+
+function changeAllItemStatus(element, areaId) {
+    $j("#"+areaId+" INPUT[type='checkbox']:not(:disabled)").prop("checked", element.checked);
+}
+
+function changeItemStatus(element, areaId, selectAllElemId) {
+    if(!element.checked && $(selectAllElemId).checked) {
+        $(selectAllElemId).checked = false;
+    }
+
+    checkedCount = $j("#"+areaId+" INPUT[type='checkbox']:checked").not("INPUT[id='"+selectAllElemId+"']").size();
+    disabledCount = $j("#"+areaId+" INPUT[type='checkbox']:disabled").not("INPUT[id='"+selectAllElemId+"']").size();
+    allCount = $j("#"+areaId+" INPUT[type='checkbox']").not("INPUT[id='"+selectAllElemId+"']").size();
+
+    if (checkedCount + disabledCount == allCount && checkedCount > 0) {
+        $(selectAllElemId).checked = true;
+    }
+}
