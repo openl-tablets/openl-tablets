6264f1164a6251558373de3ddc55c52344ea1aa1
Aliaksei Bahdanau <none@none>
2013-03-01 12:17:45 +0300

    Fix of 10118 rev merge. Incorrect merge.

--- DEV/trunk/org.openl.rules/src/org/openl/rules/calc/result/convertor/CalculationStep.java
+++ DEV/trunk/org.openl.rules/src/org/openl/rules/calc/result/convertor/CalculationStep.java
@@ -1,11 +1,15 @@
 package org.openl.rules.calc.result.convertor;
 
+import java.io.Serializable;
+
 /**
  * The base abstraction for the spreadsheet step, each step is a spreadsheet row.
  *
  */
-public class CalculationStep {
+public class CalculationStep implements Serializable{
 	
+    private static final long serialVersionUID = 4067908093788043935L;
+    
     /** step name*/
 	private String stepName;
 
