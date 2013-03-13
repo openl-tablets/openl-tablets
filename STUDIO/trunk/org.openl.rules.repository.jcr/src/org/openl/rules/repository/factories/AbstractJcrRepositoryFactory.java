db95a25142734da2df81eda69dd8a13ac4481337
Pavel Tarasevich <Pavel.Tarasevich@exigenservices.com>
2013-03-14 02:54:33 +0300

    EPBDS-3567 Display error message in case user use old repository which cannot be resolved in new version

--- STUDIO/trunk/org.openl.rules.repository.jcr/src/org/openl/rules/repository/factories/AbstractJcrRepositoryFactory.java
+++ STUDIO/trunk/org.openl.rules.repository.jcr/src/org/openl/rules/repository/factories/AbstractJcrRepositoryFactory.java
@@ -176,12 +176,16 @@ public abstract class AbstractJcrRepositoryFactory implements RRepositoryFactory
     /** {@inheritDoc} */
     public RRepository getRepositoryInstance() throws RRepositoryException {
         if(rulesRepository == null){
-            rulesRepository = createRepository();
+            rulesRepository = createJcrRepository();
         }
         return rulesRepository;
     }
 
     protected RRepository createRepository() throws RRepositoryException {
+        return createJcrRepository();
+    }
+
+    protected RRepository createJcrRepository() throws RRepositoryException {
         try {
             // FIXME: do not hardcode credential info
             Session session = createSession("user", "pass");
