b834137d76f7a00cb823e2f276d72dace7a15793
abahdanau <abahdanau@localhost>
2013-04-02 19:55:57 +0300

    EPBDS-4476 Add validation for Project and Module name on forbidden symbols (Linux symbols + Windows symbols + JCR symbols)

--- STUDIO/trunk/org.openl.rules.webstudio/src/org/openl/rules/webstudio/web/repository/LocalUploadController.java
+++ STUDIO/trunk/org.openl.rules.webstudio/src/org/openl/rules/webstudio/web/repository/LocalUploadController.java
@@ -1,22 +1,24 @@
 package org.openl.rules.webstudio.web.repository;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.servlet.http.HttpSession;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.openl.commons.web.jsf.FacesUtils;
 import org.openl.rules.common.ProjectException;
 import org.openl.rules.ui.WebStudio;
+import org.openl.rules.webstudio.util.NameChecker;
 import org.openl.rules.webstudio.web.servlet.RulesUserSession;
 import org.openl.rules.webstudio.web.util.WebStudioUtils;
 import org.openl.rules.workspace.WorkspaceException;
 import org.openl.rules.workspace.dtr.DesignTimeRepository;
-
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
-import javax.servlet.http.HttpSession;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
 
 @ManagedBean(name="localUpload")
 @RequestScoped
@@ -114,9 +116,15 @@ public class LocalUploadController {
                         FacesUtils.addInfoMessage("Project " + bean.getProjectName()
                                         + " was created successfully");
                     } catch (Exception e) {
-                        String msg = "Failed to create the project '" + bean.getProjectName() + "'!";
-                        log.error(msg, e);
-                        FacesUtils.addErrorMessage(msg, e.getMessage());
+                        if (!NameChecker.checkName(bean.getProjectName())) {
+                            String msg = "Failed to create the project '" + bean.getProjectName() + "'! " + NameChecker.BAD_PROJECT_NAME_MSG;
+                            FacesUtils.addErrorMessage(msg);
+                        } else {
+                            String msg = "Failed to create the project '" + bean.getProjectName() + "'!";
+                            log.error(msg, e);
+                            FacesUtils.addErrorMessage(msg, e.getMessage());
+                        }
+                        
                     }
                 }
             }
