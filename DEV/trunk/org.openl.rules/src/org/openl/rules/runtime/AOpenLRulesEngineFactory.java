fc26d563e7006c98a44e8ad41b984640e4f716d2
NSamatov@nsamatov.internal.corp <NSamatov@nsamatov.internal.corp>
2013-04-10 10:31:54 +0400

    EBDS-4474 Memory leak in ThreadLocals usage

--- DEV/trunk/org.openl.rules/src/org/openl/rules/runtime/AOpenLRulesEngineFactory.java
+++ DEV/trunk/org.openl.rules/src/org/openl/rules/runtime/AOpenLRulesEngineFactory.java
@@ -26,12 +26,7 @@ public abstract class AOpenLRulesEngineFactory extends AOpenLEngineFactory {
 
     @Override
     protected ThreadLocal<IRuntimeEnv> initRuntimeEnvironment() {
-        return new ThreadLocal<org.openl.vm.IRuntimeEnv>() {
-            @Override
-            protected org.openl.vm.IRuntimeEnv initialValue() {
-                return new SimpleRulesVM().getRuntimeEnv();
-            }
-        };
+        return new RuntimeEnvHolder();
 
     }
     
@@ -39,5 +34,14 @@ public abstract class AOpenLRulesEngineFactory extends AOpenLEngineFactory {
     protected InvocationHandler makeInvocationHandler(Object openClassInstance, Map<Method, IOpenMember> methodMap,
             IRuntimeEnv runtimeEnv) {
         return new OpenLRulesInvocationHandler(openClassInstance, this, runtimeEnv, methodMap);
+    }
+
+    // ThreadLocals can be cached by servlet container. RuntimeEnvHolder should
+    // be nested class, not inner class - otherwise we get memory leak
+    private static final class RuntimeEnvHolder extends ThreadLocal<org.openl.vm.IRuntimeEnv> {
+        @Override
+        protected org.openl.vm.IRuntimeEnv initialValue() {
+            return new SimpleRulesVM().getRuntimeEnv();
+        }
     }
 }
