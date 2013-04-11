cce37c2af4f39047f9363ab9953ee88f861dbd52
abahdanau <abahdanau@localhost>
2013-04-11 13:38:48 +0300

    EPBDS-4460 [Table Wizard] Cannot create Aliase datatype table with numeric datatype by using wizard

--- STUDIO/trunk/org.openl.rules.webstudio/src/org/openl/rules/ui/tablewizard/AliasValue.java
+++ STUDIO/trunk/org.openl.rules.webstudio/src/org/openl/rules/ui/tablewizard/AliasValue.java
@@ -1,6 +1,5 @@
 package org.openl.rules.ui.tablewizard;
 
-import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.NotBlank;
 
@@ -10,7 +9,6 @@ import org.hibernate.validator.constraints.NotBlank;
 public class AliasValue {
 
     @NotBlank(message="Can not be empty")
-    @Pattern(regexp="([a-zA-Z_][a-zA-Z_0-9]*)?", message="Invalid value")
     private String value;
 
     public String getValue() {
