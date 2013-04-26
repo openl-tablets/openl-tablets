44b60b89fb71a7d217451b0b152eba97f72d1909
Marat Kamalov <none@none>
2013-04-26 10:46:43 +0400

    Calculation steps and convertors were extracted to separate module.

--- DEV/trunk/org.openl.rules.calculation.result/src/org/openl/rules/calculation/result/convertor/CodeStep.java
+++ DEV/trunk/org.openl.rules.calculation.result/src/org/openl/rules/calculation/result/convertor/CodeStep.java
@@ -1,26 +1,25 @@
-package org.openl.rules.calc.result.convertor;
-
+package org.openl.rules.calculation.result.convertor;
 
 /**
  * Spreadsheet step(row) that has the code value.
  * 
  * @author DLiauchuk
- *
+ * 
  */
-@Deprecated
 public class CodeStep extends CalculationStep {
-    
+
     private static final long serialVersionUID = 7372598798002605558L;
-    
+
     private String code;
-    
+
     public String getCode() {
         return code;
     }
+
     public void setCode(String code) {
         this.code = code;
     }
-    
+
     @Override
     public String toString() {
         StringBuffer stringBuffer = new StringBuffer();
--- DEV/trunk/org.openl.rules.calculation.result/src/org/openl/rules/calculation/result/convertor/SimpleStep.java
+++ DEV/trunk/org.openl.rules.calculation.result/src/org/openl/rules/calculation/result/convertor/SimpleStep.java
@@ -1,43 +1,41 @@
-package org.openl.rules.calc.result.convertor;
-
+package org.openl.rules.calculation.result.convertor;
 
 /**
- * Spreadsheet row(step) that has formula and value as column values. 
+ * Spreadsheet row(step) that has formula and value as column values.
  * 
  * @author DLiauchuk
- *
+ * 
  */
-@Deprecated
 public class SimpleStep extends CodeStep {
-    
+
     private static final long serialVersionUID = 3906469763279262117L;
-    
+
     private Double formula;
     private Double value;
     private String text;
-    
+
     public Double getFormula() {
         return formula;
     }
-    
+
     public void setFormula(Double formula) {
         this.formula = formula;
     }
-    
+
     public Double getValue() {
         return value;
     }
-    
+
     public void setValue(Double value) {
         this.value = value;
     }
-    
+
     public String getText() {
         return text;
     }
-    
+
     public void setText(String text) {
         this.text = text;
     }
-    
+
 }
