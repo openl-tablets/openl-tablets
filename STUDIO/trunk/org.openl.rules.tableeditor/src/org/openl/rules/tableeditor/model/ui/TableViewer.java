6ac7610258038813e76d2bccf1d93cfc057d540e
aabramova@kznaabramova.internal.corp <aabramova@kznaabramova.internal.corp>
2013-05-28 21:10:19 +0400

    EPBDS-4636 Incorrect element at the test result screen
    --HG--
    branch : 5.10.x

--- STUDIO/trunk/org.openl.rules.tableeditor/src/org/openl/rules/tableeditor/model/ui/TableViewer.java
+++ STUDIO/trunk/org.openl.rules.tableeditor/src/org/openl/rules/tableeditor/model/ui/TableViewer.java
@@ -133,7 +133,7 @@ public class TableViewer {
     }
 
     private boolean image(String formattedValue) {
-        return formattedValue.matches(".*<img .*>.*");
+        return formattedValue.replaceAll("\n", "").matches(".*<img .*>.*");
     }
 
     private boolean link(String formattedValue) {
