2262cac6b431501b3dbf471081101b8bf7b3703d
Nail Samatov <nail.samatov@exigenservices.com>
2013-05-30 18:23:06 +0400

    EPBDS-4638 Fix failed test

--- DEV/trunk/org.openl.rules/test/org/openl/rules/lang/xls/XlsWorkbookSourceCodeModuleTest.java
+++ DEV/trunk/org.openl.rules/test/org/openl/rules/lang/xls/XlsWorkbookSourceCodeModuleTest.java
@@ -1,5 +1,6 @@
 package org.openl.rules.lang.xls;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.mockito.Matchers.any;
@@ -7,6 +8,7 @@ import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.MalformedURLException;
@@ -14,13 +16,22 @@ import java.net.MalformedURLException;
 import org.apache.commons.io.FileUtils;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.openl.source.IOpenSourceCodeModule;
 import org.openl.source.impl.URLSourceCodeModule;
 
-public class XlsWorkbookSourceCodeModuleTest{
+public class XlsWorkbookSourceCodeModuleTest {
 
     private static final String TEST_FILE_NAME = "testCorruptedFile.xls";
+    private static final String TEXT = "test";
+
+    @Before
+    public void init() throws Exception {
+        OutputStream os = new FileOutputStream(TEST_FILE_NAME);
+        os.write(TEXT.getBytes());
+        os.close();
+    }
 
     @Test
     public void testUrlWithWhiteSpaces() {
@@ -32,7 +43,6 @@ public class XlsWorkbookSourceCodeModuleTest{
             assertFalse(true);
         }
     }
-
 
     @Test
     public void testFileIsNotCorrupted() throws IOException {
@@ -46,7 +56,9 @@ public class XlsWorkbookSourceCodeModuleTest{
         } catch (OutOfMemoryError ignored) {
         }
 
-        assertFalse("File should not be created or cleared if there are no actual write operations", new File(TEST_FILE_NAME).exists());
+        assertEquals("File should not cleared if there are no actual write operations",
+            TEXT.getBytes().length,
+            new File(TEST_FILE_NAME).length());
     }
 
     @After
