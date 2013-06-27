2aa24835813e428ab99c4fb84e9133115fd5cfc3
Marat Kamalov <none@none>
2013-06-27 19:38:27 +0400

    EPBDS-4654 Demo fix.

--- DEMO/trunk/openl-tablets-demo/org.openl.rules.demo.tut4app/src/main/java/org/openl/tablets/tutorial4/client/WebServiceTemplate.java
+++ DEMO/trunk/openl-tablets-demo/org.openl.rules.demo.tut4app/src/main/java/org/openl/tablets/tutorial4/client/WebServiceTemplate.java
@@ -2,14 +2,14 @@ package org.openl.tablets.tutorial4.client;
 
 import org.apache.cxf.aegis.databinding.AegisDatabinding;
 import org.apache.cxf.frontend.ClientProxyFactoryBean;
-import org.openl.tablets.tutorial4.Tutorial_4Wrapper;
+import org.openl.tablets.tutorial4.Tutorial_4WrapperInterface;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
 public class WebServiceTemplate {
-	private Tutorial_4Wrapper clientInterface;
+	private Tutorial_4WrapperInterface clientInterface;
 
 	private static final WebServiceTemplate INSTANCE = new WebServiceTemplate();
 
@@ -19,15 +19,15 @@ public class WebServiceTemplate {
 		return INSTANCE;
 	}
 
-    private Tutorial_4Wrapper createClientInterface() throws IOException {
+    private Tutorial_4WrapperInterface createClientInterface() throws IOException {
         ClientProxyFactoryBean factory = new ClientProxyFactoryBean();
-        factory.setServiceClass(Tutorial_4Wrapper.class);
+        factory.setServiceClass(Tutorial_4WrapperInterface.class);
         factory.setAddress(getAddress());
         factory.setDataBinding(new AegisDatabinding());
-        return (Tutorial_4Wrapper) factory.create();
+        return (Tutorial_4WrapperInterface) factory.create();
     }
 
-	private synchronized Tutorial_4Wrapper getClientInterface() throws IOException {
+	private synchronized Tutorial_4WrapperInterface getClientInterface() throws IOException {
 		if (clientInterface == null) {
 			return clientInterface = createClientInterface();
 		}
