89f37619456b1e394eb495b77e016b5e6696f89a
Marat Kamalov <none@none>
2013-07-17 19:21:16 +0400

    Performance issue with unpacking jars was fixed. (AAA team spend time for waiting on rating start)
    --HG--
    branch : 5.9.3.x

--- WSFrontend/trunk/org.openl.rules.ruleservice/src/org/openl/rules/ruleservice/loader/UnpackClasspathJarToDirectoryBean.java
+++ WSFrontend/trunk/org.openl.rules.ruleservice/src/org/openl/rules/ruleservice/loader/UnpackClasspathJarToDirectoryBean.java
@@ -1,5 +1,7 @@
 package org.openl.rules.ruleservice.loader;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -76,33 +78,52 @@ public class UnpackClasspathJarToDirectoryBean implements InitializingBean {
         if (jarPath.lastIndexOf("!") == -1) {
             return null;
         }
-        return jarPath.substring("file:".length(), jarPath.lastIndexOf("!"));
+        String path = jarPath.substring("file:".length(), jarPath.lastIndexOf("!"));
+
+        // Workaround for WebSphere 8.5
+        path = path.replaceAll("%20", " ");
+
+        return path;
     }
 
     private static void unpack(File jarFile, String destDir) throws IOException {
         File newProjectDir = new File(destDir, FilenameUtils.getBaseName(jarFile.getCanonicalPath()));
         newProjectDir.mkdirs();
 
-        JarFile jar = new JarFile(jarFile);
+        JarFile jar = null;
+        try {
+            jar = new JarFile(jarFile);
 
-        Enumeration<JarEntry> e = jar.entries();
-        while (e.hasMoreElements()) {
-            JarEntry file = e.nextElement();
-            File f = new File(newProjectDir, file.getName());
-            if (file.isDirectory()) {
-                f.mkdir();
-                continue;
-            }
-            InputStream is = jar.getInputStream(file);
+            Enumeration<JarEntry> e = jar.entries();
+            while (e.hasMoreElements()) {
+                JarEntry file = e.nextElement();
+                File f = new File(newProjectDir, file.getName());
+                if (file.isDirectory()) {
+                    f.mkdir();
+                    continue;
+                }
 
-            FileOutputStream fos = new FileOutputStream(f);
-            while (is.available() > 0) {
-                fos.write(is.read());
+                InputStream is = jar.getInputStream(file);
+                InputStream bufferedInputStream = new BufferedInputStream(is);
+
+                FileOutputStream fos = new FileOutputStream(f);
+                BufferedOutputStream bos = new BufferedOutputStream(fos);
+                int data;
+                while ((data = bufferedInputStream.read()) != -1) {
+                    bos.write(data);
+                }
+                bos.close();
+                bufferedInputStream.close();
             }
-            fos.close();
-            is.close();
+        } finally {
+            if (jar != null){
+                try{
+                    jar.close();
+                }catch(IOException e){
+                   
+                }
+            }
         }
-
     }
 
     private static boolean checkOrCreateFolder(File location) {
