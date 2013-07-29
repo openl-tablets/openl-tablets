c3d06b985fc1d22143492f1f6f3adff520ebe7ce
Marat Kamalov <Marat Kamalov@localhost>
2013-07-29 23:28:08 +0400

    EPBDS-4631 Header doesn't merged for all column Table Parts. Bug was fixed.

--- DEV/trunk/org.openl.rules/src/org/openl/rules/table/CompositeGrid.java
+++ DEV/trunk/org.openl.rules/src/org/openl/rules/table/CompositeGrid.java
@@ -6,6 +6,8 @@ package org.openl.rules.table;
 import java.util.ArrayList;
 import java.util.HashSet;
 
+import org.openl.rules.table.properties.PropertiesHelper;
+
 /**
  * An {@link IGrid} implementation that composes several {@link IGridTable}
  * together.<br>
@@ -43,11 +45,11 @@ public class CompositeGrid extends AGrid {
     public IGridTable[] getGridTables() {
         return gridTables;
     }
-    
+
     public IGridRegion getMappedRegion(int i) {
         return mappedRegions[i];
     };
-    
+
     /**
      * 
      * @param tables Tables to be composed.
@@ -65,13 +67,57 @@ public class CompositeGrid extends AGrid {
     }
 
     public ICell getCell(int column, int row) {
+        if (!vertical) { // Merge header for horizontal table parts
+            Transform t = transform(0, 0);
+            if (t == null){
+                return null;
+            }
+            ICell delegate = t.grid().getCell(t.getCol(), t.getRow());
+            if (row < delegate.getHeight()){
+                IGridRegion reg = getRegionContaining(0, 0);
+                IGridRegion region = null;
+                if (reg != null){
+                    region = new GridRegion(reg.getTop(), reg.getLeft(), reg.getBottom(),
+                            reg.getLeft() + getWidth() - 1);
+                }else{
+                    region = new GridRegion(row, column, row,
+                            column + getWidth() - 1);
+                }
+                return new CompositeCell(column, row, region, delegate);
+            }else{
+                Transform t1 = transform(0, delegate.getHeight());//Properties parsing and merge
+                if (t1 != null){
+                    ICell delegate1 = t1.grid().getCell(t1.getCol(), t1.getRow());
+                    if (row < delegate.getHeight() + delegate1.getHeight() && PropertiesHelper.PROPERTIES_HEADER.equals(delegate1.getFormattedValue())){
+                        Transform t2 = transform(delegate1.getWidth(), row);
+                        if (t2 != null){
+                            ICell delegate2 = t2.grid().getCell(t2.getCol(), t2.getRow());
+                            Transform t3 = transform(delegate1.getWidth() + delegate2.getWidth(), row);
+                            if (t3 != null){
+                                ICell delegate3 = t3.grid().getCell(t3.getCol(), t3.getRow());
+                                if (column >= delegate1.getWidth() + delegate2.getWidth()){
+                                    IGridRegion reg = getRegionContaining(delegate1.getWidth() + delegate2.getWidth(), row);
+                                    IGridRegion region = null;
+                                    if (reg != null){
+                                        region = new GridRegion(reg.getTop(), reg.getLeft(), reg.getBottom(),
+                                            reg.getLeft() + getWidth() - 1 - (delegate1.getWidth() + delegate2.getWidth()));
+                                    }else{
+                                        region = new GridRegion(row, column, row,
+                                                column + getWidth() - 1 - (delegate1.getWidth() + delegate2.getWidth()));
+                                    }
+                                    return new CompositeCell(column, row, region, delegate3);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
         Transform t = transform(column, row);
         if (t == null) {
             return null;
         }
-
         ICell delegate = t.grid().getCell(t.getCol(), t.getRow());
-
         return new CompositeCell(column, row, getRegionContaining(column, row), delegate);
     }
 
