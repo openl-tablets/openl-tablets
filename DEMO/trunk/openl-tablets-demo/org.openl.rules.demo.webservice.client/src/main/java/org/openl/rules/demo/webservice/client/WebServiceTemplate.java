367765c276ea018958393cd7b658930e64a83cab
AAstrouski <AAstrouski@aastrouski.internal.corp>
2013-10-11 17:58:42 +0300

    EPBDS-4789 - Remove Common repository EPBDS-4753 - Change old tutorials in Demo

---
+++ DEMO/trunk/openl-tablets-demo/org.openl.rules.demo.webservice.client/src/main/java/org/openl/rules/demo/webservice/client/WebServiceTemplate.java
@@ -0,0 +1,45 @@
+package org.openl.rules.demo.webservice.client;
+
+import org.apache.cxf.endpoint.Client;
+import org.apache.cxf.jaxws.endpoint.dynamic.JaxWsDynamicClientFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class WebServiceTemplate {
+
+    private Client clientInterface;
+
+	private static final WebServiceTemplate INSTANCE = new WebServiceTemplate();
+
+	private WebServiceTemplate(){}
+
+	public static WebServiceTemplate getInstance() {
+		return INSTANCE;
+	}
+
+    private Client createClientInterface() throws IOException {
+        JaxWsDynamicClientFactory clientFactory = JaxWsDynamicClientFactory.newInstance();
+        return clientFactory.createClient(getAddress() + "?wsdl");
+    }
+
+    public synchronized Client getClientInterface() throws IOException {
+    	if (clientInterface == null) {
+    		return clientInterface = createClientInterface();
+    	}
+    	return clientInterface;
+    }
+
+	public String getAddress() throws IOException {
+		Properties properties = new Properties();
+		InputStream stream = getClass().getResourceAsStream("/ws.properties");
+		try {
+			properties.load(stream);
+			return properties.getProperty("ws.address");
+		} finally {
+			stream.close();
+		}
+	}
+
+}
