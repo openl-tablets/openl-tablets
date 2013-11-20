dedbda08e52d32d8061334ece2c2a43715a17c2c
aastrouski <aastrouski@localhost>
2013-11-20 16:19:36 +0300

    Remove

--- Docs/sf.web/sf-site-tmp.bat
+++
@@ -1,6 +0,0 @@
-path
-cd workspace/sf.web
-
-"C:\Program Files\PuTTY\psftp" -b sf-site-tmp.ftp -bc snshor@shell.sourceforge.net
- 
-pause
--- Docs/sf.web/sf-site.bat
+++
@@ -1,9 +0,0 @@
-
-cd workspace\sf.web
-
-rem psftp -b sf-site.ftp -bc snshor@shell.sourceforge.net
-psftp -b sf-site.ftp snshor,openl-tablets@web.sourceforge.net
-
-
- 
-pause
