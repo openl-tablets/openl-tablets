dca93bec5dbf24354183c507d0fa4fed4d6921c7
aastrouski <none@none>
2013-12-02 16:34:44 +0300

    EPBDS-4829 - Update Rules Configuration UI

--- STUDIO/org.openl.rules.webstudio/src/org/openl/rules/ui/WebStudio.java
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/ui/WebStudio.java
@@ -199,13 +199,10 @@ public class WebStudio {
         return benchmarks.toArray(new BenchmarkInfoView[benchmarks.size()]);
     }
 
-    /**
-     * TODO Hold current project in session
-     * */
     public RulesProject getCurrentProject(HttpSession session) {
-        if (currentModule != null) {
+        if (currentProject != null) {
             try {
-                String projectFolder = currentModule.getProject().getProjectFolder().getName();
+                String projectFolder = currentProject.getProjectFolder().getName();
                 RulesUserSession rulesUserSession = WebStudioUtils.getRulesUserSession(session);
                 RulesProject project = rulesUserSession.getUserWorkspace().getProject(projectFolder);
                 return project;
@@ -348,13 +345,14 @@ public class WebStudio {
 
         currentProject = getProjectByName(name);
 
-        if (currentProject != null && getAllProjects().size() > 0) {
+        if (currentProject == null && getAllProjects().size() > 0) {
             currentProject = getAllProjects().get(0);
         }
     }
 
-    public void selectModule(String projectId, String moduleName) throws Exception {
-        if (StringUtils.isBlank(projectId) || StringUtils.isBlank(moduleName)) {
+    public void
+    selectModule(String projectName, String moduleName) throws Exception {
+        if (StringUtils.isBlank(projectName) || StringUtils.isBlank(moduleName)) {
             if (currentModule != null) {
                 return;
             }
@@ -365,7 +363,12 @@ public class WebStudio {
             return;
         }
 
-        ProjectDescriptor project = getProject(projectId);
+        ProjectDescriptor project;
+        if (currentProject != null && projectName.equals(currentProject.getName())) {
+            project = currentProject;
+        } else {
+            project = getProjectByName(projectName);
+        }
         if (project != null) {
             for (Module module : project.getModules()) {
                 if (module.getName().equals(moduleName)) {
@@ -446,7 +449,10 @@ public class WebStudio {
             model.setModuleInfo(module);
             model.getRecentlyVisitedTables().clear();
         }
+
         currentModule = module;
+        currentProject = currentModule != null ? currentModule.getProject() : null;
+
         for (StudioListener listener : listeners) {
             listener.studioReset();
         }
