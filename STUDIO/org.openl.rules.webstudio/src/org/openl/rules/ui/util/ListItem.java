624d20e8424fb56f40ec890519166d4b02867bbe
aastrouski <aastrouski@localhost>
2013-12-12 19:02:37 +0300

    EPBDS-4852 - Manage project dependencies

---
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/ui/util/ListItem.java
@@ -0,0 +1,33 @@
+package org.openl.rules.ui.util;
+
+/**
+ * Created by Andrei Ostrovski on 10.12.13.
+ */
+public class ListItem<T> {
+
+    private boolean selected;
+    private T item;
+
+    public ListItem() {}
+
+    public ListItem(boolean selected, T item) {
+        this.selected = selected;
+        this.item = item;
+    }
+
+    public boolean isSelected() {
+        return selected;
+    }
+
+    public void setSelected(boolean selected) {
+        this.selected = selected;
+    }
+
+    public T getItem() {
+        return item;
+    }
+
+    public void setItem(T dependency) {
+        this.item = dependency;
+    }
+}
