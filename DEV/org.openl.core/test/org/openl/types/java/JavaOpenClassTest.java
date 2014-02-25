a4dc58fee6e31ecbb7661755fb9b0e3548868578
Nail Samatov <nsamatov@exigeninsurance.com>
2014-02-25 12:15:05 +0400

    EPBDS-5024 Fixed bug: Sometimes DoubleValue.isSimple() returns false

--- DEV/org.openl.core/test/org/openl/types/java/JavaOpenClassTest.java
+++ DEV/org.openl.core/test/org/openl/types/java/JavaOpenClassTest.java
@@ -5,25 +5,43 @@ import static org.junit.Assert.*;
 import java.util.List;
 
 import org.junit.Test;
+import org.openl.meta.DoubleValue;
 import org.openl.meta.StringValue;
 import org.openl.types.IOpenClass;
 
 public class JavaOpenClassTest {
-    
+
     @Test
     public void testGetComponentType() {
         assertEquals(JavaOpenClass.getOpenClass(String.class), JavaOpenClass.getOpenClass(String[].class).getComponentClass());
-        
+
         assertEquals(JavaOpenClass.getOpenClass(String[].class), JavaOpenClass.getOpenClass(String[][].class).getComponentClass());
-        
+
         assertNull(JavaOpenClass.getOpenClass(int.class).getComponentClass());
-        
+
         assertEquals(JavaOpenClass.OBJECT, JavaOpenClass.getOpenClass(List.class).getComponentClass());
     }
-    
+
     @Test
     public void testIsSimple() {
-    	IOpenClass clazz = JavaOpenClass.getOpenClass(StringValue.class);
-    	assertTrue(clazz.isSimple());
+        IOpenClass clazz = JavaOpenClass.getOpenClass(StringValue.class);
+        assertTrue(clazz.isSimple());
     }
+
+    @Test
+    public void testResetClassLoader() throws Exception {
+        IOpenClass doubleValue = JavaOpenClass.getOpenClass(DoubleValue.class);
+        IOpenClass myType = JavaOpenClass.getOpenClass(MyType.class);
+
+        assertSame(doubleValue, JavaOpenClass.getOpenClass(DoubleValue.class));
+        assertSame(myType, JavaOpenClass.getOpenClass(MyType.class));
+
+        JavaOpenClass.resetClassloader(DoubleValue.class.getClassLoader());
+        JavaOpenClass.resetClassloader(Exception.class.getClassLoader());
+
+        assertSame(doubleValue, JavaOpenClass.getOpenClass(DoubleValue.class));
+        assertNotSame(myType, JavaOpenClass.getOpenClass(MyType.class));
+    }
+
+    private static class MyType {}
 }
