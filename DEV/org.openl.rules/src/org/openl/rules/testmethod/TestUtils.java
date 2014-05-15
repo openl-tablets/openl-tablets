0d2b931db0c6cbe97f4936e038c7a2024bca6cd5
aastrouski <none@none>
2014-05-15 18:12:53 +0300

    EPBDS-5212 - Testing tool performance: make lazy loading of test cases Object input parameters on Run and Run All tests

--- DEV/org.openl.rules/src/org/openl/rules/testmethod/TestUtils.java
+++ DEV/org.openl.rules/src/org/openl/rules/testmethod/TestUtils.java
@@ -1,9 +1,17 @@
 package org.openl.rules.testmethod;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.exception.ExceptionUtils;
+import org.openl.exception.OpenLException;
+import org.openl.message.OpenLErrorMessage;
+import org.openl.message.OpenLMessage;
+import org.openl.message.OpenLMessagesUtils;
 import org.openl.rules.context.IRulesRuntimeContext;
+import org.openl.syntax.exception.CompositeSyntaxNodeException;
 import org.openl.types.IParameterDeclaration;
 
 public final class TestUtils {
@@ -25,6 +33,49 @@ public final class TestUtils {
         }
 
         return params.toArray(new ParameterWithValueDeclaration[params.size()]);
+    }
+
+    public static List<OpenLMessage> getUserMessagesAndErrors(Object error) {
+        if (error instanceof Throwable) {
+            Throwable exception = (Throwable) error;
+            exception = ExceptionUtils.getRootCause(exception);
+            List<OpenLMessage> messages = new ArrayList<OpenLMessage>();
+
+            if (exception instanceof OpenLUserRuntimeException) {
+                OpenLUserRuntimeException userException = (OpenLUserRuntimeException) exception;
+                messages.add(new OpenLMessage(userException.getOriginalMessage(), StringUtils.EMPTY));
+            } else if (exception instanceof CompositeSyntaxNodeException) {
+                CompositeSyntaxNodeException compositeException = (CompositeSyntaxNodeException) exception;
+
+                for (OpenLException openLException : compositeException.getErrors()) {
+                    if (openLException instanceof OpenLUserRuntimeException) {
+                        OpenLUserRuntimeException userException = (OpenLUserRuntimeException) openLException;
+                        messages.add(new OpenLMessage(userException.getOriginalMessage(), StringUtils.EMPTY));
+                    } else {
+                        messages.add(new OpenLErrorMessage(openLException));
+                    }
+                }
+
+            } else {
+                if (exception instanceof OpenLException) {
+                    messages.add(new OpenLErrorMessage((OpenLException) exception));
+                } else {
+                    messages.add(new OpenLErrorMessage(ExceptionUtils.getRootCauseMessage(exception), StringUtils.EMPTY));
+                }
+            }
+
+            return messages;
+        }
+
+        return Collections.emptyList();
+    }
+
+    public static List<OpenLMessage> getErrors(Object error) {
+        if (error instanceof Throwable) {
+            return OpenLMessagesUtils.newMessages((Throwable) error);
+        }
+
+        return Collections.emptyList();
     }
 
 }
--- STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/web/test/TestResultsHelper.java
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/web/test/TestResultsHelper.java
@@ -1,23 +1,16 @@
 package org.openl.rules.webstudio.web.test;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import org.apache.commons.lang.StringUtils;
-import org.apache.commons.lang.exception.ExceptionUtils;
 import org.openl.commons.web.jsf.FacesUtils;
-import org.openl.exception.OpenLException;
-import org.openl.message.OpenLErrorMessage;
 import org.openl.message.OpenLMessage;
 import org.openl.message.OpenLMessagesUtils;
 import org.openl.meta.explanation.ExplanationNumberValue;
 import org.openl.rules.calc.SpreadsheetResult;
 import org.openl.rules.table.formatters.FormattersManager;
-import org.openl.rules.testmethod.OpenLUserRuntimeException;
 import org.openl.rules.ui.Explanator;
 import org.openl.rules.webstudio.web.util.Constants;
-import org.openl.syntax.exception.CompositeSyntaxNodeException;
 import org.openl.util.formatters.IFormatter;
 
 public class TestResultsHelper {
@@ -34,50 +27,7 @@ public class TestResultsHelper {
     public static int getExplanatorId(ExplanationNumberValue<?> explanationValue) {        
         return Explanator.getCurrent().getUniqueId(explanationValue);
     }
-    
-    public static List<OpenLMessage> getUserMessagesAndErrors(Object result) {
-        if (result instanceof Throwable) {
-            Throwable exception = (Throwable) result;
-            exception = ExceptionUtils.getRootCause(exception);
-            List<OpenLMessage> messages = new ArrayList<OpenLMessage>();
-            
-            if (exception instanceof OpenLUserRuntimeException) {
-                OpenLUserRuntimeException userException = (OpenLUserRuntimeException) exception;
-                messages.add(new OpenLMessage(userException.getOriginalMessage(), StringUtils.EMPTY));
-            } else if (exception instanceof CompositeSyntaxNodeException) {
-                CompositeSyntaxNodeException compositeException = (CompositeSyntaxNodeException) exception;
-                
-                for (OpenLException openLException : compositeException.getErrors()) {
-                    if (openLException instanceof OpenLUserRuntimeException) {
-                        OpenLUserRuntimeException userException = (OpenLUserRuntimeException) openLException;
-                        messages.add(new OpenLMessage(userException.getOriginalMessage(), StringUtils.EMPTY));
-                    } else {
-                        messages.add(new OpenLErrorMessage(openLException));
-                    }
-                }
-            
-            } else {
-                if (exception instanceof OpenLException) {
-                    messages.add(new OpenLErrorMessage((OpenLException) exception));
-                } else {
-                    messages.add(new OpenLErrorMessage(ExceptionUtils.getRootCauseMessage(exception), StringUtils.EMPTY));
-                }
-            }
-            
-            return messages;
-        }
 
-        return Collections.emptyList();
-    }
-    
-    public static List<OpenLMessage> getErrors(Object result) {
-        if (result instanceof Throwable) {
-            return OpenLMessagesUtils.newMessages((Throwable) result);
-        }
-
-        return Collections.emptyList();
-    }
-    
     @Deprecated
     public static String getNullResult() {
         return "null";
