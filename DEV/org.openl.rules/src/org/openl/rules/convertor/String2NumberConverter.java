14e7ca4bbcb5e6d8bce29847b1dc980cdae31e05
YMolchan <none@none>
2014-06-16 10:14:06 +0300

    EPBDS-5301 Inlined NumberFormatHelper in String2NumberConverter
    It allows to use this converter for BigDecimal and BigInteger.
    Also it helps to manage all exception messages in one place.

--- DEV/org.openl.rules/src/org/openl/rules/convertor/String2NumberConverter.java
+++ DEV/org.openl.rules/src/org/openl/rules/convertor/String2NumberConverter.java
@@ -2,44 +2,103 @@ package org.openl.rules.convertor;
 
 import org.openl.binding.IBindingContext;
 
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.text.ParsePosition;
+import java.util.Locale;
+
 /**
  * A base converter class which implements logic for parsing and formatting Java's numbers.
- * It uses <code>NumberFormatHelper</code>.
+ * This converter uses US locale for parsing and formatting numbers.
  *
  * @param <T> type of a number
  * @author Yury Molchan
- * @see org.openl.rules.convertor.NumberFormatHelper
  */
 abstract class String2NumberConverter<T extends Number> implements IString2DataConvertor {
 
-    private final NumberFormatHelper formatHelper;
+    private final String defaultFormat;
 
     String2NumberConverter() {
-        formatHelper = new NumberFormatHelper();
+        defaultFormat = "";
     }
 
     String2NumberConverter(String defaultFormat) {
-        formatHelper = new NumberFormatHelper(defaultFormat);
-    }
-
-    @Override
-    public String format(Object data, String format) {
-        return formatHelper.format((Number) data, format);
-    }
-
-    @Override
-    public T parse(String data, String format, IBindingContext cxt) {
-        if (data == null) return null;
-        Number n = formatHelper.parse(data, format);
-        return convert(n, data);
+        this.defaultFormat = defaultFormat;
     }
 
     /**
-     * Converts a number to the primitive type
+     * Format a number to String according to a format. If the input string is null then null will be returned.
+     *
+     * @param data   a number to format
+     * @param format a format of a number. If it is null then a default format will be used.
+     * @return a String or null
+     */
+    @Override
+    public String format(Object data, String format) {
+        if (data == null) return null;
+
+        DecimalFormat df = getFormatter(format);
+
+        return df.format((Number) data);
+    }
+
+    /**
+     * Parse an input string to a number. If the input string is null then null will be returned.
+     *
+     * @param data   an input string to parse
+     * @param format a format of parsed string. If it is null then a default format will be used.
+     * @return a number or null
+     * @throws NumberFormatException if the specified string cannot be parsed
+     */
+    @Override
+    public T parse(String data, String format, IBindingContext cxt) {
+        if (data == null) return null;
+        DecimalFormat df = getFormatter(format);
+        if (data.endsWith("%")) {
+            // Configure to parse percents
+            df.setMultiplier(100);
+            data = data.substring(0, data.length() - 1);
+        }
+        ParsePosition position = new ParsePosition(0);
+        Number number = df.parse(data, position);
+        int index = position.getIndex();
+        if (index < data.length() || index == 0) {
+            throw new NumberFormatException("Cannot convert String \"" + data + "\" to numeric type");
+        }
+        return convert(number, data);
+    }
+
+    /**
+     * Converts a number to the required type.
+     * Also this method should check range of numbers for avoiding overflows and check type of numbers.
      *
      * @param number a number
      * @param data   a parsed string to the number
      * @return a wrapped primitive type
      */
     abstract T convert(Number number, String data);
+
+    private DecimalFormat getFormatter(String format) {
+        // NOTE!!! Using new DecimalFormat(format), depends on the users locale.
+        // E.g. if locale on the users machine is ru_RU, the ','(comma) delimiter will
+        // be used. It is not appropriate for many cases, e.g. formatting the value for writing its
+        // value to the Java class(Java expects '.' dot delimiter).
+        //
+        // NOTE2 DecimalFormat uses <code>RoundingMode.HALF_EVEN</code> by default. This is also known as banker's rounding.
+        // It is not the same as math rounding (normal/usual rounding) which is <code>RoundingMode.HALF_UP</code>
+        //
+        DecimalFormat df;
+        if (format == null) {
+            df = new DecimalFormat(defaultFormat);
+        } else {
+            df = new DecimalFormat(format);
+        }
+
+        // Reset using a default locale and set force the US locale.
+        DecimalFormatSymbols symbols = new DecimalFormatSymbols(Locale.US);
+        symbols.setInfinity("Infinity");
+        symbols.setNaN("NaN");
+        df.setDecimalFormatSymbols(symbols);
+        return df;
+    }
 }
--- DEV/org.openl.rules/test/org/openl/rules/convertor/String2NumberConverterTest.java
+++ DEV/org.openl.rules/test/org/openl/rules/convertor/String2NumberConverterTest.java
@@ -26,104 +26,214 @@ public class String2NumberConverterTest {
 
     @Test
     public void testParse() {
-        String2NumberConverter<Double> conv = getNumberConverter();
-        Object res = conv.parse("3.1415", null, null);
+        String2NumberConverter<Number> conv = getNumberConverter();
+        Number res = conv.parse("3.1415", null, null);
         assertEquals(3.1415d, res);
     }
 
-    @Test(expected = NumberFormatException.class)
-    public void testParseNonNumber() {
-        String2NumberConverter<Double> conv = getNumberConverter();
-        conv.parse("3.1415d", null, null);
-    }
-
-    @Test(expected = NumberFormatException.class)
-    public void testParseEmpty() {
-        String2NumberConverter<Double> conv = getNumberConverter();
-        conv.parse("", null, null);
+    @Test
+    public void testParseNegative() {
+        String2NumberConverter<Number> conv = getNumberConverter();
+        Number res = conv.parse("-2.1415", null, null);
+        assertEquals(-2.1415d, res);
     }
 
     @Test
+    public void testParseWithoutLeadingZero() {
+        String2NumberConverter<Number> conv = getNumberConverter();
+        Number res = conv.parse("-.123456789", null, null);
+        assertEquals(-.123456789d, res);
+    }
+
+    @Test
+    public void testParseWithLeadingZeros() {
+        String2NumberConverter<Number> conv = getNumberConverter();
+        Number res = conv.parse("0001.111000", null, null);
+        assertEquals(1.111d, res);
+    }
+
+    @Test
+    public void testParsePrecision() {
+        String2NumberConverter<Number> conv = getNumberConverter();
+        Number res = conv.parse("-1.99999999999999934", null, null);
+        assertEquals(-1.99999999999999934d, res);
+    }
+
+    @Test
+    public void testParseMaxLong() {
+        String2NumberConverter<Number> conv = getNumberConverter();
+        Number res = conv.parse("9223372036854775807", null, null);
+        assertEquals(Long.MAX_VALUE, res);
+    }
+
+    @Test
+    public void testParseMinLong() {
+        String2NumberConverter<Number> conv = getNumberConverter();
+        Number res = conv.parse("-9223372036854775808", null, null);
+        assertEquals(Long.MIN_VALUE, res);
+    }
+
+    @Test
+    public void testParseExcessMaxLong() {
+        String2NumberConverter<Number> conv = getNumberConverter();
+        Number res = conv.parse("9223372036854775808", null, null);
+        assertEquals(9223372036854775808d, res);
+    }
+
+    @Test
+    public void testParseExcessMinLong() {
+        String2NumberConverter<Number> conv = getNumberConverter();
+        Number res = conv.parse("-9223372036854775809", null, null);
+        assertEquals(-9223372036854775809d, res);
+    }
+
+
+    @Test
     public void testParsePercents() {
-        String2NumberConverter<Double> conv = getNumberConverter();
-        Object res = conv.parse("17.5%", null, null);
+        String2NumberConverter<Number> conv = getNumberConverter();
+        Number res = conv.parse("17.5%", null, null);
         assertEquals(0.175d, res);
     }
 
     @Test
+    public void testParseE() {
+        String2NumberConverter<Number> conv = getNumberConverter();
+        Number res = conv.parse("1.234E2", null, null);
+        assertEquals(123.4d, res);
+    }
+
+    @Test
+    public void testParseENegative() {
+        String2NumberConverter<Number> conv = getNumberConverter();
+        Number res = conv.parse("-1.23E-3", null, null);
+        assertEquals(-0.00123d, res);
+    }
+
+    @Test
+    public void testParseELong() {
+        String2NumberConverter<Number> conv = getNumberConverter();
+        Number res = conv.parse("-1.23E4", null, null);
+        assertEquals(-12300L, res);
+    }
+
+    @Test
+    public void testParseWithFormat() {
+        String2NumberConverter<Number> conv = getNumberConverter();
+        Number res = conv.parse("-3.1415$", "#,###$", null);
+        assertEquals(-3.1415d, res);
+    }
+
+    @Test
     public void testFormat() {
-        String2NumberConverter<Double> conv = getNumberConverter();
+        String2NumberConverter<Number> conv = getNumberConverter();
         String res = conv.format(3.1415d, null);
         assertEquals("3.1415", res);
     }
 
     @Test
-    public void testFormatByPattern() {
-        String2NumberConverter<Double> conv = getNumberConverter();
-        String res = conv.format(3.1415d, "#.###");
+    public void testFormatByPatternWithRound() {
+        String2NumberConverter<Number> conv = getNumberConverter();
+        String res = conv.format(3.1415d, "#,###.###");
         assertEquals("3.142", res);
     }
 
     @Test
+    public void testFormatByPatternWithRequiredDigits() {
+        String2NumberConverter<Number> conv = getNumberConverter();
+        String res = conv.format(3.1d, "0,000.00");
+        assertEquals("0,003.10", res);
+    }
+
+    @Test
+    public void testFormatByPatternWithSymbols() {
+        String2NumberConverter<Number> conv = getNumberConverter();
+        String res = conv.format(1234.10, "$ #,000.0# USD");
+        assertEquals("$ 1,234.1 USD", res);
+    }
+
+    @Test
     public void testParseNull() {
-        String2NumberConverter<Double> conv = getNumberConverter();
+        String2NumberConverter<Number> conv = getNumberConverter();
         assertNull(conv.parse(null, null, null));
     }
 
     @Test
     public void testFormatNull() {
-        String2NumberConverter<Double> conv = getNumberConverter();
+        String2NumberConverter<Number> conv = getNumberConverter();
         assertNull(conv.format(null, null));
     }
 
     @Test
     public void testParseNaN() {
-        String2NumberConverter<Double> conv = getNumberConverter();
-        Object res = conv.parse("NaN", null, null);
+        String2NumberConverter<Number> conv = getNumberConverter();
+        Number res = conv.parse("NaN", null, null);
         assertEquals(Double.NaN, res);
     }
 
     @Test
     public void testFormatNaN() {
-        String2NumberConverter<Double> conv = getNumberConverter();
+        String2NumberConverter<Number> conv = getNumberConverter();
         String res = conv.format(Double.NaN, null);
         assertEquals("NaN", res);
     }
 
     @Test
     public void testParsePlusInfinity() {
-        String2NumberConverter<Double> conv = getNumberConverter();
-        Object res = conv.parse("Infinity", null, null);
+        String2NumberConverter<Number> conv = getNumberConverter();
+        Number res = conv.parse("Infinity", null, null);
         assertEquals(Double.POSITIVE_INFINITY, res);
     }
 
     @Test
     public void testFormatPlusInfinity() {
-        String2NumberConverter<Double> conv = getNumberConverter();
+        String2NumberConverter<Number> conv = getNumberConverter();
         String res = conv.format(Double.POSITIVE_INFINITY, null);
         assertEquals("Infinity", res);
     }
 
     @Test
     public void testParseMinusInfinity() {
-        String2NumberConverter<Double> conv = getNumberConverter();
-        Object res = conv.parse("-Infinity", null, null);
+        String2NumberConverter<Number> conv = getNumberConverter();
+        Number res = conv.parse("-Infinity", null, null);
         assertEquals(Double.NEGATIVE_INFINITY, res);
     }
 
     @Test
     public void testFormatMinusInfinity() {
-        String2NumberConverter<Double> conv = getNumberConverter();
+        String2NumberConverter<Number> conv = getNumberConverter();
         String res = conv.format(Double.NEGATIVE_INFINITY, "#.###");
         assertEquals("-Infinity", res);
     }
 
+    @Test(expected = NumberFormatException.class)
+    public void testParseNotNumber() {
+        String2NumberConverter<Number> conv = getNumberConverter();
+        conv.parse("3.1415d", null, null);
+    }
 
-    private String2NumberConverter<Double> getNumberConverter() {
-        return new String2NumberConverter<Double>() {
+    @Test(expected = NumberFormatException.class)
+    public void testParseEmpty() {
+        String2NumberConverter<Number> conv = getNumberConverter();
+        conv.parse("", null, null);
+    }
+
+    @Test(expected = NumberFormatException.class)
+    public void testParseNotENumber() {
+        String2NumberConverter<Number> conv = getNumberConverter();
+        conv.parse("1e1", null, null);
+    }
+
+    @Test(expected = NumberFormatException.class)
+    public void testParseWithSpaces() {
+        String2NumberConverter<Number> conv = getNumberConverter();
+        conv.parse("1 ", null, null);
+    }
+
+    private String2NumberConverter<Number> getNumberConverter() {
+        return new String2NumberConverter<Number>() {
             @Override
-            Double convert(Number number, String data) {
-                return number.doubleValue();
+            Number convert(Number number, String data) {
+                return number;
             }
         };
     }
