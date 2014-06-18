7c0b1aaee5859b245a9285a55ae56b4602749642
YMolchan <none@none>
2014-06-18 13:21:05 +0300

    EPBDS-5301 Delegated object type instead of a converter

--- DEV/org.openl.rules/test/org/openl/rules/convertor/String2ArrayConvertorTest.java
+++ DEV/org.openl.rules/test/org/openl/rules/convertor/String2ArrayConvertorTest.java
@@ -2,9 +2,67 @@ package org.openl.rules.convertor;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
 public class String2ArrayConvertorTest {
+
+    @Test
+    public void testParseEmpty() {
+        String2ArrayConvertor<Integer> converter = new String2ArrayConvertor(Integer.class);
+        Integer[] result = converter.parse("", null, null);
+        assertArrayEquals(new Integer[]{}, result);
+    }
+
+    @Test
+    public void testFormatEmpty() {
+        String2ArrayConvertor<Integer> converter = new String2ArrayConvertor(Integer.class);
+        String result = converter.format(new Integer[]{}, null);
+        assertEquals("", result);
+    }
+
+    @Test
+    public void testParseSingleElement() {
+        String2ArrayConvertor<Integer> converter = new String2ArrayConvertor(Integer.class);
+        Integer[] result = converter.parse("123", null, null);
+        assertArrayEquals(new Integer[]{123}, result);
+    }
+
+    @Test
+    public void testFormatSingleElement() {
+        String2ArrayConvertor<Integer> converter = new String2ArrayConvertor(Integer.class);
+        String result = converter.format(new Integer[]{456}, null);
+        assertEquals("456", result);
+    }
+
+    @Test
+    public void testParseTwoElements() {
+        String2ArrayConvertor<Integer> converter = new String2ArrayConvertor(Integer.class);
+        Integer[] result = converter.parse("1,23", null, null);
+        assertArrayEquals(new Integer[]{1,23}, result);
+    }
+
+    @Test
+    public void testFormatTwoElements() {
+        String2ArrayConvertor<Integer> converter = new String2ArrayConvertor(Integer.class);
+        String result = converter.format(new Integer[]{45,6}, null);
+        assertEquals("45,6", result);
+    }
+
+    @Test
+    public void testParseWithNullElement() {
+        String2ArrayConvertor<Integer> converter = new String2ArrayConvertor(Integer.class);
+        Integer[] result = converter.parse("1,,3", null, null);
+        assertArrayEquals(new Integer[]{1,null,3}, result);
+    }
+
+    @Test
+    public void testFormatWithNullElement() {
+        String2ArrayConvertor<Integer> converter = new String2ArrayConvertor(Integer.class);
+        String result = converter.format(new Integer[]{4,null,6}, null);
+        assertEquals("4,,6", result);
+    }
 
     @Test
     public void testParseNull() {
@@ -16,5 +74,17 @@ public class String2ArrayConvertorTest {
     public void testFormatNull() {
         String2ArrayConvertor converter = new String2ArrayConvertor(null);
         assertNull(converter.format(null, null));
+    }
+
+    @Test(expected = NumberFormatException.class)
+    public void testParseNotIntegers() {
+        String2ArrayConvertor<Integer> converter = new String2ArrayConvertor(Integer.class);
+        converter.parse("12.30", null, null);
+    }
+
+    @Test(expected = NumberFormatException.class)
+    public void testParseWrongValue() {
+        String2ArrayConvertor<Integer> converter = new String2ArrayConvertor(Integer.class);
+        converter.parse("12,34,_,56", null, null);
     }
 }
