418fcea1ae09eb851ff87c6e50e652961da58962
Denis Levchuk <dliauchuk@exigeninsurance.com>
2014-06-19 15:18:20 +0300

    EPBDS-5321 Fix falling test with recursive datatype field

--- DEV/org.openl.rules/test/org/openl/rules/datatype/binding/TopologicalSortTest.java
+++ DEV/org.openl.rules/test/org/openl/rules/datatype/binding/TopologicalSortTest.java
@@ -36,4 +36,32 @@ public class TopologicalSortTest {
         assertEquals("3", sortedList.get(2).getObj());
         assertEquals("4", sortedList.get(3).getObj());
     }
+
+    @Test
+    public void testRecursion() {
+        TopoGraphNode<String> node1 = new TopoGraphNode<String>("1");
+        TopoGraphNode<String> node2 = new TopoGraphNode<String>("2");
+        node2.addDependency(node1);
+        /**
+         * Added recursion dependency
+         */
+        node2.addDependency(node2);
+
+        TopoGraphNode<String> node3 = new TopoGraphNode<String>("3");
+        node3.addDependency(node2);
+
+        TopoGraphNode<String> node4 = new TopoGraphNode<String>("4");
+        node4.addDependency(node3);
+
+        List<TopoGraphNode<String>> list = Arrays.asList(node4, node3, node2, node1);
+
+        Set<TopoGraphNode<String>> sorted = new TopologicalSort<String>().sort(list);
+        List<TopoGraphNode<String>> sortedList = new ArrayList<TopoGraphNode<String>>(sorted);
+
+        assertEquals(4, sortedList.size());
+        assertEquals("1", sortedList.get(0).getObj());
+        assertEquals("2", sortedList.get(1).getObj());
+        assertEquals("3", sortedList.get(2).getObj());
+        assertEquals("4", sortedList.get(3).getObj());
+    }
 }
