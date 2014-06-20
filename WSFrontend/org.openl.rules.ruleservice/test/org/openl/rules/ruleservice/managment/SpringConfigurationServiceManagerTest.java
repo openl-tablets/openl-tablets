9bef5c65f5ede8b78602dbd75d168c71737e3919
Marat Kamalov <none@none>
2014-06-20 16:11:39 +0400

    EPBDS-5339 Improve Settings configuration in WebServices
    --HG--
    branch : 5.12.x

--- WSFrontend/org.openl.rules.ruleservice/test/org/openl/rules/ruleservice/managment/SpringConfigurationServiceManagerTest.java
+++ WSFrontend/org.openl.rules.ruleservice/test/org/openl/rules/ruleservice/managment/SpringConfigurationServiceManagerTest.java
@@ -18,7 +18,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "classpath:openl-ruleservice-beans.xml" })
+@ContextConfiguration(locations = { "classpath:SpringConfigurationServiceManagerTest/openl-ruleservice-beans.xml" })
 @DirtiesContext
 public class SpringConfigurationServiceManagerTest implements ApplicationContextAware {
 
