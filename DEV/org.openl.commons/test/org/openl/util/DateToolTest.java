7966af0dc89f559ec0308ca78c61f76f611e28dd
Denis Levchuk <dliauchuk@exigeninsurance.com>
2014-07-08 17:34:48 +0300

    Avoid any income argument to be null. Test

---
+++ DEV/org.openl.commons/test/org/openl/util/DateToolTest.java
@@ -0,0 +1,26 @@
+package org.openl.util;
+
+import org.junit.Test;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Created by dl on 7/8/14.
+ */
+public class DateToolTest {
+    @Test
+    public void testYearDiff() {
+        assertEquals(0, DateTool.yearDiff(null, null));
+        assertEquals(0, DateTool.yearDiff(new Date(), null));
+        assertEquals(0, DateTool.yearDiff(null, new Date()));
+        Calendar start = Calendar.getInstance();
+        start.set(2013, 10, 5);
+        Calendar end = Calendar.getInstance();
+        end.set(2015, 11, 6);
+
+        assertEquals(2, DateTool.yearDiff(end.getTime(), start.getTime()));
+    }
+}
