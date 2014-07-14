3f3d4eb193c2ffb0648c62476fd28bdaa51a8173
Yury Molchan <ymolchan@eisgroup.com>
2014-07-14 13:49:42 +0300

    EPBDS-5322 Fixed region calculation
    not taken into account the following situation of merged regions
    112
    332

--- DEV/org.openl.rules/src/org/openl/rules/table/GridTableUtils.java
+++ DEV/org.openl.rules/src/org/openl/rules/table/GridTableUtils.java
@@ -4,15 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * @author Andrei Ostrovski
+ * @author Andrei Ostrovski, Yury Molchan
  */
 public class GridTableUtils {
 
-    private GridTableUtils() { }
+    private GridTableUtils() {
+    }
 
     /**
      * Extracts original table.
-     * 
+     *
      * @param table Table.
      * @return Original table if table is decorator and current table otherwise.
      */
@@ -28,19 +29,35 @@ public class GridTableUtils {
 
     /**
      * Returns all regions of a table.
+     *
      * @param table the table with regions.
      * @return a the regions of the table.
      */
     public static List<IGridRegion> getGridRegions(IGridTable table) {
         int height = table.getHeight();
         int width = table.getWidth();
-
+        // create a matrix of processed cells
+        // true - has processed
+        // false - has not processed yet
+        boolean[][] mask = new boolean[height][width];
         List<IGridRegion> regions = new ArrayList<IGridRegion>();
-        ICell cell = null;
-        for (int row = 0; row < height; row += cell.getHeight()) {
-            for (int column = 0; column < width; column += cell.getWidth()) {
-                cell = table.getCell(column, row);
+
+        // Go through all possible cells
+        for (int row = 0; row < height; row++) {
+            for (int column = 0; column < width; column++) {
+                // If a cell is marked as processed skip it
+                if (mask[row][column]) {
+                    continue;
+                }
+                // store a region of the cell
+                ICell cell = table.getCell(column, row);
                 regions.add(cell.getAbsoluteRegion());
+                // mark this cell as processed, include merged cells (mark the whole region)
+                for (int h = 0; h < cell.getHeight(); h++) {
+                    for (int w = 0; w < cell.getWidth(); w++) {
+                        mask[row + h][column + w] = true;
+                    }
+                }
             }
         }
         return regions;
