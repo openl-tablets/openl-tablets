98bce8ed64316a4df9d68342c19b8ae0210385be
Marat Kamalov <Marat Kamalov@localhost>
2014-07-24 13:06:43 +0400

    Added comment for public interface.

--- WSFrontend/org.openl.rules.ruleservice.ws/src/org/openl/rules/ruleservice/servlet/AvailableServicesGroup.java
+++ WSFrontend/org.openl.rules.ruleservice.ws/src/org/openl/rules/ruleservice/servlet/AvailableServicesGroup.java
@@ -2,6 +2,12 @@ package org.openl.rules.ruleservice.servlet;
 
 import java.util.List;
 
+/**
+ * An interface for publisher that should be displayed on Servlet page. 
+ * 
+ * @author Nail Samatov
+ *
+ */
 public interface AvailableServicesGroup {
     String getGroupName();
     List<ServiceInfo> getAvailableServices();
