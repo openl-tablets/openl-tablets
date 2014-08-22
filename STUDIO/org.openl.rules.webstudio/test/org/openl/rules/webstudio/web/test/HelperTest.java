261755750e702a013cf44678a27e4697b6c26ba3
Yury Molchan <ymolchan@eisgroup.com>
2014-08-22 13:45:37 +0300

    EPBDS-5442 Extracted format() method to the separate helper class

--- STUDIO/org.openl.rules.webstudio/test/org/openl/rules/webstudio/web/test/HelperTest.java
+++ STUDIO/org.openl.rules.webstudio/test/org/openl/rules/webstudio/web/test/HelperTest.java
@@ -3,6 +3,7 @@ package org.openl.rules.webstudio.web.test;
 import org.junit.Test;
 import org.richfaces.model.TreeNode;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertSame;
 
 public class HelperTest {
@@ -14,6 +15,17 @@ public class HelperTest {
         TreeNode root = helper.getRoot(parameter);
         TreeNode child = root.getChild("FN");
         assertSame(parameter, child);
+    }
+
+    @Test
+    public void testFormat() {
+        Helper helper = new Helper();
+        assertEquals("null", helper.format(null));
+        assertEquals("Str", helper.format("Str"));
+        assertEquals("1", helper.format(1));
+        assertEquals("0.1", helper.format(0.1));
+        assertEquals("true", helper.format(true));
+        assertEquals("foo,bar", helper.format(new String[]{"foo", "bar"}));
     }
 }
 
