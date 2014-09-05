edc44ee737ffbe9029afc12792581bd52ee65658
Nail Samatov <nsamatov@eisgroup.com>
2014-09-05 13:56:17 +0400

    EPBDS-5528 There are broken unnecessary explanation links in trace window.

--- STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/web/test/SpreadsheetResultTreeNode.java
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/web/test/SpreadsheetResultTreeNode.java
@@ -8,17 +8,20 @@ import org.openl.types.IOpenClass;
 
 public class SpreadsheetResultTreeNode extends ParameterDeclarationTreeNode {
     public static final String SPREADSHEET_RESULT_TYPE = "spreadsheet";
+    private final boolean hasExplainLinks;
 
     public SpreadsheetResultTreeNode(String fieldName, Object value, IOpenClass fieldType,
-            ParameterDeclarationTreeNode parent) {
+                                     ParameterDeclarationTreeNode parent, boolean hasExplainLinks) {
         super(fieldName, value, fieldType, parent);
+        this.hasExplainLinks = hasExplainLinks;
     }
 
     @Override
     public String getDisplayedValue() {
         // TODO Refactor code and don't use deprecated class ObjectViewer.
         // TODO Instead render a table using jsf components.
-        return ObjectViewer.displaySpreadsheetResult((SpreadsheetResult) getValue());
+        SpreadsheetResult value = (SpreadsheetResult) getValue();
+        return hasExplainLinks ? ObjectViewer.displaySpreadsheetResult(value) : ObjectViewer.displaySpreadheetResultNoFilters(value);
     }
 
     @Override
