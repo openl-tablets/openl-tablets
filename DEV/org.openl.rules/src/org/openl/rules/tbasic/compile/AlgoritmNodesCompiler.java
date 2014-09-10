e74abaaa5666acce7b4334d96493ef4e951dc9aa
Denis Levchuk <dliauchuk@exigeninsurance.com>
2014-09-10 14:56:18 +0300

    EPBDS-5537 Refactoring: replace if-else with strategies

--- DEV/org.openl.rules/src/org/openl/rules/tbasic/compile/AlgoritmNodesCompiler.java
+++ DEV/org.openl.rules/src/org/openl/rules/tbasic/compile/AlgoritmNodesCompiler.java
@@ -1,9 +1,5 @@
 package org.openl.rules.tbasic.compile;
 
-import java.lang.reflect.Constructor;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.openl.meta.StringValue;
 import org.openl.rules.tbasic.AlgorithmTreeNode;
 import org.openl.rules.tbasic.runtime.operations.RuntimeOperation;
@@ -12,6 +8,12 @@ import org.openl.syntax.exception.SyntaxNodeException;
 import org.openl.syntax.exception.SyntaxNodeExceptionUtils;
 import org.openl.types.IMethodCaller;
 import org.openl.types.IOpenClass;
+
+import java.lang.reflect.Constructor;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * The <code>AlgoritmNodesCompiler</code> class compiles sequence of nodes
@@ -25,6 +27,8 @@ public class AlgoritmNodesCompiler {
     
     /** return type for some contexts that are represented as functions**/
     private IOpenClass returnType;
+
+    private Map<Class, ParameterConverter> parameterConverters = new HashMap<Class, ParameterConverter>();
 
     /**
      * Create an instance of <code>AlgoritmNodesCompiler</code>.
@@ -40,6 +44,10 @@ public class AlgoritmNodesCompiler {
         this.currentCompileContext = currentCompileContext;
         this.compiler = compiler;
         this.returnType = returnType;
+        this.parameterConverters.put(String.class, new StringConverter());
+        this.parameterConverters.put(boolean.class, new BooleanConverter());
+        this.parameterConverters.put(IMethodCaller.class, new MethodCallerConverter());
+
     }
 
     /**
@@ -139,51 +147,21 @@ public class AlgoritmNodesCompiler {
         return compileNestedNodes(nodes);
     }
 
-    private Object convertParam(List<AlgorithmTreeNode> nodesToCompile, Class<? extends Object> clazz,
-            String operationParam) throws SyntaxNodeException {
-        // FIXME !!!!
+    private Object convertParam(List<AlgorithmTreeNode> nodesToCompile,
+                                Class<?> clazz,
+                                String operationParam)
+            throws SyntaxNodeException {
 
-        if (clazz.equals(String.class)) {
-            if (labelManager.isLabelInstruction(operationParam)) {
-                return labelManager.getLabelByInstruction(operationParam);
-            } else if (AlgorithmCompilerTool.isOperationFieldInstruction(operationParam)) {
-                StringValue content = AlgorithmCompilerTool.getCellContent(nodesToCompile, operationParam);
+        ParameterConverter converter = parameterConverters.get(clazz);
 
-                return content.getValue();
-            } else {
-                // TODO FIXME Do not know how to process
-                return operationParam;
-            }
-        } else if (clazz.equals(boolean.class)) {
-            return Boolean.parseBoolean(operationParam);
-        } else if (clazz.equals(IMethodCaller.class)) {
-            if (operationParam == null) {
-                return null;
-            } else {
-                StringValue cellContent = AlgorithmCompilerTool.getCellContent(nodesToCompile, operationParam);
-                IOpenSourceCodeModule src = cellContent.getMetaInfo().getSource();
-
-                AlgorithmTreeNode executionNode = AlgorithmCompilerTool.extractOperationNode(nodesToCompile,
-                        operationParam);
-                String methodName = operationParam.replace('.', '_') + "_row_"
-                        + executionNode.getAlgorithmRow().getRowNumber();
-                
-                // return statements for the whole Algorithm(TBasic) should be casted to the return type of 
-                // whole Algorithm rule
-                if (labelManager.isReturnInstruction(operationParam)) {
-                    /** create method and cast its value to the appropriate return type*/
-                    return compiler.makeMethodWithCast(src, methodName, returnType);
-                } else {
-                    return compiler.makeMethod(src, methodName);
-                }
-            }        
-        } else {
-        
+        if (converter == null) {
             IOpenSourceCodeModule errorSource = nodesToCompile.get(0).getAlgorithmRow().getOperation()
                     .asSourceCodeModule();
             throw SyntaxNodeExceptionUtils.createError(String.format("Compilation failure. Can't convert parameter %s to type %s",
                     operationParam, clazz.toString()), errorSource);
         }
+
+        return converter.convert(nodesToCompile, operationParam);
     }
 
     private RuntimeOperation createOperation(List<AlgorithmTreeNode> nodesToCompile, ConversionRuleStep conversionStep)
@@ -302,6 +280,61 @@ public class AlgoritmNodesCompiler {
         }
 
         return emittedOperations;
+    }
+
+    public interface ParameterConverter {
+        Object convert(List<AlgorithmTreeNode> nodesToCompile, String operationParam) throws SyntaxNodeException;
+    }
+
+    private final class StringConverter implements ParameterConverter{
+
+        @Override
+        public Object convert(List<AlgorithmTreeNode> nodesToCompile, String operationParam) throws SyntaxNodeException {
+            if (labelManager.isLabelInstruction(operationParam)) {
+                return labelManager.getLabelByInstruction(operationParam);
+            } else if (AlgorithmCompilerTool.isOperationFieldInstruction(operationParam)) {
+                StringValue content = AlgorithmCompilerTool.getCellContent(nodesToCompile, operationParam);
+
+                return content.getValue();
+            } else {
+                // TODO FIXME Do not know how to process
+                return operationParam;
+            }
+        }
+    }
+
+    private final class BooleanConverter implements ParameterConverter {
+
+        @Override
+        public Object convert(List<AlgorithmTreeNode> nodesToCompile, String operationParam) throws SyntaxNodeException {
+            return Boolean.parseBoolean(operationParam);
+        }
+    }
+
+    private final class MethodCallerConverter implements ParameterConverter {
+
+        @Override
+        public Object convert(List<AlgorithmTreeNode> nodesToCompile, String operationParam) throws SyntaxNodeException {
+            if (operationParam == null) {
+                return null;
+            }
+
+            AlgorithmTreeNode executionNode = AlgorithmCompilerTool.extractOperationNode(nodesToCompile,
+                    operationParam);
+            String methodName = String.format("%s_row_%s", operationParam.replace('.', '_'),
+                    executionNode.getAlgorithmRow().getRowNumber());
+
+            StringValue cellContent = AlgorithmCompilerTool.getCellContent(nodesToCompile, operationParam);
+            IOpenSourceCodeModule src = cellContent.getMetaInfo().getSource();
+            // return statements for the whole Algorithm(TBasic) should be casted to the return type of
+            // whole Algorithm rule
+            if (labelManager.isReturnInstruction(operationParam)) {
+                /** create method and cast its value to the appropriate return type*/
+                return compiler.makeMethodWithCast(src, methodName, returnType);
+            } else {
+                return compiler.makeMethod(src, methodName);
+            }
+        }
     }
     
 }
