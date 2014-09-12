5f54aa08e2f8631070c1b0407b7d1bbab4cd4e7b
Yury Molchan <ymolchan@eisgroup.com>
2014-09-12 16:02:07 +0300

    EPBDS-5542 Deleted unused functionality from the benchmark module

--- DEV/org.openl.commons/src/org/openl/util/benchmark/RunInfo.java
+++ DEV/org.openl.commons/src/org/openl/util/benchmark/RunInfo.java
@@ -6,47 +6,13 @@ public class RunInfo {
 
     long ms;
 
-    public RunInfo(long times, long ms, long memoryBefore, long memoryDirtyAfter, long memoryCleanAfter) {
-        super();
-        this.times = times;
-        this.ms = ms;
-        this.memoryBefore = memoryBefore;
-        this.memoryDirtyAfter = memoryDirtyAfter;
-        this.memoryCleanAfter = memoryCleanAfter;
-    }
-
     public RunInfo(long runs, long ms) {
         times = runs;
         this.ms = ms;
-    }
-
-    public double avgTimesSec() {
-        return (double) times * 1000 / ms;
     }
 
     @Override
     public String toString() {
         return "[" + times + " : " + ms + "]";
     }
-
-    long memoryBefore;
-    long memoryDirtyAfter;
-    long memoryCleanAfter;
-
-    public double bytesPerRun() {
-        return (memoryDirtyAfter - memoryBefore) / (double) times;
-    }
-
-    public double leakedBytesPerRun() {
-        return (memoryCleanAfter - memoryBefore) / (double) times;
-    }
-
-    public double usedBytes() {
-        return (memoryDirtyAfter - memoryBefore);
-    }
-
-    public double leakedBytes() {
-        return (memoryCleanAfter - memoryBefore);
-    }
-
 }
