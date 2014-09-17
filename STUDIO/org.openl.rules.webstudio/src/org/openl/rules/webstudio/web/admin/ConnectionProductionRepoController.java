76c0797af226a05daf64c48ba364e2ba638a86cd
Nail Samatov <nsamatov@eisgroup.com>
2014-09-17 18:12:27 +0400

    EPBDS-5548 Fix warnings, simplify the code

--- STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/web/admin/ConnectionProductionRepoController.java
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/web/admin/ConnectionProductionRepoController.java
@@ -93,25 +93,30 @@ public class ConnectionProductionRepoController extends AbstractProductionRepoCo
 
     private boolean checkLocalRepo(RepositoryConfiguration repoConfig) {
         File repoDir = new File(repoConfig.getPath());
+        String errorMessage = "There is no repository in this folder. Please, correct folder path";
         if (repoDir.exists()) {
             File[] files = repoDir.listFiles();
             RepoDirChecker checker = new RepoDirChecker(repoConfig.getPath());
+            if (files == null) {
+                setErrorMessage(errorMessage);
+                return false;
+            }
 
-            for (int i = 0; i < files.length; i++) {
+            for (File file : files) {
                 try {
-                    checker.check(files[i].getCanonicalPath());
+                    checker.check(file.getCanonicalPath());
                 } catch (IOException e) {
-                    setErrorMessage("There is no repository in this folder. Please, correct folder path");
+                    setErrorMessage(errorMessage);
                     return false;
                 }
             }
 
             if(!checker.isRepoThere()) {
-                setErrorMessage("There is no repository in this folder. Please, correct folder path");
+                setErrorMessage(errorMessage);
                 return false;
             }
 
-            if (checker.isRepoThere() && !StringUtils.isEmpty(repoConfig.getLogin())) {
+            if (!StringUtils.isEmpty(repoConfig.getLogin())) {
                 try {
                     RRepository repository = this.getProductionRepositoryFactoryProxy().getFactory(repoConfig.getProperties()).getRepositoryInstance();
                     repository.release();
@@ -121,7 +126,7 @@ public class ConnectionProductionRepoController extends AbstractProductionRepoCo
                 }
             }
         } else {
-            setErrorMessage("There is no repository in this folder. Please, correct folder path");
+            setErrorMessage(errorMessage);
             return false;
         }
 
