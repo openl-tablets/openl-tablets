bca57dc2f5ef4ac6361b645f383fa9bf1fe2df73
Nail Samatov <nsamatov@eisgroup.com>
2014-09-17 19:49:02 +0400

    EPBDS-5548 Simplify the code

--- STUDIO/org.openl.rules.webstudio/src/org/openl/rules/ui/tablewizard/PropertyTableCreationWizard.java
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/ui/tablewizard/PropertyTableCreationWizard.java
@@ -235,9 +235,7 @@ public class PropertyTableCreationWizard extends TableCreationWizard {
         for (TableProperty property : propertiesManager.getProperties()) {
             String name = property.getName();
             Object value = property.getValue();
-            if (value == null || (value != null && (value instanceof String && StringUtils.isEmpty((String) value)))) {
-                continue;
-            } else {
+            if (value != null && (!(value instanceof String) || !StringUtils.isEmpty((String) value))) {
                 resultProperties.put(name.trim(), value);
             }
         }
