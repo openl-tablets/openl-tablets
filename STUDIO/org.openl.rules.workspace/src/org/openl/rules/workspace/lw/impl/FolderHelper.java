7955a329e122b65fb7fafcff340a4cf1dac7d353
Nail Samatov <nsamatov@eisgroup.com>
2014-09-17 13:58:24 +0400

    EPBDS-5548 Fix: Dereference of 'files' may produce 'java.lang.NullPointerException'

--- STUDIO/org.openl.rules.workspace/src/org/openl/rules/workspace/lw/impl/FolderHelper.java
+++ STUDIO/org.openl.rules.workspace/src/org/openl/rules/workspace/lw/impl/FolderHelper.java
@@ -76,6 +76,11 @@ public final class FolderHelper {
             // list sub elements
             File[] files = folder.listFiles();
 
+            if (files == null) {
+                // Some I/O error occurs
+                return false;
+            }
+
             // delete one by one
             for (File f : files) {
                 if (f.isDirectory()) {
