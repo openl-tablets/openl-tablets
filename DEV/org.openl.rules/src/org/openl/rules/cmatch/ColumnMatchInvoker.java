18cd6aa9b3b3dbff83c2046331610e78e1fc7409
Yury Molchan <ymolchan@eisgroup.com>
2014-09-18 19:22:29 +0300

    EPBDS-5453 Made invoke() method as final

--- DEV/org.openl.rules/src/org/openl/rules/cmatch/ColumnMatchInvoker.java
+++ DEV/org.openl.rules/src/org/openl/rules/cmatch/ColumnMatchInvoker.java
@@ -1,7 +1,5 @@
 package org.openl.rules.cmatch;
 
-import org.openl.exception.OpenLRuntimeException;
-import org.openl.rules.method.DefaultInvokerWithTrace;
 import org.openl.rules.method.RulesMethodInvoker;
 import org.openl.types.IOpenClass;
 import org.openl.vm.IRuntimeEnv;
@@ -11,13 +9,13 @@ public class ColumnMatchInvoker extends RulesMethodInvoker {
     public ColumnMatchInvoker(ColumnMatch columnMatch) {
         super(columnMatch);
     }
-    
+
     @Override
     public ColumnMatch getInvokableMethod() {
-        return (ColumnMatch)super.getInvokableMethod();
+        return (ColumnMatch) super.getInvokableMethod();
     }
 
-    public boolean canInvoke() {        
+    public boolean canInvoke() {
         return getInvokableMethod().getAlgorithmExecutor() != null;
     }
 
@@ -28,39 +26,21 @@ public class ColumnMatchInvoker extends RulesMethodInvoker {
             IOpenClass type = getInvokableMethod().getHeader().getType();
             if (type.getInstanceClass().isPrimitive()) {
                 throw new IllegalArgumentException("Cannot return <null> for primitive type " + type.getInstanceClass()
-                    .getName());
+                        .getName());
             }
         }
 
-        return result;        
+        return result;
     }
 
     /**
-     * Column match is traceable. But trace is handled inside algorithm executor. See implementations 
-     * of {@link IMatchAlgorithmExecutor#invoke(Object target, Object[] params, IRuntimeEnv env, ColumnMatch columnMatch)}
-     * 
+     * Column match is traceable. But trace is handled inside algorithm executor. See implementations
+     * of {@link org.openl.rules.cmatch.algorithm.IMatchAlgorithmExecutor#invoke(Object target, Object[] params, IRuntimeEnv env, ColumnMatch columnMatch)}
      */
     public Object invokeTraced(Object target, Object[] params, IRuntimeEnv env) {
         // trace operations implemented in 
         // IMatchAlgorithmExecutor#invoke(Object target, Object[] params, IRuntimeEnv env, ColumnMatch columnMatch)}
         //
-        return null;
-    }
-
-    /**
-     * Overrides {@link DefaultInvokerWithTrace#invoke()} as trace operations are implemented on other level.
-     * See {@link #invokeTraced()}
-     */
-    @Override
-    public Object invoke(Object target, Object[] params, IRuntimeEnv env) {
-        if (canInvoke()) {
-            return invokeSimple(target, params, env);
-        } else {
-            OpenLRuntimeException error = getError();
-            if (isTracerOn()) {
-                setErrorToTrace(error, params);
-            }
-            throw error;
-        }
+        return invokeSimple(target, params, env);
     }
 }
