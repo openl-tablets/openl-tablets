3b78606a4b82de351d95541125393241b87c2e69
Yury Molchan <ymolchan@eisgroup.com>
2014-09-18 19:53:36 +0300

    EPBDS-5453 Merged DefaultInvokerWithTrace to RulesMethodInvoker

--- DEV/org.openl.rules/src/org/openl/rules/method/RulesMethodInvoker.java
+++ DEV/org.openl.rules/src/org/openl/rules/method/RulesMethodInvoker.java
@@ -2,33 +2,75 @@ package org.openl.rules.method;
 
 import org.openl.exception.OpenLRuntimeException;
 import org.openl.rules.table.ATableTracerNode;
+import org.openl.vm.IRuntimeEnv;
+import org.openl.vm.trace.Tracer;
 
 /**
- * Default implementation for {@link DefaultInvokerWithTrace}. {@link ExecutableRulesMethod}
- * is used as traced object.
- * 
+ * Default implementation for invokers supporting tracing.
+ *
  * @author DLiauchuk
  *
  */
-public abstract class RulesMethodInvoker extends DefaultInvokerWithTrace {
-    
+public abstract class RulesMethodInvoker implements InvokerWithTrace {
+
     private ExecutableRulesMethod invokableMethod;
-    
-    public RulesMethodInvoker(ExecutableRulesMethod invokableMethod) {
+
+    protected RulesMethodInvoker(ExecutableRulesMethod invokableMethod) {
         this.invokableMethod = invokableMethod;
+    }
+
+    public final Object invoke(Object target, Object[] params, IRuntimeEnv env) {
+        // check if the object can be invoked
+        //
+        if (canInvoke()) {
+            if (isTracerOn()) {
+                // invoke in trace
+                return invokeTraced(target, params, env);
+            } else {
+                // simple run invoke
+                return invokeSimple(target, params, env);
+            }
+        } else {
+            // object can`t be invoked, inform user about the problem.
+            OpenLRuntimeException error = getError();
+            if (isTracerOn()) {
+                setErrorToTrace(error, params);
+            }
+            throw error;
+        }
+    }
+
+    /**
+     * Gets the error.
+     *
+     * @return error.
+     */
+    protected OpenLRuntimeException getError() {
+        return new OpenLRuntimeException(getInvokableMethod().getSyntaxNode().getErrors()[0]);
+    }
+
+
+    /**
+     * Creates traceable node for current invokable object.
+     *
+     * @param params
+     * @return {@link ATableTracerNode} for current invokable object.
+     */
+    protected ATableTracerNode getTraceObject(Object[] params) {
+        return TracedObjectFactory.getTracedObject(invokableMethod, params);
+    }
+
+    protected  boolean isTracerOn() {
+        return Tracer.isTracerOn();
+    }
+
+    protected void setErrorToTrace(OpenLRuntimeException error, Object[] params) {
+        ATableTracerNode traceObject = getTraceObject(params);
+        traceObject.setError(error);
+        Tracer.put(traceObject);
     }
 
     public ExecutableRulesMethod getInvokableMethod() {
         return invokableMethod;
-    }
-
-    @Override
-    protected ATableTracerNode getTraceObject(Object[] params) {
-        return TracedObjectFactory.getTracedObject(invokableMethod, params);        
-    }
-    
-    @Override
-    protected OpenLRuntimeException getError() {
-        return new OpenLRuntimeException(getInvokableMethod().getSyntaxNode().getErrors()[0]);
     }
 }
