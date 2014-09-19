8689204650d89240adfe407bea7462704eeebd87
Yury Molchan <ymolchan@eisgroup.com>
2014-09-19 11:57:49 +0300

    EPBDS-5453 Deleted never executed code

--- DEV/org.openl.rules/src/org/openl/rules/dt/index/ComparatorTraceDecorator.java
+++ DEV/org.openl.rules/src/org/openl/rules/dt/index/ComparatorTraceDecorator.java
@@ -1,8 +1,8 @@
 package org.openl.rules.dt.index;
 
-import java.util.Comparator;
-
 import org.openl.rules.dt.trace.DTIndexedTraceObject;
+
+import java.util.Comparator;
 
 class ComparatorTraceDecorator implements Comparator<Object> {
     private Comparator<Object> delegate;
@@ -12,7 +12,7 @@ class ComparatorTraceDecorator implements Comparator<Object> {
         if (delegate == null) {
             throw new IllegalArgumentException("delegate arg can't be null!");
         }
-        this.delegate = (Comparator<Object>)delegate;
+        this.delegate = (Comparator<Object>) delegate;
     }
 
     @Override
@@ -22,45 +22,16 @@ class ComparatorTraceDecorator implements Comparator<Object> {
         if (o1 instanceof ComparableValueTraceDecorator) {
             p1 = ((ComparableValueTraceDecorator) o1).delegate;
         }
-        if (o1 instanceof ComparableIndexTraceDecorator){
+        if (o1 instanceof ComparableIndexTraceDecorator) {
             p1 = ((ComparableIndexTraceDecorator<?>) o1).delegate;
         }
         if (o1 instanceof ComparableValueTraceDecorator) {
             p2 = ((ComparableValueTraceDecorator) o1).delegate;
         }
-        if (o2 instanceof ComparableIndexTraceDecorator){
+        if (o2 instanceof ComparableIndexTraceDecorator) {
             p2 = ((ComparableIndexTraceDecorator<?>) o2).delegate;
         }
         int result = delegate.compare(p1, p2);
-        if (o1 instanceof ComparableValueTraceDecorator) {
-            traceComparisonResult(result == 0, o2);
-        } else {
-            if (o2 instanceof ComparableValueTraceDecorator) {
-                traceComparisonResult(result == 0, o1);
-            } 
-        }
         return result;
-    }
-
-    public void traceComparisonResult(boolean successful, Object o) {
-        if (!(o instanceof ComparableIndexTraceDecorator)) {
-            return;
-        }
-        @SuppressWarnings("unchecked")
-        ComparableIndexTraceDecorator<Object> comparableIndexTraceDecorator = (ComparableIndexTraceDecorator<Object>) o;
-        if (!comparableIndexTraceDecorator.linkedRule.getRulesIterator().hasNext()) {
-            // Do not trace index value that is not mapped to any rule. This can
-            // be an excluding boundary for example.
-            return;
-        }
-        // TODO: remove side effect from the push method
-        comparableIndexTraceDecorator.traceStack.push(new DTIndexedTraceObject(comparableIndexTraceDecorator.baseTraceObject,
-            comparableIndexTraceDecorator.condition,
-            comparableIndexTraceDecorator.linkedRule,
-            successful));
-
-        if (!successful) {
-            comparableIndexTraceDecorator.traceStack.pop();
-        }
     }
 }
