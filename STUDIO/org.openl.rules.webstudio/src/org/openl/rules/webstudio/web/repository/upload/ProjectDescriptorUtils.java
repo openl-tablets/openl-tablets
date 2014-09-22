623b0f1176bf3a358e84d3fc0148c7e31d48ab1c
stanislav.shor <SShor@exigeninsurance.com>
2014-09-22 11:31:47 -0400

    Normalizing usage of "rules.xml" constant

--- STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/web/repository/upload/ProjectDescriptorUtils.java
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/web/repository/upload/ProjectDescriptorUtils.java
@@ -1,13 +1,15 @@
 package org.openl.rules.webstudio.web.repository.upload;
 
 import com.thoughtworks.xstream.XStreamException;
+
+import org.openl.rules.project.resolving.ProjectDescriptorBasedResolvingStrategy;
 import org.xml.sax.SAXParseException;
 
 public final class ProjectDescriptorUtils {
     private ProjectDescriptorUtils() {}
 
     public static String getErrorMessage(XStreamException e) {
-        StringBuilder message = new StringBuilder("Can't parse rules.xml.");
+        StringBuilder message = new StringBuilder("Can't parse project descriptor file" + ProjectDescriptorBasedResolvingStrategy.PROJECT_DESCRIPTOR_FILE_NAME + '.');
         if (e.getCause() instanceof SAXParseException) {
             SAXParseException parseException = (SAXParseException) e.getCause();
             message.append(" Line number: ").append(parseException.getLineNumber())
