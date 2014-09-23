8d62c9e0cc6fd0f85c999054b38f9e1e02961c04
Yury Molchan <ymolchan@eisgroup.com>
2014-09-23 11:41:17 +0300

    EPBDS-5453 Removed cross-child result items from the DecisionTableTraceObject
    It simplifies understanding of tracing logic

--- STUDIO/org.openl.rules.webstudio/src/org/openl/rules/ui/tree/richfaces/TraceTreeBuilder.java
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/ui/tree/richfaces/TraceTreeBuilder.java
@@ -2,11 +2,15 @@ package org.openl.rules.ui.tree.richfaces;
 
 import org.openl.commons.web.jsf.FacesUtils;
 import org.openl.rules.dt.trace.DTConditionTraceObject;
+import org.openl.rules.dt.trace.DTRuleTracerLeaf;
 import org.openl.rules.dt.trace.DecisionTableTraceObject;
 import org.openl.rules.ui.TraceHelper;
 import org.openl.rules.webstudio.web.util.Constants;
 import org.openl.rules.webstudio.web.util.WebStudioUtils;
 import org.openl.util.tree.ITreeElement;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 public class TraceTreeBuilder extends TreeBuilder {
 
@@ -41,11 +45,24 @@ public class TraceTreeBuilder extends TreeBuilder {
 
     @Override
     Iterable<? extends ITreeElement<?>> getChildrenIterator(ITreeElement<?> source) {
-        if (source instanceof DecisionTableTraceObject) {
-            DecisionTableTraceObject parent = (DecisionTableTraceObject) source;
-            return detailedTraceTree ? parent.getChildren() : parent.getTraceResults();
+        if (source instanceof DecisionTableTraceObject && !detailedTraceTree) {
+            ITreeElement<?> resultNode = findResult(source.getChildren());
+            return resultNode == null ? Collections.EMPTY_LIST : Arrays.asList(resultNode);
         }
 
         return super.getChildrenIterator(source);
+    }
+
+    private ITreeElement<?> findResult(Iterable<? extends ITreeElement<?>> children) {
+        for (ITreeElement<?> child : children) {
+            if (child instanceof DTRuleTracerLeaf) {
+                return child;
+            }
+            ITreeElement<?> result = findResult(child.getChildren());
+            if (result != null) {
+                return result;
+            }
+        }
+        return null;
     }
 }
