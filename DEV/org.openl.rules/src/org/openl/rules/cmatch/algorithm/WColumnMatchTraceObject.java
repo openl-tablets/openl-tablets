0f4e08dd14bea62213c990846b3af981469cd7bc
Yury Molchan <ymolchan@eisgroup.com>
2014-09-23 17:04:59 +0300

    EPBDS-5453 Moved up WColumnMatchTraceObject

--- DEV/org.openl.rules/src/org/openl/rules/cmatch/algorithm/WColumnMatchTraceObject.java
+++ DEV/org.openl.rules/src/org/openl/rules/cmatch/algorithm/WColumnMatchTraceObject.java
@@ -1,9 +1,13 @@
 package org.openl.rules.cmatch.algorithm;
 
 import org.openl.rules.cmatch.ColumnMatch;
+import org.openl.rules.table.ATableTracerNode;
+import org.openl.rules.table.IGridRegion;
 import org.openl.types.IOpenMethod;
 
-public class WColumnMatchTraceObject extends ColumnMatchTraceObject {
+import java.util.List;
+
+public class WColumnMatchTraceObject extends ATableTracerNode {
 
     public WColumnMatchTraceObject(ColumnMatch columnMatch, Object[] params) {
         super(columnMatch, params);
@@ -12,6 +16,11 @@ public class WColumnMatchTraceObject extends ColumnMatchTraceObject {
     @Override
     public String getDisplayName(int mode) {
         return "WCM " + asString((IOpenMethod) getTraceObject(), mode);
+    }
+
+    public List<IGridRegion> getGridRegions() {
+        // TODO Auto-generated method stub
+        return null;
     }
 
     @Override
