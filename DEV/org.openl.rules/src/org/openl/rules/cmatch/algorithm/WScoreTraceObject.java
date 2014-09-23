6cda8bd19707414b560bbb884212be04b84ff956
Yury Molchan <ymolchan@eisgroup.com>
2014-09-23 16:10:20 +0300

    EPBDS-5453 Removed duplication of getTableSyntaxNode() method

--- DEV/org.openl.rules/src/org/openl/rules/cmatch/algorithm/WScoreTraceObject.java
+++ DEV/org.openl.rules/src/org/openl/rules/cmatch/algorithm/WScoreTraceObject.java
@@ -1,20 +1,16 @@
 package org.openl.rules.cmatch.algorithm;
 
 import org.openl.rules.cmatch.ColumnMatch;
-import org.openl.rules.lang.xls.syntax.TableSyntaxNode;
 import org.openl.rules.table.ATableTracerNode;
 import org.openl.rules.table.IGridRegion;
 
 import java.util.List;
 
 public class WScoreTraceObject extends ATableTracerNode {
-    private ColumnMatch columnMatch;
     private int score;
 
     public WScoreTraceObject(ColumnMatch columnMatch, Object[] params) {
         super(columnMatch, params);
-
-        this.columnMatch = columnMatch;
     }
 
     public String getDisplayName(int mode) {
@@ -24,11 +20,6 @@ public class WScoreTraceObject extends ATableTracerNode {
     public List<IGridRegion> getGridRegions() {
         // TODO Auto-generated method stub
         return null;
-    }
-
-    @Override
-    public TableSyntaxNode getTableSyntaxNode() {
-        return columnMatch.getSyntaxNode();
     }
 
     public String getType() {
