d4040c9982da419ea00325dbc5a5445045f61052
Yury Molchan <ymolchan@eisgroup.com>
2014-09-29 17:12:59 +0300

    EPBDS-5322 Inlined ComparatorTraceDecorator

--- DEV/org.openl.rules/src/org/openl/rules/dt/index/TraceableEqualsIndex.java
+++ DEV/org.openl.rules/src/org/openl/rules/dt/index/TraceableEqualsIndex.java
@@ -1,14 +1,15 @@
 package org.openl.rules.dt.index;
 
-import java.util.HashMap;
-import java.util.SortedMap;
-import java.util.TreeMap;
-
 import org.openl.rules.dt.DecisionTableRuleNode;
 import org.openl.rules.dt.element.ICondition;
 import org.openl.rules.dt.trace.DTIndexedTraceObject;
 import org.openl.rules.dt.trace.DecisionTableTraceObject;
 import org.openl.vm.trace.TraceStack;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 public class TraceableEqualsIndex extends EqualsIndex {
     private final ICondition condition;
@@ -16,20 +17,28 @@ public class TraceableEqualsIndex extends EqualsIndex {
     private final TraceStack traceStack;
 
     public TraceableEqualsIndex(EqualsIndex delegate,
-            ICondition condition,
-            DecisionTableTraceObject baseTraceObject,
-            TraceStack traceStack) {
+                                ICondition condition,
+                                DecisionTableTraceObject baseTraceObject,
+                                TraceStack traceStack) {
         super(delegate.emptyOrFormulaNodes, null);
         this.condition = condition;
         this.baseTraceObject = baseTraceObject;
         this.traceStack = traceStack;
 
         if (delegate.valueNodes instanceof SortedMap) {
-            SortedMap<?, ?> sortedMap = (SortedMap<?, ?>) delegate.valueNodes;
+            final SortedMap<Object, ?> sortedMap = (SortedMap<Object, ?>) delegate.valueNodes;
             if (sortedMap.comparator() == null) {
                 valueNodes = new TreeMap<Object, DecisionTableRuleNode>();
             } else {
-                valueNodes = new TreeMap<Object, DecisionTableRuleNode>(new ComparatorTraceDecorator(sortedMap.comparator()));
+                valueNodes = new TreeMap<Object, DecisionTableRuleNode>(new Comparator<Object>() {
+                    @Override
+                    public int compare(Object o1, Object o2) {
+                        Object p1 = ((ComparableIndexTraceDecorator<?>) o1).delegate;
+                        Object p2 = ((ComparableIndexTraceDecorator<?>) o2).delegate;
+                        int result = sortedMap.comparator().compare(p1, p2);
+                        return result;
+                    }
+                });
             }
         } else {
             valueNodes = new HashMap<Object, DecisionTableRuleNode>(delegate.valueNodes.size());
@@ -38,12 +47,12 @@ public class TraceableEqualsIndex extends EqualsIndex {
         for (Object key : delegate.valueNodes.keySet()) {
             DecisionTableRuleNode linkedRule = delegate.valueNodes.get(key);
 
-            @SuppressWarnings({ "unchecked" })
+            @SuppressWarnings({"unchecked"})
             Comparable<?> newKey = new ComparableIndexTraceDecorator<Object>((Comparable<Object>) key,
-                linkedRule,
-                condition,
-                baseTraceObject,
-                traceStack);
+                    linkedRule,
+                    condition,
+                    baseTraceObject,
+                    traceStack);
 
             valueNodes.put(newKey, linkedRule);
         }
