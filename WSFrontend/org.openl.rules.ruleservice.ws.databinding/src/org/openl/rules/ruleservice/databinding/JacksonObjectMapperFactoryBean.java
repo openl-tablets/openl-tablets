89ea0807d62b553e818fa5bd144e3e713849bd93
Marat Kamalov <none@none>
2014-11-13 22:31:17 +0300

    EPBDS-5555 Implement RESTful service full support. Add Jackson Draft commit.

---
+++ WSFrontend/org.openl.rules.ruleservice.ws.databinding/src/org/openl/rules/ruleservice/databinding/JacksonObjectMapperFactoryBean.java
@@ -0,0 +1,76 @@
+package org.openl.rules.ruleservice.databinding;
+
+/*
+ * #%L
+ * OpenL - RuleService - RuleService - Web Services Databinding
+ * %%
+ * Copyright (C) 2013 - 2014 OpenL Tablets
+ * %%
+ * See the file LICENSE.txt for copying permission.
+ * #L%
+ */
+
+
+import org.openl.meta.BigDecimalValue;
+import org.openl.meta.BigIntegerValue;
+import org.openl.meta.ByteValue;
+import org.openl.meta.DoubleValue;
+import org.openl.meta.FloatValue;
+import org.openl.meta.IntValue;
+import org.openl.meta.LongValue;
+import org.openl.meta.ShortValue;
+import org.openl.meta.StringValue;
+import org.openl.rules.ruleservice.databinding.jackson.org.openl.meta.BigDecimalValueType.BigDecimalValueDeserializer;
+import org.openl.rules.ruleservice.databinding.jackson.org.openl.meta.BigDecimalValueType.BigDecimalValueSerializer;
+import org.openl.rules.ruleservice.databinding.jackson.org.openl.meta.BigIntegerValueType.BigIntegerValueDeserializer;
+import org.openl.rules.ruleservice.databinding.jackson.org.openl.meta.BigIntegerValueType.BigIntegerValueSerializer;
+import org.openl.rules.ruleservice.databinding.jackson.org.openl.meta.ByteValueType.ByteValueDeserializer;
+import org.openl.rules.ruleservice.databinding.jackson.org.openl.meta.ByteValueType.ByteValueSerializer;
+import org.openl.rules.ruleservice.databinding.jackson.org.openl.meta.DoubleValueType.DoubleValueDeserializer;
+import org.openl.rules.ruleservice.databinding.jackson.org.openl.meta.DoubleValueType.DoubleValueSerializer;
+import org.openl.rules.ruleservice.databinding.jackson.org.openl.meta.FloatValueType.FloatValueDeserializer;
+import org.openl.rules.ruleservice.databinding.jackson.org.openl.meta.FloatValueType.FloatValueSerializer;
+import org.openl.rules.ruleservice.databinding.jackson.org.openl.meta.IntValueType.IntValueDeserializer;
+import org.openl.rules.ruleservice.databinding.jackson.org.openl.meta.IntValueType.IntValueSerializer;
+import org.openl.rules.ruleservice.databinding.jackson.org.openl.meta.LongValueType.LongValueDeserializer;
+import org.openl.rules.ruleservice.databinding.jackson.org.openl.meta.LongValueType.LongValueSerializer;
+import org.openl.rules.ruleservice.databinding.jackson.org.openl.meta.ShortValueType.ShortValueDeserializer;
+import org.openl.rules.ruleservice.databinding.jackson.org.openl.meta.ShortValueType.ShortValueSerializer;
+import org.openl.rules.ruleservice.databinding.jackson.org.openl.meta.StringValueType.StringValueDeserializer;
+import org.openl.rules.ruleservice.databinding.jackson.org.openl.meta.StringValueType.StringValueSerializer;
+
+import com.fasterxml.jackson.core.Version;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+
+public class JacksonObjectMapperFactoryBean {
+    public ObjectMapper createAegisDatabinding() {
+        ObjectMapper mapper = new ObjectMapper();
+        
+        mapper.enableDefaultTyping();
+        
+        SimpleModule valueTypesModule = new SimpleModule("OpenL Value Types", Version.unknownVersion());
+        // Value Types binding configuration
+        valueTypesModule.addSerializer(ByteValue.class, new ByteValueSerializer());
+        valueTypesModule.addSerializer(ShortValue.class, new ShortValueSerializer());
+        valueTypesModule.addSerializer(IntValue.class, new IntValueSerializer());
+        valueTypesModule.addSerializer(LongValue.class, new LongValueSerializer());
+        valueTypesModule.addSerializer(FloatValue.class, new FloatValueSerializer());
+        valueTypesModule.addSerializer(DoubleValue.class, new DoubleValueSerializer());
+        valueTypesModule.addSerializer(BigIntegerValue.class, new BigIntegerValueSerializer());
+        valueTypesModule.addSerializer(BigDecimalValue.class, new BigDecimalValueSerializer());
+        valueTypesModule.addSerializer(StringValue.class, new StringValueSerializer());
+
+        valueTypesModule.addDeserializer(ByteValue.class, new ByteValueDeserializer());
+        valueTypesModule.addDeserializer(ShortValue.class, new ShortValueDeserializer());
+        valueTypesModule.addDeserializer(IntValue.class, new IntValueDeserializer());
+        valueTypesModule.addDeserializer(LongValue.class, new LongValueDeserializer());
+        valueTypesModule.addDeserializer(FloatValue.class, new FloatValueDeserializer());
+        valueTypesModule.addDeserializer(DoubleValue.class, new DoubleValueDeserializer());
+        valueTypesModule.addDeserializer(BigIntegerValue.class, new BigIntegerValueDeserializer());
+        valueTypesModule.addDeserializer(BigDecimalValue.class, new BigDecimalValueDeserializer());
+        valueTypesModule.addDeserializer(StringValue.class, new StringValueDeserializer());
+
+        return mapper;
+    }
+}
---
+++ WSFrontend/org.openl.rules.ruleservice.ws.databinding/src/org/openl/rules/ruleservice/databinding/aegis/org/openl/rules/variation/DeepCloningVariationType.java
@@ -0,0 +1,82 @@
+package org.openl.rules.ruleservice.databinding.aegis.org.openl.rules.variation;
+
+/*
+ * #%L
+ * OpenL - RuleService - RuleService - Web Services Databinding
+ * %%
+ * Copyright (C) 2013 OpenL Tablets
+ * %%
+ * See the file LICENSE.txt for copying permission.
+ * #L%
+ */
+
+
+import javax.xml.namespace.QName;
+
+import org.apache.cxf.aegis.Context;
+import org.apache.cxf.aegis.DatabindingException;
+import org.apache.cxf.aegis.type.AegisType;
+import org.apache.cxf.aegis.type.TypeUtil;
+import org.apache.cxf.aegis.type.basic.BeanType;
+import org.apache.cxf.aegis.type.basic.BeanTypeInfo;
+import org.apache.cxf.aegis.type.java5.Java5TypeCreator;
+import org.apache.cxf.aegis.xml.MessageReader;
+import org.openl.rules.variation.DeepCloningVariaion;
+import org.openl.rules.variation.Variation;
+
+/**
+ * Custom mapping for {@link DeepCloningVariaion} due to it is not usual bean
+ * and should be initialized through non-default constructor.
+ * 
+ * @author PUdalau
+ */
+public class DeepCloningVariationType extends BeanType {
+
+    public static final Class<?> TYPE_CLASS = DeepCloningVariaion.class;
+
+    public static final QName QNAME = new Java5TypeCreator().createQName(TYPE_CLASS);
+
+    public DeepCloningVariationType() {
+        super();
+        setTypeClass(TYPE_CLASS);
+        setSchemaType(QNAME);
+    }
+
+    @Override
+    public Object readObject(MessageReader reader, Context context) throws DatabindingException {
+        BeanTypeInfo inf = getTypeInfo();
+
+        try {
+            String variationID = "";
+            Variation variation = null;
+            // Read child elements
+            while (reader.hasMoreElementReaders()) {
+                MessageReader childReader = reader.getNextElementReader();
+                if (childReader.isXsiNil()) {
+                    childReader.readToEnd();
+                    continue;
+                }
+                QName qName = childReader.getName();
+                AegisType defaultType = inf.getType(qName);
+                AegisType type = TypeUtil.getReadType(childReader.getXMLStreamReader(),
+                    context.getGlobalContext(),
+                    defaultType);
+                if (type != null) {
+                    String propertyName = qName.getLocalPart();
+                    Object propertyValue = type.readObject(childReader, context);
+                    if ("variationID".equals(propertyName)) {
+                        variationID = String.valueOf(propertyValue);
+                    } else if ("delegatedVariation".equals(propertyName)) {
+                        variation = (Variation) propertyValue;
+                    }
+                } else {
+                    childReader.readToEnd();
+                }
+            }
+
+            return new DeepCloningVariaion(variationID, variation);
+        } catch (IllegalArgumentException e) {
+            throw new DatabindingException("Illegal argument. " + e.getMessage(), e);
+        }
+    }
+}
