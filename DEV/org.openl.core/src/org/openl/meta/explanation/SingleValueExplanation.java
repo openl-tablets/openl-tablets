a6a6ff1b3863e456aa5c9ff34e30676678fcfda0
Yury Molchan <ymolchan@eisgroup.com>
2015-01-28 19:48:49 +0300

    EPBDS-5734 Deleted unused constants

--- DEV/org.openl.core/src/org/openl/meta/explanation/SingleValueExplanation.java
+++ DEV/org.openl.core/src/org/openl/meta/explanation/SingleValueExplanation.java
@@ -1,5 +1,7 @@
 package org.openl.meta.explanation;
 
+import java.util.Collections;
+
 import org.openl.meta.IMetaInfo;
 import org.openl.meta.ValueMetaInfo;
 import org.openl.meta.number.NumberValue;
@@ -7,33 +9,21 @@ import org.openl.meta.number.NumberValue.ValueType;
 import org.openl.util.AOpenIterator;
 import org.openl.util.tree.ITreeElement;
 
-import java.util.Collections;
-
 /**
  * Explanation implementation for number values that are of type {@link ValueType#SINGLE_VALUE}, see also
  * {@link NumberValue#getValueType()}.
  * 
  * @author DLiauchuk
  *
- * @param <T> type that extends {@link ExplanationNumberValue} 
+ * @param <T> type that extends {@link ExplanationNumberValue}
  */
 public class SingleValueExplanation<T extends ExplanationNumberValue<T>> implements ExplanationForNumber<T> {
-    
-    protected static final int VALUE = 0x01;
-    protected static final int SHORT_NAME = 0x02;
-    protected static final int LONG_NAME = 0x04;
-    protected static final int URL = 0x08;
-    protected static final int EXPAND_FORMULA = 0x10;
-    protected static final int EXPAND_FUNCTION = 0x20;
-    protected static final int PRINT_VALUE_IN_EXPANDED = 0x40;
-    protected static final int EXPAND_ALL = EXPAND_FORMULA | EXPAND_FUNCTION | PRINT_VALUE_IN_EXPANDED;
-    protected static final int PRINT_ALL = EXPAND_ALL | LONG_NAME;
 
     private IMetaInfo metaInfo;
     
-    public SingleValueExplanation() {        
+    public SingleValueExplanation() {
     }
-    
+
     public SingleValueExplanation(IMetaInfo metaInfo) {
         this.metaInfo = metaInfo;
     }
@@ -43,7 +33,7 @@ public class SingleValueExplanation<T extends ExplanationNumberValue<T>> impleme
         mi.setShortName(name);
         metaInfo = mi;
     }
-    
+
     public IMetaInfo getMetaInfo() {
         return metaInfo;
     }
@@ -74,7 +64,7 @@ public class SingleValueExplanation<T extends ExplanationNumberValue<T>> impleme
     public String printValue() {
         return getName();
     }
-    
+
     public void setFullName(String name) {
         if (metaInfo == null) {
             metaInfo = new ValueMetaInfo();
@@ -104,9 +94,10 @@ public class SingleValueExplanation<T extends ExplanationNumberValue<T>> impleme
     public String getType() {
         return ValueType.SINGLE_VALUE.toString();
     }
-    
+
     /**
-     * default implementation. right implementation should be in the object that will be shown.  
+     * default implementation. right implementation should be in the object that
+     * will be shown.
      */
     public T getObject() {
         throw new UnsupportedOperationException("Should be overriden in childs");
