db5bbcd2cecbed3d8f3cfdec9a39006df648f9ae
Yury Molchan <ymolchan@eisgroup.com>
2015-02-02 20:28:34 +0300

    EPBDS-5734 Merged NumberCast into CastExplanationValue

--- DEV/org.openl.core/src/org/openl/meta/explanation/CastExplanationValue.java
+++ DEV/org.openl.core/src/org/openl/meta/explanation/CastExplanationValue.java
@@ -1,28 +1,35 @@
 package org.openl.meta.explanation;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
-import org.openl.meta.number.NumberCast;
+import org.openl.meta.number.CastOperand;
 
 @SuppressWarnings("rawtypes")
 public class CastExplanationValue extends SingleValueExplanation {
-    private NumberCast castHolder;
+    private ExplanationNumberValue<?> value;
+    private CastOperand operand;
 
-    public CastExplanationValue(NumberCast castHolder) {
-        this.castHolder = castHolder;
+    public CastExplanationValue(ExplanationNumberValue<?> value, CastOperand operand) {
+        this.value = value;
+        this.operand = operand;
+    }
+
+    public ExplanationNumberValue<?> getValue() {
+        return value;
+    }
+
+    public CastOperand getOperand() {
+        return operand;
     }
 
     @Override
     public Iterable<? extends org.openl.util.tree.ITreeElement> getChildren() {
-        List<ExplanationNumberValue<?>> list = new ArrayList<ExplanationNumberValue<?>>();
-        list.add(castHolder.getValue());
-        return list;
+        return Arrays.asList(value);
     }
 
     @Override
     public String getType() {
-        return String.format("cast.%s", castHolder.getOperand().getType());
+        return String.format("cast.%s", operand.getType());
     }
 
     @Override
