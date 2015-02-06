fc28e12e597ebaf00ed9611922e4826c65902ecc
stanislav.shor <SShor@exigeninsurance.com>
2015-02-06 00:53:18 -0500

    Initial implementation of dt2 memeory optimization

---
+++ DEV/org.openl.rules/src/org/openl/rules/dt2/element/IDecisionRow.java
@@ -0,0 +1,75 @@
+/*
+ * Created on Nov 7, 2003
+ *
+ * Developed by Intelligent ChoicePoint Inc. 2003
+ */
+
+package org.openl.rules.dt2.element;
+
+import org.openl.OpenL;
+import org.openl.binding.IBindingContextDelegator;
+import org.openl.binding.impl.component.ComponentOpenClass;
+import org.openl.rules.table.ILogicalTable;
+import org.openl.source.IOpenSourceCodeModule;
+import org.openl.types.IMethodSignature;
+import org.openl.types.IOpenClass;
+import org.openl.types.IOpenMethod;
+import org.openl.types.IParameterDeclaration;
+import org.openl.vm.IRuntimeEnv;
+
+/**
+ * @author snshor
+ * 
+ */
+public interface IDecisionRow {
+
+    String getName();
+
+    IOpenMethod getMethod();
+    
+    IOpenSourceCodeModule getSourceCodeModule();
+
+//    DecisionTableParameterInfo findParameterInfo(String name);
+
+    DecisionTableParameterInfo getParameterInfo(int i);
+
+    String[] getParamPresentation();
+
+    IParameterDeclaration[] getParams();
+
+    ILogicalTable getValueCell(int column);
+    
+    Object getParamValue(int paramIndex, int ruleN);
+	boolean isEmpty(int ruleN);
+	boolean hasFormula(int ruleN);
+	int getNumberOfRules();
+	int getNumberOfParams();
+	void loadValues(Object[] dest, int offset, int ruleN, Object target,
+			Object[] tableParams, IRuntimeEnv env);
+
+
+    
+    
+
+    void clearParamValues();
+
+    int numberOfParams();
+
+    boolean isAction();
+
+    boolean isCondition();
+
+    void prepare(IOpenClass methodType,
+            IMethodSignature signature,
+            OpenL openl,
+            ComponentOpenClass componentModule,
+            IBindingContextDelegator bindingContextDelegator,
+            RuleRow ruleRow) throws Exception;
+
+    /**
+     * @return Parsed table that contains this decision row.
+     */
+    ILogicalTable getDecisionTable();
+
+
+}
---
+++ DEV/org.openl.rules/src/org/openl/rules/dt2/index/EqualsIndex.java
@@ -0,0 +1,34 @@
+package org.openl.rules.dt2.index;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.openl.rules.dt2.DecisionTableRuleNode;
+
+public class EqualsIndex extends ARuleIndex {
+
+    protected Map<Object, DecisionTableRuleNode> valueNodes;
+
+    public EqualsIndex(DecisionTableRuleNode emptyOrFormulaNodes, Map<Object, DecisionTableRuleNode> valueNodes) {
+        super(emptyOrFormulaNodes);
+        if (valueNodes == null){
+            this.valueNodes = new HashMap<Object, DecisionTableRuleNode>();
+        }else{
+            this.valueNodes = valueNodes;
+        }
+    }
+
+    @Override
+    public DecisionTableRuleNode findNodeInIndex(Object value) {
+        if (value != null) {
+            return valueNodes.get(value);
+        }
+        return null;
+    }
+    
+    @Override
+    public Iterator<DecisionTableRuleNode> nodes() {
+        return valueNodes.values().iterator();
+    }
+}
