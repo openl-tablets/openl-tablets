41a33183f29aaf4cb623478e21982b32d541588c
stanislav.shor <SShor@exigeninsurance.com>
2015-02-06 00:14:38 -0500

    Remove potential memory leak in String2DataConvertorFactory

--- DEV/org.openl.rules/test/org/openl/rules/convertor/String2OpenClassConvertorTest.java
+++ DEV/org.openl.rules/test/org/openl/rules/convertor/String2OpenClassConvertorTest.java
@@ -9,7 +9,7 @@ public class String2OpenClassConvertorTest {
     @Test
     public void testParseNull() {
         String2OpenClassConvertor converter = new String2OpenClassConvertor();
-        assertNull(converter.parse(null, null));
+        assertNull(converter.parse(null, null, null));
     }
 
     @Test
