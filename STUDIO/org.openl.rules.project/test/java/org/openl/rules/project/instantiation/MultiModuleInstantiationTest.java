f1d5c844a5a3cfa833a0bd364878f289a6cef3c7
Yury Molchan <ymolchan@eisgroup.com>
2015-02-13 16:00:11 +0300

    EPBDS-5730 Replace *Value types with java types in the execution mode

--- STUDIO/org.openl.rules.project/test/java/org/openl/rules/project/instantiation/MultiModuleInstantiationTest.java
+++ STUDIO/org.openl.rules.project/test/java/org/openl/rules/project/instantiation/MultiModuleInstantiationTest.java
@@ -12,7 +12,6 @@ import java.util.List;
 import java.util.Map;
 
 import org.junit.Test;
-import org.openl.meta.DoubleValue;
 import org.openl.rules.context.IRulesRuntimeContext;
 import org.openl.rules.context.RulesRuntimeContextFactory;
 import org.openl.rules.project.model.Module;
@@ -93,7 +92,7 @@ public class MultiModuleInstantiationTest {
                 String.class });
         Object result = method.invoke(instance, new Object[] { context, "High Risk Driver" });
 
-        assertEquals(new DoubleValue(400), result);
+        assertEquals(new Double(400), result);
     }
 
     private List<Module> listModulesInFolder(File folder) {
