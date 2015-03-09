55b67061048b27b37342a7a2d2914a125639edb7
Alexander Ryabov <aryabov@eisgroup.com>
2015-03-09 11:04:14 +0100

    tests

--- DEV/org.openl.rules/test/org/openl/rules/helpers/IntRangeParser2Test.java
+++ DEV/org.openl.rules/test/org/openl/rules/helpers/IntRangeParser2Test.java
@@ -15,12 +15,91 @@ import static org.openl.rules.helpers.IntRangeParser2.parse;
 public class IntRangeParser2Test {
     private static final Random random = new Random(0);
 
-    static String spaced(String value) {
-        return randomSpaces() + value + randomSpaces();
-    }
-
     private static final String[] NUMBERS_STR = {"-1 B",     "-321K", "-1", "0", "1", "1234567", "999M"};
     private static final int[] NUMBERS_VALUES = {-1000000000, -321000, -1,  0,   1,    1234567,  999000000};
+
+    @Test
+    public void testArbitrary() {
+        assertIntRangeEquals(
+            // simple numbers
+            "-1",                               -1, -1,
+            "0",                                0, 0,
+            "1",                                1, 1,
+            "1234567",                          1234567, 1234567,
+            "-321K",                            -321000, -321000,
+            "999M",                             999000000, 999000000,
+            "-1 B",                             -1000000000, -1000000000,
+
+            // ranges
+            "[-100;100]",                       -100, 100,
+            "(-100;100]",                       -99, 100,
+            "(-0;10]",                          1, 10,
+            "[0; 10)",                          0, 9,
+            "[-10; -1]",                        -10, -1,
+            "[1; 2]",                           1, 2,
+            "[3.. 4]",                          3, 4,
+            "[5… 6]",                          5, 6,
+            "[7-8]",                            7, 8,
+            "6-8",                              6, 8,
+
+            // plus and minus
+            "1+",                               1, MAX_VALUE,
+            "-1+",                              -1, MAX_VALUE,
+            "1+ and 10 or less",                1, 10,
+
+            // one less/greater condition
+            "0>",                               MIN_VALUE, -1,
+            ">=1",                              1, MAX_VALUE,
+            ">1",                               2, MAX_VALUE,
+            "<10",                              MIN_VALUE, 9,
+            "<=10",                             MIN_VALUE, 10,
+
+            // two  less/greater conditions
+            ">0 and <10",                       1, 9,
+            "<=10 and >=0",                     0, 10,
+            "<=10 and >=0 and > 5",             6, 10,
+
+            // less more
+            "0 and more",                       0, MAX_VALUE,
+            "1 or less",                        MIN_VALUE, 1,
+            "0 and more and 100 or less",       0, 100,
+            "7 or less and -1 and more",        -1, 7,
+            "less than 0 ",                     MIN_VALUE, -1,
+            "more than 0 ",                     1, MAX_VALUE,
+            "more than -1 less than 11",        0, 10,
+            "more than 1000 and less than 10000", 1001, 9999,
+            "less than -1 more than -1K",       -999, -2,
+            "-100 and more",                    -100, MAX_VALUE,
+            "less than -10",                    MIN_VALUE, -11,
+            "more than 2",                      3, MAX_VALUE,
+            "-100 and more less than 500",      -100, 499,
+            "-100 and more and less than 500",  -100, 499,
+            "41 or less and more than 31",      32, 41,
+            "less than -10 more than -20",      -19, -11,
+            "more than 2 5 or less",            3, 5,
+            "-100 and more less than 500",      -100, 499,
+
+            // thousands separator
+            "123,456",                          123456, 123456,
+            "-123,456 - 987,654",               -123456, 987654,
+            ">123,456",                         123457, MAX_VALUE,
+            ">=123,456 <=987,654",              123456, 987654,
+            "123,456 and more 987,654 or less", 123456, 987654,
+            "[123,456 - 987,654)",              123456, 987653,
+            "123,456 and more 987,654 or less", 123456, 987654,
+
+            // overflow test
+            "2147483647",                       2147483647, 2147483647,
+            "-2147483648",                      -2147483648, -2147483648,
+            "-2147483649",                      null,
+            "-10000000 K",                      null,
+
+            // empty input test
+            "",                                 null,
+            " ",                                null,
+            "  ",                               null
+        );
+    }
 
     @Test
     public void testSingleNumbers() {
@@ -30,13 +109,6 @@ public class IntRangeParser2Test {
 
     @Test
     public void testOpenClosedIntervals() {
-        assertIntRangeEquals(
-            "[-100;100]", -100, 100,
-            "(-100;100]", -99, 100,
-            "(-0;10]",    1, 10,
-            "[0; 10)",    0, 9,
-            "[-10; -1]", -10, -1
-        );
         // Now do the real testing
         for (boolean leftOpen : new boolean[]{false, true})
             for (boolean rightOpen : new boolean[]{false, true})
@@ -47,28 +119,6 @@ public class IntRangeParser2Test {
                                 String str = spaced(leftOpen ? "(" : "[") + NUMBERS_STR[i] + spaced(separator) + NUMBERS_STR[j] + spaced(rightOpen ? ")" : "]");
                                 assertEquals(new IntRange(leftOpen ? NUMBERS_VALUES[i] + 1 : NUMBERS_VALUES[i], rightOpen ? NUMBERS_VALUES[j] - 1 : NUMBERS_VALUES[j]), parse(str));
                             }
-    }
-
-
-    @Test
-    public void testSingleLessGreater() {
-        assertIntRangeEquals(
-            "0>",   MIN_VALUE, -1,
-            ">=1",  1, MAX_VALUE,
-            ">1",   2, MAX_VALUE,
-            "<10",  MIN_VALUE, 9,
-            "<=10", MIN_VALUE, 10
-        );
-    }
-
-    @Test
-    public void testAndMoreOrLess() {
-        assertIntRangeEquals(
-                "0 and more", 0, MAX_VALUE,
-                "1 or less", MIN_VALUE, 1,
-                "0 and more and 100 or less", 0, 100,
-                "7 or less and -1 and more", -1, 7
-        );
     }
 
     @Test
@@ -87,75 +137,14 @@ public class IntRangeParser2Test {
             }
     }
 
-    @Test
-    public void testMoreThanNumLessThanNum() {
-        assertIntRangeEquals(
-                "less than 0 ", MIN_VALUE, -1,
-                "more than 0 ", 1, MAX_VALUE,
-                "more than -1 less than 11", 0, 10,
-                "more than 1000 and less than 10000", 1001, 9999,
-                "less than -1 more than -1K", -999, -2
-        );
-    }
-
-    @Test
-    public void testDoubleLessGreater() {
-        assertIntRangeEquals(
-            ">0 and <10", 1, 9,
-            "<=10 and >=0", 0, 10,
-            "<=10 and >=0 and > 5", 6, 10
-        );
-    }
-
-    @Test
-    public void testEmptyInput() {
-        assertIntRangeEquals(
-            "", null,
-            " ", null,
-            "  ", null
-        );
-    }
-
-    @Test
-    public void testOverflow() {
-        assertIntRangeEquals(
-            "2147483647",  2147483647, 2147483647,
-            "-2147483648", -2147483648, -2147483648,
-            "-2147483649", null,
-            "-10000000 K", null
-        );
-    }
-
-    @Test
-    public void testArbitrary() {
-        assertIntRangeEquals(
-            "6-8",                              6, 8,
-
-            // thousands separator
-            "123,456",                          123456, 123456,
-            "-123,456 - 987,654",               -123456, 987654,
-            ">123,456",                         123457, MAX_VALUE,
-            ">=123,456 <=987,654",              123456, 987654,
-            "123,456 and more 987,654 or less", 123456, 987654,
-            "[123,456 - 987,654)",              123456, 987653,
-            "123,456 and more 987,654 or less", 123456, 987654,
-
-            "-100 and more",                    -100, MAX_VALUE,
-            "less than -10",                    MIN_VALUE, -11,
-            "more than 2",                      3, MAX_VALUE,
-            "-100 and more less than 500",      -100, 499,
-            "-100 and more and less than 500",  -100, 499,
-            "41 or less and more than 31",      32, 41,
-            "less than -10 more than -20",      -19, -11,
-            "more than 2 5 or less",            3, 5,
-            "-100 and more less than 500",      -100, 499
-        );
-    }
-
 
     @Test
     public void testEmptyRange() {
         // assertEquals(new IntRange(0, -10), parse("[0;-10]"));
+    }
+
+    static String spaced(String value) {
+        return randomSpaces() + value + randomSpaces();
     }
 
     static String concat(String... args) {
