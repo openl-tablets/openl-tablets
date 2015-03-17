c92af60c4e80cdcf557005dee3a66995bedd90d9
stanislav.shor <SShor@exigeninsurance.com>
2015-03-17 00:24:58 -0400

    Handling merged cells in the first condition. Added MC condition header to mark  first conditions with merged cells

--- DEV/org.openl.rules/src/org/openl/rules/table/IWritableGrid.java
+++ DEV/org.openl.rules/src/org/openl/rules/table/IWritableGrid.java
@@ -4,27 +4,9 @@
 package org.openl.rules.table;
 
 import java.io.OutputStream;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.openl.rules.lang.xls.types.CellMetaInfo;
-import org.openl.rules.table.actions.AUndoableCellAction;
-import org.openl.rules.table.actions.GridRegionAction;
-import org.openl.rules.table.actions.IUndoableGridTableAction;
-import org.openl.rules.table.actions.MergeCellsAction;
-import org.openl.rules.table.actions.SetBorderStyleAction;
-import org.openl.rules.table.actions.UndoableClearAction;
-import org.openl.rules.table.actions.UndoableCompositeAction;
-import org.openl.rules.table.actions.UndoableCopyValueAction;
-import org.openl.rules.table.actions.UndoableResizeMergedRegionAction;
-import org.openl.rules.table.actions.UndoableSetValueAction;
-import org.openl.rules.table.actions.UndoableShiftValueAction;
-import org.openl.rules.table.actions.UnmergeByColumnsAction;
-import org.openl.rules.table.actions.GridRegionAction.ActionType;
-import org.openl.rules.table.ui.CellStyle;
 import org.openl.rules.table.ui.ICellStyle;
-
-import org.openl.util.formatters.IFormatter;
 
 /**
  * @author snshor
