b6f6da8bf320f74dbff6333ebb0d65812a917081
Yury Molchan <ymolchan@eisgroup.com>
2015-06-02 23:10:01 +0300

    EPBDS-5881 Delete unused arguments

--- DEV/org.openl.rules/src/org/openl/rules/calc/element/SpreadsheetCellField.java
+++ DEV/org.openl.rules/src/org/openl/rules/calc/element/SpreadsheetCellField.java
@@ -4,7 +4,6 @@ import org.openl.rules.calc.ASpreadsheetField;
 import org.openl.rules.calc.SpreadsheetResultCalculator;
 import org.openl.rules.table.Point;
 import org.openl.types.IOpenClass;
-import org.openl.types.IOpenMethodHeader;
 import org.openl.vm.IRuntimeEnv;
 
 public class SpreadsheetCellField extends ASpreadsheetField {
@@ -13,17 +12,15 @@ public class SpreadsheetCellField extends ASpreadsheetField {
     private SpreadsheetStructureBuilderHolder structureBuilderContainer;
 
     public static SpreadsheetCellField createSpreadsheetCellField(SpreadsheetStructureBuilderHolder structureBuilderContainer,
-            IOpenMethodHeader spreadsheetHeader,
             IOpenClass declaringClass,
             String name,
             SpreadsheetCell cell) {
         if (cell.getKind() == SpreadsheetCellType.METHOD)
-            return new SpreadsheetCellField(structureBuilderContainer, spreadsheetHeader, declaringClass, name, cell);
-        return new ConstSpreadsheetCellField(structureBuilderContainer, spreadsheetHeader, declaringClass, name, cell);
+            return new SpreadsheetCellField(structureBuilderContainer, declaringClass, name, cell);
+        return new ConstSpreadsheetCellField(structureBuilderContainer, declaringClass, name, cell);
     }
 
     SpreadsheetCellField(SpreadsheetStructureBuilderHolder structureBuilderContainer,
-            IOpenMethodHeader spreadsheetHeader,
             IOpenClass declaringClass,
             String name,
             SpreadsheetCell cell) {
@@ -35,15 +32,9 @@ public class SpreadsheetCellField extends ASpreadsheetField {
 
     @Override
     public Object get(Object target, IRuntimeEnv env) {
-
-        // if (cell.getKind() == SpreadsheetCellType.METHOD) {
         SpreadsheetResultCalculator result = (SpreadsheetResultCalculator) target;
 
         return result.getValue(cell.getRowIndex(), cell.getColumnIndex());
-        // }
-        //
-        // return cell.getValue();
-
     }
 
     public SpreadsheetCell getCell() {
@@ -79,15 +70,11 @@ public class SpreadsheetCellField extends ASpreadsheetField {
     
     static class ConstSpreadsheetCellField extends SpreadsheetCellField {
 
-        Object value;
-
         ConstSpreadsheetCellField(SpreadsheetStructureBuilderHolder structureBuilderContainer,
-                IOpenMethodHeader spreadsheetHeader,
                 IOpenClass declaringClass,
                 String name,
                 SpreadsheetCell cell) {
-            super(structureBuilderContainer, spreadsheetHeader, declaringClass, name, cell);
-            this.value = cell.getValue();
+            super(structureBuilderContainer, declaringClass, name, cell);
         }
 
         @Override
