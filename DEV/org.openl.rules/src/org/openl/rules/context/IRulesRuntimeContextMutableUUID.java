53a5c9befb5d0d7c69c50c85944dc7426bdf93d6
Marat Kamalov <Marat Kamalov@localhost>
2015-06-04 13:47:26 +0300

    Performance issue in dispatcher implementation

---
+++ DEV/org.openl.rules/src/org/openl/rules/context/IRulesRuntimeContextMutableUUID.java
@@ -0,0 +1,11 @@
+package org.openl.rules.context;
+
+import java.util.UUID;
+
+import javax.xml.bind.annotation.XmlTransient;
+
+@XmlTransient
+public interface IRulesRuntimeContextMutableUUID {
+    @XmlTransient
+    UUID getUUID();
+}
