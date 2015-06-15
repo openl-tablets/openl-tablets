bf7fec1f019bcde387d37aed47b285561ac03c33
Marat Kamalov <Marat Kamalov@localhost>
2015-06-15 14:12:26 +0300

    EPBDS-4792 Performance issue in dispatcher. Fix functional issue.

--- DEV/org.openl.rules/src/org/openl/rules/lang/xls/binding/XlsModuleOpenClass.java
+++ DEV/org.openl.rules/src/org/openl/rules/lang/xls/binding/XlsModuleOpenClass.java
@@ -333,7 +333,7 @@ public class XlsModuleOpenClass extends ModuleOpenClass implements ExtendableMod
             try {
                 if (existedMethod instanceof OpenMethodDispatcher) {
                     OpenMethodDispatcher decorator = (OpenMethodDispatcher) existedMethod;
-                    decorator.addMethod(m);
+                    decorator.addMethod(undecorateForMultimoduleDispatching(m));
                 } else {
                     if (m != existedMethod) {
                         OpenMethodDispatcher dispatcher = addDispatcherMethod(existedMethod, key);
