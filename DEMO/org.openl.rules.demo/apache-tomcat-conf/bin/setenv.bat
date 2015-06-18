da8965713dd56f9a127e0d70d75a8fb6c56cb8ab
Yury Molchan <ymolchan@eisgroup.com>
2015-06-18 17:29:38 +0300

    Allow to run a demo under a defined port
    --HG--
    branch : 5.15.x

--- DEMO/org.openl.rules.demo/apache-tomcat-conf/bin/setenv.bat
+++ DEMO/org.openl.rules.demo/apache-tomcat-conf/bin/setenv.bat
@@ -1,2 +1,3 @@
+if "%CUSTOM_PORT%"=="" SET CUSTOM_PORT=8080
 set JAVA_OPTS=%JAVA_OPTS% -Xms256m -Xmx1024m -XX:+UseParallelOldGC -XX:PermSize=128m -XX:MaxPermSize=384m
-set CATALINA_OPTS=%CATALINA_OPTS% -Dderby.stream.error.file=..\\logs\\derby.log
+set CATALINA_OPTS=%CATALINA_OPTS% -Dderby.stream.error.file=..\\logs\\derby.log -Dws.port=%CUSTOM_PORT%
