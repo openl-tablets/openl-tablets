a2e1d7a26ad79f7d1f576914453e758f04e0f218
Yury Molchan <ymolchan@eisgroup.com>
2015-06-18 17:29:38 +0300

    Allow to run a demo under a defined port

--- DEMO/org.openl.rules.demo/apache-tomcat-conf/bin/setenv.sh
+++ DEMO/org.openl.rules.demo/apache-tomcat-conf/bin/setenv.sh
@@ -1,2 +1,2 @@
 export JAVA_OPTS="$JAVA_OPTS -Xms256m -Xmx1024m -XX:+UseParallelOldGC -XX:PermSize=128m -XX:MaxPermSize=384m"
-export CATALINA_OPTS="$CATALINA_OPTS -Dderby.stream.error.file=..\\logs\\derby.log"
+export CATALINA_OPTS="$CATALINA_OPTS -Dderby.stream.error.file=..\\logs\\derby.log -Dws.port=8080"
