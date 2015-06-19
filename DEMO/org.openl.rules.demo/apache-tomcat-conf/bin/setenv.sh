6bd100f983832c1b0d96862f615720ae8c671d39
Yury Molchan <ymolchan@eisgroup.com>
2015-06-19 12:57:29 +0300

    EPBDS-5915 Update Java opts in OpenL demo

--- DEMO/org.openl.rules.demo/apache-tomcat-conf/bin/setenv.sh
+++ DEMO/org.openl.rules.demo/apache-tomcat-conf/bin/setenv.sh
@@ -1,2 +1,2 @@
-export JAVA_OPTS="$JAVA_OPTS -Xms256m -Xmx1024m -XX:+UseParallelOldGC -XX:PermSize=128m -XX:MaxPermSize=384m"
+export JAVA_OPTS="$JAVA_OPTS -Xms512m -Xmx2g -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:PermSize=128m -XX:MaxPermSize=512m"
 export CATALINA_OPTS="$CATALINA_OPTS -Dderby.stream.error.file=..\\logs\\derby.log -Dws.port=8080"
