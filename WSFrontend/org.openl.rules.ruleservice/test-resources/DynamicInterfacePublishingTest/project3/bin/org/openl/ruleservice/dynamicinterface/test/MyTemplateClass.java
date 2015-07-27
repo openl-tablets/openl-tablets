aa08238407210e7427f4631b622352290d2e150b
Marat Kamalov <none@none>
2015-07-27 16:35:44 +0300

    EPBDS-5937 Template interface improvment for Dynamic Interface

--- WSFrontend/org.openl.rules.ruleservice/test-resources/DynamicInterfacePublishingTest/project3/bin/org/openl/ruleservice/dynamicinterface/test/MyTemplateClass.java
+++ WSFrontend/org.openl.rules.ruleservice/test-resources/DynamicInterfacePublishingTest/project3/bin/org/openl/ruleservice/dynamicinterface/test/MyTemplateClass.java
@@ -1,14 +1,17 @@
 package org.openl.ruleservice.dynamicinterface.test;
 
+import javax.jws.WebService;
+
 import org.openl.rules.context.IRulesRuntimeContext;
 import org.openl.rules.ruleservice.core.interceptors.AnyType;
 import org.openl.rules.ruleservice.core.interceptors.annotations.ServiceCallAfterInterceptor;
 import org.openl.rules.ruleservice.core.interceptors.annotations.ServiceCallBeforeInterceptor;
 
+@WebService(serviceName = "TestService", targetNamespace="http://my.test.ns")
 public interface MyTemplateClass {
     @ServiceCallAfterInterceptor(value = { MyAfterAdvice.class })
     MyClass method2(IRulesRuntimeContext context, @AnyType(".*MyType") Object obj);
 
     @ServiceCallBeforeInterceptor(value = { MyBeforeAdvice.class })
-    MyClass method3(@AnyType Object obj, @AnyType Object obj1);
+    MyClass method3(@AnyType Object obj, @AnyType Object obj1); 
 }
