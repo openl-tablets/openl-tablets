d71c16181ebc1d30a636942c75be2184d36aa187
Nail Samatov <nsamatov@eisgroup.com>
2015-08-28 16:43:56 +0300

    EPBDS-5970 Fix: Formula is displayed except of result value

--- STUDIO/org.openl.rules.webstudio/src/org/openl/rules/ui/TableValueFilter.java
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/ui/TableValueFilter.java
@@ -23,8 +23,21 @@ class TableValueFilter extends AGridFilter {
     }
 
     public FormattedCell filterFormat(FormattedCell cell) {
-        int col = cell.getColumn() - startX;
-        int row = cell.getRow() - startY;
+        ILogicalTable table = res.getLogicalTable();
+
+        int columnOffset = 0;
+        int rowOffset = 0;
+
+        for (int i = 1; i < cell.getColumn() - startX; i++) {
+            columnOffset += table.getColumn(i).getSource().getWidth() - 1;
+        }
+
+        for (int i = 1; i < cell.getRow() - startY; i++) {
+            rowOffset += table.getRow(i).getSource().getHeight() - 1;
+        }
+
+        int col = cell.getColumn() - startX - columnOffset;
+        int row = cell.getRow() - startY - rowOffset;
         if (row >= 0 && col >= 0 && res.getWidth() > col && res.getHeight() > row) {
             Object v = res.getValue(row, col);
             if (v != null) {
