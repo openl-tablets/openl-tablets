a51ed163579ef3962adffb5ba35defed5291df7a
Yury Molchan <ymolchan@eisgroup.com>
2015-10-10 15:14:24 +0300

    Add unzip from an input stream

--- DEV/org.openl.commons/src/org/openl/util/ZipUtils.java
+++ DEV/org.openl.commons/src/org/openl/util/ZipUtils.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
@@ -17,16 +18,27 @@ public class ZipUtils {
     /**
      * Extract all files from a zip file into a directory.
      *
-     * @param zipFile      input zip file
-     * @param outputFolder zip file output folder
+     * @param zipFile the input zip file
+     * @param outputFolder the output folder for extracted files
      */
     public static void extractAll(File zipFile, File outputFolder) throws IOException {
+        final FileInputStream zippedStream = new FileInputStream(zipFile);
+        extractAll(zippedStream, outputFolder);
+    }
+
+    /**
+     * Extract all files from a zipped stream into a directory.
+     *
+     * @param zippedStream the zipped input stream
+     * @param outputFolder the output folder for extracted files
+     */
+    public static void extractAll(InputStream zippedStream, File outputFolder) throws IOException {
 
         byte[] buffer = new byte[8192];
 
-        ZipInputStream zis = new ZipInputStream(new FileInputStream(zipFile));
+        ZipInputStream zis = new ZipInputStream(zippedStream);
         try {
-            //get the zipped file list entry
+            // get the zipped file list entry
             ZipEntry ze = zis.getNextEntry();
 
             while (ze != null) {
@@ -34,7 +46,7 @@ public class ZipUtils {
                 if (!ze.isDirectory()) {
                     String fileName = ze.getName();
                     File unzipped = new File(outputFolder, fileName);
-                    //create all non exists folders
+                    // create all non exists folders
                     new File(unzipped.getParent()).mkdirs();
                     FileOutputStream fos = new FileOutputStream(unzipped);
 
@@ -46,9 +58,7 @@ public class ZipUtils {
                 ze = zis.getNextEntry();
             }
         } finally {
-            zis.closeEntry();
             IOUtils.closeQuietly(zis);
         }
     }
 }
-
