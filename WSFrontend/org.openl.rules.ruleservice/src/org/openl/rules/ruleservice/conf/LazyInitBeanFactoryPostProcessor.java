ffd083f992c93f95fa20bae962d7d9e8b6929610
Marat Kamalov <none@none>
2015-11-18 19:32:48 +0300

    EPBDS-6045 Logging all rule services requests and store them to DB. Fix failed tests.

---
+++ WSFrontend/org.openl.rules.ruleservice/src/org/openl/rules/ruleservice/conf/LazyInitBeanFactoryPostProcessor.java
@@ -0,0 +1,28 @@
+package org.openl.rules.ruleservice.conf;
+
+import java.util.Collection;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+
+public class LazyInitBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
+    
+    private Collection<String> lazyBeanNames;
+    
+    public Collection<String> getLazyBeanNames() {
+        return lazyBeanNames;
+    }
+    
+    public void setLazyBeanNames(Collection<String> lazyBeanNames) {
+        this.lazyBeanNames = lazyBeanNames;
+    }
+    
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        for (String beanName : beanFactory.getBeanDefinitionNames()) {
+            if (lazyBeanNames.contains(beanName)){
+                beanFactory.getBeanDefinition(beanName).setLazyInit(true);
+            }
+        }
+    }
+}
