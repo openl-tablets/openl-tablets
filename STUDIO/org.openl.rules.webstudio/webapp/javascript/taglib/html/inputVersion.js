648088de1f08a1df35f3e68d67ea6c0b7671d268
Marat Kamalov <mkamalov@eisgroup.com>
2015-12-01 16:06:27 +0300

    EPBDS-5879 Implement drop down list for numeric type values from alias datatype tables

--- STUDIO/org.openl.rules.webstudio/webapp/javascript/taglib/html/inputVersion.js
+++ STUDIO/org.openl.rules.webstudio/webapp/javascript/taglib/html/inputVersion.js
@@ -1,5 +1,6 @@
 //(function($) {
 	var initVersion = "0.0.0";
+	var initShortVersion = "0.0";
 
     function showVersionPopup(input, event) {
         var jinput = $j(input);
@@ -15,12 +16,17 @@
         });
     }
 
-    function applyVersion(element, i) {
+    function applyVersion(element, isShort,  i) {
         var value = $j(element).find("input").val();
         var popup = $j(element).closest("div");
         var input = popup.prev();
         var hidden = popup.next();
-        var version = input.val() || initVersion;
+        var version = input.val();
+        if (isShort){
+        	version = version || initVersion;
+        }else{
+        	version = version || initShortVersion;
+        }
         var subVersions = version.split(".");
         subVersions[i] = value;
         var result = subVersions.join(".");
