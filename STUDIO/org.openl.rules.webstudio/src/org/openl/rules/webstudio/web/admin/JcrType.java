4a080c5ed48dba6d026adf67bf9925d8758ffde6
Yury Molchan <ymolchan@eisgroup.com>
2015-12-02 14:47:55 +0300

    EPBDS-6061 Support JNDI based DataSource configurations

--- STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/web/admin/JcrType.java
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/web/admin/JcrType.java
@@ -4,11 +4,13 @@ public enum JcrType {
     DESIGN_LOCAL(org.openl.rules.repository.factories.LocalJackrabbitRepositoryFactory.class),
     DESIGN_RMI(org.openl.rules.repository.factories.RmiJackrabbitRepositoryFactory.class),
     DESIGN_WEBDAV(org.openl.rules.repository.factories.WebDavRepositoryFactory.class),
-    DESIGN_DB(org.openl.rules.repository.factories.DBRepositoryFactory.class),
+    DESIGN_DB(org.openl.rules.repository.factories.JdbcDBRepositoryFactory.class),
+    DESIGN_JNDI(org.openl.rules.repository.factories.JndiDBRepositoryFactory.class),
     PRODUCTION_LOCAL(org.openl.rules.repository.factories.LocalJackrabbitRepositoryFactory.class),
     PRODUCTION_RMI(org.openl.rules.repository.factories.RmiJackrabbitRepositoryFactory.class),
     PRODUCTION_WEBDAV(org.openl.rules.repository.factories.WebDavRepositoryFactory.class),
-    PRODUCTION_DB(org.openl.rules.repository.factories.DBRepositoryFactory.class);
+    PRODUCTION_DB(org.openl.rules.repository.factories.JdbcDBRepositoryFactory.class),
+    PRODUCTION_JNDI(org.openl.rules.repository.factories.JndiDBRepositoryFactory.class);
 
     public static JcrType findByAccessType(RepositoryType repositoryType, String accessType) {
         for (JcrType jcrType : values()) {
