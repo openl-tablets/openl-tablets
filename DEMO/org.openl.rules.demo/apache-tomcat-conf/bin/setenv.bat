01fc0dcb512bf3efab31dbfa72c1f97b2e866463
Yury Molchan <ymolchan@eisgroup.com>
2015-12-04 01:32:29 +0300

    Fix an issue with updating production repository settings in DEMO
    --HG--
    branch : 5.16.x

--- DEMO/org.openl.rules.demo/apache-tomcat-conf/bin/setenv.bat
+++ DEMO/org.openl.rules.demo/apache-tomcat-conf/bin/setenv.bat
@@ -1,6 +1,4 @@
 set JAVA_OPTS=%JAVA_OPTS% -Xms512m -Xmx2g -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:PermSize=128m -XX:MaxPermSize=512m
 set DEMO_OPTS=-Druleservice.datasource.type=jcr -Druleservice.rmiPort=8099 -Druleservice.openl.home=openl-demo
-set DEMO_OPTS=%DEMO_OPTS% -Dproduction-repository.factory=org.openl.rules.repository.factories.RmiJackrabbitProductionRepositoryFactory
-set DEMO_OPTS=%DEMO_OPTS% -Dproduction-repository.uri=//localhost:1099/deployment-repository
 set DEMO_OPTS=%DEMO_OPTS% -Dwebstudio.home=openl-demo -Dwebstudio.configured=true -Dws.port=8080
 set CATALINA_OPTS=%DEMO_OPTS% %CATALINA_OPTS% -Dderby.stream.error.file=..\\logs\\derby.log
