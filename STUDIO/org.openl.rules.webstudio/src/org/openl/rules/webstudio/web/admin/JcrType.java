0d74f048ef9cd9834eb7d4b0a285e7a5e2597532
Yury Molchan <ymolchan@eisgroup.com>
2015-12-07 18:38:50 +0300

    EPBDS-6061 Use single factory for WebDav-based repository
    --HG--
    branch : 5.16.x

--- STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/web/admin/JcrType.java
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/web/admin/JcrType.java
@@ -3,11 +3,11 @@ package org.openl.rules.webstudio.web.admin;
 public enum JcrType {
     DESIGN_LOCAL(org.openl.rules.repository.factories.LocalJackrabbitDesignRepositoryFactory.class),
     DESIGN_RMI(org.openl.rules.repository.factories.RmiJackrabbitDesignRepositoryFactory.class),
-    DESIGN_WEBDAV(org.openl.rules.repository.factories.WebDavJackrabbitDesignRepositoryFactory.class),
+    DESIGN_WEBDAV(org.openl.rules.repository.factories.WebDavRepositoryFactory.class),
     DESIGN_DB(org.openl.rules.repository.factories.DBRepositoryFactory.class),
     PRODUCTION_LOCAL(org.openl.rules.repository.factories.LocalJackrabbitProductionRepositoryFactory.class),
     PRODUCTION_RMI(org.openl.rules.repository.factories.RmiJackrabbitProductionRepositoryFactory.class),
-    PRODUCTION_WEBDAV(org.openl.rules.repository.factories.WebDavJackrabbitProductionRepositoryFactory.class),
+    PRODUCTION_WEBDAV(org.openl.rules.repository.factories.WebDavRepositoryFactory.class),
     PRODUCTION_DB(org.openl.rules.repository.factories.DBRepositoryFactory.class);
 
     public static JcrType findByAccessType(RepositoryType repositoryType, String accessType) {
