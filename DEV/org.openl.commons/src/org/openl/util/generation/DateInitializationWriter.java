3a31da07474ec0b62f2ea2663fa0d77baf913c95
Marat Kamalov <mkamalov@eisgroup.com>
2015-12-17 16:36:50 +0300

    EPBDS-6084 Defult values are not generated for values inside java classes. SimpleDateFormat is not thread safe fix.

--- DEV/org.openl.commons/src/org/openl/util/generation/DateInitializationWriter.java
+++ DEV/org.openl.commons/src/org/openl/util/generation/DateInitializationWriter.java
@@ -5,12 +5,12 @@ import java.util.Date;
 
 public class DateInitializationWriter implements TypeInitializationWriter {
 
-    private static final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM/dd/yyyy hh:mm:ss a");
+    private static final String DATE_FORMAT_FOR_DATE_CONSTRUCTOR = "MM/dd/yyyy hh:mm:ss a";
 
     public String getInitialization(Object value) {
         Date date = (Date) value;
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(DATE_FORMAT_FOR_DATE_CONSTRUCTOR);
         String stringValue = simpleDateFormat.format(date);
-
         return String.format("new %s(\"%s\")", JavaClassGeneratorHelper.filterTypeName(value.getClass()), stringValue);
     }
 }
