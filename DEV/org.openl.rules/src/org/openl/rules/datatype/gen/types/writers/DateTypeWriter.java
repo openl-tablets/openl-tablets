35ff8c3dbd17b681c53edae526d6cf50a30a50ba
Marat Kamalov <mkamalov@eisgroup.com>
2015-12-17 16:36:50 +0300

    EPBDS-6084 Defult values are not generated for values inside java classes. SimpleDateFormat is not thread safe fix.
    --HG--
    branch : 5.16.x

--- DEV/org.openl.rules/src/org/openl/rules/datatype/gen/types/writers/DateTypeWriter.java
+++ DEV/org.openl.rules/src/org/openl/rules/datatype/gen/types/writers/DateTypeWriter.java
@@ -6,10 +6,11 @@ import java.util.Date;
 import org.openl.rules.datatype.gen.FieldDescription;
 
 public class DateTypeWriter extends ObjectTypeWriter {
-    private static final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM/dd/yyyy hh:mm:ss a"); 
+    private static final String DATE_FORMAT_FOR_DATE_CONSTRUCTOR = "MM/dd/yyyy hh:mm:ss a";
     
     @Override
     protected String updateValue(FieldDescription fieldType) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(DATE_FORMAT_FOR_DATE_CONSTRUCTOR);
         Date date = (Date) fieldType.getDefaultValue();
         return simpleDateFormat.format(date);
     }
