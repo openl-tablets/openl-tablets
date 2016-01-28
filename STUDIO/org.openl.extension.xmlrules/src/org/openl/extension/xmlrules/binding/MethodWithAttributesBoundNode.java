24d8bb3de6693ad4fe777ccad2a9a5fb8ff1afc5
Nail Samatov <nsamatov@eisgroup.com>
2016-01-28 15:50:37 +0300

    EPBDS-6096 Attributes support

---
+++ STUDIO/org.openl.extension.xmlrules/src/org/openl/extension/xmlrules/binding/MethodWithAttributesBoundNode.java
@@ -0,0 +1,60 @@
+package org.openl.extension.xmlrules.binding;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.openl.binding.IBoundNode;
+import org.openl.binding.impl.MethodBoundNode;
+import org.openl.exception.OpenLRuntimeException;
+import org.openl.extension.xmlrules.model.Function;
+import org.openl.extension.xmlrules.model.single.Attribute;
+import org.openl.rules.binding.RulesModuleBindingContext;
+import org.openl.syntax.ISyntaxNode;
+import org.openl.types.IMethodCaller;
+import org.openl.types.IOpenClass;
+import org.openl.vm.IRuntimeEnv;
+
+public class MethodWithAttributesBoundNode extends MethodBoundNode {
+    private final IMethodCaller modifyContext;
+    private final IMethodCaller restoreContext;
+    private final List<Attribute> attributes;
+    private final int parameterCount;
+
+    public MethodWithAttributesBoundNode(ISyntaxNode syntaxNode,
+            IBoundNode[] child,
+            IMethodCaller methodCaller,
+            IMethodCaller modifyContext,
+            IMethodCaller restoreContext, List<Attribute> attributes, int parameterCount) {
+        super(syntaxNode, child, methodCaller);
+        this.modifyContext = modifyContext;
+        this.restoreContext = restoreContext;
+        this.attributes = attributes;
+        this.parameterCount = parameterCount;
+    }
+
+    @Override
+    public Object evaluateRuntime(IRuntimeEnv env) throws OpenLRuntimeException {
+        Object target = getTargetNode() == null ? env.getThis() : getTargetNode().evaluate(env);
+        Object[] arguments = evaluateChildren(env);
+        Object[] methodParameters = Arrays.copyOfRange(arguments, 0, parameterCount);
+        Object[] attributeParameters = Arrays.copyOfRange(arguments, parameterCount, attributes.size());
+
+        int attributesChanged = 0;
+        try {
+            // Modify runtime context
+            for (int i = 0; i < attributes.size(); i++) {
+                Object[] params = new Object[] {attributes.get(i).getName(), attributeParameters[i]};
+                modifyContext.invoke(target, params, env);
+                attributesChanged++;
+            }
+
+            // Invoke the function with modified context
+            return getMethodCaller().invoke(target, methodParameters, env);
+        } finally {
+            // Restore runtime context
+            for (int i = 0; i < attributesChanged; i++) {
+                restoreContext.invoke(target, new Object[0], env);
+            }
+        }
+    }
+}
--- STUDIO/org.openl.extension.xmlrules/src/org/openl/extension/xmlrules/binding/XmlRulesMethodNodeBinder.java
+++ STUDIO/org.openl.extension.xmlrules/src/org/openl/extension/xmlrules/binding/XmlRulesMethodNodeBinder.java
@@ -1,11 +1,19 @@
 package org.openl.extension.xmlrules.binding;
 
+import java.util.Arrays;
+
 import org.openl.binding.IBindingContext;
 import org.openl.binding.IBoundNode;
 import org.openl.binding.impl.MethodNodeBinder;
+import org.openl.extension.xmlrules.ProjectData;
+import org.openl.extension.xmlrules.model.Function;
+import org.openl.extension.xmlrules.model.Table;
 import org.openl.extension.xmlrules.model.single.node.IfErrorNode;
 import org.openl.syntax.ISyntaxNode;
+import org.openl.syntax.impl.ISyntaxConstants;
+import org.openl.types.IMethodCaller;
 import org.openl.types.IOpenClass;
+import org.openl.types.java.JavaOpenClass;
 
 public class XmlRulesMethodNodeBinder extends MethodNodeBinder {
     @Override
@@ -20,11 +28,86 @@ public class XmlRulesMethodNodeBinder extends MethodNodeBinder {
             return new IfErrorFunctionBoundNode(methodNode, children);
         }
 
+        IBoundNode methodWithModifiedAttributes = bindModifiedAttributes(methodNode,
+                bindingContext,
+                methodName,
+                argumentTypes,
+                children);
+
+        if (methodWithModifiedAttributes != null) {
+            return methodWithModifiedAttributes;
+        }
+
         return super.bindWithAdditionalBinders(methodNode,
                 bindingContext,
                 methodName,
                 argumentTypes,
                 children,
                 childrenCount);
+    }
+
+    private IBoundNode bindModifiedAttributes(ISyntaxNode methodNode,
+            IBindingContext bindingContext,
+            String methodName,
+            IOpenClass[] argumentTypes, IBoundNode[] children) {
+        IMethodCaller modifyContext = bindingContext.findMethodCaller(ISyntaxConstants.THIS_NAMESPACE,
+                "modifyContext",
+                new IOpenClass[] { JavaOpenClass.STRING, JavaOpenClass.OBJECT });
+        IMethodCaller restoreContext = bindingContext.findMethodCaller(ISyntaxConstants.THIS_NAMESPACE,
+                "restoreContext",
+                new IOpenClass[] {});
+        ProjectData instance = ProjectData.getCurrentInstance();
+
+        for (Function function : instance.getFunctions()) {
+            if (function.getName().equals(methodName)) {
+                int parameterCount = function.getParameters().size();
+                int possibleParameterCount = parameterCount + function.getAttributes().size();
+                if (parameterCount < children.length && possibleParameterCount >= children.length) {
+                    IOpenClass[] parameterTypes = Arrays.copyOfRange(argumentTypes, 0, parameterCount);
+                    IMethodCaller methodCaller = bindingContext.findMethodCaller(ISyntaxConstants.THIS_NAMESPACE,
+                            methodName, parameterTypes);
+                    if (methodCaller == null) {
+                        return null;
+                    }
+
+                    return new MethodWithAttributesBoundNode(methodNode,
+                            children,
+                            methodCaller,
+                            modifyContext,
+                            restoreContext,
+                            function.getAttributes(),
+                            parameterCount);
+                } else {
+                    return null;
+                }
+            }
+        }
+
+        for (Table table : instance.getTables()) {
+            if (table.getName().equals(methodName)) {
+                int parameterCount = table.getVerticalConditions().size() + table.getHorizontalConditions().size() + table.getParameters().size();
+                int possibleParameterCount = parameterCount + table.getAttributes().size();
+                if (parameterCount < children.length && possibleParameterCount >= children.length) {
+                    IOpenClass[] parameterTypes = Arrays.copyOfRange(argumentTypes, 0, parameterCount);
+                    IMethodCaller methodCaller = bindingContext.findMethodCaller(ISyntaxConstants.THIS_NAMESPACE,
+                            methodName, parameterTypes);
+                    if (methodCaller == null) {
+                        return null;
+                    }
+
+                    return new MethodWithAttributesBoundNode(methodNode,
+                            children,
+                            methodCaller,
+                            modifyContext,
+                            restoreContext,
+                            table.getAttributes(),
+                            parameterCount);
+                } else {
+                    return null;
+                }
+            }
+        }
+
+        return null;
     }
 }
