0c51559b85d935496b43b77b6b4a273dbeaab815
Yury Molchan <ymolchan@eisgroup.com>
2016-02-08 15:30:31 +0300

    Backed out changeset: 4c327a450eef

--- DEV/org.openl.rules/src/org/openl/rules/dt/index/RangeIndex.java
+++ DEV/org.openl.rules/src/org/openl/rules/dt/index/RangeIndex.java
@@ -1,6 +1,6 @@
 package org.openl.rules.dt.index;
 
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
@@ -10,8 +10,7 @@ import org.openl.rules.dt.type.IRangeAdaptor;
 
 public class RangeIndex extends ARuleIndex {
 
-    private List<DecisionTableIndexedRuleNode<?>> index;
-    private DecisionTableIndexedRuleNode<?>[] index2;
+    protected List<DecisionTableIndexedRuleNode<?>> index;
     public Comparator<? super Object> comparator = null;
 
     protected IRangeAdaptor<?, ?> adaptor;
@@ -22,14 +21,12 @@ public class RangeIndex extends ARuleIndex {
         super(emptyOrFormulaNodes);
 
         this.index = index;
-        // Search in a List is slow on IBM Java 6
-        index2 = index.toArray(new DecisionTableIndexedRuleNode[index.size()]);
         this.adaptor = adaptor;
     }
 
     @Override
     DecisionTableRuleNode findNodeInIndex(Object value) {
-        if (index2.length == 0) {
+        if (index.isEmpty()) {
             // there is no values in index to compare => no reason to search
             return null;
         }
@@ -40,16 +37,16 @@ public class RangeIndex extends ARuleIndex {
             value = adaptor.adaptValueType(value);
         }
 
-        int idx = Arrays.binarySearch(index2, value, comparator);
+        int idx = Collections.binarySearch(index, value, comparator);
 
         if (idx >= 0) {
-            return index2[idx];
+            return index.get(idx);
         }
 
         int insertionPoint = -(idx + 1);
 
-        if (insertionPoint < index2.length && insertionPoint > 0) {
-            return index2[insertionPoint - 1];
+        if (insertionPoint < index.size() && insertionPoint > 0) {
+            return index.get(insertionPoint - 1);
         }
 
         return null;
