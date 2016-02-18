e041387c5c5509a5145078a2847ff9f2cc434b2e
Nail Samatov <nsamatov@eisgroup.com>
2016-02-19 00:34:49 +0300

    EPBDS-6142 Convert arguments to correct types in ReadIn function

--- STUDIO/org.openl.extension.xmlrules/src/org/openl/extension/xmlrules/utils/ReadInFunction.java
+++ STUDIO/org.openl.extension.xmlrules/src/org/openl/extension/xmlrules/utils/ReadInFunction.java
@@ -68,12 +68,19 @@ public class ReadInFunction {
                 if (method == null) {
                     throw new IllegalArgumentException("Can't find a setter for the field'" + field.getName() + "' in the type '" + typeName + "'");
                 }
+                Class<?> expectedClass = method.getParameterTypes()[0];
+                Object value = array[i][j];
                 try {
-                    method.invoke(instance, array[i][j]);
+                    method.invoke(instance, convertArgument(expectedClass, value));
                 } catch (IllegalAccessException e) {
                     throw new IllegalArgumentException("Can't access the field '" + field.getName() + "' in the type '" + typeName + "'");
                 } catch (InvocationTargetException e) {
-                    throw new IllegalArgumentException("Setter for the field '" + field.getName() + "' in the type '" + typeName + "' throwed an error ", e);
+                    throw new IllegalArgumentException("Setter for the field '" + field.getName() + "' in the type '" + typeName + "' threw an error ", e);
+                } catch (IllegalArgumentException e) {
+                    String expected = expectedClass.getSimpleName();
+                    String actual = value != null ? value.getClass().getSimpleName() : "null";
+                    String message = "Expected field type: " + expected + ", but was " + actual +  ". Field: '" + field.getName() + "' in the type '" + typeName + "'";
+                    throw new IllegalArgumentException(message, e);
                 }
             }
 
@@ -81,6 +88,34 @@ public class ReadInFunction {
         }
 
         return objects.toArray((Object[]) Array.newInstance(clazz, objects.size()));
+    }
+
+    private static Object convertArgument(Class<?> expectedClass, Object value) {
+        if (value != null) {
+            Class<?> valueClass = value.getClass();
+            if (!expectedClass.isAssignableFrom(valueClass)) {
+                if (String.class == expectedClass && value instanceof Double) {
+                    value = String.valueOf(value);
+                } else if (Double.class == expectedClass && value instanceof String) {
+                    value = Double.valueOf((String) value);
+                } else if (expectedClass.isArray() && valueClass.isArray()) {
+                    // For example expected: Rider[], but actual: Object[] with Rider objects
+                    int size = Array.getLength(value);
+
+                    Object newValue = Array.newInstance(expectedClass.getComponentType(), size);
+                    for (int i = 0; i < size; ++i) {
+                        Array.set(newValue, i, Array.get(value, i));
+                    }
+
+                    value = newValue;
+                } else if (!expectedClass.isArray() && valueClass.isArray()) {
+                    if (Array.getLength(value) == 1) {
+                        value = convertArgument(expectedClass, Array.get(value, 0));
+                    }
+                }
+            }
+        }
+        return value;
     }
 
     public static FieldImpl findField(Type type, String fieldName) {
