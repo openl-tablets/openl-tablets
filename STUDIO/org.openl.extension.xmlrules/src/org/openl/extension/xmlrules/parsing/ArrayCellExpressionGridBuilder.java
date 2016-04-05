45f71e4af3ec16f43cbec3671b9832fc94f3851d
Nail Samatov <nsamatov@eisgroup.com>
2016-04-05 15:02:16 +0300

    EPBDS-5853 Reduce complexity
    --HG--
    branch : 5.17.x

--- STUDIO/org.openl.extension.xmlrules/src/org/openl/extension/xmlrules/parsing/ArrayCellExpressionGridBuilder.java
+++ STUDIO/org.openl.extension.xmlrules/src/org/openl/extension/xmlrules/parsing/ArrayCellExpressionGridBuilder.java
@@ -21,6 +21,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public final class ArrayCellExpressionGridBuilder {
+
+    /**
+     * "#" and "Calculation" columns
+     */
+    private static final int AUXILIARY_COLUMNS_COUNT = 2;
+
     private ArrayCellExpressionGridBuilder() {
     }
 
@@ -60,9 +66,10 @@ public final class ArrayCellExpressionGridBuilder {
             final String workbookName = sheet.getWorkbookName();
             final String sheetName = sheet.getName();
 
+            validate(cell, workbookName, sheetName);
+
             CellReference start = CellReference.parse(workbookName, sheetName, cell.getAddress());
             CellReference end = CellReference.parse(workbookName, sheetName, cell.getEndAddress());
-            String tableName = new RulesTableReference(start, end).getTable();
 
             int startColumn = start.getColumnNumber();
             int startRow = start.getRowNumber();
@@ -73,116 +80,176 @@ public final class ArrayCellExpressionGridBuilder {
             expressionContext.setCanHandleArrayOperators(true);
             ExpressionContext.setInstance(expressionContext);
 
-            int columnsCount = endColumn - startColumn + 3;
-            // TODO Add array operations parameters count
-
             Node node = cell.getNode();
-            if (node == null) {
-                throw new IllegalArgumentException("Cell [" + workbookName + "]" + sheetName + "!" + cell
-                        .getAddress()
-                        .toOpenLString() + " contains incorrect value. It will be skipped");
-            }
-
             node.setRootNode(true);
 
             boolean isOutFunction = node instanceof FunctionNode && "Out".equals(((FunctionNode) node).getName());
 
-            gridBuilder.addCell("Spreadsheet SpreadsheetResult " + tableName + "()", columnsCount).nextRow();
-
-            gridBuilder.addCell("#");
-            gridBuilder.addCell("Calculation");
-            for (int i = startColumn; i <= endColumn; i++) {
-                gridBuilder.addCell("C" + i + " : Object");
-            }
-            gridBuilder.nextRow();
+            writeHeader(gridBuilder, start, end);
+            writeColumnNames(gridBuilder, startColumn, endColumn);
 
             CellInspector.NodeSize nodeSize = CellInspector.inspect(cell.getNode(), true);
             int resultRows = nodeSize.getResultHeight();
             int resultColumns = nodeSize.getResultWidth();
 
-            if (resultColumns == 1) {
-                gridBuilder.addCell("Result : Object[]").addCell("= new Object[" + resultRows + "]");
-            } else {
-                gridBuilder.addCell("Result : Object[][]")
-                        .addCell("= new Object[" + resultRows + "][" + resultColumns + "]");
-            }
-            gridBuilder.nextRow();
+            writeVariableDeclaration(gridBuilder, resultRows, resultColumns);
 
-            for (int step = 0; step < resultRows; step++) {
-                gridBuilder.addCell("Step" + step + " : Object");
-
-                for (int column = startColumn; column < startColumn + resultColumns; column++) {
-                    expressionContext.setCurrentRow(startRow + step);
-                    expressionContext.setCurrentColumn(column);
-
-                    String expression;
-                    try {
-                        if (node instanceof ValueHolder) {
-                            expression = ((ValueHolder) node).asString();
-                        } else {
-                            String formula = isOutFunction ? ((FunctionNode) node).getArguments().get(0).toOpenLString() : node.toOpenLString();
-
-                            if (resultColumns == 1) {
-                                expression = "= $Calculation$Result[" + step + "] = " + formula;
-                            } else {
-                                expression = "= $Calculation$Result[" + step + "][" + (column - startColumn) + "] = " + formula;
-                            }
-                        }
-                    } catch (RuntimeException e) {
-                        expression = "";
-                        Logger log = LoggerFactory.getLogger(ArrayCellExpressionGridBuilder.class);
-                        log.error(e.getMessage(), e);
-                        ParseError error = GridBuilderUtils.createError(gridBuilder.getRow(), gridBuilder.getColumn(), cell, e);
-                        parseErrors.add(error);
-                    }
-                    gridBuilder.addCell(expression);
-                }
-                gridBuilder.nextRow();
-            }
+            writeCalculation(gridBuilder,
+                    expressionContext,
+                    node,
+                    isOutFunction,
+                    startRow,
+                    startColumn,
+                    resultRows,
+                    resultColumns,
+                    parseErrors,
+                    cell);
 
             expressionContext.setOutArray(isOutFunction);
 
-            // FIXME Here is simple case only
-            for (int row = startRow; row <= endRow; row++) {
-                gridBuilder.addCell("R" + row);
-                gridBuilder.addCell(null);
-
-                for (int column = startColumn; column <= endColumn; column++) {
-                    expressionContext.setCurrentRow(row);
-                    expressionContext.setCurrentColumn(column);
-
-                    String expression;
-                    try {
-                        if (node instanceof ValueHolder) {
-                            expression = ((ValueHolder) node).asString();
-                        } else {
-                            if (isOutFunction) {
-                                expression = "= " + node.toOpenLString();
-                            } else {
-                                int rowShift = row - startRow;
-                                int columnShift = column - startColumn;
-                                // TODO Replace Out() function with OutArray (the function that returns Object, not String)
-                                expression = "= Print(" + rowShift + ", " + columnShift + ", $Calculation$Result)";
-                            }
-                        }
-                    } catch (RuntimeException e) {
-                        expression = "";
-                        Logger log = LoggerFactory.getLogger(ArrayCellExpressionGridBuilder.class);
-                        log.error(e.getMessage(), e);
-                        ParseError error = GridBuilderUtils.createError(gridBuilder.getRow(),
-                                gridBuilder.getColumn(),
-                                cell,
-                                e);
-                        parseErrors.add(error);
-                    }
-                    gridBuilder.addCell(expression);
-                }
-                gridBuilder.nextRow();
-            }
+            writeOutCells(gridBuilder,
+                    expressionContext,
+                    node,
+                    isOutFunction,
+                    startRow,
+                    startColumn,
+                    endRow,
+                    endColumn,
+                    parseErrors,
+                    cell);
 
             gridBuilder.nextRow();
         } finally {
             ExpressionContext.removeInstance();
+        }
+    }
+
+    private static void writeHeader(StringGridBuilder gridBuilder, CellReference start, CellReference end) {
+        String tableName = new RulesTableReference(start, end).getTable();
+
+        int startColumn = start.getColumnNumber();
+        int endColumn = end.getColumnNumber();
+        int columnsCount = endColumn - startColumn + 1 + AUXILIARY_COLUMNS_COUNT;
+
+        gridBuilder.addCell("Spreadsheet SpreadsheetResult " + tableName + "()", columnsCount).nextRow();
+    }
+
+    private static void writeColumnNames(StringGridBuilder gridBuilder, int startColumn, int endColumn) {
+        gridBuilder.addCell("#");
+        gridBuilder.addCell("Calculation");
+        for (int i = startColumn; i <= endColumn; i++) {
+            gridBuilder.addCell("C" + i + " : Object");
+        }
+        gridBuilder.nextRow();
+    }
+
+    private static void writeVariableDeclaration(StringGridBuilder gridBuilder, int resultRows, int resultColumns) {
+        if (resultColumns == 1) {
+            gridBuilder.addCell("Result : Object[]").addCell("= new Object[" + resultRows + "]");
+        } else {
+            gridBuilder.addCell("Result : Object[][]")
+                    .addCell("= new Object[" + resultRows + "][" + resultColumns + "]");
+        }
+        gridBuilder.nextRow();
+    }
+
+    private static void writeCalculation(StringGridBuilder gridBuilder,
+            ExpressionContext expressionContext,
+            Node node,
+            boolean isOutFunction,
+            int startRow, int startColumn,
+            int resultRows,
+            int resultColumns,
+            List<ParseError> parseErrors,
+            Cell cell) {
+        for (int step = 0; step < resultRows; step++) {
+            gridBuilder.addCell("Step" + step + " : Object");
+
+            for (int column = startColumn; column < startColumn + resultColumns; column++) {
+                expressionContext.setCurrentRow(startRow + step);
+                expressionContext.setCurrentColumn(column);
+
+                String expression;
+                try {
+                    if (node instanceof ValueHolder) {
+                        expression = ((ValueHolder) node).asString();
+                    } else {
+                        String formula = isOutFunction ? ((FunctionNode) node).getArguments().get(0).toOpenLString() : node.toOpenLString();
+
+                        if (resultColumns == 1) {
+                            expression = "= $Calculation$Result[" + step + "] = " + formula;
+                        } else {
+                            expression = "= $Calculation$Result[" + step + "][" + (column - startColumn) + "] = " + formula;
+                        }
+                    }
+                } catch (RuntimeException e) {
+                    expression = "";
+                    Logger log = LoggerFactory.getLogger(ArrayCellExpressionGridBuilder.class);
+                    log.error(e.getMessage(), e);
+                    ParseError error = GridBuilderUtils.createError(gridBuilder.getRow(),
+                            gridBuilder.getColumn(),
+                            cell,
+                            e);
+                    parseErrors.add(error);
+                }
+                gridBuilder.addCell(expression);
+            }
+            gridBuilder.nextRow();
+        }
+    }
+
+    private static void writeOutCells(StringGridBuilder gridBuilder,
+            ExpressionContext expressionContext,
+            Node node,
+            boolean isOutFunction,
+            int startRow, int startColumn,
+            int endRow, int endColumn,
+            List<ParseError> parseErrors, Cell cell) {
+        // FIXME Here is simple case only
+        for (int row = startRow; row <= endRow; row++) {
+            gridBuilder.addCell("R" + row);
+            gridBuilder.addCell(null);
+
+            for (int column = startColumn; column <= endColumn; column++) {
+                expressionContext.setCurrentRow(row);
+                expressionContext.setCurrentColumn(column);
+
+                String expression;
+                try {
+                    if (node instanceof ValueHolder) {
+                        expression = ((ValueHolder) node).asString();
+                    } else {
+                        if (isOutFunction) {
+                            expression = "= " + node.toOpenLString();
+                        } else {
+                            int rowShift = row - startRow;
+                            int columnShift = column - startColumn;
+                            // TODO Replace Out() function with OutArray (the function that returns Object, not String)
+                            expression = "= Print(" + rowShift + ", " + columnShift + ", $Calculation$Result)";
+                        }
+                    }
+                } catch (RuntimeException e) {
+                    expression = "";
+                    Logger log = LoggerFactory.getLogger(ArrayCellExpressionGridBuilder.class);
+                    log.error(e.getMessage(), e);
+                    ParseError error = GridBuilderUtils.createError(gridBuilder.getRow(),
+                            gridBuilder.getColumn(),
+                            cell,
+                            e);
+                    parseErrors.add(error);
+                }
+                gridBuilder.addCell(expression);
+            }
+            gridBuilder.nextRow();
+        }
+    }
+
+    private static void validate(Cell cell, String workbookName, String sheetName) {
+        Node node = cell.getNode();
+        if (node == null) {
+            throw new IllegalArgumentException("Cell [" + workbookName + "]" + sheetName + "!" + cell
+                    .getAddress()
+                    .toOpenLString() + " contains incorrect value. It will be skipped");
         }
     }
 
