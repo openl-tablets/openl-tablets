4b67523777eeb17df75558db3c9401e91668cb07
Yury Molchan <ymolchan@eisgroup.com>
2016-04-12 21:45:25 +0300

    EPBDS-6165 Extract a method which archives files to an output stream
    --HG--
    branch : 5.17.x

--- DEV/org.openl.commons/src/org/openl/util/ZipUtils.java
+++ DEV/org.openl.commons/src/org/openl/util/ZipUtils.java
@@ -6,6 +6,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.zip.ZipEntry;
@@ -85,21 +86,38 @@ public class ZipUtils {
                 throw new FileNotFoundException("Directory '" + sourceDirectory.getAbsolutePath() + "' is empty!");
             }
         }
-        FileOutputStream fos = new FileOutputStream(targetFile);
-        ZipOutputStream zos = new ZipOutputStream(fos);
+        OutputStream fos = new FileOutputStream(targetFile);
+        try {
+            archive(sourceDirectory, fos);
+        } finally {
+            fos.close();
+        }
+    }
+
+    /**
+     * Pack a file or all files in a source directory to a zipped output stream.
+     * This method does not close the output stream.
+     *
+     * @param source a file or a not empty directory for compressing
+     * @param output a stream for zipped files. It is not closed by this method.
+     * @throws IOException
+     */
+    private static void archive(File source, OutputStream output) throws IOException {
+        ZipOutputStream zos = new ZipOutputStream(output);
         byte data[] = new byte[BUFFER];
-        final String sourceDirAbsolutePath = sourceDirectory.getAbsolutePath() + File.separator;
-        int prefixLength = sourceDirAbsolutePath.length();
-        if (sourceDirectory.isDirectory()) {
-            // This is directory
+        if (source.isDirectory()) {
+            // This is a directory
+            String sourceDirAbsolutePath = source.getAbsolutePath() + File.separator;
+            int prefixLength = sourceDirAbsolutePath.length();
             Queue<File> directoryList = new LinkedList<File>();
-            directoryList.add(sourceDirectory);
+            directoryList.add(source);
             while (!directoryList.isEmpty()) {
                 File directory = directoryList.poll();
 
                 File[] files = directory.listFiles();
 
                 for (File file : files) {
+                    // Remove the absolute part of path
                     String entryName = file.getAbsolutePath().substring(prefixLength);
                     entryName = entryName.replaceAll("\\\\", "/");
                     if (file.isDirectory()) {
@@ -115,11 +133,13 @@ public class ZipUtils {
                 }
             }
         } else {
-            // This is File
-            String name = sourceDirectory.getName();
-            archiveFile(sourceDirectory, name, zos, data);
+            // This is a single File
+            String name = source.getName();
+            archiveFile(source, name, zos, data);
         }
-        zos.close();
+        // Do not close the zip output stream, because it cause closing the
+        // output stream. So finish() is used instead.
+        zos.finish();
     }
 
     private static void archiveFile(File file, String name, ZipOutputStream zos, byte[] data) throws IOException {
