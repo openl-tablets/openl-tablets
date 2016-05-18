27f845a239a96b661440826c335bea3b078c56c1
Yury Molchan <ymolchan@eisgroup.com>
2016-05-18 21:57:03 +0300

    generated comments

--- DEV/org.openl.rules.calculation.result/src/org/openl/rules/calc/result/SpreadsheetResultHelper.java
+++ DEV/org.openl.rules.calculation.result/src/org/openl/rules/calc/result/SpreadsheetResultHelper.java
@@ -1,5 +1,16 @@
 package org.openl.rules.calc.result;
 
+/*
+ * #%L
+ * OpenL - DEV - Rules - Calculation Result
+ * %%
+ * Copyright (C) 2015 - 2016 OpenL Tablets
+ * %%
+ * See the file LICENSE.txt for copying permission.
+ * #L%
+ */
+
+
 public class SpreadsheetResultHelper {
     private SpreadsheetResultHelper() {
     }
--- DEV/org.openl.rules.calculation.result/src/org/openl/rules/calc/result/convertor/package-info.java
+++ DEV/org.openl.rules.calculation.result/src/org/openl/rules/calc/result/convertor/package-info.java
@@ -3,3 +3,13 @@
     elementFormDefault=javax.xml.bind.annotation.XmlNsForm.QUALIFIED
 )
 package org.openl.rules.calc.result.convertor;
+
+/*
+ * #%L
+ * OpenL - DEV - Rules - Calculation Result
+ * %%
+ * Copyright (C) 2015 - 2016 OpenL Tablets
+ * %%
+ * See the file LICENSE.txt for copying permission.
+ * #L%
+ */
