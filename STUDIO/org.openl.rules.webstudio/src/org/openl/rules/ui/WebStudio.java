954e5c2f381b3888a880ed43c6a4ea8f998e9141
Yury Molchan <ymolchan@eisgroup.com>
2016-06-01 17:29:42 +0300

    EPBDS-6204 EPBDS-6205 Reset 'modified' status when a project is reloaded

--- STUDIO/org.openl.rules.webstudio/src/org/openl/rules/ui/WebStudio.java
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/ui/WebStudio.java
@@ -344,6 +344,7 @@ public class WebStudio {
     public void resetProjects() {
         needCompile = true;
         projects = null;
+        model.resetSourceModified();
     }
 
     public void reset() {
