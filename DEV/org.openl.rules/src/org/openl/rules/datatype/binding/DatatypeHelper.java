528c7b1cf0021d7034f960c083d698bc0818fba6
Nail Samatov <nsamatov@eisgroup.com>
2016-06-01 20:21:51 +0300

    EPBDS-6202 EPBDS-6174 Reduce amount of "Cannot find type" logging

--- DEV/org.openl.rules/src/org/openl/rules/datatype/binding/DatatypeHelper.java
+++ DEV/org.openl.rules/src/org/openl/rules/datatype/binding/DatatypeHelper.java
@@ -3,24 +3,20 @@ package org.openl.rules.datatype.binding;
 import org.openl.OpenL;
 import org.openl.binding.IBindingContext;
 import org.openl.binding.IBindingContextDelegator;
-import org.openl.domain.EnumDomain;
-import org.openl.domain.IDomain;
 import org.openl.engine.OpenLManager;
 import org.openl.exception.OpenLCompilationException;
-import org.openl.rules.OpenlToolAdaptor;
-import org.openl.rules.binding.RuleRowHelper;
 import org.openl.rules.lang.xls.XlsNodeTypes;
 import org.openl.rules.lang.xls.syntax.TableSyntaxNode;
+import org.openl.rules.table.ICell;
 import org.openl.rules.table.ILogicalTable;
 import org.openl.rules.table.openl.GridCellSourceCodeModule;
 import org.openl.rules.table.properties.PropertiesHelper;
 import org.openl.source.IOpenSourceCodeModule;
-import org.openl.syntax.exception.SyntaxNodeException;
 import org.openl.syntax.exception.SyntaxNodeExceptionUtils;
 import org.openl.syntax.impl.IdentifierNode;
 import org.openl.syntax.impl.Tokenizer;
 import org.openl.types.IOpenClass;
-import org.openl.util.ArrayTool;
+import org.openl.util.StringUtils;
 
 public class DatatypeHelper {
 
@@ -31,6 +27,9 @@ public class DatatypeHelper {
      * 3) Third column - default value
      */
     private static final int MAXIMUM_COLUMNS_COUNT = 3;
+    private static final int TYPE_NAME_COLUMN = 0;
+    private static final int FIELD_NAME_COLUMN = 1;
+    private static final int DEFAULTS_COLUMN = 2;
 
     public static ILogicalTable getNormalizedDataPartTable(ILogicalTable table, OpenL openl, IBindingContext cxt) {
         
@@ -60,7 +59,19 @@ public class DatatypeHelper {
             return dataPart.transpose();
         }
 
+        if (dataPart.getWidth() == MAXIMUM_COLUMNS_COUNT && isThirdColumnForDefaults(dataPart)) {
+            return dataPart;
+        }
+
+        if (dataPart.getHeight() == MAXIMUM_COLUMNS_COUNT && isThirdColumnForDefaults(dataPart.transpose())) {
+            return dataPart.transpose();
+        }
+
         int verticalCount = countTypes(dataPart, openl, cxt);
+        if (verticalCount == dataPart.getHeight() && verticalCount >= dataPart.getWidth()) {
+            // There is no need to check horizontal types.
+            return dataPart;
+        }
         int horizontalCount = countTypes(dataPart.transpose(), openl, cxt);
 
         if (verticalCount < horizontalCount) {
@@ -70,12 +81,31 @@ public class DatatypeHelper {
         return dataPart;
     }
 
+    private static boolean isThirdColumnForDefaults(ILogicalTable table) {
+        // If first or second row is blank or starts with number, it can't be a type name and field name respectively,
+        // in this case we can assume that the third column is definitely for defaults
+        return isDefault(table.getCell(DEFAULTS_COLUMN, TYPE_NAME_COLUMN)) ||
+                isDefault(table.getCell(DEFAULTS_COLUMN, FIELD_NAME_COLUMN));
+    }
+
+    private static boolean isDefault(ICell cell) {
+        // Type name and field name can't be blank or start with number but default value can.
+        String value = cell.getStringValue();
+        if (StringUtils.isBlank(value)) {
+            return true;
+        }
+
+        char firstChar = value.charAt(0);
+        return '0' <= firstChar && firstChar <= '9';
+
+    }
+
     private static int countTypes(ILogicalTable table, OpenL openl, IBindingContext cxt) {
 
         int height = table.getHeight();
-        int count = 0;
+        int count = 1; // The first cell is always type name, there is no need to check it. Start from the second one.
 
-        for (int i = 0; i < height; ++i) {
+        for (int i = 1; i < height; ++i) {
             try {
                 IOpenClass type = makeType(table.getRow(i), openl, cxt);
                 if (type != null) {
