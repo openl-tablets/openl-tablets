d579bb5e3a97b7a224c949702bed1125bfafee99
Marat Kamalov <mkamalov@eisgroup.com>
2016-06-21 21:22:11 +0300

    EPBDS-6248 Error doesn't display "There more than one activ table.

--- DEV/org.openl.rules/src/org/openl/rules/validation/TablesValidator.java
+++ DEV/org.openl.rules/src/org/openl/rules/validation/TablesValidator.java
@@ -1,6 +1,10 @@
 package org.openl.rules.validation;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.openl.OpenL;
+import org.openl.dependency.CompiledDependency;
 import org.openl.rules.lang.xls.binding.XlsMetaInfo;
 import org.openl.rules.lang.xls.binding.XlsModuleOpenClass;
 import org.openl.rules.lang.xls.syntax.TableSyntaxNode;
@@ -10,16 +14,30 @@ import org.openl.validation.ValidationResult;
 
 public abstract class TablesValidator implements IOpenLValidator {
 
+    private void findAllTableSyntaxNodes(List<TableSyntaxNode> tableSyntaxNodes, IOpenClass openClass) {
+        for (CompiledDependency compiledDependency : ((XlsModuleOpenClass) openClass).getDependencies()) {
+            IOpenClass dependencyOpenClass = compiledDependency.getCompiledOpenClass().getOpenClass();
+            findAllTableSyntaxNodes(tableSyntaxNodes, dependencyOpenClass);
+        }
+
+        XlsMetaInfo xlsMetaInfo = ((XlsModuleOpenClass) openClass).getXlsMetaInfo();
+        TableSyntaxNode[] xlsTableSyntaxNodes = xlsMetaInfo.getXlsModuleNode().getXlsTableSyntaxNodes();
+        for (TableSyntaxNode tableSyntaxNode : xlsTableSyntaxNodes) {
+            tableSyntaxNodes.add(tableSyntaxNode);
+        }
+    }
+
     public ValidationResult validate(OpenL openl, IOpenClass openClass) {
 
         if (openClass instanceof XlsModuleOpenClass) {
 
             // Get all table syntax nodes of xls module.
             //
-            XlsMetaInfo xlsMetaInfo = ((XlsModuleOpenClass) openClass).getXlsMetaInfo();
-            TableSyntaxNode[] tableSyntaxNodes = xlsMetaInfo.getXlsModuleNode().getXlsTableSyntaxNodes();
+            List<TableSyntaxNode> tableSyntaxNodes = new ArrayList<TableSyntaxNode>();
 
-            return validateTables(openl, tableSyntaxNodes, openClass);
+            findAllTableSyntaxNodes(tableSyntaxNodes, openClass);
+
+            return validateTables(openl, tableSyntaxNodes.toArray(new TableSyntaxNode[] {}), openClass);
         }
 
         // Skip validation if passed open class is not instance of
@@ -28,5 +46,7 @@ public abstract class TablesValidator implements IOpenLValidator {
         return ValidationUtils.validationSuccess();
     }
 
-    public abstract ValidationResult validateTables(OpenL openl, TableSyntaxNode[] tableSyntaxNodes, IOpenClass openClass);
+    public abstract ValidationResult validateTables(OpenL openl,
+            TableSyntaxNode[] tableSyntaxNodes,
+            IOpenClass openClass);
 }
