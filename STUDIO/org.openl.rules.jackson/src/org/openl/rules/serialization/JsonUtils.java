ef81edaeaff417bfb365b22078d5768b436f9496
Marat Kamalov <mkamalov@eisgroup.com>
2016-10-05 14:34:24 +0300

    EPBDS-6321 Implement JsonUtils

---
+++ STUDIO/org.openl.rules.jackson/src/org/openl/rules/serialization/JsonUtils.java
@@ -0,0 +1,109 @@
+package org.openl.rules.serialization;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/*
+ * #%L
+ * OpenL - Rules - Serialization
+ * %%
+ * Copyright (C) 2016 OpenL Tablets
+ * %%
+ * See the file LICENSE.txt for copying permission.
+ * #L%
+ */
+
+public final class JsonUtils {
+    public static final class DefaultObjectMapperHolder {
+        private static final ObjectMapper INSTANCE;
+        static {
+            JacksonObjectMapperFactoryBean jacksonObjectMapperFactoryBean = new JacksonObjectMapperFactoryBean();
+            jacksonObjectMapperFactoryBean.setEnableDefaultTyping(false);
+            jacksonObjectMapperFactoryBean.setSupportVariations(true);
+            INSTANCE = jacksonObjectMapperFactoryBean.createJacksonObjectMapper();
+        }
+    }
+
+    public static ObjectMapper createJacksonObjectMapper(Class<?>[] types, boolean enableDefaultTyping) {
+        JacksonObjectMapperFactoryBean jacksonObjectMapperFactoryBean = new JacksonObjectMapperFactoryBean();
+        jacksonObjectMapperFactoryBean.setEnableDefaultTyping(enableDefaultTyping);
+        jacksonObjectMapperFactoryBean.setSupportVariations(true);
+        Set<String> overideTypes = new HashSet<String>();
+        for (Class<?> type : types) {
+            overideTypes.add(type.getCanonicalName());
+        }
+        jacksonObjectMapperFactoryBean.setOverrideTypes(overideTypes);
+        return jacksonObjectMapperFactoryBean.createJacksonObjectMapper();
+    }
+
+    private static ObjectMapper getDefaultJacksonObjectMapper() {
+        return DefaultObjectMapperHolder.INSTANCE;
+    }
+
+    public static String toJSON(Object value) throws JsonProcessingException {
+        return getDefaultJacksonObjectMapper().writeValueAsString(value);
+    }
+
+    public static String toJSON(Object value, ObjectMapper objectMapper) throws JsonProcessingException {
+        return objectMapper.writeValueAsString(value);
+    }
+
+    public static String toJSON(Object value, Class<?>[] types) throws JsonProcessingException {
+        return toJSON(value, types, false);
+    }
+
+    public static String toJSON(Object value,
+            Class<?>[] types,
+            boolean enableDefaultTyping) throws JsonProcessingException {
+        if (types == null) {
+            types = new Class<?>[0];
+        }
+        ObjectMapper objectMapper = createJacksonObjectMapper(types, enableDefaultTyping);
+        return objectMapper.writeValueAsString(value);
+    }
+
+    public static Map<String, String> splitJSON(String jsonString) throws IOException, JsonProcessingException {
+        ObjectMapper objectMapper = getDefaultJacksonObjectMapper();
+        JsonNode rootNode = objectMapper.readTree(jsonString);
+        Map<String, String> splitMap = new HashMap<String, String>();
+        Iterator<Map.Entry<String, JsonNode>> fieldsIterator = rootNode.fields();
+        while (fieldsIterator.hasNext()) {
+            Map.Entry<String, JsonNode> field = fieldsIterator.next();
+            splitMap.put(field.getKey(), objectMapper.writeValueAsString(field.getValue()));
+            System.out.println("Key: " + field.getKey() + "\tValue:" + field.getValue());
+        }
+        return splitMap;
+    }
+
+    public static <T> T fromJSON(String jsonString, Class<T> readType, ObjectMapper objectMapper) throws IOException, JsonProcessingException {
+        return objectMapper.readValue(jsonString, readType);
+    }
+
+    public static <T> T fromJSON(String jsonString, Class<T> readType) throws IOException, JsonProcessingException {
+        return getDefaultJacksonObjectMapper().readValue(jsonString, readType);
+    }
+    
+    public static <T> T fromJSON(String jsonString, Class<T> readType, Class<?>[] types) throws IOException,
+                                                                                         JsonProcessingException {
+        return fromJSON(jsonString, readType, types, false);
+    }
+
+    public static <T> T fromJSON(String jsonString,
+            Class<T> readType,
+            Class<?>[] types,
+            boolean enableDefaultTyping) throws IOException, JsonProcessingException {
+        if (types == null) {
+            types = new Class<?>[0];
+        }
+        ObjectMapper objectMapper = createJacksonObjectMapper(types, enableDefaultTyping);
+        return objectMapper.readValue(jsonString, readType);
+    }
+}
