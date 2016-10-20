d77e4a09f0865a452c0a5a60ad3fdb6e9d4d1af9
Yury Molchan <ymolchan@eisgroup.com>
2016-10-20 13:53:56 +0300

    EPBDS-6333 EPBDS-6330 Use in-memory DB for deployment repository by default

--- DEMO/org.openl.rules.demo/apache-tomcat-conf/bin/setenv.sh
+++ DEMO/org.openl.rules.demo/apache-tomcat-conf/bin/setenv.sh
@@ -1,5 +1,4 @@
 export JAVA_OPTS="$JAVA_OPTS -Xms512m -Xmx2g -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:PermSize=128m -XX:MaxPermSize=512m"
 export DEMO_OPTS="$DEMO_OPTS -Druleservice.rmiPort=8099 -Druleservice.openl.home=openl-demo"
-export DEMO_OPTS="$DEMO_OPTS -Druleservice.datasource.repositoryPropertiesFile=openl-demo\system-settings\rules-production.properties"
 export DEMO_OPTS="$DEMO_OPTS -Dwebstudio.home=openl-demo -Dwebstudio.configured=true -Dws.port=8080"
 export CATALINA_OPTS="$DEMO_OPTS $CATALINA_OPTS -Dderby.stream.error.file=..\\logs\\derby.log"
