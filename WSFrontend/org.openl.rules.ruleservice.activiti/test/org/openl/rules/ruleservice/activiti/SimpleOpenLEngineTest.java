ab951fad7877a57a1c9f925794a0e87696d67bfe
Yury Molchan <ymolchan@eisgroup.com>
2016-10-20 22:57:17 +0300

    EPBDS-6333 Remove dublicated configs

--- WSFrontend/org.openl.rules.ruleservice.activiti/test/org/openl/rules/ruleservice/activiti/SimpleOpenLEngineTest.java
+++ WSFrontend/org.openl.rules.ruleservice.activiti/test/org/openl/rules/ruleservice/activiti/SimpleOpenLEngineTest.java
@@ -11,9 +11,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
+@TestPropertySource(properties = { "ruleservice.datasource.type=local",
+        "ruleservice.datasource.deploy.classpath.jars=false",
+        "ruleservice.isProvideRuntimeContext=false",
+        "ruleservice.datasource.dir=test-resources/datasource" })
 @ContextConfiguration(locations = { "classpath:activiti.cfg.xml" })
 public class SimpleOpenLEngineTest {
 
