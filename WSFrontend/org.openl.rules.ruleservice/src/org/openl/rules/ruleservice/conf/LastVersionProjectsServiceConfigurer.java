27d904c1c7878267e1dfee8b8a8724dc13db1fcf
Yury Molchan <ymolchan@eisgroup.com>
2016-10-31 19:04:48 +0300

    Declare rulesDeploy in the place of usage

--- WSFrontend/org.openl.rules.ruleservice/src/org/openl/rules/ruleservice/conf/LastVersionProjectsServiceConfigurer.java
+++ WSFrontend/org.openl.rules.ruleservice/src/org/openl/rules/ruleservice/conf/LastVersionProjectsServiceConfigurer.java
@@ -57,13 +57,12 @@ public class LastVersionProjectsServiceConfigurer implements ServiceConfigurer {
             for (AProject project : deployment.getProjects()) {
                 try {
                     InputStream content = null;
-                    RulesDeploy rulesDeploy = null;
                     try {
                         AProjectArtefact artifact = project.getArtefact(RULES_DEPLOY_XML);
                         if (artifact instanceof AProjectResource) {
                             AProjectResource resource = (AProjectResource) artifact;
                             content = resource.getContent();
-                            rulesDeploy = getRulesDeploySerializer().deserialize(content);
+                            RulesDeploy rulesDeploy = getRulesDeploySerializer().deserialize(content);
                             hasRulesDeployXML = true;
                             String version = null;
                             if (StringUtils.isNotEmpty(rulesDeploy.getVersion())) {
