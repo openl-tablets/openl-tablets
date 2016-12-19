f5fb8b5497a879ada752f3bd7b77eef29834368e
Yury Molchan <ymolchan@eisgroup.com>
2016-12-19 13:40:03 +0300

    EPBDS-6414 EPBDS-6366 Fix NPE on URLs parsing

--- DEV/org.openl.rules/src/org/openl/rules/table/xls/XlsUrlParser.java
+++ DEV/org.openl.rules/src/org/openl/rules/table/xls/XlsUrlParser.java
@@ -8,13 +8,13 @@ package org.openl.rules.table.xls;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.URL;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.StringTokenizer;
 
-import org.openl.main.SourceCodeURLTool;
 import org.openl.rules.table.syntax.XlsURLConstants;
-import org.openl.util.FileTypeHelper;
 import org.openl.util.RuntimeExceptionWrapper;
+import org.openl.util.StringTool;
 
 /**
  * @author sam
@@ -22,156 +22,67 @@ import org.openl.util.RuntimeExceptionWrapper;
 
 public class XlsUrlParser implements XlsURLConstants {
 
-    public static final String FILE_PROTOCOL = "file:";
-
     public String wbPath;
     public String wbName;
     public String wsName;
 
     public String range;
-
     public String cell;
 
-    static boolean endsWithSlash(String s) {
-        return s.length() > 0 && isSlash(s.charAt(s.length() - 1));
-    }
-
-    static boolean isSlash(char c) {
-        return c == '/' || c == '\\';
-    }
-
-    /**
-     * Guesses if path is File URL.
-     *
-     * @return canonical URL for the File or null.
-     */
-    URL guessFileURL(String url) {
-        url = ltrimFileURL(url);
-
-        // Wintel's "C:..." are files, other - not (poor Mac:)
-        int idx = url.indexOf(':');
-        if (idx == 0 || idx > 1) {
-            return null;
-        }
-
-        try {
-            File f = new File(url); 
-            return (f.getCanonicalFile().toURI()).toURL();
-        } catch (Throwable t) {
-            throw RuntimeExceptionWrapper.wrap(t);
-        }
-    }
-
-    String ltrimFileURL(String url) {
-        url = url.replace('\\', '/');
-
-        if (url.startsWith(FILE_PROTOCOL)) {
-            url = url.substring(FILE_PROTOCOL.length());
-        }
-
-        // Wintel's "/C:..." - just in case...
-        if (url.indexOf('/') == 0 && url.indexOf(':') == 2) {
-            url = url.substring(1);
-        }
-
-        return url;
-    }
-
-    public boolean isExcelFile() {
-	    return FileTypeHelper.isExcelFile(wbName);
-	}
-
     public void parse(String url) {
-        // String SCRIPT_NAME = "LaunchExcel.vbs";
 
-        // scriptName =
-        // toCanonicalUrl(OpenlRulesPlugin.getDefault().getBundle(),
-        // SCRIPT_NAME);
+        String file;
+        Map<String, String> map = new HashMap<String, String>();
+        int indexQuestionMark = url.indexOf('?');
+        if (indexQuestionMark >= 0) {
+            file = url.substring(0, indexQuestionMark);
+            String query = url.substring(indexQuestionMark + 1);
 
-        Map<String, String> urlMap = SourceCodeURLTool.parseUrl(url);
+            StringTokenizer st = new StringTokenizer(query, QSEP);
 
-        String file = urlMap.get(FILE);
-        wsName = urlMap.get(SHEET);
+            while (st.hasMoreTokens()) {
+                String pair = st.nextToken();
 
-        range = urlMap.get(RANGE);
+                int idx = pair.indexOf('=');
+
+                if (idx < 0) {
+                    map.put(pair, "");
+                } else {
+                    String key = pair.substring(0, idx);
+                    String value = StringTool.decodeURL(pair.substring(idx + 1, pair.length()));
+                    map.put(key, value);
+                }
+            }
+        } else {
+            file = url;
+        }
+        file = StringTool.decodeURL(file);
+        wsName = map.get(SHEET);
+        range = map.get(RANGE);
+        cell = map.get(CELL);
 
         if (range == null) {
             // TODO line, col
-            range = urlMap.get(CELL);
+            range = cell;
         }
 
-        cell = urlMap.get(CELL);
-
-        File f = null;
-        try {
-            f = new File(file).getCanonicalFile();
-            wbPath = f.getParent();
-            wbName = f.getName();
-        } catch (IOException e) {
-            throw RuntimeExceptionWrapper.wrap(e);
-        } catch (NullPointerException ex) {
+        if ("null".equals(file)) {
             // there is no file representation
             // FIXME tempory hack to support generated dispatch tables
             wbPath = "/unexistingPath/";
             wbName = "unexistingSourceFile.xls";
+        } else {
+            if (file.startsWith("file:/")) {
+                file = file.substring(6);
+            }
+            try {
+                File f = new File(file).getCanonicalFile();
+                wbPath = f.getParent();
+                wbName = f.getName();
+            } catch (IOException e) {
+                throw RuntimeExceptionWrapper.wrap(e);
+            }
         }
-
-        
-
-        // wsName = url.substring(iPound + 1, iAt);
-
-        // range = url.substring(iAt + 1);
-
-    }
-
-    /**
-     * Returns canonical URL for the path. Note: File URLs are without "file:"
-     * protocol.
-     */
-    public String toCanonicalURL(String path) {
-        String url = toURL(path).toExternalForm();
-
-        url = ltrimFileURL(url);
-
-        return url;
-    }
-
-    /**
-     * Returns canonical URL for the (parent,child)-resource.
-     */
-    public String toCanonicalURL(String parent, String child) {
-        String url = toCanonicalURL(parent);
-        if (child != null && child.length() > 0) {
-            url += '/' + child;
-        }
-        return url;
-    }
-
-    /**
-     * Returns URL for the path.
-     */
-    public URL toURL(String path) {
-        // 'new URL()' is NOGOOD for Wintel's names - guess ourself.
-        URL url = guessFileURL(path);
-        if (url != null) {
-            return url;
-        }
-
-        try {
-            return new URL(path);
-        } catch (Throwable t) {
-            throw RuntimeExceptionWrapper.wrap(t);
-        }
-    }
-
-    public URL[] toURL(String[] path) {
-        URL[] urls = new URL[path.length];
-
-        for (int i = 0; i < path.length; i++) {
-            urls[i] = toURL(path[i]);
-        }
-
-        return urls;
     }
 
 }
