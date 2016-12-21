25105bd11822f2a19f70b858180fd56c86199f8b
Yury Molchan <ymolchan@eisgroup.com>
2016-12-21 23:48:07 +0300

    EPBDS-6357 Delete an ignored test wich used obsolete deployment repository API

--- WSFrontend/org.openl.rules.ruleservice.ws/test/org/openl/rules/ruleservice/publish/WebServicesExposingTest.java
+++ WSFrontend/org.openl.rules.ruleservice.ws/test/org/openl/rules/ruleservice/publish/WebServicesExposingTest.java
@@ -1,21 +1,12 @@
 package org.openl.rules.ruleservice.publish;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNotSame;
 import static org.junit.Assert.assertTrue;
 
 import org.apache.cxf.frontend.ServerFactoryBean;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.openl.rules.project.abstraction.ADeploymentProject;
-import org.openl.rules.project.abstraction.Deployment;
-import org.openl.rules.repository.ProductionRepositoryFactoryProxy;
-import org.openl.rules.ruleservice.core.OpenLService;
 import org.openl.rules.ruleservice.management.ServiceManagerImpl;
-import org.openl.rules.workspace.WorkspaceUserImpl;
-import org.openl.rules.workspace.deploy.impl.jcr.JcrProductionDeployer;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
@@ -43,32 +34,6 @@ public class WebServicesExposingTest implements ApplicationContextAware {
         ServerFactoryBean firstServer = webServicesRuleServicePublisher.getServerFactoryBean();
         ServerFactoryBean secondServer = webServicesRuleServicePublisher.getServerFactoryBean();
         assertTrue(firstServer != secondServer);
-    }
-
-    @Test
-    @Ignore
-    public void testRedeployAfterChanges() throws Exception {
-        assertNotNull(applicationContext);
-        ServiceManagerImpl serviceManager = applicationContext.getBean("serviceManager", ServiceManagerImpl.class);
-        assertNotNull(serviceManager);
-        serviceManager.start();
-        OpenLService multimoduleService = serviceManager.getRuleService().getServiceByName("multimodule");
-        // RuleServiceLoader rulesLoader = serviceManager.getRuleServiceLoader();
-        Deployment domainDeployment = null; //rulesLoader.getDeployment("multimodule", new CommonVersionImpl(1));
-        ADeploymentProject testDeploymentProject = null;// FIXME: new ADeploymentProject(null, , );
-        ProductionRepositoryFactoryProxy proxy = applicationContext.getBean("productionRepositoryFactoryProxy", ProductionRepositoryFactoryProxy.class);
-        new JcrProductionDeployer(proxy, ProductionRepositoryFactoryProxy.DEFAULT_REPOSITORY_PROPERTIES_FILE).deploy(testDeploymentProject,
-                domainDeployment.getProjects(), new WorkspaceUserImpl("test"));
-        for (int i = 0; i < 12; i++) {// waiting for redeploying of services
-                                      // during.
-            Thread.sleep(5000); // notifications come asynchroniously
-            if (multimoduleService != serviceManager.getRuleService().getServiceByName("multimodule")) {
-                break;
-            }
-        }
-        assertEquals(2, applicationContext.getBean(JAXWSRuleServicePublisher.class).getServices()
-                .size());
-        assertNotSame(multimoduleService, serviceManager.getRuleService().getServiceByName("multimodule"));
     }
 
     public static void main(String[] args) throws Exception {
