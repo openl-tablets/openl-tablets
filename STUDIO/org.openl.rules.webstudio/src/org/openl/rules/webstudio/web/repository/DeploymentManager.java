64835c74281b67c0f3f1780a0f568a396e71394c
Yury Molchan <ymolchan@eisgroup.com>
2016-12-21 22:46:16 +0300

    EPBDS-6357 Deploy to Repository directly without AProject abstraction

--- STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/web/repository/DeploymentManager.java
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/web/repository/DeploymentManager.java
@@ -1,26 +1,25 @@
 package org.openl.rules.webstudio.web.repository;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.openl.commons.web.jsf.FacesUtils;
 import org.openl.rules.common.ProjectDescriptor;
 import org.openl.rules.common.ProjectException;
+import org.openl.rules.common.ProjectVersion;
 import org.openl.rules.project.abstraction.ADeploymentProject;
-import org.openl.rules.project.abstraction.AProject;
 import org.openl.rules.repository.ProductionRepositoryFactoryProxy;
+import org.openl.rules.repository.api.FileData;
+import org.openl.rules.repository.api.FileItem;
+import org.openl.rules.repository.api.Repository;
 import org.openl.rules.repository.exceptions.RRepositoryException;
 import org.openl.rules.webstudio.web.util.WebStudioUtils;
 import org.openl.rules.workspace.WorkspaceException;
-import org.openl.rules.workspace.WorkspaceUserImpl;
 import org.openl.rules.workspace.deploy.DeployID;
+import org.openl.rules.workspace.deploy.DeployUtils;
 import org.openl.rules.workspace.deploy.DeploymentException;
-import org.openl.rules.workspace.deploy.ProductionDeployer;
-import org.openl.rules.workspace.deploy.impl.jcr.JcrProductionDeployer;
 import org.openl.rules.workspace.dtr.DesignTimeRepository;
-import org.openl.rules.workspace.dtr.RepositoryException;
 import org.springframework.beans.factory.InitializingBean;
 
 /**
@@ -32,44 +31,61 @@ public class DeploymentManager implements InitializingBean {
     private String[] initialProductionRepositoryConfigNames;
     private DesignTimeRepository designRepository;
 
-    private Map<String, ProductionDeployer> deployers = new HashMap<String, ProductionDeployer>();
+    private Set<String> deployers = new HashSet<String>();
 
     public void addRepository(String repositoryConfigName) {
-        deployers.put(repositoryConfigName, new JcrProductionDeployer(repositoryFactoryProxy, repositoryConfigName));
+        deployers.add(repositoryConfigName);
     }
 
     public void removeRepository(String repositoryConfigName) throws RRepositoryException {
-        if (deployers.containsKey(repositoryConfigName)) {
-            deployers.get(repositoryConfigName).destroy();
-            deployers.remove(repositoryConfigName);
-        }
+        deployers.remove(repositoryConfigName);
+        repositoryFactoryProxy.releaseRepository(repositoryConfigName);
     }
 
     public Collection<String> getRepositoryConfigNames() {
-        return deployers.keySet();
+        return deployers;
     }
 
-    public DeployID deploy(ADeploymentProject project, String repositoryConfigName) throws WorkspaceException, ProjectException {
-        ProductionDeployer deployer = deployers.get(repositoryConfigName);
-        if (deployer == null) {
+    public DeployID deploy(ADeploymentProject project, String repositoryConfigName) throws WorkspaceException,
+                                                                                    ProjectException {
+        if (!deployers.contains(repositoryConfigName)) {
             throw new IllegalArgumentException("No such repository '" + repositoryConfigName + "'");
         }
 
-        WorkspaceUserImpl user = new WorkspaceUserImpl(WebStudioUtils.getRulesUserSession(FacesUtils.getSession()).getUserName());
+        String userName = WebStudioUtils.getRulesUserSession(FacesUtils.getSession()).getUserName();
 
         @SuppressWarnings("rawtypes")
         Collection<ProjectDescriptor> projectDescriptors = project.getProjectDescriptors();
-        Collection<AProject> projects = new ArrayList<AProject>();
 
-        for (ProjectDescriptor<?> pd : projectDescriptors) {
-            try {
-                projects.add(designRepository.getProject(pd.getProjectName(), pd.getProjectVersion()));
-            } catch (RepositoryException e) {
-                throw new DeploymentException(e.getMessage(), e);
+        try {
+            Repository deployRepo = repositoryFactoryProxy.getRepositoryInstance(repositoryConfigName);
+            StringBuilder sb = new StringBuilder(project.getName());
+            ProjectVersion projectVersion = project.getVersion();
+            if (projectVersion != null) {
+                int version = DeployUtils.getNextDeploymentVersion(deployRepo, project);
+                sb.append('#').append(version);
             }
-        }
+            DeployID id = new DeployID(sb.toString());
 
-        return deployer.deploy(project, projects, user);
+            String deploymentPath = DeployUtils.DEPLOY_PATH + id.getName();
+            Repository designRepo = designRepository.getRepository();
+            for (ProjectDescriptor<?> pd : projectDescriptors) {
+                String version = pd.getProjectVersion().getVersionName();
+                String projectName = pd.getProjectName();
+                FileItem srcPrj = designRepo.readHistory("DESIGN/rules/" + projectName, version);
+                FileData dest = new FileData();
+                dest.setName(deploymentPath + "/" + projectName);
+                dest.setAuthor(userName);
+                dest.setComment(srcPrj.getData().getComment());
+                deployRepo.save(dest, srcPrj.getStream());
+            }
+
+            // TODO: Some analogue of notifyChanges() possibly will be needed
+            // rRepository.notifyChanges();
+            return id;
+        } catch (Exception e) {
+            throw new DeploymentException("Failed to deploy: " + e.getMessage(), e);
+        }
     }
 
     private ProductionRepositoryFactoryProxy repositoryFactoryProxy;
@@ -81,7 +97,7 @@ public class DeploymentManager implements InitializingBean {
     public void setInitialProductionRepositoryConfigNames(String[] initialProductionRepositoryConfigNames) {
         this.initialProductionRepositoryConfigNames = initialProductionRepositoryConfigNames;
     }
-    
+
     public void setDesignRepository(DesignTimeRepository designRepository) {
         this.designRepository = designRepository;
     }
