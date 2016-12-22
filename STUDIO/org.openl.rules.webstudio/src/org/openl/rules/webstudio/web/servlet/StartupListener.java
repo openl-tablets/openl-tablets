356659b2fb0bff29231cc164349ae1f97db5198f
Yury Molchan <ymolchan@eisgroup.com>
2016-12-22 16:14:02 +0300

    EPBDS-6426 Delete old unused functionality from org.openl.config

--- STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/web/servlet/StartupListener.java
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/web/servlet/StartupListener.java
@@ -7,7 +7,6 @@ import org.slf4j.LoggerFactory;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
-import java.io.InputStream;
 import java.util.Map;
 
 /**
@@ -19,46 +18,10 @@ public class StartupListener implements ServletContextListener {
 
     private final Logger log = LoggerFactory.getLogger(StartupListener.class);
 
-    private static class WebConfigLocator extends ConfigLocator {
-        private final ServletContext context;
-
-        private WebConfigLocator(ServletContext context) {
-            this.context = context;
-        }
-
-        @Override
-        public InputStream locate(String fullName) {
-            String resPath = "/WEB-INF/conf/";
-
-            if (fullName.startsWith("/")) {
-                resPath += fullName.substring(1);
-            } else {
-                resPath += fullName;
-            }
-
-            return context.getResourceAsStream(resPath);
-        }
-
-    }
-
     public void contextInitialized(ServletContextEvent event) {
         ServletContext context = event.getServletContext();
         String name = context.getServletContextName();
         log.info("Starting {}...", name);
-
-        ConfigManager configManager = new ConfigManager();
-
-        ClassPathConfigLocator defLocator = new ClassPathConfigLocator();
-        defLocator.setPriority(0);
-
-        WebConfigLocator webLocator = new WebConfigLocator(context);
-        webLocator.setPriority(100);
-
-        configManager.addLocator(webLocator);
-        configManager.addLocator(defLocator);
-
-        // Replace system config manager
-        SysConfigManager.setConfigManager(configManager);
 
         initSystemProperties();
     }
@@ -87,8 +50,6 @@ public class StartupListener implements ServletContextListener {
     }
 
     public void contextDestroyed(ServletContextEvent event) {
-        // Clear previous ConfigManager
-        SysConfigManager.setConfigManager(new ConfigManager());
         String name = event.getServletContext().getServletContextName();
         log.info("{} is down.", name);
     }
