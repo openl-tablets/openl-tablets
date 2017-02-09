6ee98c279697953ab2e66d89a90291863597ee54
Yury Molchan <ymolchan@eisgroup.com>
2017-02-09 21:03:33 +0300

    Simplify configuration to generate classes in openl-maven-plugin

--- Util/openl-maven-plugin/src/main/java/org/openl/rules/maven/BaseOpenLMojo.java
+++ Util/openl-maven-plugin/src/main/java/org/openl/rules/maven/BaseOpenLMojo.java
@@ -1,24 +1,29 @@
 package org.openl.rules.maven;
 
+import java.io.File;
+
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 
-import java.io.File;
+import org.openl.CompiledOpenClass;
+import org.openl.rules.project.instantiation.SimpleProjectEngineFactory;
+import org.openl.rules.project.instantiation.SimpleProjectEngineFactory.SimpleProjectEngineFactoryBuilder;
 
-public abstract class BaseOpenLMojo extends AbstractMojo {
+abstract class BaseOpenLMojo extends AbstractMojo {
 
     /**
-     * Folder that contains all OpenL-related resources (OpenL rules,
-     * project descriptor etc.). For example: "${project.basedir}/src/main/openl".
+     * Folder that contains all OpenL-related resources (OpenL rules, project
+     * descriptor etc.). For example: "${project.basedir}/src/main/openl".
      */
     @Parameter(defaultValue = "${project.build.sourceDirectory}/../openl")
     private File sourceDirectory;
 
     /**
-     * Folder that contains all OpenL-related resources (OpenL rules,
-     * project descriptor etc.).
+     * Folder that contains all OpenL-related resources (OpenL rules, project
+     * descriptor etc.).
+     * 
      * @deprecated Use sourceDirectory instead.
      */
     @Deprecated
@@ -26,18 +31,28 @@ public abstract class BaseOpenLMojo extends AbstractMojo {
     private String openlResourcesDirectory;
 
     /**
-     * Folder used by OpenL to compile rules. For example: ${project.build.directory}/openl".
+     * Folder used by OpenL to compile rules. For example:
+     * ${project.build.directory}/openl".
      */
     @Parameter(defaultValue = "${project.build.directory}/openl")
-    protected String openlOutputDirectory;
+    String openlOutputDirectory;
 
     @Component
     protected MavenProject project;
 
-    protected File getSourceDirectory() {
+    File getSourceDirectory() {
         if (openlResourcesDirectory != null) {
             return new File(openlResourcesDirectory);
         }
         return sourceDirectory;
+    }
+
+    CompiledOpenClass compileOpenLRules() throws Exception {
+        String openlRoot = getSourceDirectory().getPath();
+
+        SimpleProjectEngineFactoryBuilder<?> builder = new SimpleProjectEngineFactoryBuilder<Object>();
+        SimpleProjectEngineFactory<?> factory = builder.setProject(openlRoot).build();
+
+        return factory.getCompiledOpenClass();
     }
 }
