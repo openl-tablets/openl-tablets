0eaa09d36b5a67adc3211eb9489b1304bf3e62a5
Yury Molchan <ymolchan@eisgroup.com>
2017-02-10 00:50:24 +0300

    Move common parts to BaseOpenLMojo
    --HG--
    branch : 5.19.x

--- Util/openl-maven-plugin/src/main/java/org/openl/rules/maven/BaseOpenLMojo.java
+++ Util/openl-maven-plugin/src/main/java/org/openl/rules/maven/BaseOpenLMojo.java
@@ -3,6 +3,8 @@ package org.openl.rules.maven;
 import java.io.File;
 
 import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
@@ -12,6 +14,7 @@ import org.openl.rules.project.instantiation.SimpleProjectEngineFactory;
 import org.openl.rules.project.instantiation.SimpleProjectEngineFactory.SimpleProjectEngineFactoryBuilder;
 
 abstract class BaseOpenLMojo extends AbstractMojo {
+    private static final String SEPARATOR = "--------------------------------------------------";
 
     /**
      * Folder that contains all OpenL-related resources (OpenL rules, project
@@ -23,7 +26,7 @@ abstract class BaseOpenLMojo extends AbstractMojo {
     /**
      * Folder that contains all OpenL-related resources (OpenL rules, project
      * descriptor etc.).
-     * 
+     *
      * @deprecated Use sourceDirectory instead.
      */
     @Deprecated
@@ -41,15 +44,60 @@ abstract class BaseOpenLMojo extends AbstractMojo {
         return sourceDirectory;
     }
 
-    CompiledOpenClass compileOpenLRules() throws Exception {
-        String openlRoot = getSourceDirectory().getPath();
-        if (getLog().isInfoEnabled()) {
-            getLog().info("Compiling the OpenL project from " + openlRoot);
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        if (isDisabled()) {
+            return;
         }
+        CompiledOpenClass openLRules;
+        try {
+            String openlRoot = getSourceDirectory().getPath();
+            info("Compiling the OpenL project from " + openlRoot);
 
-        SimpleProjectEngineFactoryBuilder<?> builder = new SimpleProjectEngineFactoryBuilder<Object>();
-        SimpleProjectEngineFactory<?> factory = builder.setProject(openlRoot).setExecutionMode(false).build();
+            SimpleProjectEngineFactoryBuilder<?> builder = new SimpleProjectEngineFactoryBuilder<Object>();
+            SimpleProjectEngineFactory<?> factory = builder.setProject(openlRoot).setExecutionMode(false).build();
 
-        return factory.getCompiledOpenClass();
+            openLRules = factory.getCompiledOpenClass();
+        } catch (Exception e) {
+            throw new MojoFailureException("Failed to compile OpenL project", e);
+        }
+        info(SEPARATOR);
+        info(getHeader());
+        info(SEPARATOR);
+        String message = null;
+        try {
+            message = execute(openLRules);
+        } catch (Exception e) {
+            getLog().error(e);
+            throw new MojoExecutionException("Execution failure.", e);
+        } finally {
+            info(SEPARATOR);
+        }
+        if (message != null) {
+            throw new MojoExecutionException(message);
+        }
+    }
+
+    abstract String execute(CompiledOpenClass openLRules) throws Exception;
+
+
+    boolean isDisabled() {
+        return false;
+    }
+
+    String getHeader() {
+        return getClass().getSimpleName();
+    }
+
+    void info(String message) {
+        if (getLog().isInfoEnabled()) {
+            getLog().info(message);
+        }
+    }
+
+    void error(Exception ex) {
+        if (getLog().isErrorEnabled()) {
+            getLog().error(ex);
+        }
     }
 }
