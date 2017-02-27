35ffbd2918272a19c08a50438ecf843473b0b05a
Yury Molchan <ymolchan@eisgroup.com>
2017-02-27 13:45:52 +0300

    Update to POI v.3.15
    Backed out changeset: 46e63b583f52

--- DEV/org.openl.rules/src/org/openl/rules/lang/xls/load/WorkbookLoadUtils.java
+++ DEV/org.openl.rules/src/org/openl/rules/lang/xls/load/WorkbookLoadUtils.java
@@ -1,6 +1,7 @@
 package org.openl.rules.lang.xls.load;
 
 import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.apache.poi.openxml4j.util.ZipSecureFile;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.openl.exception.OpenLRuntimeException;
@@ -21,6 +22,11 @@ final class WorkbookLoadUtils {
 
         InputStream is = null;
         Workbook workbook;
+
+        // ZIP bomb detection tuning. Don't disable it by setting it in 0.
+        // https://bz.apache.org/bugzilla/show_bug.cgi?id=58499
+        // 0.001 is when 1MByte expands to 1 GByte
+        ZipSecureFile.setMinInflateRatio(0.001);
 
         try {
             is = fileSource.getByteStream();
