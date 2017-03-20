e1605c941f7c79c3366ebfc51e9f9e1ea979c5d3
Nail Samatov <nsamatov@eisgroup.com>
2017-03-20 20:32:19 +0300

    EPBDS-6490 Extract wider type finding algorithm to separate class

---
+++ DEV/org.openl.j/src/org/openl/binding/impl/CastToWiderType.java
@@ -0,0 +1,133 @@
+package org.openl.binding.impl;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.openl.binding.IBindingContext;
+import org.openl.binding.impl.cast.IOpenCast;
+import org.openl.types.IOpenClass;
+import org.openl.types.NullOpenClass;
+import org.openl.types.java.JavaOpenClass;
+
+/**
+ * Contains information needed to cast two types to wider type
+ */
+public final class CastToWiderType {
+    private final IOpenClass widerType;
+    private final IOpenCast cast1;
+    private final IOpenCast cast2;
+
+    private CastToWiderType(IOpenClass widerType, IOpenCast cast1, IOpenCast cast2) {
+        this.widerType = widerType;
+        this.cast1 = cast1;
+        this.cast2 = cast2;
+    }
+
+
+    public IOpenClass getWiderType() {
+        return widerType;
+    }
+
+    /**
+     * The cast from type1 to {@link #widerType}
+     *
+     * @return Type cast. Can be null if cast isn't needed
+     */
+    public IOpenCast getCast1() {
+        return cast1;
+    }
+
+    /**
+     * The cast from type2 to {@link #widerType}
+     *
+     * @return Type cast. Can be null if cast isn't needed
+     */
+    public IOpenCast getCast2() {
+        return cast2;
+    }
+
+    /**
+     * Find wider type for types type1 and type 2 and needed casts for them.
+     *
+     * @param bindingContext binding context
+     * @param type1 first type
+     * @param type2 second type
+     * @return cast information
+     */
+    public static CastToWiderType create(IBindingContext bindingContext, IOpenClass type1, IOpenClass type2) {
+        if (NullOpenClass.the.equals(type1)) {
+            return new CastToWiderType(type2, null, null);
+        } else {
+            IOpenCast cast1To2 = bindingContext.getCast(type1, type2);
+            IOpenCast cast2To1 = bindingContext.getCast(type2, type1);
+
+            if (cast1To2 == null && cast2To1 == null) {
+                // Find parent class for cast both nodes
+                IOpenClass parentClass = findParentClass(type1, type2);
+                if (parentClass != null) {
+                    IOpenCast castToParent1 = bindingContext.getCast(type1, parentClass);
+                    IOpenCast castToParent2 = bindingContext.getCast(type2, parentClass);
+                    return new CastToWiderType(parentClass, castToParent1, castToParent2);
+                }
+            } else {
+                if ((cast1To2 == null || !cast1To2.isImplicit()) && cast2To1 != null && cast2To1.isImplicit()) {
+                    return new CastToWiderType(type1, null, cast2To1);
+                } else {
+                    if ((cast2To1 == null || !cast2To1.isImplicit()) && cast1To2 != null && cast1To2.isImplicit()) {
+                        return new CastToWiderType(type2, cast1To2, null);
+                    } else {
+                        if (cast1To2 != null && cast2To1 != null && cast1To2.isImplicit() && cast2To1.isImplicit()) {
+                            if (cast1To2.getDistance(type1, type2) < cast2To1.getDistance(type2, type1)) {
+                                return new CastToWiderType(type2, cast1To2, null);
+                            } else {
+                                return new CastToWiderType(type1, null, cast2To1);
+                            }
+                        }
+                    }
+                }
+            }
+
+            return new CastToWiderType(type1, null, null);
+        }
+
+    }
+
+    private static IOpenClass findParentClass(IOpenClass child1Type, IOpenClass child2Type) {
+        Set<IOpenClass> superClasses = new HashSet<IOpenClass>();
+        IOpenClass openClass = child1Type;
+        while (!openClass.equals(JavaOpenClass.OBJECT)) {
+            Iterable<IOpenClass> itr = openClass.superClasses();
+            boolean f = false;
+            for (IOpenClass superClass : itr) {
+                if (!superClass.getInstanceClass().isInterface()) {
+                    superClasses.add(superClass);
+                    openClass = superClass;
+                    f = true;
+                    break;
+                }
+            }
+            if (!f) {
+                break;
+            }
+        }
+        openClass = child2Type;
+        while (!openClass.equals(JavaOpenClass.OBJECT)) {
+            Iterable<IOpenClass> itr = openClass.superClasses();
+            boolean f = false;
+            for (IOpenClass superClass : itr) {
+                if (!superClass.getInstanceClass().isInterface()) {
+                    if (superClasses.contains(superClass)) {
+                        return superClass;
+                    }
+                    openClass = superClass;
+                    f = true;
+                    break;
+                }
+            }
+            if (!f) {
+                break;
+            }
+        }
+        return null;
+    }
+}
--- DEV/org.openl.j/src/org/openl/binding/impl/QMarkNodeBinder.java
+++ DEV/org.openl.j/src/org/openl/binding/impl/QMarkNodeBinder.java
@@ -4,16 +4,10 @@
 
 package org.openl.binding.impl;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import org.openl.binding.IBindingContext;
 import org.openl.binding.IBoundNode;
-import org.openl.binding.impl.cast.IOpenCast;
 import org.openl.syntax.ISyntaxNode;
 import org.openl.types.IOpenClass;
-import org.openl.types.NullOpenClass;
-import org.openl.types.java.JavaOpenClass;
 
 /**
  * @author snshor
@@ -38,85 +32,18 @@ public class QMarkNodeBinder extends ANodeBinder {
         IOpenClass type1 = children[1].getType();
         IOpenClass type2 = children[2].getType();
 
-        IOpenClass type = type1;
-        if (NullOpenClass.the.equals(type1)) {
-            type = type2;
-        } else {
-            IOpenCast cast1To2 = bindingContext.getCast(type1, type2);
-            IOpenCast cast2To1 = bindingContext.getCast(type2, type1);
+        CastToWiderType castToWiderType = CastToWiderType.create(bindingContext, type1, type2);
 
-            if (cast1To2 == null && cast2To1 == null) {
-                // Find parent class for cast both nodes
-                IOpenClass parentClass = findParentClass(type1, type2);
-                if (parentClass != null) {
-                    type = parentClass;
-                    IOpenCast castToParent1 = bindingContext.getCast(type1, parentClass);
-                    children[1] = new CastNode(null, children[1], castToParent1, parentClass);
-                    IOpenCast castToParent2 = bindingContext.getCast(type2, parentClass);
-                    children[2] = new CastNode(null, children[2], castToParent2, parentClass);
-                }
-            } else {
-                if ((cast1To2 == null || !cast1To2.isImplicit()) && cast2To1 != null && cast2To1.isImplicit()) {
-                    children[2] = new CastNode(null, children[2], cast2To1, type1);
-                } else {
-                    if ((cast2To1 == null || !cast2To1.isImplicit()) && cast1To2 != null && cast1To2.isImplicit()) {
-                        children[1] = new CastNode(null, children[1], cast1To2, type2);
-                        type = type2;
-                    } else {
-                        if (cast1To2 != null && cast2To1 != null && cast1To2.isImplicit() && cast2To1.isImplicit()) {
-                            if (cast1To2.getDistance(type1, type2) < cast2To1
-                                .getDistance(type2, type1)) {
-                                children[1] = new CastNode(null, children[1], cast1To2, type2);
-                                type = type2;
-                            } else {
-                                children[2] = new CastNode(null, children[2], cast2To1, type1);
-                            }
-                        }
-                    }
-                }
-            }
+        IOpenClass type = castToWiderType.getWiderType();
+        if (castToWiderType.getCast1() != null) {
+            children[1] = new CastNode(null, children[1], castToWiderType.getCast1(), type);
+        }
+        if (castToWiderType.getCast2() != null) {
+            children[2] = new CastNode(null, children[2], castToWiderType.getCast2(), type);
         }
 
         return new QMarkNode(node, children, type);
     }
 
-    private IOpenClass findParentClass(IOpenClass child1Type, IOpenClass child2Type) {
-        Set<IOpenClass> superClasses = new HashSet<IOpenClass>();
-        IOpenClass openClass = child1Type;
-        while (!openClass.equals(JavaOpenClass.OBJECT)) {
-            Iterable<IOpenClass> itr = openClass.superClasses();
-            boolean f = false;
-            for (IOpenClass superClass : itr) {
-                if (!superClass.getInstanceClass().isInterface()) {
-                    superClasses.add(superClass);
-                    openClass = superClass;
-                    f = true;
-                    break;
-                }
-            }
-            if (!f){
-                break;
-            }
-        }
-        openClass = child2Type;
-        while (!openClass.equals(JavaOpenClass.OBJECT)) {
-            Iterable<IOpenClass> itr = openClass.superClasses();
-            boolean f = false;
-            for (IOpenClass superClass : itr) {
-                if (!superClass.getInstanceClass().isInterface()) {
-                    if (superClasses.contains(superClass)) {
-                        return superClass;
-                    }
-                    openClass = superClass;
-                    f = true;
-                    break;
-                }
-            }
-            if (!f){
-                break;
-            }
-        }
-        return null;
-    }
 
 }
