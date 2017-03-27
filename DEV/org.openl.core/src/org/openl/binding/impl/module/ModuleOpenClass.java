b731ea3f533347439564023a32a638eb4536f82c
Yury Molchan <ymolchan@eisgroup.com>
2017-03-28 00:07:17 +0300

    EPBDS-6543 No needs to wrap the map of the datatypes, because dependencyTypes is always empty
    --HG--
    branch : 5.19.x

--- DEV/org.openl.core/src/org/openl/binding/impl/module/ModuleOpenClass.java
+++ DEV/org.openl.core/src/org/openl/binding/impl/module/ModuleOpenClass.java
@@ -47,8 +47,6 @@ public class ModuleOpenClass extends ComponentOpenClass {
      */
     private Map<String, IOpenClass> internalTypes = new HashMap<String, IOpenClass>();
     
-    private Map<String, IOpenClass> dependencyTypes = new HashMap<String, IOpenClass>();
-    
     /**
      * Set of dependencies for current module.
      * 
@@ -227,9 +225,7 @@ public class ModuleOpenClass extends ComponentOpenClass {
      */
     @Override
     public Map<String, IOpenClass> getTypes() {
-        Map<String, IOpenClass> currentModuleDatatypes = new HashMap<String, IOpenClass>(internalTypes);
-        currentModuleDatatypes.putAll(dependencyTypes);
-        return currentModuleDatatypes;
+        return internalTypes;
     }
 
     /**
