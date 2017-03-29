357d366cacfff251bb7f62a7ad8324580af6d5fd
Nail Samatov <nsamatov@eisgroup.com>
2017-03-29 16:01:29 +0300

    EPBDS-6100 Don't throw error for correct expression in spreadsheet if File System is case insensitive. Add unit tests.

--- DEV/org.openl.core/src/org/openl/types/java/JavaImportTypeLibrary.java
+++ DEV/org.openl.core/src/org/openl/types/java/JavaImportTypeLibrary.java
@@ -72,10 +72,21 @@ public class JavaImportTypeLibrary implements ITypeLibrary {
             } catch (ClassNotFoundException ignored) {
                 // Type isn't found in the package. Search in another.
             } catch (NoClassDefFoundError e) {
-                // Type is found but can't be loaded because of absent dependent class.
-                String noClassMessage = e.getCause() != null ? e.getCause().getMessage() : e.getMessage();
-                String message = String.format("Type '%s' can't be loaded because of absent type '%s'.", name, noClassMessage);
-                throw RuntimeExceptionWrapper.wrap(message, e);
+                if (e.getCause() instanceof ClassNotFoundException) {
+                    // Type is found but can't be loaded because of absent dependent class.
+                    String noClassMessage = e.getCause().getMessage();
+                    String message = String.format("Type '%s' can't be loaded because of absent type '%s'.",
+                            name,
+                            noClassMessage);
+                    throw RuntimeExceptionWrapper.wrap(message, e);
+                }
+                // NoClassDefFoundError can also be thrown in these cases:
+                // 1. Class was compiled in one package but it was moved manually to another package in file system without changing package in class binary
+                // 2. Class was compiled with one name but was manually renamed in file system to another name
+                // 3. If File System is case insensitive and we are trying to find the class org.work.address but exists the class org.work.Address
+                // In all these cases NoClassDefFoundError will be thrown instead of ClassNotFoundException and message will be like:
+                //   java.lang.NoClassDefFoundError: org/work/address (wrong name: org/work/Address)
+                // We just skip such classes and continue searching them in another packages.
             } catch (UnsupportedClassVersionError e) {
                 // Type is found but it's compiled using newer version of JDK
                 String message = String.format("Can't load the class \"%s\" compiled using newer version of JDK than current JRE (%s)", name, System.getProperty("java.version"));
