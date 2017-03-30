a2986f27a1bdf32e523c8e200b29b337ecc56973
Marat Kamalov <mkamalov@eisgroup.com>
2017-03-30 09:40:32 +0300

    EPBDS-6543 Cleanup needless lines

--- WSFrontend/org.openl.rules.ruleservice.ws/src/org/openl/rules/ruleservice/servlet/WSServlet.java
+++ WSFrontend/org.openl.rules.ruleservice.ws/src/org/openl/rules/ruleservice/servlet/WSServlet.java
@@ -1,15 +1,10 @@
 package org.openl.rules.ruleservice.servlet;
 
-import java.lang.reflect.Proxy;
-
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.cxf.configuration.Configurer;
-import org.apache.cxf.transport.http.DestinationRegistry;
-import org.apache.cxf.transport.http.HTTPTransportFactory;
 import org.apache.cxf.transport.servlet.CXFServlet;
 
 /**
@@ -24,7 +19,6 @@ public class WSServlet extends CXFServlet {
         super.init();
         ServletConfig servletConfig = getServletConfig();
         loadBus(servletConfig);
-        configureDestinationRegistry(servletConfig.getInitParameter("uriEncoding"));
     }
     
     @Override
@@ -37,49 +31,4 @@ public class WSServlet extends CXFServlet {
         super.redirect(request, response, pathInfo);
     }
 
-    /**
-     * Currently CXF doesn't support UTF-8 uri encoding, only ISO-8859-1.
-     * This method adds decoding from any encoding functionality to DestinationRegistry.
-     */
-    private void configureDestinationRegistry(final String uriEncoding) {
-        if (uriEncoding == null) {
-            // Proceed with default implementation.
-            return;
-        }
-
-        final Configurer configurer = getBus().getExtension(Configurer.class);
-        getBus().setExtension(new Configurer() {
-            @Override
-            public void configureBean(Object beanInstance) {
-                configurer.configureBean(beanInstance);
-                configureHttpTransportFactory(beanInstance);
-            }
-
-            @Override
-            public void configureBean(String name, Object beanInstance) {
-                configurer.configureBean(name, beanInstance);
-                configureHttpTransportFactory(beanInstance);
-            }
-
-            private void configureHttpTransportFactory(Object beanInstance) {
-                if (beanInstance instanceof HTTPTransportFactory) {
-                    HTTPTransportFactory factory = (HTTPTransportFactory) beanInstance;
-                    factory.setRegistry(createDestinationRegistry(factory.getRegistry()));
-                }
-            }
-
-            private DestinationRegistry createDestinationRegistry(DestinationRegistry oldRegistry) {
-                if (Proxy.isProxyClass(oldRegistry.getClass()) && Proxy.getInvocationHandler(oldRegistry) instanceof DestinationRegistryInvocationHandler) {
-                    return oldRegistry;
-                }
-
-                Class<? extends DestinationRegistry> type = oldRegistry.getClass();
-                return (DestinationRegistry) Proxy.newProxyInstance(
-                        type.getClassLoader(),
-                        type.getInterfaces(),
-                        new DestinationRegistryInvocationHandler(oldRegistry, uriEncoding)
-                );
-            }
-        }, Configurer.class);
-    }
 }
