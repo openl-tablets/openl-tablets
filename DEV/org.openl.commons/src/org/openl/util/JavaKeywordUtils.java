f0decb369ae14a4d48ecdb1e7f07544e84ed18b4
Marat Kamalov <mkamalov@eisgroup.com>
2017-03-31 20:16:09 +0300

    EPBDS-6555 Add support for java keywords parameters in rules (Plugin + WS)
    --HG--
    branch : 5.19.x

---
+++ DEV/org.openl.commons/src/org/openl/util/JavaKeywordUtils.java
@@ -0,0 +1,65 @@
+package org.openl.util;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class JavaKeywordUtils {
+
+    static private final Set<String> JAVA_KEYWORDS = new HashSet<String>();
+
+    static {
+        JAVA_KEYWORDS.add("abstract");
+        JAVA_KEYWORDS.add("continue");
+        JAVA_KEYWORDS.add("for");
+        JAVA_KEYWORDS.add("new");
+        JAVA_KEYWORDS.add("switch");
+        JAVA_KEYWORDS.add("assert");
+        JAVA_KEYWORDS.add("default");
+        JAVA_KEYWORDS.add("goto");
+        JAVA_KEYWORDS.add("package");
+        JAVA_KEYWORDS.add("synchronized");
+        JAVA_KEYWORDS.add("boolean");
+        JAVA_KEYWORDS.add("do");
+        JAVA_KEYWORDS.add("if");
+        JAVA_KEYWORDS.add("private");
+        JAVA_KEYWORDS.add("this");
+        JAVA_KEYWORDS.add("break");
+        JAVA_KEYWORDS.add("double");
+        JAVA_KEYWORDS.add("implements");
+        JAVA_KEYWORDS.add("protected");
+        JAVA_KEYWORDS.add("throw");
+        JAVA_KEYWORDS.add("byte");
+        JAVA_KEYWORDS.add("else");
+        JAVA_KEYWORDS.add("import");
+        JAVA_KEYWORDS.add("public");
+        JAVA_KEYWORDS.add("throws");
+        JAVA_KEYWORDS.add("case");
+        JAVA_KEYWORDS.add("enum");
+        JAVA_KEYWORDS.add("instanceof");
+        JAVA_KEYWORDS.add("return");
+        JAVA_KEYWORDS.add("transient");
+        JAVA_KEYWORDS.add("catch");
+        JAVA_KEYWORDS.add("extends");
+        JAVA_KEYWORDS.add("int");
+        JAVA_KEYWORDS.add("short");
+        JAVA_KEYWORDS.add("try");
+        JAVA_KEYWORDS.add("char");
+        JAVA_KEYWORDS.add("final");
+        JAVA_KEYWORDS.add("interface");
+        JAVA_KEYWORDS.add("static");
+        JAVA_KEYWORDS.add("void");
+        JAVA_KEYWORDS.add("class");
+        JAVA_KEYWORDS.add("finally");
+        JAVA_KEYWORDS.add("long");
+        JAVA_KEYWORDS.add("strictfp");
+        JAVA_KEYWORDS.add("volatile");
+        JAVA_KEYWORDS.add("const");
+        JAVA_KEYWORDS.add("float");
+        JAVA_KEYWORDS.add("native");
+        JAVA_KEYWORDS.add("while");
+    }
+    
+    public static boolean isJavaKeyword(String s){
+        return JAVA_KEYWORDS.contains(s);
+    }
+}
