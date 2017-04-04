b014a78f57668e49294aff6c14841fe65f767e07
Yury Molchan <ymolchan@eisgroup.com>
2017-04-04 14:48:07 +0300

    EPBDS-6544 Use OpenL types directly

--- DEV/org.openl.rules/test/org/openl/rules/helpers/DoubleRangeRulesParsingTest.java
+++ DEV/org.openl.rules/test/org/openl/rules/helpers/DoubleRangeRulesParsingTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import org.openl.rules.BaseOpenlBuilderHelper;
 import org.openl.types.IOpenClass;
-import org.openl.types.java.OpenClassHelper;
+import org.openl.types.java.JavaOpenClass;
 
 public class DoubleRangeRulesParsingTest extends BaseOpenlBuilderHelper {
     
@@ -27,8 +27,7 @@ public class DoubleRangeRulesParsingTest extends BaseOpenlBuilderHelper {
     
     private Object invokeMethod(String param1, double param2) {
         return invokeMethod("testDoubleNegativeRange", 
-            new IOpenClass[]{OpenClassHelper.getOpenClass(getCompiledOpenClass().getOpenClass(), String.class), 
-                OpenClassHelper.getOpenClass(getCompiledOpenClass().getOpenClass(), double.class)}, 
+            new IOpenClass[]{JavaOpenClass.STRING, JavaOpenClass.DOUBLE},
                 new Object[]{param1, param2});
     }
     
@@ -38,8 +37,7 @@ public class DoubleRangeRulesParsingTest extends BaseOpenlBuilderHelper {
     @Test    
     public void testLongRange() {
         Object result = invokeMethod("testLongRange", 
-            new IOpenClass[]{OpenClassHelper.getOpenClass(getCompiledOpenClass().getOpenClass(), boolean.class), 
-                OpenClassHelper.getOpenClass(getCompiledOpenClass().getOpenClass(), double.class)}, 
+            new IOpenClass[]{JavaOpenClass.BOOLEAN, JavaOpenClass.DOUBLE},
                 new Object[]{true, 210});
         assertEquals("rule1", result);
     }
@@ -50,8 +48,7 @@ public class DoubleRangeRulesParsingTest extends BaseOpenlBuilderHelper {
     @Test    
     public void testIntRange() {
         Object result = invokeMethod("testIntegerRange", 
-            new IOpenClass[]{OpenClassHelper.getOpenClass(getCompiledOpenClass().getOpenClass(), boolean.class), 
-                OpenClassHelper.getOpenClass(getCompiledOpenClass().getOpenClass(), double.class)}, 
+            new IOpenClass[]{JavaOpenClass.BOOLEAN, JavaOpenClass.DOUBLE},
                 new Object[]{true, 105});
         assertEquals("rule2", result);
     }
@@ -62,8 +59,7 @@ public class DoubleRangeRulesParsingTest extends BaseOpenlBuilderHelper {
     @Test    
     public void testIntRange1() {
         Object result = invokeMethod("testIntegerRange1", 
-            new IOpenClass[]{OpenClassHelper.getOpenClass(getCompiledOpenClass().getOpenClass(), boolean.class), 
-                OpenClassHelper.getOpenClass(getCompiledOpenClass().getOpenClass(), double.class)}, 
+            new IOpenClass[]{JavaOpenClass.BOOLEAN, JavaOpenClass.DOUBLE},
                 new Object[]{true, 105});
         assertEquals("rule2", result);
     }
@@ -74,8 +70,7 @@ public class DoubleRangeRulesParsingTest extends BaseOpenlBuilderHelper {
     @Test    
     public void testByteRange() {
         Object result = invokeMethod("testByteRange", 
-            new IOpenClass[]{OpenClassHelper.getOpenClass(getCompiledOpenClass().getOpenClass(), boolean.class), 
-                OpenClassHelper.getOpenClass(getCompiledOpenClass().getOpenClass(), double.class)}, 
+            new IOpenClass[]{JavaOpenClass.BOOLEAN, JavaOpenClass.DOUBLE},
                 new Object[]{true, 99});
         assertEquals("rule2", result);
     }
@@ -86,8 +81,7 @@ public class DoubleRangeRulesParsingTest extends BaseOpenlBuilderHelper {
     @Test    
     public void testShortRange() {
         Object result = invokeMethod("testShortRange", 
-            new IOpenClass[]{OpenClassHelper.getOpenClass(getCompiledOpenClass().getOpenClass(), boolean.class), 
-                OpenClassHelper.getOpenClass(getCompiledOpenClass().getOpenClass(), double.class)}, 
+            new IOpenClass[]{JavaOpenClass.BOOLEAN, JavaOpenClass.DOUBLE},
                 new Object[]{false, -50});
         assertEquals("rule3", result);
     }
@@ -98,12 +92,8 @@ public class DoubleRangeRulesParsingTest extends BaseOpenlBuilderHelper {
     @Test    
     public void testFloatRange() {
         Object result = invokeMethod("testFloatRange", 
-            new IOpenClass[]{OpenClassHelper.getOpenClass(getCompiledOpenClass().getOpenClass(), boolean.class), 
-                OpenClassHelper.getOpenClass(getCompiledOpenClass().getOpenClass(), double.class)}, 
+            new IOpenClass[]{JavaOpenClass.BOOLEAN, JavaOpenClass.DOUBLE},
                 new Object[]{true, 20.56});
         assertEquals("rule1", result);
     }
-    
-    
-
 }
