34b291e03bf49e55d273d03a0ed8d9e16d671bee
Marat Kamalov <mkamalov@eisgroup.com>
2017-05-04 22:20:17 +0300

    EPBDS-6585 Code format.

--- WSFrontend/org.openl.rules.ruleservice/src/org/openl/rules/ruleservice/core/RuleServiceImpl.java
+++ WSFrontend/org.openl.rules.ruleservice/src/org/openl/rules/ruleservice/core/RuleServiceImpl.java
@@ -117,28 +117,27 @@ public class RuleServiceImpl implements RuleService {
             throw new RuleServiceDeployException(
                 "The service with name '" + serviceDescription.getName() + "' has already been deployed!");
         }
-        try {
-            OpenLService newService = ruleServiceInstantiationFactory.createService(serviceDescription);
-            ServiceDescription sd = mapping.get(serviceDescription.getName());
-            if (sd != null) {
-                throw new IllegalStateException("Illegal State!!");
-            }
-            
+		try {
+			OpenLService newService = ruleServiceInstantiationFactory.createService(serviceDescription);
+			ServiceDescription sd = mapping.get(serviceDescription.getName());
+			if (sd != null) {
+				throw new IllegalStateException("Illegal State!!");
+			}
+
 			if (newService.getServiceClass().getMethods().length == 0) { // Skip
-					   		  									    // deploy empty services
+				// deploy empty services
 				if (log.isWarnEnabled()) {
 					log.warn("Service '{}' doesn't have any methods. It has been skiped.", newService.getName());
 				}
 				return;
 			}
-            
-            ruleServicePublisher.deploy(newService);
-            mapping.put(serviceDescription.getName(), serviceDescription);
-            log.info("Service '{}' was deployed succesfully.",
-                serviceDescription.getName());
-        } catch (RuleServiceInstantiationException e) {
-            throw new RuleServiceDeployException("Failed on deploy a service.", e);
-        }
+
+			ruleServicePublisher.deploy(newService);
+			mapping.put(serviceDescription.getName(), serviceDescription);
+			log.info("Service '{}' was deployed succesfully.", serviceDescription.getName());
+		} catch (RuleServiceInstantiationException e) {
+			throw new RuleServiceDeployException("Failed on deploy a service.", e);
+		}
     }
 
     public RuleServicePublisher getRuleServicePublisher() {
