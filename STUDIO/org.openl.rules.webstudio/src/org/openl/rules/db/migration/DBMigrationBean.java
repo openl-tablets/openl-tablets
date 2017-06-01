8c316080a31e123f2c1b72b361cc50f92aebcb3f
Yury Molchan <ymolchan@eisgroup.com>
2017-06-01 20:15:13 +0300

    EPBDS-6674 Use properties files instead of Hiberante dialects

--- STUDIO/org.openl.rules.webstudio/src/org/openl/rules/db/migration/DBMigrationBean.java
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/db/migration/DBMigrationBean.java
@@ -1,60 +1,60 @@
 package org.openl.rules.db.migration;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
-import java.sql.Types;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
+import java.util.TreeMap;
 
 import javax.sql.DataSource;
 
 import org.flywaydb.core.Flyway;
-import org.hibernate.dialect.Dialect;
-import org.hibernate.engine.jdbc.dialect.internal.StandardDialectResolver;
-import org.hibernate.engine.jdbc.dialect.spi.DatabaseMetaDataDialectResolutionInfoAdapter;
+import org.openl.util.IOUtils;
 import org.openl.util.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DBMigrationBean {
+    private final Logger log = LoggerFactory.getLogger(DBMigrationBean.class);
+
     private String additionalMigrationPaths;
+
     private DataSource dataSource;
 
     public void init() throws Exception {
         Connection connection = dataSource.getConnection();
-        Dialect dialect;
         String databaseCode;
         try {
             DatabaseMetaData metaData = connection.getMetaData();
             databaseCode = metaData.getDatabaseProductName().toLowerCase().replace(" ", "_");
-            DatabaseMetaDataDialectResolutionInfoAdapter dialectResolutionInfo = new DatabaseMetaDataDialectResolutionInfoAdapter(
-                connection.getMetaData());
-            dialect = new StandardDialectResolver().resolveDialect(dialectResolutionInfo);
         } finally {
             connection.close();
         }
-        // Set path to V1_Base_version.sql script
-        Flyway flyway = new Flyway();
-        flyway.setDataSource(dataSource);
-
-        Map<String, String> placeholders = new HashMap<String, String>();
-        placeholders.put("schemaPrefix", "");
-        placeholders.put("identity_column", getIdentityColumn(dialect));
-        placeholders.put("create_hibernate_sequence", "");
-        placeholders.put("bigint", dialect.getTypeName(Types.BIGINT));
-        placeholders.put("longtext", dialect.getTypeName(Types.VARCHAR, 1000, 0, 0));
-        flyway.setPlaceholders(placeholders);
 
         ArrayList<String> locations = new ArrayList<String>();
-        locations.add("db/migration/common");
-        locations.add("db/migration/" + databaseCode);
+        locations.add("/db/migration/common");
+        locations.add("/db/migration/" + databaseCode);
+
+        TreeMap<String, String> placeholders = new TreeMap<String, String>();
+        for (String location : locations) {
+            fillQueries(placeholders, location + "/placeholders.properties");
+        }
+
         // Additional migrations
         if (StringUtils.isNotBlank(additionalMigrationPaths)) {
             String[] split = StringUtils.split(additionalMigrationPaths, ',');
             locations.addAll(Arrays.asList(split));
         }
-        flyway.setLocations(locations.toArray(new String[0]));
 
+        Flyway flyway = new Flyway();
+        flyway.setDataSource(dataSource);
+        flyway.setPlaceholders(placeholders);
+        flyway.setLocations(locations.toArray(new String[0]));
         flyway.setBaselineVersionAsString("0");
         flyway.setBaselineOnMigrate(true);
         flyway.migrate();
@@ -68,12 +68,24 @@ public class DBMigrationBean {
         this.dataSource = dataSource;
     }
 
-    private String getIdentityColumn(Dialect dialect) {
-        if (dialect.supportsIdentityColumns()) {
-            String dataType = dialect.hasDataTypeInIdentityColumn() ? dialect.getTypeName(Types.BIGINT) : "";
-            return dataType + " " + dialect.getIdentityColumnString(Types.BIGINT);
-        } else {
-            return dialect.getTypeName(Types.BIGINT) + " not null";
+    private void fillQueries(Map<String, String> queries, String propertiesFileName) throws IOException {
+        URL resource = getClass().getResource(propertiesFileName);
+        if (resource == null) {
+            log.info("File '{}' is not found.", propertiesFileName);
+            return;
+        }
+        log.info("Load properties from '{}'.", resource);
+        InputStream is = resource.openStream();
+        try {
+            Properties properties = new Properties();
+            properties.load(is);
+            for (String key : properties.stringPropertyNames()) {
+                queries.put(key, properties.getProperty(key));
+            }
+            is.close();
+        } finally {
+            IOUtils.closeQuietly(is);
         }
     }
+
 }
