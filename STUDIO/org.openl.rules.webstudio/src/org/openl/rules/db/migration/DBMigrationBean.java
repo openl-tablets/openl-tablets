8b8ab2b78986297b7647d4081cc5f63ee04cbfd6
Yury Molchan <ymolchan@eisgroup.com>
2017-06-02 14:25:37 +0300

    EPBDS-6674 Break old migration.
    It will be fixed in the next release.

--- STUDIO/org.openl.rules.webstudio/src/org/openl/rules/db/migration/DBMigrationBean.java
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/db/migration/DBMigrationBean.java
@@ -5,8 +5,8 @@ import java.io.InputStream;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Map;
 import java.util.Properties;
 import java.util.TreeMap;
@@ -22,46 +22,41 @@ import org.slf4j.LoggerFactory;
 public class DBMigrationBean {
     private final Logger log = LoggerFactory.getLogger(DBMigrationBean.class);
 
-    private String additionalMigrationPaths;
-
     private DataSource dataSource;
 
     public void init() throws Exception {
         Connection connection = dataSource.getConnection();
         String databaseCode;
+        boolean oldMigrationExists = false;
         try {
             DatabaseMetaData metaData = connection.getMetaData();
             databaseCode = metaData.getDatabaseProductName().toLowerCase().replace(" ", "_");
+            String tableName = "schema_version";
+            String repoTable = metaData.storesUpperCaseIdentifiers() ? tableName.toUpperCase() : tableName;
+
+            ResultSet rs = null;
+            try {
+                if ("oracle".equals(databaseCode)) {
+                    rs = metaData.getTables(null, metaData.getUserName(), repoTable, new String[] { "TABLE" });
+                } else {
+                    rs = metaData.getTables(null, null, repoTable, new String[] { "TABLE" });
+                }
+                oldMigrationExists = rs.next();
+            } finally {
+                if (rs != null) {
+                    try {
+                        rs.close();
+                    } catch (SQLException e) {
+                        log.warn("Unexpected sql failure", e);
+                    }
+                }
+            }
         } finally {
             connection.close();
         }
-
-        ArrayList<String> locations = new ArrayList<String>();
-        locations.add("/db/migration/common");
-        locations.add("/db/migration/" + databaseCode);
-
-        TreeMap<String, String> placeholders = new TreeMap<String, String>();
-        for (String location : locations) {
-            fillQueries(placeholders, location + "/placeholders.properties");
+        if (oldMigrationExists) {
+            throw new IllegalStateException("Incompatible OpenL WebStudio version");
         }
-
-        // Additional migrations
-        if (StringUtils.isNotBlank(additionalMigrationPaths)) {
-            String[] split = StringUtils.split(additionalMigrationPaths, ',');
-            locations.addAll(Arrays.asList(split));
-        }
-
-        Flyway flyway = new Flyway();
-        flyway.setDataSource(dataSource);
-        flyway.setPlaceholders(placeholders);
-        flyway.setLocations(locations.toArray(new String[0]));
-        flyway.setBaselineVersionAsString("0");
-        flyway.setBaselineOnMigrate(true);
-        flyway.migrate();
-    }
-
-    public void setAdditionalMigrationPaths(String additionalMigrationPaths) {
-        this.additionalMigrationPaths = additionalMigrationPaths;
     }
 
     public void setDataSource(DataSource dataSource) {
