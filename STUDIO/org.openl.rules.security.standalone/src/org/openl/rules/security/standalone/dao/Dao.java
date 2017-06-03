812c833a72c804e8e3cfb1292ddf340a77c0ace4
Yury Molchan <ymolchan@eisgroup.com>
2017-06-03 15:00:04 +0300

    EPBDS-6680 Delete unused methods

--- STUDIO/org.openl.rules.security.standalone/src/org/openl/rules/security/standalone/dao/Dao.java
+++ STUDIO/org.openl.rules.security.standalone/src/org/openl/rules/security/standalone/dao/Dao.java
@@ -10,43 +10,13 @@ import java.util.List;
  */
 public interface Dao<T> {
     /**
-     * Checks whether given object can be deleted. i.e. has no non-deletable
-     * relations in DB.
-     *
-     * @param obj object to delete
-     *
-     * @return <code>true</code>
-     */
-    boolean canBeDeleted(T obj);
-
-    /**
      * Delete given object.
      *
      * @param obj object to delete.
      */
     void delete(T obj);
 
-    /**
-     * Gets object by primary id.
-     *
-     * @param id primary id
-     *
-     * @return object with given id or <code>null</code> if it can not be
-     *         found.
-     */
-    Object getById(Long id);
-
     List<T> getAll();
-
-    /**
-     * Load object by primary id.
-     *
-     * @param id primary id
-     *
-     * @return object with given id or throws runtime exception if object is not
-     *         found.
-     */
-    Object loadById(Long id);
 
     /**
      * Saves object.
