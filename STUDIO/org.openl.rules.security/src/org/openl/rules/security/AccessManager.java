0c4326db3fda853fad848171918f389931f57f5f
Yury Molchan <ymolchan@eisgroup.com>
2017-06-03 10:17:19 +0300

    EPBDS-6680 Inline private-access-only methods.
    --HG--
    branch : 5.19.x

--- STUDIO/org.openl.rules.security/src/org/openl/rules/security/AccessManager.java
+++ STUDIO/org.openl.rules.security/src/org/openl/rules/security/AccessManager.java
@@ -1,49 +1,23 @@
 package org.openl.rules.security;
 
-import java.util.ArrayList;
 import java.util.Collection;
 
 import org.springframework.security.access.AccessDecisionManager;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.access.ConfigAttribute;
 import org.springframework.security.access.SecurityConfig;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 /**
- * @author Aliaksandr Antonik.
+ * A utility class for privilege checking.
+ *
+ * @author Yury Molchan.
  */
 public final class AccessManager {
 
     private static AccessDecisionManager accessDecisionManager;
-
-    /**
-     * Checks that current security context authentication is authorized to
-     * access a secured object with given security attributes.
-     *
-     * @param config the configuration attributes of a secured object.
-     * @throws AccessDeniedException if current authentication context does not
-     *             hold a required authority
-     */
-    public static void check(Collection<ConfigAttribute> configAttributes) throws AccessDeniedException {
-        accessDecisionManager.decide(SecurityContextHolder.getContext().getAuthentication(), null, configAttributes);
-    }
-
-    /**
-     * Converts <code>privilege</code> to
-     * collection of <code>ConfigAttribute</code> objects.
-     *
-     * @param privilege privilege to check.
-     * @throws AccessDeniedException if current authentication context does not
-     *             hold a required authority
-     */
-    public static void check(String privilege) throws AccessDeniedException {
-        Collection<ConfigAttribute> configAttributes = new ArrayList<ConfigAttribute>();
-        configAttributes.add(new SecurityConfig(privilege));
-
-        check(configAttributes);
-    }
-
 
     /**
      * Inquires whether current user has specified privilege.
@@ -53,19 +27,11 @@ public final class AccessManager {
      *         <code>false</code> otherwise.
      */
     public static boolean isGranted(GrantedAuthority authority) {
-        return isGranted(authority.getAuthority());
-    }
-    
-    /**
-     * Inquires whether current user has specified privilege.
-     *
-     * @param privilege privilege to check.
-     * @return <code>true</code> if current user has the privilege;
-     *         <code>false</code> otherwise.
-     */
-    public static boolean isGranted(String privilege) {
+        String privilege = authority.getAuthority();
         try {
-            check(privilege);
+            Collection<ConfigAttribute> configAttributes = SecurityConfig.createList(privilege);
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            accessDecisionManager.decide(authentication, null, configAttributes);
         } catch (AccessDeniedException e) {
             // not granted
             return false;
@@ -76,26 +42,11 @@ public final class AccessManager {
 
     /**
      * Sets static <code>accessDecisionManager</code> property for further use
-     * in <code>check</code> methods.
+     * in <code>check</code> methods. It is used in the Spring configuration.
      *
-     * @param adm <code>AccessDecisionManager</code> instance.
-     */
-    public static void useAccessDecisionManager(AccessDecisionManager adm) {
-        accessDecisionManager = adm;
-    }
-
-    /**
-     * Sets static <code>accessDecisionManager</code> property for further use
-     * in <code>check</code> methods. <br/> The only difference of this method
-     * from
-     * {@link #useAccessDecisionManager(org.springframework.security.access.AccessDecisionManager)}
-     * that this one is not <i>static</i>.
-     *
-     * @param accessDecisionManager <code>AccessDecisionManager</code>
-     *            instance.
+     * @param accessDecisionManager <code>AccessDecisionManager</code> instance.
      */
     public void setStaticAccessDecisionManager(AccessDecisionManager accessDecisionManager) {
-        useAccessDecisionManager(accessDecisionManager);
+        AccessManager.accessDecisionManager = accessDecisionManager;
     }
-
 }
