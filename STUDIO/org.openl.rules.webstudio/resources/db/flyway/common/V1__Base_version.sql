23914665b4f378e92a19eac0dd3a7ef4c12a7fb6
Yury Molchan <ymolchan@eisgroup.com>
2017-06-05 23:31:49 +0300

    EPBDS-6680 Use CASCADE deletion for groups and users

--- STUDIO/org.openl.rules.webstudio/resources/db/flyway/common/V1__Base_version.sql
+++ STUDIO/org.openl.rules.webstudio/resources/db/flyway/common/V1__Base_version.sql
@@ -18,21 +18,22 @@ CREATE TABLE OpenL_Groups (
 CREATE TABLE OpenL_Group_Authorities (
     groupID ${bigint} not null,
     authority varchar(40) not null,
-    PRIMARY KEY (groupID, authority)
+    PRIMARY KEY (groupID, authority),
+    CONSTRAINT fk_OpenL_Group_Authorities1 FOREIGN KEY (groupID) REFERENCES OpenL_Groups(id) ON DELETE CASCADE
 );
 
 CREATE TABLE OpenL_User2Group (
     loginName varchar(50) not null,
     groupID ${bigint} not null,
     PRIMARY KEY (loginName, groupID),
-    CONSTRAINT fk_OpenL_User2Group1 FOREIGN KEY (loginName) REFERENCES OpenL_Users(loginName),
-    CONSTRAINT fk_OpenL_User2Group2 FOREIGN KEY (groupID) REFERENCES OpenL_Groups(id)
+    CONSTRAINT fk_OpenL_User2Group1 FOREIGN KEY (loginName) REFERENCES OpenL_Users(loginName) ON DELETE CASCADE,
+    CONSTRAINT fk_OpenL_User2Group2 FOREIGN KEY (groupID) REFERENCES OpenL_Groups(id) ON DELETE CASCADE
 );
 
 CREATE TABLE OpenL_Group2Group (
     IncludedGroupID ${bigint} not null,
     groupID ${bigint} not null,
     PRIMARY KEY (IncludedGroupID, groupID),
-    CONSTRAINT fk_OpenL_Group2Group1 FOREIGN KEY (IncludedGroupID) REFERENCES OpenL_Groups(id),
-    CONSTRAINT fk_OpenL_Group2Group2 FOREIGN KEY (groupID) REFERENCES OpenL_Groups(id)
+    CONSTRAINT fk_OpenL_Group2Group1 FOREIGN KEY (IncludedGroupID) REFERENCES OpenL_Groups(id) ON DELETE CASCADE,
+    CONSTRAINT fk_OpenL_Group2Group2 FOREIGN KEY (groupID) REFERENCES OpenL_Groups(id) ON DELETE CASCADE
 );
