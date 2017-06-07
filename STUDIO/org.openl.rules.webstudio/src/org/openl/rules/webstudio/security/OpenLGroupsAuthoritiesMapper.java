04f38961afcb7db83f0682e1e0a41afdb1c05323
Nail Samatov <nsamatov@eisgroup.com>
2017-06-07 18:37:21 +0300

    EPBDS-6680 Refactor security code

--- STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/security/OpenLGroupsAuthoritiesMapper.java
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/security/OpenLGroupsAuthoritiesMapper.java
@@ -1,9 +1,9 @@
 package org.openl.rules.webstudio.security;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
-import org.openl.rules.security.DefaultPrivileges;
-import org.openl.rules.security.Privilege;
 import org.openl.rules.security.SimpleGroup;
 import org.openl.rules.security.standalone.dao.GroupDao;
 import org.openl.rules.security.standalone.persistence.Group;
