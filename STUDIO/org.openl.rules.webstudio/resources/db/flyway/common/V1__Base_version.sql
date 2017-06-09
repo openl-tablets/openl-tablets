543f7e6ea0f260bc849e56c4921f3d72b19d7925
Yury Molchan <ymolchan@eisgroup.com>
2017-06-09 13:20:28 +0300

    EPBDS-6674 Support national symbols for Oracle and MSSQL DB
    --HG--
    branch : 5.19.x

--- STUDIO/org.openl.rules.webstudio/resources/db/flyway/common/V1__Base_version.sql
+++ STUDIO/org.openl.rules.webstudio/resources/db/flyway/common/V1__Base_version.sql
@@ -1,29 +1,28 @@
 CREATE TABLE OpenL_Users (
-    loginName varchar(50) not null,
-    password varchar(128),
-    lastLogin TIMESTAMP,
-    origin varchar(50),
-    firstName varchar(50),
-    surname varchar(50),
+    loginName ${varchar}(50) not null,
+    password ${varchar}(128),
+    origin ${varchar}(50),
+    firstName ${varchar}(50),
+    surname ${varchar}(50),
     PRIMARY KEY (loginName)
 );
 
 CREATE TABLE OpenL_Groups (
     id ${identity},
-    groupName varchar(40) not null unique,
-    description varchar(200),
+    groupName ${varchar}(50) not null unique,
+    description ${varchar}(200),
     PRIMARY KEY (id)
 );
 
 CREATE TABLE OpenL_Group_Authorities (
     groupID ${bigint} not null,
-    authority varchar(40) not null,
+    authority ${varchar}(50) not null,
     PRIMARY KEY (groupID, authority),
     CONSTRAINT fk_OpenL_Group_Authorities1 FOREIGN KEY (groupID) REFERENCES OpenL_Groups(id) ON DELETE CASCADE
 );
 
 CREATE TABLE OpenL_User2Group (
-    loginName varchar(50) not null,
+    loginName ${varchar}(50) not null,
     groupID ${bigint} not null,
     PRIMARY KEY (loginName, groupID),
     CONSTRAINT fk_OpenL_User2Group1 FOREIGN KEY (loginName) REFERENCES OpenL_Users(loginName) ON DELETE CASCADE,
