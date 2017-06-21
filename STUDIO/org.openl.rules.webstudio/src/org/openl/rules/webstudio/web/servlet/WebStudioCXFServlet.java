9fc07b538bd9289024dab494b56f18295bb18969
Nail Samatov <nsamatov@eisgroup.com>
2017-06-21 20:28:16 +0300

    EPBDS-6710 Don't fail with NoSuchBeanDefinitionException and NullPointerException during InstallWizard if there is no spring configuration for CXF when webstudio mode is "installer".

---
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/web/servlet/WebStudioCXFServlet.java
@@ -0,0 +1,43 @@
+package org.openl.rules.webstudio.web.servlet;
+
+import javax.servlet.ServletConfig;
+
+import org.apache.cxf.BusFactory;
+import org.apache.cxf.transport.servlet.CXFServlet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+
+public class WebStudioCXFServlet extends CXFServlet {
+    private final Logger log = LoggerFactory.getLogger(WebStudioCXFServlet.class);
+    private boolean busCreated;
+
+    @Override
+    protected void loadBus(ServletConfig servletConfig) {
+        try {
+            super.loadBus(servletConfig);
+        } catch (NoSuchBeanDefinitionException e) {
+            // In Install Wizard CXF isn't configured and it's ok that spring bean for CXF isn't found.
+            // Finish servlet initialization correctly: initialize the bus ourselves (and lately destroy it ourselves).
+            log.info("Bean '" + e.getBeanName() + "' isn't found. Create bus ourselves.");
+
+            setBus(BusFactory.newInstance().createBus());
+            busCreated = true;
+        } catch (RuntimeException e) {
+            // Log unexpected exception and rethrow it.
+            log.error(e.getMessage(), e);
+            throw e;
+        }
+    }
+
+    @Override
+    public void destroyBus() {
+        super.destroyBus();
+
+        if (busCreated) {
+            // If we created the Bus, we must destroy it.
+            getBus().shutdown(true);
+            setBus(null);
+        }
+    }
+}
