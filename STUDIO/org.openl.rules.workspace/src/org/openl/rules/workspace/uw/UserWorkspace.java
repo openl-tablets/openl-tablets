959a75e60cd7da0dbde2360800020c0bdfc3d4ae
Nail Samatov <nsamatov@eisgroup.com>
2017-06-30 17:24:46 +0300

    EPBDS-6730 Move method to UserWorkspace
    --HG--
    branch : 5.19.x

--- STUDIO/org.openl.rules.workspace/src/org/openl/rules/workspace/uw/UserWorkspace.java
+++ STUDIO/org.openl.rules.workspace/src/org/openl/rules/workspace/uw/UserWorkspace.java
@@ -29,6 +29,8 @@ public interface UserWorkspace extends ProjectsContainer {
 
     ADeploymentProject getDDProject(String name) throws ProjectException;
 
+    ADeploymentProject getLatestDeploymentConfiguration(String name);
+
     List<ADeploymentProject> getDDProjects() throws ProjectException;
 
     DesignTimeRepository getDesignTimeRepository();
