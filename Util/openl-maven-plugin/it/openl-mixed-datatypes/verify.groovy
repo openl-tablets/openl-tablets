945c51965df97ab216bd11a56ec5f4fff6e70407
Yury Molchan <ymolchan@eisgroup.com>
2017-07-24 14:22:47 +0300

    EPBDS-6640 Add verify.groovy script which validates a successfull build

---
+++ Util/openl-maven-plugin/it/openl-mixed-datatypes/verify.groovy
@@ -0,0 +1,20 @@
+try {
+    assert new File(basedir, 'target/generated-sources/openl/com/example/Service.java').exists()
+    assert new File(basedir, 'target/generated-sources/openl/com/example/beans/openl/Auto.java').exists()
+    assert new File(basedir, 'target/generated-sources/openl/com/example/beans/openl/Person.java').exists()
+
+    assert new File(basedir, 'target/classes/com/example/Service.class').exists()
+    assert new File(basedir, 'target/classes/com/example/beans/Address.class').exists()
+    assert new File(basedir, 'target/classes/com/example/beans/openl/Auto.class').exists()
+    assert new File(basedir, 'target/classes/com/example/beans/openl/Person.class').exists()
+
+    assert new File(basedir, 'target/org.openl.test-0.0.0.zip').exists()
+
+    assert new File(basedir, 'target').list({File file, String name -> name.startsWith("org.openl.test-0.0.0") && name.endsWith("-lib.jar")}).length == 1
+
+    return true
+
+} catch(Throwable e) {
+    e.printStackTrace()
+    return false
+}
