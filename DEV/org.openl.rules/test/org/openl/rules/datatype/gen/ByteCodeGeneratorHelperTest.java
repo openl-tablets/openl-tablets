35e81e43aaef4c42137cd0ab0305a2231182e8b5
Yury Molchan <ymolchan@eisgroup.com>
2017-07-27 20:22:44 +0300

    EPBDS-6773 Use asm-commons to generate default constructor

--- DEV/org.openl.rules/test/org/openl/rules/datatype/gen/ByteCodeGeneratorHelperTest.java
+++ DEV/org.openl.rules/test/org/openl/rules/datatype/gen/ByteCodeGeneratorHelperTest.java
@@ -3,23 +3,10 @@ package org.openl.rules.datatype.gen;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
-import org.openl.rules.datatype.gen.bean.writers.DefaultValue;
 import org.openl.util.StringUtils;
 
 public class ByteCodeGeneratorHelperTest {
     
-    @Test
-    public void testGetTypeWriter() {
-        assertEquals("org.openl.rules.datatype.gen.types.writers.StringTypeWriter",
-                ByteCodeGeneratorHelper.getTypeWriter(new DefaultFieldDescription(String.class)).getClass().getName());
-
-        DefaultFieldDescription field = new DefaultFieldDescription(DriverTest.class);
-        field.setDefaultValueAsString(DefaultValue.DEFAULT);
-        assertEquals("org.openl.rules.datatype.gen.types.writers.DefaultConstructorTypeWriter",
-                ByteCodeGeneratorHelper.getTypeWriter(field).getClass().getName());
-
-    }
-
     @Test
     public void testGetJavaType() {
         assertEquals(StringUtils.EMPTY, ByteCodeGeneratorHelper.getJavaType((String) null));
@@ -41,11 +28,4 @@ public class ByteCodeGeneratorHelperTest {
         assertEquals("[[Lorg/test/MyType;", ByteCodeGeneratorHelper.getJavaType("org.test.MyType[][]"));
         assertEquals("Lorg/test/MyType;", ByteCodeGeneratorHelper.getJavaType("org.test.MyType"));
     }
-
-
-
-    private static class DriverTest {
-
-    }
-
 }
