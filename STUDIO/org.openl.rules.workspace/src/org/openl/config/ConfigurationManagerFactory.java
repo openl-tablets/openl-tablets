847e7471362b63a848fdadc2269dece550d98dbd
Nail Samatov <nsamatov@eisgroup.com>
2017-08-02 20:02:26 +0300

    EPBDS-6589 Implement support of predefined configuration with several production repositories
    --HG--
    branch : 5.19.x

--- STUDIO/org.openl.rules.workspace/src/org/openl/config/ConfigurationManagerFactory.java
+++ STUDIO/org.openl.rules.workspace/src/org/openl/config/ConfigurationManagerFactory.java
@@ -6,9 +6,15 @@ public class ConfigurationManagerFactory {
     private boolean useSystemProperties;
     private String defaultPropertiesLocation;
     private String propertiesFolder;
+    private String propertiesInContextFolder;
 
     public ConfigurationManagerFactory(boolean useSystemProperties, String defaultPropertiesLocation,
             String propertiesFolder) {
+        this(useSystemProperties, defaultPropertiesLocation, propertiesFolder, null);
+    }
+
+    public ConfigurationManagerFactory(boolean useSystemProperties, String defaultPropertiesLocation,
+            String propertiesFolder, String propertiesInContextFolder) {
         this.useSystemProperties = useSystemProperties;
         this.defaultPropertiesLocation = StringUtils.trimToNull(defaultPropertiesLocation);
 
@@ -17,12 +23,19 @@ public class ConfigurationManagerFactory {
             propertiesFolder += "/";
         }
         this.propertiesFolder = StringUtils.trimToEmpty(propertiesFolder);
+        this.propertiesInContextFolder = propertiesInContextFolder;
     }
 
     public ConfigurationManager getConfigurationManager(String propertiesName) {
         String fullPath = propertiesFolder + propertiesName;
+        String contextPath = propertiesInContextFolder == null ? null : propertiesInContextFolder + propertiesName;
         String defaultFile = defaultPropertiesLocation != null ? defaultPropertiesLocation : fullPath;
-        return new ConfigurationManager(useSystemProperties, StringUtils.trimToNull(fullPath),
-                StringUtils.trimToNull(defaultFile));
+        return new ConfigurationManager(
+                useSystemProperties,
+                StringUtils.trimToNull(fullPath),
+                StringUtils.trimToNull(contextPath),
+                StringUtils.trimToNull(defaultFile),
+                false
+        );
     }
 }
