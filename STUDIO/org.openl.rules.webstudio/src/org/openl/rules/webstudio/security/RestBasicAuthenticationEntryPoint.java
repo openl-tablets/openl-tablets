327d6d980a80532aee07bc6317496f310014fb1f
Nail Samatov <nsamatov@eisgroup.com>
2017-08-15 17:45:41 +0300

    EPBDS-6813 Fix the bug: html page is displayed if Viewer uploads file in Active Directory mode instead of simple error message

---
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/security/RestBasicAuthenticationEntryPoint.java
@@ -0,0 +1,21 @@
+package org.openl.rules.webstudio.security;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
+
+public class RestBasicAuthenticationEntryPoint extends BasicAuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request,
+            HttpServletResponse response,
+            AuthenticationException authException) throws IOException, ServletException {
+        response.addHeader("WWW-Authenticate", "Basic realm=\"" + getRealmName() + "\"");
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.getWriter().println(authException.getMessage());
+    }
+}
