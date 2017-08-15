1b4426ea7e60bf086e5cb585523eadf8b3f11dbc
Nail Samatov <nsamatov@eisgroup.com>
2017-08-15 20:27:42 +0300

    EPBDS-6812 Add possibility to use browser to access REST API for GET requests.

--- STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/security/RestBasicAuthenticationEntryPoint.java
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/security/RestBasicAuthenticationEntryPoint.java
@@ -9,6 +9,9 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 
+/**
+ * Authentication entry point for REST services
+ */
 public class RestBasicAuthenticationEntryPoint extends BasicAuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request,
@@ -16,6 +19,6 @@ public class RestBasicAuthenticationEntryPoint extends BasicAuthenticationEntryP
             AuthenticationException authException) throws IOException, ServletException {
         response.addHeader("WWW-Authenticate", "Basic realm=\"" + getRealmName() + "\"");
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-        response.getWriter().println(authException.getMessage());
+        response.getWriter().println("Unauthorized: " + authException.getMessage());
     }
 }
