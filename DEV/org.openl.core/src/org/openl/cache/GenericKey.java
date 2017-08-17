be44d9a2a506e9fdc8de40150545c0644a23a744
Yury Molchan <ymolchan@eisgroup.com>
2017-08-17 15:13:53 +0300

    EPBDS-6827 Improve GenericKey by removing usage of EqualsBuilder

--- DEV/org.openl.core/src/org/openl/cache/GenericKey.java
+++ DEV/org.openl.core/src/org/openl/cache/GenericKey.java
@@ -1,59 +1,100 @@
 package org.openl.cache;
 
-import org.apache.commons.lang3.builder.EqualsBuilder;
-
-import java.util.Arrays;
-
 /**
  * This is immutable object for using it as a key. Build key by array of
  * objects. This is instance controlled class. For instantiate class use static
  * factory method "getInstance" [EJ1].
  * 
  */
-public final class GenericKey {
-    private static final GenericKey EMPTY_GENERIC_KEY = new GenericKey(new Object[] {});
-    private Object[] objects;
+public class GenericKey {
+    private static final GenericKey NULL_KEY = new GenericKey();
 
-    private GenericKey(Object... objects) {
-        if (objects == null) {
-            throw new IllegalArgumentException("objects arg can't be null");
-        }
-        this.objects = Arrays.copyOf(objects, objects.length);
+    private GenericKey() {
     }
 
     /**
      * Returns a key instance for this objects
-     * 
-     * @param objects
-     * @return
      */
-    public static GenericKey getInstance(Object... objects) {
-        if (objects == null || objects.length == 0) {
-            return EMPTY_GENERIC_KEY;
+    public static GenericKey getInstance(Object object1, Object object2) {
+        if (object1 == null) {
+            if (object2 == null) {
+                return NULL_KEY;
+            }
+            return new Single2Key(object2);
+        } else if (object2 == null) {
+            return new Single1Key(object1);
+        } else {
+            return new ToupleKey(object1, object2);
         }
-        return new GenericKey(objects);
     }
 
-    @Override
-    public boolean equals(Object x) {
-        if (!(x instanceof GenericKey)) {
-            return false;
+    private static class SingleKey extends GenericKey {
+        Object object;
+
+        SingleKey(Object object) {
+            assert object != null;
+            this.object = object;
         }
-        GenericKey anotherKey = (GenericKey) x;
-        if (objects.length != anotherKey.objects.length) {
-            return false;
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o)
+                return true;
+            if (o == null || getClass() != o.getClass())
+                return false;
+
+            SingleKey singleKey = (SingleKey) o;
+
+            return object.equals(singleKey.object);
         }
-        // FIXME: Remove usage of the EqualsBuilder
-        //
-        EqualsBuilder equalsBuilder = new EqualsBuilder();
-        for (int i = 0; i < objects.length; i++) {
-            equalsBuilder.append(objects[i], anotherKey.objects[i]);
+
+        @Override
+        public int hashCode() {
+            return object.hashCode();
         }
-        return equalsBuilder.isEquals();
     }
 
-    @Override
-    public int hashCode() {
-        return objects != null ? Arrays.hashCode(objects) : 0;
+    private static class ToupleKey extends GenericKey {
+        Object object1;
+        Object object2;
+
+        ToupleKey(Object object1, Object object2) {
+            assert object1 != null && object2 != null;
+            this.object1 = object1;
+            this.object2 = object2;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o)
+                return true;
+            if (o == null || getClass() != o.getClass())
+                return false;
+
+            ToupleKey toupleKey = (ToupleKey) o;
+
+            if (!object1.equals(toupleKey.object1))
+                return false;
+            return object2.equals(toupleKey.object2);
+        }
+
+        @Override
+        public int hashCode() {
+            int result = object1.hashCode();
+            result = 31 * result + object2.hashCode();
+            return result;
+        }
+    }
+
+    private static class Single1Key extends SingleKey {
+        Single1Key(Object object) {
+            super(object);
+        }
+    }
+
+    private static class Single2Key extends SingleKey {
+        Single2Key(Object object) {
+            super(object);
+        }
     }
 }
