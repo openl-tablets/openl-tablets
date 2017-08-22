86433b06cbefed445fa3e359f80fce543bc5493e
Yury Molchan <ymolchan@eisgroup.com>
2017-08-23 01:17:59 +0300

    EPBDS-6830 Support Java7 bytecode

--- DEV/org.openl.rules/src/org/openl/rules/datatype/gen/bean/writers/EqualsWriter.java
+++ DEV/org.openl.rules/src/org/openl/rules/datatype/gen/bean/writers/EqualsWriter.java
@@ -4,20 +4,20 @@ import java.util.Map;
 
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.Label;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
-import org.objectweb.asm.Label;
 import org.objectweb.asm.Type;
-import org.openl.rules.datatype.gen.ByteCodeGeneratorHelper;
-import org.openl.rules.datatype.gen.FieldDescription;
+
 import org.openl.rules.asm.invoker.EqualsBuilderInvoker;
+import org.openl.rules.datatype.gen.FieldDescription;
 
 public class EqualsWriter extends MethodWriter {
-    
+
     /**
      * 
-     * @param beanNameWithPackage name of the class being generated with package, symbol '/' is used as separator<br> 
-     * (e.g. <code>my/test/TestClass</code>)
+     * @param beanNameWithPackage name of the class being generated with package, symbol '/' is used as separator<br>
+     *            (e.g. <code>my/test/TestClass</code>)
      * @param allFields collection of fields for current class and parent`s ones.
      */
     public EqualsWriter(String beanNameWithPackage, Map<String, FieldDescription> allFields) {
@@ -28,12 +28,6 @@ public class EqualsWriter extends MethodWriter {
         MethodVisitor methodVisitor;
         methodVisitor = classWriter.visitMethod(Opcodes.ACC_PUBLIC, "equals", "(Ljava/lang/Object;)Z", null, null);
 
-        // create EqualsBuilder
-        methodVisitor.visitTypeInsn(Opcodes.NEW, Type.getInternalName(EqualsBuilder.class));
-        methodVisitor.visitInsn(Opcodes.DUP);
-        methodVisitor
-                .visitMethodInsn(Opcodes.INVOKESPECIAL, Type.getInternalName(EqualsBuilder.class), "<init>", "()V");
-
         Label comparingLabel = new Label();
 
         // check "instance of" object
@@ -42,9 +36,16 @@ public class EqualsWriter extends MethodWriter {
         methodVisitor.visitJumpInsn(Opcodes.IFNE, comparingLabel);
         methodVisitor.visitLdcInsn(Boolean.FALSE);
         methodVisitor.visitInsn(Opcodes.IRETURN);
+        methodVisitor.visitLabel(comparingLabel);
+        methodVisitor.visitFrame(Opcodes.F_SAME, 0, null, 0, null);
+
+        // create EqualsBuilder
+        methodVisitor.visitTypeInsn(Opcodes.NEW, Type.getInternalName(EqualsBuilder.class));
+        methodVisitor.visitInsn(Opcodes.DUP);
+        methodVisitor
+            .visitMethodInsn(Opcodes.INVOKESPECIAL, Type.getInternalName(EqualsBuilder.class), "<init>", "()V");
 
         // cast
-        methodVisitor.visitLabel(comparingLabel);
         methodVisitor.visitVarInsn(Opcodes.ALOAD, 1);
         methodVisitor.visitTypeInsn(Opcodes.CHECKCAST, getBeanNameWithPackage());
         methodVisitor.visitVarInsn(Opcodes.ASTORE, 2);
