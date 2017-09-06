07d4eef54dcf574c9f5112d7815def06b4a5b237
Yury Molchan <ymolchan@eisgroup.com>
2017-09-06 22:48:30 +0300

    EPBDS-6830 No needs to remove 'invalid' datatypes from the binding context.
    Removing makes more difficult to understanding the root issue of the compilation error.
    
    --HG--
    branch : 5.19.x

--- DEV/org.openl.core/src/org/openl/binding/IBindingContext.java
+++ DEV/org.openl.core/src/org/openl/binding/IBindingContext.java
@@ -49,24 +49,6 @@ public interface IBindingContext extends ICastFactory {
 	
 	void addTypes(Map<String, IOpenClass> types) throws Exception;
 
-	/**
-	 * Removes type from binding context.
-	 * 
-	 * @param namespace
-	 *            type namespace
-	 * @param type
-	 *            type
-	 * @throws Exception
-	 *             if an error occurs
-	 */
-	void removeType(String namespace, IOpenClass type) throws Exception;
-
-//	NOTE: A temporary implementation of multi-module feature.
-/*
- void addImport(IOpenClass type);
- Collection<IOpenClass> getImports();
-*/	
-
 	ILocalVar addVar(String namespace, String name, IOpenClass type)
 			throws DuplicatedVarException;
 
