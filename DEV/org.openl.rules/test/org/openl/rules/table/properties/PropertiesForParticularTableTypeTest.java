e5dca0adf490fe317d1f607ac404487ace687969
Marat Kamalov <mkamalov@eisgroup.com>
2017-09-18 12:30:05 +0300

    EPBDS-5148 Support parallel execution for array parameter calls. Fix tests.

--- DEV/org.openl.rules/test/org/openl/rules/table/properties/PropertiesForParticularTableTypeTest.java
+++ DEV/org.openl.rules/test/org/openl/rules/table/properties/PropertiesForParticularTableTypeTest.java
@@ -31,7 +31,7 @@ public class PropertiesForParticularTableTypeTest extends BaseOpenlBuilderHelper
             ITableProperties tableProperties = resultTsn.getTableProperties();
             assertNotNull(tableProperties);
 
-            assertEquals(5, tableProperties.getAllProperties().size());
+            assertEquals(6, tableProperties.getAllProperties().size());
 
         } else {
             fail();
@@ -57,7 +57,7 @@ public class PropertiesForParticularTableTypeTest extends BaseOpenlBuilderHelper
 
             Map<String, Object> allProperties = tableProperties.getAllProperties();
             assertEquals("AllProperties size is 10, ignore property 'scope' and including default properties",
-                8,
+                9,
                 allProperties.size());
             assertTrue("There is no property 'scope' applied for this table, as it can`t be defined in such table type",
                 !allProperties.containsKey("scope"));
