fee8c3b5ac75813d9eb045d86542e40b3a73472f
Yury Molchan <ymolchan@eisgroup.com>
2017-09-20 18:06:44 +0300

    EPBDS-6888 Remove duplication. Merge AllProjectsTest into AllExcelTest

--- STUDIO/org.openl.rules.functionality.test/test/org/openl/core/AllExcelTest.java
+++ STUDIO/org.openl.rules.functionality.test/test/org/openl/core/AllExcelTest.java
@@ -11,6 +11,9 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openl.CompiledOpenClass;
+import org.openl.rules.project.instantiation.RulesInstantiationException;
+import org.openl.rules.project.instantiation.SimpleProjectEngineFactory;
+import org.openl.rules.project.resolving.ProjectResolvingException;
 import org.openl.rules.runtime.RulesEngineFactory;
 import org.openl.rules.testmethod.TestSuiteMethod;
 import org.openl.rules.testmethod.TestUnitsResults;
@@ -53,22 +56,38 @@ public final class AllExcelTest {
             LOG.warn("Tests directory doesn't exist!");
             return;
         }
-        
-        final String[] files = sourceDir.list();
-        for (String sourceFile : files) {
 
-            try {
-                new FileInputStream(new File(sourceDir, sourceFile)).close();
-            } catch (Exception ex) {
-                LOG.error("Failed to read file [" + sourceFile + "]", ex);
-                hasErrors = true;
-                continue;
-            }
-
-            RulesEngineFactory<?> engineFactory = new RulesEngineFactory<Object>(DIR + sourceFile);
-            engineFactory.setExecutionMode(false);
+        for (File file : sourceDir.listFiles()) {
+            String sourceFile = file.getName();
             IRuntimeEnv env = new SimpleRulesVM().getRuntimeEnv();
-            final CompiledOpenClass compiledOpenClass = engineFactory.getCompiledOpenClass();
+            final CompiledOpenClass compiledOpenClass;
+            if (file.isFile()) {
+                try {
+                    new FileInputStream(file).close();
+                } catch (Exception ex) {
+                    LOG.error("Failed to read file [" + sourceFile + "]", ex);
+                    hasErrors = true;
+                    continue;
+                }
+
+                RulesEngineFactory<?> engineFactory = new RulesEngineFactory<Object>(DIR + sourceFile);
+                engineFactory.setExecutionMode(false);
+                compiledOpenClass = engineFactory.getCompiledOpenClass();
+            } else {
+                SimpleProjectEngineFactory.SimpleProjectEngineFactoryBuilder<Object> engineFactoryBuilder = new SimpleProjectEngineFactory.SimpleProjectEngineFactoryBuilder<Object>();
+                engineFactoryBuilder.setExecutionMode(false);
+                engineFactoryBuilder.setProject(file.getPath());
+                SimpleProjectEngineFactory<Object> engineFactory = engineFactoryBuilder.build();
+
+                try {
+                    compiledOpenClass = engineFactory.getCompiledOpenClass();
+                } catch (ClassNotFoundException | ProjectResolvingException | RulesInstantiationException e) {
+                    LOG.error("Compilation fails for [" + sourceFile + "].", e);
+                    hasErrors = true;
+                    continue;
+                }
+
+            }
 
             if (compiledOpenClass.hasErrors()) {
                 LOG.error("Compilation errors in [" + sourceFile + "].");
