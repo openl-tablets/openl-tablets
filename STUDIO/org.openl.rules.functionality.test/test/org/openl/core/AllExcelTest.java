6c29bf3224cee0f25dc67f15d69e88c1f9b7d378
Yury Molchan <ymolchan@eisgroup.com>
2017-09-20 18:54:21 +0300

    EPBDS-6888 Remove duplication. Merge NegativeTest into AllExcelTest

--- STUDIO/org.openl.rules.functionality.test/test/org/openl/core/AllExcelTest.java
+++ STUDIO/org.openl.rules.functionality.test/test/org/openl/core/AllExcelTest.java
@@ -4,6 +4,9 @@ import static org.junit.Assert.assertFalse;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.TimeZone;
 
@@ -11,6 +14,8 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openl.CompiledOpenClass;
+import org.openl.message.OpenLMessage;
+import org.openl.message.Severity;
 import org.openl.rules.project.instantiation.RulesInstantiationException;
 import org.openl.rules.project.instantiation.SimpleProjectEngineFactory;
 import org.openl.rules.project.resolving.ProjectResolvingException;
@@ -20,15 +25,16 @@ import org.openl.rules.testmethod.TestUnitsResults;
 import org.openl.rules.vm.SimpleRulesVM;
 import org.openl.types.IOpenClass;
 import org.openl.types.IOpenMethod;
+import org.openl.util.IOUtils;
+import org.openl.util.StringUtils;
 import org.openl.vm.IRuntimeEnv;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public final class AllExcelTest {
 
-    private static final Logger LOG = LoggerFactory.getLogger(AllExcelTest.class);
-
     public static final String DIR = "test-resources/functionality/";
+    private static final Logger LOG = LoggerFactory.getLogger(AllExcelTest.class);
     private Locale defaultLocale;
     private TimeZone defaultTimeZone;
 
@@ -48,10 +54,10 @@ public final class AllExcelTest {
 
     @Test
     public void testAllExcelFiles() throws NoSuchMethodException {
-        LOG.info(">>> Positive tests...");
+        LOG.info(">>> Negative tests...");
         boolean hasErrors = false;
         final File sourceDir = new File(DIR);
-        
+
         if (!sourceDir.exists()) {
             LOG.warn("Tests directory doesn't exist!");
             return;
@@ -59,9 +65,8 @@ public final class AllExcelTest {
 
         for (File file : sourceDir.listFiles()) {
             String sourceFile = file.getName();
-            IRuntimeEnv env = new SimpleRulesVM().getRuntimeEnv();
             final CompiledOpenClass compiledOpenClass;
-            if (file.isFile()) {
+            if (file.isFile() && (sourceFile.endsWith(".xlsx") || sourceFile.endsWith(".xls"))) {
                 try {
                     new FileInputStream(file).close();
                 } catch (Exception ex) {
@@ -73,7 +78,7 @@ public final class AllExcelTest {
                 RulesEngineFactory<?> engineFactory = new RulesEngineFactory<Object>(DIR + sourceFile);
                 engineFactory.setExecutionMode(false);
                 compiledOpenClass = engineFactory.getCompiledOpenClass();
-            } else {
+            } else if (file.isDirectory()) {
                 SimpleProjectEngineFactory.SimpleProjectEngineFactoryBuilder<Object> engineFactoryBuilder = new SimpleProjectEngineFactory.SimpleProjectEngineFactoryBuilder<Object>();
                 engineFactoryBuilder.setExecutionMode(false);
                 engineFactoryBuilder.setProject(file.getPath());
@@ -86,40 +91,128 @@ public final class AllExcelTest {
                     hasErrors = true;
                     continue;
                 }
-
-            }
-
-            if (compiledOpenClass.hasErrors()) {
-                LOG.error("Compilation errors in [" + sourceFile + "].");
-                LOG.error(compiledOpenClass.getMessages().toString());
-
-                hasErrors = true;
+            } else {
+                // Skip not project files
                 continue;
             }
 
-            IOpenClass openClass = compiledOpenClass.getOpenClass();
-            Object target = openClass.newInstance(env);
-            int errors = 0;
-            for (IOpenMethod method : openClass.getDeclaredMethods()) {
-                if (method instanceof TestSuiteMethod) {
-                    TestUnitsResults res = (TestUnitsResults) method.invoke(target, new Object[0], env);
-                    final int numberOfFailures = res.getNumberOfFailures();
-                    errors += numberOfFailures;
-                    if (numberOfFailures != 0) {
-                        LOG.error("Errors in [" + sourceFile + "]. Failed test: " + res
-                            .getName() + "  Errors #:" + numberOfFailures);
+            if (anyMessageFileExists(sourceDir, sourceFile)) {
+                List<OpenLMessage> actualMessages = compiledOpenClass.getMessages();
+                boolean hasAllMessages = true;
+                for (Severity severity : Severity.values()) {
+                    if (!assertMessages(sourceDir, sourceFile, actualMessages, severity)) {
+                        hasAllMessages = false;
                     }
+                }
 
+                if (hasAllMessages) {
+                    LOG.info("OK in [" + sourceFile + "].");
+                } else {
+                    hasErrors = true;
+                }
+            } else {
+                if (compiledOpenClass.hasErrors()) {
+                    LOG.error("Compilation errors in [" + sourceFile + "].");
+                    LOG.error(compiledOpenClass.getMessages().toString());
+
+                    hasErrors = true;
+                    continue;
+                }
+
+                IRuntimeEnv env = new SimpleRulesVM().getRuntimeEnv();
+                IOpenClass openClass = compiledOpenClass.getOpenClass();
+                Object target = openClass.newInstance(env);
+                int errors = 0;
+                for (IOpenMethod method : openClass.getDeclaredMethods()) {
+                    if (method instanceof TestSuiteMethod) {
+                        TestUnitsResults res = (TestUnitsResults) method.invoke(target, new Object[0], env);
+                        final int numberOfFailures = res.getNumberOfFailures();
+                        errors += numberOfFailures;
+                        if (numberOfFailures != 0) {
+                            LOG.error("Errors in [" + sourceFile + "]. Failed test: " + res.getName() + "  Errors #:" + numberOfFailures);
+                        }
+
+                    }
+                }
+                if (errors != 0) {
+                    hasErrors = true;
+                    LOG.error("Errors in [" + sourceFile + "]. Total failures #: " + errors);
+                } else {
+                    LOG.info("OK in [" + sourceFile + "]. ");
                 }
             }
-            if (errors != 0) {
-                hasErrors = true;
-                LOG.error("Errors in [" + sourceFile + "]. Total failures #: " + errors);
-            } else {
-                LOG.info("OK in [" + sourceFile + "]. ");
+        }
+
+        assertFalse("Some tests have been failed!", hasErrors);
+    }
+
+    private boolean anyMessageFileExists(File sourceDir, String projectFile) {
+        for (Severity severity : Severity.values()) {
+            File messagesFile = new File(sourceDir, projectFile + "." + severity.name().toLowerCase() + ".txt");
+            if (messagesFile.exists()) {
+                return true;
             }
         }
-        
-        assertFalse("Some tests have been failed!", hasErrors);
+
+        return false;
+    }
+
+    private boolean assertMessages(File sourceDir,
+                                   String projectFile,
+                                   List<OpenLMessage> actualMessages,
+                                   Severity severity) {
+        boolean hasAllMessages = true;
+
+        File file = new File(sourceDir, projectFile + "." + severity.name().toLowerCase() + ".txt");
+        try {
+            for (String expectedMessage : getExpectedMessages(file)) {
+                if (!findExpectedMessage(actualMessages, expectedMessage, severity)) {
+                    LOG.error("The message \"" + expectedMessage + "\" with severity " + severity
+                            .name() + " is missed for [" + projectFile + "].");
+                    hasAllMessages = false;
+                }
+            }
+        } catch (IOException e) {
+            LOG.error("Failed to read file [" + file + "].", e);
+            hasAllMessages = false;
+        }
+
+        if (!hasAllMessages) {
+            StringBuilder sb = new StringBuilder();
+            sb.append("Actual messages:");
+            for (OpenLMessage openLMessage : actualMessages) {
+                sb.append(System.lineSeparator());
+                sb.append("\t\"" + openLMessage.getSummary() + "\" with severity " + openLMessage.getSeverity() + ".");
+            }
+            LOG.error(sb.toString());
+        }
+
+        return hasAllMessages;
+    }
+
+    private List<String> getExpectedMessages(File file) throws IOException {
+        List<String> result = new ArrayList<>();
+
+        if (!file.exists()) {
+            return result;
+        }
+
+        String content = IOUtils.toStringAndClose(new FileInputStream(file));
+        for (String message : content.split("\\u000D\\u000A|[\\u000A\\u000B\\u000C\\u000D\\u0085\\u2028\\u2029]")) {
+            if (!StringUtils.isBlank(message)) {
+                result.add(message.trim());
+            }
+        }
+
+        return result;
+    }
+
+    private boolean findExpectedMessage(List<OpenLMessage> actualMessages, String expectedMessage, Severity severity) {
+        for (OpenLMessage message : actualMessages) {
+            if (message.getSummary().equals(expectedMessage) && message.getSeverity() == severity) {
+                return true;
+            }
+        }
+        return false;
     }
 }
