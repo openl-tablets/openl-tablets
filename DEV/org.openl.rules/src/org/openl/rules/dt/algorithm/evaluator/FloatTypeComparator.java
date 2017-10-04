f29f7aca60f22cc526bd8d3663959cd83929f085
Yury Molchan <ymolchan@eisgroup.com>
2017-10-05 01:11:02 +0300

    EPBDS-6914 Use Java's comparison of double values
    --HG--
    branch : 5.19.x

--- DEV/org.openl.rules/src/org/openl/rules/dt/algorithm/evaluator/FloatTypeComparator.java
+++ DEV/org.openl.rules/src/org/openl/rules/dt/algorithm/evaluator/FloatTypeComparator.java
@@ -3,14 +3,13 @@ package org.openl.rules.dt.algorithm.evaluator;
 import java.util.Comparator;
 
 import org.openl.rules.helpers.NumberUtils;
-import org.openl.util.math.MathUtils;
 
 public class FloatTypeComparator implements Comparator<Object> {
     private static FloatTypeComparator INSTANCE = new FloatTypeComparator();
-    
+
     private FloatTypeComparator() {
     }
-    
+
     public static FloatTypeComparator getInstance() {
         return INSTANCE;
     }
@@ -19,12 +18,13 @@ public class FloatTypeComparator implements Comparator<Object> {
     public int compare(Object o1, Object o2) {
         double d1 = NumberUtils.convertToDouble(o1);
         double d2 = NumberUtils.convertToDouble(o2);
-        if (MathUtils.lt(d1, d2)) {
-            return -1;
+        int compare = Double.compare(d1, d2);
+        if (compare == 0) {
+            return 0;
+        } else if (Math.abs(d1 - d2) <= Math.ulp(d1)) {
+            return 0;
+        } else {
+            return compare;
         }
-        if (MathUtils.gt(d1, d2)) {
-            return 1;
-        }
-        return 0;
     }
 }
