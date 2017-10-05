f62f179e62020132d4516167579ec54bdd87700d
Yury Molchan <ymolchan@eisgroup.com>
2017-10-05 14:11:31 +0300

    Replace while and for with foreach. Formatting
    --HG--
    branch : 5.19.x

--- DEV/org.openl.rules/src/org/openl/rules/dt/element/Condition.java
+++ DEV/org.openl.rules/src/org/openl/rules/dt/element/Condition.java
@@ -1,7 +1,5 @@
 package org.openl.rules.dt.element;
 
-import java.util.Iterator;
-
 import org.openl.binding.BindingDependencies;
 import org.openl.binding.ILocalVar;
 import org.openl.rules.binding.RulesBindingDependencies;
@@ -11,22 +9,13 @@ import org.openl.rules.table.ILogicalTable;
 import org.openl.types.IMethodCaller;
 import org.openl.types.IOpenField;
 import org.openl.types.IParameterDeclaration;
-import org.openl.types.impl.CompositeMethod;
 import org.openl.types.impl.OpenFieldDelegator;
 import org.openl.vm.IRuntimeEnv;
 
 public class Condition extends FunctionalRow implements ICondition {
 
     private IMethodCaller evaluator;
-    public void setEvaluator(IMethodCaller evaluator) {
-		this.evaluator = evaluator;
-	}
-
-	public void setConditionEvaluator(IConditionEvaluator conditionEvaluator) {
-		this.conditionEvaluator = conditionEvaluator;
-	}
-
-	private IConditionEvaluator conditionEvaluator;
+    private IConditionEvaluator conditionEvaluator;
 
     public Condition(String name, int row, ILogicalTable decisionTable, DTScale.RowScale scale) {
         super(name, row, decisionTable, scale);
@@ -34,6 +23,10 @@ public class Condition extends FunctionalRow implements ICondition {
 
     public IConditionEvaluator getConditionEvaluator() {
         return conditionEvaluator;
+    }
+
+    public void setConditionEvaluator(IConditionEvaluator conditionEvaluator) {
+        this.conditionEvaluator = conditionEvaluator;
     }
 
     public boolean isAction() {
@@ -48,7 +41,10 @@ public class Condition extends FunctionalRow implements ICondition {
         return evaluator == null ? getMethod() : evaluator;
     }
 
- 
+    public void setEvaluator(IMethodCaller evaluator) {
+        this.evaluator = evaluator;
+    }
+
     public DecisionValue calculateCondition(int ruleN, Object target, Object[] dtParams, IRuntimeEnv env) {
 
         if (isEmpty(ruleN)) {
@@ -91,19 +87,16 @@ public class Condition extends FunctionalRow implements ICondition {
         IParameterDeclaration[] params = getParams();
 
         BindingDependencies dependencies = new RulesBindingDependencies();
-        ((CompositeMethod) getMethod()).updateDependency(dependencies);
+        getMethod().updateDependency(dependencies);
 
-        Iterator<IOpenField> iter = dependencies.getFieldsMap().values().iterator();
+        for (IOpenField field : dependencies.getFieldsMap().values()) {
 
-        while (iter.hasNext()) {
-
-            IOpenField field = iter.next();
             field = getLocalField(field);
 
             if (field instanceof ILocalVar) {
 
-                for (int i = 0; i < params.length; i++) {
-                    if (params[i].getName().equals(field.getName())) {
+                for (IParameterDeclaration param : params) {
+                    if (param.getName().equals(field.getName())) {
                         return true;
                     }
                 }
