6de939a5ad0a07d2bf63c08837b9249933030f0c
Marat Kamalov <mkamalov@eisgroup.com>
2017-10-13 00:18:05 +0300

    EPBDS-6928 Multithread test was added for lazy functionality.

---
+++ WSFrontend/org.openl.rules.ruleservice/src/org/openl/rules/ruleservice/core/MaxThreadsForCompileSemaphore.java
@@ -0,0 +1,48 @@
+package org.openl.rules.ruleservice.core;
+
+import java.util.concurrent.Semaphore;
+
+public final class MaxThreadsForCompileSemaphore {
+    private Semaphore limitCompilationThreadsSemaphore = new Semaphore(
+        RuleServiceStaticConfigurationUtil.getMaxThreadsForCompile());
+    private ThreadLocal<Object> threadsMarker = new ThreadLocal<Object>();
+
+    private MaxThreadsForCompileSemaphore() {
+    }
+
+    private static class MaxThreadsForCompileSemaphoreHolder {
+        private static final MaxThreadsForCompileSemaphore INSTANCE = new MaxThreadsForCompileSemaphore();
+    }
+
+    public static final MaxThreadsForCompileSemaphore getInstance() {
+        return MaxThreadsForCompileSemaphoreHolder.INSTANCE;
+    }
+
+    private Semaphore getLimitCompilationThreadsSemaphore() {
+        return limitCompilationThreadsSemaphore;
+    }
+
+    private ThreadLocal<Object> getThreadsMarker() {
+        return threadsMarker;
+    }
+
+    public <T> T run(Callable<T> callable) throws Exception {
+        boolean requiredSemophore = MaxThreadsForCompileSemaphore.getInstance().getThreadsMarker().get() == null;
+        try {
+            if (requiredSemophore) {
+                MaxThreadsForCompileSemaphore.getInstance().getThreadsMarker().set(Thread.currentThread());
+                MaxThreadsForCompileSemaphore.getInstance().getLimitCompilationThreadsSemaphore().acquire();
+            }
+            return callable.call();
+        } finally {
+            if (requiredSemophore) {
+                MaxThreadsForCompileSemaphore.getInstance().getThreadsMarker().remove();
+                MaxThreadsForCompileSemaphore.getInstance().getLimitCompilationThreadsSemaphore().release();
+            }
+        }
+    }
+
+    public interface Callable<T> {
+        T call() throws Exception;
+    }
+}
---
+++ WSFrontend/org.openl.rules.ruleservice/test/org/openl/rules/ruleservice/publish/lazy/LazyCompilationTest.java
@@ -0,0 +1,91 @@
+package org.openl.rules.ruleservice.publish.lazy;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openl.rules.context.IRulesRuntimeContext;
+import org.openl.rules.context.RulesRuntimeContextFactory;
+import org.openl.rules.ruleservice.management.ServiceManager;
+import org.openl.rules.ruleservice.simple.RulesFrontend;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@TestPropertySource(properties = { "ruleservice.datasource.dir=test-resources/LazyCompilationTest",
+        "ruleservice.datasource.type = local" })
+@ContextConfiguration({ "classpath:openl-ruleservice-beans.xml" })
+public class LazyCompilationTest {
+
+    private static final int MAX_THREADS = 8;
+
+    private static final String SERVICE_NAME = "LazyCompilationTest_multimodule";
+
+    private ApplicationContext applicationContext;
+
+    private volatile boolean failed = false;
+    private volatile boolean running = true;
+
+    @Autowired
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    @Test
+    public void lazyCompilationTest() throws Exception {
+        assertNotNull(applicationContext);
+        ServiceManager serviceManager = applicationContext.getBean("serviceManager", ServiceManager.class);
+        assertNotNull(serviceManager);
+        RulesFrontend frontend = applicationContext.getBean("frontend", RulesFrontend.class);
+
+        Thread[] threads = new Thread[MAX_THREADS];
+        for (int i = 0; i < MAX_THREADS; i++) {
+            threads[i] = new Thread(new LazyCompilationTestRunnable(frontend));
+            threads[i].start();
+        }
+
+        Thread.sleep(120000);
+
+        running = false;
+        for (int i = 0; i < MAX_THREADS; i++) {
+            threads[i].join();
+        }
+        assertFalse(failed);
+    }
+
+    public class LazyCompilationTestRunnable implements Runnable {
+        private RulesFrontend frontend;
+
+        public LazyCompilationTestRunnable(RulesFrontend frontend) {
+            this.frontend = frontend;
+        }
+
+        @Override
+        public void run() {
+            int i = 0;
+            while (running) {
+                i++;
+                if (i == 10) {
+                    i = 1;
+                }
+                try {
+                    IRulesRuntimeContext cxt = RulesRuntimeContextFactory.buildRulesRuntimeContext();
+                    cxt.setLob("module" + i);
+                    if (!("module" + i).equals(frontend.execute(SERVICE_NAME, "hello", new Object[] { cxt }))) {
+                        failed = true;
+                        running = false;
+                    }
+                } catch (Exception e) {
+                    failed = true;
+                    running = false;
+                }
+            }
+        }
+    }
+
+}
