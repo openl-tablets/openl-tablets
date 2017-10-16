351a9feeca4209afd8a847c898abfca71e3c6799
Marat Kamalov <mkamalov@eisgroup.com>
2017-10-16 16:00:48 +0300

    EPBDS-6942 Incorrect behavior with generics. Additional tests were added.

--- DEV/org.openl.core/test/org/openl/binding/impl/MethodSearchTest.java
+++ DEV/org.openl.core/test/org/openl/binding/impl/MethodSearchTest.java
@@ -1,135 +1,182 @@
 package org.openl.binding.impl;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import java.io.Serializable;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.openl.binding.ICastFactory;
 import org.openl.conf.ConfigurableResourceContext;
 import org.openl.conf.OpenLConfiguration;
 import org.openl.conf.TypeCastFactory;
-import org.openl.meta.DoubleValue;
 import org.openl.types.IMethodCaller;
 import org.openl.types.IOpenClass;
 import org.openl.types.java.JavaOpenClass;
 
 public class MethodSearchTest {
 
-	public static class ClassWithMethods {
-		public void method1(int arg1, double arg2) {
-		};
+    public static class ClassWithMethods {
+        public void method1(int arg1, double arg2) {
+        };
 
-		public void method1(int arg1, byte arg2) {
-		};
+        public void method1(int arg1, byte arg2) {
+        };
 
-		public void method2(int arg1, double arg2) {
-		};
-	}
+        public void method2(int arg1, double arg2) {
+        };
+    }
 
-	public static class SecondClassWithMethods extends ClassWithMethods implements Serializable {
-		private static final long serialVersionUID = 1L;
+    public static class SecondClassWithMethods extends ClassWithMethods implements Serializable {
+        private static final long serialVersionUID = 1L;
 
-		public void method1(int arg1, double arg2) {
-		};
+        public void method1(int arg1, double arg2) {
+        };
 
-		public void method3(int arg1, double arg2) {
-		};
-	}
+        public void method3(int arg1, double arg2) {
+        };
+    }
 
-	public static class ClassWithGenerics {
-		public <T> T[] method1(T arg1, T arg2) {
-			return null;
-		};
-	}
+    public static class ClassWithGenerics {
+        public <T> T[] method1(T arg1, T arg2) {
+            return null;
+        };
 
-	public static class ThirdClassWithMethods extends SecondClassWithMethods {
-		private static final long serialVersionUID = 1L;
-	}
+        public <T> T[] method2(T arg1, T arg2) {
+            return null;
+        };
+    }
 
-	private ICastFactory getCastFactory() {
-		TypeCastFactory typecast = new TypeCastFactory();
-		TypeCastFactory.JavaCastComponent javacast = new TypeCastFactory.JavaCastComponent();
-		javacast.setLibraryClassName(org.openl.binding.impl.cast.CastOperators.class.getName());
-		javacast.setClassName(org.openl.binding.impl.cast.CastFactory.class.getName());
-		typecast.addJavaCast(javacast);
-		OpenLConfiguration openLConfiguration = new OpenLConfiguration();
-		openLConfiguration.setTypeCastFactory(typecast);
-		openLConfiguration.setConfigurationContext(new ConfigurableResourceContext(null));
-		return openLConfiguration;
-	}
+    public static class ThirdClassWithMethods extends SecondClassWithMethods {
+        private static final long serialVersionUID = 1L;
+    }
 
-	private static boolean methodEquals(IMethodCaller methodCaller1, IMethodCaller methodCaller2) {
-		if (methodCaller1.getMethod().getName().equals(methodCaller2.getMethod().getName())) {
-			if (methodCaller1.getMethod().getSignature().equals(methodCaller2.getMethod().getSignature())) {
-				return true;
-			}
-		}
-		return false;
-	}
+    private ICastFactory getCastFactory() {
+        TypeCastFactory typecast = new TypeCastFactory();
+        TypeCastFactory.JavaCastComponent javacast = new TypeCastFactory.JavaCastComponent();
+        javacast.setLibraryClassName(org.openl.binding.impl.cast.CastOperators.class.getName());
+        javacast.setClassName(org.openl.binding.impl.cast.CastFactory.class.getName());
+        typecast.addJavaCast(javacast);
+        OpenLConfiguration openLConfiguration = new OpenLConfiguration();
+        openLConfiguration.setTypeCastFactory(typecast);
+        openLConfiguration.setConfigurationContext(new ConfigurableResourceContext(null));
+        return openLConfiguration;
+    }
 
-	@Test
-	public void testMethodChoosing() {
-		ICastFactory castFactory = getCastFactory();
+    private static boolean methodEquals(IMethodCaller methodCaller1, IMethodCaller methodCaller2) {
+        if (methodCaller1.getMethod().getName().equals(methodCaller2.getMethod().getName())) {
+            if (methodCaller1.getMethod().getSignature().equals(methodCaller2.getMethod().getSignature())) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-		JavaOpenClass javaOpenClass = JavaOpenClass.getOpenClass(ClassWithMethods.class);
-		JavaOpenClass javaOpenClass2 = JavaOpenClass.getOpenClass(SecondClassWithMethods.class);
+    @Test
+    public void testMethodChoosing() {
+        ICastFactory castFactory = getCastFactory();
 
-		IMethodCaller methodCaller1 = MethodSearch.getMethodCaller("method1",
-				new IOpenClass[] { JavaOpenClass.INT, JavaOpenClass.INT }, castFactory, javaOpenClass);
+        JavaOpenClass javaOpenClass = JavaOpenClass.getOpenClass(ClassWithMethods.class);
+        JavaOpenClass javaOpenClass2 = JavaOpenClass.getOpenClass(SecondClassWithMethods.class);
 
-		assertTrue(methodEquals(
-				javaOpenClass.getMethod("method1", new IOpenClass[] { JavaOpenClass.INT, JavaOpenClass.DOUBLE }),
-				methodCaller1));
+        IMethodCaller methodCaller1 = MethodSearch.getMethodCaller("method1",
+            new IOpenClass[] { JavaOpenClass.INT, JavaOpenClass.INT },
+            castFactory,
+            javaOpenClass);
 
-		IMethodCaller methodCaller2 = MethodSearch.getMethodCaller("method1",
-				new IOpenClass[] { JavaOpenClass.INT, JavaOpenClass.INT }, castFactory, javaOpenClass2);
+        assertTrue(methodEquals(
+            javaOpenClass.getMethod("method1", new IOpenClass[] { JavaOpenClass.INT, JavaOpenClass.DOUBLE }),
+            methodCaller1));
 
-		assertTrue(methodEquals(
-				javaOpenClass2.getMethod("method1", new IOpenClass[] { JavaOpenClass.INT, JavaOpenClass.DOUBLE }),
-				methodCaller2));
+        IMethodCaller methodCaller2 = MethodSearch.getMethodCaller("method1",
+            new IOpenClass[] { JavaOpenClass.INT, JavaOpenClass.INT },
+            castFactory,
+            javaOpenClass2);
 
-	}
+        assertTrue(methodEquals(
+            javaOpenClass2.getMethod("method1", new IOpenClass[] { JavaOpenClass.INT, JavaOpenClass.DOUBLE }),
+            methodCaller2));
 
-	@Test
-	public void testMethodChoosingWithGenerics() {
-		ICastFactory castFactory = getCastFactory();
+    }
 
-		JavaOpenClass javaOpenClass = JavaOpenClass.getOpenClass(ClassWithGenerics.class);
+    @Test
+    public void testMethodChoosingWithGenerics() {
+        ICastFactory castFactory = getCastFactory();
 
-		IMethodCaller methodCaller1 = MethodSearch.getMethodCaller("method1",
-				new IOpenClass[] { JavaOpenClass.STRING, JavaOpenClass.STRING }, castFactory, javaOpenClass);
+        JavaOpenClass javaOpenClass = JavaOpenClass.getOpenClass(ClassWithGenerics.class);
 
-		assertNotNull(methodCaller1);
+        IMethodCaller methodCaller1 = MethodSearch.getMethodCaller("method1",
+            new IOpenClass[] { JavaOpenClass.STRING, JavaOpenClass.STRING },
+            castFactory,
+            javaOpenClass);
 
-		IMethodCaller methodCaller2 = MethodSearch.getMethodCaller("method1",
-				new IOpenClass[] { JavaOpenClass.getOpenClass(Integer.class), JavaOpenClass.STRING }, castFactory,
-				javaOpenClass);
+        assertNotNull(methodCaller1);
 
-		Assert.assertNull(methodCaller2);
-	}
+        IMethodCaller methodCaller2 = MethodSearch.getMethodCaller("method1",
+            new IOpenClass[] { JavaOpenClass.getOpenClass(Integer.class), JavaOpenClass.STRING },
+            castFactory,
+            javaOpenClass);
 
-	@Test
-	public void testMethodInheritance() {
-		ICastFactory castFactory = getCastFactory();
+        assertNull(methodCaller2);
 
-		JavaOpenClass javaOpenClass = JavaOpenClass.getOpenClass(ClassWithMethods.class);
-		JavaOpenClass javaOpenClass2 = JavaOpenClass.getOpenClass(SecondClassWithMethods.class);
-		JavaOpenClass javaOpenClass3 = JavaOpenClass.getOpenClass(ThirdClassWithMethods.class);
-		assertNotNull(javaOpenClass.getMethod("method2", new IOpenClass[] { JavaOpenClass.INT, JavaOpenClass.DOUBLE }));
-		assertNotNull(
-				javaOpenClass2.getMethod("method2", new IOpenClass[] { JavaOpenClass.INT, JavaOpenClass.DOUBLE }));
+        IMethodCaller methodCaller3 = MethodSearch.getMethodCaller("method2",
+            new IOpenClass[] { JavaOpenClass.STRING, JavaOpenClass.STRING },
+            castFactory,
+            javaOpenClass);
 
-		assertNotNull(MethodSearch.getMethodCaller("method2",
-				new IOpenClass[] { JavaOpenClass.INT, JavaOpenClass.DOUBLE }, castFactory, javaOpenClass2));
+        assertNotNull(methodCaller3);
 
-		assertNotNull(MethodSearch.getMethodCaller("method3",
-				new IOpenClass[] { JavaOpenClass.INT, JavaOpenClass.DOUBLE }, castFactory, javaOpenClass2));
+        IMethodCaller methodCaller4 = MethodSearch.getMethodCaller("method2",
+            new IOpenClass[] { JavaOpenClass.getOpenClass(Integer.class), JavaOpenClass.getOpenClass(Long.class) },
+            castFactory,
+            javaOpenClass);
 
-		assertNotNull(MethodSearch.getMethodCaller("method3",
-				new IOpenClass[] { JavaOpenClass.INT, JavaOpenClass.DOUBLE }, castFactory, javaOpenClass3));
-	}
+        assertNotNull(methodCaller4);
+        
+        javaOpenClass = JavaOpenClass.getOpenClass(org.openl.binding.impl.operator.Comparison.class);
+
+        IMethodCaller methodCaller5 = MethodSearch.getMethodCaller("gt",
+            new IOpenClass[] { JavaOpenClass.getOpenClass(Byte.class), JavaOpenClass.getOpenClass(Integer.class) },
+            castFactory,
+            javaOpenClass);
+        
+        assertNotNull(methodCaller5);
+        
+        IMethodCaller methodCaller6 = MethodSearch.getMethodCaller("gt",
+            new IOpenClass[] { JavaOpenClass.STRING, JavaOpenClass.getOpenClass(Integer.class) },
+            castFactory,
+            javaOpenClass);
+        
+        assertNull(methodCaller6);
+
+    }
+
+    @Test
+    public void testMethodInheritance() {
+        ICastFactory castFactory = getCastFactory();
+
+        JavaOpenClass javaOpenClass = JavaOpenClass.getOpenClass(ClassWithMethods.class);
+        JavaOpenClass javaOpenClass2 = JavaOpenClass.getOpenClass(SecondClassWithMethods.class);
+        JavaOpenClass javaOpenClass3 = JavaOpenClass.getOpenClass(ThirdClassWithMethods.class);
+        assertNotNull(javaOpenClass.getMethod("method2", new IOpenClass[] { JavaOpenClass.INT, JavaOpenClass.DOUBLE }));
+        assertNotNull(
+            javaOpenClass2.getMethod("method2", new IOpenClass[] { JavaOpenClass.INT, JavaOpenClass.DOUBLE }));
+
+        assertNotNull(MethodSearch.getMethodCaller("method2",
+            new IOpenClass[] { JavaOpenClass.INT, JavaOpenClass.DOUBLE },
+            castFactory,
+            javaOpenClass2));
+
+        assertNotNull(MethodSearch.getMethodCaller("method3",
+            new IOpenClass[] { JavaOpenClass.INT, JavaOpenClass.DOUBLE },
+            castFactory,
+            javaOpenClass2));
+
+        assertNotNull(MethodSearch.getMethodCaller("method3",
+            new IOpenClass[] { JavaOpenClass.INT, JavaOpenClass.DOUBLE },
+            castFactory,
+            javaOpenClass3));
+    }
 
 }
