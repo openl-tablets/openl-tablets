676e7d11fde8e4763aa803e556311a385389c50c
Nail Samatov <nsamatov@eisgroup.com>
2017-10-24 13:14:38 +0300

    EPBDS-6968 Resource and folder in JCR repository must be created with same revision as project revision

--- STUDIO/org.openl.rules.repository.jcr/src/org/openl/rules/repository/jcr/ZipJcrRepository.java
+++ STUDIO/org.openl.rules.repository.jcr/src/org/openl/rules/repository/jcr/ZipJcrRepository.java
@@ -197,7 +197,7 @@ public class ZipJcrRepository implements Repository, Closeable, EventListener {
                     } else {
                         artefact = createResource(resourceName, in);
                     }
-                    artefact.commit(user, Integer.parseInt(artefact.getVersion().getRevision()) + 1);
+                    artefact.commit(user, Integer.parseInt(project.getVersion().getRevision()) + 1);
                 }
 
                 entry = zipInputStream.getNextEntry();
@@ -209,7 +209,7 @@ public class ZipJcrRepository implements Repository, Closeable, EventListener {
             while (foldersIterator.hasNext()) {
                 String folder = foldersIterator.next();
                 ArtefactAPI artefact = getArtefact(name + "/" + folder);
-                artefact.commit(user, Integer.parseInt(artefact.getVersion().getRevision()) + 1);
+                artefact.commit(user, Integer.parseInt(project.getVersion().getRevision()) + 1);
             }
 
             project.commit(user, Integer.parseInt(project.getVersion().getRevision()) + 1);
