28cf56a0e0828a5f003aa94ccc00a54c5bbf98d1
Yury Molchan <ymolchan@eisgroup.com>
2017-10-27 15:45:18 +0300

    EPBDS-6918 Add test to check exceptions in a log

---
+++ Util/openl-maven-plugin/it/openl-test-user-error/verify.groovy
@@ -0,0 +1,21 @@
+try {
+    File folder = basedir
+    def lines = new File(folder, 'build.log').readLines('UTF-8')
+
+    // Check that the log hasn't any exceptions
+    assert !lines.any { it.contains('[ERROR]') }
+    assert !lines.any { it.contains('Exception') }
+    assert !lines.any { it.contains('\tat ') }
+    assert !lines.any { it.contains('Caused by:') }
+
+    // Check that all tests are run
+    assert lines.any { it.contains('Running halfTest from the module Simple Rules') }
+
+    // Check total tests statistics
+    assert lines.any { it.contains('Total tests run: 2, Failures: 0, Errors: 0') }
+
+    return true
+} catch (Throwable e) {
+    e.printStackTrace()
+    return false
+}
