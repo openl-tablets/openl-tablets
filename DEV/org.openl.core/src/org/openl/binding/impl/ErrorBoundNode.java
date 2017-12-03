c87d385b909eabceeb9b7a2e2cda61e63cfb442a
Yury Molchan <ymolchan@eisgroup.com>
2017-12-04 00:14:09 +0300

    Remove overriding of default methods from ErrorBoundNode

--- DEV/org.openl.core/src/org/openl/binding/impl/ErrorBoundNode.java
+++ DEV/org.openl.core/src/org/openl/binding/impl/ErrorBoundNode.java
@@ -1,65 +1,21 @@
 package org.openl.binding.impl;
 
-import org.openl.binding.IBoundNode;
 import org.openl.syntax.ISyntaxNode;
-import org.openl.types.IOpenClass;
-import org.openl.types.NullOpenClass;
 import org.openl.vm.IRuntimeEnv;
 
 /**
  * Defines bound node that cannot be bound successfully.
  * 
- * @author snshor
+ * @author snshor, Yury Molchan
  * 
  */
 public class ErrorBoundNode extends ABoundNode {
 
     public ErrorBoundNode(ISyntaxNode node) {
-        super(node, new IBoundNode[0]);
-    }
-
-    /*
-     * (non-Javadoc)
-     * @see org.openl.binding.IBoundNode#assign(java.lang.Object)
-     */
-    @Override
-    public void assign(Object value, IRuntimeEnv env) {
-        throw new UnsupportedOperationException();
+        super(node);
     }
 
     protected Object evaluateRuntime(IRuntimeEnv env) {
         throw new UnsupportedOperationException("You are trying to run openl code with a compile error in it");
     }
-
-    /*
-     * (non-Javadoc)
-     * @see org.openl.binding.IBoundNode#getChild()
-     */
-    @Override
-    public IBoundNode[] getChildren() {
-        return new IBoundNode[0];
-    }
-
-    @Override
-    public IBoundNode getTargetNode() {
-        return null;
-    }
-
-    /*
-     * (non-Javadoc)
-     * @see org.openl.binding.IBoundNode#getType()
-     */
-    public IOpenClass getType() {
-        return NullOpenClass.the;
-    }
-
-    /*
-     * (non-Javadoc)
-     * @see org.openl.binding.IBoundNode#isLvalue()
-     */
-    @Override
-    public boolean isLvalue() {
-        return false;
-    }
-
 }
