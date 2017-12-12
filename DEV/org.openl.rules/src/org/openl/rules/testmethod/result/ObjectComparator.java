2439b0835d9c29ddc550890df8cdb3697452fa8a
Yury Molchan <ymolchan@eisgroup.com>
2017-12-12 19:22:37 +0300

    EPBDS-7026 Test actual values hidden by Object class

--- DEV/org.openl.rules/src/org/openl/rules/testmethod/result/ObjectComparator.java
+++ DEV/org.openl.rules/src/org/openl/rules/testmethod/result/ObjectComparator.java
@@ -1,5 +1,8 @@
 package org.openl.rules.testmethod.result;
 
+import org.openl.rules.convertor.IString2DataConvertor;
+import org.openl.rules.convertor.String2DataConvertorFactory;
+
 /**
  * @author Yury Molchan
  */
@@ -20,8 +23,24 @@ class ObjectComparator extends GenericComparator<Object> {
         if (Object.class.equals(clazz)) {
             return expectedVal.equals(actualVal);
         }
+        Class<?> clazz2 = actualVal.getClass();
+        if (Object.class.equals(clazz2)) {
+            return expectedVal.equals(actualVal);
+        }
+        if (!clazz.equals(clazz2)) {
+            if (String.class.equals(clazz)) {
+                clazz = clazz2;
+                try {
+                    IString2DataConvertor convertor = String2DataConvertorFactory.getConvertor(clazz2);
+                    expectedVal = convertor.parse((String) expectedVal, null);
+                } catch (Exception ex) {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
         TestResultComparator comparator = TestResultComparatorFactory.getComparator(clazz, delta);
         return comparator.compareResult(actualVal, expectedVal);
     }
-
 }
