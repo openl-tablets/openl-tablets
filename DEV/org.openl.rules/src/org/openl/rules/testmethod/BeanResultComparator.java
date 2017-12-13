964fc05aa10816a8d69c0b9fa483731a85075c9b
Yury Molchan <ymolchan@eisgroup.com>
2017-12-14 00:37:10 +0300

    Join two 'for' statements and exctract common code

--- DEV/org.openl.rules/src/org/openl/rules/testmethod/BeanResultComparator.java
+++ DEV/org.openl.rules/src/org/openl/rules/testmethod/BeanResultComparator.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.lang3.exception.ExceptionUtils;
+
 import org.openl.rules.calc.SpreadsheetResult;
 import org.openl.rules.convertor.IString2DataConvertor;
 import org.openl.rules.convertor.String2DataConvertorFactory;
@@ -62,67 +63,51 @@ public class BeanResultComparator implements TestResultComparator {
 
     public boolean isEqual(Object expectedResult, Object actualResult) {
         boolean success = true;
-        if (actualResult == null || expectedResult == null) {
+        comparisonResults = new ArrayList<>();
 
-            for (IOpenField field : fields) {
-                Object actualFieldValue = getFieldValueOrNull(actualResult, field);
-                Object expectedFieldValue = getFieldValueOrNull(expectedResult, field);
+        for (IOpenField field : fields) {
+            Object actualFieldValue = getFieldValueOrNull(actualResult, field);
+            Object expectedFieldValue = getFieldValueOrNull(expectedResult, field);
+            boolean equal = isCompare(actualFieldValue, expectedFieldValue, expectedResult, field);
+            success = success && equal;
 
-                boolean equal = actualFieldValue == null && expectedFieldValue == null;
-                if (!equal) {
-                    success = false;
-                }
-
-                ComparedResult fieldComparisonResults = new ComparedResult();
-                fieldComparisonResults.setFieldName(field.getName());
-                fieldComparisonResults.setActualValue(actualFieldValue);
-                fieldComparisonResults.setExpectedValue(expectedFieldValue);
-                fieldComparisonResults.setStatus(equal ? TestStatus.TR_OK : TestStatus.TR_NEQ);
-                comparisonResults.add(fieldComparisonResults);
-            }
-        } else {
-            comparisonResults = new ArrayList<>();
-            for (IOpenField field : fields) {
-                Object actualFieldValue = getFieldValueOrNull(actualResult, field);
-                Object expectedFieldValue = getFieldValueOrNull(expectedResult, field);
-
-                ComparedResult fieldComparisonResults = new ComparedResult();
-                fieldComparisonResults.setFieldName(field.getName());
-
-
-                // Get delta for field if setted
-                Double columnDelta = null;
-                if (field instanceof PrecisionFieldChain) {
-                    if (((PrecisionFieldChain) field).hasDelta()) {
-                        columnDelta = ((PrecisionFieldChain) field).getDelta();
-                    }
-                }
-                boolean compare = false;
-                try{
-                    // Additional convertation for spreadsheet. It is required for spreadsheet(StubSpreadsheet) created on compilation state.
-                    if (expectedFieldValue != null && (actualFieldValue != null && expectedFieldValue.getClass() != actualFieldValue.getClass()) && expectedResult instanceof SpreadsheetResult && expectedFieldValue instanceof String) {
-                        IString2DataConvertor convertor = String2DataConvertorFactory.getConvertor(actualFieldValue.getClass());
-                        expectedFieldValue = convertor.parse((String) expectedFieldValue, null);
-                    }
-                    TestResultComparator comparator = TestResultComparatorFactory.getComparator(field.getType().getInstanceClass(), columnDelta);
-                    compare = comparator.isEqual(expectedFieldValue, actualFieldValue);
-                }catch(Exception e){
-                }
-
-                fieldComparisonResults.setActualValue(actualFieldValue);
-                fieldComparisonResults.setExpectedValue(expectedFieldValue);
-
-                if (!compare) {
-                    fieldComparisonResults.setStatus(TestStatus.TR_NEQ);
-                    success = false;
-                } else {
-                    fieldComparisonResults.setStatus(TestStatus.TR_OK);
-                }
-                comparisonResults.add(fieldComparisonResults);
-            }
+            ComparedResult fieldComparisonResults = new ComparedResult();
+            fieldComparisonResults.setFieldName(field.getName());
+            fieldComparisonResults.setActualValue(actualFieldValue);
+            fieldComparisonResults.setExpectedValue(expectedFieldValue);
+            fieldComparisonResults.setStatus(equal ? TestStatus.TR_OK : TestStatus.TR_NEQ);
+            comparisonResults.add(fieldComparisonResults);
         }
         return success;
 
+    }
+
+    private boolean isCompare(Object actualFieldValue,
+            Object expectedFieldValue,
+            Object expectedResult,
+            IOpenField field) {
+        // Get delta for field if setted
+        Double columnDelta = null;
+        if (field instanceof PrecisionFieldChain) {
+            if (((PrecisionFieldChain) field).hasDelta()) {
+                columnDelta = ((PrecisionFieldChain) field).getDelta();
+            }
+        }
+
+        try {
+            // Additional convertation for spreadsheet. It is required for spreadsheet(StubSpreadsheet) created on
+            // compilation state.
+            if (expectedFieldValue != null && (actualFieldValue != null && expectedFieldValue
+                .getClass() != actualFieldValue
+                    .getClass()) && expectedResult instanceof SpreadsheetResult && expectedFieldValue instanceof String) {
+                IString2DataConvertor convertor = String2DataConvertorFactory.getConvertor(actualFieldValue.getClass());
+                expectedFieldValue = convertor.parse((String) expectedFieldValue, null);
+            }
+        } catch (Exception e) {
+        }
+        Class<?> clazz = field.getType().getInstanceClass();
+        TestResultComparator comparator = TestResultComparatorFactory.getComparator(clazz, columnDelta);
+        return comparator.isEqual(expectedFieldValue, actualFieldValue);
     }
 
     private Object getFieldValueOrNull(Object result, IOpenField field) {
