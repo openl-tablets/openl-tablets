dfb37cf0bb432002ce0ba75d2f5a406d7cc1ce5c
Nail Samatov <nsamatov@eisgroup.com>
2018-01-09 20:50:50 +0300

    EPBDS-7121 Add validation for ambiguous expectation in the test case.

--- DEV/org.openl.rules/src/org/openl/rules/testmethod/TestMethodBoundNode.java
+++ DEV/org.openl.rules/src/org/openl/rules/testmethod/TestMethodBoundNode.java
@@ -1,13 +1,18 @@
-/**
- * Created Jan 3, 2007
- */
 package org.openl.rules.testmethod;
 
 import org.openl.binding.IBindingContext;
+import org.openl.binding.impl.BindHelper;
 import org.openl.binding.impl.module.ModuleOpenClass;
 import org.openl.rules.data.DataTableBoundNode;
+import org.openl.rules.data.ITable;
 import org.openl.rules.lang.xls.binding.XlsModuleOpenClass;
 import org.openl.rules.lang.xls.syntax.TableSyntaxNode;
+import org.openl.rules.table.IGridTable;
+import org.openl.rules.table.openl.GridCellSourceCodeModule;
+import org.openl.source.IOpenSourceCodeModule;
+import org.openl.syntax.exception.SyntaxNodeException;
+import org.openl.syntax.exception.SyntaxNodeExceptionUtils;
+import org.openl.types.impl.DynamicObject;
 
 /**
  * @author snshor
@@ -35,6 +40,24 @@ public class TestMethodBoundNode extends DataTableBoundNode {
     @Override
     public void finalizeBind(IBindingContext cxt) throws Exception {
         super.finalizeBind(cxt);
+
+        DynamicObject[] testCases = (DynamicObject[]) getField().getData();
+        for (DynamicObject testCase : testCases) {
+            if (testCase.getFieldValue(TestMethodHelper.EXPECTED_ERROR) != null
+                    && testCase.getFieldValue(TestMethodHelper.EXPECTED_RESULT_NAME) != null) {
+                ITable table = getTable();
+                int row = table.getRowIndex(testCase);
+                int column = table.getColumnIndex(TestMethodHelper.EXPECTED_ERROR);
+                IGridTable cell = table.getRowTable(row).getColumn(column);
+                IOpenSourceCodeModule cellSourceCodeModule = new GridCellSourceCodeModule(cell, cxt);
+
+                SyntaxNodeException error = SyntaxNodeExceptionUtils.createError(
+                        "Ambiguous expectation in the test case. Both expected result and expected error have been declared.",
+                        cellSourceCodeModule);
+                getTableSyntaxNode().addError(error);
+                BindHelper.processError(error, cxt);
+            }
+        }
     }
 
     @Override
