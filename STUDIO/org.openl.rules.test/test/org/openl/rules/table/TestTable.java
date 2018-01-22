bbdd79fc95b5179ef1db983386c13c4c3b4eed89
Nail Samatov <nsamatov@eisgroup.com>
2018-01-22 18:04:11 +0300

    EPBDS-7145 Add unit test

---
+++ STUDIO/org.openl.rules.test/test/org/openl/rules/table/TestTable.java
@@ -0,0 +1,30 @@
+package org.openl.rules.table;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.openl.rules.project.instantiation.SimpleProjectEngineFactory;
+import org.openl.rules.testmethod.ProjectHelper;
+import org.openl.rules.testmethod.TestSuiteMethod;
+import org.openl.types.IOpenClass;
+
+public class TestTable {
+    @Test
+    public void canRetrieveTestSuiteForIncorrectFieldArrayAccess() throws Exception {
+        SimpleProjectEngineFactory.SimpleProjectEngineFactoryBuilder<Object> simpleProjectEngineFactoryBuilder = new SimpleProjectEngineFactory.SimpleProjectEngineFactoryBuilder<>();
+        SimpleProjectEngineFactory<Object> simpleProjectEngineFactory = simpleProjectEngineFactoryBuilder
+                .setExecutionMode(false)
+                .setProject("test-resources/org/openl/rules/table")
+                .setModule("EPBDS-7145")
+                .build();
+        IOpenClass openClass = simpleProjectEngineFactory.getCompiledOpenClass().getOpenClassWithErrors();
+
+        TestSuiteMethod[] tests = ProjectHelper.allTesters(openClass);
+        assertNotNull(tests);
+        assertEquals(0, tests.length);
+
+        TestSuiteMethod hiTest = (TestSuiteMethod) openClass.getMethod("hiTest", new IOpenClass[0]);
+        assertNotNull(hiTest);
+        assertTrue(hiTest.isRunmethodTestable());
+    }
+}
