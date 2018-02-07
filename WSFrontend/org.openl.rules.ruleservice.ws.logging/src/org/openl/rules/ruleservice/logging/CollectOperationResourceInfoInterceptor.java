a03ee11eab5e8c08cc79e8b03dc0a80dbf97620c
Marat Kamalov <mkamalov@eisgroup.com>
2018-02-07 18:05:57 +0300

    EPBDS-7184 Add annotation to ignore logging to the storage for annotated methods.
    --HG--
    branch : 5.21.x

--- WSFrontend/org.openl.rules.ruleservice.ws.logging/src/org/openl/rules/ruleservice/logging/CollectOperationResourceInfoInterceptor.java
+++ WSFrontend/org.openl.rules.ruleservice.ws.logging/src/org/openl/rules/ruleservice/logging/CollectOperationResourceInfoInterceptor.java
@@ -11,31 +11,35 @@ import org.apache.cxf.phase.Phase;
 import org.apache.cxf.service.Service;
 import org.apache.cxf.service.invoker.MethodDispatcher;
 import org.apache.cxf.service.model.BindingOperationInfo;
+import org.openl.rules.ruleservice.logging.annotation.IgnoreLogging;
 
 public class CollectOperationResourceInfoInterceptor extends AbstractPhaseInterceptor<Message> {
 
-    public CollectOperationResourceInfoInterceptor(String phase) {
-        super(phase);
-        addBefore(StaxOutInterceptor.class.getName());
-    }
+	public CollectOperationResourceInfoInterceptor(String phase) {
+		super(phase);
+		addBefore(StaxOutInterceptor.class.getName());
+	}
 
-    public CollectOperationResourceInfoInterceptor() {
-        this(Phase.USER_LOGICAL);
-    }
+	public CollectOperationResourceInfoInterceptor() {
+		this(Phase.USER_LOGICAL);
+	}
 
-    @Override
-    public void handleMessage(Message message) throws Fault {
-        RuleServiceLoggingInfo ruleServiceLoggingInfo = RuleServiceLoggingInfoHolder.get();
-        OperationResourceInfo operationResourceInfo = message.getExchange().get(OperationResourceInfo.class);
-        if (operationResourceInfo != null) {
-            ruleServiceLoggingInfo.setServiceMethod(operationResourceInfo.getAnnotatedMethod());
-        } else {
-            BindingOperationInfo bop = message.getExchange().get(BindingOperationInfo.class);
-            MethodDispatcher md = (MethodDispatcher) message.getExchange()
-                .get(Service.class)
-                .get(MethodDispatcher.class.getName());
-            Method method = md.getMethod(bop);
-            ruleServiceLoggingInfo.setServiceMethod(method);
-        }
-    }
+	@Override
+	public void handleMessage(Message message) throws Fault {
+		RuleServiceLoggingInfo ruleServiceLoggingInfo = RuleServiceLoggingInfoHolder.get();
+		OperationResourceInfo operationResourceInfo = message.getExchange().get(OperationResourceInfo.class);
+		if (operationResourceInfo != null) {
+			Method serviceMethod = operationResourceInfo.getAnnotatedMethod();
+			ruleServiceLoggingInfo.setServiceMethod(serviceMethod);
+			if (serviceMethod.isAnnotationPresent(IgnoreLogging.class)) {
+				ruleServiceLoggingInfo.setIgnorable(true);
+			}
+		} else {
+			BindingOperationInfo bop = message.getExchange().get(BindingOperationInfo.class);
+			MethodDispatcher md = (MethodDispatcher) message.getExchange().get(Service.class)
+					.get(MethodDispatcher.class.getName());
+			Method method = md.getMethod(bop);
+			ruleServiceLoggingInfo.setServiceMethod(method);
+		}
+	}
 }
