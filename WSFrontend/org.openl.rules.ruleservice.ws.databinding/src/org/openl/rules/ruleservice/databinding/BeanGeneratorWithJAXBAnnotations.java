934149097fdd978f7306248b1e0959ae5a65186b
Marat Kamalov <mkamalov@eisgroup.com>
2018-02-08 17:27:13 +0300

    EPBDS-7187 REST services does not work with capitalized
    --HG--
    branch : 5.21.x

--- WSFrontend/org.openl.rules.ruleservice.ws.databinding/src/org/openl/rules/ruleservice/databinding/BeanGeneratorWithJAXBAnnotations.java
+++ WSFrontend/org.openl.rules.ruleservice.ws.databinding/src/org/openl/rules/ruleservice/databinding/BeanGeneratorWithJAXBAnnotations.java
@@ -14,25 +14,36 @@ package org.openl.rules.ruleservice.databinding;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
+
+import org.objectweb.asm.AnnotationVisitor;
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.FieldVisitor;
+import org.objectweb.asm.Type;
 
 import net.sf.cglib.core.ClassEmitter;
 import net.sf.cglib.core.CodeEmitter;
 import net.sf.cglib.core.Constants;
+import net.sf.cglib.core.DebuggingClassWriter;
+import net.sf.cglib.core.DefaultGeneratorStrategy;
 import net.sf.cglib.core.EmitUtils;
 import net.sf.cglib.core.Signature;
 import net.sf.cglib.core.TypeUtils;
 
-import org.objectweb.asm.AnnotationVisitor;
-import org.objectweb.asm.ClassVisitor;
-import org.objectweb.asm.Type;
-
 @SuppressWarnings("rawtypes")
 public class BeanGeneratorWithJAXBAnnotations extends net.sf.cglib.beans.BeanGenerator {
 
-    private String xmlTypeName;
+    private static final String $CGLIB_PROP = "$cglib_prop_";
+    
+	private String xmlTypeName;
     private String xmlTypeNamespace;
+    
+    public BeanGeneratorWithJAXBAnnotations() {
+    	setStrategy(new GeneratorStrategyWithJAXBAnnotations());
+	}
 
     public void setXmlTypeName(String xmlTypeName) {
         this.xmlTypeName = xmlTypeName;
@@ -64,9 +75,8 @@ public class BeanGeneratorWithJAXBAnnotations extends net.sf.cglib.beans.BeanGen
     
     public static void add_properties(ClassEmitter ce, String[] names, Type[] types) {
         for (int i = 0; i < names.length; i++) {
-            String fieldName = "$cglib_prop_" + names[i];
+            String fieldName = $CGLIB_PROP + names[i];
             ce.declare_field(Constants.ACC_PRIVATE, fieldName, types[i], null);
-            
             add_property(ce, names[i], types[i], fieldName);
         }
     }
@@ -134,4 +144,30 @@ public class BeanGeneratorWithJAXBAnnotations extends net.sf.cglib.beans.BeanGen
         add_properties(ce, names, types);
         ce.end_class();
     }
+    
+    private static class GeneratorStrategyWithJAXBAnnotations extends DefaultGeneratorStrategy {
+		@Override
+		protected DebuggingClassWriter getClassVisitor() throws Exception {
+			return new DebuggingClassWriterWithJAXBAnnotations();
+		}
+	}
+
+	private static class DebuggingClassWriterWithJAXBAnnotations extends DebuggingClassWriter {
+		public DebuggingClassWriterWithJAXBAnnotations() {
+			super(ClassWriter.COMPUTE_MAXS);
+		}
+
+		@Override
+		public FieldVisitor visitField(int access, String name, String desc, String signature, Object value) {
+			FieldVisitor fv = super.visitField(access, name, desc, signature, value);
+			AnnotationVisitor av1 = fv.visitAnnotation(Type.getDescriptor(XmlElement.class), true);
+			if (name.startsWith($CGLIB_PROP)) {
+				av1.visit("name", name.substring($CGLIB_PROP.length()));	
+			}else {
+				av1.visit("name", name);
+			}
+			av1.visitEnd();
+			return fv;
+		}
+	}
 }
--- WSFrontend/org.openl.rules.ruleservice.ws.databinding/src/org/openl/rules/ruleservice/databinding/JAXRSArgumentWrapperGenerator.java
+++ WSFrontend/org.openl.rules.ruleservice.ws.databinding/src/org/openl/rules/ruleservice/databinding/JAXRSArgumentWrapperGenerator.java
@@ -15,53 +15,52 @@ import java.util.Map;
 
 public class JAXRSArgumentWrapperGenerator {
 
-    Map<String, Class<?>> props = new HashMap<String, Class<?>>();
-    String xmlTypeName;
-    String xmlTypeNamespace;
+	Map<String, Class<?>> props = new HashMap<String, Class<?>>();
+	String xmlTypeName;
+	String xmlTypeNamespace;
 
-    public JAXRSArgumentWrapperGenerator() {
-    }
-    
-    public JAXRSArgumentWrapperGenerator(String xmlTypeName, String namespace) {
-        this.xmlTypeName = xmlTypeName;
-        this.xmlTypeNamespace = namespace;
-    }
-    
-    public String getXmlTypeName() {
-        return xmlTypeName;
-    }
-    
-    public String getXmlTypeNamespace() {
-        return xmlTypeNamespace;
-    }
-    
-    public void setXmlTypeName(String xmlTypeName) {
-        this.xmlTypeName = xmlTypeName;
-    }
-    
-    public void setXmlTypeNamespace(String xmlTypeNamespace) {
-        this.xmlTypeNamespace = xmlTypeNamespace;
-    }
-    
-    public void addProperty(String name, Class<?> type) {
-        props.put(name, type);
-    }
+	public JAXRSArgumentWrapperGenerator() {
+	}
 
-    public Class<?> generateClass() throws Exception {
-        return generateClass(Thread.currentThread().getContextClassLoader());
-    }
+	public JAXRSArgumentWrapperGenerator(String xmlTypeName, String namespace) {
+		this.xmlTypeName = xmlTypeName;
+		this.xmlTypeNamespace = namespace;
+	}
 
-    public Class<?> generateClass(ClassLoader classLoader) throws Exception {
-        BeanGeneratorWithJAXBAnnotations beanGenerator = new BeanGeneratorWithJAXBAnnotations();
-        for (String name : props.keySet()) {
-            beanGenerator.addProperty(name, props.get(name));
-        }
-        beanGenerator.setClassLoader(classLoader);
-        beanGenerator.setXmlTypeName(xmlTypeName);
-        beanGenerator.setXmlTypeNamespace(xmlTypeNamespace);
-        
-        Class<?> generatedClass = (Class<?>) beanGenerator.createClass();
-        return generatedClass;
-    }
+	public String getXmlTypeName() {
+		return xmlTypeName;
+	}
 
+	public String getXmlTypeNamespace() {
+		return xmlTypeNamespace;
+	}
+
+	public void setXmlTypeName(String xmlTypeName) {
+		this.xmlTypeName = xmlTypeName;
+	}
+
+	public void setXmlTypeNamespace(String xmlTypeNamespace) {
+		this.xmlTypeNamespace = xmlTypeNamespace;
+	}
+
+	public void addProperty(String name, Class<?> type) {
+		props.put(name, type);
+	}
+
+	public Class<?> generateClass() throws Exception {
+		return generateClass(Thread.currentThread().getContextClassLoader());
+	}
+
+	public Class<?> generateClass(ClassLoader classLoader) throws Exception {
+		BeanGeneratorWithJAXBAnnotations beanGenerator = new BeanGeneratorWithJAXBAnnotations();
+		for (String name : props.keySet()) {
+			beanGenerator.addProperty(name, props.get(name));
+		}
+		beanGenerator.setClassLoader(classLoader);
+		beanGenerator.setXmlTypeName(xmlTypeName);
+		beanGenerator.setXmlTypeNamespace(xmlTypeNamespace);
+
+		Class<?> generatedClass = (Class<?>) beanGenerator.createClass();
+		return generatedClass;
+	}
 }
