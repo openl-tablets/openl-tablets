a6399255c70dd2c72ba903241a761e7cfef235a0
Nail Samatov <nsamatov@eisgroup.com>
2018-04-20 21:02:56 +0300

    EPBDS-7372 Reformat sources

--- DEV/org.openl.rules/src/org/openl/rules/dt/storage/StorageInfo.java
+++ DEV/org.openl.rules/src/org/openl/rules/dt/storage/StorageInfo.java
@@ -4,42 +4,41 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class StorageInfo {
-	private int numberOfSpaces = 0;
-	private int numberOfFormulas = 0;
-	private int numberOfElses = 0;
+    private int numberOfSpaces = 0;
+    private int numberOfFormulas = 0;
+    private int numberOfElses = 0;
 
-	private Map<Object, Integer> uniqueIndex = new HashMap<>();
+    private Map<Object, Integer> uniqueIndex = new HashMap<>();
 
-	public int getNumberOfSpaces() {
-		return numberOfSpaces;
-	}
+    public int getNumberOfSpaces() {
+        return numberOfSpaces;
+    }
 
-	public int getNumberOfFormulas() {
-		return numberOfFormulas;
-	}
+    public int getNumberOfFormulas() {
+        return numberOfFormulas;
+    }
 
-	public Map<Object, Integer> getUniqueIndex() {
-		return uniqueIndex;
-	}
+    public Map<Object, Integer> getUniqueIndex() {
+        return uniqueIndex;
+    }
 
-	public int getTotalNumberOfUniqueValues()
-	{
-		return  uniqueIndex.size()   + numberOfFormulas + (numberOfSpaces > 0 ? 1 : 0) + (numberOfElses  > 0 ? 1 : 0);
-	}
+    public int getTotalNumberOfUniqueValues() {
+        return uniqueIndex.size() + numberOfFormulas + (numberOfSpaces > 0 ? 1 : 0) + (numberOfElses > 0 ? 1 : 0);
+    }
 
-	public int getNumberOfElses() {
-		return numberOfElses;
-	}
+    public int getNumberOfElses() {
+        return numberOfElses;
+    }
 
-	public void addSpaceIndex() {
-		numberOfSpaces++;
-	}
+    public void addSpaceIndex() {
+        numberOfSpaces++;
+    }
 
-	public void addElseIndex() {
-		numberOfElses++;
-	}
-	
-	public void addFormulaIndex() {
-		numberOfFormulas++;
-	}
+    public void addElseIndex() {
+        numberOfElses++;
+    }
+
+    public void addFormulaIndex() {
+        numberOfFormulas++;
+    }
 }
