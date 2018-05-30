2f12e64e14ff23b863e56fa6b4bf333133bc54ba
Yury Molchan <ymolchan@eisgroup.com>
2018-05-30 12:40:50 +0300

    EPBDS-7531 More descriptive log
    --HG--
    branch : 5.21.x

--- DEV/org.openl.commons/src/org/openl/util/db/JDBCDriverRegister.java
+++ DEV/org.openl.commons/src/org/openl/util/db/JDBCDriverRegister.java
@@ -1,5 +1,8 @@
 package org.openl.util.db;
 
+import java.net.URL;
+import java.sql.Driver;
+
 import org.openl.util.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,13 +25,14 @@ public class JDBCDriverRegister {
         registerDrivers(drivers);
 
         // Defaults drivers
-        registerDrivers("com.mysql.jdbc.Driver",
+        registerDrivers("org.h2.Driver",
+            "org.hsqldb.jdbcDriver",
+            "org.postgresql.Driver",
             "org.mariadb.jdbc.Driver",
+            "com.mysql.cj.jdbc.Driver",
+            "com.mysql.jdbc.Driver",
             "com.ibm.db2.jcc.DB2Driver",
             "oracle.jdbc.OracleDriver",
-            "org.postgresql.Driver",
-            "org.hsqldb.jdbcDriver",
-            "org.h2.Driver",
             "com.microsoft.sqlserver.jdbc.SQLServerDriver");
     }
 
@@ -38,12 +42,39 @@ public class JDBCDriverRegister {
         }
         Logger log = LoggerFactory.getLogger(JDBCDriverRegister.class);
         for (String driver : drivers) {
+            Class<?> aClass;
             try {
-                Class.forName(driver);
-                log.info("JDBC Driver: '{}' - OK.", driver);
+                aClass = Class.forName(driver);
             } catch (ClassNotFoundException e) {
                 log.info("JDBC Driver: '{}' - NOT FOUND.", driver);
+                continue;
             }
+            String path = getPath(aClass);
+            try {
+                Driver dr = Driver.class.cast(aClass.newInstance());
+                int majorVersion = dr.getMajorVersion();
+                int minorVersion = dr.getMinorVersion();
+
+                log.info("JDBC Driver: '{}' - OK.\n      Path: {}\n      Version: {}.{}",
+                    driver,
+                    path,
+                    majorVersion,
+                    minorVersion);
+            } catch (Exception e) {
+                log.info("JDBC Driver: '{}' - ERROR.\n      Path: {}", driver, path, e);
+            }
+        }
+    }
+
+    private static String getPath(Class<?> aClass) {
+        try {
+            URL resource = aClass.getResource(aClass.getSimpleName() + ".class");
+            if (resource != null) {
+                return resource.toString();
+            }
+            return "UNKNOWN";
+        } catch (Exception ignored) {
+            return "UNKNOWN, because of an exception has been happened.";
         }
     }
 
