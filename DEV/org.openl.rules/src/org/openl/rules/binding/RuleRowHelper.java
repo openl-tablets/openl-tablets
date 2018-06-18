16ddbf07b37c8c91791254cef8a3abc500a3fcaa
Marat Kamalov <mkamalov@eisgroup.com>
2018-06-18 19:26:18 +0300

    Refactoring
    --HG--
    branch : 5.21.x

--- DEV/org.openl.rules/src/org/openl/rules/binding/RuleRowHelper.java
+++ DEV/org.openl.rules/src/org/openl/rules/binding/RuleRowHelper.java
@@ -59,7 +59,7 @@ import org.openl.vm.IRuntimeEnv;
 
 public class RuleRowHelper {
 
-    public static final String COMMETARY = "//";
+    private static final String COMMENTARY = "//";
     public static final String ARRAY_ELEMENTS_SEPARATOR_ESCAPER = "\\";
     public static final String ARRAY_ELEMENTS_SEPARATOR = ",";
     public static final String CONSTRUCTOR = "constructor";
@@ -278,7 +278,7 @@ public class RuleRowHelper {
                         if ((theCell.getAbsoluteRegion().getTop() != cell.getAbsoluteRegion().getTop() || theCell
                             .getAbsoluteRegion()
                             .getLeft() != cell.getAbsoluteRegion().getLeft()) && cell.getStringValue() != null) {
-                            if (!cell.getStringValue().startsWith(COMMETARY)) {
+                            if (!cell.getStringValue().startsWith(COMMENTARY)) {
                                 IGridTable cellTable = getTopLeftCellFromMergedRegion(table.getSource());
                                 throw SyntaxNodeExceptionUtils.createError(
                                     "Table structure is wrong. More than one cell with data found where only one cell is expected.",
