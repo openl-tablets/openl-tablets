eb628b85413680294def8e04d7779450aa14fa4e
Yury Molchan <ymolchan@eisgroup.com>
2018-07-07 18:56:32 +0300

    Fix NPE
    --HG--
    branch : 5.21.x

--- DEV/org.openl.core/src/org/openl/syntax/exception/formatter/NoClassDefFoundErrorFormatter.java
+++ DEV/org.openl.core/src/org/openl/syntax/exception/formatter/NoClassDefFoundErrorFormatter.java
@@ -4,8 +4,9 @@ package org.openl.syntax.exception.formatter;
 public class NoClassDefFoundErrorFormatter implements ExceptionMessageFormatter {
 
     public String format(Throwable error) {
-        if (error instanceof NoClassDefFoundError) {
-            return String.format("Can't load type '%s'!", error.getCause().getMessage());
+        Throwable cause = error.getCause();
+        if (error instanceof NoClassDefFoundError && cause != null) {
+            return String.format("Can't load type '%s'!", cause.getMessage());
         }
         return error.getMessage();
         
