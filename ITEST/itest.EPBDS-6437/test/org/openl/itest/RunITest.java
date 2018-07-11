69aba5d9dade82e29bd892433ae361769f8ad939
Yury Molchan <ymolchan@eisgroup.com>
2018-07-11 14:45:59 +0300

    EPBDS-6437 ITEST cleanup and improvements

--- ITEST/itest.EPBDS-6437/test/org/openl/itest/RunITest.java
+++ ITEST/itest.EPBDS-6437/test/org/openl/itest/RunITest.java
@@ -1,14 +1,25 @@
 package org.openl.itest;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.TimeZone;
+
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openl.generated.beans.Vehicle;
-import org.openl.itest.core.client.RestClientFactory;
-import org.openl.itest.core.client.SoapClientProxyFactory;
-import org.openl.itest.core.jetty.JettyManager;
-import org.openl.itest.core.jetty.JettyServer;
+import org.openl.itest.core.RestClientFactory;
+import org.openl.itest.core.SoapClientFactory;
+import org.openl.itest.core.JettyServer;
 import org.openl.rules.context.DefaultRulesRuntimeContext;
 import org.openl.rules.context.IRulesRuntimeContext;
 import org.openl.rules.itest.rules.Service;
@@ -16,38 +27,33 @@ import org.openl.rules.variation.JXPathVariation;
 import org.openl.rules.variation.VariationException;
 import org.openl.rules.variation.VariationsPack;
 import org.openl.rules.variation.VariationsResult;
-import org.openl.rules.workspace.deploy.ProductionRepositoryDeployer;
-import org.springframework.http.*;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
-
-import java.io.File;
-import java.util.*;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
 public class RunITest {
 
-    private static final String CalVehicleYear_REQUEST = "{\"runtimeContext\": {},"
-            + "\t\"v\": {\n"
-            + "\t\t\"modelYear\": 2007,\n"
-            + "\t\t\"vehEffectiveYear\": \"2016-12-31T22:00:00\"}}";
+    private static final String CalVehicleYear_REQUEST = "{\"runtimeContext\": {}," + "\t\"v\": {\n" + "\t\t\"modelYear\": 2007,\n" + "\t\t\"vehEffectiveYear\": \"2016-12-31T22:00:00\"}}";
 
     private static final Locale DEFAULT_LOCALE = Locale.getDefault();
     private static final TimeZone DEFAULT_TIMEZONE = TimeZone.getDefault();
     private static final HttpHeaders DEF_HTTPHEADERS;
 
+    private static JettyServer server;
     private static String baseURI;
-
-    private RestTemplate rest;
-    private Service soapClient;
 
     static {
         DEF_HTTPHEADERS = new HttpHeaders();
         DEF_HTTPHEADERS.setContentType(MediaType.APPLICATION_JSON);
         DEF_HTTPHEADERS.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
     }
+
+    private RestTemplate rest;
+    private Service soapClient;
 
     @BeforeClass
     public static void setUp() throws Exception {
@@ -57,50 +63,41 @@ public class RunITest {
         TimeZone defaultTimeZone = TimeZone.getTimeZone("Europe/Helsinki");
         TimeZone.setDefault(defaultTimeZone);
 
-        ProductionRepositoryDeployer deployer = new ProductionRepositoryDeployer();
-        File rules = new File("target/openl-itest-rules.zip");
-        deployer.deploy(rules, "test-deployment.properties");
-
-        JettyServer server = JettyManager.createServer(RunITest.class.getClassLoader(), "target/ws");
-
-        System.setProperty("production-repository.factory", "org.openl.rules.repository.file.FileRepository");
-        System.setProperty("version-in-deployment-name", "true");
-        System.setProperty("ruleservice.datasource.type", "jcr");
-        System.setProperty("production-repository.uri", "target/openl-test/openl-repository");
-
-        server.start();
-        baseURI = server.getBaseURI();
+        server = new JettyServer(true);
+        baseURI = server.start();
     }
 
     @AfterClass
     public static void tearDown() throws Exception {
         try {
-            JettyManager.removeAndStopServer();
+            server.stop();
         } finally {
             Locale.setDefault(DEFAULT_LOCALE);
             TimeZone.setDefault(DEFAULT_TIMEZONE);
-            System.clearProperty("production-repository.factory");
-            System.clearProperty("version-in-deployment-name");
-            System.clearProperty("ruleservice.datasource.type");
-            System.clearProperty("production-repository.uri");
         }
+    }
+
+    private static Date toDate(int year, int month, int dayOfMonth, int hour, TimeZone tz) {
+        Calendar calendar = Calendar.getInstance(tz);
+        calendar.set(year, month - 1, dayOfMonth, hour, 0, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+        return calendar.getTime();
     }
 
     @Before
     public void before() {
         rest = new RestClientFactory(baseURI).setSupportVariations(true).create();
 
-        soapClient = new SoapClientProxyFactory<>(baseURI + "/EPBDS-6437", Service.class)
-                .setSupportVariations(true)
-                .createProxy();
+        soapClient = new SoapClientFactory<>(baseURI + "/EPBDS-6437", Service.class).setSupportVariations(true)
+            .createProxy();
     }
 
     @Test
     public void testCalVehicleYear_StringRequest_JSON() {
         HttpEntity<String> request = new HttpEntity<>(CalVehicleYear_REQUEST, DEF_HTTPHEADERS);
 
-        ResponseEntity<String> responseEntity = rest.exchange(baseURI + "/REST/EPBDS-6437/calVehicleYear",
-                HttpMethod.POST, request, String.class);
+        ResponseEntity<String> responseEntity = rest
+            .exchange(baseURI + "/REST/EPBDS-6437/calVehicleYear", HttpMethod.POST, request, String.class);
         assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
         assertEquals("9", responseEntity.getBody());
     }
@@ -113,8 +110,8 @@ public class RunITest {
 
         HttpEntity<?> request = new HttpEntity<>(requestBody, DEF_HTTPHEADERS);
 
-        ResponseEntity<String> responseEntity = rest.exchange(baseURI + "/REST/EPBDS-6437/calVehicleYear",
-                HttpMethod.POST, request, String.class);
+        ResponseEntity<String> responseEntity = rest
+            .exchange(baseURI + "/REST/EPBDS-6437/calVehicleYear", HttpMethod.POST, request, String.class);
         assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
         assertEquals("9", responseEntity.getBody());
     }
@@ -126,8 +123,8 @@ public class RunITest {
 
         HttpEntity<?> request = new HttpEntity<>(rulesRuntimeContext, DEF_HTTPHEADERS);
 
-        ResponseEntity<String> responseEntity = rest.exchange(baseURI + "/REST/EPBDS-6437/checkRulesModule",
-                HttpMethod.POST, request, String.class);
+        ResponseEntity<String> responseEntity = rest
+            .exchange(baseURI + "/REST/EPBDS-6437/checkRulesModule", HttpMethod.POST, request, String.class);
         assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
         assertEquals("20170101", responseEntity.getBody());
     }
@@ -136,8 +133,8 @@ public class RunITest {
     public void testCheckRulesModule_usingUTCTimezone_JSON() {
         HttpEntity<?> request = new HttpEntity<>("{ \"requestDate\": \"2017-12-31T22:00:00Z\" }", DEF_HTTPHEADERS);
 
-        ResponseEntity<String> responseEntity = rest.exchange(baseURI + "/REST/EPBDS-6437/checkRulesModule",
-                HttpMethod.POST, request, String.class);
+        ResponseEntity<String> responseEntity = rest
+            .exchange(baseURI + "/REST/EPBDS-6437/checkRulesModule", HttpMethod.POST, request, String.class);
         assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
         assertEquals("20180101", responseEntity.getBody());
     }
@@ -146,8 +143,8 @@ public class RunITest {
     public void testCheckRulesModule_usingUTCTimestamp_JSON() {
         HttpEntity<?> request = new HttpEntity<>("{ \"requestDate\": 1514757600000 }", DEF_HTTPHEADERS);
 
-        ResponseEntity<String> responseEntity = rest.exchange(baseURI + "/REST/EPBDS-6437/checkRulesModule",
-                HttpMethod.POST, request, String.class);
+        ResponseEntity<String> responseEntity = rest
+            .exchange(baseURI + "/REST/EPBDS-6437/checkRulesModule", HttpMethod.POST, request, String.class);
         assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
         assertEquals("20180101", responseEntity.getBody());
     }
@@ -197,10 +194,14 @@ public class RunITest {
 
         Vehicle v = new Vehicle(2007, toDate(2016, 12, 31, 22, TimeZone.getDefault()));
         VariationsPack variationPack = new VariationsPack();
-        variationPack.addVariation(new JXPathVariation("variation_0", 0, "/vehEffectiveYear",
-                toDate(2016, 12, 31, 22, TimeZone.getDefault())));
-        variationPack.addVariation(new JXPathVariation("variation_1", 0, "/vehEffectiveYear",
-                toDate(2016, 12, 31, 22, TimeZone.getTimeZone("UTC"))));
+        variationPack.addVariation(new JXPathVariation("variation_0",
+            0,
+            "/vehEffectiveYear",
+            toDate(2016, 12, 31, 22, TimeZone.getDefault())));
+        variationPack.addVariation(new JXPathVariation("variation_1",
+            0,
+            "/vehEffectiveYear",
+            toDate(2016, 12, 31, 22, TimeZone.getTimeZone("UTC"))));
 
         VariationsResult<Integer> result = soapClient.calVehicleYear(context, v, variationPack);
         assertFalse(result.getVariationResults().isEmpty());
@@ -209,13 +210,6 @@ public class RunITest {
         assertEquals(new Integer(9), result.getVariationResults().get("Original calculation"));
 
         assertTrue(result.getVariationFailures().isEmpty());
-    }
-
-    private static Date toDate(int year, int month, int dayOfMonth, int hour, TimeZone tz) {
-        Calendar calendar = Calendar.getInstance(tz);
-        calendar.set(year, month - 1, dayOfMonth, hour, 0, 0);
-        calendar.set(Calendar.MILLISECOND, 0);
-        return calendar.getTime();
     }
 
 }
--- ITEST/server-core/src/org/openl/itest/core/RestClientFactory.java
+++ ITEST/server-core/src/org/openl/itest/core/RestClientFactory.java
@@ -1,23 +1,54 @@
-package org.openl.itest.core.client;
+package org.openl.itest.core;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Collections;
+
 import org.openl.rules.ruleservice.databinding.JacksonObjectMapperFactoryBean;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.ResponseErrorHandler;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.DefaultUriTemplateHandler;
 import org.springframework.web.util.UriTemplateHandler;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 /**
- * @author Vladyslav Pikus
+ * @author Vladyslav Pikus, Yury Molchan
  */
 public class RestClientFactory {
+
+    private static final ResponseErrorHandler NO_ERROR_HANDLER = new ResponseErrorHandler() {
+        @Override
+        public boolean hasError(ClientHttpResponse response) {
+            return false;
+        }
+
+        @Override
+        public void handleError(ClientHttpResponse response) {
+            // To prevent exception throwing and to provide ability to check error codes
+        }
+    };
+
+    private static final HttpHeaders JSON_HEADERS;
+    static {
+        JSON_HEADERS = new HttpHeaders();
+        JSON_HEADERS.setContentType(MediaType.APPLICATION_JSON);
+        JSON_HEADERS.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+    }
 
     private final String address;
     private boolean supportVariations = false;
 
     public RestClientFactory(String address) {
         this.address = address;
+    }
+
+    public static <T> HttpEntity<T> request(T body) {
+        return new HttpEntity<>(body, JSON_HEADERS);
     }
 
     public RestClientFactory setSupportVariations(boolean supportVariations) {
@@ -35,6 +66,7 @@ public class RestClientFactory {
         }
 
         rest.setUriTemplateHandler(createUriHandler());
+        rest.setErrorHandler(NO_ERROR_HANDLER);
         return rest;
     }
 
