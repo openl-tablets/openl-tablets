4570b2888e78a1681c13868a267e41590e71a9d2
Yury Molchan <ymolchan@eisgroup.com>
2018-08-09 20:40:13 +0300

    EPBDS-7709 Use Module name as a package for grouping test reports on Jenkins

--- Util/openl-maven-plugin/it/openl-tests-big-testsuite/verify.groovy
+++ Util/openl-maven-plugin/it/openl-tests-big-testsuite/verify.groovy
@@ -4,7 +4,7 @@ try {
 
     assert logs.contains('Total tests run: 1000, Failures: 0, Errors: 0')
 
-    def junitReport = new File(basedir, "target/openl-test-reports/TEST-OpenL-Big_SR-case1\$Test\$0.xml")
+    def junitReport = new File(basedir, "target/openl-test-reports/TEST-OpenL.Big_SR.case1\$Test\$0.xml")
     assert junitReport.exists()
     assert junitReport.readLines().get(1).contains("tests=\"1000\" skipped=\"0\" failures=\"0\" errors=\"0\"")
 
