9aa796daf23a57974c677dcb38ccfca9c6806f75
Yury Molchan <ymolchan@eisgroup.com>
2018-08-16 13:19:28 +0300

    EPBDS-7727 Test 10k rows Data table

--- Util/openl-maven-plugin/it/openl-tests-big-testsuite/verify.groovy
+++ Util/openl-maven-plugin/it/openl-tests-big-testsuite/verify.groovy
@@ -2,11 +2,15 @@ try {
     File folder = basedir
     def logs = new File(folder, 'build.log').text
 
-    assert logs.contains('Total tests run: 1000, Failures: 0, Errors: 0')
+    assert logs.contains('Total tests run: 11000, Failures: 0, Errors: 0')
 
-    def junitReport = new File(basedir, "target/openl-test-reports/TEST-OpenL.Big_SR.case1\$Test\$0.xml")
-    assert junitReport.exists()
-    assert junitReport.readLines().get(1).contains("tests=\"1000\" skipped=\"0\" failures=\"0\" errors=\"0\"")
+    def junitReport1 = new File(basedir, "target/openl-test-reports/TEST-OpenL.Big_SR.bigSRTest.xml")
+    assert junitReport1.exists()
+    assert junitReport1.readLines().get(1).contains("tests=\"1000\" skipped=\"0\" failures=\"0\" errors=\"0\"")
+
+    def junitReport2 = new File(basedir, "target/openl-test-reports/TEST-OpenL.Big_Data.bigDataTest.xml")
+    assert junitReport2.exists()
+    assert junitReport2.readLines().get(1).contains("tests=\"10000\" skipped=\"0\" failures=\"0\" errors=\"0\"")
 
     return true
 } catch(Throwable e) {
