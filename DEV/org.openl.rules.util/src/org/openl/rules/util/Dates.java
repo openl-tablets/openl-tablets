bd2db002455e71f3e5776b2ce80587459fe9fc04
Yury Molchan <ymolchan@eisgroup.com>
2018-08-22 21:51:31 +0300

    EPBDS-7754 Implement date() and toDate(text, pattern)

--- DEV/org.openl.rules.util/src/org/openl/rules/util/Dates.java
+++ DEV/org.openl.rules.util/src/org/openl/rules/util/Dates.java
@@ -22,10 +22,23 @@ import org.openl.rules.util.dates.DateInterval.Unit;
 public final class Dates {
 
     /**
+     * Converts a string to a date using a pattern.
+     *
+     * @see SimpleDateFormat
+     */
+    public static Date date(int year, int month, int day) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month - 1, day, 0, 0, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+        calendar.setLenient(false); // Strict matching
+        return calendar.getTime();
+    }
+
+    /**
      * Converts a date to a string using a default pattern. The default pattern is system and setting dependent.
      */
     public static String toString(Date date) {
-        return toString(date, null);
+        return toString(date, "MM/dd/yyyy");
     }
 
     /**
@@ -34,19 +47,27 @@ public final class Dates {
      * @see SimpleDateFormat
      */
     public static String toString(Date date, String pattern) {
-        return date == null ? null
-                            : new SimpleDateFormat(pattern == null ? "MM/dd/yyyy" : pattern, Locale.US).format(date);
+        return date == null ? null : getDateFormat(pattern).format(date);
     }
 
     /**
      * Converts a string to a date using a default pattern. The default pattern is system and setting dependent.
      */
     public static Date toDate(String str) throws ParseException {
-        return (str == null || str.trim().isEmpty()) ? null : getDateFormat().parse(str);
+        return toDate(str, "MM/dd/yy");
     }
 
-    private static DateFormat getDateFormat() {
-        DateFormat df = DateFormat.getDateInstance(DateFormat.SHORT, Locale.US);
+    /**
+     * Converts a string to a date using a pattern.
+     * 
+     * @see SimpleDateFormat
+     */
+    public static Date toDate(String str, String pattern) throws ParseException {
+        return (str == null || str.trim().isEmpty()) ? null : getDateFormat(pattern).parse(str);
+    }
+
+    private static DateFormat getDateFormat(String pattern) {
+        DateFormat df = new SimpleDateFormat(pattern == null || pattern.trim().isEmpty() ? "MM/dd/yyyy" : pattern, Locale.US);
         df.setLenient(false); // Strict matching
         df.getCalendar().set(0, 0, 0, 0, 0, 0); // at
         df.getCalendar().set(Calendar.MILLISECOND, 0);
--- DEV/org.openl.rules.util/test/org/openl/rules/util/DatesTest.java
+++ DEV/org.openl.rules.util/test/org/openl/rules/util/DatesTest.java
@@ -12,6 +12,31 @@ public class DatesTest {
 
 
     @Test
+    public void testDate() {
+        assertNull(Dates.toString(null));
+        assertEquals(new Date(-1899, 0, 1), Dates.date(1,1,1));
+        assertEquals(new Date(118, 6, 12), Dates.date(2018,7,12));
+        assertEquals(new Date(80, 6, 12), Dates.date(1980,7,12));
+        assertEquals(new Date(-1820, 6, 12), Dates.date(80,7,12));
+        assertEquals(new Date(116, 1, 29), Dates.date(2016,2,29));
+    }
+
+    @Test(expected = Exception.class)
+    public void testDateWrongMonth() {
+        Dates.date(2018,13,1);
+    }
+
+    @Test(expected = Exception.class)
+    public void testDateWrongDay() {
+        Dates.date(2018,2,29);
+    }
+
+    @Test(expected = Exception.class)
+    public void testDateWrongYear() {
+        Dates.date(0,1,1);
+    }
+
+    @Test
     public void testToString() {
         assertNull(Dates.toString(null));
         assertEquals("07/12/1980", Dates.toString(new Date(80, 6, 12, 23, 59)));
@@ -26,13 +51,40 @@ public class DatesTest {
         assertNull(Dates.toDate(""));
         assertNull(Dates.toDate(" "));
         assertNull(Dates.toDate("  \t  "));
+
+        assertEquals(new Date(50, 11, 31), Dates.toDate("12/31/50"));
         assertEquals(new Date(80, 6, 12), Dates.toDate("7/12/80"));
+        assertEquals(new Date(138, 6, 12), Dates.toDate("7/12/38"));
+        assertEquals(new Date(-1820, 6, 12), Dates.toDate("7/12/080"));
         assertEquals(new Date(80, 6, 12), Dates.toDate("07/12/1980"));
+
+        assertNull(Dates.toDate(null, null));
+        assertNull(Dates.toDate("", ""));
+        assertNull(Dates.toDate(" ", " "));
+        assertNull(Dates.toDate("  \t  ", "  \t"));
+
+        assertEquals(new Date(80, 6, 12), Dates.toDate("07/12/1980", null));
+        assertEquals(new Date(80, 6, 12), Dates.toDate("07/12/1980", ""));
+        assertEquals(new Date(80, 6, 12), Dates.toDate("07/12/1980", " "));
+        assertEquals(new Date(80, 6, 12), Dates.toDate("07/12/1980", "  \t"));
+
+        assertEquals(new Date(80, 6, 12), Dates.toDate("7/12/80", "MM/dd/yy"));
+        assertEquals(new Date(-1820, 6, 12), Dates.toDate("7/12/80", "M/d/yyyy"));
+        assertEquals(new Date(-1820, 6, 12), Dates.toDate("07/12/0080", "M/d/yyyy"));
+        assertEquals(new Date(80, 6, 12), Dates.toDate("Date is: 12 Jul 1980 [+]", "'Date is: 'dd MMM yyyy"));
+        assertEquals(new Date(80, 6, 12), Dates.toDate("Date is: 12 Jul 1980 year", "'Date is: 'dd MMM yyyy 'year'"));
+        assertEquals(new Date(80, 0, 1), Dates.toDate("Date: 1980 year", "'Date: 'yyyy 'year'"));
+        assertEquals(new Date(70, 0, 19), Dates.toDate("Date: 19 days from the 1th January 1970", "'Date: 'd 'days'"));
     }
 
     @Test(expected = Exception.class)
     public void testToDateLetter() throws Exception {
         Dates.toDate("13/13/2013");
+    }
+
+    @Test(expected = Exception.class)
+    public void testToDateWrongPattern() throws Exception {
+        Dates.toDate("12/12/2013", "a");
     }
 
     private static final Date DEF_DATE = new Date();
