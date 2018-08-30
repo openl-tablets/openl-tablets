8c2da94e84a81097c3fca0a0a762c708f78ec5f0
Nail Samatov <nsamatov@eisgroup.com>
2018-08-30 21:41:14 +0300

    EPBDS-7671 openl-maven-plugin don't package dependencies if runtime scope is defined in transitive dependencies
    --HG--
    branch : 5.21.x

---
+++ Util/openl-maven-plugin/it/openl-multimodule/openl-dependency-c/src/main/java/org/openl/internal/multimodule/TypeC.java
@@ -0,0 +1,15 @@
+package org.openl.internal.multimodule;
+
+import org.openl.meta.DoubleValue;
+
+public class TypeC {
+    private DoubleValue value;
+
+    public DoubleValue getValue() {
+        return value;
+    }
+
+    public void setValue(DoubleValue value) {
+        this.value = value;
+    }
+}
