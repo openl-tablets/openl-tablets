5012b620665e2408658c7983e5d59bac4b9a9cc7
Vladyslav Pikus <vpikus@eisgroup.com>
2018-09-20 19:43:39 +0300

    EPBDS-7819 added ITests
    --HG--
    branch : 5.21.x

---
+++ ITEST/itest.EPBDS-7819/test/org/openl/itest/RunITest.java
@@ -0,0 +1,112 @@
+package org.openl.itest;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openl.itest.core.JettyServer;
+import org.openl.itest.core.RestClientFactory;
+import org.openl.rules.calc.SpreadsheetResult;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+import org.w3c.dom.Node;
+import org.xml.sax.InputSource;
+
+public class RunITest {
+
+    private static JettyServer server;
+    private static String baseURI;
+
+    private RestTemplate rest;
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        server = new JettyServer();
+        baseURI = server.start();
+    }
+
+    @AfterClass
+    public static void tearDown() throws Exception {
+        server.stop();
+    }
+
+    @Before
+    public void before() {
+        rest = new RestClientFactory(baseURI).create();
+    }
+
+    @Test
+    public void testSpreadsheetResultWadlSchema() throws XPathExpressionException {
+        ResponseEntity<String> response = rest.getForEntity("/wadl-and-spreadsheetresult?_wadl", String.class);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+
+        String result = response.getBody();
+        assertNotNull(result);
+        //cleanup xml
+        result = result.replaceAll("\\\\\"", "\"").replaceAll(">\\\\n\\s*<", "><");
+
+        XPathFactory xPathFactory = XPathFactory.newInstance();
+        XPath xpath = xPathFactory.newXPath();
+        InputSource inputSource = new InputSource(new StringReader(result.substring(1, result.length() - 1)));
+
+        final Node root = (Node) xpath.evaluate("/", inputSource, XPathConstants.NODE);
+        final String pathToSpreadsheetResultSchema = "/application/grammars/*[local-name()='schema'][1]";
+        final Node spreadsheetResultSchemaNode = (Node) xpath.evaluate(pathToSpreadsheetResultSchema + "/*[local-name()='element']", root, XPathConstants.NODE);
+        assertEquals("spreadsheetResult", spreadsheetResultSchemaNode.getAttributes().getNamedItem("name").getTextContent());
+
+        final String pathToComplexType = pathToSpreadsheetResultSchema + "/*[local-name()='complexType']";
+        final Node spreadsheetResultComplexTypeNode = (Node) xpath.evaluate(pathToComplexType, root, XPathConstants.NODE);
+        assertEquals("spreadsheetResult", spreadsheetResultComplexTypeNode.getAttributes().getNamedItem("name").getTextContent());
+
+        final String pathToSequence = pathToComplexType + "/*[local-name()='sequence']";
+        Node element = (Node) xpath.evaluate(pathToSequence, root, XPathConstants.NODE);
+        assertEquals(3, element.getChildNodes().getLength());
+
+        element = (Node) xpath.evaluate(pathToSequence + "/*[@name='columnNames']", root, XPathConstants.NODE);
+        assertEquals("xs:element", element.getNodeName());
+        assertEquals("xs:string", element.getAttributes().getNamedItem("type").getTextContent());
+
+        element = (Node) xpath.evaluate(pathToSequence + "/*[@name='results']", root, XPathConstants.NODE);
+        assertEquals("xs:element", element.getNodeName());
+        assertEquals("ns1:anyTypeArray", element.getAttributes().getNamedItem("type").getTextContent());
+
+        element = (Node) xpath.evaluate(pathToSequence + "/*[@name='rowNames']", root, XPathConstants.NODE);
+        assertEquals("xs:element", element.getNodeName());
+        assertEquals("xs:string", element.getAttributes().getNamedItem("type").getTextContent());
+    }
+
+    @Test
+    public void testPost_tiktakMethod_shouldReturnSpreadsheetResult() {
+        Map<String, Object> requestBody = new HashMap<>();
+        requestBody.put("i", 100);
+        requestBody.put("j", "foo");
+
+        ResponseEntity<SpreadsheetResult> response = rest.exchange("/wadl-and-spreadsheetresult/tiktak",
+            HttpMethod.POST,
+            RestClientFactory.request(requestBody),
+            SpreadsheetResult.class);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+
+        SpreadsheetResult result = response.getBody();
+        assertNotNull(result);
+        assertEquals(100, result.getFieldValue("$calc$INT"));
+        assertEquals("foo", result.getFieldValue("$calc$String"));
+    }
+
+}
