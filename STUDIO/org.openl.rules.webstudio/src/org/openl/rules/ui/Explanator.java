d2a872ac086759a398a646bdfa25344ec83b09c0
Nail Samatov <nsamatov@eisgroup.com>
2018-09-25 17:53:26 +0300

    EPBDS-7828 EPBDS-7742 Extract methods
    --HG--
    branch : 5.21.x

--- STUDIO/org.openl.rules.webstudio/src/org/openl/rules/ui/Explanator.java
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/ui/Explanator.java
@@ -19,26 +19,41 @@ public class Explanator {
 
     private Map<Integer, ExplanationNumberValue<?>> id2value = new HashMap<>();
 
+    public Integer getId(ExplanationNumberValue<?> value) {
+        return value2id.get(value);
+    }
+
+    public ExplanationNumberValue<?> getValue(Integer id) {
+        return id2value.get(id);
+    }
+
+    public Integer putIfAbsent(ExplanationNumberValue<?> value) {
+        Integer id = getId(value);
+        if (id != null) {
+            return id;
+        }
+
+        id = ++uniqueId;
+        value2id.put(value, id);
+        id2value.put(id, value);
+        return id;
+    }
+
     private static Explanator getCurrent(String requestId) {
         return (Explanator) FacesUtils.getSessionParam(Constants.SESSION_PARAM_EXPLANATOR + requestId);
     }
 
-    public static int getUniqueId(String requestId, ExplanationNumberValue<?> value) {
+    private static Explanator getCurrentOrCreate(String requestId) {
         Explanator explanator = getCurrent(requestId);
         if (explanator == null) {
             explanator = new Explanator();
             FacesUtils.getSessionMap().put(Constants.SESSION_PARAM_EXPLANATOR + requestId, explanator);
         }
+        return explanator;
+    }
 
-        Integer id = explanator.value2id.get(value);
-        if (id != null) {
-            return id;
-        }
-
-        id = ++explanator.uniqueId;
-        explanator.value2id.put(value, id);
-        explanator.id2value.put(id, value);
-        return id;
+    public static int getUniqueId(String requestId, ExplanationNumberValue<?> value) {
+        return getCurrentOrCreate(requestId).putIfAbsent(value);
     }
 
     public static TreeNode getExplainTree(String requestId, String rootID) {
@@ -48,7 +63,7 @@ public class Explanator {
         }
 
         int id = Integer.parseInt(rootID);
-        ExplanationNumberValue<?> root = explanator.id2value.get(id);
+        ExplanationNumberValue<?> root = explanator.getValue(id);
         return new ExplainTreeBuilder().buildWithRoot(root);
     }
 
@@ -59,7 +74,7 @@ public class Explanator {
         }
 
         int id = Integer.parseInt(rootID);
-        ExplanationNumberValue<?> root = explanator.id2value.get(id);
+        ExplanationNumberValue<?> root = explanator.getValue(id);
         return new Explanation().build(root);
     }
 
