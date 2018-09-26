e393c5f75645e3b5c35bd35ffa5f8afa5602a9db
Yury Molchan <ymolchan@eisgroup.com>
2018-09-26 17:15:22 +0300

    EPBDS-7810 Show JNDI resources in the log
    --HG--
    branch : 5.21.x

---
+++ DEV/org.openl.commons/src/org/openl/info/JndiLogger.java
@@ -0,0 +1,93 @@
+package org.openl.info;
+
+import java.beans.BeanInfo;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.Method;
+import java.util.Collection;
+import java.util.Map;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NameClassPair;
+import javax.naming.NamingEnumeration;
+import javax.naming.NamingException;
+import javax.naming.NoInitialContextException;
+
+final class JndiLogger extends OpenLLogger {
+
+    @Override
+    protected String getName() {
+        return "jndi";
+    }
+
+    @Override
+    protected void discover() throws Exception {
+        log("JNDI Context:");
+        InitialContext ctx;
+        try {
+            ctx = new InitialContext();
+        } catch (NoInitialContextException ex) {
+            log("  No initial JNDI context found.");
+            return;
+        }
+        String path = ctx.getNameInNamespace();
+        toMap(ctx, path);
+    }
+
+    private void toMap(Context ctx, String path) throws NamingException {
+        NamingEnumeration<NameClassPair> list = ctx.list(path);
+        if (!list.hasMoreElements()) {
+            log("  {} = [Empty] {}", "JNDI/PATH", ctx);
+            return;
+        }
+        while (list.hasMoreElements()) {
+            NameClassPair next = list.next();
+            String name = next.getName();
+            String jndiPath = path + name;
+            try {
+                Object value = ctx.lookup(jndiPath);
+                if (value instanceof Context) {
+                    toMap(ctx, jndiPath + "/");
+                } else {
+                    log("  {} = {}", jndiPath, value);
+
+                    if (value instanceof Number || value instanceof CharSequence) {
+                        // Nothing
+                    } else if (value instanceof Map) {
+                        log("  {} = {}", jndiPath, value);
+                        for (Map.Entry<?, ?> entry : ((Map<?, ?>) value).entrySet()) {
+                            log("    '{}' = {}", entry.getKey(), entry.getValue());
+                        }
+                    } else if (value instanceof Collection) {
+                        log("  {} = {}", jndiPath, value.getClass());
+                        int i = 0;
+                        for (Object item : (Collection) value) {
+                            log("    [{}] = {}", i++, item);
+                        }
+                    } else {
+                        BeanInfo bi = Introspector.getBeanInfo(value.getClass());
+                        PropertyDescriptor[] pds = bi.getPropertyDescriptors();
+                        for (PropertyDescriptor pd : pds) {
+                            String propName = pd.getName();
+                            try {
+                                Method readMethod = pd.getReadMethod();
+                                if (readMethod != null) {
+                                    Object propValue = readMethod.invoke(value);
+                                    log("    {} = {}", propName, propValue);
+                                } else {
+                                    log("    {} = <no access>", propName);
+                                }
+                            } catch (Exception ex) {
+                                log("    {} = <exception>", propName);
+                            }
+                        }
+                    }
+                }
+
+            } catch (Throwable t) {
+                log("  {} ! {}", jndiPath, t.toString());
+            }
+        }
+    }
+}
