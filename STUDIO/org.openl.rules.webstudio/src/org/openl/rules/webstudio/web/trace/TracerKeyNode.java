cf2302fa5fb9a1efee4a66015235ac570b01a048
Vladyslav Pikus <vpikus@eisgroup.com>
2018-10-08 11:19:54 +0300

    EPBDS-6607 fix problem with complex formula in tracer if it's been already calculated

---
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/web/trace/TracerKeyNode.java
@@ -0,0 +1,44 @@
+package org.openl.rules.webstudio.web.trace;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+import org.openl.types.Invokable;
+import org.openl.vm.IRuntimeEnv;
+
+public class TracerKeyNode<T, E extends IRuntimeEnv> {
+
+    private Invokable<? super T, E> executor;
+    private T target;
+    private Object[] params;
+    private E env;
+    private Object source;
+
+    public TracerKeyNode(Invokable<? super T, E> executor, T target, Object[] params, E env, Object source) {
+        this.executor = executor;
+        this.target = target;
+        this.params = params;
+        this.env = env;
+        this.source = source;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        TracerKeyNode<?, ?> that = (TracerKeyNode<?, ?>) o;
+        return Objects.equals(executor, that.executor) && Objects.equals(target, that.target) && Arrays.equals(params,
+            that.params) && Objects.equals(env, that.env) && Objects.equals(source, that.source);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(executor, target, env, source);
+        result = 31 * result + Arrays.hashCode(params);
+        return result;
+    }
+}
