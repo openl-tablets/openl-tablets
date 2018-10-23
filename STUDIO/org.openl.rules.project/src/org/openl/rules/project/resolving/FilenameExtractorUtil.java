52759679d19e2f73067092984475db7d840c64a5
Yury Molchan <ymolchan@eisgroup.com>
2018-10-23 11:37:06 +0300

    EPBDS-7891 Fix an issue whith incorrect extracting of module name.
    A file extension must be excluded from the module name
    
    --HG--
    branch : 5.21.x

--- STUDIO/org.openl.rules.project/src/org/openl/rules/project/resolving/FilenameExtractorUtil.java
+++ STUDIO/org.openl.rules.project/src/org/openl/rules/project/resolving/FilenameExtractorUtil.java
@@ -1,26 +1,18 @@
 package org.openl.rules.project.resolving;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import org.openl.rules.project.model.Module;
+import org.openl.util.FileUtils;
 
 public final class FilenameExtractorUtil {
 
     private FilenameExtractorUtil() {
     }
 
-    private static Pattern pathPattern = Pattern.compile(".*[^A-Za-z0-9-_,\\s]([A-Za-z0-9-_,\\s]+)\\..*");
-
     public static String extractFileNameFromModule(Module module) {
         if (module.getRulesRootPath() == null) {
             return module.getName();
         }
         String path = module.getRulesRootPath().getPath();
-        Matcher matcher = pathPattern.matcher(path);
-        if (matcher.find()) {
-            return matcher.group(1);
-        }
-        return module.getName();
+        return FileUtils.getBaseName(path);
     }
 }
