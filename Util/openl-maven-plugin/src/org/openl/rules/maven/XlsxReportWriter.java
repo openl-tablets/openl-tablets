d6176af1123e16994c2a94abfd8dd36b5435dd75
Nail Samatov <nsamatov@eisgroup.com>
2018-11-02 23:25:14 +0300

    EPBDS-7709 Export tests results to xlsx format in OpenL Maven plugin

---
+++ Util/openl-maven-plugin/src/org/openl/rules/maven/XlsxReportWriter.java
@@ -0,0 +1,35 @@
+package org.openl.rules.maven;
+
+import org.openl.rules.testmethod.TestSuite;
+import org.openl.rules.testmethod.TestUnitsResults;
+import org.openl.rules.testmethod.export.TestResultExport;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+class XlsxReportWriter {
+    private final File dir;
+
+    XlsxReportWriter(File dir) {
+        this.dir = dir;
+    }
+
+    public void write(TestUnitsResults result) throws Exception {
+        TestSuite testSuite = result.getTestSuite();
+        String testName = testSuite.getTestSuiteMethod().getName();
+        String moduleName = testSuite.getTestSuiteMethod().getModuleName();
+
+        String suitName = "OpenL." + moduleName + "." + testName;
+        String filename = "TEST-" + suitName + ".xlsx";
+
+        if (!dir.mkdirs() && !dir.exists()) {
+            throw new IOException("Can't create folder '" + dir.getAbsolutePath() + "'.");
+        }
+
+        File file = new File(dir, filename);
+        try (FileOutputStream outputStream = new FileOutputStream(file)) {
+            new TestResultExport().export(outputStream, -1, result);
+        }
+    }
+}
