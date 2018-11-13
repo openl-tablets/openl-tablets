f2fcabfc5b0c51523d7c075ab17f9eceb142a1b9
Vladyslav Pikus <vpikus@eisgroup.com>
2018-11-13 16:57:04 +0200

    EPBDS-6822 junit to test that methods of superclass are visible if inheritor doesn't have any method

---
+++ DEV/org.openl.core/test/org/openl/types/impl/AOpenClassTest.java
@@ -0,0 +1,62 @@
+package org.openl.types.impl;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import java.util.Collection;
+
+import org.junit.Test;
+import org.openl.types.IOpenMethod;
+import org.openl.types.java.JavaOpenClass;
+
+public class AOpenClassTest {
+
+    @Test
+    public void superclass_methods_mustBePresent_whenInheritorDoesNotHaveMethods() {
+        AOpenClass openClass = JavaOpenClass.getOpenClass(B.class);
+        Collection<IOpenMethod> methods = openClass.getMethods();
+        assertNotNull(methods);
+        assertFalse(methods.isEmpty());
+        assertNotNull(findMethod(methods, "foo"));
+        assertNotNull(findMethod(methods, "bar"));
+
+        openClass = JavaOpenClass.getOpenClass(C.class);
+        methods = openClass.getMethods();
+        assertNotNull(methods);
+        assertFalse(methods.isEmpty());
+        assertNotNull(findMethod(methods, "foo"));
+        assertNotNull(findMethod(methods, "bar"));
+        assertNull(findMethod(methods, "getC"));
+    }
+
+    private static IOpenMethod findMethod(Collection<IOpenMethod> methods, String name) {
+        for (IOpenMethod method : methods) {
+            if (name.equals(method.getName())) {
+                return method;
+            }
+        }
+        return null;
+    }
+
+    private static class A {
+
+        public void foo() {
+        }
+
+        public void bar() {
+        }
+
+    }
+
+    public static class B extends A {
+    }
+
+    public static class C extends A {
+
+        private void getC() {
+        }
+
+    }
+
+}
