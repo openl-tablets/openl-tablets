dc1457b690210e019c6933e25992e219aff3700c
Vladyslav Pikus <vpikus@eisgroup.com>
2018-12-03 15:47:38 +0200

    EPBDS-7999 implemented equals, hashCode and toString methods for ComponentTypeArrayOpenClass
    --HG--
    branch : 5.21.x

---
+++ DEV/org.openl.core/test/org/openl/types/impl/ComponentTypeArrayOpenClassTest.java
@@ -0,0 +1,38 @@
+package org.openl.types.impl;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openl.types.IOpenClass;
+import org.openl.types.java.JavaOpenClass;
+
+public class ComponentTypeArrayOpenClassTest {
+
+    private IOpenClass from;
+    private IOpenClass to;
+
+    @Before
+    public void setUp() {
+        from = new ComponentTypeArrayOpenClass(new JavaOpenClass(org.openl.generated.packA.MyType.class));
+    }
+
+    @Test
+    public void testEquals_componentClassWithDiffPackages() {
+        to = new ComponentTypeArrayOpenClass(new JavaOpenClass(org.openl.generated.packB.MyType.class));
+        assertNotEquals(from, to);
+    }
+
+    @Test
+    public void testEquals_componentClassWithSamePackages() {
+        to = new ComponentTypeArrayOpenClass(new JavaOpenClass(org.openl.generated.packA.MyType.class));
+        assertEquals(from, to);
+    }
+
+    @Test
+    public void test_toString() {
+        assertEquals("[Lorg.openl.generated.packA.MyType;", from.toString());
+    }
+
+}
--- DEV/org.openl.rules/test/org/openl/rules/lang/xls/types/DatatypeOpenClassTest.java
+++ DEV/org.openl.rules/test/org/openl/rules/lang/xls/types/DatatypeOpenClassTest.java
@@ -2,7 +2,10 @@ package org.openl.rules.lang.xls.types;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.openl.types.IOpenClass;
+import org.openl.types.impl.ComponentTypeArrayOpenClass;
 
 /**
  * 
@@ -10,52 +13,75 @@ import org.junit.Test;
  *
  */
 public class DatatypeOpenClassTest {
-	
+
 	private final String DEFAULT_PACKAGE = "default.test";
 	private final String DEFAULT_NAME = "DatatypeTest";
 	private final String ANY_URL = "file://hello";
-	
-	
+
+	private IOpenClass from;
+
+	@Before
+	public void setUp() {
+		from = new ComponentTypeArrayOpenClass(new DatatypeOpenClass("MyType", "org.openl.generated.packA"));
+	}
+
 	@Test
 	public void testEquals() {
 		DatatypeOpenClass doc1 = new DatatypeOpenClass(DEFAULT_NAME, DEFAULT_PACKAGE);
 		doc1.setMetaInfo(new DatatypeMetaInfo(DEFAULT_NAME, ANY_URL));
-		
+
 		DatatypeOpenClass doc2 = new DatatypeOpenClass(DEFAULT_NAME, DEFAULT_PACKAGE);
 		doc2.setMetaInfo(new DatatypeMetaInfo(DEFAULT_NAME, ANY_URL));
-		
+
 		DatatypeOpenClass doc3 = new DatatypeOpenClass(DEFAULT_NAME, DEFAULT_PACKAGE);
 		doc3.setMetaInfo(new DatatypeMetaInfo(DEFAULT_NAME, ANY_URL));
 		// reflexive check
 		//
 		assertTrue(doc1.equals(doc1));
 		assertEquals(doc1.hashCode(), doc1.hashCode());
-		
+
 		// symmetric check
 		//
 		assertTrue(doc1.equals(doc2));
-		assertTrue(doc2.equals(doc1));		
+		assertTrue(doc2.equals(doc1));
 		assertEquals(doc1.hashCode(), doc2.hashCode());
-		
+
 		// transitive check
 		//
 		assertTrue(doc1.equals(doc2));
 		assertTrue(doc2.equals(doc3));
 		assertTrue(doc3.equals(doc1));
-		
+
 		//consistent check
 		//
 		assertTrue(doc1.equals(doc2));
 		assertTrue(doc1.equals(doc2));
 		assertTrue(doc1.equals(doc2));
-		
+
 		// null check
 		//
 		assertFalse(doc1.equals(null));
-		
+
 		DatatypeOpenClass doc4 = new DatatypeOpenClass(DEFAULT_NAME, DEFAULT_PACKAGE + "suffix");
 		assertFalse(doc1.equals(doc4));
 		assertFalse(doc4.equals(doc1));
 		assertFalse(doc1.hashCode() == doc4.hashCode());
+	}
+
+	@Test
+	public void testEquals_ComponentTypeArrayOpenClass_componentClassWithDiffPackages() {
+		IOpenClass to = new ComponentTypeArrayOpenClass(new DatatypeOpenClass("MyType", "org.openl.generated.packB"));
+		assertNotEquals(from, to);
+	}
+
+	@Test
+	public void testEquals_ComponentTypeArrayOpenClass_componentClassWithSamePackages() {
+		IOpenClass to = new ComponentTypeArrayOpenClass(new DatatypeOpenClass("MyType", "org.openl.generated.packA"));
+		assertEquals(from, to);
+	}
+
+	@Test
+	public void test_toString() {
+		assertEquals("[Lorg.openl.generated.packA.MyType;", from.toString());
 	}
 }
