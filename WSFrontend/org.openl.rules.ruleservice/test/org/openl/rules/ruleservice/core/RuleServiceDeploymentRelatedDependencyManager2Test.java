b76b70c7f0085a2264c5badfcc9c0c15cf0f7cf4
Marat Kamalov <mkamalov@eisgroup.com>
2018-12-04 20:46:48 +0300

    EPBDS-8003 Project is not deployed if project contains property lazy-modules-for-compilation
    --HG--
    branch : 5.21.x

---
+++ WSFrontend/org.openl.rules.ruleservice/test/org/openl/rules/ruleservice/core/RuleServiceDeploymentRelatedDependencyManager2Test.java
@@ -0,0 +1,38 @@
+package org.openl.rules.ruleservice.core;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openl.rules.ruleservice.management.ServiceManager;
+import org.openl.rules.ruleservice.simple.RulesFrontend;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@TestPropertySource(properties={"ruleservice.datasource.dir=test-resources/RuleServiceDeploymentRelatedDependencyManager2Test",
+        "ruleservice.datasource.deploy.clean.datasource=false",
+        "ruleservice.isProvideRuntimeContext=false" })
+@ContextConfiguration({ "classpath:openl-ruleservice-beans.xml" })
+public class RuleServiceDeploymentRelatedDependencyManager2Test implements ApplicationContextAware {
+
+    private ApplicationContext applicationContext;
+
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    @Test
+    public void testInstantiation() throws Exception {
+        assertNotNull(applicationContext);
+        ServiceManager serviceManager = applicationContext.getBean("serviceManager", ServiceManager.class);
+        assertNotNull(serviceManager);
+        RulesFrontend frontend = applicationContext.getBean("frontend", RulesFrontend.class);
+        assertEquals(1, frontend.getServiceNames().size());
+    }
+}
