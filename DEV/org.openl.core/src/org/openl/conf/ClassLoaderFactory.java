ed664183f121d9e7c4a18fb9c42f191d1b345fec
Yury Molchan <ymolchan@eisgroup.com>
2018-12-05 22:50:29 +0300

    EPBDS-8014 Rewrite ClassLoaderFactory to remove using FileTreeIterator

--- DEV/org.openl.core/src/org/openl/conf/ClassLoaderFactory.java
+++ DEV/org.openl.core/src/org/openl/conf/ClassLoaderFactory.java
@@ -1,51 +1,58 @@
-/*
- * Created on Jul 19, 2003
- *
- * Developed by Intelligent ChoicePoint Inc. 2003
- */
-
 package org.openl.conf;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.nio.file.FileSystems;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.PathMatcher;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-import java.util.StringTokenizer;
 
-import org.openl.util.ASelector;
-import org.openl.util.ISelector;
-import org.openl.util.RuntimeExceptionWrapper;
-import org.openl.util.tree.FileTreeIterator;
+import org.openl.util.StringUtils;
 
 public class ClassLoaderFactory {
 
-    public static ClassLoader createClassLoader(String cp,
+    public static ClassLoader createClassLoader(String classpath,
             ClassLoader parent,
             String userHome) throws Exception {
-        String[] classpath = splitClassPath(cp);
-        List<URL> urls = new ArrayList<URL>();
-        for (int i = 0; i < classpath.length; i++) {
 
-            if (classpath[i].endsWith("*")) {
-                makeWildcardPath(makeFile(userHome, classpath[i].substring(0, classpath[i].length() - 1)), urls);
+        final List<URL> urls = new ArrayList<>();
+        String[] cps = StringUtils.split(classpath, File.pathSeparatorChar);
+        // PathMatcher is not thread-safe.
+        final PathMatcher matcher = FileSystems.getDefault().getPathMatcher("glob:**.{zip,jar}");
+        for (String cp : cps) {
+
+            if (cp.endsWith("*")) {
+                String substring = cp.substring(0, cp.length() - 1);
+                Files.walkFileTree(makeFile(userHome, substring).toPath(), new SimpleFileVisitor<Path>() {
+                    @Override
+                    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                        if (matcher.matches(file)) {
+                            urls.add(file.toUri().toURL());
+                        }
+                        return super.visitFile(file, attrs);
+                    }
+                });
             } else {
 
-                File f = makeFile(userHome, classpath[i]);
+                File f = makeFile(userHome, cp);
 
                 if (!f.exists()) {
                     throw new IOException("File " + f.getPath() + " does not exist");
                 }
 
-                urls.add(makeFile(userHome, classpath[i]).toURI().toURL());
+                urls.add(f.toURI().toURL());
             }
 
         }
 
-        URL[] uurl = urls.toArray(new URL[urls.size()]);
+        URL[] uurl = urls.toArray(new URL[0]);
         return new URLClassLoader(uurl, parent);
     }
 
@@ -57,41 +64,5 @@ public class ClassLoaderFactory {
         }
 
         return new File(root, name).getCanonicalFile();
-
     }
-
-    private static void makeWildcardPath(File root, List<URL> urls) {
-
-        ISelector<File> sel = new ASelector<File>() {
-            public boolean select(File f) {
-                String apath = f.getAbsolutePath();
-                boolean res = apath.endsWith(".jar") || apath.endsWith(".zip");
-                return res;
-            }
-
-        };
-
-        Iterator<File> iter = new FileTreeIterator(root, 0).select(sel);
-
-        for (; iter.hasNext();) {
-            File f = iter.next();
-            try {
-                urls.add(f.toURI().toURL());
-            } catch (MalformedURLException e) {
-                throw RuntimeExceptionWrapper.wrap(e);
-            }
-        }
-
-    }
-
-    private static String[] splitClassPath(String classpath) {
-        StringTokenizer st = new StringTokenizer(classpath, File.pathSeparator);
-
-        String[] res = new String[st.countTokens()];
-        for (int i = 0; i < res.length; i++) {
-            res[i] = st.nextToken();
-        }
-        return res;
-    }
-
 }
--- STUDIO/org.openl.rules.test/test/org/openl/util/TreeIteratorTest.java
+++ STUDIO/org.openl.rules.test/test/org/openl/util/TreeIteratorTest.java
@@ -6,19 +6,11 @@
 
 package org.openl.util;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.net.URL;
 import java.util.Iterator;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipFile;
-
-import org.openl.util.tree.FileTreeIterator;
-import org.openl.util.tree.TreeIterator;
 
 import org.junit.Assert;
+import org.openl.util.tree.TreeIterator;
+
 import junit.framework.TestCase;
 
 /**
@@ -27,20 +19,6 @@ import junit.framework.TestCase;
  */
 public class TreeIteratorTest extends TestCase {
 
-    static class Counter {
-        int files;
-        int lines;
-    }
-    static class XTreeAdaptor implements TreeIterator.TreeAdaptor {
-        public Iterator children(Object node) {
-            if (node.getClass().isArray()) {
-                return OpenIterator.fromArray((Object[]) node);
-            }
-            return null;
-        }
-    }
-    static int count = 0;
-
     String[] x1 = { "aaa", "bbb", "ccc" };
 
     String[] x2 = { "ddd", "eee", "fff" };
@@ -48,44 +26,6 @@ public class TreeIteratorTest extends TestCase {
     String[] x3 = { "ggg", "hhh" };
 
     String[][] root = { x1, x2, x3 };
-
-    static void findInZip(String zipName, final String fnameEnd) throws IOException {
-        ZipFile zip = new ZipFile(zipName);
-
-        IOpenIterator it = OpenIterator.fromEnumeration(zip.entries());
-
-        ISelector sel = new ASelector() {
-            public boolean select(Object obj) {
-                ZipEntry ze = (ZipEntry) obj;
-                boolean res = ze.getName().indexOf(fnameEnd) >= 0;
-                if (res) {
-                    System.out.println("  " + ze.getName());
-                }
-                return res;
-            }
-
-        };
-
-        count += it.select(sel).size();
-    }
-
-    public static void main(String[] args) throws Exception {
-        String fname = "c:/3p/jakarta-tomcat-5.0.25";
-        String suffix = ".jar";
-        String x = "catalina/con";
-
-        FileTreeIterator it = new FileTreeIterator(new File(fname), 0);
-
-        for (; it.hasNext();) {
-            File f = it.next();
-            if (f.getName().endsWith(suffix)) {
-                String jar = f.getCanonicalPath();
-                System.out.println(jar);
-                findInZip(jar, x);
-            }
-        }
-        System.out.println("Total found: " + count);
-    }
 
     /**
      * Constructor for TreeIteratorTest.
@@ -96,33 +36,15 @@ public class TreeIteratorTest extends TestCase {
         super(arg0);
     }
 
-    void countFilesAndLines(Counter cnt, String root, ISelector selector) throws IOException {
-        FileTreeIterator fti = new FileTreeIterator(new File(root), 0);
-
-        for (; fti.hasNext();) {
-            File ff = fti.nextFile();
-            if (selector.select(ff)) {
-                ++cnt.files;
-                cnt.lines += countLines(ff);
-            }
-        }
-
-    }
-
-    private int countLines(File ff) throws IOException {
-        int lines = 0;
-
-        FileReader fr = new FileReader(ff);
-        BufferedReader br = new BufferedReader(fr);
-        while (br.readLine() != null) {
-            ++lines;
-        }
-
-        return lines;
-    }
-
     TreeIterator create(int mode) {
-        return new TreeIterator(root, new XTreeAdaptor(), mode);
+        return new TreeIterator(root, new TreeIterator.TreeAdaptor() {
+            public Iterator children(Object node) {
+                if (node.getClass().isArray()) {
+                    return OpenIterator.fromArray((Object[]) node);
+                }
+                return null;
+            }
+        }, mode);
     }
 
     public void testCount() {
@@ -135,29 +57,6 @@ public class TreeIteratorTest extends TestCase {
         Assert.assertEquals(12, size);
     }
 
-    public void testFile() throws Exception {
-        FileTreeIterator it = new FileTreeIterator(new File(".").getCanonicalFile(), 0);
-
-        ISelector sel = new ASelector() {
-            public boolean select(Object obj) {
-                File f = (File) obj;
-                boolean res = f.getAbsolutePath().endsWith("TreeIteratorTest.java");
-                Log.info(f.getAbsolutePath());
-                if (res) {
-                    Log.info(f.getAbsolutePath());
-                }
-                return res;
-            }
-
-        };
-
-        IOpenIterator<?> iterator = it.select(sel);
-        Assert.assertTrue(iterator.hasNext());
-        iterator.next();
-        Assert.assertFalse(iterator.hasNext());
-
-    }
-
     /*
      * Test for int size()
      */
@@ -166,29 +65,4 @@ public class TreeIteratorTest extends TestCase {
         Assert.assertEquals(-1, it.size());
 
     }
-
-    public void testZip() throws Exception {
-
-        URL url = this.getClass().getClassLoader().getResource("util.zip");
-        ZipFile zip = new ZipFile(url.toURI().getPath());
-
-        IOpenIterator it = OpenIterator.fromEnumeration(zip.entries());
-
-        ISelector sel = new ASelector() {
-            public boolean select(Object obj) {
-                ZipEntry ze = (ZipEntry) obj;
-                boolean res = ze.getName().endsWith("TreeIteratorTest.java");
-                Log.info(ze.getName());
-                return res;
-            }
-
-        };
-
-        IOpenIterator<?> iterator = it.select(sel);
-        Assert.assertTrue(iterator.hasNext());
-        iterator.next();
-        Assert.assertFalse(iterator.hasNext());
-
-    }
-
 }
