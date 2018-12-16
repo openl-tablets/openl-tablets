ea77f1d2854bf36e29867251d47a1ee1a1c60c63
Marat Kamalov <mkamalov@eisgroup.com>
2018-12-16 16:15:14 +0300

    EPBDS-8002 Error "Datatype 'org.openl.generated.beans.myType' validation is failed..." is displayed to user on update datatype is project has dependency
    --HG--
    branch : 5.21.x

--- DEV/org.openl.core/src/org/openl/classloader/SimpleBundleClassLoader.java
+++ DEV/org.openl.core/src/org/openl/classloader/SimpleBundleClassLoader.java
@@ -1,5 +1,6 @@
 package org.openl.classloader;
 
+import java.io.InputStream;
 import java.net.URL;
 import java.util.Collections;
 import java.util.IdentityHashMap;
@@ -81,6 +82,28 @@ public class SimpleBundleClassLoader extends OpenLBundleClassLoader {
         }
 
         return null;
+    }
+
+    @Override
+    public URL getResource(String name) {
+        for (ClassLoader bundleClassLoader : bundleClassLoaders) {
+            URL url = bundleClassLoader.getResource(name);
+            if (url != null) {
+                return url;
+            }
+        }
+        return super.getResource(name);
+    }
+
+    @Override
+    public InputStream getResourceAsStream(String name) {
+        for (ClassLoader bundleClassLoader : bundleClassLoaders) {
+            InputStream inputStream = bundleClassLoader.getResourceAsStream(name);
+            if (inputStream != null) {
+                return inputStream;
+            }
+        }
+        return super.getResourceAsStream(name);
     }
 
     protected Class<?> findLoadedClassInBundle(String name) {
