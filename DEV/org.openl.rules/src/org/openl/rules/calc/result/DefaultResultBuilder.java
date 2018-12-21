1463869886c3811ac0ef8644ef171ed9b29535be
Yury Molchan <ymolchan@eisgroup.com>
2018-12-21 22:31:01 +0300

    EPBDS-8059 Move getAbsoluteSpreadsheetFieldCoordinates() to TestBean

--- DEV/org.openl.rules/src/org/openl/rules/calc/result/DefaultResultBuilder.java
+++ DEV/org.openl.rules/src/org/openl/rules/calc/result/DefaultResultBuilder.java
@@ -6,11 +6,8 @@ import java.util.Map;
 import org.openl.rules.calc.Spreadsheet;
 import org.openl.rules.calc.SpreadsheetResult;
 import org.openl.rules.calc.SpreadsheetResultCalculator;
-import org.openl.rules.calc.SpreadsheetStructureBuilder;
 import org.openl.rules.calc.element.SpreadsheetCellField;
 import org.openl.rules.lang.xls.syntax.TableSyntaxNode;
-import org.openl.rules.table.ICell;
-import org.openl.rules.table.IGridTable;
 import org.openl.rules.table.ILogicalTable;
 import org.openl.rules.table.Point;
 import org.openl.types.IOpenField;
@@ -68,60 +65,6 @@ public class DefaultResultBuilder implements IResultBuilder {
             return cellField.getRelativeCoordinates();
         }
         return null;
-    }
-
-    public static Map<String, Point> getAbsoluteSpreadsheetFieldCoordinates(SpreadsheetResult spreadsheetResult) {
-        Map<String, Point> absoluteCoordinates = new HashMap<String, Point>();
-
-        IGridTable sourceTable = spreadsheetResult.getLogicalTable().getSource();
-        
-        String[] rowNames = spreadsheetResult.getRowNames();
-        String[] columnNames = spreadsheetResult.getColumnNames();
-        
-        for (int i = 0; i < rowNames.length; i++) {
-            for (int j = 0; j < columnNames.length; j++) {
-                int column = getColumn(sourceTable, j);
-                int row = getRow(sourceTable, i);
-                ICell cell = sourceTable.getCell(column, row);
-                Point absolute = new Point(cell.getAbsoluteColumn(), cell.getAbsoluteRow());
-                StringBuilder sb = new StringBuilder();
-                sb.append(SpreadsheetStructureBuilder.DOLLAR_SIGN)
-                    .append(columnNames[j])
-                    .append(SpreadsheetStructureBuilder.DOLLAR_SIGN)
-                    .append(rowNames[i]);
-                absoluteCoordinates.put(sb.toString(), absolute);
-            }
-        }
-
-        return absoluteCoordinates;
-    }
-
-    /**
-     * Get the column of a field in Spreadsheet.
-     * 
-     * @return column number
-     */
-    private static int getColumn(IGridTable spreadsheet, int columnFieldNumber) {
-        int column = 0;
-        // The column 0 contains row headers that's why "<=" instead of "<"
-        for (int i = 0; i <= columnFieldNumber; i++) {
-            column += spreadsheet.getCell(i, 0).getWidth();
-        }
-        return column;
-    }
-
-    /**
-     * Get the row of a field in Spreadsheet.
-     * 
-     * @return row number
-     */
-    private static int getRow(IGridTable spreadsheet, int rowFieldNumber) {
-        int row = 0;
-        // The row 0 contains column headers that's why "<=" instead of "<"
-        for (int i = 0; i <= rowFieldNumber; i++) {
-            row += spreadsheet.getCell(0, i).getHeight();
-        }
-        return row;
     }
 
     private Object[][] getResultArray(SpreadsheetResultCalculator result) {
