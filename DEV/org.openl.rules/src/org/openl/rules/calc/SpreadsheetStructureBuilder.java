726bba3214f7156524239095051afdf4a8f51538
Yury Molchan <ymolchan@eisgroup.com>
2018-12-26 12:28:40 +0300

    EPBDS-8059 Move autoType argument in addCellFields()
    --HG--
    branch : 5.21.x

--- DEV/org.openl.rules/src/org/openl/rules/calc/SpreadsheetStructureBuilder.java
+++ DEV/org.openl.rules/src/org/openl/rules/calc/SpreadsheetStructureBuilder.java
@@ -55,8 +55,6 @@ public class SpreadsheetStructureBuilder {
 
     private IOpenMethodHeader spreadsheetHeader;
 
-    private Boolean autoType;
-
     private SpreadsheetStructureBuilderHolder spreadsheetStructureBuilderHolder = new SpreadsheetStructureBuilderHolder(
         this);
 
@@ -65,11 +63,9 @@ public class SpreadsheetStructureBuilder {
     }
 
     public SpreadsheetStructureBuilder(SpreadsheetComponentsBuilder componentsBuilder,
-            IOpenMethodHeader spreadsheetHeader,
-            Boolean autoType) {
+            IOpenMethodHeader spreadsheetHeader) {
         this.componentsBuilder = componentsBuilder;
         this.spreadsheetHeader = spreadsheetHeader;
-        this.autoType = autoType;
     }
 
     private Map<Integer, IBindingContext> rowContexts = new HashMap<Integer, IBindingContext>();
@@ -94,7 +90,7 @@ public class SpreadsheetStructureBuilder {
      *
      * @param spreadsheetType open class of the spreadsheet
      */
-    public void addCellFields(SpreadsheetOpenClass spreadsheetType) {
+    public void addCellFields(SpreadsheetOpenClass spreadsheetType, boolean autoType) {
         IBindingContext generalBindingContext = componentsBuilder.getBindingContext();
 
         CellsHeaderExtractor cellsHeadersExtractor = componentsBuilder.getCellsHeadersExtractor();
@@ -105,12 +101,12 @@ public class SpreadsheetStructureBuilder {
         cells = new SpreadsheetCell[rowsCount][columnsCount];
 
         /** create the binding context for the spreadsheet level **/
-        spreadsheetBindingContext = initSpreadsheetBindingContext(spreadsheetType, generalBindingContext);
+        spreadsheetBindingContext = new SpreadsheetContext(generalBindingContext, spreadsheetType);
 
         for (int rowIndex = 0; rowIndex < rowsCount; rowIndex++) {
             for (int columnIndex = 0; columnIndex < columnsCount; columnIndex++) {
                 /** build spreadsheet cell */
-                SpreadsheetCell spreadsheetCell = buildCell(rowIndex, columnIndex);
+                SpreadsheetCell spreadsheetCell = buildCell(rowIndex, columnIndex, autoType);
 
                 /** init cells array with appropriate cell */
                 cells[rowIndex][columnIndex] = spreadsheetCell;
@@ -119,11 +115,6 @@ public class SpreadsheetStructureBuilder {
                 addSpreadsheetFields(spreadsheetType, spreadsheetCell, rowIndex, columnIndex);
             }
         }
-    }
-
-    private IBindingContext initSpreadsheetBindingContext(SpreadsheetOpenClass spreadsheetType,
-            IBindingContext generalBindingContext) {
-        return new SpreadsheetContext(generalBindingContext, spreadsheetType);
     }
 
     private void extractCellValues() {
@@ -337,7 +328,7 @@ public class SpreadsheetStructureBuilder {
         return (DOLLAR_SIGN + columnName + DOLLAR_SIGN + rowName).intern();
     }
 
-    private SpreadsheetCell buildCell(int rowIndex, int columnIndex) {
+    private SpreadsheetCell buildCell(int rowIndex, int columnIndex, boolean autoType) {
         Map<Integer, SpreadsheetHeaderDefinition> columnHeaders = componentsBuilder.getColumnHeaders();
         Map<Integer, SpreadsheetHeaderDefinition> rowHeaders = componentsBuilder.getRowHeaders();
 
