74625bcab7f18e5fe0a36fc2be301808adb580d9
Vladyslav Pikus <vpikus@eisgroup.com>
2019-01-28 16:20:56 +0200

    EPBDS-8099 added functional tests
    --HG--
    branch : 5.21.x

---
+++ STUDIO/org.openl.rules.test/test/org/openl/generated/epbds8099/BeanA.java
@@ -0,0 +1,25 @@
+package org.openl.generated.epbds8099;
+
+public class BeanA extends SuperBeanA implements IBeanA {
+
+    private String childField1;
+    private Integer childField2;
+
+    @Override
+    public String getChildField1() {
+        return childField1;
+    }
+
+    public void setChildField1(String childField1) {
+        this.childField1 = childField1;
+    }
+
+    @Override
+    public Integer getChildField2() {
+        return childField2;
+    }
+
+    public void setChildField2(Integer childField2) {
+        this.childField2 = childField2;
+    }
+}
---
+++ STUDIO/org.openl.rules.test/test/org/openl/generated/epbds8099/SuperBeanA.java
@@ -0,0 +1,23 @@
+package org.openl.generated.epbds8099;
+
+public class SuperBeanA implements ISuperBeanA {
+
+    private String superField1;
+    private Integer superField2;
+
+    public String getSuperField1() {
+        return superField1;
+    }
+
+    public void setSuperField1(String superField1) {
+        this.superField1 = superField1;
+    }
+
+    public Integer getSuperField2() {
+        return superField2;
+    }
+
+    public void setSuperField2(Integer superField2) {
+        this.superField2 = superField2;
+    }
+}
