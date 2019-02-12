c4b0a58590ce874e33f3e661a18381009ad99895
Marat Kamalov <mkamalov@eisgroup.com>
2019-02-12 15:05:40 +0300

    EPBDS-8198 Module is not opened if java.lang.NoClassDefFoundError occures.

---
+++ STUDIO/org.openl.rules.test/test-resources/functionality/EPBDS-8198_LinkageError/bin/org/openl/beans/BeanA.java
@@ -0,0 +1,13 @@
+package org.openl.beans;
+
+public class BeanA {
+    BeanB beanB;
+    
+    public BeanB getBeanB() {
+        return beanB;
+    }
+    
+    public void setBeanB(BeanB beanB) {
+        this.beanB = beanB;
+    }
+}
