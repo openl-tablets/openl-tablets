c04aa09172279eab0b0b3dd1f2e81a3ada8c94b6
Yury Molchan <ymolchan@eisgroup.com>
2019-02-21 21:16:45 +0300

    Fix Sonar issues

--- DEV/org.openl.commons/src/org/openl/util/ZipUtils.java
+++ DEV/org.openl.commons/src/org/openl/util/ZipUtils.java
@@ -7,22 +7,20 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
-import java.util.zip.ZipOutputStream;
 
 /**
- * A utility class to work with zip files.
+ * A utility class to work with zip files. File names in a zip are in UTF-8.
  *
  * @author Yury Molchan
  */
-public class ZipUtils {
-
+public final class ZipUtils {
     private static final int BUFFER_SIZE = 64 * 1024;
+
+    private ZipUtils() {
+        throw new IllegalStateException("Utility class");
+    }
 
     /**
      * Extract all files from a zip file into a directory.
@@ -31,19 +29,8 @@ public class ZipUtils {
      * @param outputFolder the output folder for extracted files
      */
     public static void extractAll(File zipFile, File outputFolder) throws IOException {
-        extractAll(zipFile, StandardCharsets.UTF_8, outputFolder);
-    }
-
-    /**
-     * Extract all files from a zip file into a directory.
-     *
-     * @param zipFile the input zip file
-     * @param charset charset used to uncompress zip from input stream
-     * @param outputFolder the output folder for extracted files
-     */
-    public static void extractAll(File zipFile, Charset charset, File outputFolder) throws IOException {
         final FileInputStream zippedStream = new FileInputStream(zipFile);
-        extractAll(zippedStream, charset, outputFolder);
+        extractAll(zippedStream, outputFolder);
     }
 
     /**
@@ -53,43 +40,29 @@ public class ZipUtils {
      * @param outputFolder the output folder for extracted files
      */
     public static void extractAll(InputStream zippedStream, File outputFolder) throws IOException {
-        extractAll(zippedStream, StandardCharsets.UTF_8, outputFolder);
-    }
-
-    /**
-     * Extract all files from a zipped stream into a directory.
-     *
-     * @param zippedStream the zipped input stream
-     * @param charset charset used to uncompress zip from input stream
-     * @param outputFolder the output folder for extracted files
-     */
-    public static void extractAll(InputStream zippedStream, Charset charset, File outputFolder) throws IOException {
 
         byte[] buffer = new byte[BUFFER_SIZE];
 
-        ZipInputStream zis = new ZipInputStream(zippedStream, charset);
-        try {
+        try (ZipInputStream zis = new ZipInputStream(zippedStream)) {
             // get the zipped file list entry
             ZipEntry ze = zis.getNextEntry();
-
             while (ze != null) {
 
                 if (!ze.isDirectory()) {
                     String fileName = ze.getName();
                     File unzipped = new File(outputFolder, fileName);
-                    // create all non exists folders
-                    new File(unzipped.getParent()).mkdirs();
-                    FileOutputStream fos = new FileOutputStream(unzipped);
-
-                    IOUtils.copy(zis, fos, buffer);
-
-                    fos.close();
+                    extractOneFile(zis, unzipped, buffer);
                 }
-
                 ze = zis.getNextEntry();
             }
-        } finally {
-            IOUtils.closeQuietly(zis);
+        }
+    }
+
+    private static void extractOneFile(ZipInputStream zis, File targetFile, byte[] buffer) throws IOException {
+        // create all non exists folders
+        new File(targetFile.getParent()).mkdirs();
+        try (FileOutputStream fos = new FileOutputStream(targetFile)) {
+            IOUtils.copy(zis, fos, buffer);
         }
     }
 
@@ -110,71 +83,8 @@ public class ZipUtils {
                 throw new FileNotFoundException("Directory '" + sourceDirectory.getAbsolutePath() + "' is empty!");
             }
         }
-        OutputStream fos = new FileOutputStream(targetFile);
-        try {
-            archive(sourceDirectory, fos);
-        } finally {
-            fos.close();
-        }
-    }
-
-    /**
-     * Pack a file or all files in a source directory to a zipped output stream.
-     * This method does not close the output stream.
-     *
-     * @param source a file or a not empty directory for compressing
-     * @param output a stream for zipped files. It is not closed by this method.
-     * @throws IOException
-     */
-    private static void archive(File source, OutputStream output) throws IOException {
-        ZipOutputStream zos = new ZipOutputStream(output);
-        byte[] buffer = new byte[BUFFER_SIZE];
-        if (source.isDirectory()) {
-            // This is a directory
-            String sourceDirAbsolutePath = source.getAbsolutePath() + File.separator;
-            int prefixLength = sourceDirAbsolutePath.length();
-            Queue<File> directoryList = new LinkedList<File>();
-            directoryList.add(source);
-            while (!directoryList.isEmpty()) {
-                File directory = directoryList.poll();
-
-                File[] files = directory.listFiles();
-
-                for (File file : files) {
-                    // Remove the absolute part of path
-                    String entryName = file.getAbsolutePath().substring(prefixLength);
-                    entryName = entryName.replaceAll("\\\\", "/");
-                    if (file.isDirectory()) {
-                        if (file.listFiles().length == 0) {
-                            ZipEntry entry = new ZipEntry(entryName + "/");
-                            zos.putNextEntry(entry);
-                        } else {
-                            directoryList.add(file);
-                        }
-                    } else {
-                        archiveFile(file, entryName, zos, buffer);
-                    }
-                }
-            }
-        } else {
-            // This is a single File
-            String name = source.getName();
-            archiveFile(source, name, zos, buffer);
-        }
-        // Do not close the zip output stream, because it cause closing the
-        // output stream. So finish() is used instead.
-        zos.finish();
-    }
-
-    private static void archiveFile(File file, String name, ZipOutputStream zos, byte[] data) throws IOException {
-        FileInputStream fis = new FileInputStream(file);
-        try {
-            ZipEntry entry = new ZipEntry(name);
-            zos.putNextEntry(entry);
-
-            IOUtils.copy(fis, zos, data);
-        } finally {
-            fis.close();
+        try (OutputStream fos = new FileOutputStream(targetFile)) {
+            ZipCompressor.archive(sourceDirectory, fos);
         }
     }
 }
