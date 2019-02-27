7da42260c1bb1bd59e13b44998e3d2268746b236
Yury Molchan <ymolchan@eisgroup.com>
2019-02-27 08:59:14 +0300

    EPBDS-8193 Backed out locking by file
    --HG--
    branch : 5.21.x

--- STUDIO/org.openl.rules.workspace/src/org/openl/rules/project/abstraction/LockEngine.java
+++ STUDIO/org.openl.rules.workspace/src/org/openl/rules/project/abstraction/LockEngine.java
@@ -5,8 +5,6 @@ import org.openl.rules.common.LockInfo;
 public interface LockEngine {
     boolean tryLock(String projectName, String userName) throws LockException;
 
-    void unlock(String projectName, String userName);
-
     void unlock(String projectName);
 
     LockInfo getLockInfo(String projectName);
