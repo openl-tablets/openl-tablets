08ecb87e7a06d976b5aecd1fa1860b7288c9a5b1
Yury Molchan <ymolchan@eisgroup.com>
2019-03-13 00:34:25 +0300

    EPBDS-8287 Remove dependency on BaseOpenlBuilderHelper

--- DEV/org.openl.rules/test/org/openl/rules/dt/SmartDTTest.java
+++ DEV/org.openl.rules/test/org/openl/rules/dt/SmartDTTest.java
@@ -2,64 +2,75 @@ package org.openl.rules.dt;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openl.meta.DoubleValue;
-import org.openl.rules.BaseOpenlBuilderHelper;
-import org.openl.rules.vm.SimpleRulesVM;
-import org.openl.types.IOpenClass;
-import org.openl.types.IOpenMethod;
-import org.openl.types.java.JavaOpenClass;
-import org.openl.vm.SimpleVM.SimpleRuntimeEnv;
+import org.openl.rules.TestUtils;
 
-public class SmartDTTest extends BaseOpenlBuilderHelper {
-    private static final String SRC = "./test/rules/dt/SmartDTTest.xls";
+public class SmartDTTest {
+    private static Object instance;
 
-    public SmartDTTest() {
-        super(SRC);
+    @BeforeClass
+    public static void init() {
+        instance = TestUtils.create("test/rules/dt/SmartDTTest.xls");
     }
 
     @Test
     public void test1() {
-        IOpenMethod method = getCompiledOpenClass().getOpenClass().getMethod("test1",
-            new IOpenClass[] { JavaOpenClass.INT, JavaOpenClass.STRING });
-        Object instance = newInstance();
-        SimpleRuntimeEnv env = new SimpleRulesVM().getRuntimeEnv();
-        assertEquals(new DoubleValue(0.02), method.invoke(instance, new Object[] { 2, "v2" }, env));
-        assertEquals(new DoubleValue(0.05), method.invoke(instance, new Object[] { 5, "v5" }, env));
+        assertEquals(new DoubleValue(0.02),
+            TestUtils.invoke(instance, "test1", new Class[] { int.class, String.class }, new Object[] { 2, "v2" }));
+        assertEquals(new DoubleValue(0.05),
+            TestUtils.invoke(instance, "test1", new Class[] { int.class, String.class }, new Object[] { 5, "v5" }));
     }
-    
+
     @Test
     public void test2() {
-        IOpenMethod method = getCompiledOpenClass().getOpenClass().getMethod("test2",
-            new IOpenClass[] { JavaOpenClass.STRING, JavaOpenClass.INT });
-        Object instance = newInstance();
-        SimpleRuntimeEnv env = new SimpleRulesVM().getRuntimeEnv();
-        assertEquals(new DoubleValue(0.02), method.invoke(instance, new Object[] { "v2", 2 }, env));
-        assertEquals(new DoubleValue(0.05), method.invoke(instance, new Object[] { "v5", 5 }, env));
+        assertEquals(new DoubleValue(0.02),
+            TestUtils.invoke(instance, "test2", new Class[] { String.class, int.class }, new Object[] { "v2", 2 }));
+        assertEquals(new DoubleValue(0.05),
+            TestUtils.invoke(instance, "test2", new Class[] { String.class, int.class }, new Object[] { "v5", 5 }));
     }
 
     @Test
     public void testLookup2D2params() {
-        IOpenMethod method = getCompiledOpenClass().getOpenClass().getMethod("simple2D2params",
-            new IOpenClass[] { JavaOpenClass.INT, JavaOpenClass.STRING });
-        Object instance = newInstance();
-        SimpleRuntimeEnv env = new SimpleRulesVM().getRuntimeEnv();
-        assertEquals(new DoubleValue(0.01), method.invoke(instance, new Object[] { 1, "v1" }, env));
-        assertEquals(new DoubleValue(0.09), method.invoke(instance, new Object[] { 3, "v2" }, env));
-        assertEquals(new DoubleValue(0.17), method.invoke(instance, new Object[] { 5, "v3" }, env));
+        assertEquals(new DoubleValue(0.01),
+            TestUtils.invoke(instance,
+                "simple2D2params",
+                new Class[] { int.class, String.class },
+                new Object[] { 1, "v1" }));
+        assertEquals(new DoubleValue(0.09),
+            TestUtils.invoke(instance,
+                "simple2D2params",
+                new Class[] { int.class, String.class },
+                new Object[] { 3, "v2" }));
+        assertEquals(new DoubleValue(0.17),
+            TestUtils.invoke(instance,
+                "simple2D2params",
+                new Class[] { int.class, String.class },
+                new Object[] { 5, "v3" }));
     }
 
     @Test
     public void testLookup2D3params() {
-        IOpenMethod method = getCompiledOpenClass().getOpenClass().getMethod("simple2D3params",
-            new IOpenClass[] { JavaOpenClass.INT, JavaOpenClass.STRING, JavaOpenClass.STRING });
-        Object instance = newInstance();
-        SimpleRuntimeEnv env = new SimpleRulesVM().getRuntimeEnv();
-        assertEquals(new DoubleValue(0.01), method.invoke(instance, new Object[] { 1, "v1", "v1" }, env));
-        assertEquals(new DoubleValue(0.08), method.invoke(instance, new Object[] { 2, "v1", "v2" }, env));
-        assertEquals(new DoubleValue(0.15), method.invoke(instance, new Object[] { 3, "v2", "v1" }, env));
-        assertEquals(new DoubleValue(0.22), method.invoke(instance, new Object[] { 4, "v2", "v2" }, env));
+        assertEquals(new DoubleValue(0.01),
+            TestUtils.invoke(instance,
+                "simple2D3params",
+                new Class[] { int.class, String.class, String.class },
+                new Object[] { 1, "v1", "v1" }));
+        assertEquals(new DoubleValue(0.08),
+            TestUtils.invoke(instance,
+                "simple2D3params",
+                new Class[] { int.class, String.class, String.class },
+                new Object[] { 2, "v1", "v2" }));
+        assertEquals(new DoubleValue(0.15),
+            TestUtils.invoke(instance,
+                "simple2D3params",
+                new Class[] { int.class, String.class, String.class },
+                new Object[] { 3, "v2", "v1" }));
+        assertEquals(new DoubleValue(0.22),
+            TestUtils.invoke(instance,
+                "simple2D3params",
+                new Class[] { int.class, String.class, String.class },
+                new Object[] { 4, "v2", "v2" }));
     }
-
 }
-
