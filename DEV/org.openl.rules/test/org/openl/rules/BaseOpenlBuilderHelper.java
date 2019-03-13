91bdcce4451cfa74c6dff128d7cde1697836caff
Yury Molchan <ymolchan@eisgroup.com>
2019-03-13 18:39:56 +0300

    EPBDS-8287 Deprecate BaseOpenlBuilderHelper
    For testing xlsx files it is better to use OpenLTest functionality testing
    or TestUtils class

--- DEV/org.openl.rules/test/org/openl/rules/BaseOpenlBuilderHelper.java
+++ DEV/org.openl.rules/test/org/openl/rules/BaseOpenlBuilderHelper.java
@@ -3,14 +3,11 @@ package org.openl.rules;
 import org.openl.CompiledOpenClass;
 import org.openl.rules.lang.xls.binding.XlsMetaInfo;
 import org.openl.rules.lang.xls.syntax.TableSyntaxNode;
-import org.openl.rules.lang.xls.syntax.XlsModuleSyntaxNode;
 import org.openl.rules.runtime.RulesEngineFactory;
 import org.openl.rules.validation.properties.dimentional.DispatcherTablesBuilder;
-import org.openl.rules.vm.SimpleRulesVM;
 import org.openl.runtime.EngineFactory;
 import org.openl.types.IOpenClass;
 import org.openl.types.IOpenMethod;
-import org.openl.vm.SimpleVM.SimpleRuntimeEnv;
 
 /**
  * Helper class for building IOpenClass and getting XlsModuleSyntaxNode from it. To get everything you need for your
@@ -19,24 +16,18 @@ import org.openl.vm.SimpleVM.SimpleRuntimeEnv;
  * 
  * @author DLiauchuk
  *
+ * @deprecated Use {@link TestUtils} instead
+ *
  */
+@Deprecated
 public abstract class BaseOpenlBuilderHelper {
 
-    private XlsModuleSyntaxNode xsn;
-    private CompiledOpenClass compiledOpenClass;
     protected Object instance;
+    private CompiledOpenClass compiledOpenClass;
 
     public BaseOpenlBuilderHelper(String src) {
         EngineFactory<Object> engineFactory = new RulesEngineFactory<>(src);
-        engineFactory.setExecutionMode(false);
         compiledOpenClass = engineFactory.getCompiledOpenClass();
-        XlsMetaInfo xmi = (XlsMetaInfo) compiledOpenClass.getOpenClassWithErrors().getMetaInfo();
-        xsn = xmi.getXlsModuleNode();
-    }
-
-    public Object newInstance() {
-        SimpleRuntimeEnv env = new SimpleRulesVM().getRuntimeEnv();
-        return getCompiledOpenClass().getOpenClass().newInstance(env);
     }
 
     public CompiledOpenClass getCompiledOpenClass() {
@@ -67,7 +58,7 @@ public abstract class BaseOpenlBuilderHelper {
     }
 
     protected TableSyntaxNode[] getTableSyntaxNodes() {
-        return xsn.getXlsTableSyntaxNodes();
+        return ((XlsMetaInfo) compiledOpenClass.getOpenClassWithErrors().getMetaInfo()).getXlsModuleNode()
+            .getXlsTableSyntaxNodes();
     }
-
 }
