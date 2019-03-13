05690c6547f3109f46e8b9e20b513d7e08cd1680
Nail Samatov <nsamatov@eisgroup.com>
2019-03-13 17:42:30 +0300

    EPBDS-8240 Support path to git repo on file system as uri

--- STUDIO/org.openl.rules.repository.git/src/org/openl/rules/repository/git/GitRepository.java
+++ STUDIO/org.openl.rules.repository.git/src/org/openl/rules/repository/git/GitRepository.java
@@ -2,6 +2,7 @@ package org.openl.rules.repository.git;
 
 import java.io.*;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
 import java.text.MessageFormat;
 import java.util.*;
@@ -356,8 +357,12 @@ public class GitRepository implements FolderRepository, BranchRepository, Closea
                                     .getString(ConfigConstants.CONFIG_REMOTE_SECTION,
                                             Constants.DEFAULT_REMOTE_NAME,
                                             ConfigConstants.CONFIG_KEY_URL);
-                            if (!new URI(uri).equals(new URI(remoteUrl))) {
-                                throw new IOException("Folder '" + local + "' already contains local git repository but is configured for different URI (" + remoteUrl + ").\nDelete it or choose another local path or set correct URL for repository.");
+                            if (!uri.equals(remoteUrl)) {
+                                URI proposedUri = getUri(uri);
+                                URI savedUri = getUri(remoteUrl);
+                                if (!proposedUri.equals(savedUri)) {
+                                    throw new IOException("Folder '" + local + "' already contains local git repository but is configured for different URI (" + remoteUrl + ").\nDelete it or choose another local path or set correct URL for repository.");
+                                }
                             }
                         }
                         shouldClone = false;
@@ -975,6 +980,15 @@ public class GitRepository implements FolderRepository, BranchRepository, Closea
         File parentFile = file.getParentFile();
         if (!parentFile.mkdirs() && !parentFile.exists()) {
             throw new FileNotFoundException("Can't create the folder " + parentFile.getAbsolutePath());
+        }
+    }
+
+    private URI getUri(String uriOrPath) {
+        try {
+            return new URI(uriOrPath);
+        } catch (URISyntaxException e) {
+            // uri can be a folder path. It's not valid URI but git accepts paths too.
+            return new File(uriOrPath).toURI();
         }
     }
 
