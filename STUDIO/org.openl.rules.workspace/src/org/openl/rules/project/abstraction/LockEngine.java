7a893b65f74baeb8b8ba143a27f5675612469b40
Nail Samatov <nsamatov@eisgroup.com>
2019-04-19 17:21:02 +0300

    EPBDS-8431 Project editing is locked for User1, if User2 is edited it, users works in different branches

--- STUDIO/org.openl.rules.workspace/src/org/openl/rules/project/abstraction/LockEngine.java
+++ STUDIO/org.openl.rules.workspace/src/org/openl/rules/project/abstraction/LockEngine.java
@@ -3,9 +3,9 @@ package org.openl.rules.project.abstraction;
 import org.openl.rules.common.LockInfo;
 
 public interface LockEngine {
-    boolean tryLock(String projectName, String userName) throws LockException;
+    boolean tryLock(String branch, String projectName, String userName) throws LockException;
 
-    void unlock(String projectName);
+    void unlock(String branch, String projectName);
 
-    LockInfo getLockInfo(String projectName);
+    LockInfo getLockInfo(String branch, String projectName);
 }
--- STUDIO/org.openl.rules.workspace/src/org/openl/rules/project/impl/local/DummyLockEngine.java
+++ STUDIO/org.openl.rules.workspace/src/org/openl/rules/project/impl/local/DummyLockEngine.java
@@ -5,16 +5,16 @@ import org.openl.rules.project.abstraction.LockEngine;
 
 public class DummyLockEngine implements LockEngine {
     @Override
-    public boolean tryLock(String projectName, String userName) {
+    public boolean tryLock(String branch, String projectName, String userName) {
         return true;
     }
 
     @Override
-    public void unlock(String projectName) {
+    public void unlock(String branch, String projectName) {
     }
 
     @Override
-    public LockInfo getLockInfo(String projectName) {
+    public LockInfo getLockInfo(String branch, String projectName) {
         return new SimpleLockInfo(false, null, null);
     }
 }
