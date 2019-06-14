703f2e534ace37ccd2448823a9ba9a6dc4b50970
Vladyslav Pikus <vpikus@eisgroup.com>
2019-06-14 18:12:29 +0300

    EPBDS-8725 support to deploy a deployment archive via REST

--- WSFrontend/org.openl.rules.ruleservice.deployer/src/org/openl/rules/ruleservice/deployer/DeploymentUtils.java
+++ WSFrontend/org.openl.rules.ruleservice.deployer/src/org/openl/rules/ruleservice/deployer/DeploymentUtils.java
@@ -1,13 +1,21 @@
 package org.openl.rules.ruleservice.deployer;
 
-import java.io.File;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
 
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathExpression;
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 
+import org.openl.util.IOUtils;
 import org.xml.sax.InputSource;
 
 class DeploymentUtils {
@@ -15,17 +23,17 @@ class DeploymentUtils {
     private DeploymentUtils() {
     }
 
-    public static final String API_VERSION_SEPARATOR = "_V";
+    static final String API_VERSION_SEPARATOR = "_V";
 
-    public static String getProjectName(InputStream stream) {
+    static String getProjectName(InputStream stream) {
         return evaluateXPath(stream, "/project/name");
     }
 
-    public static String getApiVersion(InputStream stream) {
+    static String getApiVersion(InputStream stream) {
         return evaluateXPath(stream, "/version");
     }
 
-    public static String evaluateXPath(InputStream stream, String expression) {
+    static String evaluateXPath(InputStream stream, String expression) {
         try {
             InputSource inputSource = new InputSource(stream);
             XPath xPath = XPathFactory.newInstance().newXPath();
@@ -36,8 +44,34 @@ class DeploymentUtils {
         }
     }
 
-    public static String getFileName(String filePath) {
-        String[] pathTokens = filePath.split(File.pathSeparator);
-        return pathTokens[pathTokens.length - 1];
+    static Map<String, byte[]> unzip(InputStream in) throws IOException {
+        Map<String, byte[]> entries = new HashMap<>();
+        try (ZipInputStream zipStream = new ZipInputStream(in)) {
+            ZipEntry zipEntry;
+            while ((zipEntry = zipStream.getNextEntry()) != null) {
+                if (zipEntry.isDirectory()) {
+                    continue;
+                }
+                String name = zipEntry.getName();
+                ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+                IOUtils.copyAndClose(new ZippedFileInputStream(zipStream), outputStream);
+                entries.put(name, outputStream.toByteArray());
+            }
+        }
+        return entries;
     }
+
+    static ByteArrayOutputStream archiveAsZip(Map<String, byte[]> zipEntries) throws IOException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        try (ZipOutputStream zipos = new ZipOutputStream(baos)) {
+            for (Map.Entry<String, byte[]> entry : zipEntries.entrySet()) {
+                ZipEntry zipEntry = new ZipEntry(entry.getKey());
+                zipos.putNextEntry(zipEntry);
+                IOUtils.copy(new ByteArrayInputStream(entry.getValue()), zipos);
+                zipos.closeEntry();
+            }
+        }
+        return baos;
+    }
+
 }
