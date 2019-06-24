744501823ac5aef831e2b42bd1721ec20a38898a
Yury Molchan <ymolchan@eisgroup.com>
2019-06-24 15:49:17 +0300

    EPBDS-8599 Fix incorrect MIME types of webresources

--- STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/filter/WebResourceFilter.java
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/filter/WebResourceFilter.java
@@ -1,11 +1,22 @@
 package org.openl.rules.webstudio.filter;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import javax.servlet.*;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.openl.util.FileUtils;
 import org.openl.util.IOUtils;
@@ -46,12 +57,28 @@ public class WebResourceFilter implements Filter {
             // "lastIndexOf" cuts off all prefixes at once.
             path = path.substring(path.lastIndexOf(WEBRESOURCE_PREFIX) + WEBRESOURCE_PREFIX.length());
 
-            try (InputStream stream = initializeInputStream(path)) {
-                // IE 9 fix
-                if ("css".equals(FileUtils.getExtension(path))) {
+            String extension = FileUtils.getExtension(path);
+            switch (extension) {
+                case "css":
                     response.setContentType("text/css");
-                }
+                    break;
+                case "js":
+                    response.setContentType("text/javascript");
+                    break;
+                case "gif":
+                    response.setContentType("image/gif");
+                    break;
+                case "png":
+                    response.setContentType("image/png");
+                    break;
+                default:
+                    // Do not process other resources, such as .class or .properties
+                    ((HttpServletResponse) response).sendError(HttpServletResponse.SC_BAD_REQUEST);
+                    return;
 
+            }
+
+            try (InputStream stream = initializeInputStream(path)) {
                 OutputStream out = response.getOutputStream();
                 IOUtils.copy(stream, out);
             }
