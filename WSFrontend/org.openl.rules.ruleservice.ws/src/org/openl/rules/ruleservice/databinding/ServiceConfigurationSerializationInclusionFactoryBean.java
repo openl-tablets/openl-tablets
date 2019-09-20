37cf910e2860ce06e8c51ec2369ea3f88eef4dbf
Yauheni Biruk <ybiruk@eisgroup.com>
2019-09-20 14:45:43 +0300

    EPBDS-8942 Remove empty and null values from REST response

---
+++ WSFrontend/org.openl.rules.ruleservice.ws/src/org/openl/rules/ruleservice/databinding/ServiceConfigurationSerializationInclusionFactoryBean.java
@@ -0,0 +1,54 @@
+package org.openl.rules.ruleservice.databinding;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+public class ServiceConfigurationSerializationInclusionFactoryBean extends ServiceConfigurationFactoryBean<JsonInclude.Include> {
+
+    private static final String SERIALIZATION_INCLUSION = "jackson.serializationInclusion";
+
+    private String defaultSerializationInclusion;
+
+    @Override
+    protected JsonInclude.Include createInstance() throws Exception {
+        JsonInclude.Include serializationInclusion = null;
+        Object value = getValue(SERIALIZATION_INCLUSION);
+        if (value != null) {
+            if (value instanceof String) {
+                String stringValue = (String) value;
+                try {
+                    serializationInclusion = JsonInclude.Include.valueOf(stringValue);
+                    return serializationInclusion;
+                } catch (IllegalArgumentException e) {
+                    throw new ServiceConfigurationException(String.format(
+                        "Invalid serializationInclusion value is used for '%s' in the configuration for service '%s'.",
+                        SERIALIZATION_INCLUSION,
+                        getServiceDescription().getName()), e);
+                }
+            } else {
+                if (value != null) {
+                    throw new ServiceConfigurationException(
+                        String.format("Expected string value for '%s' in the configuration for service '%s'.",
+                            SERIALIZATION_INCLUSION,
+                            getServiceDescription().getName()));
+                }
+            }
+        } else {
+            try {
+                serializationInclusion = JsonInclude.Include.valueOf(defaultSerializationInclusion);
+                return serializationInclusion;
+            } catch (IllegalArgumentException e) {
+                throw new ServiceConfigurationException("Invalid serializationInclusion value", e);
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public Class<?> getObjectType() {
+        return JsonInclude.Include.class;
+    }
+
+    public void setDefaultSerializationInclusion(String defaultSerializationInclusion) {
+        this.defaultSerializationInclusion = defaultSerializationInclusion;
+    }
+}
