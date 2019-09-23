71f69493904816b2c19d4fe38b22365a0834516d
Yury Molchan <ymolchan@eisgroup.com>
2019-09-23 17:39:07 +0300

    EPBDS-8961 Support valueOf(String) and parse(CharSequence) to instantiate types.

--- DEV/org.openl.rules/test/org/openl/rules/convertor/String2ConstructorConvertorTest.java
+++ DEV/org.openl.rules/test/org/openl/rules/convertor/String2ConstructorConvertorTest.java
@@ -1,5 +1,9 @@
 package org.openl.rules.convertor;
 
+import java.sql.Timestamp;
+import java.time.LocalDate;
+
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
@@ -8,10 +12,24 @@ import org.junit.Test;
 public class String2ConstructorConvertorTest {
 
     @Test
-    public void testParse() {
+    public void testParseConstructor() {
         String2ConstructorConvertor<Integer> converter = new String2ConstructorConvertor<>(Integer.class);
         Integer result = converter.parse("123", null);
         assertEquals((Integer) 123, result);
+    }
+
+    @Test
+    public void testParseValueOf() {
+        String2ConstructorConvertor<Timestamp> converter = new String2ConstructorConvertor<>(Timestamp.class);
+        Timestamp result = converter.parse("1980-07-12 00:00:00", null);
+        assertEquals(Timestamp.valueOf("1980-07-12 00:00:00"), result);
+    }
+
+    @Test
+    public void testParseParse() {
+        String2ConstructorConvertor<LocalDate> converter = new String2ConstructorConvertor<>(LocalDate.class);
+        LocalDate result = converter.parse("1980-07-12", null);
+        assertEquals(LocalDate.of(1980,7,12), result);
     }
 
     @Test
