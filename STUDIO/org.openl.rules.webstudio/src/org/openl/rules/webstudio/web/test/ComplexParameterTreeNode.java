f193380f65fb1b60ea31f9627210f93c17bf6c4f
Roman Platonov <rplatonov@eisgroup.com>
2019-11-05 11:45:24 +0300

    EPBDS-7760 Using NumericComparableString instead of simple string to correct sorting string with numbers part

--- STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/web/test/ComplexParameterTreeNode.java
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/web/test/ComplexParameterTreeNode.java
@@ -7,6 +7,7 @@ import java.util.SortedMap;
 import java.util.TreeMap;
 
 import org.openl.base.INamedThing;
+import org.openl.binding.impl.NumericComparableString;
 import org.openl.types.IOpenClass;
 import org.openl.types.IOpenField;
 import org.openl.types.java.JavaOpenClass;
@@ -61,18 +62,20 @@ public class ComplexParameterTreeNode extends ParameterDeclarationTreeNode {
         } else {
             LinkedHashMap<Object, ParameterDeclarationTreeNode> fields = new LinkedHashMap<>();
             IRuntimeEnv env = new SimpleVM().getRuntimeEnv();
-            SortedMap<String, IOpenField> fieldMap;
+            SortedMap<NumericComparableString, IOpenField> fieldMap = new TreeMap<>();
             try {
                 Map<String, IOpenField> openFieldMap = getType().getFields();
-                fieldMap = new TreeMap<>(openFieldMap);
+                for (Entry<String, IOpenField> entry : openFieldMap.entrySet()) {
+                    fieldMap.put(NumericComparableString.valueOf(entry.getKey()), entry.getValue());
+                }
             } catch (LinkageError e) {
                 return fields;
             }
 
-            for (Entry<String, IOpenField> fieldEntry : fieldMap.entrySet()) {
+            for (Entry<NumericComparableString, IOpenField> fieldEntry : fieldMap.entrySet()) {
                 IOpenField field = fieldEntry.getValue();
                 if (!field.isConst() && field.isReadable()) {
-                    String fieldName = fieldEntry.getKey();
+                    String fieldName = fieldEntry.getKey().getValue();
                     Object fieldValue;
                     IOpenClass fieldType = field.getType();
 
