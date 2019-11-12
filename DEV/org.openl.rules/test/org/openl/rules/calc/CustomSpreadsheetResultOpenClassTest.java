da9a8ba45aa38bca6de3ad428c22cc55f31bbe40
Marat Kamalov <mkamalov@eisgroup.com>
2019-11-13 00:32:59 +0300

    EPBDS-9077 Fix UnsupportedOperationException. Read only map was used for no case sensitive field names in AOpenClass.

---
+++ DEV/org.openl.rules/test/org/openl/rules/calc/CustomSpreadsheetResultOpenClassTest.java
@@ -0,0 +1,14 @@
+package org.openl.rules.calc;
+
+import org.junit.Test;
+import org.openl.types.java.JavaOpenClass;
+
+public class CustomSpreadsheetResultOpenClassTest {
+    @Test
+    public void test() {
+        CustomSpreadsheetResultOpenClass openClass = new CustomSpreadsheetResultOpenClass("CSR1");
+        openClass.getField("$f1", false);
+        openClass.addField(new CustomSpreadsheetResultField(null, "$f1", JavaOpenClass.OBJECT));
+        openClass.addField(new CustomSpreadsheetResultField(null, "$F1", JavaOpenClass.OBJECT));
+    }
+}
