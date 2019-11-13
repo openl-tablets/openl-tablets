41ce89f6882180488d34e7e99c4884fad1865f49
Oleksandr Sahaidak <none@none>
2019-11-13 12:20:12 +0200

    EPBDS-9008 Error is displayed in Test table - fixed

--- DEV/org.openl.rules/src/org/openl/rules/data/ForeignKeyColumnDescriptor.java
+++ DEV/org.openl.rules/src/org/openl/rules/data/ForeignKeyColumnDescriptor.java
@@ -337,6 +337,7 @@ public class ForeignKeyColumnDescriptor extends ColumnDescriptor {
                     try {
                         result = foreignTable.findObject(foreignKeyIndex, s, cxt);
                     } catch (SyntaxNodeException ex) {
+                        foreignTable.getTableSyntaxNode().addError(ex);
                         throw createIndexNotFoundError(foreignTable, valuesTable, s, ex, cxt);
                     }
                     if (result != null) {
