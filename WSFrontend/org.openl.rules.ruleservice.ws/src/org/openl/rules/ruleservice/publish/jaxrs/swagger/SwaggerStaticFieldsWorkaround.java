6db0b3551df6c759cccdce9c07f2d2ab5a426a0e
Marat Kamalov <mkamalov@eisgroup.com>
2019-11-14 12:37:29 +0300

    The getDeclaredField method has to return a new object each time, exactly because this object has the mutable accessible flag. So there is no need to reset the flag.

--- WSFrontend/org.openl.rules.ruleservice.ws/src/org/openl/rules/ruleservice/publish/jaxrs/swagger/SwaggerStaticFieldsWorkaround.java
+++ WSFrontend/org.openl.rules.ruleservice.ws/src/org/openl/rules/ruleservice/publish/jaxrs/swagger/SwaggerStaticFieldsWorkaround.java
@@ -16,31 +16,21 @@ public final class SwaggerStaticFieldsWorkaround {
     public static void reset() throws NoSuchFieldException, IllegalAccessException {
         Field initializedScannerField = BaseApiListingResource.class.getDeclaredField("initializedScanner");
         Field initializedConfigField = BaseApiListingResource.class.getDeclaredField("initializedConfig");
-        boolean initializedScannerFieldAccessible = initializedScannerField.isAccessible();
-        boolean initializedConfigFieldAccessible = initializedScannerField.isAccessible();
-
         Field swaggerMapField = SwaggerConfigLocator.class.getDeclaredField("swaggerMap");
-        boolean swaggerMapFieldAccessible = swaggerMapField.isAccessible();
 
-        try {
-            swaggerMapField.setAccessible(true);
-            ConcurrentMap<String, Swagger> swaggerMap = (ConcurrentMap<String, Swagger>) swaggerMapField
-                .get(SwaggerConfigLocator.getInstance());
-            swaggerMap.clear();
+        swaggerMapField.setAccessible(true);
+        ConcurrentMap<String, Swagger> swaggerMap = (ConcurrentMap<String, Swagger>) swaggerMapField
+            .get(SwaggerConfigLocator.getInstance());
+        swaggerMap.clear();
 
-            initializedScannerField.setAccessible(true);
-            ConcurrentMap<String, Boolean> initializedScanner = (ConcurrentMap<String, Boolean>) initializedScannerField
-                .get(null);
-            initializedScanner.clear();
+        initializedScannerField.setAccessible(true);
+        ConcurrentMap<String, Boolean> initializedScanner = (ConcurrentMap<String, Boolean>) initializedScannerField
+            .get(null);
+        initializedScanner.clear();
 
-            initializedConfigField.setAccessible(true);
-            ConcurrentMap<String, Boolean> initializedConfig = (ConcurrentMap<String, Boolean>) initializedConfigField
-                .get(null);
-            initializedConfig.clear();
-        } finally {
-            initializedConfigField.setAccessible(initializedConfigFieldAccessible);
-            initializedScannerField.setAccessible(initializedScannerFieldAccessible);
-            swaggerMapField.setAccessible(swaggerMapFieldAccessible);
-        }
+        initializedConfigField.setAccessible(true);
+        ConcurrentMap<String, Boolean> initializedConfig = (ConcurrentMap<String, Boolean>) initializedConfigField
+            .get(null);
+        initializedConfig.clear();
     }
 }
