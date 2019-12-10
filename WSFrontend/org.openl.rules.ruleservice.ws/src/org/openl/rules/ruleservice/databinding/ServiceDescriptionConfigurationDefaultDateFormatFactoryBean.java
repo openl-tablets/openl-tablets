014a7bdbf8a9ef454413ae4640e9be405b87ecce
Vladyslav Pikus <vpikus@eisgroup.com>
2019-12-10 18:09:47 +0200

    EPBDS-9201 use ExtendedStdDateFormat if defaultDateFormat is defined
    --HG--
    branch : 5.22.x

--- WSFrontend/org.openl.rules.ruleservice.ws/src/org/openl/rules/ruleservice/databinding/ServiceDescriptionConfigurationDefaultDateFormatFactoryBean.java
+++ WSFrontend/org.openl.rules.ruleservice.ws/src/org/openl/rules/ruleservice/databinding/ServiceDescriptionConfigurationDefaultDateFormatFactoryBean.java
@@ -3,7 +3,9 @@ package org.openl.rules.ruleservice.databinding;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 
+import org.apache.commons.lang3.StringUtils;
 import org.openl.rules.ruleservice.core.ServiceDescription;
+import org.openl.rules.ruleservice.databinding.util.ExtendedStdDateFormat;
 import org.openl.rules.ruleservice.management.ServiceDescriptionHolder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,6 +17,12 @@ public class ServiceDescriptionConfigurationDefaultDateFormatFactoryBean extends
 
     private static final String DEFAULT_DATE_FORMAT = "jacksondatabinding.defaultDateFormat";
 
+    private String defaultDateFormat;
+
+    public void setDefaultDateFormat(String defaultDateFormat) {
+        this.defaultDateFormat = defaultDateFormat;
+    }
+
     @Override
     public boolean isSingleton() {
         return false;
@@ -22,33 +30,54 @@ public class ServiceDescriptionConfigurationDefaultDateFormatFactoryBean extends
 
     @Override
     protected DateFormat createInstance() {
-        ServiceDescription serviceDescription = ServiceDescriptionHolder.getInstance().getServiceDescription();
-        if (serviceDescription != null && serviceDescription.getConfiguration() != null) {
-            Object value = serviceDescription.getConfiguration().get(DEFAULT_DATE_FORMAT);
-            if (value instanceof String) {
-                String v = (String) value;
-                try {
-                    SimpleDateFormat simpleDateFormat = new SimpleDateFormat(v);
-                    log.info("Service '{}' uses default date format '{}'.", serviceDescription.getName(), v);
-                    return simpleDateFormat;
-                } catch (Exception e) {
-                    if (log.isErrorEnabled()) {
-                        log.error(
-                            "Error in service '{}' configuration. Invalid date format is used in '" + DEFAULT_DATE_FORMAT + "'! Default value is used!",
-                            serviceDescription.getName());
-                    }
+        String value = getValueAsString(DEFAULT_DATE_FORMAT);
+        if (StringUtils.isNotBlank(value)) {
+            try {
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat(value);
+                log.info("Service '{}' uses default date format '{}'.", getServiceDescription().getName(), value);
+                return simpleDateFormat;
+            } catch (Exception e) {
+                if (log.isErrorEnabled()) {
+                    log.error(
+                        "Error in service '{}' configuration. Invalid date format is used in '{}'! Default value is used!",
+                        getServiceDescription().getName(),
+                        DEFAULT_DATE_FORMAT);
                 }
-            } else {
-                if (value != null) {
-                    if (log.isErrorEnabled()) {
-                        log.error(
-                            "Error in service '{}' configuration. Unsupported value is used in '" + DEFAULT_DATE_FORMAT + "'! Default value is used!",
-                            serviceDescription.getName());
-                    }
+            }
+        } else if (StringUtils.isNotBlank(defaultDateFormat)) {
+            try {
+                return new ExtendedStdDateFormat(defaultDateFormat);
+            } catch (Exception e) {
+                if (log.isErrorEnabled()) {
+                    log.error("Invalid date format is used in the rule service's default configuration.", e);
                 }
             }
         }
 
+        return null;
+    }
+
+    private ServiceDescription getServiceDescription() {
+        return ServiceDescriptionHolder.getInstance().getServiceDescription();
+    }
+
+    private String getValueAsString(String property) {
+        ServiceDescription serviceDescription = getServiceDescription();
+        if (serviceDescription == null || serviceDescription.getConfiguration() == null) {
+            return null;
+        }
+        Object value = serviceDescription.getConfiguration().get(property);
+        if (value instanceof String) {
+            return ((String) value).trim();
+        }
+        if (value != null) {
+            if (log.isErrorEnabled()) {
+                log.error(
+                    "Error in service '{}' configuration. Unsupported value is used in '{}'! Default value is used!",
+                    serviceDescription.getName(),
+                    property);
+            }
+        }
         return null;
     }
 
