6a07e54c0c7aad08a36ddd741787ee26faf8ec2f
Vladyslav Pikus <vpikus@eisgroup.com>
2019-12-11 11:15:35 +0200

    EPBDS-9201 adapt tests
    --HG--
    branch : 5.22.x

--- ITEST/itest.jacksonbinding/test/org/openl/itest/RunITest.java
+++ ITEST/itest.jacksonbinding/test/org/openl/itest/RunITest.java
@@ -1,8 +1,5 @@
 package org.openl.itest;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
-
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -16,6 +13,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import static org.openl.itest.core.RestClientFactory.request;
 
 public class RunITest {
 
@@ -48,7 +49,15 @@ public class RunITest {
         ResponseEntity<String> response = rest.exchange("/getDate", HttpMethod.GET, entity, String.class);
 
         assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertEquals("\"190223000000\"", response.getBody());
+        assertEquals("\"23/02/2019 00:00:00\"", response.getBody());
+
+        ResponseEntity<DateWrapper> response2 = rest.postForEntity("/spr", request("{\"date1\":1514768645000,\"date2\":\"2019-01-01\",\"date3\":\"2019-11-11T03:03:03.000\",\"date4\":\"29/12/2019 04:04:04\"}"), DateWrapper.class);
+        assertEquals(HttpStatus.OK, response2.getStatusCode());
+        DateWrapper body = response2.getBody();
+        assertEquals( "01/01/2018 03:04:05", body.date1);
+        assertEquals( "01/01/2019 00:00:00", body.date2);
+        assertEquals( "11/11/2019 03:03:03", body.date3);
+        assertEquals( "29/12/2019 04:04:04", body.date4);
     }
 
     @Test
@@ -83,6 +92,15 @@ public class RunITest {
         if (s == null || s.trim().isEmpty()) {
             fail("String cannot be blank");
         }
+    }
+
+    public static class DateWrapper {
+
+        public String date1;
+        public String date2;
+        public String date3;
+        public String date4;
+
     }
 
 }
