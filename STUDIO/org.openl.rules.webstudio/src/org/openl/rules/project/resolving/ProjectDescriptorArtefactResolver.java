a1d458bf950892997a618a3da82c099460f8a0f8
Nail Samatov <nsamatov@eisgroup.com>
2020-01-17 20:08:32 +0300

    EPBDS-9280 NPE in log file on project deploy, if user changed Flat folder structure: flag, project created from Workspace

--- STUDIO/org.openl.rules.webstudio/src/org/openl/rules/project/resolving/ProjectDescriptorArtefactResolver.java
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/project/resolving/ProjectDescriptorArtefactResolver.java
@@ -14,6 +14,7 @@ import org.openl.rules.project.abstraction.AProjectResource;
 import org.openl.rules.project.model.ProjectDependencyDescriptor;
 import org.openl.rules.project.model.ProjectDescriptor;
 import org.openl.rules.project.xml.XmlProjectDescriptorSerializer;
+import org.openl.rules.repository.api.FileData;
 import org.openl.util.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -31,7 +32,11 @@ public class ProjectDescriptorArtefactResolver {
     private final Map<String, ProjectDescriptor> cache = new WeakHashMap<>();
 
     private ProjectDescriptor getProjectDescriptor(AProject project) throws ProjectException {
-        String version = project.getFileData().getVersion();
+        FileData fileData = project.getFileData();
+        if (fileData == null) {
+            return null;
+        }
+        String version = fileData.getVersion();
         String versionName = version == null ? "" : version;
         String key = String.format("%s:%s:%b", project.getName(), versionName, project.isModified());
 
