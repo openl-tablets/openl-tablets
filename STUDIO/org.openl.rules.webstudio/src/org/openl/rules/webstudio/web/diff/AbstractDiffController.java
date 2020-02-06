553c5212d3c1081248b475a98f73a214bd0e8825
Nail Samatov <nsamatov@eisgroup.com>
2020-02-06 12:54:35 +0300

    EPBDS-9343 Change field type, and it's name

--- STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/web/diff/AbstractDiffController.java
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/web/diff/AbstractDiffController.java
@@ -77,7 +77,7 @@ public abstract class AbstractDiffController {
     public IOpenLTable getTable(int i) {
         XlsProjection projection = projectionTable(i);
         if (projection != null) {
-            return (IOpenLTable) projection.getData();
+            return projection.getTable();
         }
         return null;
     }
@@ -93,7 +93,7 @@ public abstract class AbstractDiffController {
     public IGridFilter getFilter(int i) {
         XlsProjection projection = projectionTable(i);
         if (projection != null) {
-            IOpenLTable table = (IOpenLTable) projection.getData();
+            IOpenLTable table = projection.getTable();
             List<ICell> diffCells = projection.getDiffCells();
             if (diffCells != null) {
                 return makeFilter(table.getGridTable(), diffCells);
