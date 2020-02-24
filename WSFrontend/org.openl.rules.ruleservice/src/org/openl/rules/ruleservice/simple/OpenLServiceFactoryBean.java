7da1c698be3d20f1cbac07986b3d7c838e199ca7
Yury Molchan <ymolchan@eisgroup.com>
2020-02-24 14:27:31 +0300

    Separate RulesFrontend from a proxy factory

--- WSFrontend/org.openl.rules.ruleservice/src/org/openl/rules/ruleservice/simple/OpenLServiceFactoryBean.java
+++ WSFrontend/org.openl.rules.ruleservice/src/org/openl/rules/ruleservice/simple/OpenLServiceFactoryBean.java
@@ -1,18 +1,45 @@
 package org.openl.rules.ruleservice.simple;
 
+import org.openl.runtime.ASMProxyFactory;
 import org.springframework.beans.factory.FactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Required;
 
+/**
+ * Creates a proxy object for defined service.
+ * 
+ * @param <T> The facade interface type
+ */
 public class OpenLServiceFactoryBean<T> implements FactoryBean<T> {
     private Class<T> proxyInterface;
     private String serviceName;
+    private ClassLoader classLoader;
     private RulesFrontend rulesFrontend;
 
+    /**
+     * @param proxyInterface a facade interface for work with OpenL rules.
+     * @param serviceName a name of OpenL rules saved in Frontend.
+     */
+    public OpenLServiceFactoryBean(Class<T> proxyInterface, String serviceName) {
+        this.proxyInterface = proxyInterface;
+        this.serviceName = serviceName;
+    }
+
     @Override
-    public T getObject() throws Exception {
-        return rulesFrontend.buildServiceProxy(serviceName, proxyInterface);
+    public T getObject() {
+        ClassLoader cl = classLoader != null ? classLoader : Thread.currentThread().getContextClassLoader();
+        return ASMProxyFactory.newProxyInstance(cl, (proxy, method, args) -> {
+            try {
+                return rulesFrontend.execute(serviceName, method.getName(), method.getParameterTypes(), args);
+            } catch (MethodInvocationException e) {
+                if (e.getCause() instanceof RuntimeException) {
+                    throw (RuntimeException) e.getCause();
+                } else {
+                    throw new MethodInvocationRuntimeException(e);
+                }
+            }
+        }, proxyInterface);
+
     }
 
     @Override
@@ -31,13 +58,31 @@ public class OpenLServiceFactoryBean<T> implements FactoryBean<T> {
         this.rulesFrontend = rulesFrontend;
     }
 
-    @Required
+    public void setClassLoader(ClassLoader classLoader) {
+        this.classLoader = classLoader;
+    }
+
+    /**
+     * Kept for backward compatibility.
+     */
+    @Deprecated
+    public OpenLServiceFactoryBean() {
+    }
+
+    /**
+     * @deprecated Use constructor-arg instead
+     */
+    @Deprecated
     public void setProxyInterface(Class<T> proxyInterface) {
         this.proxyInterface = proxyInterface;
     }
 
-    @Required
+    /**
+     * @deprecated Use constructor-arg instead
+     */
+    @Deprecated
     public void setServiceName(String serviceName) {
         this.serviceName = serviceName;
     }
+
 }
--- WSFrontend/org.openl.rules.ruleservice/test/org/openl/rules/ruleservice/simple/OpenLServiceFactoryBeanTest.java
+++ WSFrontend/org.openl.rules.ruleservice/test/org/openl/rules/ruleservice/simple/OpenLServiceFactoryBeanTest.java
@@ -6,45 +6,38 @@ import static org.junit.Assert.fail;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.BeansException;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @TestPropertySource(properties = { "ruleservice.datasource.dir=test-resources/RulesFrontendTest",
         "ruleservice.datasource.deploy.clean.datasource=false",
         "ruleservice.isProvideRuntimeContext=false" })
 @ContextConfiguration({ "classpath:openl-ruleservice-beans.xml", "classpath:OpenLServiceFactoryBeanTest.xml" })
-public class OpenLServiceFactoryBeanTest implements ApplicationContextAware {
+public class OpenLServiceFactoryBeanTest {
 
-    private ApplicationContext applicationContext;
+    @Resource
+    private ServiceInterface ruleService1;
 
-    @Override
-    public void setApplicationContext(ApplicationContext arg0) throws BeansException {
-        this.applicationContext = arg0;
-    }
+    @Resource
+    private ServiceInterface ruleService2;
 
     @Test
     public void testDefaultFrontend() {
-        assertNotNull(applicationContext);
-        ServiceInterface service = applicationContext.getBean("ruleService1", ServiceInterface.class);
-        assertNotNull(service);
-        assertEquals("World, Good Morning!", service.worldHello(10));
-        assertEquals("i: 10 s: 5", service.worldHello(10, "5"));
-        assertEquals("i: null s: null", service.worldHello(null, null));
-        assertEquals("i: null s: 5", service.worldHello(null, "5"));
+        assertNotNull(ruleService1);
+        assertEquals("World, Good Morning!", ruleService1.worldHello(10));
+        assertEquals("i: 10 s: 5", ruleService1.worldHello(10, "5"));
+        assertEquals("i: null s: null", ruleService1.worldHello(null, null));
+        assertEquals("i: null s: 5", ruleService1.worldHello(null, "5"));
     }
 
     @Test
     public void testDefaultFrontendAbsentMethods() {
-        assertNotNull(applicationContext);
-        ServiceInterface service = applicationContext.getBean("ruleService1", ServiceInterface.class);
-        assertNotNull(service);
         try {
-            service.worldHello(null);
+            ruleService1.worldHello(null);
             fail();
         } catch (Exception e) {
             assertEquals("org.openl.rules.ruleservice.simple.MethodInvocationException: Method 'worldHello(java.lang.String)' is not found in service 'RulesFrontendTest_multimodule'.", e.getMessage());
@@ -53,28 +46,27 @@ public class OpenLServiceFactoryBeanTest implements ApplicationContextAware {
 
     @Test
     public void testDefaultFrontendAbsentMethods2() {
-        assertNotNull(applicationContext);
-        ServiceInterface service = applicationContext.getBean("ruleService1", ServiceInterface.class);
-        assertNotNull(service);
         try {
-            service.absent(null);
+            ruleService1.absent(null);
             fail();
         } catch (Exception e) {
             assertEquals("org.openl.rules.ruleservice.simple.MethodInvocationException: Method 'absent(java.lang.String)' is not found in service 'RulesFrontendTest_multimodule'.", e.getMessage());
         }
     }
 
+    @Test(expected = org.openl.rules.ruleservice.simple.MethodInvocationRuntimeException.class)
+    public void testAbsentMethods() {
+        ruleService1.absent("X");
+    }
+
     @Test
     public void testOverridedFrontend() {
-        assertNotNull(applicationContext);
-        ServiceInterface service = applicationContext.getBean("ruleService2", ServiceInterface.class);
-        assertNotNull(service);
-        assertEquals("Invoked: worldHello 1 int", service.worldHello(10));
-        assertEquals("Invoked: worldHello 2 Integer", service.worldHello(10, "5"));
-        assertEquals("Invoked: worldHello 2 Integer", service.worldHello(null, null));
-        assertEquals("Invoked: worldHello 2 Integer", service.worldHello(null, "5"));
-        assertEquals("Invoked: worldHello 1 String", service.worldHello(null));
-        assertEquals("Invoked: absent 1 String", service.absent(null));
+        assertEquals("Invoked: worldHello 1 int", ruleService2.worldHello(10));
+        assertEquals("Invoked: worldHello 2 Integer", ruleService2.worldHello(10, "5"));
+        assertEquals("Invoked: worldHello 2 Integer", ruleService2.worldHello(null, null));
+        assertEquals("Invoked: worldHello 2 Integer", ruleService2.worldHello(null, "5"));
+        assertEquals("Invoked: worldHello 1 String", ruleService2.worldHello(null));
+        assertEquals("Invoked: absent 1 String", ruleService2.absent(null));
     }
 
     public interface ServiceInterface {
@@ -84,7 +76,7 @@ public class OpenLServiceFactoryBeanTest implements ApplicationContextAware {
         String absent(String s);
     }
 
-    public static abstract class FrontendImpl extends AbstractRulesFrontend {
+    public static abstract class FrontendImpl implements RulesFrontend {
 
         @Override
         public Object execute(String serviceName, String ruleName, Class<?>[] inputParamsTypes, Object... params) throws MethodInvocationException {
