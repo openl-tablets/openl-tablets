0088b2fb8f91bbb57b802524284a17bca19c806a
Yury Molchan <ymolchan@eisgroup.com>
2020-03-13 13:43:21 +0300

    EPBDS-9524 Fix NPE when load props outside of JSF

--- STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/web/Props.java
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/web/Props.java
@@ -1,16 +1,21 @@
 package org.openl.rules.webstudio.web;
 
-import org.openl.rules.webstudio.web.util.WebStudioUtils;
-import org.springframework.core.env.PropertyResolver;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.core.env.Environment;
 
-
-public class Props {
+public class Props implements EnvironmentAware {
+    private static Environment env;
 
     public static String text(String key) {
-        return  WebStudioUtils.getBean("environment", PropertyResolver.class).getProperty(key);
+        return env.getProperty(key);
     }
 
     public static boolean bool(String key) {
         return Boolean.parseBoolean(text(key));
+    }
+
+    @Override
+    public void setEnvironment(Environment environment) {
+        env = environment;
     }
 }
