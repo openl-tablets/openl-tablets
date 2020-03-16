17b9577490db1fb760199ba5b5c2aa8a56a325af
Marat Kamalov <mkamalov@eisgroup.com>
2020-03-16 23:14:27 +0300

    EPBDS-8844 Avoid nulls for CSR bean fields in response. Simplify JSON/XML.

---
+++ WSFrontend/org.openl.rules.ruleservice.ws/src/org/openl/rules/ruleservice/databinding/ServiceJacksonObjectMapperFactoryBean.java
@@ -0,0 +1,46 @@
+package org.openl.rules.ruleservice.databinding;
+
+import org.openl.rules.calc.CustomSpreadsheetResultOpenClass;
+import org.openl.rules.ruleservice.core.OpenLService;
+import org.openl.rules.ruleservice.core.OpenLServiceHolder;
+import org.openl.rules.ruleservice.databinding.jackson.NonNullMixIn;
+import org.openl.types.IOpenClass;
+import org.springframework.beans.factory.config.AbstractFactoryBean;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ServiceJacksonObjectMapperFactoryBean extends AbstractFactoryBean<ObjectMapper> {
+
+    private JacksonObjectMapperFactoryBean jacksonObjectMapperFactoryBean;
+
+    public JacksonObjectMapperFactoryBean getJacksonObjectMapperFactoryBean() {
+        return jacksonObjectMapperFactoryBean;
+    }
+
+    public void setJacksonObjectMapperFactoryBean(JacksonObjectMapperFactoryBean jacksonObjectMapperFactoryBean) {
+        this.jacksonObjectMapperFactoryBean = jacksonObjectMapperFactoryBean;
+    }
+
+    @Override
+    public Class<?> getObjectType() {
+        return ObjectMapper.class;
+    }
+
+    @Override
+    protected ObjectMapper createInstance() throws Exception {
+        ObjectMapper objectMapper = getJacksonObjectMapperFactoryBean().createJacksonObjectMapper();
+        OpenLService openLService = OpenLServiceHolder.getInstance().get();
+        if (openLService == null) {
+            throw new ServiceConfigurationException("Failed to locate a service.");
+        }
+        if (openLService.getOpenClass() != null) {
+            for (IOpenClass openClass : openLService.getOpenClass().getTypes()) {
+                if (openClass instanceof CustomSpreadsheetResultOpenClass) {
+                    objectMapper.addMixIn(((CustomSpreadsheetResultOpenClass) openClass).getBeanClass(),
+                        NonNullMixIn.class);
+                }
+            }
+        }
+        return objectMapper;
+    }
+}
---
+++ WSFrontend/org.openl.rules.ruleservice.ws/src/org/openl/rules/ruleservice/databinding/jackson/NonNullMixIn.java
@@ -0,0 +1,7 @@
+package org.openl.rules.ruleservice.databinding.jackson;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class NonNullMixIn {
+}
