1a41c83d3fb5bdc7f41d6e6e9b0093ac5f27c739
Yury Molchan <ymolchan@eisgroup.com>
2020-03-20 13:24:03 +0300

    EPBDS-9437 Add tests for shpreadsheets

--- ITEST/itest.SpreadsheetResult/test/org/openl/itest/RunSpreadsheetResultITest.java
+++ ITEST/itest.SpreadsheetResult/test/org/openl/itest/RunSpreadsheetResultITest.java
@@ -25,11 +25,19 @@ public class RunSpreadsheetResultITest {
     @Test
     public void SpreadsheetResult_Wadl() {
         client.get("/REST/spreadsheetresult?_wadl", "/spreadsheetresult_wadl.resp.xml");
+        client.get("/REST/EPBDS-9437?_wadl", "/EPBDS-9437_wadl.resp.xml");
+    }
+
+    @Test
+    public void SpreadsheetResult_Swagger() {
+        client.get("/REST/spreadsheetresult/swagger.json", "/spreadsheetresult_swagger.resp.json");
+        client.get("/REST/EPBDS-9437/swagger.json", "/EPBDS-9437_swagger.resp.json");
     }
 
     @Test
     public void SpreadsheetResult_Wsdl() {
         client.get("/spreadsheetresult?wsdl", "/spreadsheetresult_wsdl.resp.xml");
+        client.get("/EPBDS-9437?wsdl", "/EPBDS-9437_wsdl.resp.xml");
     }
 
     @Test
@@ -37,12 +45,14 @@ public class RunSpreadsheetResultITest {
         client.post("/REST/spreadsheetresult/tiktak",
             "/spreadsheetresult_tiktak.req.json",
             "/spreadsheetresult_tiktak.resp.json");
+        client.post("/REST/EPBDS-9437/tiktak", "/EPBDS-9437_tiktak.req.json", "/EPBDS-9437_tiktak.resp.json");
+        client.post("/REST/EPBDS-9437/EPBDS_9437", "/EPBDS-9437_arr.req.txt", "/EPBDS-9437_arr.resp.json");
     }
 
     @Test
-    public void SpreadsheetResult_SOAP() {
-        client.post("/spreadsheetresult",
-            "/spreadsheetresult_tiktak.req.xml",
-            "/spreadsheetresult_tiktak.resp.xml");
+    public void SpreadsheetResult_SOAP() throws InterruptedException {
+        client.post("/spreadsheetresult", "/spreadsheetresult_tiktak.req.xml", "/spreadsheetresult_tiktak.resp.xml");
+        client.post("/EPBDS-9437", "/EPBDS-9437_tiktak.req.xml", "/EPBDS-9437_tiktak.resp.xml");
+        client.post("/EPBDS-9437", "/EPBDS-9437_arr.req.xml", "/EPBDS-9437_arr.resp.xml");
     }
 }
