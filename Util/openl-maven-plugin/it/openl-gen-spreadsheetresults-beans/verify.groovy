928e615e9720cbf39f3daee2b4363fa7eeffd3ec
Marat Kamalov <mkamalov@eisgroup.com>
2020-03-20 19:50:28 +0300

    EPBDS-9556 Maven Plugin test was renamed

--- Util/openl-maven-plugin/it/openl-gen-spreadsheetresults-beans/verify.groovy
+++ Util/openl-maven-plugin/it/openl-gen-spreadsheetresults-beans/verify.groovy
@@ -6,9 +6,9 @@ try {
     assert new File(basedir, 'target/classes/org/openl/generated/outputmodel/Spr1.class').exists()
     assert new File(basedir, 'target/classes/org/openl/generated/outputmodel/RunMain.class').exists()
 
-    assert new File(basedir, 'target/openl-gen-csr-beans-0.0.0.zip').exists()
+    assert new File(basedir, 'target/openl-gen-spreadsheetresults-beans-0.0.0.zip').exists()
 
-    assert new File(basedir, 'target').list({File file, String name -> name.startsWith("openl-gen-csr-beans-0.0.0") && name.endsWith("-lib.jar")}).length == 1
+    assert new File(basedir, 'target').list({File file, String name -> name.startsWith("openl-gen-spreadsheetresults-beans-0.0.0") && name.endsWith("-lib.jar")}).length == 1
 
     int threadCount = Runtime.runtime.availableProcessors() * 2.5
     assert new File(basedir, 'build.log').text.contains("Run tests using $threadCount threads.")
