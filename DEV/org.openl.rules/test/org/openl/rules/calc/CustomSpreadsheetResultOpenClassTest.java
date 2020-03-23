771e82692dcd8852b75a2d48f1a12ce94dc41b31
Marat Kamalov <mkamalov@eisgroup.com>
2020-03-23 12:31:43 +0300

    EPBDS-9066 "Something went wrong" error appear on expanding SpreadsheetResult input item

--- DEV/org.openl.rules/test/org/openl/rules/calc/CustomSpreadsheetResultOpenClassTest.java
+++ DEV/org.openl.rules/test/org/openl/rules/calc/CustomSpreadsheetResultOpenClassTest.java
@@ -6,7 +6,7 @@ import org.openl.types.java.JavaOpenClass;
 public class CustomSpreadsheetResultOpenClassTest {
     @Test
     public void test() {
-        CustomSpreadsheetResultOpenClass openClass = new CustomSpreadsheetResultOpenClass("CSR1", null);
+        CustomSpreadsheetResultOpenClass openClass = new CustomSpreadsheetResultOpenClass("CSR1", null, null);
         openClass.getField("$f1", false);
         openClass.addField(new CustomSpreadsheetResultField(null, "$f1", JavaOpenClass.OBJECT));
         openClass.addField(new CustomSpreadsheetResultField(null, "$F1", JavaOpenClass.OBJECT));
