f589128adff2b5ecf117453ca0b7b0ccf514f76e
Yauheni Biruk <ybiruk@eisgroup.com>
2020-03-26 10:01:50 +0300

    EPBDS-7311 add created_at, created_by info to cache
    --HG--
    branch : EPBDS-7311

--- STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/web/repository/cache/ProjectVersionCacheDB.java
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/web/repository/cache/ProjectVersionCacheDB.java
@@ -4,8 +4,10 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Timestamp;
 
 import org.openl.rules.common.ProjectVersion;
+import org.openl.rules.webstudio.WebStudioFormats;
 import org.openl.util.db.SqlDBUtils;
 
 public class ProjectVersionCacheDB extends H2CacheDB {
@@ -15,41 +17,51 @@ public class ProjectVersionCacheDB extends H2CacheDB {
         DEPLOY
     }
 
-    private static final String CREATE_QUERY = "CREATE TABLE IF NOT EXISTS VERSION_HASHES(project_name varchar(255),version varchar(255), created_at TIMESTAMP, created_by varchar(255), hash varchar(255), repository varchar(255))";
-    private static final String SELECT_VERSION_QUERY = "select version from VERSION_HASHES WHERE created_at=(SELECT MAX(created_at) FROM VERSION_HASHES WHERE project_name=? and hash=? and repository=?)";
-    private static final String SELECT_HASH_QUERY = "select hash from VERSION_HASHES WHERE created_at=(SELECT MAX(created_at) FROM VERSION_HASHES WHERE project_name=? and version=? and repository=?)";
-    private static final String INSERT_QUERY = "INSERT INTO VERSION_HASHES" + "(project_name, version, created_at, created_by, hash, repository) values" + "(?,?,?,?,?,?)";
-    private static final String SELECT_COUNT_QUERY = "select COUNT(*) from VERSION_HASHES";
+    private static final String CACHE_NAME = "projectsCache";
 
-    private static final String VERSION_FIELD = "version";
-    private static final String HASH_FIELD = "hash";
+    // cache fields
+    private static final String TABLE_NAME = "VERSION_HASHES";
+    private static final String PROJECT_NAME = "project_name";
+    private static final String VERSION = "version";
+    private static final String HASH = "hash";
+    private static final String CREATED_AT = "created_at";
+    private static final String CREATED_BY = "created_by";
+    private static final String REPOSITORY = "repository";
+
+    private static final String CREATE_QUERY = "CREATE TABLE IF NOT EXISTS " + TABLE_NAME + "(" + PROJECT_NAME + " varchar(50)," + VERSION + " varchar(50), " + CREATED_AT + " TIMESTAMP, " + CREATED_BY + " varchar(50), " + HASH + " varchar(32), " + REPOSITORY + " varchar(6))";
+    private static final String SELECT_VERSION_QUERY = "select " + VERSION + " from " + TABLE_NAME + " WHERE " + CREATED_AT + "=(SELECT MAX(" + CREATED_AT + ") FROM " + TABLE_NAME + " WHERE " + PROJECT_NAME + "=? and " + HASH + "=? and " + REPOSITORY + "=?)";
+    private static final String SELECT_HASH_QUERY = "select " + HASH + " from " + TABLE_NAME + " WHERE " + CREATED_AT + "=(SELECT MAX(" + CREATED_AT + ") FROM " + TABLE_NAME + " WHERE " + PROJECT_NAME + "=? and " + VERSION + "=? and " + REPOSITORY + "=?)";
+    private static final String SELECT_DESIGN_VERSION_QUERY = "select " + CREATED_AT + ", " + CREATED_BY + " from " + TABLE_NAME + " WHERE " + CREATED_AT + "=(SELECT MAX(" + CREATED_AT + ") FROM " + TABLE_NAME + " WHERE " + PROJECT_NAME + "=? and " + HASH + "=? and " + REPOSITORY + "=?)";
+    private static final String INSERT_QUERY = "INSERT INTO " + TABLE_NAME + "" + "(" + PROJECT_NAME + ", " + VERSION + ", " + CREATED_AT + ", " + CREATED_BY + ", " + HASH + ", " + REPOSITORY + ") values" + "(?,?,?,?,?,?)";
+    private static final String SELECT_COUNT_QUERY = "select COUNT(*) from " + TABLE_NAME;
 
     public ProjectVersionCacheDB() {
-        super("projectsCache");
+        super(CACHE_NAME);
     }
 
     public String getHash(String name, String version, RepoType repoType) throws IOException {
-        return getProjectData(name, version, repoType, HASH_FIELD, SELECT_HASH_QUERY);
+        return getProjectData(name, version, repoType, HASH, SELECT_HASH_QUERY);
     }
 
     public String getVersion(String name, String hash, RepoType repoType) throws IOException {
-        return getProjectData(name, hash, repoType, VERSION_FIELD, SELECT_VERSION_QUERY);
+        return getProjectData(name, hash, repoType, VERSION, SELECT_VERSION_QUERY);
     }
 
     public void insertProject(String projectName,
             ProjectVersion version,
             String hash,
             RepoType repoType) throws IOException {
-        ensureCacheIsExist();
         Connection connection = null;
         PreparedStatement insertPreparedStatement = null;
         try {
             connection = getDBConnection();
+            ensureCacheIsExist(connection);
             connection.setAutoCommit(false);
             insertPreparedStatement = connection.prepareStatement(INSERT_QUERY);
             insertPreparedStatement.setString(1, projectName);
             insertPreparedStatement.setString(2, version.getVersionName());
-            insertPreparedStatement.setTimestamp(3, new java.sql.Timestamp(version.getVersionInfo().getCreatedAt().getTime()));
+            insertPreparedStatement.setTimestamp(3,
+                new java.sql.Timestamp(version.getVersionInfo().getCreatedAt().getTime()));
             insertPreparedStatement.setString(4, version.getVersionInfo().getCreatedBy());
             insertPreparedStatement.setString(5, hash);
             insertPreparedStatement.setString(6, repoType.name());
@@ -65,14 +77,13 @@ public class ProjectVersionCacheDB extends H2CacheDB {
     }
 
     public boolean isCacheEmpty() throws IOException {
-        ensureCacheIsExist();
         int count = 0;
         Connection connection = null;
         PreparedStatement selectPreparedStatement = null;
         ResultSet rs = null;
-
         try {
             connection = getDBConnection();
+            ensureCacheIsExist(connection);
             connection.setAutoCommit(false);
             selectPreparedStatement = connection.prepareStatement(SELECT_COUNT_QUERY);
             rs = selectPreparedStatement.executeQuery();
@@ -92,11 +103,45 @@ public class ProjectVersionCacheDB extends H2CacheDB {
         return count == 0;
     }
 
-    private void ensureCacheIsExist() throws IOException {
+    public String getDesignBusinessVersion(String name, String hash, RepoType repoType) throws IOException {
         Connection connection = null;
-        PreparedStatement createPreparedStatement = null;
+        ResultSet rs = null;
+        PreparedStatement selectPreparedStatement = null;
         try {
             connection = getDBConnection();
+            ensureCacheIsExist(connection);
+            selectPreparedStatement = connection.prepareStatement(SELECT_DESIGN_VERSION_QUERY);
+            selectPreparedStatement.setString(1, name);
+            selectPreparedStatement.setString(2, hash);
+            selectPreparedStatement.setString(3, repoType.name());
+            connection.setAutoCommit(false);
+            rs = selectPreparedStatement.executeQuery();
+            Timestamp createdAt = null;
+            String createdBy = null;
+            while (rs.next()) {
+                createdAt = rs.getTimestamp(CREATED_AT);
+                createdBy = rs.getString(CREATED_BY);
+            }
+            String businessVersion = null;
+            if (createdAt != null && createdBy != null) {
+                String modifiedOnStr = WebStudioFormats.getInstance().formatDateTime(createdAt);
+                businessVersion = createdBy + ": " + modifiedOnStr;
+            }
+            selectPreparedStatement.close();
+            connection.commit();
+            return businessVersion;
+        } catch (Exception e) {
+            throw new IOException(e);
+        } finally {
+            SqlDBUtils.safeClose(rs);
+            SqlDBUtils.safeClose(selectPreparedStatement);
+            SqlDBUtils.safeClose(connection);
+        }
+    }
+
+    private void ensureCacheIsExist(Connection connection) throws IOException {
+        PreparedStatement createPreparedStatement = null;
+        try {
             connection.setAutoCommit(false);
             createPreparedStatement = connection.prepareStatement(CREATE_QUERY);
             createPreparedStatement.executeUpdate();
@@ -106,21 +151,20 @@ public class ProjectVersionCacheDB extends H2CacheDB {
             throw new IOException(e);
         } finally {
             SqlDBUtils.safeClose(createPreparedStatement);
-            SqlDBUtils.safeClose(connection);
         }
     }
 
     private String getProjectData(String name,
             String field,
             RepoType repoType,
-            String versionField,
+            String resultField,
             String query) throws IOException {
-        ensureCacheIsExist();
         Connection connection = null;
         ResultSet rs = null;
         PreparedStatement selectPreparedStatement = null;
         try {
             connection = getDBConnection();
+            ensureCacheIsExist(connection);
             selectPreparedStatement = connection.prepareStatement(query);
             selectPreparedStatement.setString(1, name);
             selectPreparedStatement.setString(2, field);
@@ -129,7 +173,7 @@ public class ProjectVersionCacheDB extends H2CacheDB {
             rs = selectPreparedStatement.executeQuery();
             String version = null;
             while (rs.next()) {
-                version = rs.getString(versionField);
+                version = rs.getString(resultField);
             }
             selectPreparedStatement.close();
             connection.commit();
