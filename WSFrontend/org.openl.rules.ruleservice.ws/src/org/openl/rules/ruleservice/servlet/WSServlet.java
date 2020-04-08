8aa818978f3962895dfd4ce6365cf6018edcafd0
Yury Molchan <ymolchan@eisgroup.com>
2020-04-08 09:56:42 +0300

    EPBDS-9579 Fix WebService admin page on Jetty

--- WSFrontend/org.openl.rules.ruleservice.ws/src/org/openl/rules/ruleservice/servlet/WSServlet.java
+++ WSFrontend/org.openl.rules.ruleservice.ws/src/org/openl/rules/ruleservice/servlet/WSServlet.java
@@ -6,7 +6,7 @@ import javax.servlet.annotation.WebServlet;
 import org.apache.cxf.transport.servlet.CXFServlet;
 
 @WebServlet(value = "/*", loadOnStartup = 1, initParams = {
-        @WebInitParam(name = "static-welcome-file", value = "index.html"),
+        @WebInitParam(name = "static-welcome-file", value = "/index.html"),
         @WebInitParam(name = "static-resources-list", value = "/favicon.ico"),
         @WebInitParam(name = "service-list-path", value = "cxf-services"),
         @WebInitParam(name = "hide-service-list-page", value = "true") })
