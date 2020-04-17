cc0938b5ec5ff5ca88362aa01d4a1f430359383c
Nail Samatov <nsamatov@eisgroup.com>
2020-04-17 23:11:18 +0300

    Fix imports after merge conflict
    --HG--
    branch : EPBDS-7033

--- STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/web/tableeditor/ShowMessageBean.java
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/web/tableeditor/ShowMessageBean.java
@@ -4,9 +4,6 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
-
 import org.openl.message.OpenLMessage;
 import org.openl.message.Severity;
 import org.openl.rules.webstudio.web.util.WebStudioUtils;
