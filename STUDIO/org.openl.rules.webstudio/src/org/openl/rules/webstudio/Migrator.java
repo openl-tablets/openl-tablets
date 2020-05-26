589b5c5c35a812da13318c7e86032b21d982b14d
Yury Molchan <ymolchan@eisgroup.com>
2020-05-26 17:17:33 +0300

    EPBDS-9851 EPBDS-9698 Migrate project.history.unlimited property

---
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/Migrator.java
@@ -0,0 +1,26 @@
+package org.openl.rules.webstudio;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import org.openl.info.OpenLVersion;
+import org.openl.rules.webstudio.web.Props;
+import org.openl.spring.env.DynamicPropertySource;
+
+/**
+ * For setting migration purposes. It cleans up default settings and reconfigure user defined properties.
+ * 
+ * @author Yury Molchan
+ */
+public class Migrator {
+
+    public void migrate() throws IOException {
+        HashMap<String, String> props = new HashMap<>();
+        if (Props.bool("project.history.unlimited")) {
+            props.put("project.history.count", ""); // Define unlimited
+        }
+        props.put("project.history.unlimited", null); // Remove
+        props.put(".version", OpenLVersion.getBuildNumber()); // Mark the file version
+        DynamicPropertySource.get().save(props);
+    }
+}
