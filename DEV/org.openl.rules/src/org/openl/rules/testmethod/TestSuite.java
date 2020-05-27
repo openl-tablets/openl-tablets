97e22e230ed669629249a0de888d8e91ec9a20c3
Yauheni Biruk <ybiruk@eisgroup.com>
2020-05-27 17:55:01 +0300

    EPBDS-9432 build fix

--- DEV/org.openl.rules/src/org/openl/rules/testmethod/TestSuite.java
+++ DEV/org.openl.rules/src/org/openl/rules/testmethod/TestSuite.java
@@ -4,6 +4,7 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Executor;
 
 import org.openl.base.INamedThing;
+import org.openl.rules.lang.xls.syntax.TableSyntaxNode;
 import org.openl.rules.method.ExecutableRulesMethod;
 import org.openl.rules.table.OpenLArgumentsCloner;
 import org.openl.rules.vm.SimpleRulesVM;
@@ -63,11 +64,13 @@ public class TestSuite implements INamedThing {
         final CountDownLatch countDownLatch = new CountDownLatch(THREAD_COUNT);
         final ITestUnit[] testUnitResultsArray = new ITestUnit[getNumberOfTests()];
 
-        TestSuiteMethod testSuiteMethod = getTestSuiteMethod();
-        if (testSuiteMethod instanceof ExecutableRulesMethod && ((ExecutableRulesMethod) testSuiteMethod
-            .getTestedMethod()).getSyntaxNode().getErrors().length > 0) {
-            testUnitResults.setTestedRulesHaveErrors(true);
-            return testUnitResults;
+        IOpenMethod testedMethod = getTestSuiteMethod().getTestedMethod();
+        if (testedMethod instanceof ExecutableRulesMethod) {
+            TableSyntaxNode syntaxNode = ((ExecutableRulesMethod) testedMethod).getSyntaxNode();
+            if (syntaxNode.getErrors().length > 0) {
+                testUnitResults.setTestedRulesHaveErrors(true);
+                return testUnitResults;
+            }
         }
 
         for (int i = 0; i < THREAD_COUNT; i++) {
