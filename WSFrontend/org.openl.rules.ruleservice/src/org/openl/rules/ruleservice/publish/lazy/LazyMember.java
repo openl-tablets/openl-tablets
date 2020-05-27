5d41c3c720b2689e5882f602c5344b0cb906731a
Yury Molchan <ymolchan@eisgroup.com>
2020-05-27 04:15:14 +0300

    EPBDS-9826 Refactoring. Inline logic in the single place

--- WSFrontend/org.openl.rules.ruleservice/src/org/openl/rules/ruleservice/publish/lazy/LazyMember.java
+++ WSFrontend/org.openl.rules.ruleservice/src/org/openl/rules/ruleservice/publish/lazy/LazyMember.java
@@ -1,22 +1,6 @@
 package org.openl.rules.ruleservice.publish.lazy;
 
-import java.util.Collections;
-import java.util.Map;
-
-import org.openl.CompiledOpenClass;
-import org.openl.exception.OpenLCompilationException;
-import org.openl.rules.lang.xls.prebind.IPrebindHandler;
-import org.openl.rules.lang.xls.prebind.XlsLazyModuleOpenClass;
-import org.openl.rules.project.dependencies.ProjectExternalDependenciesHelper;
-import org.openl.rules.project.instantiation.RulesInstantiationStrategy;
-import org.openl.rules.project.instantiation.RulesInstantiationStrategyFactory;
-import org.openl.rules.project.model.Module;
-import org.openl.rules.ruleservice.core.DeploymentDescription;
-import org.openl.rules.ruleservice.core.MaxThreadsForCompileSemaphore;
-import org.openl.rules.ruleservice.core.RuleServiceDependencyManager;
 import org.openl.types.IOpenMember;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * Lazy IOpenMember that contains info about module where it was declared. When we try to do some operations with lazy
@@ -25,23 +9,8 @@ import org.slf4j.LoggerFactory;
  * @author Marat Kamalov
  */
 public abstract class LazyMember<T extends IOpenMember> {
-    private final Logger log = LoggerFactory.getLogger(LazyMember.class);
 
-    private final RuleServiceDependencyManager dependencyManager;
-
-    /**
-     * ClassLoader used in "lazy" compilation. It should be reused because it contains generated classes for
-     * datatypes.(If we use different ClassLoaders we can get ClassCastException because generated classes for datatypes
-     * have been loaded by different ClassLoaders).
-     */
-    private final ClassLoader classLoader;
     private volatile T cachedMember;
-
-    LazyMember(RuleServiceDependencyManager dependencyManager,
-               ClassLoader classLoader) {
-        this.dependencyManager = dependencyManager;
-        this.classLoader = classLoader;
-    }
 
     protected abstract T initMember();
 
@@ -56,79 +25,4 @@ public abstract class LazyMember<T extends IOpenMember> {
     void clearCachedMember() {
         cachedMember = null;
     }
-
-    CompiledOpenClass getCompiledOpenClassWithThrowErrorExceptionsIfAny() throws Exception {
-        CompiledOpenClass compiledOpenClass = getCompiledOpenClass();
-        if (compiledOpenClass.hasErrors()) {
-            compiledOpenClass.throwErrorExceptionsIfAny();
-        }
-        return compiledOpenClass;
-    }
-
-    private CompiledOpenClass getCompiledOpenClass() throws Exception {
-        final Module module = getModule();
-        DeploymentDescription deployment = getDeployment();
-        CompiledOpenClass compiledOpenClass = CompiledOpenClassCache.getInstance().get(deployment, module.getName());
-        if (compiledOpenClass != null) {
-            return compiledOpenClass;
-        }
-
-        synchronized (getXlsLazyModuleOpenClass()) {
-            compiledOpenClass = CompiledOpenClassCache.getInstance().get(deployment, module.getName());
-            if (compiledOpenClass != null) {
-                return compiledOpenClass;
-            }
-            try {
-                return MaxThreadsForCompileSemaphore.getInstance().run(() -> {
-                    CompiledOpenClass compiledOpenClass1 = null;
-                    IPrebindHandler prebindHandler = LazyBinderMethodHandler.getPrebindHandler();
-                    try {
-                        LazyBinderMethodHandler.removePrebindHandler();
-                        RulesInstantiationStrategy rulesInstantiationStrategy = RulesInstantiationStrategyFactory
-                            .getStrategy(module, true, dependencyManager, classLoader);
-                        rulesInstantiationStrategy.setServiceClass(EmptyInterface.class);// Prevent
-                        Map<String, Object> parameters = ProjectExternalDependenciesHelper
-                            .getExternalParamsWithProjectDependencies(dependencyManager.getExternalParameters(),
-                                Collections.singleton(module));
-                        rulesInstantiationStrategy.setExternalParameters(parameters);
-                        compiledOpenClass1 = rulesInstantiationStrategy.compile();
-                        CompiledOpenClassCache.getInstance()
-                            .putToCache(deployment, module.getName(), compiledOpenClass1);
-                        if (log.isDebugEnabled()) {
-                            log.debug(
-                                "CompiledOpenClass for deploymentName='{}', deploymentVersion='{}', dependencyName='{}' was stored to cache.",
-                                deployment.getName(),
-                                deployment.getVersion().getVersionName(),
-                                module.getName());
-                        }
-                        return compiledOpenClass1;
-                    } catch (Exception ex) {
-                        log.error("Failed to load dependency '{}'.", module.getName(), ex);
-                        return compiledOpenClass1;
-                    } finally {
-                        LazyBinderMethodHandler.setPrebindHandler(prebindHandler);
-                    }
-                });
-            } catch (OpenLCompilationException e) {
-                throw e;
-            } catch (InterruptedException e) {
-                throw new OpenLCompilationException("Interrupted exception.", e);
-            } catch (Exception e) {
-                throw new OpenLCompilationException("Failed to compile.", e);
-            }
-        }
-    }
-
-    public abstract XlsLazyModuleOpenClass getXlsLazyModuleOpenClass();
-
-    /**
-     * @return Module containing current member.
-     */
-    public abstract Module getModule();
-
-    /**
-     * @return Deployment containing current module.
-     */
-    public abstract DeploymentDescription getDeployment();
-
 }
--- WSFrontend/org.openl.rules.ruleservice/src/org/openl/rules/ruleservice/publish/lazy/LazyPrebindHandler.java
+++ WSFrontend/org.openl.rules.ruleservice/src/org/openl/rules/ruleservice/publish/lazy/LazyPrebindHandler.java
@@ -3,14 +3,19 @@ package org.openl.rules.ruleservice.publish.lazy;
 import java.io.File;
 import java.nio.file.Paths;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Map;
 
 import org.openl.CompiledOpenClass;
+import org.openl.exception.OpenLCompilationException;
 import org.openl.exception.OpenlNotCheckedException;
 import org.openl.rules.lang.xls.prebind.IPrebindHandler;
-import org.openl.rules.lang.xls.prebind.XlsLazyModuleOpenClass;
+import org.openl.rules.project.dependencies.ProjectExternalDependenciesHelper;
+import org.openl.rules.project.instantiation.RulesInstantiationStrategy;
+import org.openl.rules.project.instantiation.RulesInstantiationStrategyFactory;
 import org.openl.rules.project.model.Module;
 import org.openl.rules.ruleservice.core.DeploymentDescription;
+import org.openl.rules.ruleservice.core.MaxThreadsForCompileSemaphore;
 import org.openl.rules.ruleservice.core.RuleServiceDependencyManager;
 import org.openl.rules.ruleservice.core.RuleServiceOpenLCompilationException;
 import org.openl.rules.ruleservice.publish.lazy.wrapper.LazyWrapperLogic;
@@ -51,26 +56,26 @@ class LazyPrebindHandler implements IPrebindHandler {
             argTypes[i] = method.getSignature().getParameterType(i).getInstanceClass();
         }
         final Map<String, Object> dimensionProperties = (method instanceof ITableProperties) ? PropertiesHelper
-                .getTableProperties(method)
-                .getAllDimensionalProperties() : null;
-        final LazyMember<IOpenMethod> lazyMethod = new LazyMember<IOpenMethod>(dependencyManager, getClassLoader()) {
-
+            .getTableProperties(method)
+            .getAllDimensionalProperties() : null;
+        final LazyMember<IOpenMethod> lazyMethod = new LazyMember<IOpenMethod>() {
 
             protected IOpenMethod initMember() {
                 IOpenMethod openMethod;
                 try {
-                    CompiledOpenClass compiledOpenClass = getCompiledOpenClassWithThrowErrorExceptionsIfAny();
+                    CompiledOpenClass compiledOpenClass = getCompiledOpenClassWithThrowErrorExceptionsIfAny(method,
+                        module);
                     openMethod = OpenClassHelper
-                            .findRulesMethod(compiledOpenClass.getOpenClass(), method.getName(), argTypes);
+                        .findRulesMethod(compiledOpenClass.getOpenClass(), method.getName(), argTypes);
                     if (openMethod instanceof OpenMethodDispatcher && dimensionProperties != null) {
                         OpenMethodDispatcher openMethodDispatcher = (OpenMethodDispatcher) openMethod;
                         for (IOpenMethod candidate : openMethodDispatcher.getCandidates()) {
                             if (candidate instanceof ITableProperties) {
                                 Map<String, Object> candidateDimensionProperties = PropertiesHelper
-                                        .getTableProperties(candidate)
-                                        .getAllDimensionalProperties();
+                                    .getTableProperties(candidate)
+                                    .getAllDimensionalProperties();
                                 if (DimensionPropertiesMethodKey.compareMethodDimensionProperties(dimensionProperties,
-                                        candidateDimensionProperties)) {
+                                    candidateDimensionProperties)) {
                                     openMethod = candidate;
                                     break;
                                 }
@@ -83,20 +88,6 @@ class LazyPrebindHandler implements IPrebindHandler {
                 return openMethod;
             }
 
-            @Override
-            public DeploymentDescription getDeployment() {
-                return deployment;
-            }
-
-            @Override
-            public Module getModule() {
-                return module;
-            }
-
-            @Override
-            public XlsLazyModuleOpenClass getXlsLazyModuleOpenClass() {
-                return (XlsLazyModuleOpenClass) method.getDeclaringClass();
-            }
         };
         CompiledOpenClassCache.getInstance()
             .registerEvent(deployment, module.getName(), new LazyMemberEvent(lazyMethod));
@@ -106,37 +97,90 @@ class LazyPrebindHandler implements IPrebindHandler {
     @Override
     public IOpenField processPrebindField(final IOpenField field) {
         final Module module = getModuleForMember(field, modules);
-        final LazyMember<IOpenField> lazyField = new LazyMember<IOpenField>(dependencyManager, getClassLoader()) {
+        final LazyMember<IOpenField> lazyField = new LazyMember<IOpenField>() {
 
             protected IOpenField initMember() {
                 try {
-                    CompiledOpenClass compiledOpenClass = getCompiledOpenClassWithThrowErrorExceptionsIfAny();
+                    CompiledOpenClass compiledOpenClass = getCompiledOpenClassWithThrowErrorExceptionsIfAny(field,
+                        module);
                     return compiledOpenClass.getOpenClass().getField(field.getName());
                 } catch (Exception e) {
                     throw new RuleServiceOpenLCompilationException("Failed to load a lazy field.", e);
                 }
             }
 
-            @Override
-            public DeploymentDescription getDeployment() {
-                return deployment;
-            }
-
-            @Override
-            public Module getModule() {
-                return module;
-            }
-
-            @Override
-            public XlsLazyModuleOpenClass getXlsLazyModuleOpenClass() {
-                return (XlsLazyModuleOpenClass) field.getDeclaringClass();
-            }
         };
         CompiledOpenClassCache.getInstance()
             .registerEvent(deployment, module.getName(), new LazyMemberEvent(lazyField));
         return LazyWrapperLogic.wrapField(lazyField, field);
     }
 
+    private CompiledOpenClass getCompiledOpenClassWithThrowErrorExceptionsIfAny(IOpenMember sync,
+            Module module) throws Exception {
+        CompiledOpenClass compiledOpenClass = getCompiledOpenClass(sync, module);
+        if (compiledOpenClass.hasErrors()) {
+            compiledOpenClass.throwErrorExceptionsIfAny();
+        }
+        return compiledOpenClass;
+    }
+
+    private CompiledOpenClass getCompiledOpenClass(IOpenMember sync, Module module) throws Exception {
+        CompiledOpenClass compiledOpenClass = CompiledOpenClassCache.getInstance().get(deployment, module.getName());
+        if (compiledOpenClass != null) {
+            return compiledOpenClass;
+        }
+
+        synchronized (sync.getDeclaringClass()) {
+            compiledOpenClass = CompiledOpenClassCache.getInstance().get(deployment, module.getName());
+            if (compiledOpenClass != null) {
+                return compiledOpenClass;
+            }
+            try {
+                return MaxThreadsForCompileSemaphore.getInstance().run(() -> {
+                    CompiledOpenClass compiledOpenClass1 = null;
+                    IPrebindHandler prebindHandler = LazyBinderMethodHandler.getPrebindHandler();
+                    try {
+                        LazyBinderMethodHandler.removePrebindHandler();
+                        RulesInstantiationStrategy rulesInstantiationStrategy = RulesInstantiationStrategyFactory
+                            .getStrategy(module, true, dependencyManager, getClassLoader());
+                        rulesInstantiationStrategy.setServiceClass(EmptyInterface.class);// Prevent
+                        Map<String, Object> parameters = ProjectExternalDependenciesHelper
+                            .getExternalParamsWithProjectDependencies(dependencyManager.getExternalParameters(),
+                                Collections.singleton(module));
+                        rulesInstantiationStrategy.setExternalParameters(parameters);
+                        compiledOpenClass1 = rulesInstantiationStrategy.compile();
+                        CompiledOpenClassCache.getInstance()
+                            .putToCache(deployment, module.getName(), compiledOpenClass1);
+                        if (LOG.isDebugEnabled()) {
+                            LOG.debug(
+                                "CompiledOpenClass for deploymentName='{}', deploymentVersion='{}', dependencyName='{}' was stored to cache.",
+                                deployment.getName(),
+                                deployment.getVersion().getVersionName(),
+                                module.getName());
+                        }
+                        return compiledOpenClass1;
+                    } catch (Exception ex) {
+                        LOG.error("Failed to load dependency '{}'.", module.getName(), ex);
+                        return compiledOpenClass1;
+                    } finally {
+                        LazyBinderMethodHandler.setPrebindHandler(prebindHandler);
+                    }
+                });
+            } catch (OpenLCompilationException e) {
+                throw e;
+            } catch (InterruptedException e) {
+                throw new OpenLCompilationException("Interrupted exception.", e);
+            } catch (Exception e) {
+                throw new OpenLCompilationException("Failed to compile.", e);
+            }
+        }
+    }
+
+    /**
+     * ClassLoader used in "lazy" compilation. It should be reused because it contains generated classes for
+     * datatypes.(If we use different ClassLoaders we can get ClassCastException because generated classes for datatypes
+     * have been loaded by different ClassLoaders).
+     */
     private ClassLoader getClassLoader() {
         return classLoader == null ? Thread.currentThread().getContextClassLoader() : classLoader;
     }
