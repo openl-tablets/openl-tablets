9a9e7ef4f61559f08a8c7cd077e131b9c0d00907
Yury Molchan <ymolchan@eisgroup.com>
2020-05-29 01:01:43 +0300

    EPBDS-9858 Remove spring-web to reduce scanning scope under JBoss

--- WSFrontend/org.openl.rules.ruleservice.ws/src/org/openl/rules/ruleservice/servlet/CXFServlet.java
+++ WSFrontend/org.openl.rules.ruleservice.ws/src/org/openl/rules/ruleservice/servlet/CXFServlet.java
@@ -1,14 +1,24 @@
 package org.openl.rules.ruleservice.servlet;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 
-import org.apache.cxf.transport.servlet.CXFServlet;
+import org.apache.cxf.Bus;
+import org.apache.cxf.transport.servlet.CXFNonSpringServlet;
+import org.springframework.context.ApplicationContext;
 
 @WebServlet(value = "/*", loadOnStartup = 1, initParams = {
         @WebInitParam(name = "static-welcome-file", value = "/index.html"),
         @WebInitParam(name = "static-resources-list", value = "/favicon.ico"),
         @WebInitParam(name = "service-list-path", value = "cxf-services"),
         @WebInitParam(name = "hide-service-list-page", value = "true") })
-public class WSServlet extends CXFServlet {
+public class CXFServlet extends CXFNonSpringServlet {
+
+    @Override
+    protected void loadBus(ServletConfig servletConfig) {
+        ApplicationContext ac = SpringInitializer.getApplicationContext(servletConfig.getServletContext());
+        Bus cxf = ac.getBean("cxf", Bus.class);
+        setBus(cxf);
+    }
 }
