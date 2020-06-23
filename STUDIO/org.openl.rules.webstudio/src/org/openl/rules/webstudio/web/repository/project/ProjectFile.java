6d6f0b90fafa8005e15a21358e734a43081d16e1
Yauheni Biruk <ybiruk@eisgroup.com>
2020-06-23 13:33:23 +0300

    EPBDS-9993 EPBDS-9996 fix deploy Can't create project on JBOSS and Websphere

--- STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/web/repository/project/ProjectFile.java
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/web/repository/project/ProjectFile.java
@@ -2,10 +2,12 @@ package org.openl.rules.webstudio.web.repository.project;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
 import org.openl.util.FileUtils;
+import org.openl.util.IOUtils;
 import org.richfaces.model.UploadedFile;
 
 public class ProjectFile {
@@ -23,9 +25,9 @@ public class ProjectFile {
     public ProjectFile(UploadedFile uploadedFile) throws IOException {
         this.name = FileUtils.getName(uploadedFile.getName());
         this.size = uploadedFile.getSize();
-
         this.tempFile = File.createTempFile("openl-upload", null);
-        uploadedFile.write(tempFile.getPath());
+        IOUtils.copyAndClose(uploadedFile.getInputStream(), new FileOutputStream(this.tempFile));
+        uploadedFile.delete();
     }
 
     public String getName() {
