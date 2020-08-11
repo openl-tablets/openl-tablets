baa52af9282d000d0ed5bbd947085c5b8c383bcd
Yury Molchan <ymolchan@eisgroup.com>
2020-08-11 23:07:41 +0300

    EPBDS-9977 EPBDS-10191 EPBDS-10192 Rework UI to do not use RichFaces components
    REST + JavaScript (jQuery) + HTML + CSS3

---
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/rest/NotificationService.java
@@ -0,0 +1,48 @@
+package org.openl.rules.rest;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.openl.rules.webstudio.web.Props;
+import org.openl.util.StringUtils;
+import org.springframework.stereotype.Service;
+
+@Service
+@Produces(MediaType.TEXT_PLAIN)
+@Consumes(MediaType.TEXT_PLAIN)
+public class NotificationService {
+
+    private final java.nio.file.Path NOTIFICATION_FILE = Paths.get(Props.text("admin.notification-file"));
+
+    @GET
+    @Path("/public/notification.txt")
+    public String getNotification() throws IOException {
+        if (!Files.exists(NOTIFICATION_FILE)) {
+            return null;
+        }
+        try (Stream<String> lines = Files.lines(NOTIFICATION_FILE)) {
+            return lines.collect(Collectors.joining("\r\n"));
+        }
+    }
+
+    @POST
+    @Path("/admin/notification.txt")
+    public void postNotification(String notification) throws IOException {
+        if (StringUtils.isBlank(notification)) {
+            Files.deleteIfExists(NOTIFICATION_FILE);
+        } else {
+            Files.write(NOTIFICATION_FILE, notification.getBytes(StandardCharsets.UTF_8));
+        }
+    }
+}
