01e639ad23fb29b1ba16186b789edbd8e69d1436
Yauheni Biruk <ybiruk@eisgroup.com>
2020-09-08 11:12:46 +0300

    EPBDS-10300 remove module from list after deleting

--- STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/web/ProjectBean.java
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/web/ProjectBean.java
@@ -472,7 +472,7 @@ public class ProjectBean {
             clean(newProjectDescriptor);
             save(newProjectDescriptor);
         }
-
+        studio.resetProjects();
     }
 
     public void removeDependency(String name) {
