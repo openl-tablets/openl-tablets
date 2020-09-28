664dd378964c87bf2d5e4a5724615b726e0fdfa5
Yury Molchan <ymolchan@eisgroup.com>
2020-09-28 15:58:09 +0300

    Move getDuplicatedMethodMessage() method into UriMemberHelper
    --HG--
    branch : 5.23.x

--- DEV/org.openl.rules/src/org/openl/rules/types/UriMemberHelper.java
+++ DEV/org.openl.rules/src/org/openl/rules/types/UriMemberHelper.java
@@ -2,8 +2,13 @@ package org.openl.rules.types;
 
 import org.openl.binding.exception.DuplicatedFieldException;
 import org.openl.binding.exception.DuplicatedMethodException;
+import org.openl.rules.testmethod.TestSuiteMethod;
+import org.openl.types.IModuleInfo;
 import org.openl.types.IOpenField;
 import org.openl.types.IOpenMethod;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public final class UriMemberHelper {
 
@@ -20,7 +25,7 @@ public final class UriMemberHelper {
     public static void validateMethodDuplication(IOpenMethod method, IOpenMethod existedMethod) {
         if (method instanceof IUriMember && existedMethod instanceof IUriMember) {
             if (isNotTheSame((IUriMember) method, (IUriMember) existedMethod)) {
-                String message = ValidationMessages.getDuplicatedMethodMessage(existedMethod, method);
+                String message = getDuplicatedMethodMessage(existedMethod, method);
                 throw new DuplicatedMethodException(message, existedMethod, method);
             }
         } else {
@@ -36,5 +41,65 @@ public final class UriMemberHelper {
         } else {
             throw new IllegalStateException("Implementation supports only IUriMember.");
         }
+    }
+
+    private static String getDuplicatedMethodMessage(IOpenMethod existedMethod, IOpenMethod newMethod) {
+        String message;// Modules to which methods belongs to
+        List<String> modules = new ArrayList<>();
+        if (newMethod instanceof IModuleInfo) {
+            // Get the name of the module for the newMethod
+            String moduleName = ((IModuleInfo) newMethod).getModuleName();
+            if (moduleName != null) {
+                modules.add(moduleName);
+            }
+        }
+        if (existedMethod instanceof IModuleInfo) {
+            // Get the name of the module for the existedMethod
+            String moduleName = ((IModuleInfo) existedMethod).getModuleName();
+            if (moduleName != null) {
+                modules.add(moduleName);
+            }
+        }
+
+        boolean canBeDispatched = !(existedMethod instanceof TestSuiteMethod);
+
+        if (modules.isEmpty()) {
+            // Case module names where not set to the methods
+            if (canBeDispatched) {
+                message = String.format(
+                    "Method '%s' is already used with the same version, active status, properties set.",
+                    existedMethod.getName());
+            } else {
+                message = String.format("Method '%s' is already used.", existedMethod.getName());
+            }
+        } else {
+            // Case when the module names where set to the methods
+            String modulesString = modules.get(0);
+            if (modules.size() > 1) {
+                if (canBeDispatched) {
+                    message = String.format(
+                        "Method '%s' is already used in modules '%s' and '%s' with the same version, active status, properties set.",
+                        existedMethod.getName(),
+                        modulesString,
+                        modules.get(1));
+                } else {
+                    message = String.format("Method '%s' is already used in modules '%s' and '%s'.",
+                        existedMethod.getName(),
+                        modulesString,
+                        modules.get(1));
+                }
+            } else {
+                if (canBeDispatched) {
+                    message = String.format(
+                        "Method '%s' is already used in module '%s' with the same version, active status, properties set.",
+                        existedMethod.getName(),
+                        modulesString);
+                } else {
+                    message = String
+                        .format("Method '%s' is already used in module '%s'.", existedMethod.getName(), modulesString);
+                }
+            }
+        }
+        return message;
     }
 }
