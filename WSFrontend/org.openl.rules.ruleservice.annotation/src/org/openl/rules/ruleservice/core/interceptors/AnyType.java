b9ffaa75429f976d15eede81610a05ba630805be
Yury Molchan <ymolchan@eisgroup.com>
2020-10-23 17:40:05 +0300

    EPBDS-10595 JavaDocs
    --HG--
    branch : 5.23.x

--- WSFrontend/org.openl.rules.ruleservice.annotation/src/org/openl/rules/ruleservice/core/interceptors/AnyType.java
+++ WSFrontend/org.openl.rules.ruleservice.annotation/src/org/openl/rules/ruleservice/core/interceptors/AnyType.java
@@ -5,6 +5,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * @deprecated Use {@link RulesType} instead.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.PARAMETER)
 @Deprecated
