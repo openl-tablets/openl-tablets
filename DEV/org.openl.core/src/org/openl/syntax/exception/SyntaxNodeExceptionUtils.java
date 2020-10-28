103c3159378d5bb56eb2ce88153f5b1e8f03afb5
Marat Kamalov <mkamalov@eisgroup.com>
2020-10-28 15:30:17 +0300

    EPBDS-10622 Errors are not displayed on top of spreadsheets
    --HG--
    branch : 5.23.x

--- DEV/org.openl.core/src/org/openl/syntax/exception/SyntaxNodeExceptionUtils.java
+++ DEV/org.openl.core/src/org/openl/syntax/exception/SyntaxNodeExceptionUtils.java
@@ -2,6 +2,7 @@ package org.openl.syntax.exception;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 import org.openl.source.IOpenSourceCodeModule;
 import org.openl.syntax.ISyntaxNode;
@@ -55,6 +56,25 @@ public class SyntaxNodeExceptionUtils {
 
     public static SyntaxNodeException createError(Throwable ex, ISyntaxNode syntaxNode) {
         return createError(formatErrorMessage(ex), ex, syntaxNode);
+    }
+
+    public static boolean isErrorPresented(SyntaxNodeException e, SyntaxNodeException[] errors) {
+        for (SyntaxNodeException sne : errors) {
+            if (Objects.equals(sne.getLocation(), e.getLocation()) && Objects.equals(e.getMessage(),
+                sne.getMessage())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isErrorPresented(CompositeSyntaxNodeException e, SyntaxNodeException[] errors) {
+        for (SyntaxNodeException sne : e.getErrors()) {
+            if (isErrorPresented(sne, errors)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     private static String formatErrorMessage(Throwable ex) {
