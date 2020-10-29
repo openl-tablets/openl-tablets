624a6660da308ab83387af559b6c68a35078f725
Nail Samatov <nsamatov@eisgroup.com>
2020-10-29 21:01:46 +0300

    EPBDS-10576 Creating project with the same name as local project creates one project in status "No Changes"

--- STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/web/repository/RepositoryTreeController.java
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/web/repository/RepositoryTreeController.java
@@ -658,7 +658,8 @@ public class RepositoryTreeController {
                 msg = "Project name must not be empty.";
             } else if (!NameChecker.checkName(projectName)) {
                 msg = "Specified name is not a valid project name." + " " + NameChecker.BAD_NAME_MSG;
-            } else if (userWorkspace.getDesignTimeRepository().hasProject(repositoryId, projectName)) {
+            } else if (userWorkspace.getDesignTimeRepository().hasProject(repositoryId, projectName)
+                || userWorkspace.getLocalWorkspace().hasProject(null, projectName)) {
                 msg = "Cannot create project because project with such name already exists.";
             } else {
                 Repository repository = userWorkspace.getDesignTimeRepository().getRepository(repositoryId);
