8db6e7833006def20bf66728b8ee2bc83b770680
Yauheni Biruk <ybiruk@eisgroup.com>
2020-10-30 10:45:18 +0300

    EPBDS-10142 remove unused imports
    --HG--
    branch : 5.23.x

--- STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/web/test/CollectionParameterTreeNode.java
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/web/test/CollectionParameterTreeNode.java
@@ -2,13 +2,9 @@ package org.openl.rules.webstudio.web.test;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 import org.openl.types.IAggregateInfo;
 import org.openl.types.IOpenClass;
