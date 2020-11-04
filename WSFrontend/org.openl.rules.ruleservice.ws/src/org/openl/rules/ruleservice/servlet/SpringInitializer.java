2447137cbe8daca6317986b1b7f3cce475f97961
Yury Molchan <ymolchan@eisgroup.com>
2020-11-04 04:20:39 +0300

    EPBDS-10667 Fix disappearing of projects.
    --HG--
    branch : 5.23.x

--- WSFrontend/org.openl.rules.ruleservice.ws/src/org/openl/rules/ruleservice/servlet/SpringInitializer.java
+++ WSFrontend/org.openl.rules.ruleservice.ws/src/org/openl/rules/ruleservice/servlet/SpringInitializer.java
@@ -25,12 +25,22 @@ public final class SpringInitializer implements ServletContextListener {
         servletContext.log("Initializing Spring root ApplicationContext");
         applicationContext = new ClassPathXmlApplicationContext();
         applicationContext.setId("OpenL_RuleService");
+
+        // If not define classloader at this time, then CXF bus will get random classloader from the current thread
+        // because of CXF bus can be initialized much later lazily. And at that time the classloader in the thread can
+        // be set in one of instances of org.openl.classloader.OpenLBundleClassLoader
+        // So to prevent it we set current classloader which is usually a Web application root class loader.
+        // We don't use classloader of this class because of this class can be packaged outside of the application.
+        // e.g. as a common dependency for the Spring Boot application.
+        applicationContext.setClassLoader(Thread.currentThread().getContextClassLoader());
+
         applicationContext.setConfigLocations("classpath:openl-ruleservice-beans.xml",
             "classpath:openl-ruleservice-store-log-data-beans.xml",
             "classpath:openl-ruleservice-admin-beans.xml",
             "classpath:openl-ruleservice-override-beans.xml");
         new PropertySourcesLoader().initialize(applicationContext, servletContext);
-        applicationContext.addBeanFactoryPostProcessor(bf -> bf.registerSingleton("servletContextPath", servletContext.getContextPath()));
+        applicationContext.addBeanFactoryPostProcessor(
+            bf -> bf.registerSingleton("servletContextPath", servletContext.getContextPath()));
         applicationContext.refresh();
         applicationContext.registerShutdownHook();
         servletContext.setAttribute(THIS, this);
