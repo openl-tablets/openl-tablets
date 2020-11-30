bdc664274e6327b204cbae6f5c08078f30764efc
Yury Molchan <ymolchan@eisgroup.com>
2020-11-30 23:24:23 +0300

    EPBDS-10709 Remove usage of SyntaxNodeExceptionCollector.addSyntaxNodeException()
    --HG--
    branch : 5.23.x

--- DEV/org.openl.core/src/org/openl/syntax/exception/SyntaxNodeExceptionCollector.java
+++ DEV/org.openl.core/src/org/openl/syntax/exception/SyntaxNodeExceptionCollector.java
@@ -3,9 +3,6 @@ package org.openl.syntax.exception;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
-
-import org.openl.util.StringUtils;
 
 public class SyntaxNodeExceptionCollector {
     private final List<SyntaxNodeException> syntaxNodeExceptions = new ArrayList<>();
@@ -20,16 +17,6 @@ public class SyntaxNodeExceptionCollector {
                 Collections.addAll(syntaxNodeExceptions, e.getErrors());
             }
         }
-    }
-
-    public void addSyntaxNodeException(SyntaxNodeException e) {
-        for (SyntaxNodeException sne : syntaxNodeExceptions) {
-            if (Objects.equals(sne.getMessage(), e.getMessage()) && sne.getSourceUri() != null && Objects
-                .equals(sne.getSourceUri(), e.getSourceUri())) {
-                return;
-            }
-        }
-        syntaxNodeExceptions.add(e);
     }
 
     public void throwIfAny(String msg) throws SyntaxNodeException {
