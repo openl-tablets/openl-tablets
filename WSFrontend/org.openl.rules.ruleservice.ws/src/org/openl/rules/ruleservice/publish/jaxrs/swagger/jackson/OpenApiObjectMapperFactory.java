7fa8830e4e73a4a0c3ca32024a6db4c32cce6ddc
Yury Molchan <ymolchan@eisgroup.com>
2020-12-14 00:53:45 +0300

    EPBDS-10891 Build failure fix. Backed out changeset: 88287c48eed7
    --HG--
    branch : 5.23.x

--- WSFrontend/org.openl.rules.ruleservice.ws/src/org/openl/rules/ruleservice/publish/jaxrs/swagger/jackson/OpenApiObjectMapperFactory.java
+++ WSFrontend/org.openl.rules.ruleservice.ws/src/org/openl/rules/ruleservice/publish/jaxrs/swagger/jackson/OpenApiObjectMapperFactory.java
@@ -1,21 +1,16 @@
 package org.openl.rules.ruleservice.publish.jaxrs.swagger.jackson;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-
 import org.openl.rules.serialization.ObjectMapperFactory;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import io.swagger.v3.core.util.Json;
+
 public final class OpenApiObjectMapperFactory implements ObjectMapperFactory {
     @Override
     public ObjectMapper createObjectMapper() {
-        try {
-            Method createJsonMethod = io.swagger.v3.core.util.ObjectMapperFactory.class.getDeclaredMethod("createJson");
-            createJsonMethod.setAccessible(true);
-            return (ObjectMapper) createJsonMethod.invoke(null);
-        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
-            throw new IllegalStateException(e);
-        }
+        // Be careful
+        // It is not the same as OpenApiObjectMapperFactory
+        return Json.mapper();
     }
 }
