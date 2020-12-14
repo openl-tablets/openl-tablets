7d10ef67d71a2f696385bbbb39db6f9dd8b51e27
Marat Kamalov <mkamalov@eisgroup.com>
2020-12-14 13:06:40 +0300

    Backed out changeset: e0cd1a51f7d3
    --HG--
    branch : 5.23.x

--- WSFrontend/org.openl.rules.ruleservice.ws/src/org/openl/rules/ruleservice/publish/jaxrs/swagger/jackson/SwaggerObjectMapperFactory.java
+++ WSFrontend/org.openl.rules.ruleservice.ws/src/org/openl/rules/ruleservice/publish/jaxrs/swagger/jackson/SwaggerObjectMapperFactory.java
@@ -1,14 +1,21 @@
 package org.openl.rules.ruleservice.publish.jaxrs.swagger.jackson;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 import org.openl.rules.serialization.ObjectMapperFactory;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import io.swagger.util.Json;
-
 public final class SwaggerObjectMapperFactory implements ObjectMapperFactory {
     @Override
     public ObjectMapper createObjectMapper() {
-        return Json.mapper();
+        try {
+            Method createJsonMethod = io.swagger.util.ObjectMapperFactory.class.getDeclaredMethod("createJson");
+            createJsonMethod.setAccessible(true);
+            return (ObjectMapper) createJsonMethod.invoke(null);
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            throw new IllegalStateException(e);
+        }
     }
 }
