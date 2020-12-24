6d20e47da0fdaaa6f28529e747e6efaee741d005
Nail Samatov <nsamatov@eisgroup.com>
2020-12-24 18:53:03 +0300

    Add test for sys.json

--- ITEST/server-core/src/org/openl/itest/core/HttpClient.java
+++ ITEST/server-core/src/org/openl/itest/core/HttpClient.java
@@ -119,6 +119,10 @@ public class HttpClient {
         send(HttpMethod.GET, url, null, status, NO_BODY);
     }
 
+    public void getAny(String url, int status) {
+        send(HttpMethod.GET, url, null, status, ANY_BODY);
+    }
+
     public void get(String url, int status, String responseFile) {
         send(HttpMethod.GET, url, null, status, responseFile);
     }
