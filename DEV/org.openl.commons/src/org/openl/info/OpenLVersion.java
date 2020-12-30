e804660ba6e3614f776e8d13e69160a77ea480da
Yury Molchan <ymolchan@eisgroup.com>
2020-12-30 13:13:40 +0300

    EPBDS-10976 Use buildDate for rendering copyright year. Remove outdated information.

--- DEV/org.openl.commons/src/org/openl/info/OpenLVersion.java
+++ DEV/org.openl.commons/src/org/openl/info/OpenLVersion.java
@@ -39,15 +39,17 @@ public final class OpenLVersion {
             }
         }
         url = props.getProperty("openl.url", "??");
+        String email = props.getProperty("openl.email");
         version = props.getProperty("openl.version", "???");
         buildDate = props.getProperty("openl.build.date", "????-??-??");
         buildNumber = props.getProperty("openl.commit.hash", "????");
 
         HashMap<String, String> source = new HashMap<>(6);
-        source.put("openl.site", OpenLVersion.getUrl());
-        source.put("openl.version", OpenLVersion.getVersion());
-        source.put("openl.build.date", OpenLVersion.getBuildDate());
-        source.put("openl.build.number", OpenLVersion.getBuildNumber());
+        source.put("openl.site", url);
+        source.put("openl.email", email);
+        source.put("openl.version", version);
+        source.put("openl.build.date", buildDate);
+        source.put("openl.build.number", buildNumber);
         source.put("openl.start.time", ZonedDateTime.now().toString());
         source.put("openl.start.milli", Long.toString(Instant.now().toEpochMilli()));
         info = Collections.unmodifiableMap(source);
