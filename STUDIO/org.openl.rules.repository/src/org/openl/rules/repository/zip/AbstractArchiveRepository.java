69be9aa2dfb03bf6d4463c81187b9374f97b7451
Vladyslav Pikus <vpikus@eisgroup.com>
2021-01-29 17:13:43 +0200

    EPBDS-11087 fix compilation for 5.23.x
    --HG--
    branch : 5.23.x

--- STUDIO/org.openl.rules.repository/src/org/openl/rules/repository/zip/AbstractArchiveRepository.java
+++ STUDIO/org.openl.rules.repository/src/org/openl/rules/repository/zip/AbstractArchiveRepository.java
@@ -1,7 +1,6 @@
 package org.openl.rules.repository.zip;
 
 import java.io.Closeable;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
@@ -10,16 +9,13 @@ import java.net.URI;
 import java.nio.file.FileSystem;
 import java.nio.file.FileSystemNotFoundException;
 import java.nio.file.FileSystems;
-import java.nio.file.FileVisitOption;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
@@ -52,7 +48,7 @@ import org.openl.util.ZipUtils;
  *
  * @author Vladyslav Pikus
  */
-abstract class AbstractArchiveRepository implements FolderRepository, Closeable {
+public abstract class AbstractArchiveRepository implements FolderRepository, Closeable {
 
     private static final int REGULAR_ARCHIVE_FILE_SIGN = 0x504B0304;
     private static final int EMPTY_ARCHIVE_FILE_SIGN = 0x504B0506;
