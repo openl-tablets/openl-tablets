f7073b6ea228bca467da8c5d72f0c44a8c5dd0a5
Marat Kamalov <mkamalov@eisgroup.com>
2021-02-18 22:01:26 +0300

    EPBDS-11153 Webstudio Performance degradation in big project. Refactoring.

--- DEV/org.openl.commons/src/org/openl/util/StringTool.java
+++ DEV/org.openl.commons/src/org/openl/util/StringTool.java
@@ -30,16 +30,14 @@ public class StringTool {
 
     // TODO Move to URLUtils class
     public static String decodeURL(String url) {
-        String decodedUrl = null;
         if (StringUtils.isBlank(url)) {
             return url;
         }
         try {
-            decodedUrl = URLDecoder.decode(url, "UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
+            return URLDecoder.decode(url, "UTF-8");
+        } catch (UnsupportedEncodingException ignored) {
         }
-        return decodedUrl;
+        return null;
     }
 
     public static String[] tokenize(String src, String delim) {
