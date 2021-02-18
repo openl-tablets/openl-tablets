a756546d2dd4a1bae9995d219b20e07578480481
Marat Kamalov <mkamalov@eisgroup.com>
2021-02-18 12:19:39 +0300

    EPBDS-11153 Webstudio Performance degradation in big project.

--- DEV/org.openl.rules/src/org/openl/rules/validation/UniqueMethodParameterNamesValidator.java
+++ DEV/org.openl.rules/src/org/openl/rules/validation/UniqueMethodParameterNamesValidator.java
@@ -1,8 +1,12 @@
 package org.openl.rules.validation;
 
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.openl.base.INamedThing;
 import org.openl.binding.MethodUtil;
@@ -23,42 +27,148 @@ public class UniqueMethodParameterNamesValidator implements IOpenLValidator {
     private static final String MSG_FOR_TYPES = "Method '%s' conflicts with another method '%s', because of parameter types are different.";
     private static final String MSG_FOR_NAMES = "Method '%s' conflicts with another method '%s', because of parameter names are different.";
 
+    private interface ParameterKey {
+        IOpenMethod getMethod();
+    }
+
+    private static class ParameterNameKey implements ParameterKey {
+        String name;
+        IOpenMethod method;
+
+        public ParameterNameKey(String name, IOpenMethod method) {
+            this.name = Objects.requireNonNull(name, "name cannot be null");
+            this.method = Objects.requireNonNull(method, "method cannot be null");
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o)
+                return true;
+            if (o == null || getClass() != o.getClass())
+                return false;
+            ParameterNameKey that = (ParameterNameKey) o;
+            return name.equals(that.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(name);
+        }
+
+        @Override
+        public IOpenMethod getMethod() {
+            return method;
+        }
+    }
+
+    private static class ParameterTypeKey implements ParameterKey {
+        IOpenClass type;
+        IOpenMethod method;
+
+        public ParameterTypeKey(IOpenClass type, IOpenMethod method) {
+            this.type = Objects.requireNonNull(type, "type cannot be null");
+            this.method = Objects.requireNonNull(method, "method cannot be null");
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o)
+                return true;
+            if (o == null || getClass() != o.getClass())
+                return false;
+            ParameterTypeKey that = (ParameterTypeKey) o;
+            return type.equals(that.type);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(type);
+        }
+
+        @Override
+        public IOpenMethod getMethod() {
+            return method;
+        }
+    }
+
+    private static class MethodPairKey {
+        IOpenMethod methodA;
+        IOpenMethod methodB;
+
+        public MethodPairKey(IOpenMethod methodA, IOpenMethod methodB) {
+            this.methodA = Objects.requireNonNull(methodA, "methodA cannot be null");
+            this.methodB = Objects.requireNonNull(methodB, "methodB cannot be null");
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o)
+                return true;
+            if (o == null || getClass() != o.getClass())
+                return false;
+            MethodPairKey that = (MethodPairKey) o;
+            return Objects.equals(methodA, that.methodA) && Objects.equals(methodB, that.methodB);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(methodA, methodB);
+        }
+    }
+
     @Override
     public ValidationResult validate(IOpenClass openClass) {
         Collection<OpenLMessage> messages = new LinkedHashSet<>();
         for (IOpenMethod method : openClass.getMethods()) {
             if (method instanceof OpenMethodDispatcher) {
                 OpenMethodDispatcher openMethodDispatcher = (OpenMethodDispatcher) method;
-                for (int i = 0; i < openMethodDispatcher.getCandidates().size() - 1; i++) {
-                    for (int j = i + 1; j < openMethodDispatcher.getCandidates().size(); j++) {
-                        IOpenMethod methodA = openMethodDispatcher.getCandidates().get(i);
-                        IOpenMethod methodB = openMethodDispatcher.getCandidates().get(j);
-
-                        validateSignature(messages, methodA, methodB);
+                List<IOpenMethod> candidates = openMethodDispatcher.getCandidates();
+                int parameterCount = candidates.iterator().next().getSignature().getNumberOfParameters();
+                Set<ParameterNameKey>[] parameterKeysByName = new HashSet[parameterCount];
+                Set<ParameterTypeKey>[] parameterKeysByType = new HashSet[parameterCount];
+                for (int i = 0; i < parameterCount; i++) {
+                    parameterKeysByName[i] = new HashSet<>();
+                    parameterKeysByType[i] = new HashSet<>();
+                }
+                for (IOpenMethod candidate : candidates) {
+                    IMethodSignature signature = candidate.getSignature();
+                    for (int j = 0; j < parameterCount; j++) {
+                        parameterKeysByName[j].add(new ParameterNameKey(signature.getParameterName(j), candidate));
+                        parameterKeysByType[j].add(new ParameterTypeKey(signature.getParameterType(j), candidate));
                     }
+                }
+                for (MethodPairKey methodPair : buildMethodPairs(parameterKeysByName, parameterCount)) {
+                    addWarnForMethods(methodPair.methodA, methodPair.methodB, messages, MSG_FOR_NAMES);
+                }
+                for (MethodPairKey methodPair : buildMethodPairs(parameterKeysByType, parameterCount)) {
+                    addWarnForMethods(methodPair.methodA, methodPair.methodB, messages, MSG_FOR_TYPES);
                 }
             }
         }
         return ValidationUtils.withMessages(messages);
     }
 
-    private void validateSignature(Collection<OpenLMessage> messages, IOpenMethod methodA, IOpenMethod methodB) {
-        IMethodSignature signatureA = methodA.getSignature();
-        IMethodSignature signatureB = methodB.getSignature();
-        for (int i1 = 0; i1 < signatureA.getNumberOfParameters(); i1++) {
-            if (!Objects.equals(signatureA.getParameterName(i1), signatureB.getParameterName(i1))) {
-                addWarnForMethods(methodA, methodB, messages, MSG_FOR_NAMES);
-                break;
-            }
-            if (!Objects.equals(signatureA.getParameterType(i1), signatureB.getParameterType(i1))) {
-                addWarnForMethods(methodA, methodB, messages, MSG_FOR_TYPES);
-                break;
+    private Set<MethodPairKey> buildMethodPairs(Set<? extends ParameterKey>[] parameterKeys, int parameterCount) {
+        Set<MethodPairKey> methodPairs = new HashSet<>();
+        for (int i = 0; i < parameterCount; i++) {
+            if (parameterKeys[i].size() > 1) {
+                List<IOpenMethod> conflictMethods = parameterKeys[i].stream()
+                    .map(ParameterKey::getMethod)
+                    .collect(Collectors.toList());
+                for (int j = 0; j < conflictMethods.size() - 1; j++) {
+                    for (int k = j + 1; k < conflictMethods.size(); k++) {
+                        methodPairs.add(new MethodPairKey(conflictMethods.get(j), conflictMethods.get(k)));
+                    }
+                }
             }
         }
+        return methodPairs;
     }
 
-    private void addWarnForMethods(IOpenMethod methodA, IOpenMethod methodB, Collection<OpenLMessage> messages,
-                                   String message) {
+    private void addWarnForMethods(IOpenMethod methodA,
+            IOpenMethod methodB,
+            Collection<OpenLMessage> messages,
+            String message) {
         ISyntaxNode syntaxNodeA = ((IMemberMetaInfo) methodA).getSyntaxNode();
         ISyntaxNode syntaxNodeB = ((IMemberMetaInfo) methodB).getSyntaxNode();
         String signA = MethodUtil.printSignature(methodA, INamedThing.REGULAR);
