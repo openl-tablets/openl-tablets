3827a9294bbf325f9bc2c3c26303ec75f7ecf0be
Vladyslav Pikus <vpikus@eisgroup.com>
2021-02-22 22:15:10 +0200

    EPBDS-8987 delete empty directories if file item is saved with ChangesetType.FULL

--- STUDIO/org.openl.rules.repository/src/org/openl/rules/repository/file/FileSystemRepository.java
+++ STUDIO/org.openl.rules.repository/src/org/openl/rules/repository/file/FileSystemRepository.java
@@ -383,6 +383,12 @@ public class FileSystemRepository implements FolderRepository, Closeable {
                 }
                 if (file.isDirectory()) {
                     removeAbsentFiles(file, toSave);
+                    File[] files = file.listFiles();
+                    if (files == null || files.length == 0) {
+                        if (!file.delete()) {
+                            LOG.warn("Failed to delete an empty directory: '{}'", file);
+                        }
+                    }
                 } else {
                     if (!toSave.contains(file)) {
                         FileUtils.deleteQuietly(file);
