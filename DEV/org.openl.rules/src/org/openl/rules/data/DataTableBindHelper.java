fd2f9549fc4710f10d9ea69b86f639b1a7e5e9be
Marat Kamalov <mkamalov@eisgroup.com>
2021-03-30 20:20:28 +0300

    EPBDS-11309 Syntax [][] is not supported in Test tables for 2d arrays. Fix regular expression, because not expected condition happens.

--- DEV/org.openl.rules/src/org/openl/rules/data/DataTableBindHelper.java
+++ DEV/org.openl.rules/src/org/openl/rules/data/DataTableBindHelper.java
@@ -72,9 +72,9 @@ public class DataTableBindHelper {
 
     // patter for field like addressArry[0]
     public static final Pattern COLLECTION_ACCESS_BY_INDEX_PATTERN = Pattern
-        .compile("\\s*[^\\:\\s]+\\s*\\[\\s*[0-9]+\\s*\\]\\s*(\\:\\s*[^\\:\\s]+|)\\s*$");
+        .compile("\\s*[^\\:\\s\\[\\]]+\\s*\\[\\s*[0-9]+\\s*\\]\\s*(\\:\\s*[^\\:\\s]+|)\\s*$");
     public static final Pattern COLLECTION_ACCESS_BY_KEY_PATTERN = Pattern
-        .compile("\\s*[^\\:\\s]+\\s*\\[\\s*(\\\".*\\\"|[0-9]+)\\s*\\]\\s*(\\:\\s*[^\\:\\s]+|)\\s*$");
+        .compile("\\s*[^\\:\\s\\[\\]]+\\s*\\[\\s*(\\\".*\\\"|[0-9]+)\\s*\\]\\s*(\\:\\s*[^\\:\\s]+|)\\s*$");
 
     static final Pattern THIS_ARRAY_ACCESS_PATTERN = Pattern.compile("\\s*\\[\\s*[0-9]+\\s*\\]\\s*$");
     static final Pattern THIS_LIST_ACCESS_PATTERN = Pattern.compile("\\s*\\[\\s*[0-9]+\\s*\\]\\s*(\\:\\s*[^\\:]+|)$");
