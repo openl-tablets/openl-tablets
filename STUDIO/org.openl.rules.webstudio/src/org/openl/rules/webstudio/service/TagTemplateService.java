1c95ec8e1a65bf8848cf0e7213165a4ba7402b48
Nail Samatov <nsamatov@eisgroup.com>
2021-06-08 19:54:34 +0300

    EPBDS-11275 improve tag template parsing

--- STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/service/TagTemplateService.java
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/service/TagTemplateService.java
@@ -77,7 +77,7 @@ public class TagTemplateService {
                 List<Tag> tags = new ArrayList<>();
                 for (int i = 0; i < tagTypes.size(); i++) {
                     TagType tagType = tagTypes.get(i);
-                    final String tagValue = matcher.group(i + 1);
+                    final String tagValue = matcher.group(i + 1).trim();
                     Tag tag = tagDao.getByTagTypeAndName(tagType.getName(), tagValue);
                     if (tag == null) {
                         tag = new Tag();
