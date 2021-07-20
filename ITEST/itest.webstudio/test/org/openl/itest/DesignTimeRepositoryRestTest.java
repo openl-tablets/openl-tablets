abdc35229abd0794991ac93477d1a38acdcc35e9
Vladyslav Pikus <vpikus@eisgroup.com>
2021-07-20 19:38:54 +0300

    EPBDS-11646 [REST API] forbid any push actions if selected branch is protected for branch repositories

--- ITEST/itest.webstudio/test/org/openl/itest/DesignTimeRepositoryRestTest.java
+++ ITEST/itest.webstudio/test/org/openl/itest/DesignTimeRepositoryRestTest.java
@@ -1,8 +1,5 @@
 package org.openl.itest;
 
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -59,6 +56,9 @@ public class DesignTimeRepositoryRestTest {
         client.send("desing-time-repo/git-non-flat/update-proj-4.put");
         client.send("desing-time-repo/git-non-flat/update-proj-5.put");
         client.send("desing-time-repo/git-non-flat/repos-projects-2.get");
+
+        client.send("desing-time-repo/git-protected-flat/repos-projects.get");
+        client.send("desing-time-repo/git-protected-flat/create-proj-403.put");
     }
 
 }
