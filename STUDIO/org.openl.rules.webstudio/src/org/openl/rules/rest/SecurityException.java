1e579cbb62b9b8a63d448ba06838c1de5bed2617
Vladyslav Pikus <vpikus@eisgroup.com>
2021-07-20 19:41:16 +0300

    EPBDS-11646 clean up

--- STUDIO/org.openl.rules.webstudio/src/org/openl/rules/rest/SecurityException.java
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/rest/SecurityException.java
@@ -1,10 +1,7 @@
 package org.openl.rules.rest;
 
 import org.openl.rules.rest.exception.ForbiddenException;
-import org.openl.rules.rest.exception.RestRuntimeException;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 class SecurityException extends ForbiddenException {
 
