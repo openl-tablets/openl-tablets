30d824f546d057f525cc38c4663bcf9de92227e2
Vladyslav Pikus <vpikus@eisgroup.com>
2021-08-02 17:36:41 +0300

    EPBDS-11736 Fix issue when downloaded zip contains '+' char instead of whitespace in the archive name

--- ITEST/server-core/src/org/openl/itest/core/Comparators.java
+++ ITEST/server-core/src/org/openl/itest/core/Comparators.java
@@ -1,14 +1,25 @@
 package org.openl.itest.core;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedHashSet;
+import java.util.Map;
 import java.util.Objects;
+import java.util.function.Function;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
+import org.springframework.util.StreamUtils;
 import org.w3c.dom.Node;
 import org.xmlunit.builder.DiffBuilder;
 import org.xmlunit.diff.ComparisonResult;
@@ -21,6 +32,8 @@ import org.xmlunit.diff.ElementSelectors;
 import com.fasterxml.jackson.databind.JsonNode;
 
 final class Comparators {
+
+    private static final String CRLF = "\r\n";
 
     private Comparators() {
     }
@@ -120,5 +133,61 @@ final class Comparators {
 
     private static void failDiff(JsonNode expectedJson, JsonNode actualJson, String path) {
         assertEquals("Path: \\" + path, expectedJson, actualJson);
+    }
+
+    static void zip(byte[] expectedBytes, byte[] actualBytes) throws IOException {
+        final Map<String, byte[]> expectedZipEntries = getZipEntries(expectedBytes);
+        final Map<String, byte[]> actualZipEntries = getZipEntries(actualBytes);
+
+        final Iterator<Map.Entry<String, byte[]>> actual = actualZipEntries.entrySet().iterator();
+        while (actual.hasNext()) {
+            final Map.Entry<String, byte[]> actualEntry = actual.next();
+            if (expectedZipEntries.containsKey(actualEntry.getKey())) {
+                assertArrayEquals(String.format("Zip entry [%s]: ", actualEntry.getKey()),
+                    expectedZipEntries.remove(actualEntry.getKey()),
+                    actualEntry.getValue());
+                actual.remove();
+            }
+        }
+
+        boolean failed = false;
+        StringBuilder errorMessage = new StringBuilder();
+        Function<String, String> tab = s -> "    " + s;
+        if (!actualZipEntries.isEmpty()) {
+            failed = true;
+            errorMessage.append("UNEXPECTED entries:")
+                .append(CRLF)
+                .append(actualZipEntries.keySet().stream().map(tab).collect(Collectors.joining(CRLF)));
+        }
+        if (!expectedZipEntries.isEmpty()) {
+            if (failed) {
+                errorMessage.append(CRLF);
+            } else {
+                failed = true;
+            }
+            errorMessage.append("MISSED entries:")
+                .append(CRLF)
+                .append(expectedZipEntries.keySet().stream().map(tab).collect(Collectors.joining(CRLF)));
+        }
+        if (failed) {
+            fail(errorMessage.toString());
+        }
+    }
+
+    private static Map<String, byte[]> getZipEntries(byte[] src) throws IOException {
+        Map<String, byte[]> dest = new HashMap<>();
+        try (ZipInputStream actual = new ZipInputStream(new ByteArrayInputStream(src))) {
+            ZipEntry actualEntry;
+            while ((actualEntry = actual.getNextEntry()) != null) {
+                if (actualEntry.getName().endsWith("/")) {
+                    // skip folder
+                    continue;
+                }
+                ByteArrayOutputStream target = new ByteArrayOutputStream();
+                StreamUtils.copy(actual, target);
+                dest.put(actualEntry.getName(), target.toByteArray());
+            }
+        }
+        return dest;
     }
 }
