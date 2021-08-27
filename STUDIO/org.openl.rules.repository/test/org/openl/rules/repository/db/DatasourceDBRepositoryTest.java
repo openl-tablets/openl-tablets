a9b680d1c11d91609fc1c0e80d9758f1f697c8e8
Yury Molchan <ymolchan@eisgroup.com>
2021-08-27 18:00:18 +0300

    EPBDS-11954 Add a test for JNDI located datasource DB repository

---
+++ STUDIO/org.openl.rules.repository/test/org/openl/rules/repository/db/DatasourceDBRepositoryTest.java
@@ -0,0 +1,26 @@
+package org.openl.rules.repository.db;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openl.rules.repository.RepositoryVerifier;
+import org.openl.rules.repository.api.Repository;
+import org.openl.spring.env.PropertySourcesLoader;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@TestPropertySource(properties = {"repoID=jndi", "openl.config.location=file:test-resources/application.properties"})
+@ContextConfiguration(initializers = PropertySourcesLoader.class, locations = {"classpath:application.xml"})
+public class DatasourceDBRepositoryTest {
+
+    @Autowired
+    Repository repository;
+
+    @Test
+    public void test() throws Exception {
+        RepositoryVerifier.testRepo(repository);
+    }
+}
