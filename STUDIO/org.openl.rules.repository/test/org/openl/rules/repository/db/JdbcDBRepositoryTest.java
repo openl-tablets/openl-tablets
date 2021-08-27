090179077ada809a54d62ff2d0408cea8b0268f6
Yury Molchan <ymolchan@eisgroup.com>
2021-08-27 16:21:17 +0300

    EPBDS-11954 Rewrite a test by using full cycle initialization from Spring

--- STUDIO/org.openl.rules.repository/test/org/openl/rules/repository/db/JdbcDBRepositoryTest.java
+++ STUDIO/org.openl.rules.repository/test/org/openl/rules/repository/db/JdbcDBRepositoryTest.java
@@ -1,16 +1,26 @@
 package org.openl.rules.repository.db;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.openl.rules.repository.RepositoryVerifier;
+import org.openl.rules.repository.api.Repository;
+import org.openl.spring.env.PropertySourcesLoader;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@TestPropertySource(properties = {"repoID=jdbc", "openl.config.location=file:test-resources/application.properties"})
+@ContextConfiguration(initializers = PropertySourcesLoader.class, locations = {"classpath:application.xml"})
 public class JdbcDBRepositoryTest {
+
+    @Autowired
+    Repository repository;
 
     @Test
     public void test() throws Exception {
-        JdbcDBRepository repository = new JdbcDBRepository();
-        repository.setUri("jdbc:h2:mem:repo1;DB_CLOSE_DELAY=-1");
-        repository.initialize();
         RepositoryVerifier.testRepo(repository);
     }
 }
