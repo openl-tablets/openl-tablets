d5d938867fbcfe0c44d8f9adcc2193ed841c1d0e
Yury Molchan <ymolchan@eisgroup.com>
2021-11-11 03:39:22 +0300

    EPBDS-12221 Improve memory usage for NumericComparableString instances

--- DEV/org.openl.core/src/org/openl/binding/impl/NumericComparableString.java
+++ DEV/org.openl.core/src/org/openl/binding/impl/NumericComparableString.java
@@ -2,18 +2,13 @@ package org.openl.binding.impl;
 
 import org.openl.util.StringPool;
 
+import javax.xml.bind.annotation.XmlRootElement;
 import java.math.BigInteger;
 import java.util.Objects;
-import java.util.regex.Pattern;
-
-import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement
 public class NumericComparableString implements Comparable<NumericComparableString> {
-    private static final Pattern NUMBERS = Pattern.compile("(?<=\\D)(?=\\d)|(?<=\\d)(?=\\D)");
 
-    private final String[] splits;
-    private final BigInteger[] splitsNumbers;
     private final String value;
 
     public static NumericComparableString valueOf(String value) {
@@ -30,41 +25,13 @@ public class NumericComparableString implements Comparable<NumericComparableStri
         return new NumericComparableString(value.toString());
     }
 
-    private void initSplitsNumbers() {
-        int i = 0;
-        for (String s : splits) {
-            if (s.charAt(0) >= '0' && s.charAt(0) <= '9') {
-                splitsNumbers[i] = new BigInteger(s);
-            }
-            i++;
-        }
-    }
-
     private NumericComparableString(String value) {
-        this.value = Objects.requireNonNull(value,
-            "Error initializing StringRangeValue class. Parameter 'value' cannot be null");
-        splits = NUMBERS.split(value);
-        splitsNumbers = new BigInteger[splits.length];
-        initSplitsNumbers();
+        this.value = Objects.requireNonNull(value, "Error initializing StringRangeValue class. Parameter 'value' cannot be null");
     }
 
     @Override
     public int compareTo(NumericComparableString v) {
-        int length = Math.min(splits.length, v.splits.length);
-
-        for (int i = 0; i < length; i++) {
-            int cmp;
-            if (splitsNumbers[i] != null && v.splitsNumbers[i] != null) {
-                cmp = splitsNumbers[i].compareTo(v.splitsNumbers[i]);
-            } else {
-                cmp = splits[i].compareTo(v.splits[i]);
-
-            }
-            if (cmp != 0) {
-                return cmp;
-            }
-        }
-        return splits.length - v.splits.length;
+        return NumericStringComparator.INSTANCE.compare(value, v.value);
     }
 
     public String getValue() {
@@ -87,20 +54,7 @@ public class NumericComparableString implements Comparable<NumericComparableStri
 
     @Override
     public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        int length = value.length();
-        boolean leading = true;
-        for(int i = 0; i < length; i++) {
-            char c = value.charAt(i);
-            if (leading && Character.digit(c, 10) == 0) {
-                // don't calculate hash for insignificant zero.
-                continue;
-            }
-            leading = !Character.isDigit(c);
-            result = prime * result + c;
-        }
-        return result;
+        return NumericStringComparator.hashCode(value);
     }
 
     @Override
