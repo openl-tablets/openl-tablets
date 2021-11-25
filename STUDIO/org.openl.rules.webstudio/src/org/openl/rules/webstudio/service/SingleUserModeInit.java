415ed5113bbfa10833a053eb3cc68f178d9dbb59
dastraukh <dastraukh@eisgroup.com>
2021-11-25 17:49:44 +0300

    EPBDS-12278	Fixed default user initialization on WebStudio migration.

--- STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/service/SingleUserModeInit.java
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/service/SingleUserModeInit.java
@@ -8,12 +8,23 @@ import org.openl.rules.security.standalone.persistence.User;
  */
 public class SingleUserModeInit {
     public SingleUserModeInit(UserDao userDao, String userName, String email, String displayName) {
-        if (userDao.getUserByName(userName) == null) {
+        User defaultUser = userDao.getUserByName(userName);
+        if (defaultUser == null) {
             User persistUser = new User();
             persistUser.setLoginName(userName);
             persistUser.setEmail(email);
+            persistUser.setEmailVerified(true);
             persistUser.setDisplayName(displayName);
             userDao.save(persistUser);
+        } else {
+            if (defaultUser.getEmail() == null) {
+                defaultUser.setEmailVerified(true);
+                defaultUser.setEmail(email);
+            }
+            if (defaultUser.getDisplayName() == null) {
+                defaultUser.setDisplayName(displayName);
+            }
+            userDao.update(defaultUser);
         }
     }
 }
