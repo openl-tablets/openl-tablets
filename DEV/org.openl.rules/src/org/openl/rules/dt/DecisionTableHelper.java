ce5ee6919577f1770c1d42383fbba76c6c8c5c51
Marat Kamalov <mkamalov@eisgroup.com>
2021-12-15 15:22:11 +0300

    EPBDS-12385 Smart Lookup Horizontal condition is matched incorrectly with the input data, if the columns are merged

--- DEV/org.openl.rules/src/org/openl/rules/dt/DecisionTableHelper.java
+++ DEV/org.openl.rules/src/org/openl/rules/dt/DecisionTableHelper.java
@@ -3034,15 +3034,14 @@ public final class DecisionTableHelper {
                     } else if (StringUtils.isBlank(part1) && StringUtils.isNotBlank(part2)) {
                         return Pair.of(i + originalTable.getSource().getCell(i, 0).getWidth(),
                             WithVerticalTitles.EMPTY_COLUMN);
-                    }
-                }
-                if (i > 0) {
-                    int w1 = originalTable.getSource().getCell(i - 1, firstColumnHeight).getWidth();
-                    int w2 = originalTable.getSource().getCell(i - 1, firstColumnHeight - 1).getWidth();
-                    int w3 = originalTable.getSource().getCell(i, firstColumnHeight - 1).getWidth();
-                    if (w1 == w2 + w3) {
-                        return Pair.of(i + originalTable.getSource().getCell(i, 0).getWidth(),
-                            WithVerticalTitles.MERGED_COLUMN);
+                    } else if (i > 0 && StringUtils.isBlank(part1) && StringUtils.isNotBlank(part2)) {
+                        int w1 = originalTable.getSource().getCell(i - 1, firstColumnHeight).getWidth();
+                        int w2 = originalTable.getSource().getCell(i - 1, firstColumnHeight - 1).getWidth();
+                        int w3 = originalTable.getSource().getCell(i, firstColumnHeight - 1).getWidth();
+                        if (w1 == w2 + w3) {
+                            return Pair.of(i + originalTable.getSource().getCell(i, 0).getWidth(),
+                                WithVerticalTitles.MERGED_COLUMN);
+                        }
                     }
                 }
                 i = i + originalTable.getSource().getCell(i, 0).getWidth();
