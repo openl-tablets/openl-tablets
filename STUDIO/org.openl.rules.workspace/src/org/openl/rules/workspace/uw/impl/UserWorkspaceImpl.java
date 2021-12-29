openl-tablets/openl-tablets

commit c1916f9b380077d2a4a8f789a2eab82988bf662e

EPBDS-12250 Fix low performance in Repository tab when press refresh button. No need to refresh projects list twice. We can combine refresh() and syncProjects() to create projects list in one pass-through.

STUDIO/org.openl.rules.workspace/src/org/openl/rules/workspace/uw/impl/UserWorkspaceImpl.java
@@ -65,6 +65,7 @@ public class UserWorkspaceImpl implements UserWorkspace {
 
     private boolean projectsRefreshNeeded = true;
     private boolean deploymentsRefreshNeeded = true;
+    private boolean syncNeeded = true;
     private boolean cleanUpOnActivation = false;
 
     private final List<UserWorkspaceListener> listeners = new ArrayList<>();
@@ -294,7 +295,14 @@ public class UserWorkspaceImpl implements UserWorkspace {
 
     @Override
     public void syncProjects() {
-        for (RulesProject rPr : getProjects()) {
+        syncNeeded = true;
+    }
+
+    private void doSyncProjects() {
+        syncNeeded = false;
+
+        boolean anyProjectRenamed = false;
+        for (RulesProject rPr : getProjects(false)) {
             Repository repository = designTimeRepository.getRepository(rPr.getRepository().getId());
             if (repository != null && repository.supports().mappedFolders()) {
                 if (rPr.isOpened() && !rPr.isLocalOnly()) {
@@ -309,7 +317,9 @@ public class UserWorkspaceImpl implements UserWorkspace {
                             int index = prevPath.lastIndexOf('/');
                             String newPath = prevPath.substring(0, index + 1) + realProjectName;
                             boolean renamed = new File(repoRoot, prevPath).renameTo(new File(repoRoot, newPath));
-                            if (!renamed) {
+                            if (renamed) {
+                                anyProjectRenamed = true;
+                            } else {
                                 log.warn("Can't rename folder from " + prevPath + " to " + newPath);
                             }
                         }
@@ -320,6 +330,11 @@ public class UserWorkspaceImpl implements UserWorkspace {
                     }
                 }
             }
+        }
+
+        if (anyProjectRenamed) {
+            // We need to recreate projects list in user workspace.
+            refreshRulesProjects();
         }
     }
 
@@ -597,6 +612,10 @@ public class UserWorkspaceImpl implements UserWorkspace {
 
             projectsRefreshNeeded = false;
             cleanUpOnActivation = false;
+
+            if (syncNeeded) {
+                doSyncProjects();
+            }
         }
     }
 

commit 845388d99fcb8a224e70bed1cda2f774faac9590

EPBDS-12440 EPBDS-12250 Renamed project is broken and non-workable. The bug was introduced during optimization in c1916f9b380077d2a4a8f789a2eab82988bf662e

STUDIO/org.openl.rules.workspace/src/org/openl/rules/workspace/uw/impl/UserWorkspaceImpl.java
@@ -168,6 +168,10 @@ public class UserWorkspaceImpl implements UserWorkspace {
 
     @Override
     public LocalWorkspace getLocalWorkspace() {
+        if (syncNeeded) {
+            // We must ensure that all folders are renamed to correct names before using local workspace.
+            doSyncProjects();
+        }
         return localWorkspace;
     }
 
@@ -311,7 +315,6 @@ public class UserWorkspaceImpl implements UserWorkspace {
                         if (!rPr.getLocalFolderName().equals(realProjectName)) {
                             // We can't close and then open a project in workspace, we should rename the folder
                             // in file system directly. Otherwise we will lose unsaved user changes.
-                            LocalWorkspace localWorkspace = getLocalWorkspace();
                             File repoRoot = localWorkspace.getRepository(rPr.getRepository().getId()).getRoot();
                             String prevPath = rPr.getFolderPath();
                             int index = prevPath.lastIndexOf('/');