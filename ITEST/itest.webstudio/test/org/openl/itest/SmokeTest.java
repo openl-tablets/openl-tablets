dc777fa44d998d90ead86520db4277f875fb685d
Vladyslav Pikus <vpikus@eisgroup.com>
2022-01-14 15:09:13 +0200

    EPBDS-12511 Added smoke tests for WebStudio

---
+++ ITEST/itest.webstudio/test/org/openl/itest/SmokeTest.java
@@ -0,0 +1,32 @@
+package org.openl.itest;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openl.itest.core.HttpClient;
+import org.openl.itest.core.JettyServer;
+
+public class SmokeTest {
+
+    private static JettyServer server;
+    private static HttpClient client;
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        server = JettyServer.startWithWebXml("smoke");
+        client = server.client();
+    }
+
+    @AfterClass
+    public static void tearDown() throws Exception {
+        server.stop();
+    }
+
+    @Test
+    public void smoke() {
+        client.send("public/info/sys.info.get");
+        client.send("public/info/openl.json.get");
+        client.send("public/notification.txt.get");
+    }
+
+}
