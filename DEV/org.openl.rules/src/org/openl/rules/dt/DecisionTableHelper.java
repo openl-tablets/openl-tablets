8ca8e9402a806ec5d0c9309a999fedc24a22a3a1
Marat Kamalov <mkamalov@eisgroup.com>
2022-01-24 17:20:16 +0300

    EPBDS-12515 Wrong condition matching if return and input param partially match up

--- DEV/org.openl.rules/src/org/openl/rules/dt/DecisionTableHelper.java
+++ DEV/org.openl.rules/src/org/openl/rules/dt/DecisionTableHelper.java
@@ -2932,7 +2932,9 @@ public final class DecisionTableHelper {
         boolean fuzzyReturn = false;
         for (DTHeader dtHeader : dtHeaders) {
             if (dtHeader.isReturn()) {
-                if (dtHeader instanceof FuzzyDTHeader) {
+                // If fieldsChain == null then it is a return for whole return type. It is not a part of fuzzy columns
+                // returns.
+                if (dtHeader instanceof FuzzyDTHeader && ((FuzzyDTHeader) dtHeader).getFieldsChain() != null) {
                     if (!fuzzyReturn) {
                         countReturns++;
                     }
