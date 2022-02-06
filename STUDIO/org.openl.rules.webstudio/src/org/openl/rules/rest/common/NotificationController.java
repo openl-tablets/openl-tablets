c6d603a89154e81a2e06e7f496a385b33868bd72
Vladyslav Pikus <vpikus@eisgroup.com>
2022-02-06 19:43:29 +0200

    EPBDS-12511 Added missing integration tests

--- STUDIO/org.openl.rules.webstudio/src/org/openl/rules/rest/common/NotificationController.java
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/rest/common/NotificationController.java
@@ -19,6 +19,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,7 +46,7 @@ public class NotificationController {
 
     @PostMapping(value = "/admin/notification.txt", consumes = MediaType.TEXT_PLAIN_VALUE)
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void postNotification(String notification) throws IOException {
+    public void postNotification(@RequestBody(required = false) String notification) throws IOException {
         if (StringUtils.isBlank(notification)) {
             Files.deleteIfExists(NOTIFICATION_FILE);
         } else {
