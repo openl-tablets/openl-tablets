fe47930b12a7c27144c4dc18c17c6bb6769df640
Marat Kamalov <mkamalov@eisgroup.com>
2022-03-23 21:19:26 +0300

    EPBDS-11748	Verify goal failure on groovy scripts

---
+++ Util/openl-maven-plugin/it/openl-verify-groovy/java-integration/src/main/java/com/example/java/CollectAfter.java
@@ -0,0 +1,20 @@
+package com.example.java;
+
+import java.util.Map;
+
+import org.openl.rules.ruleservice.storelogdata.advice.StoreLogDataAdvice;
+
+import com.datastax.oss.driver.api.querybuilder.QueryBuilder;
+import com.datastax.oss.driver.api.querybuilder.relation.Relation;
+
+public class CollectAfter implements StoreLogDataAdvice {
+
+    public void prepare(Map<String, Object> map, Object[] objects, Object o, Exception e) {
+        QueryBuilder.selectFrom("some_table")
+                .column("field3")
+                .where(Relation.column("field1").isEqualTo(QueryBuilder.bindMarker()))
+                .where(Relation.column("field2").isLessThanOrEqualTo(QueryBuilder.bindMarker()))
+                .limit(1)
+                .allowFiltering();
+    }
+}
