af3f5489e37ee561f143d27f5d17320d842e2e9e
Vladyslav Pikus <vpikus@eisgroup.com>
2022-05-17 18:19:58 +0300

    EPBDS-12811 reset cell to blank before setting new values.

--- DEV/org.openl.rules/src/org/openl/rules/table/xls/writers/XlsCellFormulaWriter.java
+++ DEV/org.openl.rules/src/org/openl/rules/table/xls/writers/XlsCellFormulaWriter.java
@@ -1,7 +1,6 @@
 package org.openl.rules.table.xls.writers;
 
 import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.CellType;
 import org.openl.rules.table.xls.PoiExcelHelper;
 import org.openl.rules.table.xls.XlsSheetGridModel;
 
@@ -15,6 +14,18 @@ public class XlsCellFormulaWriter extends AXlsCellWriter {
     public void writeCellValue() {
         String formula = getStringValue();
         Cell cellToWrite = getCellToWrite();
+        /*
+         * FIXME if cell has a RichText content like:
+         * <xml-fragment t="inlineStr" xmlns:main="http://schemas.openxmlformats.org/spreadsheetml/2006/main">
+         *   <main:is>
+         *    <main:t>= ""</main:t>
+         *  </main:is>
+         * </xml-fragment>
+         * Apache POI 5.0 doesn't remove it and set new values next to <main:is> element.
+         * As a result cell is in invalid state because contains old values and new.
+         * Set cell to blank to fix this issue and fully override old values
+         */
+        cellToWrite.setBlank();
         try {
             String excelFormula = formula.replaceFirst("=", "");
             cellToWrite.setCellFormula(excelFormula);
