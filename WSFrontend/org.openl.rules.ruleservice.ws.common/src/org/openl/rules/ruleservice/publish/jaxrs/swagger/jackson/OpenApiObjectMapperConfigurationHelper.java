c4818f2d2c2899c352e28faefd342c858ee42b65
Vladyslav Pikus <vpikus@eisgroup.com>
2022-05-27 11:54:48 +0300

    EPBDS-12833 Update swagger v3 version, sync swagger-models with core. Exclude exampleSetFlag from OpenAPI serialization

--- WSFrontend/org.openl.rules.ruleservice.ws.common/src/org/openl/rules/ruleservice/publish/jaxrs/swagger/jackson/OpenApiObjectMapperConfigurationHelper.java
+++ WSFrontend/org.openl.rules.ruleservice.ws.common/src/org/openl/rules/ruleservice/publish/jaxrs/swagger/jackson/OpenApiObjectMapperConfigurationHelper.java
@@ -2,6 +2,8 @@ package org.openl.rules.ruleservice.publish.jaxrs.swagger.jackson;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import io.swagger.v3.core.jackson.mixin.MediaTypeMixin;
+import io.swagger.v3.oas.models.media.MediaType;
 import io.swagger.v3.oas.models.media.Schema;
 
 public final class OpenApiObjectMapperConfigurationHelper {
@@ -10,5 +12,6 @@ public final class OpenApiObjectMapperConfigurationHelper {
 
     public static void configure(ObjectMapper objectMapper) {
         objectMapper.addMixIn(Schema.class, OpenApiXmlIgnoreMixIn.class);
+        objectMapper.addMixIn(MediaType.class, MediaTypeMixin.class);
     }
 }
