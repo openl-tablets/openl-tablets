a2ff66d3cad3392f097bd170481d69de14c0b520
Yury Molchan <ymolchan@eisgroup.com>
2022-08-04 23:37:05 +0300

    EPBDS-13088 More details when a service is failed

--- WSFrontend/org.openl.rules.ruleservice/src/org/openl/rules/ruleservice/core/AbstractOpenLServiceInitializer.java
+++ WSFrontend/org.openl.rules.ruleservice/src/org/openl/rules/ruleservice/core/AbstractOpenLServiceInitializer.java
@@ -2,26 +2,19 @@ package org.openl.rules.ruleservice.core;
 
 public abstract class AbstractOpenLServiceInitializer implements OpenLServiceInitializer {
 
-    private volatile boolean initializated;
+    private volatile boolean initialized;
     private volatile boolean initializationStarted;
 
     @Override
     public void ensureInitialization(OpenLService openLService) throws RuleServiceInstantiationException {
-        if (!initializated) {
+        if (!initialized) {
             synchronized (this) {
-                if (!initializated && !initializationStarted) {
+                if (!initialized && !initializationStarted) {
                     initializationStarted = true;
                     init(openLService);
-                    initializated = true;
+                    initialized = true;
                 }
             }
-        }
-    }
-
-    protected void validate(OpenLService openLService) throws RuleServiceInstantiationException {
-        if (openLService.getServiceClass().getMethods().length == 0) {
-            throw new RuleServiceInstantiationException(
-                String.format("Service '%s' does not have any methods to deploy.", openLService.getDeployPath()));
         }
     }
 
