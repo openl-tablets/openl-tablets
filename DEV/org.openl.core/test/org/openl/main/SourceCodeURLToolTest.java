bce97416159eb8f9166771cc24a0cc802d09c472
Vladyslav Pikus <vpikus@eisgroup.com>
2022-08-15 13:19:02 +0300

    EPBDS-13059 fix style for stacktrace, and fix position of "^" characters

---
+++ DEV/org.openl.core/test/org/openl/main/SourceCodeURLToolTest.java
@@ -0,0 +1,43 @@
+package org.openl.main;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import org.junit.Test;
+import org.openl.util.text.ILocation;
+import org.openl.util.text.IPosition;
+import org.openl.util.text.TextInterval;
+
+public class SourceCodeURLToolTest {
+
+    @Test
+    public void printCodeAndErrorTest() {
+        IPosition start = mock(IPosition.class);
+        when(start.getColumn(any())).thenReturn(28);
+        when(start.getLine(any())).thenReturn(0);
+
+        IPosition end = mock(IPosition.class);
+        when(end.getColumn(any())).thenReturn(34);
+        when(end.getLine(any())).thenReturn(0);
+
+        ILocation location = new TextInterval(start, end);
+
+        StringWriter stringWriter = new StringWriter();
+        try (PrintWriter printWriter = new PrintWriter(stringWriter)) {
+            SourceCodeURLTool.printCodeAndError(location,
+                " SpreadsheetResult MyS1pr (Stri1ng currentAgeBand,  String SIC)",
+                printWriter);
+        }
+
+        final String actual = stringWriter.toString();
+        final String expected = "Openl Code Fragment:\r\n" + "=======================\r\n" + " SpreadsheetResult MyS1pr (Stri1ng currentAgeBand,  String SIC)\r\n" + "                           ^^^^^^^\r\n" + "=======================\r\n";
+
+        assertEquals(expected, actual);
+    }
+
+}
