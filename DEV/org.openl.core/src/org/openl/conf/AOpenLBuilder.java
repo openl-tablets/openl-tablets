44403c49f967f40d57a3619893f6fb505762fbfe
Yury Molchan <ymolchan@eisgroup.com>
2022-08-18 17:22:03 +0300

    Clean up. Remove unused code

--- DEV/org.openl.core/src/org/openl/conf/AOpenLBuilder.java
+++ DEV/org.openl.core/src/org/openl/conf/AOpenLBuilder.java
@@ -1,21 +1,12 @@
 package org.openl.conf;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
-import java.util.Properties;
-
 import org.openl.OpenL;
 import org.openl.binding.impl.Binder;
 import org.openl.syntax.impl.Parser;
 import org.openl.util.RuntimeExceptionWrapper;
 import org.openl.vm.SimpleVM;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public abstract class AOpenLBuilder extends BaseOpenLBuilder {
-
-    private static final Logger LOG = LoggerFactory.getLogger(AOpenLBuilder.class);
 
     protected SimpleVM createVM() {
         return new SimpleVM();
@@ -43,29 +34,4 @@ public abstract class AOpenLBuilder extends BaseOpenLBuilder {
     }
 
     public abstract NoAntOpenLTask getNoAntOpenLTask();
-
-    protected Properties getProperties(String openl) {
-        URL url = getResourceContext().findClassPathResource(openl.replace('.', '/') + '/' + openl + ".ant.properties");
-        if (url == null) {
-            return null;
-        }
-        InputStream is = null;
-        try {
-            is = url.openStream();
-            Properties p = new Properties();
-            p.load(is);
-            return p;
-        } catch (IOException e) {
-            throw RuntimeExceptionWrapper.wrap(e);
-        } finally {
-            try {
-                if (is != null) {
-                    is.close();
-                }
-            } catch (Exception t) {
-                LOG.error("Failed to close an input stream.", t);
-            }
-        }
-
-    }
 }
