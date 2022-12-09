cdd54bb935cd67381e7ff1c982c4f63f0c954486
Marat Kamalov <mkamalov@eisgroup.com>
2022-12-09 12:38:31 +0300

    EPBDS-13392 Incorrect casting of primitive is selected between Object and Number

--- DEV/org.openl.core/src/org/openl/binding/impl/cast/CastsLinkageCast.java
+++ DEV/org.openl.core/src/org/openl/binding/impl/cast/CastsLinkageCast.java
@@ -1,5 +1,7 @@
 package org.openl.binding.impl.cast;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public final class CastsLinkageCast implements IOpenCast {
@@ -8,13 +10,26 @@ public final class CastsLinkageCast implements IOpenCast {
     private int distance;
 
     public CastsLinkageCast(IOpenCast... casts) {
-        this.casts = Objects.requireNonNull(casts, "casts cannot be null");
-        for (IOpenCast cast : casts) {
+        this.casts = optimizeCasts(Objects.requireNonNull(casts, "casts cannot be null"));
+        for (IOpenCast cast : this.casts) {
             int d = cast.getDistance();
             if (distance < d) {
                 distance = d;
             }
         }
+    }
+
+    private IOpenCast[] optimizeCasts(IOpenCast[] casts) {
+        List<IOpenCast> openCasts = new ArrayList<>();
+        for (IOpenCast cast : casts) {
+            if (cast instanceof JavaUpCast && !openCasts.isEmpty() && openCasts
+                .get(openCasts.size() - 1) instanceof JavaBoxingCast) {
+                openCasts.set(openCasts.size() - 1, JavaBoxingUpCast.getInstance());
+            } else {
+                openCasts.add(cast);
+            }
+        }
+        return openCasts.toArray(new IOpenCast[0]);
     }
 
     @Override
