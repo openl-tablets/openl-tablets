0c1e98c970a42e647266979233f94c65d73f4445
Yury Molchan <ymolchan@eisgroup.com>
2022-12-27 15:27:41 +0100

    EPBDS-13416 Add locale property in the runtime context

--- DEV/org.openl.rules/src/org/openl/rules/context/RulesRuntimeContextFactory.java
+++ DEV/org.openl.rules/src/org/openl/rules/context/RulesRuntimeContextFactory.java
@@ -1,7 +1,22 @@
 package org.openl.rules.context;
 
+import java.util.Locale;
+
 public class RulesRuntimeContextFactory {
+
+    private static final ThreadLocal<Locale> LOCALE_HOLDER = new ThreadLocal<>();
+
+    public static void setLocale(Locale locale) {
+        LOCALE_HOLDER.set(locale);
+    }
+
+    public static void removeLocale() {
+        LOCALE_HOLDER.remove();
+    }
+
     public static IRulesRuntimeContext buildRulesRuntimeContext() {
-        return new DefaultRulesRuntimeContext();
+        DefaultRulesRuntimeContext rulesRuntimeContext = new DefaultRulesRuntimeContext();
+        rulesRuntimeContext.setLocale(LOCALE_HOLDER.get());
+        return rulesRuntimeContext;
     }
 }
--- WSFrontend/org.openl.rules.ruleservice.ws/src/org/openl/rules/ruleservice/servlet/CXFServlet.java
+++ WSFrontend/org.openl.rules.ruleservice.ws/src/org/openl/rules/ruleservice/servlet/CXFServlet.java
@@ -1,12 +1,16 @@
 package org.openl.rules.ruleservice.servlet;
 
 import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.cxf.Bus;
 import org.apache.cxf.resource.ResourceManager;
 import org.apache.cxf.transport.servlet.CXFNonSpringServlet;
+import org.openl.rules.context.RulesRuntimeContextFactory;
 import org.springframework.context.ApplicationContext;
 
 @WebServlet(urlPatterns = "/*", loadOnStartup = 1, initParams = {
@@ -24,5 +28,15 @@ public class CXFServlet extends CXFNonSpringServlet {
         ResourceManager resourceManager = cxf.getExtension(ResourceManager.class);
         resourceManager.addResourceResolver(new StaticResourceResolver());
         resourceManager.addResourceResolver(new SwaggerUIResolver());
+    }
+
+    @Override
+    protected void invoke(HttpServletRequest request, HttpServletResponse response) throws ServletException {
+        RulesRuntimeContextFactory.setLocale(request.getLocale());
+        try {
+            super.invoke(request, response);
+        } finally {
+            RulesRuntimeContextFactory.removeLocale();
+        }
     }
 }
