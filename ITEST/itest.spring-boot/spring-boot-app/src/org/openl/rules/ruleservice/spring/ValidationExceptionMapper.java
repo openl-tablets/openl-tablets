4dff418f5816b614b8a3376e1552d78694fc9fa7
Yury Molchan <ymolchan@eisgroup.com>
2023-01-05 14:56:00 +0200

    EPBDS-13417 Add test for overriding of custom ExceptionMapper

---
+++ ITEST/itest.spring-boot/spring-boot-app/src/org/openl/rules/ruleservice/spring/ValidationExceptionMapper.java
@@ -0,0 +1,29 @@
+package org.openl.rules.ruleservice.spring;
+
+import java.util.Map;
+import javax.annotation.Priority;
+import javax.ws.rs.Priorities;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+import org.springframework.stereotype.Component;
+
+import org.openl.rules.ruleservice.core.ExceptionType;
+import org.openl.rules.ruleservice.core.RuleServiceWrapperException;
+
+@Component
+@Provider
+@Priority(Priorities.USER - 1)
+public class ValidationExceptionMapper implements ExceptionMapper<RuleServiceWrapperException> {
+    RuleServiceExceptionMapper parent = new RuleServiceExceptionMapper();
+    @Override
+    public Response toResponse(RuleServiceWrapperException exception) {
+
+        if (exception.getType() != ExceptionType.USER_ERROR) {
+            return Response.status(422).type(MediaType.APPLICATION_JSON).entity(Map.of("code", exception.getType().name(), "message", exception.getDetails().getMessage())).build();
+        }
+        return parent.toResponse(exception);
+    }
+}
