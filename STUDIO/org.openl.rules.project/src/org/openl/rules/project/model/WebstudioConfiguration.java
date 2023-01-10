614cd00ee12cdc93607d46f7a2a0a0929c4920c5
Marat Kamalov <mkamalov@eisgroup.com>
2023-01-10 20:14:14 +0300

    EPBDS-13463 The "Load This Module Only:" checkbox doesn't display

--- STUDIO/org.openl.rules.project/src/org/openl/rules/project/model/WebstudioConfiguration.java
+++ STUDIO/org.openl.rules.project/src/org/openl/rules/project/model/WebstudioConfiguration.java
@@ -1,5 +1,13 @@
 package org.openl.rules.project.model;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import org.openl.rules.project.xml.XmlProjectDescriptorSerializer;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement(name = XmlProjectDescriptorSerializer.WEBSTUDIO_CONFIGURATION)
 public class WebstudioConfiguration {
     private boolean compileThisModuleOnly = false;
 
