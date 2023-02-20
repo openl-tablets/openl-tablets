1da7e7cf72156e5a1d89bea6a5a5824f62afcb5c
Vladyslav Pikus <vpikus@eisgroup.com>
2023-02-20 14:43:33 +0200

    EPBDS-13550 Fix opening project dependencies from another repository

--- STUDIO/org.openl.rules.webstudio/src/org/openl/rules/rest/service/ProjectDependencyResolverImpl.java
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/rest/service/ProjectDependencyResolverImpl.java
@@ -1,28 +1,29 @@
 package org.openl.rules.rest.service;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
+
+import javax.xml.bind.JAXBException;
 
 import org.openl.rules.common.ProjectException;
 import org.openl.rules.project.abstraction.RulesProject;
 import org.openl.rules.project.model.ProjectDependencyDescriptor;
 import org.openl.rules.project.resolving.ProjectDescriptorArtefactResolver;
-import org.openl.rules.webstudio.web.util.WebStudioUtils;
 import org.openl.rules.workspace.uw.UserWorkspace;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Lookup;
 import org.springframework.stereotype.Service;
 
-import javax.xml.bind.JAXBException;
-
 @Service
-public class ProjectDependencyResolverImpl implements ProjectDependencyResolver {
+public abstract class ProjectDependencyResolverImpl implements ProjectDependencyResolver {
 
     private final Logger log = LoggerFactory.getLogger(ProjectDependencyResolverImpl.class);
 
@@ -33,10 +34,13 @@ public class ProjectDependencyResolverImpl implements ProjectDependencyResolver 
         this.projectDescriptorResolver = projectDescriptorResolver;
     }
 
+    @Lookup
+    protected abstract UserWorkspace getUserWorkspace();
+
     @Override
     public List<RulesProject> getProjectDependencies(RulesProject project) {
         List<RulesProject> dependencies = new ArrayList<>();
-        calcDependencies(project, new ArrayList<>(List.of(project.getBusinessName())), dependencies);
+        calcDependencies(project, new HashSet<>(Set.of(project.getBusinessName())), dependencies);
         return dependencies;
     }
 
@@ -53,7 +57,7 @@ public class ProjectDependencyResolverImpl implements ProjectDependencyResolver 
     }
 
     private void calcDependencies(RulesProject project,
-            Collection<String> processedProjects,
+            Set<String> processedProjects,
             Collection<RulesProject> result) {
         List<ProjectDependencyDescriptor> dependenciesDescriptors;
         try {
@@ -67,9 +71,9 @@ public class ProjectDependencyResolverImpl implements ProjectDependencyResolver 
             return;
         }
 
-        List<String> dependencyNames = dependenciesDescriptors.stream()
+        Set<String> dependencyNames = dependenciesDescriptors.stream()
             .map(ProjectDependencyDescriptor::getName)
-            .collect(Collectors.toList());
+            .collect(Collectors.toSet());
         String repoId = project.getRepository().getId();
 
         // Separate projects based on the match of the repository with the repository of the project for which the
@@ -79,40 +83,37 @@ public class ProjectDependencyResolverImpl implements ProjectDependencyResolver 
             .collect(Collectors.partitioningBy(p -> p.getRepository().getId().equals(repoId)));
 
         for (String dependencyName : dependencyNames) {
-            if (processedProjects.contains(dependencyName)) {
+            if (!processedProjects.add(dependencyName)) {
                 continue;
-            } else {
-                processedProjects.add(dependencyName);
             }
             // Since there can be projects with the same name even in the same repository, if the source repository has
             // a project with the search name in the same branch as the project for which the dependency is searched,
             // then it is returned. But if the dependent project is in another branch, then the search in other
             // repositories
             // is not performed.
-            Optional<RulesProject> dependentProject;
-            if (!projects.get(true).isEmpty()) {
-                dependentProject = projects.get(true)
+            Optional<RulesProject> dependentProject = Optional.empty();
+            if (!projects.get(Boolean.TRUE).isEmpty()) {
+                dependentProject = projects.get(Boolean.TRUE)
                     .stream()
                     .filter(
                         p -> p.getBusinessName().equals(dependencyName) && p.getBranch().equals(project.getBranch()))
                     .findFirst();
-            } else {
-                dependentProject = projects.get(false)
+            }
+            if (dependentProject.isEmpty() && !projects.get(Boolean.FALSE).isEmpty()) {
+                dependentProject = projects.get(Boolean.FALSE)
                     .stream()
                     .filter(p -> p.getBusinessName().equals(dependencyName))
                     .findFirst();
             }
 
-            if (dependentProject.isPresent()) {
-                result.add(dependentProject.get());
-                calcDependencies(dependentProject.get(), processedProjects, result);
-            }
-
+            dependentProject.ifPresent(dep -> {
+                result.add(dep);
+                calcDependencies(dep, processedProjects, result);
+            });
         }
     }
 
     private Collection<RulesProject> getAllProjects() {
-        UserWorkspace userWorkspace = WebStudioUtils.getUserWorkspace(WebStudioUtils.getSession());
-        return userWorkspace.getProjects();
+        return getUserWorkspace().getProjects();
     }
 }
