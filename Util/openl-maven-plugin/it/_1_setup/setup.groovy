3ffca41882a72a1cea4a6f2267024c2bc4cf0bac
Yury Molchan <ymolchan@eisgroup.com>
2023-04-14 18:58:29 +0300

    EPBDS-13475 Calculate timeouts depending on count of modules

---
+++ Util/openl-maven-plugin/it/_1_setup/setup.groovy
@@ -0,0 +1,19 @@
+StringBuilder modules = new StringBuilder(10000);
+
+File folder = basedir
+def itFolder = folder.getParentFile()
+for (def test: itFolder.listFiles()) {
+    if (!test.isDirectory() || test.getName().startsWith("_")) {
+        // Skip processing not folders (files, links and etc.)
+        continue
+    }
+    modules.append('        <module>../').append(test.getName()).append('</module>\n')
+}
+
+println (modules)
+
+def pomFile = new File(itFolder, '_2_install/pom.xml')
+def pomText = pomFile.getText('UTF-8').replace('        <module>generated.pom</module>', modules);
+pomFile.write(pomText, 'UTF-8')
+
+return true
