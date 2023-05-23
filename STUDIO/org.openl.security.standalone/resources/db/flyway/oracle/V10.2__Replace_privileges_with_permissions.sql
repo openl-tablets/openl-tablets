2ead2050f5f7360a77c025d393decc005ae32292
Marat Kamalov <mkamalov@eisgroup.com>
2023-05-23 17:14:26 +0300

    EPBDS-13823 Webstudio is unable to use the MySQL for users and permissions storage

---
+++ STUDIO/org.openl.security.standalone/resources/db/flyway/oracle/V10.2__Replace_privileges_with_permissions.sql
@@ -0,0 +1,21 @@
+CREATE SEQUENCE ace_entry_row_num_1;
+ALTER SEQUENCE ace_entry_row_num_1 RESTART WITH 1;
+
+INSERT INTO acl_entry (acl_object_identity, sid, mask, granting, audit_success, audit_failure, ace_order)
+    SELECT e.bid, e.aid, e.mask, true, false, false, (SELECT count(*) FROM acl_entry d WHERE e.bid = d.acl_object_identity) + nextval('ace_entry_row_num_1') - 1
+    FROM (
+        SELECT DISTINCT b.id as bid, a.id as aid, c.mask as mask
+        FROM acl_sid a,
+             acl_object_identity b,
+             acl_permission_mapping c,
+             acl_class d,
+             OpenL_Group_Authorities t1 INNER JOIN OpenL_Groups t2 ON t1.groupID = t2.ID
+        WHERE b.object_id_identity = c.object_id_identity
+          AND b.object_id_class  = d.id
+          AND d.class = c.class
+          AND a.principal = false
+          AND c.authority = t1.authority
+          AND a.sid = t2.groupName
+        ) as e;
+
+DROP SEQUENCE ace_entry_row_num_1;
