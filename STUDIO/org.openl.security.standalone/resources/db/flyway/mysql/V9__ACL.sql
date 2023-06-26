404c6683c750475ba1c89894bad5141a5588bd7c
Marat Kamalov <mkamalov@eisgroup.com>
2023-06-26 17:08:18 +0300

    EPBDS-13899 The error appears on Project Creating if user db is MySQL

--- STUDIO/org.openl.security.standalone/resources/db/flyway/mysql/V9__ACL.sql
+++ STUDIO/org.openl.security.standalone/resources/db/flyway/mysql/V9__ACL.sql
@@ -15,11 +15,10 @@ CREATE TABLE acl_class (
 CREATE TABLE acl_object_identity (
 	id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	object_id_class BIGINT NOT NULL,
-	object_id_identity VARCHAR(766) NOT NULL,
+	object_id_identity TEXT NOT NULL,
 	parent_object BIGINT,
 	owner_sid BIGINT,
 	entries_inheriting BOOLEAN NOT NULL,
-	UNIQUE KEY uk_acl_object_identity (object_id_class, object_id_identity),
 	CONSTRAINT fk_acl_object_identity_parent FOREIGN KEY (parent_object) REFERENCES acl_object_identity (id),
 	CONSTRAINT fk_acl_object_identity_class FOREIGN KEY (object_id_class) REFERENCES acl_class (id),
 	CONSTRAINT fk_acl_object_identity_owner FOREIGN KEY (owner_sid) REFERENCES acl_sid (id)
