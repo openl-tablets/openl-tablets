1368ce7ed4198f2e08115e96aeed6c6a9e7a9d0e
Yury Molchan <ymolchan@eisgroup.com>
2023-07-13 11:29:13 +0300

    EPBDS-13901 Check path for the static resources against path traversal vulnerability

---
+++ WSFrontend/org.openl.rules.ruleservice.ws/test/org/openl/rules/ruleservice/servlet/RuleServicesFilterTest.java
@@ -0,0 +1,33 @@
+package org.openl.rules.ruleservice.servlet;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RuleServicesFilterTest {
+
+    @Test
+    public void isAllowedPath() {
+        assertFalse(RuleServicesFilter.isAllowedPath(""));
+        assertFalse(RuleServicesFilter.isAllowedPath("file"));
+        assertFalse(RuleServicesFilter.isAllowedPath("file.txt"));
+        assertFalse(RuleServicesFilter.isAllowedPath("path/file.txt"));
+        assertFalse(RuleServicesFilter.isAllowedPath("//file"));
+        assertFalse(RuleServicesFilter.isAllowedPath(".file"));
+        assertFalse(RuleServicesFilter.isAllowedPath("/.file"));
+        assertFalse(RuleServicesFilter.isAllowedPath("/./file"));
+        assertFalse(RuleServicesFilter.isAllowedPath("/../file"));
+        assertFalse(RuleServicesFilter.isAllowedPath("/file..txt"));
+        assertFalse(RuleServicesFilter.isAllowedPath("/%2E/file.txt"));
+        assertFalse(RuleServicesFilter.isAllowedPath("/path%2Epath/file.txt"));
+        assertFalse(RuleServicesFilter.isAllowedPath("/%2E%2E/file.txt"));
+        assertFalse(RuleServicesFilter.isAllowedPath("%2File.txt"));
+        assertFalse(RuleServicesFilter.isAllowedPath("/%44File.txt"));
+
+        assertTrue(RuleServicesFilter.isAllowedPath("/file"));
+        assertTrue(RuleServicesFilter.isAllowedPath("/file.txt"));
+        assertTrue(RuleServicesFilter.isAllowedPath("/path/file.txt"));
+        assertTrue(RuleServicesFilter.isAllowedPath("/path/file.txt.gz"));
+        assertTrue(RuleServicesFilter.isAllowedPath("/path/path.ext/file.txt.gz"));
+    }
+}
