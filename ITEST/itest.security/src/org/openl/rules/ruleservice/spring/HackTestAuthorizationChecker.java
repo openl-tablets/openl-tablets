3d89eac73f8e856650c16e72f3c9936b96c803ae
Yury Molchan <ymolchan@eisgroup.com>
2023-07-13 21:06:15 +0300

    EPBDS-13679 Allow to customize an access denied response

--- ITEST/itest.security/src/org/openl/rules/ruleservice/spring/HackTestAuthorizationChecker.java
+++ ITEST/itest.security/src/org/openl/rules/ruleservice/spring/HackTestAuthorizationChecker.java
@@ -1,12 +1,12 @@
 package org.openl.rules.ruleservice.spring;
 
-import org.openl.rules.ruleservice.api.AuthorizationChecker;
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.http.HttpServletRequest;
+import org.openl.rules.ruleservice.api.AuthorizationChecker;
 
 @Component
 @Order(2)
