8b56a3498644e0bf2708b17fd0b0027d06749705
Yury Molchan <ymolchan@eisgroup.com>
2023-09-28 05:04:12 +0300

    EPBDS-14061 Support Spring autowiring

--- WSFrontend/org.openl.rules.ruleservice.annotation/src/org/openl/rules/ruleservice/core/interceptors/InjectOpenClass.java
+++ WSFrontend/org.openl.rules.ruleservice.annotation/src/org/openl/rules/ruleservice/core/interceptors/InjectOpenClass.java
@@ -5,12 +5,20 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
 import org.openl.types.IOpenClass;
 
 /**
  * This annotation is designed to inject @{@link IOpenClass} related to compiled project to ruleservice interceptors.
+ *
+ * @deprecated use @Autowire IOpenClass openClass
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ ElementType.PARAMETER, ElementType.METHOD })
+@Deprecated
+@Autowired
+@Qualifier("openClass")
 public @interface InjectOpenClass {
 }
--- WSFrontend/org.openl.rules.ruleservice.annotation/src/org/openl/rules/ruleservice/core/interceptors/InjectRulesDeploy.java
+++ WSFrontend/org.openl.rules.ruleservice.annotation/src/org/openl/rules/ruleservice/core/interceptors/InjectRulesDeploy.java
@@ -5,13 +5,21 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
 import org.openl.rules.project.model.RulesDeploy;
 
 /**
  * This annotation is designed to inject @{@link RulesDeploy} related to compiled project to ruleservice interceptors.
  * If a project doesn't have @{@link RulesDeploy} then null be injected.
+ *
+ * @deprecated use @Autowire RulesDeploy rulesDeploy
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ ElementType.PARAMETER, ElementType.METHOD })
+@Deprecated
+@Autowired
+@Qualifier("rulesDeploy")
 public @interface InjectRulesDeploy {
 }
