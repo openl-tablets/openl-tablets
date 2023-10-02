f7b56ec8cbf3825e53e6f0962c25ba75dbf1f16f
Marat Kamalov <mkamalov@eisgroup.com>
2023-10-02 22:28:02 +0300

    EPBDS-14072 The old constructor syntax doesn't support the parameter assignment in assigning a parameter in parentheses

--- DEV/org.openl.core/src/org/openl/binding/impl/NewNodeBinder.java
+++ DEV/org.openl.core/src/org/openl/binding/impl/NewNodeBinder.java
@@ -1,14 +1,19 @@
 package org.openl.binding.impl;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
 import org.openl.binding.IBindingContext;
 import org.openl.binding.IBoundNode;
 import org.openl.binding.MethodUtil;
 import org.openl.binding.impl.module.ModuleSpecificOpenMethod;
+import org.openl.message.OpenLMessage;
 import org.openl.syntax.ISyntaxNode;
+import org.openl.syntax.exception.SyntaxNodeException;
 import org.openl.syntax.impl.ISyntaxConstants;
 import org.openl.syntax.impl.IdentifierNode;
-import org.openl.types.IMethodCaller;
-import org.openl.types.IOpenClass;
 import org.openl.util.MessageUtils;
 
 /**
@@ -31,9 +36,9 @@ public class NewNodeBinder extends ANodeBinder {
             return makeErrorNode("New node must have at least one sub-node.", node, bindingContext);
         }
 
-        ISyntaxNode typeNode = node.getChild(0);
-        String typeName = ((IdentifierNode) typeNode).getIdentifier();
-        IOpenClass type = bindingContext.findType(ISyntaxConstants.THIS_NAMESPACE, typeName);
+        var typeNode = node.getChild(0);
+        var typeName = ((IdentifierNode) typeNode).getIdentifier();
+        var type = bindingContext.findType(ISyntaxConstants.THIS_NAMESPACE, typeName);
 
         if (type == null) {
             return makeErrorNode(MessageUtils.getTypeNotFoundMessage(typeName), typeNode, bindingContext);
@@ -41,22 +46,55 @@ public class NewNodeBinder extends ANodeBinder {
         if (type.getInstanceClass() == null) {
             return makeErrorNode(MessageUtils.getTypeDefinedErrorMessage(typeName), typeNode, bindingContext);
         }
-
-        IBoundNode[] children = bindChildren(node, bindingContext, 1, childrenCount);
-        if (hasErrorBoundNode(children)) {
-            return new ErrorBoundNode(node);
+        bindingContext.pushErrors();
+        bindingContext.pushMessages();
+        boolean errorsAndMessagesPopped = false;
+        boolean sugarConstructor = false;
+        List<SyntaxNodeException> syntaxNodeExceptions = Collections.emptyList();
+        Collection<OpenLMessage> openLMessages = Collections.emptyList();
+        try {
+            var children = bindChildren(node, bindingContext, 1, childrenCount);
+            syntaxNodeExceptions = bindingContext.popErrors();
+            openLMessages = bindingContext.popMessages();
+            errorsAndMessagesPopped = true;
+            var childNodes = new ISyntaxNode[node.getNumberOfChildren() - 1];
+            for (int i = 0; i < childNodes.length; i++) {
+                childNodes[i] = node.getChild(i + 1);
+            }
+            if (hasErrorBoundNode(children)) {
+                var iBoundNode = Optional.of(type)
+                    .map(t -> ConstructorSugarSupport
+                        .makeSugarConstructor(node, childNodes, bindingContext, t, node.getChild(childrenCount - 1)));
+                if (iBoundNode.isPresent()) {
+                    sugarConstructor = true;
+                    return iBoundNode.get();
+                }
+                return new ErrorBoundNode(node);
+            }
+            var paramTypes = getTypes(children);
+            var methodCaller = ModuleSpecificOpenMethod.findConstructorCaller(type, paramTypes, bindingContext);
+            BindHelper.checkOnDeprecation(node, bindingContext, methodCaller);
+            if (methodCaller == null) {
+                var iBoundNode = Optional.of(type)
+                    .map(t -> ConstructorSugarSupport
+                        .makeSugarConstructor(node, childNodes, bindingContext, t, node.getChild(childrenCount - 1)));
+                if (iBoundNode.isPresent()) {
+                    sugarConstructor = true;
+                    return iBoundNode.get();
+                }
+                var constructor = MethodUtil.printMethod(type.getName(), paramTypes);
+                return makeErrorNode(MessageUtils.getConstructorNotFoundMessage(constructor), typeNode, bindingContext);
+            }
+            return new ConstructorParamsNode(new MethodBoundNode(typeNode, methodCaller, children));
+        } finally {
+            if (!sugarConstructor) {
+                bindingContext.addMessages(openLMessages);
+                syntaxNodeExceptions.forEach(bindingContext::addError);
+            }
+            if (!errorsAndMessagesPopped) {
+                bindingContext.popErrors();
+                bindingContext.popMessages();
+            }
         }
-        IOpenClass[] paramTypes = getTypes(children);
-
-        IMethodCaller methodCaller = ModuleSpecificOpenMethod.findConstructorCaller(type, paramTypes, bindingContext);
-        BindHelper.checkOnDeprecation(node, bindingContext, methodCaller);
-
-        if (methodCaller == null) {
-            String constructor = MethodUtil.printMethod(type.getName(), paramTypes);
-            return makeErrorNode(MessageUtils.getConstructorNotFoundMessage(constructor), typeNode, bindingContext);
-        }
-
-        return new ConstructorParamsNode(new MethodBoundNode(typeNode, methodCaller, children));
     }
-
 }
