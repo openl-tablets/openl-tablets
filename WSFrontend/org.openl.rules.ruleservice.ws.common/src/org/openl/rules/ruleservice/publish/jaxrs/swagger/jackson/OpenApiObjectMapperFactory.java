dc65a9216c5432f3db45ad461f31d190792f494c
Yury Molchan <ymolchan@eisgroup.com>
2023-10-04 23:27:33 +0300

    EPBDS-13749 Configure common ObjectMapper instance for OpenAPI

--- WSFrontend/org.openl.rules.ruleservice.ws.common/src/org/openl/rules/ruleservice/publish/jaxrs/swagger/jackson/OpenApiObjectMapperFactory.java
+++ WSFrontend/org.openl.rules.ruleservice.ws.common/src/org/openl/rules/ruleservice/publish/jaxrs/swagger/jackson/OpenApiObjectMapperFactory.java
@@ -3,11 +3,11 @@ package org.openl.rules.ruleservice.publish.jaxrs.swagger.jackson;
 import org.openl.rules.serialization.ObjectMapperFactory;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.swagger.v3.oas.integration.IntegrationObjectMapperFactory;
+import io.swagger.v3.core.util.Json;
 
 public final class OpenApiObjectMapperFactory implements ObjectMapperFactory {
     @Override
     public ObjectMapper createObjectMapper() {
-        return IntegrationObjectMapperFactory.createJson();
+        return Json.mapper().copy();
     }
 }
