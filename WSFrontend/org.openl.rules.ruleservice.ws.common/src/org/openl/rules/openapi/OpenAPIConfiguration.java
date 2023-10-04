734ec5e763ba7597cb2695cfff0edee0fdc771f4
Yury Molchan <ymolchan@eisgroup.com>
2023-10-04 21:54:00 +0300

    EPBDS-13749 Register streamed types as an OpenAPI binary file

--- WSFrontend/org.openl.rules.ruleservice.ws.common/src/org/openl/rules/openapi/OpenAPIConfiguration.java
+++ WSFrontend/org.openl.rules.ruleservice.ws.common/src/org/openl/rules/openapi/OpenAPIConfiguration.java
@@ -1,6 +1,13 @@
 package org.openl.rules.openapi;
 
+import java.io.InputStream;
+import java.io.Reader;
+
+import io.swagger.v3.core.converter.ModelConverters;
+import io.swagger.v3.core.util.Json;
 import io.swagger.v3.core.util.PrimitiveType;
+import io.swagger.v3.oas.models.media.BinarySchema;
+import org.springframework.core.io.InputStreamSource;
 
 /**
  * Customization of the OpenAPI schema generation with the converters of common types.
@@ -15,5 +22,18 @@ public class OpenAPIConfiguration {
 
     static {
         PrimitiveType.customClasses().put("java.util.Locale", PrimitiveType.STRING);
+
+        ModelConverters.getInstance().addConverter((type, context, chain) -> {
+            var clazz = Json.mapper().constructType(type.getType()).getRawClass();
+
+            // Binary file format
+            if (InputStream.class.isAssignableFrom(clazz)
+                    || Reader.class.isAssignableFrom(clazz)
+                    || InputStreamSource.class.isAssignableFrom(clazz)) {
+                return new BinarySchema();
+            }
+
+            return chain.hasNext() ? chain.next().resolve(type, context, chain) : null;
+        });
     }
 }
