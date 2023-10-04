f182ff0bd740065c3f116f3641a1f8bc711f254d
Yury Molchan <ymolchan@eisgroup.com>
2023-10-05 00:43:38 +0300

    EPBDS-13749 Sort fields in OpenAPI schema

--- WSFrontend/org.openl.rules.ruleservice.ws.common/src/org/openl/rules/openapi/OpenAPIConfiguration.java
+++ WSFrontend/org.openl.rules.ruleservice.ws.common/src/org/openl/rules/openapi/OpenAPIConfiguration.java
@@ -2,13 +2,19 @@ package org.openl.rules.openapi;
 
 import java.io.InputStream;
 import java.io.Reader;
+import java.util.Map;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import io.swagger.v3.core.converter.ModelConverters;
+import io.swagger.v3.core.jackson.mixin.OpenAPIMixin;
 import io.swagger.v3.core.jackson.mixin.SchemaMixin;
 import io.swagger.v3.core.util.Json;
 import io.swagger.v3.core.util.PrimitiveType;
 import io.swagger.v3.core.util.Yaml;
+import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.media.BinarySchema;
 import io.swagger.v3.oas.models.media.Schema;
 import io.swagger.v3.oas.models.media.XML;
@@ -28,9 +34,16 @@ public class OpenAPIConfiguration {
     static {
         PrimitiveType.customClasses().put("java.util.Locale", PrimitiveType.STRING);
 
-        // Remove needless xml namesapce attribute from the generated OpenAPI schema
+
         Json.mapper().addMixIn(Schema.class, OpenApiXmlIgnoreMixIn.class);
         Yaml.mapper().addMixIn(Schema.class, OpenApiXmlIgnoreMixIn.class);
+        Json.mapper().addMixIn(OpenAPI.class, SortedOpenAPIMixin.class);
+        Yaml.mapper().addMixIn(OpenAPI.class, SortedOpenAPIMixin.class);
+
+        Json.mapper().enable(SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS);
+        Json.mapper().enable(MapperFeature.SORT_PROPERTIES_ALPHABETICALLY);
+        Yaml.mapper().enable(SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS);
+        Yaml.mapper().enable(MapperFeature.SORT_PROPERTIES_ALPHABETICALLY);
 
         ModelConverters.getInstance().addConverter((type, context, chain) -> {
             var clazz = Json.mapper().constructType(type.getType()).getRawClass();
@@ -46,9 +59,22 @@ public class OpenAPIConfiguration {
         });
     }
 
-    public static abstract class OpenApiXmlIgnoreMixIn extends SchemaMixin {
+    @JsonPropertyOrder(value = {"openapi", "info", "externalDocs", "servers", "security", "tags", "paths", "components"}, alphabetic = true)
+    static abstract class SortedOpenAPIMixin extends OpenAPIMixin {
+
+        @JsonPropertyOrder(alphabetic = true)
+        public abstract Map<String, Object> getExtensions();
+
+    }
+
+    @JsonPropertyOrder(value = {"type", "format"}, alphabetic = true)
+    static abstract class OpenApiXmlIgnoreMixIn extends SchemaMixin {
+
+        @JsonPropertyOrder(alphabetic = true)
+        public abstract Map<String, Object> getExtensions();
+
         @JsonIgnore
-        public abstract XML getXml();
+        public abstract XML getXml();  // Remove needless xml namespace attribute from the generated OpenAPI schema
     }
 
 }
