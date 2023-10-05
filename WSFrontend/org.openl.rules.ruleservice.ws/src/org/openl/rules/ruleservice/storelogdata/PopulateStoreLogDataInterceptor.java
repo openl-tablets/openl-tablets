30e6260a8ffcf12f1b62682f52ff9c866787b42e
Yury Molchan <ymolchan@eisgroup.com>
2023-10-06 01:35:58 +0300

    EPBDS-13127 Reduce amount of interceptors

---
+++ WSFrontend/org.openl.rules.ruleservice.ws/src/org/openl/rules/ruleservice/storelogdata/PopulateStoreLogDataInterceptor.java
@@ -0,0 +1,49 @@
+package org.openl.rules.ruleservice.storelogdata;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.cxf.message.Message;
+import org.apache.cxf.phase.AbstractPhaseInterceptor;
+import org.apache.cxf.phase.Phase;
+
+import org.openl.rules.project.model.RulesDeploy;
+import org.openl.rules.ruleservice.core.OpenLService;
+import org.openl.rules.ruleservice.core.RuleServiceInstantiationException;
+import org.openl.rules.ruleservice.publish.jaxrs.storelogdata.JacksonObjectSerializer;
+
+/**
+ * CXF interceptor for collecting service data for logging to external source feature.
+ *
+ * @author Yury Molchan
+ *
+ */
+public class PopulateStoreLogDataInterceptor extends AbstractPhaseInterceptor<Message> {
+
+    private final String serviceName;
+    private final Class<?> serviceClass;
+    private final ObjectSerializer objectSerializer;
+
+    public PopulateStoreLogDataInterceptor(OpenLService service, ObjectMapper objectMapper) throws RuleServiceInstantiationException {
+        super(Phase.PRE_STREAM);
+        this.serviceClass = service.getServiceClass();
+        this.serviceName = service.getName();
+        this.objectSerializer = new JacksonObjectSerializer(objectMapper);
+    }
+
+    @Override
+    public void handleMessage(Message message) {
+        populateStoreLogData();
+    }
+
+    @Override
+    public void handleFault(Message message) {
+        populateStoreLogData();
+    }
+
+    private void populateStoreLogData() {
+        var storeLogData = StoreLogDataHolder.get();
+        storeLogData.setServiceClass(serviceClass);
+        storeLogData.setServiceName(serviceName);
+        storeLogData.setPublisherType(RulesDeploy.PublisherType.RESTFUL);
+        storeLogData.setObjectSerializer(objectSerializer);
+    }
+}
