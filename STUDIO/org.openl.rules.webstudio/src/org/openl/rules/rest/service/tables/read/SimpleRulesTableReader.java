1e2075aab3755a8c6d9c70dc84c88b240c8b60a9
Vladyslav Pikus <vpikus@eisgroup.com>
2023-10-13 14:08:55 +0300

    EPBDS-14075 Update SimpleRules API in favor new design

--- STUDIO/org.openl.rules.webstudio/src/org/openl/rules/rest/service/tables/read/SimpleRulesTableReader.java
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/rest/service/tables/read/SimpleRulesTableReader.java
@@ -2,9 +2,12 @@ package org.openl.rules.rest.service.tables.read;
 
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.List;
 
+import org.openl.rules.rest.model.tables.SimpleRuleHeaderView;
 import org.openl.rules.rest.model.tables.SimpleRulesView;
 import org.openl.rules.rest.service.tables.OpenLTableUtils;
+import org.openl.rules.table.ILogicalTable;
 import org.openl.rules.table.IOpenLTable;
 import org.springframework.stereotype.Component;
 
@@ -31,9 +34,9 @@ public class SimpleRulesTableReader extends ExecutableTableReader<SimpleRulesVie
 
         var tsn = openLTable.getSyntaxNode();
         var tableBody = tsn.getTableBody();
-        var headerSource = tsn.getHeader().getSourceString();
 
-        var args = getArgs(headerSource, 0);
+        var headers = getConditionHeaders(tableBody.getRow(0));
+        builder.headers(headers);
         var list = new ArrayList<LinkedHashMap<String, Object>>();
         var height = OpenLTableUtils.getHeightWithoutEmptyRows(tableBody);
         var width = OpenLTableUtils.getWidthWithoutEmptyColumns(tableBody);
@@ -41,19 +44,26 @@ public class SimpleRulesTableReader extends ExecutableTableReader<SimpleRulesVie
         for (int row = 1; row < height; row++) {
             var rule = new LinkedHashMap<String, Object>();
             for (int col = 0; col < width; col++) {
-                String argName = null;
-                if (col == width - 1) {
-                    argName = "return";
-                } else if (col < args.size()) {
-                    argName = args.get(col).name;
+                String ruleName = null;
+                if (col < headers.size()) {
+                    ruleName = headers.get(col).title;
                 }
-                if (argName == null) {
-                    argName = "arg" + col;
+                if (ruleName == null) {
+                    ruleName = SimpleRuleHeaderView.UNKNOWN_HEADER_NAME + col;
                 }
-                rule.put(argName, tableBody.getCell(col, row).getObjectValue());
+                rule.put(ruleName, tableBody.getCell(col, row).getObjectValue());
             }
             list.add(rule);
         }
         builder.rules(list);
+    }
+
+    public static List<SimpleRuleHeaderView> getConditionHeaders(ILogicalTable headerBody) {
+        List<SimpleRuleHeaderView> headers = new ArrayList<>();
+        var width = OpenLTableUtils.getWidthWithoutEmptyColumns(headerBody);
+        for (int col = 0; col < width; col++) {
+            headers.add(SimpleRuleHeaderView.builder().title(headerBody.getCell(col, 0).getStringValue()).build());
+        }
+        return headers;
     }
 }
