589f0eaf0be215e84b9f89f78685bd35b3cd9d93
Marat Kamalov <mkamalov@eisgroup.com>
2023-10-17 17:02:34 +0300

    EPBDS-14021 Smart Assistant Testing (Evals)

--- STUDIO/org.openl.rules.webstudio/src/org/openl/rules/rest/AssistantController.java
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/rest/AssistantController.java
@@ -1,20 +1,16 @@
 package org.openl.rules.rest;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import javax.servlet.http.HttpSession;
 
-import org.openl.rules.lang.xls.syntax.TableSyntaxNode;
 import org.openl.rules.method.ExecutableRulesMethod;
 import org.openl.rules.project.ai.OpenL2TextUtils;
-import org.openl.rules.table.IOpenLTable;
-import org.openl.rules.ui.WebStudio;
-import org.openl.rules.webstudio.ai.WebstudioAIServiceGrpc;
 import org.openl.rules.webstudio.ai.WebstudioAi;
 import org.openl.rules.webstudio.grpc.AIService;
 import org.openl.rules.webstudio.web.util.WebStudioUtils;
@@ -87,10 +83,61 @@ public class AssistantController {
         }
     }
 
+    public static class TSN {
+        String uri;
+        String table;
+        String type;
+        String description;
+        String businessDimensionProperties;
+
+        public String getUri() {
+            return uri;
+        }
+
+        public void setUri(String uri) {
+            this.uri = uri;
+        }
+
+        public String getTable() {
+            return table;
+        }
+
+        public void setTable(String table) {
+            this.table = table;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public void setType(String type) {
+            this.type = type;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        public String getBusinessDimensionProperties() {
+            return businessDimensionProperties;
+        }
+
+        public void setBusinessDimensionProperties(String businessDimensionProperties) {
+            this.businessDimensionProperties = businessDimensionProperties;
+        }
+    }
+
     public static class Message {
         String type;
         String text;
-        private List<Ref> refs;
+        List<Ref> refs;
+        List<TSN> tableSyntaxNodes = new ArrayList<>();
+        List<String> refTypes = new ArrayList<>();
+        List<String> refMethods = new ArrayList<>();
 
         @JsonCreator
         public Message(@JsonProperty("text") String text,
@@ -124,17 +171,44 @@ public class AssistantController {
         public void setRefs(List<Ref> refs) {
             this.refs = refs;
         }
+
+        public List<TSN> getTableSyntaxNodes() {
+            return tableSyntaxNodes;
+        }
+
+        public void setTableSyntaxNodes(List<TSN> tableSyntaxNodes) {
+            this.tableSyntaxNodes = tableSyntaxNodes;
+        }
+
+        public List<String> getRefTypes() {
+            return refTypes;
+        }
+
+        public void setRefTypes(List<String> refTypes) {
+            this.refTypes = refTypes;
+        }
+
+        public List<String> getRefMethods() {
+            return refMethods;
+        }
+
+        public void setRefMethods(List<String> refMethods) {
+            this.refMethods = refMethods;
+        }
     }
 
     public static class MessageArrayWrapper {
         private List<Message> messages;
         private final String tableId;
+        private final Boolean rate;
 
         @JsonCreator
         public MessageArrayWrapper(@JsonProperty("tableId") String tableId,
-                @JsonProperty("messages") List<Message> messages) {
+                @JsonProperty("messages") List<Message> messages,
+                @JsonProperty("rate") boolean rate) {
             this.messages = messages;
             this.tableId = tableId;
+            this.rate = rate;
         }
 
         public List<Message> getMessages() {
@@ -148,6 +222,10 @@ public class AssistantController {
         public void setMessages(List<Message> messages) {
             this.messages = messages;
         }
+
+        public boolean getRate() {
+            return rate;
+        }
     }
 
     private static List<Ref> buildRefs(List<WebstudioAi.Ref> refs) {
@@ -157,20 +235,21 @@ public class AssistantController {
         return refs.stream().map(e -> new Ref(e.getUrl(), e.getTitle())).collect(Collectors.toList());
     }
 
-    @PostMapping(value = "/ask_help")
-    public Message[] askHelp(HttpSession httpSession, @RequestBody MessageArrayWrapper messageArrayWrapper) {
-        Message[] messages = messageArrayWrapper.getMessages().toArray(new Message[0]);
+    private static WebstudioAi.ChatRequest getChatRequest(HttpSession httpSession,
+            MessageArrayWrapper messageArrayWrapper,
+            WebstudioAi.ChatType chatType) {
+        var messages = messageArrayWrapper.getMessages().toArray(new Message[0]);
         // get all messages except the last one are ignored
-        Message[] history = new Message[messages.length - 1];
+        var history = new Message[messages.length - 1];
         if (history.length > 0) {
             System.arraycopy(messages, 0, history, 0, messages.length - 1);
         }
-        WebStudio studio = WebStudioUtils.getWebStudio(httpSession);
-        IOpenLTable table = StringUtils.isNotBlank(messageArrayWrapper.getTableId()) ? studio.getModel()
+        var studio = WebStudioUtils.getWebStudio(httpSession);
+        var table = StringUtils.isNotBlank(messageArrayWrapper.getTableId()) ? studio.getModel()
             .getTableById(messageArrayWrapper.getTableId()) : null;
-        WebstudioAi.ChatRequest.Builder chatRequestBuilder = WebstudioAi.ChatRequest.newBuilder();
+        var chatRequestBuilder = WebstudioAi.ChatRequest.newBuilder();
         if (table != null) {
-            TableSyntaxNode tableSyntaxNode = studio.getModel().findNode(table.getUri());
+            var tableSyntaxNode = studio.getModel().findNode(table.getUri());
             if (tableSyntaxNode != null) {
                 String currentOpenedTable;
                 if (tableSyntaxNode.getMember() instanceof ExecutableRulesMethod) {
@@ -184,11 +263,11 @@ public class AssistantController {
                     currentOpenedTable = OpenL2TextUtils
                         .tableSyntaxNodeToString(tableSyntaxNode, false, false, Integer.MAX_VALUE);
                 }
-                Set<IOpenClass> types = new HashSet<>();
+                var types = new HashSet<IOpenClass>();
                 for (IOpenClass type : OpenL2TextUtils.methodTypes(tableSyntaxNode)) {
                     OpenL2TextUtils.collectTypes(type, types, MAX_DEPTH_COLLECT_TYPES, REPLACE_ALIAS_TYPES_WITH_BASE);
                 }
-                Set<IOpenMethod> methodRefs = OpenL2TextUtils.methodRefs(tableSyntaxNode);
+                var methodRefs = OpenL2TextUtils.methodRefs(tableSyntaxNode);
                 for (IOpenMethod method : methodRefs) {
                     OpenL2TextUtils
                         .collectTypes(method.getType(), types, MAX_DEPTH_COLLECT_TYPES, REPLACE_ALIAS_TYPES_WITH_BASE);
@@ -200,31 +279,30 @@ public class AssistantController {
                 methodRefs.stream()
                     .map(e -> OpenL2TextUtils.methodHeaderToString(e, REPLACE_ALIAS_TYPES_WITH_BASE) + "{}")
                     .forEach(chatRequestBuilder::addRefMethods);
-                WebstudioAi.TableSyntaxNode.Builder tableSyntaxNodeMessageBuilder = WebstudioAi.TableSyntaxNode
-                    .newBuilder()
+                var tableSyntaxNodeMessageBuilder = WebstudioAi.TableSyntaxNode.newBuilder()
                     .setUri(table.getUri())
                     .setType(tableSyntaxNode.getType())
                     .setTable(currentOpenedTable);
                 if (tableSyntaxNode.getMember() instanceof ExecutableRulesMethod) {
-                    String dimensionalProperties = OpenL2TextUtils.dimensionalPropertiesToString(
+                    var dimensionalProperties = OpenL2TextUtils.dimensionalPropertiesToString(
                         (ExecutableRulesMethod) tableSyntaxNode.getMember(),
                         objectMapper);
                     if (dimensionalProperties != null) {
                         tableSyntaxNodeMessageBuilder.setBusinessDimensionProperties(dimensionalProperties);
                     }
-                    String description = ((ExecutableRulesMethod) tableSyntaxNode.getMember()).getMethodProperties()
+                    var description = ((ExecutableRulesMethod) tableSyntaxNode.getMember()).getMethodProperties()
                         .getDescription();
                     if (StringUtils.isNotBlank(description)) {
                         tableSyntaxNodeMessageBuilder.setDescription(description);
                     }
                 }
-                WebstudioAi.TableSyntaxNode tableSyntaxNodeMessage = tableSyntaxNodeMessageBuilder.build();
+                var tableSyntaxNodeMessage = tableSyntaxNodeMessageBuilder.build();
                 chatRequestBuilder.addTableSyntaxNodes(tableSyntaxNodeMessage);
             }
         }
 
-        Message lastMessage = messages[messages.length - 1];
-        chatRequestBuilder.setChatType(WebstudioAi.ChatType.KNOWLEDGE)
+        var lastMessage = messages[messages.length - 1];
+        chatRequestBuilder.setChatType(chatType)
             .setMessage(lastMessage.text)
             .addAllHistory(Stream.of(history)
                 .map(e -> WebstudioAi.ChatMessage.newBuilder()
@@ -233,12 +311,75 @@ public class AssistantController {
                                                                 : WebstudioAi.MessageType.ASSISTANT)
                     .build())
                 .collect(Collectors.toList()));
-        WebstudioAi.ChatRequest request = chatRequestBuilder.build();
-        WebstudioAIServiceGrpc.WebstudioAIServiceBlockingStub blockingStub = aiService.getBlockingStub();
-        WebstudioAi.ChatReply response = blockingStub.chat(request);
+        return chatRequestBuilder.build();
+    }
+
+    private Message buildMessage(WebstudioAi.ChatRequest chatRequest, String text, String type, List<Ref> refs) {
+        var message = new Message(text, type, refs);
+        for (var tableSyntaxNode : chatRequest.getTableSyntaxNodesList()) {
+            var tsn = new TSN();
+            tsn.setUri(tableSyntaxNode.getUri());
+            tsn.setTable(tableSyntaxNode.getTable());
+            tsn.setType(tableSyntaxNode.getType());
+            tsn.setDescription(tableSyntaxNode.getDescription());
+            tsn.setBusinessDimensionProperties(tableSyntaxNode.getBusinessDimensionProperties());
+            message.getTableSyntaxNodes().add(tsn);
+        }
+        chatRequest.getRefTypesList().forEach(message.getRefTypes()::add);
+        chatRequest.getRefMethodsList().forEach(message.getRefMethods()::add);
+        return message;
+    }
+
+    @PostMapping(value = "/ask_help")
+    public Message[] askHelp(HttpSession httpSession, @RequestBody MessageArrayWrapper messageArrayWrapper) {
+        var request = getChatRequest(httpSession, messageArrayWrapper, WebstudioAi.ChatType.KNOWLEDGE);
+        var blockingStub = aiService.getBlockingStub();
+        var response = blockingStub.chat(request);
         return response.getMessagesList()
             .stream()
-            .map(e -> new Message(e.getText(), CHAT_TYPE_ASSISTANT, buildRefs(e.getRefsList())))
+            .map(e -> buildMessage(request, e.getText(), CHAT_TYPE_ASSISTANT, buildRefs(e.getRefsList())))
             .toArray(Message[]::new);
+    }
+
+    private WebstudioAi.TableSyntaxNode buildGrpcTSN(TSN tsn) {
+        var builder = WebstudioAi.TableSyntaxNode.newBuilder();
+        builder.setUri(tsn.getUri());
+        builder.setTable(tsn.getTable());
+        builder.setType(tsn.getType());
+        if (tsn.getBusinessDimensionProperties() != null) {
+            builder.setBusinessDimensionProperties(tsn.getBusinessDimensionProperties());
+        }
+        if (tsn.getDescription() != null) {
+            builder.setDescription(tsn.getDescription());
+        }
+        return builder.build();
+    }
+
+    @PostMapping(value = "/rate")
+    public void rate(@RequestBody MessageArrayWrapper messageArrayWrapper) {
+        var chatRequestBuilder = WebstudioAi.ChatRequest.newBuilder();
+        chatRequestBuilder.setChatType(WebstudioAi.ChatType.RATE);
+        var lastMessage = messageArrayWrapper.getMessages().get(messageArrayWrapper.getMessages().size() - 1);
+        chatRequestBuilder.setMessage(lastMessage.getText());
+        lastMessage.refTypes.forEach(chatRequestBuilder::addRefTypes);
+        lastMessage.refMethods.forEach(chatRequestBuilder::addRefMethods);
+        lastMessage.tableSyntaxNodes.forEach(e -> chatRequestBuilder.addTableSyntaxNodes(buildGrpcTSN(e)));
+        // get all messages except the last one, build history
+        for (int i = 0; i < messageArrayWrapper.getMessages().size() - 1; i++) {
+            var message = messageArrayWrapper.getMessages().get(i);
+            var chatMessageBuilder = WebstudioAi.ChatMessage.newBuilder();
+            chatMessageBuilder.setText(message.getText());
+            chatMessageBuilder.setType(CHAT_TYPE_USER.equals(message.getType()) ? WebstudioAi.MessageType.USER
+                                                                                : WebstudioAi.MessageType.ASSISTANT);
+            chatRequestBuilder.addHistory(chatMessageBuilder.build());
+        }
+
+        var chatRequest = chatRequestBuilder.build();
+        var rateRequest = WebstudioAi.RateRequest.newBuilder()
+            .setRate(messageArrayWrapper.getRate())
+            .setChatRequest(chatRequest)
+            .build();
+        var blockingStub = aiService.getBlockingStub();
+        blockingStub.rate(rateRequest);
     }
 }
