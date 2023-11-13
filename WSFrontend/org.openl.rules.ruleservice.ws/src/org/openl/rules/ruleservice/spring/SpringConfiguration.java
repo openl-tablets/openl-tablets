ce64314bec75832f771ea17e58e0925bd86f73ae
Yury Molchan <ymolchan@eisgroup.com>
2023-11-13 23:22:56 +0200

    EPBDS-14133 Allow to load any CXF features as Spring beans

---
+++ WSFrontend/org.openl.rules.ruleservice.ws/src/org/openl/rules/ruleservice/spring/SpringConfiguration.java
@@ -0,0 +1,33 @@
+package org.openl.rules.ruleservice.spring;
+
+import org.apache.cxf.ext.logging.LoggingFeature;
+import org.apache.cxf.transport.common.gzip.GZIPFeature;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.openl.spring.config.ConditionalOnEnable;
+
+/**
+ * Spring Java configuration for using instead of XML configuration.
+ *
+ * @author Yury Molchan
+ */
+@Configuration
+public class SpringConfiguration {
+
+    @Bean
+    @ConditionalOnEnable("ruleservice.gzip.threshold")
+    GZIPFeature gzipFeature(@Value("${ruleservice.gzip.threshold}") Integer gzipThreshold) {
+        var gzipFeature = new GZIPFeature();
+        gzipFeature.setThreshold(gzipThreshold);
+        return gzipFeature;
+    }
+
+    @Bean
+    @ConditionalOnEnable("ruleservice.logging.enabled")
+    LoggingFeature loggingFeature() {
+        return new LoggingFeature();
+    }
+
+}
