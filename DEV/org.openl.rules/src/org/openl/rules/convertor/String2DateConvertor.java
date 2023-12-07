b2c44a6157562d222da13b37022e6bf59e8b7140
Yury Molchan <ymolchan@eisgroup.com>
2023-12-07 12:27:25 +0200

    EPBDS-14148 Allow to parse timezone in +00:00 format and fractional part in nanoseconds

--- DEV/org.openl.rules/src/org/openl/rules/convertor/String2DateConvertor.java
+++ DEV/org.openl.rules/src/org/openl/rules/convertor/String2DateConvertor.java
@@ -12,7 +12,6 @@ import java.time.temporal.ChronoField;
 import java.time.temporal.TemporalQueries;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.List;
 
 class String2DateConvertor implements IString2DataConvertor<Date> {
 
@@ -33,57 +32,81 @@ class String2DateConvertor implements IString2DataConvertor<Date> {
                 return df.parse(data);
             } catch (ParseException e) {
                 throw new IllegalArgumentException(
-                    String.format("Cannot convert '%s' to date type using: '%s' format", data, format));
+                        String.format("Cannot convert '%s' to date type using: '%s' format", data, format));
             }
         }
 
         try {
             // Special case for the two digit year in US format.
             LocalDateTime localDateTime = new DateTimeFormatterBuilder().appendPattern("M/d/")
-                .appendValueReduced(ChronoField.YEAR, 2, 2, BASE_DATE)
-                .parseStrict()
-                .toFormatter(LocaleDependConvertor.getLocale())
-                .parse(data, TemporalQueries.localDate())
-                .atTime(0, 0);
+                    .appendValueReduced(ChronoField.YEAR, 2, 2, BASE_DATE)
+                    .parseStrict()
+                    .toFormatter(LocaleDependConvertor.getLocale())
+                    .parse(data, TemporalQueries.localDate())
+                    .atTime(0, 0);
             return Date.from(localDateTime.toInstant(ZoneId.systemDefault().getRules().getOffset(localDateTime)));
         } catch (Exception ignore) {
             // Ignore
         }
 
-        for (var pattern : List.of("y-M-d['T'H:m[:s[.SSS]][X]]", "M/d/y[ H:m[:s]]", "M/d/y hh:mm a")) {
-            try {
-                var parsePosition = new ParsePosition(0);
-                var parser = new DateTimeFormatterBuilder().appendPattern(pattern)
-                    .parseStrict()
-                    .toFormatter(LocaleDependConvertor.getLocale());
-                var result = parser.parse(data, parsePosition);
-                if (parsePosition.getIndex() == data.length()) {
-                    var date = result.query(TemporalQueries.localDate());
-                    var time = result.query(TemporalQueries.localTime());
+        DateTimeFormatterBuilder patternBuilder = new DateTimeFormatterBuilder()
+                .appendPattern("y-M-d['T'H:m[:s[.")
+                .appendFraction(ChronoField.NANO_OF_SECOND, 0, 9, false)
+                .appendPattern("]][X]]");
+        Date cal = parsePattern(data, patternBuilder);
+        if (cal != null) return cal;
 
-                    var cal = Calendar.getInstance();
-                    cal.clear();
-                    cal.set(date.getYear(), date.getMonthValue() - 1, date.getDayOfMonth());
+        patternBuilder = new DateTimeFormatterBuilder()
+                .appendPattern("y-M-d['T'H:m[:s[.")
+                .appendFraction(ChronoField.NANO_OF_SECOND, 0, 9, false)
+                .appendPattern("]][XXX]]");
+        cal = parsePattern(data, patternBuilder);
+        if (cal != null) return cal;
 
-                    if (time != null) {
-                        cal.set(Calendar.HOUR_OF_DAY, time.getHour());
-                        cal.set(Calendar.MINUTE, time.getMinute());
-                        cal.set(Calendar.SECOND, time.getSecond());
-                        cal.set(Calendar.MILLISECOND, time.getNano() / 1_000_000);
-                    }
+        patternBuilder = new DateTimeFormatterBuilder().appendPattern("M/d/y[ H:m[:s]]");
+        cal = parsePattern(data, patternBuilder);
+        if (cal != null) return cal;
 
-                    if (result.isSupported(ChronoField.OFFSET_SECONDS)) {
-                        cal.set(Calendar.ZONE_OFFSET, result.get(ChronoField.OFFSET_SECONDS) * 1_000);
-                        cal.set(Calendar.DST_OFFSET, 0);
-                    }
+        patternBuilder = new DateTimeFormatterBuilder().appendPattern("M/d/y hh:mm a");
+        cal = parsePattern(data, patternBuilder);
+        if (cal != null) return cal;
 
-                    return cal.getTime();
-                }
-            } catch (Exception ignore) {
-                // Loop on
-            }
-        }
         throw new IllegalArgumentException(String.format("Cannot convert '%s' to Date type", data));
+    }
+
+    private static Date parsePattern(String data, DateTimeFormatterBuilder patternBuilder) {
+        var parser = patternBuilder
+                .parseStrict()
+                .toFormatter(LocaleDependConvertor.getLocale());
+        var parsePosition = new ParsePosition(0);
+        try {
+            var result = parser.parse(data, parsePosition);
+            if (parsePosition.getIndex() != data.length()) {
+                return null;
+            }
+            var date = result.query(TemporalQueries.localDate());
+            var time = result.query(TemporalQueries.localTime());
+
+            var cal = Calendar.getInstance();
+            cal.clear();
+            cal.set(date.getYear(), date.getMonthValue() - 1, date.getDayOfMonth());
+
+            if (time != null) {
+                cal.set(Calendar.HOUR_OF_DAY, time.getHour());
+                cal.set(Calendar.MINUTE, time.getMinute());
+                cal.set(Calendar.SECOND, time.getSecond());
+                cal.set(Calendar.MILLISECOND, time.getNano() / 1_000_000);
+            }
+
+            if (result.isSupported(ChronoField.OFFSET_SECONDS)) {
+                cal.set(Calendar.ZONE_OFFSET, result.get(ChronoField.OFFSET_SECONDS) * 1_000);
+                cal.set(Calendar.DST_OFFSET, 0);
+            }
+
+            return cal.getTime();
+        } catch (Exception ignore) {
+            return null;
+        }
     }
 
 }
