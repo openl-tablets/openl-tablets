73afafba7c6a251705feb6c183f0ce78253e3bb1
Yury Molchan <ymolchan@eisgroup.com>
2024-01-11 16:56:53 +0200

    EPBDS-13749 Update Jackson and Swagger libs

--- WSFrontend/org.openl.rules.ruleservice.ws.common/src/org/openl/rules/ruleservice/publish/jaxrs/swagger/jackson/OpenApiXmlIgnoreMixIn.java
+++ WSFrontend/org.openl.rules.ruleservice.ws.common/src/org/openl/rules/ruleservice/publish/jaxrs/swagger/jackson/OpenApiXmlIgnoreMixIn.java
@@ -2,9 +2,10 @@ package org.openl.rules.ruleservice.publish.jaxrs.swagger.jackson;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import io.swagger.v3.core.jackson.mixin.SchemaMixin;
 import io.swagger.v3.oas.models.media.XML;
 
-public abstract class OpenApiXmlIgnoreMixIn {
+public abstract class OpenApiXmlIgnoreMixIn extends SchemaMixin {
     @JsonIgnore
     public abstract XML getXml();
 }
