d4f1a2e5f5b106287ca044800524f920705a4cd6
Aliaksandr Samuseu <asamuseu@eisgroup.com>
2024-01-24 11:06:00 +0100

    EPBDS-14150 User Drawer Component/ added NoMatch page / added Redirect component

--- NEW_ADMIN_UI/webstudioadmin/src/components/MainMenu.tsx
+++ NEW_ADMIN_UI/webstudioadmin/src/components/MainMenu.tsx
@@ -1,16 +1,22 @@
 import React, { useEffect, useState } from 'react'
 import { Menu } from 'antd'
 import { useLocation, useNavigate } from 'react-router-dom'
+import { useTranslation } from 'react-i18next'
 import {
-    FolderOutlined,
+    DatabaseOutlined,
+    FormOutlined,
     MailOutlined,
     NotificationOutlined,
     NumberOutlined,
     SettingOutlined,
+    TeamOutlined,
+    ToolOutlined,
     UserOutlined,
 } from '@ant-design/icons'
+import './MainMenu.scss'
 
 const MainMenu: React.FC = () => {
+    const { t } = useTranslation()
     const navigate = useNavigate()
     const location = useLocation()
     const [ selectedKeys, setSelectedKeys ] = useState<string[]>([])
@@ -20,7 +26,7 @@ const MainMenu: React.FC = () => {
     }, [ location.pathname ])
 
     return (
-        <div>
+        <div id="main-menu">
             <Menu
                 selectedKeys={selectedKeys}
                 onClick={({ key }) => {
@@ -28,13 +34,16 @@ const MainMenu: React.FC = () => {
                     navigate(key)
                 }}
             >
-                <Menu.Item key="/repository/design" icon={<FolderOutlined />}>Repositories</Menu.Item>
-                <Menu.Item key="/system" icon={<SettingOutlined />}>System</Menu.Item>
-                <Menu.Item key="/users" icon={<UserOutlined />}>Users</Menu.Item>
-                <Menu.Item key="/admin/management/groups" icon={<UserOutlined />}>Groups & Privileges</Menu.Item>
-                <Menu.Item key="/notification" icon={<NotificationOutlined />}>Notification</Menu.Item>
-                <Menu.Item key="/tags" icon={<NumberOutlined />}>Tags</Menu.Item>
-                <Menu.Item key="/mail" icon={<MailOutlined />}>Mail</Menu.Item>
+                <Menu.Item key="/user/profile" icon={<UserOutlined />}>{t('common:menu.my_profile')}</Menu.Item>
+                <Menu.Item key="/user/settings" icon={<SettingOutlined />}>{t('common:menu.my_settings')}</Menu.Item>
+                <Menu.Divider />
+                <Menu.Item key="/repository/design" icon={<DatabaseOutlined />}>{t('common:menu.repositories')}</Menu.Item>
+                <Menu.Item key="/system" icon={<ToolOutlined />}>{t('common:menu.system')}</Menu.Item>
+                <Menu.Item key="/users" icon={<TeamOutlined />}>{t('common:menu.users')}</Menu.Item>
+                <Menu.Item key="/admin/management/groups" icon={<FormOutlined />}>{t('common:menu.groups_and_privileges')}</Menu.Item>
+                <Menu.Item key="/notification" icon={<NotificationOutlined />}>{t('common:menu.notification')}</Menu.Item>
+                <Menu.Item key="/tags" icon={<NumberOutlined />}>{t('common:menu.tags')}</Menu.Item>
+                <Menu.Item key="/mail" icon={<MailOutlined />}>{t('common:menu.mail')}</Menu.Item>
             </Menu>
         </div>
     )
--- NEW_ADMIN_UI/webstudioadmin/src/layouts/DefaultLayout.tsx
+++ NEW_ADMIN_UI/webstudioadmin/src/layouts/DefaultLayout.tsx
@@ -1,14 +1,31 @@
 import React from 'react'
 import MainMenu from 'components/MainMenu'
+import { Layout } from 'antd'
+import { Header } from 'containers/Header'
+import { Footer } from '../containers/Footer'
+
+const { Content: AntContent } = Layout
+
+const layoutStyle: React.CSSProperties = {
+    backgroundColor: '#fff',
+    minHeight: '100vh',
+}
 
 function DefaultLayout({ children }: { children: React.ReactNode }) {
     return (
-        <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'left' }}>
-            <MainMenu />
-            <div style={{ padding: 20, width: '100%' }}>
-                {children}
-            </div>
-        </div>
+        <Layout style={layoutStyle}>
+            <Header />
+            <AntContent>
+                <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'left' }}>
+                    <MainMenu />
+                    <div style={{ padding: 20, width: '100%' }}>
+                        {children}
+                    </div>
+                </div>
+            </AntContent>
+            <Footer />
+        </Layout>
     )
 }
+
 export default DefaultLayout
