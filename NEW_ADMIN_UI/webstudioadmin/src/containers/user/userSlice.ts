5da90e8e0844101d7ec04be2bd2f7c1f24daa07c
Aliaksandr Samuseu <asamuseu@eisgroup.com>
2024-01-26 17:30:25 +0100

    EPBDS-14150 notifications / user profile / user settings

---
+++ NEW_ADMIN_UI/webstudioadmin/src/containers/user/userSlice.ts
@@ -0,0 +1,103 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import { apiCall } from '../../services'
+import { createAppAsyncThunk } from '../../store/store'
+
+interface UserState {
+    profile: {
+        email?: string
+        displayName?: string
+        firstName?: string
+        lastName?: string
+        showHeader?: boolean
+        showFormulas?: boolean
+        testsPerPage?: number
+        testsFailuresOnly?: boolean
+        testsFailuresPerTest?: number
+        showComplexResult?: boolean
+        showRealNumbers?: boolean
+        treeView?: string
+        username?: string
+        externalFlags?: {
+            displayNameExternal: boolean
+            firstNameExternal: boolean
+            lastNameExternal: boolean
+            emailExternal: boolean
+            emailVerified: boolean
+        },
+        profiles?: {
+            displayName: string
+            description: string
+            name: string
+        }[],
+        // Not provided by the API
+        displayNameSelect?: string
+        // Not provided by the API but mandatory for the API
+        changePassword?: {
+            currentPassword: string
+            newPassword: string
+            confirmPassword: string
+        }
+    }
+    status: 'idle' | 'loading' | 'succeeded' | 'failed'
+    isLoggedIn: boolean
+}
+
+const initialState: UserState = {
+    profile: {},
+    status: 'idle',
+    isLoggedIn: false,
+}
+
+export const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {
+        logout: state => {
+            state.profile = {}
+            state.isLoggedIn = false
+        }
+    },
+    extraReducers: builder => {
+        builder
+            .addCase(fetchUserProfile.pending, (state, action) => {
+                state.status = 'loading'
+            })
+            .addCase(fetchUserProfile.fulfilled, (state, action) => {
+                state.status = 'succeeded'
+                state.profile = action.payload
+                state.isLoggedIn = true
+            })
+            .addCase(fetchUserProfile.rejected, (state, action) => {
+                state.status = 'failed'
+                state.isLoggedIn = false
+            })
+            .addCase(updateUserProfile.pending, (state, action) => {
+                state.status = 'loading'
+            })
+            .addCase(updateUserProfile.fulfilled, (state, action) => {
+                state.status = 'succeeded'
+                state.profile = { ...state.profile, ...action.payload }
+            })
+            .addCase(updateUserProfile.rejected, (state, action) => {
+                state.status = 'failed'
+            })
+    }
+})
+
+export const fetchUserProfile = createAsyncThunk('user/fetchUserProfile', async () => {
+    return await apiCall('/users/profile') as UserState['profile']
+})
+
+export const updateUserProfile = createAsyncThunk('user/updateUserProfile', async (data: UserState['profile']) => {
+    await apiCall('/users/profile', {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+    })
+
+    return data
+})
+
+export default userSlice.reducer
---
+++ NEW_ADMIN_UI/webstudioadmin/src/pages/UserPage.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+export const UserPage: React.FC = () => {
+    return (
+        <Outlet />
+    )
+}
