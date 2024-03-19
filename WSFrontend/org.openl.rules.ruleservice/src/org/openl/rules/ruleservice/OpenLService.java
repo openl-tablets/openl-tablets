4079d1a13146ded6a3bdf090bd139ab986a08224
Yury Molchan <ymolchan@eisgroup.com>
2024-03-19 19:20:58 +0200

    EPBDS-14346 Invoke OpenLService with arguments in JSON

--- WSFrontend/org.openl.rules.ruleservice/src/org/openl/rules/ruleservice/OpenLService.java
+++ WSFrontend/org.openl.rules.ruleservice/src/org/openl/rules/ruleservice/OpenLService.java
@@ -1,8 +1,12 @@
 package org.openl.rules.ruleservice;
 
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import org.openl.rules.ruleservice.simple.RulesFrontend;
+import org.openl.rules.serialization.JsonUtils;
 import org.openl.runtime.ASMProxyFactory;
 import org.openl.spring.env.PropertySourcesLoader;
 
@@ -13,6 +17,7 @@ import org.openl.spring.env.PropertySourcesLoader;
  */
 public class OpenLService {
 
+    private static final Class<?>[] EMPTY_CLASSES = new Class[0];
     static RulesFrontend rulesFrontend; // non-private opened for testing purposes
     private static ClassPathXmlApplicationContext context;
 
@@ -26,6 +31,49 @@ public class OpenLService {
      */
     public static Object call(String serviceName, String ruleName, Object... params) throws Exception {
         return getRulesFrontend().execute(serviceName, ruleName, params);
+    }
+
+    public static String callJSON(String serviceName, String ruleName, String json) throws Exception {
+        var instance = get(serviceName);
+        if (instance == null) {
+            throw new IllegalArgumentException(String.format("Service '%s' is not found.", serviceName));
+        }
+        ArrayList<Method> methods = new ArrayList<>(2);
+        for (Method method : instance.getClass().getMethods()) {
+            if (method.getName().equals(ruleName)) {
+                methods.add(method);
+            }
+        }
+        if (methods.isEmpty()) {
+            throw new IllegalArgumentException(String.format("Method '%s' is not found in service '%s'.", ruleName, serviceName));
+        }
+
+        if (methods.size() > 1) {
+            throw new IllegalArgumentException(String.format("Non-unique '%s' method name in service '%s'. There are %d methods with the same name.", ruleName, serviceName, methods.size()));
+        }
+
+        var caller = methods.get(0);
+
+        var args = new Object[caller.getParameterCount()];
+
+        var mapper = JsonUtils.getCachedObjectMapper(caller, EMPTY_CLASSES);
+        if (json != null) {
+            if (caller.getParameterCount() == 1) {
+                args[0] = mapper.readValue(json, caller.getParameterTypes()[0]);
+            } else {
+                var tree = mapper.readTree(json);
+                for (int i = 0; i < caller.getParameterCount(); i++) {
+                    var parameter = caller.getParameters()[i];
+                    var name = parameter.getName();
+                    var type = parameter.getType();
+                    var node = tree.get(name);
+                    args[i] = mapper.treeToValue(node, type);
+                }
+            }
+        }
+        var result = caller.invoke(instance, args);
+        return result == null || result instanceof String ? (String) result : mapper.writeValueAsString(result);
+
     }
 
     /**
@@ -47,9 +95,10 @@ public class OpenLService {
      */
     public static <T> T proxy(String serviceName, Class<T> proxyInterface) throws Exception {
         ClassLoader cl = Thread.currentThread().getContextClassLoader();
-        return ASMProxyFactory.newProxyInstance(cl, (method, args) -> {
-            return getRulesFrontend().execute(serviceName, method.getName(), method.getParameterTypes(), args);
-        }, proxyInterface);
+        return ASMProxyFactory.newProxyInstance(cl,
+                (method, args) -> getRulesFrontend().execute(serviceName, method.getName(), method.getParameterTypes(), args),
+                proxyInterface
+        );
     }
 
     /**
