b865901c372f5572ba097496a97073a1ed5d5ed0
Yury Molchan <ymolchan@eisgroup.com>
2024-04-20 00:15:49 +0300

    EPBDS-14389 Merge ExceptionDetails into RuleServiceWrapperException

--- WSFrontend/org.openl.rules.ruleservice.common/src/org/openl/rules/ruleservice/core/RuleServiceWrapperException.java
+++ WSFrontend/org.openl.rules.ruleservice.common/src/org/openl/rules/ruleservice/core/RuleServiceWrapperException.java
@@ -1,6 +1,13 @@
 package org.openl.rules.ruleservice.core;
 
-import java.util.Objects;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+
+import org.openl.binding.impl.cast.OutsideOfValidDomainException;
+import org.openl.exception.OpenLCompilationException;
+import org.openl.exception.OpenLRuntimeException;
+import org.openl.exception.OpenLUserRuntimeException;
+import org.openl.rules.calc.SpreadsheetResult;
+import org.openl.rules.calc.SpreadsheetResultBeanPropertyNamingStrategy;
 
 /**
  * Exception for wrapping exceptions for returning them from ruleservice.
@@ -11,7 +18,7 @@ public class RuleServiceWrapperException extends RuleServiceRuntimeException {
 
     private static final long serialVersionUID = 3618613334261575918L;
 
-    private final ExceptionDetails details;
+    private final Object body;
     private final ExceptionType type;
 
     /**
@@ -23,17 +30,48 @@ public class RuleServiceWrapperException extends RuleServiceRuntimeException {
      * @param cause   the cause (which is saved for later retrieval by the {@link #getCause()} method). (A <tt>null</tt>
      *                value is permitted, and indicates that the cause is nonexistent or unknown.)
      */
-    public RuleServiceWrapperException(ExceptionDetails details, ExceptionType type, String message, Throwable cause) {
+    private RuleServiceWrapperException(Object details, ExceptionType type, String message, Throwable cause) {
         super(message, cause);
-        this.details = Objects.requireNonNull(details, "Details is required!");
+        this.body = details;
         this.type = type;
+    }
+
+    public RuleServiceWrapperException(String message, ExceptionType type) {
+        super(message);
+        this.body = null;
+        this.type = type;
+    }
+
+    public static RuleServiceWrapperException create(Throwable ex, SpreadsheetResultBeanPropertyNamingStrategy namingStrategy) {
+
+        Object body = null;
+        var type = ExceptionType.SYSTEM;
+        var message = ex.getMessage();
+        for (Throwable t : ExceptionUtils.getThrowableList(ex)) {
+            if (t instanceof OpenLUserRuntimeException) {
+                body = ((OpenLUserRuntimeException) t).getBody();
+                body = SpreadsheetResult.convertSpreadsheetResult(body, namingStrategy);
+                type = ExceptionType.USER_ERROR;
+                message = t.getMessage();
+            } else if (t instanceof OutsideOfValidDomainException) {
+                type = ExceptionType.VALIDATION;
+                message = t.getMessage();
+            } else if (t instanceof OpenLRuntimeException) {
+                type = ExceptionType.RULES_RUNTIME;
+                message = t.getMessage();
+            } else if (t instanceof OpenLCompilationException) {
+                type = ExceptionType.COMPILATION;
+                message = t.getMessage();
+            }
+        }
+        return new RuleServiceWrapperException(body, type, message, ex);
     }
 
     /**
      * Returns simple Message
      */
-    public ExceptionDetails getDetails() {
-        return details;
+    public Object getBody() {
+        return body;
     }
 
     /**
