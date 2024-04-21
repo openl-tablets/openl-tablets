191e69966e0175ee89a9eb1a241f3fa2cabb7c43
Yury Molchan <ymolchan@eisgroup.com>
2024-04-21 13:44:24 +0300

    EPBDS-14259 Drop Cassandra support

--- ITEST/itest.storelogdata/src/org/openl/itest/serviceclass/Simple4ServiceAnnotationTemplate.java
+++ ITEST/itest.storelogdata/src/org/openl/itest/serviceclass/Simple4ServiceAnnotationTemplate.java
@@ -1,23 +1,14 @@
 package org.openl.itest.serviceclass;
 
-import org.openl.itest.cassandra.HelloEntity1;
-import org.openl.itest.cassandra.HelloEntity2;
-import org.openl.itest.cassandra.HelloEntity3;
-import org.openl.itest.cassandra.HelloEntity8;
 import org.openl.itest.db.HelloEntity9;
 import org.openl.rules.context.IRulesRuntimeContext;
 import org.openl.rules.ruleservice.core.interceptors.annotations.ServiceCallAfterInterceptor;
 import org.openl.rules.ruleservice.core.interceptors.annotations.ServiceCallBeforeInterceptor;
 import org.openl.rules.ruleservice.storelogdata.annotation.PrepareStoreLogData;
-import org.openl.rules.ruleservice.storelogdata.cassandra.annotation.StoreLogDataToCassandra;
 import org.openl.rules.ruleservice.storelogdata.db.annotation.StoreLogDataToDB;
 
 public interface Simple4ServiceAnnotationTemplate {
 
-    @StoreLogDataToCassandra(value = {HelloEntity1.class,
-            HelloEntity2.class,
-            HelloEntity3.class,
-            HelloEntity8.class}, sync = true)
     @StoreLogDataToDB(value = {org.openl.itest.db.HelloEntity1.class,
             org.openl.itest.db.HelloEntity2.class,
             org.openl.itest.db.HelloEntity3.class,
@@ -25,7 +16,6 @@ public interface Simple4ServiceAnnotationTemplate {
     @PrepareStoreLogData(PrepareStoreLogDataValues.class)
     String Hello(IRulesRuntimeContext runtimeContext, Integer hour);
 
-    @StoreLogDataToCassandra(HelloEntity1.class)
     @StoreLogDataToDB(org.openl.itest.db.HelloEntity1.class)
     @ServiceCallBeforeInterceptor(Simple4ServiceMethodBeforeAdvice.class)
     @ServiceCallAfterInterceptor(Simple4ServiceMethodAfterAdvice.class)
