957240e9e126aaa92753127081ca9d1656cb9257
Yury Molchan <ymolchan@eisgroup.com>
2024-04-21 20:45:05 +0300

    EPBDS-14260 Drop 'SpreadsheetResult converters' support

--- DEV/org.openl.rules.calculation.result/test/org/openl/rules/calculation/result/convertor2/sample/result/SimpleStep.java
+++
@@ -1,7 +0,0 @@
-package org.openl.rules.calculation.result.convertor2.sample.result;
-
-public class SimpleStep extends org.openl.rules.calculation.result.convertor2.SimpleStep {
-
-    private static final long serialVersionUID = 1L;
-
-}
