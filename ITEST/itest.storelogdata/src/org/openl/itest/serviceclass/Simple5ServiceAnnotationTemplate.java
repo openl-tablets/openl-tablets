96e27c1c98e1f245be6bc273e8a6f1f9cb3d3c49
Yury Molchan <ymolchan@eisgroup.com>
2024-04-21 13:44:24 +0300

    EPBDS-14259 Drop Cassandra support

--- ITEST/itest.storelogdata/src/org/openl/itest/serviceclass/Simple5ServiceAnnotationTemplate.java
+++ ITEST/itest.storelogdata/src/org/openl/itest/serviceclass/Simple5ServiceAnnotationTemplate.java
@@ -1,12 +1,10 @@
 package org.openl.itest.serviceclass;
 
 import org.openl.rules.context.IRulesRuntimeContext;
-import org.openl.rules.ruleservice.storelogdata.cassandra.annotation.StoreLogDataToCassandra;
 import org.openl.rules.ruleservice.storelogdata.db.annotation.StoreLogDataToDB;
 
 public interface Simple5ServiceAnnotationTemplate {
 
-    @StoreLogDataToCassandra
     @StoreLogDataToDB
     String Hello(IRulesRuntimeContext runtimeContext, Integer hour);
 
--- WSFrontend/org.openl.rules.ruleservice.ws.storelogdata.cassandra/src/org/openl/rules/ruleservice/storelogdata/cassandra/CassandraOperationException.java
+++
@@ -1,7 +0,0 @@
-package org.openl.rules.ruleservice.storelogdata.cassandra;
-
-class CassandraOperationException extends RuntimeException {
-    public CassandraOperationException(String message) {
-        super(message);
-    }
-}
