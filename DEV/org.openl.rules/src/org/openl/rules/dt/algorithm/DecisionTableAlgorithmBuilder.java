0441d1de848aab9ebfdc53a83c0b3637cf3780c1
Vladyslav Pikus <vpikus@eisgroup.com>
2024-04-26 14:59:33 +0300

    EPBDS-14408 Fix NPE and fallback to the default evaluator
    (cherry picked from commit a472c414846e812a90f90d007ce15be29645da31)

--- DEV/org.openl.rules/src/org/openl/rules/dt/algorithm/DecisionTableAlgorithmBuilder.java
+++ DEV/org.openl.rules/src/org/openl/rules/dt/algorithm/DecisionTableAlgorithmBuilder.java
@@ -107,6 +107,9 @@ public class DecisionTableAlgorithmBuilder implements IAlgorithmBuilder {
                 v = v.substring(0, v.indexOf("["));
             }
             field = type.getField(v);
+            if (field == null) {
+                return null;
+            }
             type = field.getType();
             if (type.isArray() && arrayAccess) {
                 type = type.getComponentClass();
