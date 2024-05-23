1068c806a95417b50be1e24c153ae8caca6c432a
Aliaksandr Samuseu <asamuseu@eisgroup.com>
2024-05-23 11:45:53 +0200

    EPBDS-14428 review plugin import

---
+++ NEW_ADMIN_UI/webstudioadmin/src/plugins.config.example.ts
@@ -0,0 +1,14 @@
+export interface PluginConfiguration {
+    name: string
+    library: string
+}
+
+if (typeof window.pluginsConfiguration === 'undefined') {
+    // define default plugins configuration
+    window.pluginsConfiguration = [
+        {
+            name: 'claimEditorPlugin',
+            library: 'http://localhost:3101/claim_editor_plugin.js',
+        },
+    ]
+}
--- NEW_ADMIN_UI/webstudioadmin/src/plugins.config.ts
+++ NEW_ADMIN_UI/webstudioadmin/src/plugins.config.ts
@@ -1,25 +1,13 @@
-import * as path from 'path'
-
 export interface PluginConfiguration {
     name: string
-    routeRoot: string
     library: string
-    rules?: string[]
 }
-
-console.log('window.pluginsConfiguration', window.pluginsConfiguration)
 
 if (typeof window.pluginsConfiguration === 'undefined') {
     // define default plugins configuration
     window.pluginsConfiguration = [
-        // {
-        //     name: 'claimEditorPlugin',
-        //     routeRoot: '/faces/pages/ui.xhtml',
-        //     library: 'http://localhost:3101/claim_editor_plugin.js',
-        // },
         {
             name: 'claimEditorPlugin',
-            routeRoot: '/claim',
             library: 'http://localhost:3101/claim_editor_plugin.js',
         },
     ]
