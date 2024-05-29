23bf552c1919b379940d161d8d052871fc1073c0
Yury Molchan <ymolchan@eisgroup.com>
2024-05-29 22:30:19 +0300

    EPBDS-14249 Do not use alternative configuration file names
    It is confused when two configuration exists

--- WSFrontend/org.openl.rules.ruleservice.ws/src/org/openl/rules/ruleservice/kafka/conf/KafkaDeployUtils.java
+++ WSFrontend/org.openl.rules.ruleservice.ws/src/org/openl/rules/ruleservice/kafka/conf/KafkaDeployUtils.java
@@ -10,23 +10,15 @@ import org.openl.rules.ruleservice.core.ResourceLoader;
 import org.openl.rules.ruleservice.core.ServiceDescription;
 
 public final class KafkaDeployUtils {
-    private static final String KAFKA_DEPLOY_FILE_NAME = "kafka-deploy.yaml";
-
-    private static final String KAFKA_DEPLOY_ALTERNATIVE_FILE_NAME = "kafka-deploy.yml";
 
     private KafkaDeployUtils() {
     }
 
     public static KafkaDeploy getKafkaDeploy(ServiceDescription serviceDescription) throws IOException {
         ResourceLoader resourceLoader = serviceDescription.getResourceLoader();
-        Resource resource = resourceLoader.getResource(KAFKA_DEPLOY_FILE_NAME);
+        Resource resource = resourceLoader.getResource("kafka-deploy.yaml");
         if (!resource.exists()) {
-            resource = resourceLoader.getResource(KAFKA_DEPLOY_ALTERNATIVE_FILE_NAME);
-            if (!resource.exists()) {
-                throw new FileNotFoundException(String.format("File '%s' or '%s' is not found.",
-                        KAFKA_DEPLOY_FILE_NAME,
-                        KAFKA_DEPLOY_ALTERNATIVE_FILE_NAME));
-            }
+            throw new FileNotFoundException("File 'kafka-deploy.yaml' is not found.");
         }
         ObjectMapper mapper = YamlObjectMapperBuilder.newInstance();
         return mapper.readValue(resource.getResourceAsStream(), KafkaDeploy.class);
