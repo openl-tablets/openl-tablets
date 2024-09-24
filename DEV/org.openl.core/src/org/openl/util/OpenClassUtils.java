5b33340cb68db14c38aa421d264525cf8390e0a9
Raveena <rdeshmane@eisgroup.com>
2024-09-24 13:31:38 +0300

    EPBDS-14557 Validation of Alias Datatype literals during  compilation

--- DEV/org.openl.core/src/org/openl/util/OpenClassUtils.java
+++ DEV/org.openl.core/src/org/openl/util/OpenClassUtils.java
@@ -1,5 +1,12 @@
 package org.openl.util;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import org.openl.domain.EnumDomain;
+import org.openl.domain.IDomain;
 import org.openl.types.IOpenClass;
 import org.openl.types.java.JavaOpenClass;
 
@@ -48,5 +55,77 @@ public final class OpenClassUtils {
 
     public static boolean isVoid(IOpenClass type) {
         return type == JavaOpenClass.VOID || type == JavaOpenClass.CLS_VOID;
+    }
+
+    @SuppressWarnings("unchecked")
+    public static String isValidValue(Object value, IOpenClass paramType) {
+        var domain = (IDomain<Object>) paramType.getDomain();
+
+        if (domain != null) {
+            return validateDomain(value, domain, paramType);
+        }
+        return null;
+    }
+
+    private static String validateDomain(Object value,
+                                         IDomain<Object> domain,
+                                         IOpenClass paramType) {
+        String validationMessage = null;
+        if (value == null) {
+            return validationMessage;
+        }
+        if (value.getClass().isArray()) {
+            int length = Array.getLength(value);
+            for (int i = 0; i < length && validationMessage == null; i++) {
+                var element = Array.get(value, i);
+                validationMessage = validateDomain(element, domain, paramType);
+            }
+        } else if (value instanceof Iterable) {
+            var list = (Iterable) value;
+            for (var iterator = list.iterator(); iterator.hasNext() && validationMessage == null; ) {
+                var element = iterator.next();
+                validationMessage = validateDomain(element, domain, paramType);
+            }
+        } else {
+            // block is surrounded by try block, as EnumDomain
+            // implementation throws a
+            // RuntimeException when value doesn`t belong to domain.
+            //
+            var contains = true;
+            if (domain instanceof EnumDomain) {
+                contains = belongsToEnum(((EnumDomain) domain).getAllObjects(), value.toString());
+            } else {
+                contains = domain.selectObject(value);
+            }
+
+            if (!contains) {
+                validationMessage = String.format("The value '%s' is outside of valid domain '%s'. Valid values: %s",
+                        value,
+                        paramType.getName(),
+                        DomainUtils.toString(domain));
+            }
+        }
+        return validationMessage;
+    }
+
+
+    public static boolean belongsToEnum(Object[] arrayEnum, String inputKey) {
+        String generatedEnumKey = null;
+        for (int i = 0; i < arrayEnum.length && !inputKey.equals(generatedEnumKey); i++) {
+            if (arrayEnum[i] instanceof Object[]) {
+                var arrayExtracted = (Object[]) arrayEnum[i];
+                generatedEnumKey = generateKey(arrayExtracted);
+            } else {
+                generatedEnumKey = arrayEnum[i].toString();
+            }
+        }
+        return inputKey.equals(generatedEnumKey);
+    }
+
+    public static String generateKey(Object[] array) {
+        return Arrays.stream(array)
+                .filter(Objects::nonNull)
+                .map(Object::toString)  // Convert each element to a string
+                .collect(Collectors.joining(","));
     }
 }
