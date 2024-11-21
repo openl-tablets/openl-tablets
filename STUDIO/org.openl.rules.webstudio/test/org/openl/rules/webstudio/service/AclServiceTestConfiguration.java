39c4924510ae53af34183f50d00ca8298f480b41
Vladyslav Pikus <vpikus@eisgroup.com>
2024-11-21 19:51:01 +0200

    EPBDS-14505 Replace old security checker with security annotations

--- STUDIO/org.openl.rules.webstudio/test/org/openl/rules/webstudio/service/AclServiceTestConfiguration.java
+++ STUDIO/org.openl.rules.webstudio/test/org/openl/rules/webstudio/service/AclServiceTestConfiguration.java
@@ -3,14 +3,13 @@ package org.openl.rules.webstudio.service;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
+import org.openl.rules.security.config.MethodSecurityConfig;
 import org.openl.rules.webstudio.config.SpringCachingConfig;
 import org.openl.security.acl.config.OpenLAclConfiguration;
 
 @Configuration
-@Import({OpenLAclConfiguration.class, SpringCachingConfig.class})
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@Import({OpenLAclConfiguration.class, SpringCachingConfig.class, MethodSecurityConfig.class})
 public class AclServiceTestConfiguration {
 
     @Bean
