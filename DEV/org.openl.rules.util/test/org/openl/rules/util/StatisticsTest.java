f6c1d3790e29b01f94d3df749080d85405ad6c1b
Raveena <rdeshmane@eisgroup.com>
2024-12-09 11:32:56 +0200

    EPBDS-14157 Bigdecimal and biginteger support

--- DEV/org.openl.rules.util/test/org/openl/rules/util/StatisticsTest.java
+++ DEV/org.openl.rules.util/test/org/openl/rules/util/StatisticsTest.java
@@ -11,12 +11,12 @@ import static org.openl.rules.util.Statistics.correl;
 import static org.openl.rules.util.Statistics.max;
 import static org.openl.rules.util.Statistics.min;
 import static org.openl.rules.util.Statistics.covarP;
-import static org.openl.rules.util.Statistics.varP;
 import static org.openl.rules.util.Statistics.covarS;
 import static org.openl.rules.util.Statistics.stdevS;
-import static org.openl.rules.util.Statistics.varS;
 import static org.openl.rules.util.Statistics.slope;
 import static org.openl.rules.util.Statistics.stdevP;
+import static org.openl.rules.util.Statistics.varP;
+import static org.openl.rules.util.Statistics.varS;
 import static org.openl.rules.util.Sum.sum;
 
 import java.math.BigDecimal;
@@ -103,7 +103,7 @@ public class StatisticsTest {
 
     @Test
     public void testStandardPopulationDeviation() {
-        assertNull(stdevP(null));
+        assertNull(stdevP((Number) null));
         assertNull(stdevP(new Double[2]));
 
         assertEquals(4.0276819911981905, stdevP(1, 10, 9), DELTA);
@@ -118,17 +118,17 @@ public class StatisticsTest {
         assertEquals(2.160246899469287, stdevP(3f, 4f, 8f), DELTA);
         assertEquals(2.160246899469287, stdevP(3d, 4d, 8d), DELTA);
         assertEquals(2.160246899469287, stdevP((byte) 3, (short) 4, 8), DELTA);
-        assertEquals(2.494438257849294,
-                stdevP(BigInteger.valueOf(10), BigInteger.valueOf(4), BigInteger.valueOf(8)), DELTA);
-        assertEquals(0.5, stdevP(BigDecimal.valueOf(3), BigDecimal.valueOf(4)), DELTA);
-        assertEquals(2.0548046676563256,
-                stdevP(BigInteger.valueOf(3), BigInteger.valueOf(5), BigInteger.valueOf(8)), DELTA);
+        assertEquals(new BigDecimal("2.494438257849294257055832488211033"),
+                stdevP(BigInteger.valueOf(10), BigInteger.valueOf(4), BigInteger.valueOf(8)));
+        assertEquals(new BigDecimal("0.5"), stdevP(BigDecimal.valueOf(3), BigDecimal.valueOf(4)));
+        assertEquals(new BigDecimal("2.054804667656325483416730793818081"),
+                stdevP(BigInteger.valueOf(3), BigInteger.valueOf(5), BigInteger.valueOf(8)));
 
     }
 
     @Test
     public void testPopulationVariance() {
-        assertNull(varP(null));
+        assertNull(varP((Number) null));
         assertNull(varP(new Double[2]));
 
         assertEquals(16.22222222222222, varP(1, 10, 9), DELTA);
@@ -143,17 +143,17 @@ public class StatisticsTest {
         assertEquals(4.666666666666667, varP(3f, 4f, 8f), DELTA);
         assertEquals(4.666666666666667, varP(3d, 4d, 8d), DELTA);
         assertEquals(4.666666666666667, varP((byte) 3, (short) 4, 8), DELTA);
-        assertEquals(6.222222222222222,
-                varP(BigInteger.valueOf(10), BigInteger.valueOf(4), BigInteger.valueOf(8)), DELTA);
-        assertEquals(0.25, varP(BigDecimal.valueOf(3), BigDecimal.valueOf(4)), DELTA);
-        assertEquals(4.222222222222222,
-                varP(BigInteger.valueOf(3), BigInteger.valueOf(5), BigInteger.valueOf(8)), DELTA);
+        assertEquals(new BigDecimal("6.222222222222222222222222222222222"),
+                varP(BigInteger.valueOf(10), BigInteger.valueOf(4), BigInteger.valueOf(8)));
+        assertEquals(new BigDecimal("0.25"), varP(BigDecimal.valueOf(3), BigDecimal.valueOf(4)));
+        assertEquals(new BigDecimal("4.222222222222222222222222222222222"),
+                varP(BigInteger.valueOf(3), BigInteger.valueOf(5), BigInteger.valueOf(8)));
 
     }
 
     @Test
     public void testSampleVariance() {
-        assertNull(varS(null));
+        assertNull(varS((Number) null));
         assertNull(varS(new Double[2]));
 
         assertEquals(24.333333333333332, varS(1, 10, 9), DELTA);
@@ -168,17 +168,21 @@ public class StatisticsTest {
         assertEquals(7.0, varS(3f, 4f, 8f), DELTA);
         assertEquals(7.0, varS(3d, 4d, 8d), DELTA);
         assertEquals(7.0, varS((byte) 3, (short) 4, 8), DELTA);
-        assertEquals(9.333333333333334,
-                varS(BigInteger.valueOf(10), BigInteger.valueOf(4), BigInteger.valueOf(8)), DELTA);
-        assertEquals(0.5, varS(BigDecimal.valueOf(3), BigDecimal.valueOf(4)), DELTA);
-        assertEquals(6.333333333333333,
-                varS(BigInteger.valueOf(3), BigInteger.valueOf(5), BigInteger.valueOf(8)), DELTA);
+        assertEquals(new BigDecimal("9.333333333333333333333333333333333"),
+                varS(BigInteger.valueOf(10), BigInteger.valueOf(4), BigInteger.valueOf(8)));
+        BigDecimal [] testArray = new BigDecimal[2];
+        testArray[0] = new BigDecimal("3");
+        testArray[1] = new BigDecimal("4");
+        assertEquals(new BigDecimal("0.50"), varS(testArray));
+        assertEquals(new BigDecimal("0.50"), varS(BigDecimal.valueOf(3), BigDecimal.valueOf(4)));
+        assertEquals(new BigDecimal("6.333333333333333333333333333333333"),
+                varS(BigInteger.valueOf(3), BigInteger.valueOf(5), BigInteger.valueOf(8)));
 
     }
 
     @Test
     public void testSampleStandardDeviation() {
-        assertNull(stdevS(null));
+        assertNull(stdevS((Number) null));
         assertNull(stdevS(new Double[2]));
 
         assertEquals(4.932882862316247, stdevS(1, 10, 9), DELTA);
@@ -193,17 +197,17 @@ public class StatisticsTest {
         assertEquals(2.6457513110645907, stdevS(3f, 4f, 8f), DELTA);
         assertEquals(2.6457513110645907, stdevS(3d, 4d, 8d), DELTA);
         assertEquals(2.6457513110645907, stdevS((byte) 3, (short) 4, 8), DELTA);
-        assertEquals(3.0550504633038935,
-                stdevS(BigInteger.valueOf(10), BigInteger.valueOf(4), BigInteger.valueOf(8)), DELTA);
-        assertEquals(0.7071067811865476, stdevS(BigDecimal.valueOf(3), BigDecimal.valueOf(4)), DELTA);
-        assertEquals(2.516611478423583,
-                stdevS(BigInteger.valueOf(3), BigInteger.valueOf(5), BigInteger.valueOf(8)), DELTA);
+        assertEquals(new BigDecimal("3.055050463303893337725364795818672"),
+                stdevS(BigInteger.valueOf(10), BigInteger.valueOf(4), BigInteger.valueOf(8)));
+        assertEquals(new BigDecimal("0.707106781186547524400844362104849"), stdevS(BigDecimal.valueOf(3), BigDecimal.valueOf(4)));
+        assertEquals(new BigDecimal("2.516611478423583232412228268982039"),
+                stdevS(BigInteger.valueOf(3), BigInteger.valueOf(5), BigInteger.valueOf(8)));
 
     }
 
     @Test
     public void testSampleCovariance() {
-        assertNull(covarS(null, null));
+        assertNull(covarS((Number[])null, null));
         assertNull(covarS(new Double[2], new Double[2]));
         assertNull(covarS(new Double[]{1.0, null}, new Double[2]));
         assertNull(covarS(new Double[]{1.0, 2.0}, new Double[2]));
@@ -217,19 +221,20 @@ public class StatisticsTest {
         assertEquals(7, covarS(new Byte[]{(byte) 3, (byte) 4, (byte) 8}, new Byte[]{(byte) 3, (byte) 4, (byte) 8}), DELTA);
         assertEquals(7, covarS(new Short[]{(short) 3, (short) 4, (short) 8}, new Short[]{(short) 3, (short) 4, (short) 8}), DELTA);
         assertEquals(7, covarS(new Integer[]{3, 4, 8}, new Integer[]{3, 4, 8}), DELTA);
+        assertEquals(0.0, covarS(new Integer[]{10, 4, 8}, new Integer[]{3, 4, 8}), DELTA);
         assertEquals(7, covarS(new Long[]{3L, 4L, 8L}, new Long[]{3L, 4L, 8L}), DELTA);
         assertEquals(7, covarS(new Float[]{3f, 4f, 8f}, new Float[]{3f, 4f, 8f}), DELTA);
         assertEquals(7, covarS(new Byte[]{(byte) 3, (byte) 4, (byte) 8}, new Short[]{(short) 3, (short) 4, (short) 8}), DELTA);
-        assertEquals(0.0,
+        assertEquals(new BigDecimal("0E-33"),
                 covarS(new BigInteger[]{BigInteger.valueOf(10), BigInteger.valueOf(4), BigInteger.valueOf(8)},
-                        new BigInteger[]{BigInteger.valueOf(3), BigInteger.valueOf(4), BigInteger.valueOf(8)}), DELTA);
-        assertEquals(0.5, covarS(new BigDecimal[]{BigDecimal.valueOf(3), BigDecimal.valueOf(4)}, new BigDecimal[]{BigDecimal.valueOf(3), BigDecimal.valueOf(4)}), DELTA);
+                        new BigInteger[]{BigInteger.valueOf(3), BigInteger.valueOf(4), BigInteger.valueOf(8)}));
+        assertEquals(new BigDecimal("0.50"), covarS(new BigDecimal[]{BigDecimal.valueOf(3), BigDecimal.valueOf(4)}, new BigDecimal[]{BigDecimal.valueOf(3), BigDecimal.valueOf(4)}));
 
     }
 
     @Test
     public void testPopulationCovariance() {
-        assertNull(covarP(null, null));
+        assertNull(covarP((Number[])null, null));
         assertNull(covarP(new Double[2], new Double[2]));
         assertNull(covarP(new Double[]{1.0, null}, new Double[]{null, null}));
         assertNull(covarP(new Double[]{1.0, 2.0}, new Double[]{null, null}));
@@ -246,16 +251,16 @@ public class StatisticsTest {
         assertEquals(4.666666666666667, covarP(new Long[]{3L, 4L, 8L}, new Long[]{3L, 4L, 8L}), DELTA);
         assertEquals(4.666666666666667, covarP(new Float[]{3f, 4f, 8f}, new Float[]{3f, 4f, 8f}), DELTA);
         assertEquals(4.666666666666667, covarP(new Byte[]{(byte) 3, (byte) 4, (byte) 8}, new Short[]{(short) 3, (short) 4, (short) 8}), DELTA);
-        assertEquals(0.0,
+        assertEquals(new BigDecimal("0E-33"),
                 covarP(new BigInteger[]{BigInteger.valueOf(10), BigInteger.valueOf(4), BigInteger.valueOf(8)},
-                        new BigInteger[]{BigInteger.valueOf(3), BigInteger.valueOf(4), BigInteger.valueOf(8)}), DELTA);
-        assertEquals(0.25, covarP(new BigDecimal[]{BigDecimal.valueOf(3), BigDecimal.valueOf(4)}, new BigDecimal[]{BigDecimal.valueOf(3), BigDecimal.valueOf(4)}), DELTA);
+                        new BigInteger[]{BigInteger.valueOf(3), BigInteger.valueOf(4), BigInteger.valueOf(8)}));
+        assertEquals(new BigDecimal("0.25"), covarP(new BigDecimal[]{BigDecimal.valueOf(3), BigDecimal.valueOf(4)}, new BigDecimal[]{BigDecimal.valueOf(3), BigDecimal.valueOf(4)}));
 
     }
 
     @Test
     public void testCorrelationCoefficient() {
-        assertNull(correl(null, null));
+        assertNull(correl((Number[]) null, null));
         assertNull(correl(new Double[2], new Double[2]));
         assertNull(correl(new Double[]{1.0, null}, new Double[]{null, null}));
         assertNull(correl(new Double[]{1.0, 2.0}, new Double[]{null, null}));
@@ -272,15 +277,15 @@ public class StatisticsTest {
         assertEquals(0.9999999999999999, correl(new Long[]{3L, 4L, 8L}, new Long[]{3L, 4L, 8L}), DELTA);
         assertEquals(0.9999999999999999, correl(new Float[]{3f, 4f, 8f}, new Float[]{3f, 4f, 8f}), DELTA);
         assertEquals(0.9999999999999999, correl(new Byte[]{(byte) 3, (byte) 4, (byte) 8}, new Short[]{(short) 3, (short) 4, (short) 8}), DELTA);
-        assertEquals(0.0,
+        assertEquals(new BigDecimal("0E+32"),
                 correl(new BigInteger[]{BigInteger.valueOf(10), BigInteger.valueOf(4), BigInteger.valueOf(8)},
-                        new BigInteger[]{BigInteger.valueOf(3), BigInteger.valueOf(4), BigInteger.valueOf(8)}), DELTA);
-        assertEquals(0.9999999999999998, correl(new BigDecimal[]{BigDecimal.valueOf(3), BigDecimal.valueOf(4)}, new BigDecimal[]{BigDecimal.valueOf(3), BigDecimal.valueOf(4)}), DELTA);
+                        new BigInteger[]{BigInteger.valueOf(3), BigInteger.valueOf(4), BigInteger.valueOf(8)}));
+        assertEquals(new BigDecimal("1.000000000000000000000000000000000"), correl(new BigDecimal[]{BigDecimal.valueOf(3), BigDecimal.valueOf(4)}, new BigDecimal[]{BigDecimal.valueOf(3), BigDecimal.valueOf(4)}));
     }
 
     @Test
     public void testRSQ() {
-        assertNull(rsq(null, null));
+        assertNull(rsq((Number[])null, null));
         assertNull(rsq(new Double[2], new Double[2]));
         assertNull(rsq(new Double[]{1.0, null}, new Double[]{null, null}));
         assertNull(rsq(new Double[]{1.0, 2.0}, new Double[]{null, null}));
@@ -297,15 +302,15 @@ public class StatisticsTest {
         assertEquals(0.9999999999999998, rsq(new Long[]{3L, 4L, 8L}, new Long[]{3L, 4L, 8L}), DELTA);
         assertEquals(0.9999999999999998, rsq(new Float[]{3f, 4f, 8f}, new Float[]{3f, 4f, 8f}), DELTA);
         assertEquals(0.9999999999999998, rsq(new Byte[]{(byte) 3, (byte) 4, (byte) 8}, new Short[]{(short) 3, (short) 4, (short) 8}), DELTA);
-        assertEquals(0,
+        assertEquals(new BigDecimal("0E+64"),
                 rsq(new BigInteger[]{BigInteger.valueOf(10), BigInteger.valueOf(4), BigInteger.valueOf(8)},
-                        new BigInteger[]{BigInteger.valueOf(3), BigInteger.valueOf(4), BigInteger.valueOf(8)}), DELTA);
-        assertEquals(0.9999999999999996, rsq(new BigDecimal[]{BigDecimal.valueOf(3), BigDecimal.valueOf(4)}, new BigDecimal[]{BigDecimal.valueOf(3), BigDecimal.valueOf(4)}), DELTA);
+                        new BigInteger[]{BigInteger.valueOf(3), BigInteger.valueOf(4), BigInteger.valueOf(8)}));
+        assertEquals(new BigDecimal("1.000000000000000000000000000000000"), rsq(new BigDecimal[]{BigDecimal.valueOf(3), BigDecimal.valueOf(4)}, new BigDecimal[]{BigDecimal.valueOf(3), BigDecimal.valueOf(4)}));
     }
 
     @Test
     public void testSlope() {
-        assertNull(slope(null, null));
+        assertNull(slope((Number[])null, null));
         assertNull(slope(new Double[2], new Double[2]));
         assertNull(slope(new Double[]{1.0, null}, new Double[]{null, null}));
         assertNull(slope(new Double[]{1.0, 2.0}, new Double[]{null, null}));
@@ -322,15 +327,15 @@ public class StatisticsTest {
         assertEquals(1, slope(new Long[]{3L, 4L, 8L}, new Long[]{3L, 4L, 8L}), DELTA);
         assertEquals(1, slope(new Float[]{3f, 4f, 8f}, new Float[]{3f, 4f, 8f}), DELTA);
         assertEquals(1, slope(new Byte[]{(byte) 3, (byte) 4, (byte) 8}, new Short[]{(short) 3, (short) 4, (short) 8}), DELTA);
-        assertEquals(0,
-                slope(new BigInteger[]{BigInteger.valueOf(10), BigInteger.valueOf(4), BigInteger.valueOf(8)},
-                        new BigInteger[]{BigInteger.valueOf(3), BigInteger.valueOf(4), BigInteger.valueOf(8)}), DELTA);
-        assertEquals(1, slope(new BigDecimal[]{BigDecimal.valueOf(3), BigDecimal.valueOf(4)}, new BigDecimal[]{BigDecimal.valueOf(3), BigDecimal.valueOf(4)}), DELTA);
+        assertEquals(new BigDecimal("0E-33"),
+                slope(new BigInteger[] { BigInteger.valueOf(10), BigInteger.valueOf(4), BigInteger.valueOf(8) },
+                        new BigInteger[] { BigInteger.valueOf(3), BigInteger.valueOf(4), BigInteger.valueOf(8) }));
+        assertEquals(new BigDecimal("1"), slope(new BigDecimal[]{BigDecimal.valueOf(3), BigDecimal.valueOf(4)}, new BigDecimal[]{BigDecimal.valueOf(3), BigDecimal.valueOf(4)}));
     }
 
     @Test
     public void testIntercept() {
-        assertNull(intercept(null, null));
+        assertNull(intercept((Number[]) null, null));
         assertNull(intercept(new Double[2], new Double[2]));
         assertNull(intercept(new Double[]{1.0, null}, new Double[]{null, null}));
         assertNull(intercept(new Double[]{1.0, 2.0}, new Double[]{null, null}));
@@ -348,15 +353,15 @@ public class StatisticsTest {
         assertEquals(0, intercept(new Long[]{3L, 4L, 8L}, new Long[]{3L, 4L, 8L}), DELTA);
         assertEquals(0, intercept(new Float[]{3f, 4f, 8f}, new Float[]{3f, 4f, 8f}), DELTA);
         assertEquals(0, intercept(new Byte[]{(byte) 3, (byte) 4, (byte) 8}, new Short[]{(short) 3, (short) 4, (short) 8}), DELTA);
-        assertEquals(7.333333333333333,
+        assertEquals(new BigDecimal("7.333333333333333333333333333333333"),
                 intercept(new BigInteger[]{BigInteger.valueOf(10), BigInteger.valueOf(4), BigInteger.valueOf(8)},
-                        new BigInteger[]{BigInteger.valueOf(3), BigInteger.valueOf(4), BigInteger.valueOf(8)}), DELTA);
-        assertEquals(0, intercept(new BigDecimal[]{BigDecimal.valueOf(3), BigDecimal.valueOf(4)}, new BigDecimal[]{BigDecimal.valueOf(3), BigDecimal.valueOf(4)}), DELTA);
+                        new BigInteger[]{BigInteger.valueOf(3), BigInteger.valueOf(4), BigInteger.valueOf(8)}));
+        assertEquals(new BigDecimal("0.0"), intercept(new BigDecimal[]{BigDecimal.valueOf(3), BigDecimal.valueOf(4)}, new BigDecimal[]{BigDecimal.valueOf(3), BigDecimal.valueOf(4)}));
     }
 
     @Test
     public void testForecast() {
-        assertNull(forecast(null, null, null));
+        assertNull(forecast((Number) null, null, null));
         assertNull(forecast(0, new Double[2], new Double[2]));
         assertNull(forecast(1.0, new Double[]{1.0, null}, new Double[]{null, null}));
         assertNull(forecast(1.0, new Double[]{1.0, 2.0}, new Double[]{null, null}));
@@ -374,9 +379,9 @@ public class StatisticsTest {
         assertEquals(3.0, forecast(3L, new Long[]{3L, 4L, 8L}, new Long[]{3L, 4L, 8L}), DELTA);
         assertEquals(3.0, forecast(3f, new Float[]{3f, 4f, 8f}, new Float[]{3f, 4f, 8f}), DELTA);
         assertEquals(3.0, forecast((byte) 3, new Byte[]{(byte) 3, (byte) 4, (byte) 8}, new Short[]{(short) 3, (short) 4, (short) 8}), DELTA);
-        assertEquals(7.333333333333333,
+        assertEquals(new BigDecimal("7.333333333333333333333333333333333"),
                 forecast(BigInteger.valueOf(3), new BigInteger[]{BigInteger.valueOf(10), BigInteger.valueOf(4), BigInteger.valueOf(8)},
-                        new BigInteger[]{BigInteger.valueOf(3), BigInteger.valueOf(4), BigInteger.valueOf(8)}), DELTA);
-        assertEquals(3.0, forecast(BigDecimal.valueOf(3), new BigDecimal[]{BigDecimal.valueOf(3), BigDecimal.valueOf(4)}, new BigDecimal[]{BigDecimal.valueOf(3), BigDecimal.valueOf(4)}), DELTA);
+                        new BigInteger[]{BigInteger.valueOf(3), BigInteger.valueOf(4), BigInteger.valueOf(8)}));
+        assertEquals(new BigDecimal("3.0"), forecast(BigDecimal.valueOf(3), new BigDecimal[]{BigDecimal.valueOf(3), BigDecimal.valueOf(4)}, new BigDecimal[]{BigDecimal.valueOf(3), BigDecimal.valueOf(4)}));
     }
 }
