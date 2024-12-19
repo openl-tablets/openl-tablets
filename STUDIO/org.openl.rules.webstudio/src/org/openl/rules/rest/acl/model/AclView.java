58086ab5acf35f61ae7fe2e51bf49ffe05ca3578
Vladyslav Pikus <vpikus@eisgroup.com>
2024-12-19 11:00:04 +0200

    EPBDS-14670 Update OpenAPI schema

---
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/rest/acl/model/AclView.java
@@ -0,0 +1,19 @@
+package org.openl.rules.rest.acl.model;
+
+public final class AclView {
+
+    private AclView() {
+    }
+
+    public interface Repository {
+    }
+
+    public interface Root {
+    }
+
+    public interface Project {
+    }
+
+    public interface Sid {
+    }
+}
