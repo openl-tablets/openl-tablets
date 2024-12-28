b1b35084955414f42f290cdb1ba3d621fb3c189f
Yury Molchan <ymolchan@eisgroup.com>
2024-12-28 20:40:41 +0100

    EPBDS-14715 Replace XlsVM with SimpleRulesVM

--- DEV/org.openl.rules/src/org/openl/xls/OpenLBuilder.java
+++ DEV/org.openl.rules/src/org/openl/xls/OpenLBuilder.java
@@ -1,16 +1,9 @@
-/*
- * Created on Oct 2, 2003
- *
- * Developed by Intelligent ChoicePoint Inc. 2003
- */
-
 package org.openl.xls;
 
-import org.openl.IOpenParser;
 import org.openl.OpenL;
 import org.openl.conf.BaseOpenLBuilder;
 import org.openl.rules.lang.xls.XlsBinder;
-import org.openl.rules.lang.xls.XlsVM;
+import org.openl.rules.vm.SimpleRulesVM;
 
 /**
  * @author snshor
@@ -22,14 +15,11 @@ public class OpenLBuilder extends BaseOpenLBuilder {
 
         OpenL openl = new OpenL();
 
-        openl.setParser(createParser());
+        openl.setParser(new Parser(getUserEnvironmentContext()));
         openl.setBinder(new XlsBinder(new RulesCompileContext(), getUserEnvironmentContext()));
-        openl.setVm(new XlsVM());
+        openl.setVm(new SimpleRulesVM());
 
         return openl;
     }
 
-    protected IOpenParser createParser() {
-        return new Parser(getUserEnvironmentContext());
-    }
 }
