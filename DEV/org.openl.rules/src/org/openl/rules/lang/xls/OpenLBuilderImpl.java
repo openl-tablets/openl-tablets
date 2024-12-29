77deb3da7df4ae8598699860039a762e183c6225
Yury Molchan <ymolchan@eisgroup.com>
2024-12-29 12:06:14 +0100

    EPBDS-14715 Push down AOpenLBuilder into the final implementations

--- DEV/org.openl.rules/src/org/openl/rules/lang/xls/OpenLBuilderImpl.java
+++ DEV/org.openl.rules/src/org/openl/rules/lang/xls/OpenLBuilderImpl.java
@@ -3,31 +3,26 @@ package org.openl.rules.lang.xls;
 import java.util.Collection;
 
 import org.openl.OpenL;
-import org.openl.conf.AOpenLBuilder;
+import org.openl.binding.impl.Binder;
+import org.openl.conf.IOpenLBuilder;
+import org.openl.conf.IOpenLConfiguration;
 import org.openl.conf.IUserContext;
 import org.openl.conf.LibrariesRegistry;
 import org.openl.conf.OpenLConfiguration;
+import org.openl.conf.OpenLConfigurationException;
 import org.openl.conf.OperatorsNamespace;
 import org.openl.conf.TypeCastFactory;
 import org.openl.conf.TypeResolver;
 import org.openl.rules.vm.SimpleRulesVM;
-import org.openl.vm.SimpleVM;
+import org.openl.syntax.impl.Parser;
 
-public class OpenLBuilderImpl extends AOpenLBuilder {
-
-    private String extendsCategory = OpenL.OPENL_J_NAME;
+public class OpenLBuilderImpl implements IOpenLBuilder {
 
     private String category;
 
     private String[] packageImports = new String[]{};
     private Collection<Class<?>> classImports;
-
     private Collection<Class<?>> libraries;
-
-    @Override
-    protected SimpleVM createVM() {
-        return new SimpleRulesVM();
-    }
 
     @Override
     public OpenL build(IUserContext userContext) {
@@ -35,25 +30,36 @@ public class OpenLBuilderImpl extends AOpenLBuilder {
         ClassLoader userEnvironmentContextClassLoader = userContext.getUserClassLoader();
         try {
             Thread.currentThread().setContextClassLoader(userEnvironmentContextClassLoader);
-            OpenL.getInstance(extendsCategory, userContext);
+            OpenL.getInstance(OpenL.OPENL_J_NAME, userContext);
         } finally {
             Thread.currentThread().setContextClassLoader(oldClassLoader);
         }
-        return super.build(userContext);
+        IOpenLConfiguration conf = userContext.getOpenLConfiguration(category);
+        if (conf == null) {
+            OpenLConfiguration oPconf = getOpenLConfiguration();
+            IOpenLConfiguration extendsConfiguration = null;
+            if ((extendsConfiguration = userContext.getOpenLConfiguration(OpenL.OPENL_J_NAME)) == null) {
+                throw new OpenLConfigurationException(
+                        "The extended category " + OpenL.OPENL_J_NAME + " must have been loaded first",
+                        null);
+            }
+
+            oPconf.setParent(extendsConfiguration);
+            oPconf.setClassLoader(userContext.getUserClassLoader());
+            oPconf.validate();
+
+            userContext.registerOpenLConfiguration(category, oPconf);
+            conf = oPconf;
+        }
+
+        OpenL op = new OpenL();
+        op.setParser(new Parser(conf));
+        op.setBinder(new Binder(conf, conf, conf, conf, conf, op));
+        op.setVm(new SimpleRulesVM());
+        return op;
     }
 
-    @Override
-    protected String getCategory() {
-        return category;
-    }
-
-    @Override
-    protected String getExtendsCategory() {
-        return extendsCategory;
-    }
-
-    @Override
-    protected OpenLConfiguration getOpenLConfiguration() {
+    private OpenLConfiguration getOpenLConfiguration() {
 
         var op = new OpenLConfiguration();
 
