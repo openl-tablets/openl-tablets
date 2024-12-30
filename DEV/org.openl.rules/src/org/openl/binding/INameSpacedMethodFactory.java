928778e6d3c487c260579f05de9baafb676a193e
Yury Molchan <ymolchan@eisgroup.com>
2024-12-30 22:31:48 +0100

    EPBDS-14715 Move static method overriding logic to the LibrariesRegistry

--- DEV/org.openl.rules/src/org/openl/binding/INameSpacedMethodFactory.java
+++ DEV/org.openl.rules/src/org/openl/binding/INameSpacedMethodFactory.java
@@ -1,18 +1,11 @@
-/*
- * Created on Jun 11, 2003
- *
- * Developed by Intelligent ChoicePoint Inc. 2003
- */
-
 package org.openl.binding;
 
 import org.openl.binding.exception.AmbiguousMethodException;
 import org.openl.types.IMethodCaller;
 import org.openl.types.IOpenClass;
-import org.openl.types.IOpenMethod;
 
 /**
- * @author snshor
+ * @author Yury Molchan
  */
 public interface INameSpacedMethodFactory {
 
@@ -20,7 +13,5 @@ public interface INameSpacedMethodFactory {
                                   String name,
                                   IOpenClass[] params,
                                   ICastFactory casts) throws AmbiguousMethodException;
-
-    IOpenMethod[] getMethods(String namespace, String name);
 
 }
