9e69d590c408f29fbd7291f7e8e3cd54c6fcb8a1
Yury Molchan <ymolchan@eisgroup.com>
2025-01-09 15:02:36 +0100

    EPBDS-14715 Extract inner SimpleRuntimeEnv class

---
+++ DEV/org.openl.rules/src/org/openl/vm/SimpleRuntimeEnv.java
@@ -0,0 +1,121 @@
+package org.openl.vm;
+
+import java.util.ArrayDeque;
+
+import org.openl.IOpenRunner;
+import org.openl.runtime.DefaultRuntimeContext;
+import org.openl.runtime.IRuntimeContext;
+
+public class SimpleRuntimeEnv implements IRuntimeEnv {
+
+    private static final Object[] NO_PARAMS = {};
+    private static final Object NULL_THIS = new Object();
+
+    private final IOpenRunner runner;
+    protected final ArrayDeque<Object> thisStack = new ArrayDeque<>();
+    protected final ArrayDeque<Object[]> frameStack = new ArrayDeque<>();
+    protected ArrayDeque<IRuntimeContext> contextStack;
+
+    public SimpleRuntimeEnv() {
+        this(SimpleRunner.SIMPLE_RUNNER, 0, NO_PARAMS);
+    }
+
+    SimpleRuntimeEnv(IOpenRunner runner, int frameSize, Object[] params) {
+        Object[] aLocalFrame = new Object[frameSize];
+        this.runner = runner;
+        contextStack = new ArrayDeque<>();
+        System.arraycopy(params, 0, aLocalFrame, 0, params.length);
+        pushLocalFrame(aLocalFrame);
+        pushContext(buildDefaultRuntimeContext());
+    }
+
+    protected IRuntimeContext buildDefaultRuntimeContext() {
+        return new DefaultRuntimeContext();
+    }
+
+    public SimpleRuntimeEnv(SimpleRuntimeEnv env) {
+        this.runner = SimpleRunner.SIMPLE_RUNNER;
+        contextStack = new ArrayDeque<>(env.contextStack);
+        pushThis(env.getThis());
+        pushLocalFrame(env.getLocalFrame());
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see org.openl.vm.IRuntimeEnv#getLocalFrame()
+     */
+    @Override
+    public Object[] getLocalFrame() {
+        return frameStack.peek();
+    }
+
+    @Override
+    public IOpenRunner getRunner() {
+        return runner;
+    }
+
+    @Override
+    public Object getThis() {
+        return thisStack.peek();
+    }
+
+    @Override
+    public Object[] popLocalFrame() {
+        return frameStack.pop();
+    }
+
+    @Override
+    public Object popThis() {
+        return thisStack.pop();
+    }
+
+    @Override
+    public void pushLocalFrame(Object[] frame) {
+        frameStack.push(frame);
+    }
+
+    @Override
+    public void pushThis(Object thisObject) {
+        thisStack.push(thisObject == null ? NULL_THIS : thisObject); // To prevent NPE
+    }
+
+    @Override
+    public IRuntimeContext getContext() {
+        return contextStack.peek();
+    }
+
+    @Override
+    public void setContext(IRuntimeContext context) {
+        if (context == null) {
+            context = buildDefaultRuntimeContext();
+        }
+        contextStack.clear();
+        pushContext(context);
+    }
+
+    @Override
+    public IRuntimeContext popContext() {
+        return contextStack.pop();
+    }
+
+    @Override
+    public void pushContext(IRuntimeContext context) {
+        contextStack.push(context);
+    }
+
+    @Override
+    public boolean isContextManagingSupported() {
+        return true;
+    }
+
+    @Override
+    public IRuntimeEnv clone() {
+        return copy();
+    }
+
+    @Override
+    public SimpleRuntimeEnv copy() {
+        return new SimpleRuntimeEnv(this);
+    }
+}
