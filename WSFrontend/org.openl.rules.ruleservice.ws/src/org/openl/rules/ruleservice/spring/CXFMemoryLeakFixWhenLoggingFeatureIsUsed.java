f8e51ee790700f3fffd091548e5e33b463edd89d
Yury Molchan <ymolchan@eisgroup.com>
2025-01-11 09:06:28 +0100

    EPBDS-14726 Customize CXF SpringBus to disable `DelayedCachedOutputStreamCleaner` due to memory leak issue
    See https://issues.apache.org/jira/browse/CXF-9110

---
+++ WSFrontend/org.openl.rules.ruleservice.ws/src/org/openl/rules/ruleservice/spring/CXFMemoryLeakFixWhenLoggingFeatureIsUsed.java
@@ -0,0 +1,23 @@
+package org.openl.rules.ruleservice.spring;
+
+import org.apache.cxf.buslifecycle.BusCreationListener;
+import org.apache.cxf.io.CachedConstants;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Workaround for the case when a LoggingFeature is used.
+ * See implementations in https://issues.apache.org/jira/browse/CXF-7396
+ * <p>
+ * FIXME: Delete this configuration when a memory leak will be fixed in CXF-9110.
+ *
+ * @author Yury Molchan
+ */
+@Configuration
+public class CXFMemoryLeakFixWhenLoggingFeatureIsUsed {
+
+    @Bean
+    BusCreationListener cxfBusConfigurer() {
+        return cxf -> cxf.setProperty(CachedConstants.CLEANER_DELAY_BUS_PROP, 0);
+    }
+}
