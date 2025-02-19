815acf01277bc42d9c7de1062375d6573de41712
Aliaksandr Samuseu <asamuseu@eisgroup.com>
2025-02-19 14:13:55 +0100

    EPBDS-14682 use system setting

---
+++ NEW_ADMIN_UI/webstudioadmin/src/types/system.d.ts
@@ -0,0 +1,13 @@
+import {SystemUserMode} from "../constants/system";
+
+export interface SystemSettings {
+    entrypoint: {
+        logoutUrl: string
+    }
+    supportedFeatures: {
+        emailVerification: boolean
+        groupsManagement: boolean
+        userManagement: boolean
+    },
+    "userMode": SystemUserMode
+}
