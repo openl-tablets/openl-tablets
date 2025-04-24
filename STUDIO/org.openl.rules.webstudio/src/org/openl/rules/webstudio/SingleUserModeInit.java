bea2154b2146d2afb2c42722ba450250f76b74be
Vladyslav Pikus <vpikus@eisgroup.com>
2025-04-24 16:33:39 +0300

    EPBDS-14071 Add ability to customize user profile for default user in single mode

--- STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/SingleUserModeInit.java
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/SingleUserModeInit.java
@@ -1,6 +1,7 @@
 package org.openl.rules.webstudio;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 
 import org.openl.rules.webstudio.service.UserManagementService;
 
@@ -9,10 +10,42 @@ import org.openl.rules.webstudio.service.UserManagementService;
  */
 public class SingleUserModeInit {
 
+    private final String username;
+    private final String firstName;
+    private final String lastName;
+    private final String email;
+    private final String displayName;
+
+    private final UserManagementService userManagementService;
+
     @Autowired
-    private UserManagementService userManagementService;
+    public SingleUserModeInit(UserManagementService userManagementService,
+                              @Value("${security.single.username}") String username,
+                              @Value("${security.single.first-name}") String firstName,
+                              @Value("${security.single.last-name}") String lastName,
+                              @Value("${security.single.email}") String email,
+                              @Value("${security.single.display-name}") String displayName) {
+        this.userManagementService = userManagementService;
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.displayName = displayName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
 
     public void init() {
-        userManagementService.syncUserData("DEFAULT", "De", "Fault", "default@example.com", "DEFAULT");
+        userManagementService.syncUserData(username, firstName, lastName, email, displayName);
     }
 }
