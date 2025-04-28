73b99778b9807fd370f5ad480bfb9a8a40e01ac3
Vladyslav Pikus <vpikus@eisgroup.com>
2025-04-28 17:46:30 +0300

    EPBDS-14936 Added ability to define external react application for development mode. Initialize contextPath as JS environment variable

--- STUDIO/org.openl.rules.webstudio/src/org/openl/rules/rest/ui/ReactUIController.java
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/rest/ui/ReactUIController.java
@@ -1,5 +1,8 @@
 package org.openl.rules.rest.ui;
 
+import java.util.function.Supplier;
+
+import org.springframework.beans.factory.annotation.Lookup;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -8,9 +11,16 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping({"/administration", "/claim"})
 public class ReactUIController {
 
+    @Lookup("reactUiRoot")
+    public Supplier<String> getReactUiRoot() {
+        throw new IllegalStateException();
+    }
+
     @RequestMapping("/**")
     public ModelAndView handleRequests() {
-        return new ModelAndView("react-ui");
+        var view = new ModelAndView("react-ui");
+        view.addObject("reactUiRoot", getReactUiRoot().get());
+        return view;
     }
 
 }
