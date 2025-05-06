7311fb4defc455a3d45a55abe79e81eb8b215314
Aliaksandr Samuseu <asamuseu@eisgroup.com>
2025-05-06 15:45:43 +0200

    EPBDS-14962 settings page

--- STUDIO/org.openl.rules.webstudio/ui/src/components/form/index.ts
+++ STUDIO/org.openl.rules.webstudio/ui/src/components/form/index.ts
@@ -1,11 +1,13 @@
 import Checkbox from './Checkbox'
 import CheckboxGroup from './CheckboxGroup'
 import Input from './Input'
+import InputNumber from './InputNumber'
 import Select from './Select'
 
 export {
     Checkbox,
     CheckboxGroup,
     Input,
+    InputNumber,
     Select,
 }
