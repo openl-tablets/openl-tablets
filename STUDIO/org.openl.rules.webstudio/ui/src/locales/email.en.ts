c0c205e37617b79539ed2dbd1d950758ae422132
Aliaksandr Samuseu <asamuseu@eisgroup.com>
2025-05-08 11:43:03 +0200

    EPBDS-14962 email page

--- STUDIO/org.openl.rules.webstudio/ui/src/locales/email.en.ts
+++ STUDIO/org.openl.rules.webstudio/ui/src/locales/email.en.ts
@@ -6,5 +6,8 @@ i18next.addResourceBundle('en', 'email', {
     url: 'URL',
     username: 'Username',
     password: 'Password',
-    apply: 'Apply',
+    email_server_configuration_deleted: 'Email server configuration deleted',
+    email_server_configuration_saved: 'Email server configuration saved',
+    confirm_change_email_settings: 'Are you sure you want to change email settings',
+    confirm_change_email_settings_message: 'All users working with OpenL Studio now will lose all unsaved changes. Are you sure you want to change email settings now?',
 })
