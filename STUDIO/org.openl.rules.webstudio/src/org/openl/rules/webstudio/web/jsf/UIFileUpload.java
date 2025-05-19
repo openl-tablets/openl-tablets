986d66df23c63ac4a4a5da83ae856598cf53b35a
Yury Molchan <ymolchan@eisgroup.com>
2025-05-19 23:58:08 +0200

    EPBDS-14965 Upgrade to the latest version of RichFaces
    EPBDS-9903 Implement workaround to fix the issue with `fileUpload` tag introduced in
    https://github.com/albfernandez/richfaces/pull/31

--- STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/web/jsf/UIFileUpload.java
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/web/jsf/UIFileUpload.java
@@ -4,6 +4,8 @@ import java.util.concurrent.atomic.AtomicInteger;
 import javax.faces.component.FacesComponent;
 import javax.faces.event.AbortProcessingException;
 import javax.faces.event.ComponentSystemEvent;
+import javax.faces.event.ListenerFor;
+import javax.faces.event.PostAddToViewEvent;
 
 /**
  * Workaround: fixed issue with file upload component on EDITOR page. For some reason,
@@ -11,6 +13,7 @@ import javax.faces.event.ComponentSystemEvent;
  * if {@link javax.faces.event.PreRenderComponentEvent} is fired. But in our case, it is not fired.
  */
 @FacesComponent(value = "openl.UIFileUpload")
+@ListenerFor(systemEventClass = PostAddToViewEvent.class)
 public class UIFileUpload extends org.richfaces.component.UIFileUpload {
 
     private static final String QUEUED_FILE_UPLOAD_EVENTS_ATTR = "queuedFileUploadEvents";
@@ -18,8 +21,8 @@ public class UIFileUpload extends org.richfaces.component.UIFileUpload {
     @Override
     public void processEvent(ComponentSystemEvent event) throws AbortProcessingException {
         super.processEvent(event);
-        if (event.getSource() == this && getAttributes().get(QUEUED_FILE_UPLOAD_EVENTS_ATTR) == null) {
-            // if QUEUED_FILE_UPLOAD_EVENTS_ATTR is not initialized, initialize it!
+        if (event.getSource() == this && event instanceof PostAddToViewEvent) {
+            // Restore behavior as it was in 4.6.8.ayg
             getAttributes().put(QUEUED_FILE_UPLOAD_EVENTS_ATTR, new AtomicInteger());
         }
     }
