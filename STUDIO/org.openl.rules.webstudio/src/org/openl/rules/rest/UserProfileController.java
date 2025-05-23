a7128d1c85bad4a6732b8f98a8d6a53e35afd3f9
Vladyslav Pikus <vpikus@eisgroup.com>
2025-05-23 13:59:49 +0300

    EPBDS-14986 Improve configuration: disable REST API for user management if `user.mode` is single. As it does not support this functionality

--- STUDIO/org.openl.rules.webstudio/src/org/openl/rules/rest/UserProfileController.java
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/rest/UserProfileController.java
@@ -9,174 +9,73 @@ import static org.openl.rules.ui.WebStudio.TEST_RESULT_COMPLEX_SHOW;
 import static org.openl.rules.ui.WebStudio.TEST_TESTS_PERPAGE;
 import static org.openl.rules.ui.WebStudio.TRACE_REALNUMBERS_SHOW;
 
-import java.util.Comparator;
-import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.PropertyResolver;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.security.acls.domain.PrincipalSid;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import org.openl.rules.lang.xls.IXlsTableNames;
-import org.openl.rules.rest.exception.NotFoundException;
 import org.openl.rules.rest.model.ChangePasswordModel;
-import org.openl.rules.rest.model.GroupModel;
-import org.openl.rules.rest.model.GroupType;
-import org.openl.rules.rest.model.UserCreateModel;
-import org.openl.rules.rest.model.UserEditModel;
 import org.openl.rules.rest.model.UserInfoModel;
-import org.openl.rules.rest.model.UserModel;
 import org.openl.rules.rest.model.UserProfileEditModel;
 import org.openl.rules.rest.model.UserProfileModel;
-import org.openl.rules.rest.validation.BeanValidationProvider;
 import org.openl.rules.security.AccessManager;
-import org.openl.rules.security.AdminPrivilege;
-import org.openl.rules.security.Group;
-import org.openl.rules.security.OwnerOrAdminPrivilege;
 import org.openl.rules.security.Privileges;
-import org.openl.rules.security.SimpleGroup;
 import org.openl.rules.security.User;
 import org.openl.rules.ui.WebStudio;
 import org.openl.rules.ui.tree.view.Profile;
 import org.openl.rules.webstudio.mail.MailSender;
 import org.openl.rules.webstudio.security.CurrentUserInfo;
-import org.openl.rules.webstudio.service.AdminUsers;
-import org.openl.rules.webstudio.service.ExternalGroupService;
 import org.openl.rules.webstudio.service.UserManagementService;
 import org.openl.rules.webstudio.service.UserSettingManagementService;
 import org.openl.rules.webstudio.web.util.WebStudioUtils;
-import org.openl.security.acl.JdbcMutableAclService;
-import org.openl.util.StreamUtils;
 import org.openl.util.StringUtils;
 
 @RestController
 @RequestMapping(value = "/users", produces = MediaType.APPLICATION_JSON_VALUE)
 @Tag(name = "Users")
-public class UsersController {
+@Validated
+public class UserProfileController {
 
     private final UserManagementService userManagementService;
-    private final Boolean canCreateInternalUsers;
-    private final AdminUsers adminUsersInitializer;
-    private final CurrentUserInfo currentUserInfo;
-    private final BeanValidationProvider validationProvider;
     private final UserSettingManagementService userSettingsManager;
-    private final PropertyResolver environment;
-    private final PasswordEncoder passwordEncoder;
-    private final ExternalGroupService extGroupService;
+    private final CurrentUserInfo currentUserInfo;
     private final MailSender mailSender;
-    private final JdbcMutableAclService aclService;
+    private final PropertyResolver environment;
+    private final Boolean canCreateInternalUsers;
 
-    @Autowired
-    public UsersController(UserManagementService userManagementService,
-                           Boolean canCreateInternalUsers,
-                           AdminUsers adminUsersInitializer,
-                           CurrentUserInfo currentUserInfo,
-                           PasswordEncoder passwordEncoder,
-                           PropertyResolver environment,
-                           BeanValidationProvider validationService,
-                           UserSettingManagementService userSettingsManager,
-                           ExternalGroupService extGroupService,
-                           MailSender mailSender,
-                           @Autowired(required = false) JdbcMutableAclService aclService) {
+    public UserProfileController(UserManagementService userManagementService,
+                                 UserSettingManagementService userSettingsManager,
+                                 CurrentUserInfo currentUserInfo,
+                                 MailSender mailSender,
+                                 PropertyResolver environment,
+                                 Boolean canCreateInternalUsers) {
         this.userManagementService = userManagementService;
-        this.canCreateInternalUsers = canCreateInternalUsers;
-        this.adminUsersInitializer = adminUsersInitializer;
-        this.currentUserInfo = currentUserInfo;
-        this.passwordEncoder = passwordEncoder;
         this.userSettingsManager = userSettingsManager;
-        this.environment = environment;
-        this.validationProvider = validationService;
-        this.extGroupService = extGroupService;
+        this.currentUserInfo = currentUserInfo;
         this.mailSender = mailSender;
-        this.aclService = aclService;
-    }
-
-    @Operation(description = "users.get-users.desc", summary = "users.get-users.summary")
-    @GetMapping
-    public List<UserModel> getAllUsers() {
-        return userManagementService.getAllUsers().stream().map(this::mapUser).collect(Collectors.toList());
-    }
-
-    @Operation(description = "users.get-user.desc", summary = "users.get-user.summary")
-    @GetMapping("/{username}")
-    @OwnerOrAdminPrivilege
-    public UserModel getUser(
-            @Parameter(description = "users.field.username") @PathVariable("username") String username) {
-        checkUserExists(username);
-        return Optional.ofNullable(userManagementService.getUser(username)).map(this::mapUser).orElse(null);
-    }
-
-    @Operation(description = "users.add-user.desc", summary = "users.add-user.summary")
-    @PutMapping
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    @AdminPrivilege
-    public void addUser(HttpServletRequest request, @RequestBody UserCreateModel userModel) {
-        validationProvider.validate(userModel);
-        userManagementService.addUser(userModel.getUsername(),
-                userModel.getFirstName(),
-                userModel.getLastName(),
-                canCreateInternalUsers ? userModel.getInternalPassword().getPassword() : null,
-                userModel.getEmail(),
-                userModel.getDisplayName());
-        userManagementService.updateAuthorities(userModel.getUsername(), userModel.getGroups());
-        if (StringUtils.isNotBlank(userModel.getEmail())) {
-            mailSender.sendVerificationMail(userManagementService.getUser(userModel.getUsername()), request);
-        }
-    }
-
-    @Operation(description = "users.edit-user.desc", summary = "users.edit-user.summary")
-    @PutMapping("/{username}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    @OwnerOrAdminPrivilege
-    public void editUser(HttpServletRequest request,
-                         @RequestBody UserEditModel userModel,
-                         @Parameter(description = "users.field.username") @PathVariable("username") String username) {
-        checkUserExists(username);
-        validationProvider.validate(userModel);
-        User dbUser = userManagementService.getUser(username);
-        boolean emailChanged = !Objects.equals(dbUser.getEmail(), userModel.getEmail()) && !dbUser.getExternalFlags()
-                .isEmailExternal();
-        userManagementService.updateUserData(username,
-                userModel.getFirstName(),
-                userModel.getLastName(),
-                userModel.getPassword(),
-                userModel.getEmail(),
-                userModel.getDisplayName(),
-                !emailChanged && dbUser.getExternalFlags().isEmailVerified());
-        boolean leaveAdminGroups = adminUsersInitializer.isSuperuser(username) || Objects
-                .equals(currentUserInfo.getUserName(), username);
-        userManagementService.updateAuthorities(username, userModel.getGroups(), leaveAdminGroups);
-
-        if (StringUtils.isNotBlank(userModel.getEmail()) && emailChanged) {
-            mailSender.sendVerificationMail(userManagementService.getUser(username), request);
-        }
+        this.environment = environment;
+        this.canCreateInternalUsers = canCreateInternalUsers;
     }
 
     @Operation(description = "users.edit-user-info.desc", summary = "users.edit-user-info.summary")
     @PutMapping("/info")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void editUserInfo(HttpServletRequest request, @RequestBody UserInfoModel userModel) {
-        validationProvider.validate(userModel);
+    public void editUserInfo(HttpServletRequest request, @Valid @RequestBody UserInfoModel userModel) {
         User dbUser = userManagementService.getUser(currentUserInfo.getUserName());
         boolean emailChanged = !Objects.equals(dbUser.getEmail(), userModel.getEmail()) && !dbUser.getExternalFlags()
                 .isEmailExternal();
@@ -196,8 +95,7 @@ public class UsersController {
     @Operation(description = "users.edit-user-profile.desc", summary = "users.edit-user-profile.summary")
     @PutMapping("/profile")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void editUserProfile(HttpServletRequest request, @RequestBody UserProfileEditModel userModel) {
-        validationProvider.validate(userModel);
+    public void editUserProfile(HttpServletRequest request, @Valid @RequestBody UserProfileEditModel userModel) {
         User dbUser = userManagementService.getUser(currentUserInfo.getUserName());
         boolean emailChanged = !Objects.equals(dbUser.getEmail(), userModel.getEmail()) && !dbUser.getExternalFlags()
                 .isEmailExternal();
@@ -282,17 +180,6 @@ public class UsersController {
 
     }
 
-    @Operation(description = "users.delete-user.desc", summary = "users.delete-user.summary")
-    @DeleteMapping("/{username}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteUser(@Parameter(description = "users.field.username") @PathVariable("username") String username) {
-        checkUserExists(username);
-        userManagementService.deleteUser(username);
-        if (aclService != null) {
-            aclService.deleteSid(new PrincipalSid(username));
-        }
-    }
-
     @Deprecated
     @Operation(description = "users.options.desc", summary = "users.options.summary")
     @GetMapping("/options")
@@ -304,61 +191,7 @@ public class UsersController {
                 .build();
     }
 
-    @Operation(description = "users.get-user-external-groups.desc", summary = "users.get-user-external-groups.summary")
-    @GetMapping("/{username}/groups/external")
-    @AdminPrivilege
-    public Set<String> getUserExternalGroups(
-            @Parameter(description = "users.field.username") @PathVariable("username") String username,
-            @Parameter(description = "users.get-user-external-groups.param.matched.username") @RequestParam(value = "matched", required = false) Boolean matched) {
-        checkUserExists(username);
-        List<Group> extGroups;
-        if (matched == null) {
-            extGroups = extGroupService.findAllForUser(username);
-        } else if (matched) {
-            extGroups = extGroupService.findMatchedForUser(username);
-        } else {
-            extGroups = extGroupService.findNotMatchedForUser(username);
-        }
-        return extGroups.stream().map(Group::getName).collect(StreamUtils.toTreeSet(String.CASE_INSENSITIVE_ORDER));
-    }
-
-    private UserModel mapUser(User user) {
-        List<Group> extGroups = extGroupService.findMatchedForUser(user.getUsername());
-        Stream<GroupModel> matchedExtGroupsStream = extGroups.stream()
-                .map(simpleGroup -> new GroupModel().setName(simpleGroup.getName())
-                        .setType(simpleGroup.hasPrivilege(Privileges.ADMIN.name()) ? GroupType.ADMIN : GroupType.EXTERNAL));
-        Stream<GroupModel> internalGroupStream = user.getAuthorities()
-                .stream()
-                .map(SimpleGroup.class::cast)
-                // resolve collisions when the same group external and internal
-                .filter(g -> extGroups.stream().noneMatch(ext -> Objects.equals(ext.getName(), g.getName())))
-                .map(simpleGroup -> new GroupModel().setName(simpleGroup.getName())
-                        .setType(simpleGroup.hasPrivilege(Privileges.ADMIN.name()) ? GroupType.ADMIN : GroupType.DEFAULT));
-
-        long cntNotMatchedExtGroups = extGroupService.countNotMatchedForUser(user.getUsername());
-        return new UserModel().setFirstName(user.getFirstName())
-                .setLastName(user.getLastName())
-                .setEmail(user.getEmail())
-                .setUserGroups(Stream.concat(matchedExtGroupsStream, internalGroupStream)
-                        .collect(StreamUtils.toTreeSet(Comparator.comparing(GroupModel::getType)
-                                .thenComparing(GroupModel::getName, String.CASE_INSENSITIVE_ORDER))))
-                .setUsername(user.getUsername())
-                .setCurrentUser(currentUserInfo.getUserName().equals(user.getUsername()))
-                .setSuperUser(adminUsersInitializer.isSuperuser(user.getUsername()))
-                .setUnsafePassword(
-                        user.getPassword() != null && passwordEncoder.matches(user.getUsername(), user.getPassword()))
-                .setNotMatchedExternalGroupsCount(cntNotMatchedExtGroups)
-                .setDisplayName(user.getDisplayName())
-                .setOnline(userManagementService.isUserOnline(user.getUsername()))
-                .setExternalFlags(user.getExternalFlags());
-    }
-
-    private void checkUserExists(String username) {
-        if (!userManagementService.existsByName(username)) {
-            throw new NotFoundException("users.message", username);
-        }
-    }
-
+    @Deprecated
     public static class UserOptions {
 
         @Parameter(description = "Can create internal users")
@@ -411,5 +244,6 @@ public class UsersController {
         }
 
     }
+
 
 }
