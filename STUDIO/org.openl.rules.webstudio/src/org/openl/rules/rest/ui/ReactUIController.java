6b29593ec773909f1f871d1b1b517fde2f6a8e90
Aliaksandr Samuseu <asamuseu@eisgroup.com>
2025-06-10 15:32:48 +0200

    EPBDS-15024 add route for help page

--- STUDIO/org.openl.rules.webstudio/src/org/openl/rules/rest/ui/ReactUIController.java
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/rest/ui/ReactUIController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping({"/administration", "/claim"})
+@RequestMapping({"/administration", "/claim", "/help"})
 public class ReactUIController {
 
     @Lookup("reactUiRoot")
