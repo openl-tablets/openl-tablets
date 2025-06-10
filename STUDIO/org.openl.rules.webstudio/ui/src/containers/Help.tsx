83dc164a0b98814d2b9755ad31e279f389432bd0
Aliaksandr Samuseu <asamuseu@eisgroup.com>
2025-06-10 14:35:00 +0200

    EPBDS-15024 fix links list location

--- STUDIO/org.openl.rules.webstudio/ui/src/containers/Help.tsx
+++ STUDIO/org.openl.rules.webstudio/ui/src/containers/Help.tsx
@@ -5,11 +5,11 @@ import { SystemContext } from '../contexts'
 import { Link } from 'react-router-dom'
 
 const documentationItems = [
-    'OpenL Tablets - Installation Guide',
-    'OpenL Tablets - Reference Guide',
-    'OpenL Tablets - Developer Guide',
-    'OpenL Tablets - WebStudio User Guide',
-    'OpenL Tablets - Rule Services Usage and Customization Guide'
+    'Installation Guide',
+    'Reference Guide',
+    'Developer Guide',
+    'WebStudio User Guide',
+    'Rule Services Usage and Customization Guide'
 ]
 
 export const Help: React.FC = () => {
@@ -19,7 +19,7 @@ export const Help: React.FC = () => {
     const openlVersion = openlInfo['openl.version'] || 'unknown'
     const openlUrl = openlInfo['openl.site'] || 'https://openl-tablets.org'
 
-    const baseLink = `${openlUrl}/files/openl-tablets/${openlVersion}/`
+    const baseLink = `${openlUrl}/files/openl-tablets/${openlVersion}/OpenL Tablets - `
 
     return (
         <Row justify="center">
@@ -41,26 +41,26 @@ export const Help: React.FC = () => {
                     <List.Item>
                         <Link rel="noopener noreferrer" target="_blank" to={`${openlUrl}/news`}>OpenL Tablets News</Link>
                     </List.Item>
-                    <List.Item>
-                        <Link rel="noopener noreferrer" target="_blank" to="https://github.com/openl-tablets/openl-tablets/discussions">OpenL Tablets Discussions</Link>
-                    </List.Item>
                 </List>
+            </Col>
+            <Col span={1} />
+            <Col span={6} style={{ marginTop: 16 }}>
                 <Divider orientation="left">{t('common:help.additional_information')}</Divider>
                 <List>
                     <List.Item>
                         <Link rel="noopener noreferrer" target="_blank" to="/web/config/application.properties">application.properties example</Link>
                     </List.Item>
                     <List.Item>
-                        <Link rel="noopener noreferrer" target="_blank" to="/rest/api-docs">Internal REST API documentation based on the OpenAPI schema</Link>
+                        <Link rel="noopener noreferrer" target="_blank" to="/rest/api-docs">Internal REST API Documentation</Link>
                     </List.Item>
                 </List>
-            </Col>
-            <Col span={1} />
-            <Col span={6} style={{ marginTop: 16 }}>
                 <Divider orientation="left">{t('common:help.openl_tablets_issues')}</Divider>
                 <List>
                     <List.Item>
-                        <Link rel="noopener noreferrer" target="_blank" to="https://github.com/openl-tablets/openl-tablets/issues/">Report a problem</Link>
+                        <Link rel="noopener noreferrer" target="_blank" to="https://github.com/openl-tablets/openl-tablets/discussions">Ask a Question</Link>
+                    </List.Item>
+                    <List.Item>
+                        <Link rel="noopener noreferrer" target="_blank" to="https://github.com/openl-tablets/openl-tablets/issues/">Report a Problem</Link>
                     </List.Item>
                     <List.Item>
                         {openlInfo && (
