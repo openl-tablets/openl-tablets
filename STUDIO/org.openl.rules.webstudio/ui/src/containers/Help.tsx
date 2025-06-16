6ce8b0f4e9cb456331f0c2c672136ce68c940714
Aliaksandr Samuseu <asamuseu@eisgroup.com>
2025-06-16 16:34:18 +0200

    EPBDS-15024 update help page view

--- STUDIO/org.openl.rules.webstudio/ui/src/containers/Help.tsx
+++ STUDIO/org.openl.rules.webstudio/ui/src/containers/Help.tsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import { Col, Divider, List, Row, Typography } from 'antd'
+import { Card, Col, Divider, List, Row, Typography } from 'antd'
 import { useTranslation } from 'react-i18next'
 import { SystemContext } from '../contexts'
 import { Link } from 'react-router-dom'
@@ -22,54 +22,49 @@ export const Help: React.FC = () => {
     const baseLink = `${openlUrl}/files/openl-tablets/${openlVersion}/OpenL Tablets - `
 
     return (
-        <Row justify="center">
-            <Col style={{ marginTop: 16 }} span={6}>
-                <Divider orientation="left">{t('common:help.openl_tablets_documentation')}</Divider>
-                <List
-                    dataSource={documentationItems}
-                    renderItem={(item) => (
-                        <List.Item>
-                            <Link rel="noopener noreferrer" target="_blank" to={`${baseLink}${item}.pdf`}>{item}</Link>
-                        </List.Item>
-                    )}
-                />
-                <Divider orientation="left">{t('common:help.openl_tablets_on_the_internet')}</Divider>
-                <List>
-                    <List.Item>
-                        <Link rel="noopener noreferrer" target="_blank" to={openlUrl}>Official Website</Link>
-                    </List.Item>
-                    <List.Item>
-                        <Link rel="noopener noreferrer" target="_blank" to={`${openlUrl}/news`}>OpenL Tablets News</Link>
-                    </List.Item>
-                </List>
+        <Row gutter={[24, 24]} style={{ padding: '24px 50px' }}>
+            <Col xs={24} sm={12} md={8}>
+                <Card title={t('common:help.openl_tablets_documentation')} bordered>
+                    <List
+                        dataSource={documentationItems}
+                        renderItem={(item) => (
+                            <>
+                                <Link rel="noopener noreferrer" target="_blank" to={`${baseLink}${item}.pdf`}>{item}</Link>
+                                <br />
+                            </>
+                        )}
+                    />
+                </Card>
             </Col>
-            <Col span={1} />
-            <Col span={6} style={{ marginTop: 16 }}>
-                <Divider orientation="left">{t('common:help.additional_information')}</Divider>
-                <List>
-                    <List.Item>
-                        <Link rel="noopener noreferrer" target="_blank" to="/web/config/application.properties">application.properties example</Link>
-                    </List.Item>
-                    <List.Item>
-                        <Link rel="noopener noreferrer" target="_blank" to="/rest/api-docs">Internal REST API Documentation</Link>
-                    </List.Item>
-                </List>
-                <Divider orientation="left">{t('common:help.openl_tablets_issues')}</Divider>
-                <List>
-                    <List.Item>
-                        <Link rel="noopener noreferrer" target="_blank" to="https://github.com/openl-tablets/openl-tablets/discussions">Ask a Question</Link>
-                    </List.Item>
-                    <List.Item>
-                        <Link rel="noopener noreferrer" target="_blank" to="https://github.com/openl-tablets/openl-tablets/issues/">Report a Problem</Link>
-                    </List.Item>
-                    <List.Item>
-                        {openlInfo && (
-                            <Typography.Text type="secondary">
+            <Col xs={24} sm={12} md={8}>
+                <Card title={t('common:help.additional_information')} bordered>
+                    <Link rel="noopener noreferrer" target="_blank" to="/web/config/application.properties">application.properties example</Link>
+                    <br />
+                    <Link rel="noopener noreferrer" target="_blank" to="/rest/api-docs">Internal REST API Documentation</Link>
+                </Card>
+                <Divider />
+                <Card title={t('common:help.openl_tablets_issues')} bordered>
+                    <Link rel="noopener noreferrer" target="_blank" to="https://github.com/openl-tablets/openl-tablets/discussions">Ask a Question</Link>
+                    <br />
+                    <Link rel="noopener noreferrer" target="_blank" to="https://github.com/openl-tablets/openl-tablets/issues/">Report a Problem</Link>
+                    <br />
+                    <br />
+                    {openlInfo && (
+                        <Row justify="end">
+                            <Typography.Text type="secondary" style={{ fontSize: '0.8em' }}>
                                 {t('common:user_menu.version', { version: openlInfo['openl.version'] })}
                             </Typography.Text>
-                        )}
-                    </List.Item>
-                </List>
+                        </Row>
+                    )}
+
+                </Card>
+            </Col>
+            <Col xs={24} sm={12} md={8}>
+                <Card title={t('common:help.openl_tablets_on_the_internet')} bordered>
+                    <Link rel="noopener noreferrer" target="_blank" to={openlUrl}>Official Website</Link>
+                    <br />
+                    <Link rel="noopener noreferrer" target="_blank" to={`${openlUrl}/news`}>OpenL Tablets News</Link>
+                </Card>
             </Col>
         </Row>
     )
