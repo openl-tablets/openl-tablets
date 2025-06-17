e10a16e547e39685fc454aabd86ab4062a3797aa
Aliaksandr Samuseu <asamuseu@eisgroup.com>
2025-06-17 15:40:18 +0200

    EPBDS-15025 add scripts loading hook to header layout

--- STUDIO/org.openl.rules.webstudio/ui/src/layouts/HeaderLayout.tsx
+++ STUDIO/org.openl.rules.webstudio/ui/src/layouts/HeaderLayout.tsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Header } from '../containers/Header'
+import { SystemContext } from '../contexts'
+import { useScript } from '../hooks'
 
 export const HeaderLayout = () => {
+    const { systemSettings } = useContext(SystemContext)
+    useScript(systemSettings?.scripts)
+
     return (
         <Header />
     )
