5436705edff7607df461ccded16ad5c791c3f332
Vladyslav Pikus <vpikus@eisgroup.com>
2025-06-27 15:23:46 +0300

    EPBDS-14621 Fix method graph for multicall method

--- DEV/org.openl.rules/src/org/openl/rules/binding/RulesBindingDependencies.java
+++ DEV/org.openl.rules/src/org/openl/rules/binding/RulesBindingDependencies.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.openl.binding.BindingDependencies;
 import org.openl.binding.IBoundNode;
+import org.openl.binding.impl.method.MultiCallOpenMethod;
 import org.openl.rules.types.OpenMethodDispatcher;
 import org.openl.types.IOpenMethod;
 import org.openl.types.impl.ExecutableMethod;
@@ -11,13 +12,17 @@ import org.openl.types.impl.ExecutableMethod;
 public class RulesBindingDependencies extends BindingDependencies {
 
     @Override
-    public void addMethodDependency(IOpenMethod method, IBoundNode node) {
-        getMethodsMap().put(method, node);
+    public void addMethodDependency(IOpenMethod source, IBoundNode node) {
+        getMethodsMap().put(source, node);
+        var method = source;
+        if (source instanceof MultiCallOpenMethod multiCall) {
+            method = multiCall.getSourceMethod();
+        }
         // check if method is instance of Openl executable rules method.
-        if (method instanceof ExecutableMethod) {
-            getRulesMethodsMap().put((ExecutableMethod) method, node);
-        } else if (method instanceof OpenMethodDispatcher) {
-            List<IOpenMethod> overlappedMethods = ((OpenMethodDispatcher) method).getCandidates();
+        if (method instanceof ExecutableMethod executable) {
+            getRulesMethodsMap().put(executable, node);
+        } else if (method instanceof OpenMethodDispatcher dispatcher) {
+            List<IOpenMethod> overlappedMethods = dispatcher.getCandidates();
             for (IOpenMethod overlappedMethod : overlappedMethods) {
                 addMethodDependency(overlappedMethod, node);
             }
--- STUDIO/org.openl.rules.webstudio/src/org/openl/rules/rest/dependency/RulesDependenciesController.java
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/rest/dependency/RulesDependenciesController.java
@@ -3,36 +3,66 @@ package org.openl.rules.rest.dependency;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import javax.servlet.http.HttpSession;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.jgrapht.traverse.TopologicalOrderIterator;
+import org.springframework.beans.factory.annotation.Lookup;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import org.openl.rules.dependency.graph.DependencyRulesGraph;
 import org.openl.rules.lang.xls.syntax.TableUtils;
+import org.openl.rules.project.abstraction.RulesProject;
+import org.openl.rules.rest.service.WorkspaceProjectService;
+import org.openl.rules.ui.ProjectModel;
 import org.openl.rules.ui.WebStudio;
-import org.openl.rules.webstudio.web.util.WebStudioUtils;
 import org.openl.types.impl.ExecutableMethod;
 
 @RestController
-@RequestMapping(value = "/project/tables", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 @Tag(name = "Project Tables")
 public class RulesDependenciesController {
 
-    @GetMapping
+    private final WorkspaceProjectService projectService;
+
+    public RulesDependenciesController(WorkspaceProjectService projectService) {
+        this.projectService = projectService;
+    }
+
+    @Lookup
+    public WebStudio getWebStudio() {
+        return null;
+    }
+
+    @GetMapping("/project/tables")
     @Operation(summary = "project.tables.summary", description = "project.tables.desc")
-    public List<Table> getTablesWithDependencies(HttpSession session) {
-        WebStudio webStudio = WebStudioUtils.getWebStudio(session);
+    public List<Table> getTablesWithDependencies() {
+        var webStudio = getWebStudio();
         if (webStudio == null || webStudio.getModel() == null) {
             return Collections.emptyList();
         }
+        return getTablesWithDependencies(webStudio.getModel());
+    }
+
+    // for testing purposes only
+    @Hidden
+    @GetMapping("/projects/{projectId}/tables/graph")
+    @ResponseStatus(HttpStatus.NOT_IMPLEMENTED)
+    public List<Table> getTablesWithDependencies(@PathVariable("projectId") RulesProject project) {
+        return getTablesWithDependencies(projectService.getProjectModel(project));
+    }
+
+    private List<Table> getTablesWithDependencies(ProjectModel projectModel) {
+        var webStudio = getWebStudio();
         List<Table> tables = new ArrayList<>();
-        DependencyRulesGraph graph = webStudio.getModel().getDependencyGraph();
+        DependencyRulesGraph graph = projectModel.getDependencyGraph();
 
         var levelIterator = new TopologicalOrderIterator<>(graph);
         while (levelIterator.hasNext()) {
