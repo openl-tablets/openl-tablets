06f64ef29516bdb67d738b950f4b999473822584
Yury Molchan <ymolchan@eisgroup.com>
2025-07-05 00:33:42 +0200

    EPBDS-15057 Define explicit handling of the static resources

---
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/web/servlet/StaticResourcesServlet.java
@@ -0,0 +1,22 @@
+package org.openl.rules.webstudio.web.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Explicit handling of the static resources to be nonconflict with the React application.
+ *
+ * @author Yury Mmolchan
+ */
+@WebServlet({"/css/*", "/icons/*", "/images/*", "/javascript/*", "/favicon.ico", "/favicon.svg"})
+public class StaticResourcesServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        getServletContext().getNamedDispatcher("default").forward(req, resp);;
+    }
+}
