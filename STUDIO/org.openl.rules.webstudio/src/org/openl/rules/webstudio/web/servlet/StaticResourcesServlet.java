1420c850b60b1690182fba7c8dbbfa47e9f73f1f
Yury Molchan <ymolchan@eisgroup.com>
2025-07-11 18:30:43 +0200

    EPBDS-14264 Setup React JS app in the Maven workflow

--- STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/web/servlet/StaticResourcesServlet.java
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/web/servlet/StaticResourcesServlet.java
@@ -1,6 +1,9 @@
 package org.openl.rules.webstudio.web.servlet;
 
+import static java.util.Objects.requireNonNull;
+
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,11 +15,50 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author Yury Mmolchan
  */
-@WebServlet({"/css/*", "/icons/*", "/images/*", "/js/*", "/javascript/*", "/favicon.ico", "/favicon.svg"})
+@WebServlet("/*")
 public class StaticResourcesServlet extends HttpServlet {
+
+    private String htmlTemplate;
+
+    @Override
+    public void init() throws ServletException {
+        try (var resource = getServletContext().getResourceAsStream("/index.html")) {
+            htmlTemplate = new String(requireNonNull(resource, "index.html resource not found").readAllBytes(), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            throw new ServletException("Failed to load index.html template", e);
+        }
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        getServletContext().getNamedDispatcher("default").forward(req, resp);;
+
+        var path = req.getPathInfo();
+        // Check if the request is for a static resource
+        if (path.startsWith("/css/")
+                || path.startsWith("/js/")
+                || path.startsWith("/javascript/")
+                || path.startsWith("/images/")
+                || path.startsWith("/icons/")
+                || path.equals("/favicon.svg")
+                || path.equals("/favicon.ico")) {
+
+            // Forward the request to the container's "default" servlet.
+            // This is the standard, portable way to handle static resources
+            // This servlet correctly handles content types, caching headers (ETag, Last-Modified)
+            getServletContext().getNamedDispatcher("default").forward(req, resp);
+            return;
+        }
+
+        // Handling index.html for the React application
+        var out = htmlTemplate.replace("base href=\"/", "base href=\"" + req.getContextPath() + "/");
+
+        var reactUiRoot = System.getenv("_REACT_UI_ROOT_");
+        if (reactUiRoot != null) {
+            out = out.replace("script src=\"js/", "script src=\"" + reactUiRoot + "/");
+        }
+
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding("UTF-8");
+        resp.getWriter().println(out);
     }
 }
