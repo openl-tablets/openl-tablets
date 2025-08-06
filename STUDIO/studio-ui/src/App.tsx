cc3cd9634896862dae72ac8b7ad5b94e35d01f36
Aliaksandr Samuseu <asamuseu@eisgroup.com>
2025-08-06 11:14:08 +0200

    EPBDS-15059 Connect LoginPage to the backend

--- STUDIO/studio-ui/src/App.tsx
+++ STUDIO/studio-ui/src/App.tsx
@@ -1,7 +1,7 @@
 import React, { Suspense, useEffect } from 'react'
 import { router } from './routes'
 import { App as AntApp } from 'antd'
-import { useUserStore, useNotificationStore, useAppStore } from 'store'
+import { useAppStore, useNotificationStore, useUserStore } from 'store'
 import { RouterProvider } from 'react-router-dom'
 import { SecurityProvider } from './providers/SecurityProvider'
 import { CONFIG } from './services'
@@ -10,6 +10,9 @@ function App() {
     const { showLogin } = useAppStore()
     const { fetchUserInfo, isLoggedIn } = useUserStore()
     const { fetchNotification } = useNotificationStore()
+
+    const loginPage = `${CONFIG.CONTEXT}/login`
+    const isLoginPage = window.location.pathname === loginPage
 
     useEffect(() => {
         fetchUserInfo()
@@ -26,21 +29,19 @@ function App() {
         }
     }, [fetchNotification])
 
-    if (showLogin) {
-        window.location.href = CONFIG.LOGIN_URL + `?redirect=${encodeURIComponent(window.location.href)}`
+    if (showLogin && !isLoginPage) {
+        window.location.href = loginPage + `?from=${encodeURIComponent(window.location.href)}`
     }
 
-    return isLoggedIn
-        ? (
-            <Suspense fallback={<div>Loading...</div>}>
-                <AntApp>
-                    <SecurityProvider>
-                        <RouterProvider router={router} />
-                    </SecurityProvider>
-                </AntApp>
-            </Suspense>
-        )
-        : null
+    return(
+        <Suspense fallback={<div>Loading...</div>}>
+            <AntApp>
+                <SecurityProvider>
+                    <RouterProvider router={router} />
+                </SecurityProvider>
+            </AntApp>
+        </Suspense>
+    )
 }
 
 export default App
