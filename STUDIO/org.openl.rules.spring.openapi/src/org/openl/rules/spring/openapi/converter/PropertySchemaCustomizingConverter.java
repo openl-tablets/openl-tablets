c90910fb46697d90e3b1eaaf3b28a8663213a81d
Vladyslav Pikus <vpikus@eisgroup.com>
2025-09-05 12:19:46 +0300

    EPBDS-15181 Migrate to the latest swagger and fix model converters

--- STUDIO/org.openl.rules.spring.openapi/src/org/openl/rules/spring/openapi/converter/PropertySchemaCustomizingConverter.java
+++ STUDIO/org.openl.rules.spring.openapi/src/org/openl/rules/spring/openapi/converter/PropertySchemaCustomizingConverter.java
@@ -1,16 +1,25 @@
 package org.openl.rules.spring.openapi.converter;
 
+import java.lang.annotation.Annotation;
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.Objects;
+import java.util.stream.Stream;
 
+import com.fasterxml.jackson.databind.BeanDescription;
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.introspect.BeanPropertyDefinition;
 import io.swagger.v3.core.converter.AnnotatedType;
 import io.swagger.v3.core.converter.ModelConverter;
 import io.swagger.v3.core.converter.ModelConverterContext;
+import io.swagger.v3.core.util.ObjectMapperFactory;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.models.media.Schema;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 
 import org.openl.rules.spring.openapi.service.OpenApiPropertyResolver;
 import org.openl.util.StringUtils;
@@ -27,9 +36,11 @@ import org.openl.util.StringUtils;
 public class PropertySchemaCustomizingConverter implements ModelConverter {
 
     private final OpenApiPropertyResolver apiPropertyResolver;
+    private final ObjectMapper objectMapper;
 
     public PropertySchemaCustomizingConverter(OpenApiPropertyResolver apiPropertyResolver) {
         this.apiPropertyResolver = apiPropertyResolver;
+        this.objectMapper = ObjectMapperFactory.createJson();
     }
 
     @Override
@@ -40,26 +51,55 @@ public class PropertySchemaCustomizingConverter implements ModelConverter {
             if (resolvedSchema == null) {
                 return null;
             }
-            if (type.isSchemaProperty()) {
-                if (type.getCtxAnnotations() != null) {
-                    for (var anno : type.getCtxAnnotations()) {
-                        if (anno.annotationType() == Deprecated.class) {
-                            resolvedSchema.setDeprecated(Boolean.TRUE);
-                        } else if (anno instanceof Parameter) {
-                            // Support Parameter when it's defined on class field. Swagger doesn't support this case
-                            var paramApi = (Parameter) anno;
-                            if (StringUtils.isNotBlank(paramApi.description())) {
-                                resolvedSchema.setDescription(paramApi.description());
-                            }
-                            if (StringUtils.isNotBlank(paramApi.example())) {
-                                resolvedSchema.setExample(paramApi.example());
-                            }
-                            var schemaApi = paramApi.schema();
-                            if (schemaApi != null && schemaApi.allowableValues().length > 0) {
-                                resolvedSchema.setEnum(Arrays.asList(schemaApi.allowableValues()));
-                            }
-                            if (paramApi.required()) {
-                                type.getParent().addRequiredItem(type.getPropertyName());
+            if (resolvedSchema.get$ref() != null || (resolvedSchema.getName() != null && "object".equals(resolvedSchema.getType()))) {
+                JavaType javaType;
+                if (type.getType() instanceof JavaType) {
+                    javaType = (JavaType) type.getType();
+                } else {
+                    javaType = objectMapper.constructType(type.getType());
+                }
+                var definition = objectMapper.getSerializationConfig().introspect(javaType);
+                BeanDescription deserializationBeanDesc = null;
+                Schema definedSchema;
+                if (resolvedSchema.get$ref() != null ) {
+                    definedSchema = context.getDefinedModels().get(resolvedSchema.get$ref().substring(21));
+                } else {
+                    definedSchema = resolvedSchema;
+                }
+                for (final var originalProperty : definition.findProperties()) {
+                    var propSchema = findProperty(definedSchema, originalProperty);
+                    if (propSchema == null) {
+                        continue;
+                    }
+                    var property = originalProperty;
+                    if (originalProperty.getPrimaryMember() == null) {
+                        if (deserializationBeanDesc == null) {
+                            deserializationBeanDesc = objectMapper.getDeserializationConfig().introspect(javaType);
+                        }
+                        property = deserializationBeanDesc.findProperties().stream()
+                                .filter(p -> p.getName().equals(originalProperty.getName()))
+                                .findFirst()
+                                .orElse(originalProperty);
+                    }
+                    var deprecated = findAnnotation(property, Deprecated.class);
+                    if (deprecated != null) {
+                        propSchema.setDeprecated(Boolean.TRUE);
+                    }
+                    var paramApi = findAnnotation(property, Parameter.class);
+                    if (paramApi != null) {
+                        if (StringUtils.isNotBlank(paramApi.description())) {
+                            propSchema.setDescription(apiPropertyResolver.resolve(paramApi.description()));
+                        }
+                        if (StringUtils.isNotBlank(paramApi.example())) {
+                            propSchema.setExample(paramApi.example());
+                        }
+                        var schemaApi = paramApi.schema();
+                        if (schemaApi != null && schemaApi.allowableValues().length > 0) {
+                            propSchema.setEnum(Arrays.asList(schemaApi.allowableValues()));
+                        }
+                        if (paramApi.required()) {
+                            if (!CollectionUtils.containsInstance(definedSchema.getRequired(), property.getName())) {
+                                definedSchema.addRequiredItem(property.getName());
                             }
                         }
                     }
@@ -71,5 +111,31 @@ public class PropertySchemaCustomizingConverter implements ModelConverter {
             return resolvedSchema;
         }
         return null;
+    }
+
+    @SuppressWarnings("rawtypes")
+    private Schema findProperty(Schema definedSchema, BeanPropertyDefinition propertyDefinition) {
+        Schema propSchema = null;
+        if (definedSchema.getProperties() != null) {
+            propSchema = (Schema) definedSchema.getProperties().get(propertyDefinition.getName());
+        }
+        if (propSchema == null && definedSchema.getAllOf() != null) {
+            for (var allOf : definedSchema.getAllOf()) {
+                propSchema = findProperty((Schema) allOf, propertyDefinition);
+                if (propSchema != null) {
+                    break;
+                }
+            }
+        }
+        return propSchema;
+    }
+
+    private  <A extends Annotation> A findAnnotation(BeanPropertyDefinition propertyDefinition, Class<A> annotation) {
+        return Stream.of(propertyDefinition.getField(), propertyDefinition.getGetter(), propertyDefinition.getSetter())
+                .filter(Objects::nonNull)
+                .map(member -> member.getAnnotation(annotation))
+                .filter(Objects::nonNull)
+                .findFirst()
+                .orElse(null);
     }
 }
