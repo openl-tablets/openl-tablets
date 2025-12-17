c388e6bce95024c9c1eb22b25a29cdd6b64d5fad
Vladyslav Pikus <vpikus@eisgroup.com>
2025-12-17 10:20:32 +0200

    EPBDS-15439 API to cancel conflicts

--- STUDIO/org.openl.rules.webstudio/src/org/openl/studio/projects/rest/controller/ProjectsMergeController.java
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/studio/projects/rest/controller/ProjectsMergeController.java
@@ -11,9 +11,11 @@ import jakarta.validation.constraints.NotNull;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,6 +23,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -235,6 +238,19 @@ public class ProjectsMergeController {
                 studio.releaseProject(project.getName());
             }
         }
+    }
+
+    @DeleteMapping("/conflicts")
+    @Operation(summary = "Cancel pending merge conflicts",
+            description = "Cancels pending merge conflicts and clears the conflict state from the session")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void cancelMergeConflicts(@ProjectId @PathVariable("projectId") RulesProject project) {
+        var projectId = projectService.resolveProjectId(project);
+        if (!conflictsSessionHolder.hasConflictInfo(projectId)) {
+            throw new NotFoundException("project.merge.result.not.found.message");
+        }
+
+        conflictsSessionHolder.remove(projectId);
     }
 
     private String generateDefaultMergeMessage(MergeConflictInfo mergeConflictInfo, List<FileConflictResolution> resolutions) {
