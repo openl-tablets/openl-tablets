b28f3704eee38b3bf2cc4de822e95186deb00549
Yury Molchan <yury.molchan@gmail.com>
2026-01-22 13:17:43 +0100

    EPBDS-15346 Improve testing of multi-module projects

--- DEV/org.openl.rules.project/test/org/openl/rules/project/instantiation/SimpleProjectEngineFactoryTest.java
+++ DEV/org.openl.rules.project/test/org/openl/rules/project/instantiation/SimpleProjectEngineFactoryTest.java
@@ -1,6 +1,7 @@
 package org.openl.rules.project.instantiation;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -9,6 +10,9 @@ import java.lang.reflect.Method;
 import org.junit.jupiter.api.Test;
 
 import org.openl.exception.OpenlNotCheckedException;
+import org.openl.rules.context.DefaultRulesRuntimeContext;
+import org.openl.rules.context.IRulesRuntimeContext;
+import org.openl.rules.context.RulesRuntimeContextFactory;
 import org.openl.rules.project.instantiation.SimpleProjectEngineFactory.SimpleProjectEngineFactoryBuilder;
 import org.openl.rules.project.resolving.ProjectResolvingException;
 
@@ -54,19 +58,6 @@ public class SimpleProjectEngineFactoryTest {
         assertNotNull(sayHelloMethod);
     }
 
-    @Test
-    public void dynamicInterfaceWithRuntimeContextTest() throws Exception {
-        SimpleProjectEngineFactory<Object> simpleProjectEngineFactory = new SimpleProjectEngineFactoryBuilder<>()
-                .setProject("test-resources/test1/third")
-                .setWorkspace("test-resources/test1")
-                .build();
-        Object instance = simpleProjectEngineFactory.newInstance();
-        assertNotNull(instance);
-        Method sayHelloMethod = simpleProjectEngineFactory.getInterfaceClass()
-                .getMethod("sayHello");
-        assertNotNull(sayHelloMethod);
-    }
-
     public interface SayHello {
         String sayHello();
     }
@@ -81,7 +72,8 @@ public class SimpleProjectEngineFactoryTest {
         Object instance = simpleProjectEngineFactory.newInstance();
         assertNotNull(instance);
         assertNotNull(simpleProjectEngineFactory.getInterfaceClass());
-        assertEquals(simpleProjectEngineFactory.getInterfaceClass(), SayHello.class);
+        assertEquals(SayHello.class, simpleProjectEngineFactory.getInterfaceClass());
+        assertInstanceOf(SayHello.class, instance);
     }
 
     @Test
@@ -116,6 +108,90 @@ public class SimpleProjectEngineFactoryTest {
                     .setProjectDependencies("test-resources/test1/unknown")
                     .build();
         });
+    }
+
+    @Test
+    public void invokeSimpleTest() throws Exception {
+        SimpleProjectEngineFactory<Object> simpleProjectEngineFactory = new SimpleProjectEngineFactoryBuilder<>()
+                .setProject("test-resources/multi-module-support/test2/project2")
+                .setWorkspace("test-resources/multi-module-support/test2")
+                .build();
+        var instance = simpleProjectEngineFactory.newInstance();
+        assertNotNull(instance);
+        var interfaceClass = simpleProjectEngineFactory.getInterfaceClass();
+        assertNotNull(interfaceClass);
+        assertInstanceOf(interfaceClass, instance);
+        assertEquals("Good Morning, World!", interfaceClass.getMethod("helloWorld", int.class).invoke(instance, 10));
+        assertEquals(2, ((Object[]) interfaceClass.getMethod("getData1").invoke(instance)).length);
+    }
+
+    @Test
+    public void invokeEmptyContextTest() throws Exception {
+        SimpleProjectEngineFactory<Object> simpleProjectEngineFactory = new SimpleProjectEngineFactoryBuilder<>()
+                .setProject("test-resources/multi-module-support/test2/project3")
+                .setWorkspace("test-resources/multi-module-support/test2")
+                .build();
+        var instance = simpleProjectEngineFactory.newInstance();
+        assertNotNull(instance);
+        var interfaceClass = simpleProjectEngineFactory.getInterfaceClass();
+        assertNotNull(interfaceClass);
+        assertInstanceOf(interfaceClass, instance);
+        assertEquals("World, Good Morning!", interfaceClass.getMethod("worldHello", IRulesRuntimeContext.class, int.class).invoke(instance, null, 10));
+        assertEquals(3, ((Object[]) interfaceClass.getMethod("getData2", IRulesRuntimeContext.class).invoke(instance, new DefaultRulesRuntimeContext())).length);
+    }
+
+    @Test
+    public void invokeContextTest() throws Exception {
+        SimpleProjectEngineFactory<Object> simpleProjectEngineFactory = new SimpleProjectEngineFactoryBuilder<>()
+                .setProject("test-resources/multi-module-support/test3/project2")
+                .setWorkspace("test-resources/multi-module-support/test3")
+                .build();
+        var instance = simpleProjectEngineFactory.newInstance();
+        assertNotNull(instance);
+        Class<?> serviceClass = instance.getClass();
+
+        IRulesRuntimeContext context = RulesRuntimeContextFactory.buildRulesRuntimeContext();
+        context.setLob("lob2");
+
+        Method method = serviceClass.getMethod("hello", IRulesRuntimeContext.class, int.class);
+        Object result = method.invoke(instance, context, 10);
+        assertEquals("Good Morning, World!", result);
+
+        method = serviceClass.getMethod("getData1", IRulesRuntimeContext.class);
+        result = method.invoke(instance, context);
+        assertEquals(2, ((Object[]) result).length);
+
+        method = serviceClass.getMethod("getData2", IRulesRuntimeContext.class);
+        result = method.invoke(instance, context);
+        assertEquals(3, ((Object[]) result).length);
+
+        context.setLob("lob3");
+
+        method = serviceClass.getMethod("hello", IRulesRuntimeContext.class, int.class);
+        result = method.invoke(instance, context, 10);
+        assertEquals("World, Good Morning!", result);
+
+        method = serviceClass.getMethod("getData1", IRulesRuntimeContext.class);
+        result = method.invoke(instance, context);
+        assertEquals(2, ((Object[]) result).length);
+
+        method = serviceClass.getMethod("getData2", IRulesRuntimeContext.class);
+        result = method.invoke(instance, context);
+        assertEquals(3, ((Object[]) result).length);
+
+        context.setLob("lob1");
+
+        method = serviceClass.getMethod("hello", IRulesRuntimeContext.class, int.class);
+        result = method.invoke(instance, context, 10);
+        assertEquals("Good Morning", result);
+
+        method = serviceClass.getMethod("getData1", IRulesRuntimeContext.class);
+        result = method.invoke(instance, context);
+        assertEquals(2, ((Object[]) result).length);
+
+        method = serviceClass.getMethod("getData2", IRulesRuntimeContext.class);
+        result = method.invoke(instance, context);
+        assertEquals(3, ((Object[]) result).length);
     }
 
 }
