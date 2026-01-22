6ceac83cb84815ebf742d7b476d400b28a74e72c
Aliaksei Tsymbalist <atsymbalist@eisgroup.com>
2026-01-22 17:23:53 +0100

    EPBDS-14557 Bugfix

--- DEV/org.openl.rules/src/org/openl/binding/impl/MethodNodeBinder.java
+++ DEV/org.openl.rules/src/org/openl/binding/impl/MethodNodeBinder.java
@@ -32,6 +32,7 @@ import org.openl.syntax.impl.IdentifierNode;
 import org.openl.types.IMethodCaller;
 import org.openl.types.IOpenClass;
 import org.openl.types.IOpenMethod;
+import org.openl.types.impl.DomainOpenClass;
 import org.openl.util.DomainUtils;
 import org.openl.util.StringUtils;
 
@@ -189,7 +190,7 @@ public class MethodNodeBinder extends ANodeBinder {
                                   IOpenClass parameterType,
                                   ISyntaxNode methodInvocationNode,
                                   IBindingContext bindingContext) {
-        if (canBeValidated(methodArgumentNode)) {
+        if (parameterType instanceof DomainOpenClass && canBeValidated(methodArgumentNode)) {
             if (containsLiteralValue(methodArgumentNode)) {
                 if (parameterType.isArray()) {
                     validateArgumentForArrayParameter(methodArgumentNode, parameterType, methodInvocationNode, bindingContext);
@@ -238,6 +239,9 @@ public class MethodNodeBinder extends ANodeBinder {
                                                    IOpenClass parameterType,
                                                    ISyntaxNode methodInvocationNode,
                                                    IBindingContext bindingContext) {
+        // TODO: 1. count dim for parameter type
+        //  2. get argument on the same dim
+        //  convert
         if (methodArgumentNode instanceof LiteralBoundNode literalBoundNode) {
             tryCastLiteralArgument(literalBoundNode, parameterType, methodInvocationNode, bindingContext);
         } else if (methodArgumentNode.getChildren().length > 0) {
