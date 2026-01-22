85b449584e447ffe2be3547f4cf931bbce0575a8
Yury Molchan <yury.molchan@gmail.com>
2026-01-22 02:56:28 +0100

    EPBDS-15346 Merge OpenLMethodHandler into OpenLRulesMethodHandler

--- DEV/org.openl.rules/src/org/openl/rules/runtime/OpenLRulesMethodHandler.java
+++ DEV/org.openl.rules/src/org/openl/rules/runtime/OpenLRulesMethodHandler.java
@@ -1,5 +1,6 @@
 package org.openl.rules.runtime;
 
+import java.lang.reflect.Array;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.Map;
@@ -7,93 +8,149 @@ import java.util.Map;
 import org.openl.rules.context.IRulesRuntimeContext;
 import org.openl.rules.lang.xls.binding.XlsModuleOpenClass;
 import org.openl.runtime.IEngineWrapper;
+import org.openl.runtime.IOpenLMethodHandler;
 import org.openl.runtime.IRuntimeEnvBuilder;
-import org.openl.runtime.OpenLMethodHandler;
 import org.openl.types.IOpenField;
 import org.openl.types.IOpenMember;
 import org.openl.types.IOpenMethod;
+import org.openl.types.java.JavaOpenClass;
+import org.openl.vm.IRuntimeEnv;
 
-public class OpenLRulesMethodHandler extends OpenLMethodHandler {
+public class OpenLRulesMethodHandler implements IOpenLMethodHandler<Method, IOpenMember>, IEngineWrapper {
 
     private static final Object[] NO_PARAMS = new Object[0];
     private final ValidationHandler validationHandler = new ValidationHandler();
+    private final Object openlInstance;
+    private final Map<Method, IOpenMember> methodMap;
+    private final IRuntimeEnvBuilder runtimeEnvBuilder;
 
     public OpenLRulesMethodHandler(Object openlInstance,
                                    Map<Method, IOpenMember> methodMap,
                                    IRuntimeEnvBuilder runtimeEnvBuilder) {
-        super(openlInstance, methodMap, runtimeEnvBuilder);
+        this.openlInstance = openlInstance;
+        this.methodMap = methodMap;
+        this.runtimeEnvBuilder = runtimeEnvBuilder;
     }
 
     @Override
     public Object invoke(Method method, Object[] args) throws Exception {
-        var targetMethod = getMethodMap().get(method);
-        if (targetMethod == null) {
-            return super.invoke(method, args);
+        if (IEngineWrapper.class == method.getDeclaringClass()) {
+            return method.invoke(this, args);
         }
+        if (Object.class == method.getDeclaringClass()) {
+            return method.invoke(openlInstance, args);
+        }
+
+        var member = methodMap.get(method);
+        var env = getRuntimeEnv();
         if (args.length > 0) {
             var v = args[0];
             if (v instanceof IRulesRuntimeContext || v == null &&
-                    (targetMethod instanceof IOpenMethod m && m.getSignature().getParameterTypes().length < args.length
-                            || targetMethod instanceof IOpenField && args.length == 1
+                    (member instanceof IOpenMethod m && m.getSignature().getParameterTypes().length < args.length
+                            || member instanceof IOpenField && args.length == 1
                     )
             ) {
-                getRuntimeEnv().setContext((IRulesRuntimeContext) args[0]);
                 args = args.length > 1 ? Arrays.copyOfRange(args, 1, args.length) : NO_PARAMS;
+                if (v != null) {
+                    env.setContext((IRulesRuntimeContext) v);
+                }
             }
         }
-        if (IEngineWrapper.class != method.getDeclaringClass()) {
-            StringBuilder output = null;
-            if (LoggingHandler.isEnabled()) {
-                output = new StringBuilder();
-                var sourceClass = targetMethod.getDeclaringClass();
-                if (sourceClass instanceof XlsModuleOpenClass) {
-                    output.append("\tModule Name: ").append(((XlsModuleOpenClass) sourceClass).getModuleName())
-                            .append('\n');
-                }
-                output.append("\tMethod: ").append(targetMethod.getDisplayName(0));
-                output.append("\n\tRuntime Context: ").append(LoggingHandler.convert(getRuntimeEnv().getContext()));
-                if (args.length == 1) {
-                    output.append("\nArgs: ").append(LoggingHandler.convert(args[0]));
-                } else if (args.length > 1) {
-                    output.append("\n\tArgs: {");
-                    for (int i = 0; i < args.length; i++) {
-                        output.append('"')
-                                .append(((IOpenMethod) targetMethod).getSignature().getParameterName(i))
-                                .append("\":");
-                        output.append(LoggingHandler.convert(args[i]));
-                        if (i < (args.length - 1)) {
-                            output.append(',');
-                        }
+
+        StringBuilder output = null;
+        if (LoggingHandler.isEnabled()) {
+            output = new StringBuilder();
+            var sourceClass = member.getDeclaringClass();
+            if (sourceClass instanceof XlsModuleOpenClass) {
+                output.append("\tModule Name: ").append(((XlsModuleOpenClass) sourceClass).getModuleName())
+                        .append('\n');
+            }
+            output.append("\tMethod: ").append(member.getDisplayName(0));
+            output.append("\n\tRuntime Context: ").append(LoggingHandler.convert(env.getContext()));
+            if (args.length == 1) {
+                output.append("\nArgs: ").append(LoggingHandler.convert(args[0]));
+            } else if (args.length > 1) {
+                output.append("\n\tArgs: {");
+                for (int i = 0; i < args.length; i++) {
+                    output.append('"')
+                            .append(((IOpenMethod) member).getSignature().getParameterName(i))
+                            .append("\":");
+                    output.append(LoggingHandler.convert(args[i]));
+                    if (i < (args.length - 1)) {
+                        output.append(',');
                     }
-                    output.append('}');
                 }
-
-            }
-            Object result = null;
-            Exception exception = null;
-            try {
-                if (targetMethod instanceof IOpenMethod) {
-                    validationHandler
-                            .validateProxyArguments(((IOpenMethod) targetMethod).getSignature(), getRuntimeEnv(), args);
-                }
-                result = super.invoke(method, args);
-            } catch (Exception e) {
-                exception = e;
-            }
-            if (output != null) {
-                if (exception == null) {
-                    output.append("\n\tResult: ").append(LoggingHandler.convert(result));
-                } else {
-                    output.append("\n\tException: ").append(LoggingHandler.convert(exception));
-                }
-                LoggingHandler.log(output);
+                output.append('}');
             }
 
-            if (exception != null) {
-                throw exception;
-            }
-            return result;
         }
-        return super.invoke(method, args);
+        Object result = null;
+        Exception exception = null;
+        try {
+            if (member instanceof IOpenMethod m) {
+                validationHandler.validateProxyArguments(m.getSignature(), env, args);
+                result = m.invoke(openlInstance, args, env);
+            } else {
+                result = ((IOpenField) member).get(openlInstance, env);
+            }
+            if (method.getReturnType() != void.class && member.getType() == JavaOpenClass.VOID || member
+                    .getType() == JavaOpenClass.CLS_VOID && method.getReturnType().isPrimitive()) {
+                result = Array.get(Array.newInstance(method.getReturnType(), 1), 0);
+            }
+        } catch (Exception e) {
+            exception = e;
+        }
+        if (output != null) {
+            if (exception == null) {
+                output.append("\n\tResult: ").append(LoggingHandler.convert(result));
+            } else {
+                output.append("\n\tException: ").append(LoggingHandler.convert(exception));
+            }
+            LoggingHandler.log(output);
+        }
+
+        if (exception != null) {
+            throw exception;
+        }
+        return result;
+    }
+
+    @Override
+    public IOpenMember getTargetMember(Method key) {
+        return methodMap.get(key);
+    }
+
+    @Override
+    public IOpenMember getOpenMember(Method key) {
+        return methodMap.get(key);
+    }
+
+    @Override
+    public Object getTarget() {
+        return openlInstance;
+    }
+
+    private final ThreadLocal<IRuntimeEnv> env = new ThreadLocal<>();
+
+
+    @Override
+    public Object getInstance() {
+        return openlInstance;
+    }
+
+    @Override
+    public IRuntimeEnv getRuntimeEnv() {
+        IRuntimeEnv runtimeEnv = env.get();
+        if (runtimeEnv == null) {
+            IRuntimeEnv x = runtimeEnvBuilder.buildRuntimeEnv();
+            env.set(x);
+            return x;
+        }
+        return runtimeEnv;
+    }
+
+    @Override
+    public void release() {
+        env.remove();
     }
 }
