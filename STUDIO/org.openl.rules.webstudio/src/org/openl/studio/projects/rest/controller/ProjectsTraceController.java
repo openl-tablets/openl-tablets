eca5c7ee83760a288a6ae826b9fa259fc6515197
Vladyslav Pikus <vpikus@eisgroup.com>
2026-01-27 17:40:40 +0200

    EPBDS-15551 Trace API

--- STUDIO/org.openl.rules.webstudio/src/org/openl/studio/projects/rest/controller/ProjectsTraceController.java
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/studio/projects/rest/controller/ProjectsTraceController.java
@@ -1,20 +1,11 @@
 package org.openl.studio.projects.rest.controller;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 import com.fasterxml.jackson.annotation.JsonView;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.github.victools.jsonschema.generator.OptionPreset;
-import com.github.victools.jsonschema.generator.SchemaGenerator;
-import com.github.victools.jsonschema.generator.SchemaGeneratorConfigBuilder;
-import com.github.victools.jsonschema.generator.SchemaVersion;
-import com.github.victools.jsonschema.module.swagger2.Swagger2Module;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -33,27 +24,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import org.openl.base.INamedThing;
 import org.openl.rules.context.DefaultRulesRuntimeContext;
 import org.openl.rules.context.IRulesRuntimeContext;
-import org.openl.rules.method.ExecutableRulesMethod;
 import org.openl.rules.project.abstraction.RulesProject;
 import org.openl.rules.serialization.JsonUtils;
-import org.openl.rules.testmethod.ParameterWithValueDeclaration;
 import org.openl.rules.testmethod.TestSuiteMethod;
 import org.openl.rules.ui.TraceHelper;
-import org.openl.rules.webstudio.web.trace.TraceFormatter;
-import org.openl.rules.webstudio.web.trace.node.ATableTracerNode;
-import org.openl.rules.webstudio.web.trace.node.DTRuleTraceObject;
-import org.openl.rules.webstudio.web.trace.node.DTRuleTracerLeaf;
 import org.openl.rules.webstudio.web.trace.node.ITracerObject;
-import org.openl.rules.webstudio.web.trace.node.RefToTracerNodeObject;
 import org.openl.studio.common.exception.ConflictException;
 import org.openl.studio.common.exception.NotFoundException;
 import org.openl.studio.common.model.GenericView;
 import org.openl.studio.projects.messaging.SocketTraceExecutionProgressListenerFactory;
 import org.openl.studio.projects.model.trace.TraceInputRequest;
 import org.openl.studio.projects.model.trace.TraceNodeView;
+import org.openl.studio.projects.model.trace.TraceNodeViewMapper;
 import org.openl.studio.projects.model.trace.TraceParameterValue;
 import org.openl.studio.projects.model.trace.TraceResultResponse;
 import org.openl.studio.projects.rest.annotations.ProjectId;
@@ -65,7 +49,6 @@ import org.openl.studio.projects.service.trace.TraceParameterRegistry;
 import org.openl.types.IMethodSignature;
 import org.openl.types.IOpenClass;
 import org.openl.types.IOpenMethod;
-import org.openl.util.StringUtils;
 
 /**
  * REST controller for trace execution API.
@@ -173,10 +156,10 @@ public class ProjectsTraceController {
         }
 
         var rootElement = traceHelper.getTableTracer(0);
-        var rootNodes = createSimpleNodes(rootElement.getChildren(), traceHelper, showRealNumbers);
-        var totalNodes = countTotalNodes(rootElement);
+        var mapper = createMapper();
+        var rootNodes = mapper.createSimpleNodes(rootElement.getChildren(), traceHelper, showRealNumbers);
 
-        return new TraceResultResponse(rootNodes, totalNodes);
+        return new TraceResultResponse(rootNodes, countTotalNodes(rootElement));
     }
 
     @Operation(summary = "Get trace node children (BETA)", description = "Retrieves child nodes for lazy loading")
@@ -200,7 +183,7 @@ public class ProjectsTraceController {
         }
 
         var element = traceHelper.getTableTracer(id == null ? 0 : id);
-        return createSimpleNodes(element.getChildren(), traceHelper, showRealNumbers);
+        return createMapper().createSimpleNodes(element.getChildren(), traceHelper, showRealNumbers);
     }
 
     @Operation(summary = "Get trace node details (BETA)", description = "Retrieves detailed info for a single trace node including parameters, context, and result")
@@ -228,10 +211,7 @@ public class ProjectsTraceController {
             throw new NotFoundException("trace.node.not.found.message");
         }
 
-        ObjectMapper objectMapper = configureObjectMapper();
-        SchemaGenerator schemaGenerator = initSchemaGenerator(objectMapper);
-
-        return createDetailedNode(element, traceHelper, showRealNumbers, objectMapper, schemaGenerator);
+        return createMapper().createDetailedNode(element, traceHelper, showRealNumbers);
     }
 
     @Operation(summary = "Cancel trace execution (BETA)", description = "Cancels the current trace execution if running")
@@ -240,6 +220,7 @@ public class ProjectsTraceController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void cancelTrace(@ProjectId @PathVariable("projectId") RulesProject project) {
         traceResultRegistry.cancelIfAny();
+        parameterRegistry.clear();
     }
 
     @Operation(summary = "Get lazy parameter value (BETA)", description = "Retrieves full JSON value for a lazy-loaded parameter")
@@ -260,120 +241,11 @@ public class ProjectsTraceController {
             throw new NotFoundException("trace.parameter.not.found.message");
         }
 
-        ObjectMapper objectMapper = configureObjectMapper();
-        SchemaGenerator schemaGenerator = initSchemaGenerator(objectMapper);
-
-        return buildParameterValue(param, objectMapper, schemaGenerator, null, false);
+        return createMapper().buildParameterValue(param, false);
     }
 
-    private List<TraceNodeView> createSimpleNodes(Iterable<ITracerObject> children,
-                                                   TraceHelper traceHelper,
-                                                   boolean showRealNumbers) {
-        List<TraceNodeView> nodes = new ArrayList<>();
-        for (ITracerObject child : children) {
-            nodes.add(createSimpleNode(child, traceHelper, showRealNumbers));
-        }
-        return nodes;
-    }
-
-    private TraceNodeView createSimpleNode(ITracerObject element,
-                                           TraceHelper traceHelper,
-                                           boolean showRealNumbers) {
-        if (element == null) {
-            return TraceNodeView.builder()
-                    .key(-1)
-                    .title("null")
-                    .tooltip("null")
-                    .type("value")
-                    .lazy(false)
-                    .extraClasses("value")
-                    .build();
-        }
-
-        String name = TraceFormatter.getDisplayName(element, !showRealNumbers);
-        int key = traceHelper.getNodeKey(element);
-        String type = getType(element);
-        boolean lazy = !element.isLeaf();
-
-        return TraceNodeView.builder()
-                .key(key)
-                .title(name)
-                .tooltip(name)
-                .type(type)
-                .lazy(lazy)
-                .extraClasses(type)
-                .build();
-    }
-
-    private TraceNodeView createDetailedNode(ITracerObject element,
-                                              TraceHelper traceHelper,
-                                              boolean showRealNumbers,
-                                              ObjectMapper objectMapper,
-                                              SchemaGenerator schemaGenerator) {
-        if (element == null) {
-            return TraceNodeView.builder()
-                    .key(-1)
-                    .title("null")
-                    .tooltip("null")
-                    .type("value")
-                    .lazy(false)
-                    .extraClasses("value")
-                    .build();
-        }
-
-        String name = TraceFormatter.getDisplayName(element, !showRealNumbers);
-        int key = traceHelper.getNodeKey(element);
-        String type = getType(element);
-        boolean lazy = !element.isLeaf();
-
-        // Build parameters, context, and result
-        List<TraceParameterValue> parameters = buildInputParameters(element, objectMapper, schemaGenerator);
-        TraceParameterValue context = buildContext(element, objectMapper, schemaGenerator);
-        TraceParameterValue result = buildResult(element, objectMapper, schemaGenerator);
-
-        return TraceNodeView.builder()
-                .key(key)
-                .title(name)
-                .tooltip(name)
-                .type(type)
-                .lazy(lazy)
-                .extraClasses(type)
-                .parameters(parameters)
-                .context(context)
-                .result(result)
-                .build();
-    }
-
-    private String getType(ITracerObject element) {
-        String type = element.getType();
-        if (type == null) {
-            type = StringUtils.EMPTY;
-        }
-        if (element instanceof DTRuleTraceObject condition) {
-            if (!condition.isSuccessful()) {
-                return type + " fail";
-            } else {
-                ITracerObject result = findResult(element.getChildren());
-                if (result != null) {
-                    return type + " result";
-                }
-                return type + " no_result";
-            }
-        }
-        return type;
-    }
-
-    private ITracerObject findResult(Iterable<ITracerObject> children) {
-        for (ITracerObject child : children) {
-            if (child instanceof DTRuleTracerLeaf) {
-                return child;
-            }
-            ITracerObject result = findResult(child.getChildren());
-            if (result != null) {
-                return result;
-            }
-        }
-        return null;
+    private TraceNodeViewMapper createMapper() {
+        return new TraceNodeViewMapper(configureObjectMapper(), parameterRegistry);
     }
 
     private int countTotalNodes(ITracerObject element) {
@@ -382,131 +254,6 @@ public class ProjectsTraceController {
             count += countTotalNodes(child);
         }
         return count;
-    }
-
-    private SchemaGenerator initSchemaGenerator(ObjectMapper objectMapper) {
-        var config = new SchemaGeneratorConfigBuilder(objectMapper, SchemaVersion.DRAFT_2020_12, OptionPreset.PLAIN_JSON)
-                .with(new Swagger2Module())
-                .build();
-        return new SchemaGenerator(config);
-    }
-
-    private List<TraceParameterValue> buildInputParameters(ITracerObject tto,
-                                                            ObjectMapper objectMapper,
-                                                            SchemaGenerator schemaGenerator) {
-        ATableTracerNode tracerNode = getTableTracerNode(tto);
-        if (tracerNode == null || tracerNode.getTraceObject() == null) {
-            return Collections.emptyList();
-        }
-
-        ExecutableRulesMethod method = tracerNode.getTraceObject();
-        Object[] params = tracerNode.getParameters();
-
-        List<TraceParameterValue> result = new ArrayList<>();
-        for (int i = 0; i < params.length; i++) {
-            var param = new ParameterWithValueDeclaration(
-                    method.getSignature().getParameterName(i),
-                    params[i],
-                    method.getSignature().getParameterType(i)
-            );
-            // Parameters: lazy=true for complex types
-            result.add(buildParameterValue(param, objectMapper, schemaGenerator, parameterRegistry, true));
-        }
-        return result;
-    }
-
-    private TraceParameterValue buildContext(ITracerObject tto,
-                                              ObjectMapper objectMapper,
-                                              SchemaGenerator schemaGenerator) {
-        ATableTracerNode tracerNode = getTableTracerNode(tto);
-        if (tracerNode == null || tracerNode.getContext() == null) {
-            return null;
-        }
-        var param = new ParameterWithValueDeclaration("context", tracerNode.getContext());
-        // Context: always eager (lazy=false)
-        return buildParameterValue(param, objectMapper, schemaGenerator, null, false);
-    }
-
-    private TraceParameterValue buildResult(ITracerObject tto,
-                                             ObjectMapper objectMapper,
-                                             SchemaGenerator schemaGenerator) {
-        Object resultValue = tto.getResult();
-        if (resultValue == null) {
-            return null;
-        }
-        var param = new ParameterWithValueDeclaration("return", resultValue);
-        // Result: lazy=true for complex types
-        return buildParameterValue(param, objectMapper, schemaGenerator, parameterRegistry, true);
-    }
-
-    private TraceParameterValue buildParameterValue(ParameterWithValueDeclaration param,
-                                                     ObjectMapper objectMapper,
-                                                     SchemaGenerator schemaGenerator,
-                                                     TraceParameterRegistry registry,
-                                                     boolean preferLazy) {
-        if (param == null) {
-            return null;
-        }
-
-        String name = param.getName();
-        Object rawValue = param.getValue();
-        IOpenClass type = param.getType();
-
-        // Generate schema for UI tree building
-        ObjectNode schema = null;
-        if (type != null && type.getInstanceClass() != null) {
-            try {
-                schema = schemaGenerator.generateSchema(type.getInstanceClass());
-            } catch (Exception ignored) {
-                // Schema generation may fail for some types
-            }
-        }
-
-        // Determine if value should be lazy loaded
-        boolean isSimple = type != null && type.isSimple();
-        boolean shouldBeLazy = preferLazy && rawValue != null && !isSimple;
-
-        if (shouldBeLazy && registry != null) {
-            // Lazy: register for later resolution, don't include value
-            int id = registry.register(param);
-            String typeName = type != null ? type.getDisplayName(INamedThing.SHORT) : null;
-            return TraceParameterValue.builder()
-                    .name(name)
-                    .description(typeName)
-                    .lazy(true)
-                    .parameterId(id)
-                    .schema(schema)
-                    .build();
-        } else {
-            // Eager: include value in response
-            JsonNode value = null;
-            if (rawValue != null) {
-                try {
-                    value = objectMapper.valueToTree(rawValue);
-                } catch (Exception ignored) {
-                    // Value serialization may fail for some types
-                }
-            }
-            String typeName = type != null ? type.getDisplayName(INamedThing.SHORT) : null;
-            return TraceParameterValue.builder()
-                    .name(name)
-                    .description(typeName)
-                    .lazy(false)
-                    .value(value)
-                    .schema(schema)
-                    .build();
-        }
-    }
-
-    private ATableTracerNode getTableTracerNode(ITracerObject tto) {
-        if (tto instanceof RefToTracerNodeObject refNode) {
-            return getTableTracerNode(refNode.getOriginalTracerNode());
-        } else if (tto instanceof ATableTracerNode tableNode) {
-            return tableNode;
-        } else if (tto != null && tto.getParent() instanceof ATableTracerNode tableNode) {
-            return tableNode;
-        }
-        return null;
     }
 
     private Object[] parseInputParams(TraceInputRequest request, IOpenMethod method) {
