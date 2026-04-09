10465ed19ae7ba6d32a18c7f34e1c4aa0f9afa0d
Vladyslav Pikus <vpikus@eisgroup.com>
2026-04-09 14:48:10 +0300

    EPBDS-15592 Use lombok

--- STUDIO/org.openl.security.standalone/src/org/openl/rules/security/standalone/persistence/UserGroupId.java
+++ STUDIO/org.openl.security.standalone/src/org/openl/rules/security/standalone/persistence/UserGroupId.java
@@ -1,54 +1,22 @@
 package org.openl.rules.security.standalone.persistence;
 
 import java.io.Serializable;
-import java.util.Objects;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Embeddable
 public class UserGroupId implements Serializable {
-    private String loginName;
-    private Long groupId;
-
-    public UserGroupId() {
-    }
-
-    public UserGroupId(String loginName, Long groupId) {
-        this.loginName = loginName;
-        this.groupId = groupId;
-    }
 
     @Column(name = "loginName", nullable = false, length = 50)
-    public String getLoginName() {
-        return loginName;
-    }
-
-    public void setLoginName(String loginName) {
-        this.loginName = loginName;
-    }
+    private String loginName;
 
     @Column(name = "groupID", nullable = false)
-    public Long getGroupId() {
-        return groupId;
-    }
-
-    public void setGroupId(Long groupId) {
-        this.groupId = groupId;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof UserGroupId that)) {
-            return false;
-        }
-        return Objects.equals(loginName, that.loginName) && Objects.equals(groupId, that.groupId);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(loginName, groupId);
-    }
+    private Long groupId;
 }
