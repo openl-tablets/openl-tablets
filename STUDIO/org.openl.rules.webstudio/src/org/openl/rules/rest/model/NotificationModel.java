699a56822859986264368e31b27c629df3d7e2e6
Yury Molchan <ymolchan@eisgroup.com>
2026-07-24 00:52:41 +0200

    Remove unused constructors and setters in NotificationModel

--- STUDIO/org.openl.rules.webstudio/src/org/openl/rules/rest/model/NotificationModel.java
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/rest/model/NotificationModel.java
@@ -15,43 +15,19 @@ public class NotificationModel {
     @Parameter(description = "Message arguments")
     private List<Object> args;
 
-    public NotificationModel(String message, String code, List<Object> args) {
-        this.message = message;
-        this.code = code;
-        this.args = args;
-    }
-
     public NotificationModel(String message) {
         this.message = message;
-    }
-
-    public NotificationModel() {
     }
 
     public String getMessage() {
         return message;
     }
 
-    public NotificationModel setMessage(String message) {
-        this.message = message;
-        return this;
-    }
-
     public String getCode() {
         return code;
     }
 
-    public NotificationModel setCode(String code) {
-        this.code = code;
-        return this;
-    }
-
     public List<Object> getArgs() {
         return args;
-    }
-
-    public NotificationModel setArgs(List<Object> args) {
-        this.args = args;
-        return this;
     }
 }
