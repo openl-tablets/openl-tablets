d2bc6c6b2c10130d62e39e6018710c90ecf4f51d
Yury Molchan <ymolchan@eisgroup.com>
2026-07-23 22:44:30 +0200

    Remove unused getAllRegions in AWSS3RepositorySettings

--- STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/web/admin/AWSS3RepositorySettings.java
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/web/admin/AWSS3RepositorySettings.java
@@ -7,7 +7,6 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonView;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -123,11 +122,6 @@ public class AWSS3RepositorySettings extends RepositorySettings {
 
     public void setRegionName(String regionName) {
         this.regionName = regionName;
-    }
-
-    @JsonIgnore
-    public List<Region> getAllRegions() {
-        return Region.regions();
     }
 
     public List<AWSS3Region> getAllAllowedRegions() {
