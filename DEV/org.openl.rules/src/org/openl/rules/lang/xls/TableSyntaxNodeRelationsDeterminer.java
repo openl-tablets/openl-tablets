0b691d528f9809eb028b7f4c046a9b54d92cbc97
Yury Molchan <yury.molchan@gmail.com>
2026-07-26 00:35:12 +0000

    Remove TableSyntaxNodeRelationsDeterminer, an interface with no implementation

--- DEV/org.openl.rules/src/org/openl/rules/lang/xls/TableSyntaxNodeRelationsDeterminer.java
+++
@@ -1,7 +0,0 @@
-package org.openl.rules.lang.xls;
-
-import org.openl.rules.lang.xls.syntax.TableSyntaxNode;
-
-interface TableSyntaxNodeRelationsDeterminer {
-    boolean determine(TableSyntaxNode node, TableSyntaxNode dependsOnNode);
-}
