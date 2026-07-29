f950d46d5f73b90829ca01a21c5419f1350da209
Vladyslav Pikus <vpikus@eisgroup.com>
2026-07-29 17:42:14 +0300

    EPBDS-16292 Generate SpreadsheetRangeField index accessors with Lombok

--- DEV/org.openl.rules/src/org/openl/rules/calc/element/SpreadsheetRangeField.java
+++ DEV/org.openl.rules/src/org/openl/rules/calc/element/SpreadsheetRangeField.java
@@ -3,6 +3,8 @@ package org.openl.rules.calc.element;
 import java.lang.reflect.Array;
 import java.util.Objects;
 
+import lombok.Getter;
+
 import org.openl.binding.impl.NodeDescriptionHolder;
 import org.openl.binding.impl.cast.IOpenCast;
 import org.openl.rules.calc.ASpreadsheetField;
@@ -12,29 +14,37 @@ import org.openl.vm.IRuntimeEnv;
 
 public class SpreadsheetRangeField extends ASpreadsheetField implements NodeDescriptionHolder {
 
-    private final int sx;
-    private final int sy;
-    private final int ex;
-    private final int ey;
+    /** First column index of the range within the spreadsheet. */
+    @Getter
+    private final int startColumnIndex;
+    /** First row index of the range within the spreadsheet. */
+    @Getter
+    private final int startRowIndex;
+    /** Last column index of the range within the spreadsheet, inclusive. */
+    @Getter
+    private final int endColumnIndex;
+    /** Last row index of the range within the spreadsheet, inclusive. */
+    @Getter
+    private final int endRowIndex;
     private final IOpenCast[][] casts;
     private final Class<?> rangeType;
     private final String rangeName;
 
     public SpreadsheetRangeField(String name,
                                  String rangeName,
-                                 int sx,
-                                 int sy,
-                                 int ex,
-                                 int ey,
+                                 int startColumnIndex,
+                                 int startRowIndex,
+                                 int endColumnIndex,
+                                 int endRowIndex,
                                  IOpenClass rangeType,
                                  IOpenCast[][] casts,
                                  IOpenClass declaringClass) {
         super(declaringClass, name, rangeType.getArrayType(1));
         this.rangeName = Objects.requireNonNull(rangeName, "rangeName cannot be null");
-        this.sx = sx;
-        this.sy = sy;
-        this.ex = ex;
-        this.ey = ey;
+        this.startColumnIndex = startColumnIndex;
+        this.startRowIndex = startRowIndex;
+        this.endColumnIndex = endColumnIndex;
+        this.endRowIndex = endRowIndex;
         this.casts = casts;
         this.rangeType = rangeType.getInstanceClass();
     }
@@ -45,18 +55,18 @@ public class SpreadsheetRangeField extends ASpreadsheetField implements NodeDesc
             return getType().nullObject();
         }
 
-        var w = ex - sx + 1;
-        var h = ey - sy + 1;
+        var w = endColumnIndex - startColumnIndex + 1;
+        var h = endRowIndex - startRowIndex + 1;
 
         var size = w * h;
 
         var calc = (SpreadsheetResultCalculator) target;
         Object array = Array.newInstance(rangeType, size);
         var i = 0;
-        for (var x = sx; x <= ex; ++x) {
-            for (var y = sy; y <= ey; ++y) {
+        for (var x = startColumnIndex; x <= endColumnIndex; ++x) {
+            for (var y = startRowIndex; y <= endRowIndex; ++y) {
                 var v = calc.getValue(y, x);
-                var openCast = casts[x - sx][y - sy];
+                var openCast = casts[x - startColumnIndex][y - startRowIndex];
                 if (openCast != null && openCast.isImplicit()) {
                     v = openCast.convert(v);
                 }
@@ -71,25 +81,5 @@ public class SpreadsheetRangeField extends ASpreadsheetField implements NodeDesc
     @Override
     public String getDescription() {
         return getType().getDisplayName(SHORT) + " " + rangeName;
-    }
-
-    /** First column index of the range within the spreadsheet. */
-    public int getStartColumnIndex() {
-        return sx;
-    }
-
-    /** First row index of the range within the spreadsheet. */
-    public int getStartRowIndex() {
-        return sy;
-    }
-
-    /** Last column index of the range within the spreadsheet, inclusive. */
-    public int getEndColumnIndex() {
-        return ex;
-    }
-
-    /** Last row index of the range within the spreadsheet, inclusive. */
-    public int getEndRowIndex() {
-        return ey;
     }
 }
