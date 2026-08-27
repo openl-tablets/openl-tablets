634c605692988323dd155707041dfdf083f5b954
Vladyslav Pikus <vpikus@eisgroup.com>
2026-08-27 10:03:20 +0300

    EPBDS-16251 Restore local project integration coverage

--- ITEST/itest.studio/simple/test/org/openl/itest/WebStudioTest.java
+++ ITEST/itest.studio/simple/test/org/openl/itest/WebStudioTest.java
@@ -1,5 +1,8 @@
 package org.openl.itest;
 
+import java.io.IOException;
+import java.nio.file.Path;
+
 import org.junit.jupiter.api.Test;
 
 import org.openl.itest.core.JettyServer;
@@ -8,8 +11,18 @@ class WebStudioTest {
 
     @Test
     void simple() throws Exception {
+        seedLocalProject();
         try (var client = JettyServer.get().start()) {
             client.test("test-resources-simple");
         }
+    }
+
+    private static void seedLocalProject() throws IOException {
+        var configured = System.getProperty("openl.home", "target/openl-test-${openl.start.milli}");
+        var home = Path.of(configured.replace("${openl.start.milli}", String.valueOf(System.currentTimeMillis())));
+        System.setProperty("openl.home", home.toString());
+        LocalProjectFixture.seed(home.resolve("user-workspace").resolve("jdoe"),
+                "EPBDS-15952-Project",
+                Path.of("test-resources-simple", "EPBDS-15952", "EPBDS-15952-Project.zip"));
     }
 }
