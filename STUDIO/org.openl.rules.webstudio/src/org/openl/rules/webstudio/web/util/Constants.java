a3890e4f7a6cc4162f4007efcaba008038f457bb
Yury Molchan <yury.molchan@gmail.com>
2026-09-10 21:00:39 +0000

    Drop the request parameter constants left without a reader

--- STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/web/util/Constants.java
+++ STUDIO/org.openl.rules.webstudio/src/org/openl/rules/webstudio/web/util/Constants.java
@@ -3,9 +3,6 @@ package org.openl.rules.webstudio.web.util;
 public final class Constants {
 
     public static final String REQUEST_PARAM_ID = "id";
-    public static final String REQUEST_PARAM_CURRENT_OPENED_MODULE = "currentOpenedModule";
-
-    public static final String REQUEST_PARAM_TEST_RANGES = "testRanges";
 
     public static final String SESSION_PARAM_PARAMETERS = "parameters";
 
