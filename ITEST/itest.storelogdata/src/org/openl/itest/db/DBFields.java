2e44f112bfda94b8007d43ec6015797bb5c115b8
Yury Molchan <yury.molchan@gmail.com>
2026-09-22 17:48:01 +0000

    Hold the store-log-data field names in a final class instead of an interface

--- ITEST/itest.storelogdata/src/org/openl/itest/db/DBFields.java
+++ ITEST/itest.storelogdata/src/org/openl/itest/db/DBFields.java
@@ -1,28 +1,33 @@
 package org.openl.itest.db;
 
-public interface DBFields {
-    String ID = "id";
-    String REQUEST = "request";
-    String METHOD_NAME = "methodName";
-    String SERVICE_NAME = "serviceName";
-    String PUBLISHER_TYPE = "publisherType";
-    String RESPONSE = "response";
-    String INCOMING_TIME = "incomingTime";
-    String OUTCOMING_TIME = "outcomingTime";
-    String URL = "url";
-    String VALUE = "value1";
-    String HOUR = "hour1";
-    String RESULT = "result";
-    String AWARE_INSTANCES_FOUND = "awareInstancesFound";
-    String DB_CONNECTION_FOUND = "dbConnectionFound";
-    String INT_VALUE1 = "intValue1";
-    String INT_VALUE2 = "intValue2";
-    String INT_VALUE3 = "intValue3";
-    String STRING_VALUE1 = "stringValue1";
-    String STRING_VALUE2 = "stringValue2";
-    String STRING_VALUE3 = "stringValue3";
-    String BOOL_VALUE1 = "boolValue1";
-    String INT_VALUE_TO_STRING = "intValueToString";
-    String HEADER1 = "header1";
-    String HEADER2 = "header2";
+public final class DBFields {
+
+    private DBFields() {
+        // Constants only
+    }
+
+    public static final String ID = "id";
+    public static final String REQUEST = "request";
+    public static final String METHOD_NAME = "methodName";
+    public static final String SERVICE_NAME = "serviceName";
+    public static final String PUBLISHER_TYPE = "publisherType";
+    public static final String RESPONSE = "response";
+    public static final String INCOMING_TIME = "incomingTime";
+    public static final String OUTCOMING_TIME = "outcomingTime";
+    public static final String URL = "url";
+    public static final String VALUE = "value1";
+    public static final String HOUR = "hour1";
+    public static final String RESULT = "result";
+    public static final String AWARE_INSTANCES_FOUND = "awareInstancesFound";
+    public static final String DB_CONNECTION_FOUND = "dbConnectionFound";
+    public static final String INT_VALUE1 = "intValue1";
+    public static final String INT_VALUE2 = "intValue2";
+    public static final String INT_VALUE3 = "intValue3";
+    public static final String STRING_VALUE1 = "stringValue1";
+    public static final String STRING_VALUE2 = "stringValue2";
+    public static final String STRING_VALUE3 = "stringValue3";
+    public static final String BOOL_VALUE1 = "boolValue1";
+    public static final String INT_VALUE_TO_STRING = "intValueToString";
+    public static final String HEADER1 = "header1";
+    public static final String HEADER2 = "header2";
 }
